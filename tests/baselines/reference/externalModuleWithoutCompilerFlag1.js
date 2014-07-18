81d300112ecf0d06580e72fc2e2018a213464429
Jason Freeman <jfreeman@microsoft.com>
2014-07-18 14:06:32 -0700

    Update baselines for not emitting when module flag is not specified

--- tests/baselines/reference/externalModuleWithoutCompilerFlag1.js
+++
@@ -1,7 +0,0 @@
-//// [externalModuleWithoutCompilerFlag1.ts]
-
-// Not on line 0 because we want to verify the error is placed in the appropriate location.
-  export module M {
-}
-
-//// [externalModuleWithoutCompilerFlag1.js]
--- tests/baselines/reference/genericArrayExtenstions.js
+++
@@ -1,8 +0,0 @@
-//// [genericArrayExtenstions.ts]
-export declare class ObservableArray<T> implements Array<T> { // MS.Entertainment.ObservableArray
-concat<U extends T[]>(...items: U[]): T[];
-concat(...items: T[]): T[];
-}
-
-
-//// [genericArrayExtenstions.js]
