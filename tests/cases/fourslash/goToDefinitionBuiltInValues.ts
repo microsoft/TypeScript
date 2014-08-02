8956c4947c71b7919a8437949a6de47e3ee43ebe
Mohamed Hegazy <mhegazy@microsoft.com>
2014-08-01 17:15:25 -0700

    update test to add true and false values

--- tests/cases/fourslash/goToDefinitionBuiltInValues.ts
+++ tests/cases/fourslash/goToDefinitionBuiltInValues.ts
@@ -3,6 +3,8 @@
 ////var u = /*undefined*/undefined;
 ////var n = /*null*/null;
 ////var a = function() { return /*arguments*/arguments; };
+////var t = /*true*/true;
+////var f = /*false*/false;
 
 test.markers().forEach((m, i, a) => {
     goTo.position(m.position, m.fileName);
