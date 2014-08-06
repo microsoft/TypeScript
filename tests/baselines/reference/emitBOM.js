3295cdaa19353d2cd339ad802902cd4ed8a3ca6a
Mohamed Hegazy <mhegazy@microsoft.com>
2014-08-06 14:33:06 -0700

    rename test

--- tests/baselines/reference/emitBOM.js
+++ tests/baselines/reference/emitBOM.js
@@ -1,11 +1,11 @@
-//// [generateBOM.ts]
+//// [emitBOM.ts]
 
 // JS and d.ts output should have a BOM but not the sourcemap
 var x;
 
-//// [generateBOM.js]
+//// [emitBOM.js]
 ï»¿var x;
-//# sourceMappingURL=generateBOM.js.map
+//# sourceMappingURL=emitBOM.js.map
 
-//// [generateBOM.d.ts]
+//// [emitBOM.d.ts]
 ï»¿declare var x: any;
