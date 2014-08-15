48c3abd445223bda0ab83ccd5766918c9384bd12
Sheetal Nandi <shkamat@microsoft.com>
2014-08-15 13:30:26 -0700

    Test cases for trailing comments of object literal

---
+++ tests/baselines/reference/commentsOnObjectLiteral4.js
@@ -0,0 +1,20 @@
+//// [commentsOnObjectLiteral4.ts]
+
+var v = {
+ /**
+  * @type {number}
+  */
+ get bar(): number {
+  return this._bar;
+ }
+}
+
+//// [commentsOnObjectLiteral4.js]
+var v = {
+    /**
+  * @type {number}
+  */
+    get bar() {
+        return this._bar;
+    }
+};
