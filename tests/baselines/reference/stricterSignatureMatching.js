1d91fa82b8a4635bdd10ecc6d0d515c365906947
Ryan Cavanaugh <ryan.cavanaugh@microsoft.com>
2014-09-04 12:44:33 -0700

    Discard fewer-parameter candidate overloads when performing signature assignability checks

---
+++ tests/baselines/reference/stricterSignatureMatching.js
@@ -0,0 +1,36 @@
+//// [stricterSignatureMatching.ts]
+interface koElement1 {
+    // Getter
+    (): string;
+    (value: string): void;
+}
+
+
+function invoke(fn: (c: number) => void) {
+    fn(4);
+}
+
+var x: koElement1;
+// Desired: error
+invoke(x);
+
+
+interface koElement2 {
+    (): string;
+    (value: string): void;
+    (value: number): void;
+}
+
+var y: koElement2;
+invoke(y); // Should be OK
+
+
+//// [stricterSignatureMatching.js]
+function invoke(fn) {
+    fn(4);
+}
+var x;
+// Desired: error
+invoke(x);
+var y;
+invoke(y); // Should be OK
