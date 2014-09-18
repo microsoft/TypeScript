b08cd8c9a93a224cc4e820f1999a7c4c84628772
Yui T <yuisu@microsoft.com>
2014-09-18 12:42:44 -0700

    Add fourslash testcases

---
+++ tests/cases/fourslash/getSemanticDiagnosticForNoDeclaration.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="fourslash.ts" />
+
+//// interface privateInterface {}
+//// export class Bar implements /*1*/privateInterface/*2*/{ }
+
+debugger
+verify.numberOfErrorsInCurrentFile(0);
+
+
