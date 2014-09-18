b285ef165bfe6f16994e610bb2dbde6ca0180c60
Daniel Rosenwasser <drosen@microsoft.com>
2014-09-17 17:40:32 -0700

    Added fourslash tests.

---
+++ tests/cases/fourslash/scriptLexicalStructureFunctions.ts
@@ -0,0 +1,23 @@
+/// <reference path="fourslash.ts"/>
+
+////{| "itemName": "<global>", "kind": "module" |}
+////
+////{| "itemName": "foo", "kind": "function" |}function foo() {
+////    var x = 10;
+////    {| "itemName": "bar", "kind": "function", "parentName": "foo" |}function bar() {
+////        var y = 10;
+////        {| "itemName": "biz", "kind": "function", "parentName": "bar" |}function biz() {
+////            var z = 10;
+////        }
+////    }
+////}
+////
+////{| "itemName": "baz", "kind": "function" |}function baz() {
+////    var v = 10;
+////}
+
+test.markers().forEach((marker) => {
+    verify.getScriptLexicalStructureListContains(marker.data.itemName, marker.data.kind, marker.fileName, marker.data.parentName);
+});
+
+verify.getScriptLexicalStructureListCount(5); // 4 functions + global
---
+++ tests/cases/fourslash/scriptLexicalStructureModules.ts
@@ -0,0 +1,44 @@
+﻿
+////{| "itemName": "\"X.Y.Z\"", "kind": "module" |}
+////declare module "X.Y.Z" {
+////}
+////
+////{| "itemName": "A.B.C", "kind": "module" |}
+////module A.B.C {
+////    {| "itemName": "x", "kind": "var", "parent": "A.B.C" |}
+////    export var x;
+////}
+////
+////{| "itemName": "A.B", "kind": "module" |}
+////module A.B {
+////    {| "itemName": "y", "kind": "var", "parent": "A.B" |}
+////    export var y;
+////}
+////
+////{| "itemName": "A", "kind": "module" |}
+////module A {
+////    {| "itemName": "z", "kind": "var", "parent": "A" |}
+////    export var z;
+////}
+////
+////{| "itemName": "A", "kind": "module" |}
+////module A {
+////    {| "itemName": "B", "kind": "module", "parent": "E" |}
+////    module B {
+////        {| "itemName": "C", "kind": "module", "parent": "F" |}
+////        module C {
+////            {| "itemName": "x", "kind": "var", "parent": "C" |}
+////            declare var x;
+////        }
+////    }
+////}
+
+
+test.markers().forEach((marker) => {
+    verify.getScriptLexicalStructureListContains(marker.data.itemName, marker.data.kind, marker.fileName, marker.data.parentName);
+});
+
+/// We have 7 module keywords, and 4 var keywords.
+/// The declarations of A.B.C.x do not get merged, so the 4 vars are independent.
+/// The two 'A' modules, however, do get merged, so in reality we have 6 modules.
+verify.getScriptLexicalStructureListCount(10);
