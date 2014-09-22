bfde7ece13ecc15348c2ea20e0d6624d1249554e
Jason Freeman <jfreeman@microsoft.com>
2014-09-22 14:49:33 -0700

    Add test for nested calls

--- src/services/signatureHelp.ts
+++ src/services/signatureHelp.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the Apache License, Version 2.0. 
 // See LICENSE.txt in the project root for complete license information.
 
-///<reference path='references.ts' />
+///<reference path='services.ts' />
 
 module ts.SignatureHelp {
 
