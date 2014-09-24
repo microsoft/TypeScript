684de531430336671b2e3bdc8ba40cb4cfba076e
Yui T <yuisu@microsoft.com>
2014-09-24 14:51:23 -0700

    Address code review : add test case for inside ambient module declaration and single quote

--- tests/cases/fourslash/completionListWithAmbientDeclaration.ts
+++ tests/cases/fourslash/completionListWithAmbientDeclaration.ts
@@ -2,8 +2,14 @@
 
 //// declare module "http" {
 ////    var x;
+////    /*1*/
 //// }
-//// /**/
+//// declare module 'https' {
+//// }
+//// /*2*/
 
-goTo.marker();
+goTo.marker("1");
 verify.not.completionListContains("http");
+goTo.marker("2");
+verify.not.completionListContains("http");
+verify.not.completionListContains("https");
