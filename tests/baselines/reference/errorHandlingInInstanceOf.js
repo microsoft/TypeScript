649f5c01a4d5f13665b992aeb52a0ef53ef36a13
Vladimir Matveev <vladima@microsoft.com>
2014-09-24 09:10:40 -0700

    do not report cascading errors in instanceof operator

---
+++ tests/baselines/reference/errorHandlingInInstanceOf.js
@@ -0,0 +1,14 @@
+//// [errorHandlingInInstanceOf.ts]
+if (x instanceof String) {
+}
+
+var y: any;
+if (y instanceof UnknownType) {
+}
+
+//// [errorHandlingInInstanceOf.js]
+if (x instanceof String) {
+}
+var y;
+if (y instanceof UnknownType) {
+}
