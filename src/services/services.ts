a3b59f88c1edd268cf5d810cfa627818c4a144f2
Daniel Rosenwasser <drosen@microsoft.com>
2014-09-25 22:28:02 -0700

    Fix issue where trivia is incorrectly identified as an identifier following a dot.
    Fixes #753

--- src/services/services.ts
+++ src/services/services.ts
@@ -4550,17 +4550,16 @@ module ts {
             do {
                 token = scanner.scan();
 
-                if ((token === SyntaxKind.SlashToken || token === SyntaxKind.SlashEqualsToken) && !noRegexTable[lastNonTriviaToken]) {
-                    if (scanner.reScanSlashToken() === SyntaxKind.RegularExpressionLiteral) {
-                        token = SyntaxKind.RegularExpressionLiteral;
+                if (!isTrivia(token)) {
+                    if ((token === SyntaxKind.SlashToken || token === SyntaxKind.SlashEqualsToken) && !noRegexTable[lastNonTriviaToken]) {
+                        if (scanner.reScanSlashToken() === SyntaxKind.RegularExpressionLiteral) {
+                            token = SyntaxKind.RegularExpressionLiteral;
+                        }
                     }
-                }
-                else if (lastNonTriviaToken === SyntaxKind.DotToken) {
-                    token = SyntaxKind.Identifier;
-                }
+                    else if (lastNonTriviaToken === SyntaxKind.DotToken) {
+                        token = SyntaxKind.Identifier;
+                    }
 
-                // Only recall the token if it was *not* trivia.
-                if (!(SyntaxKind.FirstTriviaToken <= token && token <= SyntaxKind.LastTriviaToken)) {
                     lastNonTriviaToken = token;
                 }
 
@@ -4611,6 +4610,10 @@ module ts {
 
                     result.entries.push({ length: length, classification: classification });
                 }
+            }
+
+            function isTrivia(token: SyntaxKind) {
+                return SyntaxKind.FirstTriviaToken <= token && token <= SyntaxKind.LastTriviaToken;
             }
         }
 
