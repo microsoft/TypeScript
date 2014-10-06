3a6c328f00dca3b957e0565907e0bebb97b98620
Cyrus Najmabadi <cyrusn@microsoft.com>
2014-10-05 21:58:37 -0700

    Update tests.

--- tests/cases/fourslash/scriptLexicalStructureFunctionsBroken.ts
+++ tests/cases/fourslash/scriptLexicalStructureFunctionsBroken.ts
@@ -9,4 +9,4 @@ test.markers().forEach((marker) => {
     verify.getScriptLexicalStructureListContains(marker.data.itemName, marker.data.kind, marker.fileName, marker.data.parentName);
 });
 
-verify.getScriptLexicalStructureListCount(3); // <global>, 'f' in the top level list, and 'f' as a child of '<global>'
+verify.getScriptLexicalStructureListCount(2); // <global> and 'f'.
--- tests/cases/fourslash/scriptLexicalStructureFunctionsBroken2.ts
+++ tests/cases/fourslash/scriptLexicalStructureFunctionsBroken2.ts
@@ -10,4 +10,4 @@ test.markers().forEach((marker) => {
     verify.getScriptLexicalStructureListContains(marker.data.itemName, marker.data.kind, marker.fileName, marker.data.parentName);
 });
 
-verify.getScriptLexicalStructureListCount(3); // <global> and 'f' as a child of 'global' and as as top level function.
+verify.getScriptLexicalStructureListCount(2); // <global> and 'f'
