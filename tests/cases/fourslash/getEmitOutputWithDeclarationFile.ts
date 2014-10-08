48c3bc800c21411c3e66c18225f65430e7290a46
Yui T <yuisu@microsoft.com>
2014-10-08 14:43:46 -0700

    Add test file

---
+++ tests/cases/fourslash/getEmitOutputWithDeclarationFile.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="fourslash.ts" />
+
+// @BaselineFile: getEmitOutputWithDeclarationFile.baseline
+
+// @Filename: decl.d.ts
+// @emitThisFile: true
+//// declare x: string;
+//// declare class Bar {
+////    x : string;
+////    y : number
+//// }
+
+// @Filename: inputFile2.ts
+// @emitThisFile: true
+//// var x1: string = "hello world";
+//// class Foo{
+////    x : string;
+////    y : number;
+//// }
+
+debugger;
+verify.baselineGetEmitOutput();
