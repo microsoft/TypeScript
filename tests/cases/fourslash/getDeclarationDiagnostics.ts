75949578ff56622ed841999425dab60044016f02
Yui T <yuisu@microsoft.com>
2014-10-13 14:25:12 -0700

    Add test case to make sure that we only report an error from target file

---
+++ tests/cases/fourslash/getDeclarationDiagnostics.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="fourslash.ts" />
+
+// @declaration: true
+// @out: true
+
+// @Filename: inputFile1.ts
+//// module m {
+////    export class C implements I { }
+////    interface I { }
+//// } /*1*/
+
+// @Filename: input2.ts
+//// var x = "hello world"; /*2*/
+
+debugger;
+goTo.marker("1");
+verify.numberOfErrorsInCurrentFile(1);
+
+goTo.marker("2");
+verify.numberOfErrorsInCurrentFile(0);
