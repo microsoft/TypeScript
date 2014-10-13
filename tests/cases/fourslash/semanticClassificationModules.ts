014d0d7143404e2c6ef818e997ae5f75788b8f81
Daniel Rosenwasser <drosen@microsoft.com>
2014-10-13 14:32:04 -0700

    Added test case, renamed test cases.

---
+++ tests/cases/fourslash/semanticClassificationModules.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="fourslash.ts"/>
+
+////module M {
+////    export var v;
+////    export interface I {
+////    }
+////}
+////
+////var x: M.I = M.v;
+////var y = M;
+
+var c = classification;
+verify.semanticClassificationsAre(c.moduleName("M"), c.interfaceName("I"), c.moduleName("M"), c.interfaceName("I"), c.moduleName("M"), c.moduleName("M"));
