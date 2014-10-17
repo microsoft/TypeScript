c00ee42b1182cf89da8c4513cbc74a9cb92f34c2
Anders Hejlsberg <andersh@microsoft.com>
2014-10-16 20:52:36 -0700

    Adding tests

---
+++ tests/cases/conformance/types/typeRelationships/typeInference/contextualSignatureInstantiation.ts
@@ -0,0 +1,22 @@
+// TypeScript Spec, section 4.12.2:
+// If e is an expression of a function type that contains exactly one generic call signature and no other members,
+// and T is a function type with exactly one non - generic call signature and no other members, then any inferences
+// made for type parameters referenced by the parameters of T's call signature are fixed, and e's type is changed
+// to a function type with e's call signature instantiated in the context of T’s call signature (section 3.8.5).
+
+declare function foo<T>(cb: (x: number, y: string) => T): T;
+declare function bar<T, U, V>(x: T, y: U, cb: (x: T, y: U) => V): V;
+
+declare function f(x: number, y: string): boolean;
+declare function g<T>(x: T, y: T): T;
+
+var a: boolean;
+var a = foo(f);  // Should be boolean
+
+var b: number | string;
+var b = foo(g);            // Should be number | string
+var b = bar(1, "one", g);  // Should be number | string
+var b = bar("one", 1, g);  // Should be number | string
+
+var c: number;
+var c = bar(1, 1, g);      // Should be number
