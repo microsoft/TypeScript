edb6543354fdb5efeb45d4e9e6ebc148fc121490
Anders Hejlsberg <andersh@microsoft.com>
2014-11-02 15:21:10 -0800

    Accepting new baselines

---
+++ tests/baselines/reference/unionTypeInference.js
@@ -0,0 +1,42 @@
+//// [unionTypeInference.ts]
+// Verify that inferences made *to* a type parameter in a union type are secondary
+// to inferences made directly to that type parameter
+
+function f<T>(x: T, y: string|T): T {
+    return x;
+}
+function g<T>(value: [string, T]): T {
+    return value[1];
+}
+
+var a: number;
+var a = f(1, 2);
+var b: number;
+var b = f(1, "hello");
+var c: number;
+var c = f(1, a || "hello");
+var d: any;
+var d = f(undefined, "abc");
+var e: boolean;
+var e = g(["string", true]);
+
+
+//// [unionTypeInference.js]
+// Verify that inferences made *to* a type parameter in a union type are secondary
+// to inferences made directly to that type parameter
+function f(x, y) {
+    return x;
+}
+function g(value) {
+    return value[1];
+}
+var a;
+var a = f(1, 2);
+var b;
+var b = f(1, "hello");
+var c;
+var c = f(1, a || "hello");
+var d;
+var d = f(undefined, "abc");
+var e;
+var e = g(["string", true]);
