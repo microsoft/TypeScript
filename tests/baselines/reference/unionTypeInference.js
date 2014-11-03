4c4484be05221ea2ede81327c0c238834d4bcc94
Anders Hejlsberg <andersh@microsoft.com>
2014-11-03 14:29:23 -0800

    Accepting new baselines

--- tests/baselines/reference/unionTypeInference.js
+++ tests/baselines/reference/unionTypeInference.js
@@ -5,20 +5,31 @@
 function f<T>(x: T, y: string|T): T {
     return x;
 }
+
+var a1: number;
+var a1 = f(1, 2);
+var a2: number;
+var a2 = f(1, "hello");
+var a3: number;
+var a3 = f(1, a1 || "hello");
+var a4: any;
+var a4 = f(undefined, "abc");
+
 function g<T>(value: [string, T]): T {
     return value[1];
 }
 
-var a: number;
-var a = f(1, 2);
-var b: number;
-var b = f(1, "hello");
-var c: number;
-var c = f(1, a || "hello");
-var d: any;
-var d = f(undefined, "abc");
-var e: boolean;
-var e = g(["string", true]);
+var b1: boolean;
+var b1 = g(["string", true]);
+
+function h<T>(x: string|boolean|T): T {
+    return typeof x === "string" || typeof x === "boolean" ? undefined : x;
+}
+
+var c1: number;
+var c1 = h(5);
+var c2: string;
+var c2 = h("abc");
 
 
 //// [unionTypeInference.js]
@@ -27,16 +38,23 @@ var e = g(["string", true]);
 function f(x, y) {
     return x;
 }
+var a1;
+var a1 = f(1, 2);
+var a2;
+var a2 = f(1, "hello");
+var a3;
+var a3 = f(1, a1 || "hello");
+var a4;
+var a4 = f(undefined, "abc");
 function g(value) {
     return value[1];
 }
-var a;
-var a = f(1, 2);
-var b;
-var b = f(1, "hello");
-var c;
-var c = f(1, a || "hello");
-var d;
-var d = f(undefined, "abc");
-var e;
-var e = g(["string", true]);
+var b1;
+var b1 = g(["string", true]);
+function h(x) {
+    return typeof x === "string" || typeof x === "boolean" ? undefined : x;
+}
+var c1;
+var c1 = h(5);
+var c2;
+var c2 = h("abc");
