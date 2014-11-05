5952036e2cb3ffef4447a272e9d48a83567a7f04
Cyrus Najmabadi <cyrusn@microsoft.com>
2014-11-04 21:13:58 -0800

    Remove unnecessary brand.

--- src/services/syntax/syntaxNodeOrToken.ts
+++ src/services/syntax/syntaxNodeOrToken.ts
@@ -2,8 +2,6 @@
 
 module TypeScript {
     export interface ISyntaxNodeOrToken extends ISyntaxElement {
-        _syntaxNodeOrTokenBrand: any;
-
         childCount: number;
         childAt(index: number): ISyntaxElement;
     }
