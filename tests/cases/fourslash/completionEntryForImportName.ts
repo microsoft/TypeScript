3c600608d3ebbae13738ee6b7361f5ef52a734e5
Sheetal Nandi <shkamat@microsoft.com>
2014-11-07 12:32:08 -0800

    Test now covers completion entry at name of the import when there is no name and there is name

--- tests/cases/fourslash/completionEntryForImportName.ts
+++ tests/cases/fourslash/completionEntryForImportName.ts
@@ -1,13 +1,23 @@
 ///<reference path="fourslash.ts" />
 
-////import /*1*/q = /*2*/
+////import /*1*/ /*2*/
 
-verifyIncompleteImport();
+goTo.marker('1');
+verify.completionListIsEmpty();
+edit.insert('q');
+verify.completionListIsEmpty();
+verifyIncompleteImportName();
+
 goTo.marker('2');
-edit.insert("a.");
-verifyIncompleteImport();
+edit.insert(" = ");
+verifyIncompleteImportName();
 
-function verifyIncompleteImport() {
+goTo.marker("2");
+edit.moveRight(" = ".length);
+edit.insert("a.");
+verifyIncompleteImportName();
+
+function verifyIncompleteImportName() {
     goTo.marker('1');
     verify.completionListIsEmpty();
     verify.quickInfoIs("import q");
