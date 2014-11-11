57af2623c69e4be08ebe9b2e36f3fedfb169db0b
Sheetal Nandi <shkamat@microsoft.com>
2014-11-10 19:19:33 -0800

    If we are comparing left hand side for is position of type node, it is false, because it is namespace position and not type position Fixes #837

---
+++ tests/cases/fourslash/completionInAugmentedClassModule.ts
@@ -0,0 +1,9 @@
+/// <reference path='fourslash.ts'/>
+
+////declare class m3f { foo(x: number): void }
+////module m3f { export interface I { foo(): void } }
+////var x: m3f./**/
+
+goTo.marker();
+debugger;
+verify.not.completionListContains("foo");
