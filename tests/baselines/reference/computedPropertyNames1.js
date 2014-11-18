0e864143deb02caaceed46331c8deb54726b0802
Jason Freeman <jfreeman@microsoft.com>
2014-11-17 16:36:30 -0800

    Emit tests for computed properties

---
+++ tests/baselines/reference/computedPropertyNames1.js
@@ -0,0 +1,14 @@
+//// [computedPropertyNames1.ts]
+var v = {
+    get [0 + 1]() { return 0 },
+    set [0 + 1](v) { }
+}
+
+//// [computedPropertyNames1.js]
+var v = {
+    get [0 + 1]() {
+        return 0;
+    },
+    set [0 + 1](v) {
+    }
+};
