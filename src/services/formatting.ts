1d78406bf8a3cc48356e043423bc8d7d9b7ce2a3
Yui T <yuisu@microsoft.com>
2014-11-18 14:08:42 -0800

    Refactoring language service to use short-hand

--- src/services/formatting.ts
+++ src/services/formatting.ts
@@ -361,8 +361,8 @@ module ts.formatting {
                 delta = Math.min(options.IndentSize, parentDynamicIndentation.getDelta() + delta);
             }
             return {
-                indentation: indentation,
-                delta: delta
+                indentation,
+                delta
             }
         }
 
@@ -834,7 +834,7 @@ module ts.formatting {
         }
 
         function newTextChange(start: number, len: number, newText: string): TextChange {
-            return { span: new TextSpan(start, len), newText: newText }
+            return { span: new TextSpan(start, len), newText }
         }
 
         function recordDelete(start: number, len: number) {
