4e18efd25cb1460c667a1282c3f45964410d456a
Daniel Rosenwasser <drosen@microsoft.com>
2014-11-18 13:48:24 -0800

    Tests for signature help on tagged templates with no overloads.

---
+++ tests/cases/fourslash/signatureHelpTaggedTemplates3.ts
@@ -0,0 +1,15 @@
+/// <reference path='fourslash.ts' />
+
+//// function f(templateStrings, x, y, z) { return 10; }
+//// function g(templateStrings, x, y, z) { return ""; }
+//// 
+//// f ` qwerty ${/*1*/ /*2*/123/*3*/ /*4*/} asdf ${   41234   }  zxcvb ${ g `    ` }    `
+
+test.markers().forEach(m => {
+    goTo.position(m.position);
+    verify.signatureHelpCountIs(1);
+    verify.currentSignatureParamterCountIs(4);
+    verify.currentSignatureHelpIs('f(templateStrings: any, x: any, y: any, z: any): number');
+    verify.currentParameterHelpArgumentNameIs("x");
+    verify.currentParameterSpanIs("x: any");
+});
---
+++ tests/cases/fourslash/signatureHelpTaggedTemplatesNested2.ts
@@ -0,0 +1,14 @@
+/// <reference path="./fourslash.ts"/>
+
+//// function f(templateStrings, x, y, z) { return 10; }
+//// function g(templateStrings, x, y, z) { return ""; }
+//// 
+//// f `/*1*/a $/*2*/{ /*3*/g /*4*/`alpha ${ 123 } beta ${ 456 } gamma`/*5*/ }/*6*/ b $/*7*/{ /*8*/g /*9*/`txt`/*10*/ } /*11*/c ${ /*12*/g /*13*/`aleph ${ 123 } beit`/*14*/ } d/*15*/`;
+debugger;
+
+test.markers().forEach(m => {
+    goTo.position(m.position);
+    verify.signatureHelpCountIs(1);
+    verify.currentSignatureParamterCountIs(4);
+    verify.currentSignatureHelpIs('f(templateStrings: any, x: any, y: any, z: any): number');
+});
