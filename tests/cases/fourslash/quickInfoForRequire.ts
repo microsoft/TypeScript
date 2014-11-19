57780944288f6f39e157fd71a43c454d0b050c47
Paul van Brenk <paul.van.brenk@microsoft.com>
2014-11-19 14:03:20 -0800

    Added test case for quickinfo on import statement.

---
+++ tests/cases/fourslash/quickInfoForRequire.ts
@@ -0,0 +1,10 @@
+/// <reference path='fourslash.ts'/>
+
+//@Filename: AA/BB.ts
+////export class a{}
+
+//@Filename: quickInfoForRequire_input.ts
+////import a = require("AA/B/*1*/B");
+
+goTo.marker('1');
+verify.quickInfoIs('module a');
