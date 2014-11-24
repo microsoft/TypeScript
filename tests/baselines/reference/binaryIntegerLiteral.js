5774e64d92789064c76c126e7b0cb8dbcad2062a
Yui T <yuisu@microsoft.com>
2014-11-23 16:23:24 -0800

    Add test cases

---
+++ tests/baselines/reference/binaryIntegerLiteral.js
@@ -0,0 +1,65 @@
+//// [binaryIntegerLiteral.ts]
+var bin1 = 0b11010;
+var bin2 = 0B11010;
+
+var obj1 = {
+    0b11010: "Hello",
+    a: bin1,
+    bin1,
+    b: 0b11010,
+}
+
+var obj2 = {
+    0B11010: "World",
+    a: bin2,
+    bin2,
+    b: 0B11010,
+}
+
+obj1[0b11010];    // string
+obj1[26];         // string
+obj1["26"];       // string
+obj1["0b11010"];  // any
+obj1["a"];        // number
+obj1["b"];        // number
+obj1["bin1"];     // number
+
+obj2[0B11010];    // string
+obj2[26];         // string
+obj2["26"];       // string
+obj2["0B11010"];  // any
+obj2["a"];        // number
+obj2["b"];        // number
+obj2["bin2"];     // number
+
+
+
+//// [binaryIntegerLiteral.js]
+var bin1 = 26;
+var bin2 = 26;
+var obj1 = {
+    26: "Hello",
+    a: bin1,
+    bin1: bin1,
+    b: 26,
+};
+var obj2 = {
+    26: "World",
+    a: bin2,
+    bin2: bin2,
+    b: 26,
+};
+obj1[26]; // string
+obj1[26]; // string
+obj1["26"]; // string
+obj1["0b11010"]; // any
+obj1["a"]; // number
+obj1["b"]; // number
+obj1["bin1"]; // number
+obj2[26]; // string
+obj2[26]; // string
+obj2["26"]; // string
+obj2["0B11010"]; // any
+obj2["a"]; // number
+obj2["b"]; // number
+obj2["bin2"]; // number
---
+++ tests/cases/conformance/es6/binaryAndOctalIntegerLiteral/octalIntegerLiteral.ts
@@ -0,0 +1,32 @@
+﻿// @target: es5
+var oct1 = 0o45436;
+var oct2 = 0O45436;
+var obj1 = {
+    0o45436: "Hello",
+    a: 0o45436, 
+    b: oct1,
+    oct1,
+}
+
+var obj2 = {
+    0O45436: "hi",
+    a: 0O45436, 
+    b: oct2,
+    oct2,
+}
+
+obj1[0o45436];    // string
+obj1["0o45436"];  // any
+obj1["19230"];       // string
+obj1[19230];         // string
+obj1["a"];        // number
+obj1["b"];        // number
+obj1["oct1"];     // number
+
+obj2[0O45436];   // string
+obj2["0O45436"]; // any
+obj2["19230"];       // string
+obj2[19230];         // string
+obj2["a"];        // number
+obj2["b"];        // number
+obj2["oct2"];     // number
