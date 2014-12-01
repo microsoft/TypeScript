7edd253307d165398ef29203fdf7c49053159bfb
Sheetal Nandi <shkamat@microsoft.com>
2014-12-01 00:26:26 -0800

    Tests for external alias

---
+++ tests/cases/fourslash/quickInfoDisplayPartsExternalModuleAlias.ts
@@ -0,0 +1,34 @@
+/// <reference path='fourslash.ts'/>
+
+// @Filename: quickInfoDisplayPartsExternalModuleAlias_file0.ts
+////export module m1 {
+////    export class c {
+////    }
+////}
+
+// @Filename: quickInfoDisplayPartsExternalModuleAlias_file1.ts
+////import /*1*/a1 = require("quickInfoDisplayPartsExternalModuleAlias_file0");
+////new /*2*/a1.m1.c();
+////export import /*3*/a2 = require("quickInfoDisplayPartsExternalModuleAlias_file0");
+////new /*4*/a2.m1.c();
+
+var marker = 0;
+function goToMarker() {
+    marker++;
+    goTo.marker(marker.toString());
+}
+
+function verifyImport(name: string, isExported: boolean) {
+    goToMarker();
+    verify.verifyQuickInfoDisplayParts("alias", isExported ? "export" : "", { start: test.markerByName(marker.toString()).position, length: name.length },
+        [{ text: "import", kind: "keyword" }, { text: " ", kind: "space" }, { text: name, kind: "aliasName" },
+            { text: " ", kind: "space" }, { text: "=", kind: "operator" }, { text: " ", kind: "space" },
+            { text: "require", kind: "keyword" }, { text: "(", kind: "punctuation" },
+            { text: "\"quickInfoDisplayPartsExternalModuleAlias_file0\"", kind: "stringLiteral" },
+            { text: ")", kind: "punctuation" }],
+        []);
+}
+verifyImport("a1", /*isExported*/false);
+verifyImport("a1", /*isExported*/false);
+verifyImport("a2", /*isExported*/true);
+verifyImport("a2", /*isExported*/true);
