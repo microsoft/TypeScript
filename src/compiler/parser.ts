97460a197795bae4ba572a481e14c13ce30cb2d4
Cyrus Najmabadi <cyrusn@microsoft.com>
2014-11-30 21:07:47 -0800

    Simplify code.

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -4620,15 +4620,10 @@ module ts {
         }
 
         function checkDeleteExpression(node: DeleteExpression) {
-            if (node.parserContextFlags & ParserContextFlags.StrictMode) {
-                // The identifier eval or arguments may not appear as the LeftHandSideExpression of an 
-                // Assignment operator(11.13) or of a PostfixExpression(11.3) or as the UnaryExpression 
-                // operated upon by a Prefix Increment(11.4.4) or a Prefix Decrement(11.4.5) operator
-                if (node.expression.kind === SyntaxKind.Identifier) {
-                    // When a delete operator occurs within strict mode code, a SyntaxError is thrown if its 
-                    // UnaryExpression is a direct reference to a variable, function argument, or function name
-                    return grammarErrorOnNode(node.expression, Diagnostics.delete_cannot_be_called_on_an_identifier_in_strict_mode);
-                }
+            if (node.parserContextFlags & ParserContextFlags.StrictMode && node.expression.kind === SyntaxKind.Identifier) {
+                // When a delete operator occurs within strict mode code, a SyntaxError is thrown if its 
+                // UnaryExpression is a direct reference to a variable, function argument, or function name
+                return grammarErrorOnNode(node.expression, Diagnostics.delete_cannot_be_called_on_an_identifier_in_strict_mode);
             }
         }
 
