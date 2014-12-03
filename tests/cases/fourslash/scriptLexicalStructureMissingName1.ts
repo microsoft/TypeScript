0e9561194bff930f6dbbf7e320ea6db8065142dc
Daniel Rosenwasser <drosen@microsoft.com>
2014-12-03 15:05:38 -0800

    Added tests.

---
+++ tests/cases/fourslash/scriptLexicalStructureMissingName1.ts
@@ -0,0 +1,16 @@
+﻿////export function
+/////**
+//// * This is a class.
+//// */
+////{| "itemName": "C", "kind": "class" |} class C {
+////    {| "itemName": "foo", "kind": "method" |} foo() {
+////    }
+////}
+
+
+test.markers().forEach((marker) => {
+    verify.getScriptLexicalStructureListContains(marker.data.itemName, marker.data.kind, marker.fileName, marker.data.parentName);
+});
+
+/// Only have two named elements.
+verify.getScriptLexicalStructureListCount(2);
