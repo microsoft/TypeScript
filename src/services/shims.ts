fbec3fe3c4732504cfca9a71eba479f909ea0e25
Vladimir Matveev <vladima@microsoft.com>
2014-12-07 15:17:20 -0800

    removed dummy parameter from getApiVersion method

--- src/services/shims.ts
+++ src/services/shims.ts
@@ -862,7 +862,7 @@ module ts {
         /*
          * Returns script API version.
          */
-        public getApiVersion(dummy: any): string {
+        public getApiVersion(): string {
             return ScriptAPIVersion;
         }
 
