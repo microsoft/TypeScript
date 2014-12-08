1fab80f3d98517aa22e1f257e2bcc509145f1ccd
Mohamed Hegazy <mhegazy@microsoft.com>
2014-12-07 21:23:03 -0800

    move OutliningSpan definitions to services to ensure it is visible in definitions file

--- src/services/services.ts
+++ src/services/services.ts
@@ -1409,6 +1409,23 @@ module ts {
         documentation: SymbolDisplayPart[];
     }
 
+    export interface OutliningSpan {
+        /** The span of the document to actually collapse. */
+        textSpan: TextSpan;
+
+        /** The span of the document to display when the user hovers over the collapsed span. */
+        hintSpan: TextSpan;
+
+        /** The text to display in the editor for the collapsed region. */
+        bannerText: string;
+
+        /** 
+          * Whether or not this region should be automatically collapsed when 
+          * the 'Collapse to Definitions' command is invoked.
+          */
+        autoCollapse: boolean;
+    }
+
     export interface EmitOutput {
         outputFiles: OutputFile[];
         emitOutputStatus: EmitReturnStatus;
