b87a7fafaf5ea74f846cf5f0456149e13d97993a
Mohamed Hegazy <mhegazy@microsoft.com>
2014-12-07 16:26:08 -0800

    move formatting.ts and smartIndernter.ts into formatting folder to match thier namespace

--- src/services/formatting/formatting.ts
+++ src/services/formatting/formatting.ts
@@ -1,7 +1,6 @@
-///<reference path='services.ts' />
-///<reference path='formatting\indentation.ts' />
-///<reference path='formatting\formattingScanner.ts' />
-///<reference path='formatting\rulesProvider.ts' />
+///<reference path='..\services.ts' />
+///<reference path='formattingScanner.ts' />
+///<reference path='references.ts' />
 
 module ts.formatting {
 
@@ -990,5 +989,57 @@ module ts.formatting {
         }
 
         return SyntaxKind.Unknown;
+    }
+
+    var internedTabsIndentation: string[];
+    var internedSpacesIndentation: string[];
+
+    export function getIndentationString(indentation: number, options: FormatCodeOptions): string {
+        if (!options.ConvertTabsToSpaces) {
+            var tabs = Math.floor(indentation / options.TabSize);
+            var spaces = indentation - tabs * options.TabSize;
+
+            var tabString: string;
+            if (!internedTabsIndentation) {
+                internedTabsIndentation = [];
+            }
+
+            if (internedTabsIndentation[tabs] === undefined) {
+                internedTabsIndentation[tabs] = tabString = repeat('\t', tabs);
+            }
+            else {
+                tabString = internedTabsIndentation[tabs];
+            }
+
+            return spaces ? tabString + repeat(" ", spaces) : tabString;
+        }
+        else {
+            var spacesString: string;
+            var quotient = Math.floor(indentation / options.IndentSize);
+            var remainder = indentation % options.IndentSize;
+            if (!internedSpacesIndentation) {
+                internedSpacesIndentation = [];
+            }
+
+            if (internedSpacesIndentation[quotient] === undefined) {
+                spacesString = repeat(" ", options.IndentSize * quotient);
+                internedSpacesIndentation[quotient] = spacesString;
+            }
+            else {
+                spacesString = internedSpacesIndentation[quotient];
+            }
+
+
+            return remainder ? spacesString + repeat(" ", remainder) : spacesString;
+        }
+
+        function repeat(value: string, count: number): string {
+            var s = "";
+            for (var i = 0; i < count; ++i) {
+                s += value;
+            }
+
+            return s;
+        }
     }
 }
