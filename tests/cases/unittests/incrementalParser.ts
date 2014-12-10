6058dbbc2f56bc7e86dbb287e8cf98303366749b
Cyrus Najmabadi <cyrusn@microsoft.com>
2014-12-09 17:21:30 -0800

    Add incremental test.

--- tests/cases/unittests/incrementalParser.ts
+++ tests/cases/unittests/incrementalParser.ts
@@ -388,5 +388,15 @@ module ts {
 
             compareTrees(oldText, newTextAndChange.text, newTextAndChange.textChangeRange, -1);
         });
+
+        it('Speculative generic lookahead 2',() => {
+            var source = "var v = F<a,b>e";
+
+            var index = source.indexOf('b');
+            var oldText = ScriptSnapshot.fromString(source);
+            var newTextAndChange = withInsert(oldText, index + 1, ",x");
+
+            compareTrees(oldText, newTextAndChange.text, newTextAndChange.textChangeRange, -1);
+        });
     });
 }
