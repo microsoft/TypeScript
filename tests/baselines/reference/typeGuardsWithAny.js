98c9f75e2165f5a187cbceda38972bd37423c6f2
Anders Hejlsberg <andersh@microsoft.com>
2014-12-10 10:17:48 -0800

    Adding test case

---
+++ tests/baselines/reference/typeGuardsWithAny.js
@@ -0,0 +1,12 @@
+//// [typeGuardsWithAny.ts]
+var x: any = { p: 0 };
+if (x instanceof Object) {
+    x.p; // No error, type any is not narrowed
+}
+
+
+//// [typeGuardsWithAny.js]
+var x = { p: 0 };
+if (x instanceof Object) {
+    x.p; // No error, type any is not narrowed
+}
