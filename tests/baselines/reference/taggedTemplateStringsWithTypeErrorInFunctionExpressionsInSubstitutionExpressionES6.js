17c822966dae2f6eaa90e3f380ab6c08cdc8c2e8
Daniel Rosenwasser <drosen@microsoft.com>
2014-12-15 16:39:12 -0800

    Added tests for erroneous function expressions in both tagged/untagged template expressions.

---
+++ tests/baselines/reference/taggedTemplateStringsWithTypeErrorInFunctionExpressionsInSubstitutionExpressionES6.js
@@ -0,0 +1,17 @@
+//// [taggedTemplateStringsWithTypeErrorInFunctionExpressionsInSubstitutionExpressionES6.ts]
+
+function foo(...rest: any[]) {
+}
+
+foo `${function (x: number) { x = "bad"; } }`;
+
+//// [taggedTemplateStringsWithTypeErrorInFunctionExpressionsInSubstitutionExpressionES6.js]
+function foo() {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i - 0] = arguments[_i];
+    }
+}
+foo `${function (x) {
+    x = "bad";
+}}`;
