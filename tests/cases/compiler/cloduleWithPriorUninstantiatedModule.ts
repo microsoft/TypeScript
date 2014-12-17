ca5d243ca7afdad7c51cb9378ccf44b1663fcd06
Daniel Rosenwasser <drosen@microsoft.com>
2014-12-17 15:49:00 -0800

    Added test from original issue.

---
+++ tests/cases/compiler/cloduleWithPriorUninstantiatedModule.ts
@@ -0,0 +1,15 @@
+﻿// Ambient/uninstantiated module.
+module Moclodule {
+    export interface Someinterface {
+        foo(): void;
+    }
+}
+
+class Moclodule {
+}
+
+// Instantiated module.
+module Moclodule {
+    export class Manager {
+    }
+}
