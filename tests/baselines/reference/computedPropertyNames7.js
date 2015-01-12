4ac2b4b9c87d4025e47bde52d2cadd31b2c18870
Jason Freeman <jfreeman@microsoft.com>
2015-01-12 14:04:34 -0800

    Add tests for object literals with computed properties

---
+++ tests/baselines/reference/computedPropertyNames7.js
@@ -0,0 +1,16 @@
+//// [computedPropertyNames7.ts]
+enum E {
+    member
+}
+var v = {
+    [E.member]: 0
+}
+
+//// [computedPropertyNames7.js]
+var E;
+(function (E) {
+    E[E["member"] = 0] = "member";
+})(E || (E = {}));
+var v = {
+    [0 /* member */]: 0
+};
