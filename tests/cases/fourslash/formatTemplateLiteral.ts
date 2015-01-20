a6d374ee21359c9001f4de161929df1d90058060
Vladimir Matveev <vladima@microsoft.com>
2015-01-20 14:07:01 -0800

    fixed smart indentation\formatting in template literals

---
+++ tests/cases/fourslash/formatTemplateLiteral.ts
@@ -0,0 +1,7 @@
+/// <reference path="fourslash.ts"/>
+////var x = `sadasdasdasdasfegsfd
+/////*1*/rasdesgeryt35t35y35 e4 ergt er 35t 3535 `;
+
+goTo.marker("1");
+edit.insert("\r\n"); // edit will trigger formatting - should succeeed
+
