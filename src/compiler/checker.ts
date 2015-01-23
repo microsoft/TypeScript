microsoft/TypeScript

commit b432f1c5e87564d1ed1cff8ecb85edaffeb05623

Adding constraint to the TypeParameter returned by method getDeclaredTypeOfTypeParameter this is generating 2 error messages. For every TS2313 , TS2315 is generated .Also this generate error in promiseIdentityWithConstraints.ts

src/compiler/checker.ts
@@ -2127,7 +2127,10 @@ module ts {
             if (!links.declaredType) {
                 var type = <TypeParameter>createType(TypeFlags.TypeParameter);
                 type.symbol = symbol;
-                if (!(<TypeParameterDeclaration>getDeclarationOfKind(symbol, SyntaxKind.TypeParameter)).constraint) {
+                var constraint = (<TypeParameterDeclaration>getDeclarationOfKind(symbol, SyntaxKind.TypeParameter)).constraint;
+                if (constraint) {
+                    type.constraint = getTypeFromTypeNode(constraint);
+                }else{
                     type.constraint = noConstraintType;
                 }
                 links.declaredType = type;

commit 9d9501bdabc78d395ef0d0f5ad40bede0e89418d

Improving error message , when a class or interface is referenced in constraint.This commit is trying to fix an issue caused by commit b432f1c5e.

src/compiler/checker.ts
@@ -2859,7 +2859,11 @@ module ts {
                             // forEach === exists
                             links.isIllegalTypeReferenceInConstraint = forEach(symbol.declarations, d => d.parent == typeParameter.parent);
                         }
+                        if (symbol && (symbol.flags & (SymbolFlags.Interface | SymbolFlags.Class ))) {
+                            links.isIllegalTypeReferenceInConstraint = forEach(symbol.declarations, d => d == typeParameter.parent);    
+                        }
                     }
+                    
                     if (links.isIllegalTypeReferenceInConstraint) {
                         error(typeParameter, Diagnostics.Constraint_of_a_type_parameter_cannot_reference_any_type_parameter_from_the_same_type_parameter_list);
                     }
@@ -2899,10 +2903,14 @@ module ts {
                             }
                         }
                         else {
-                            if (node.typeArguments) {
+                            if (type.flags & (TypeFlags.Class | TypeFlags.Interface) && getNodeLinks(node).isIllegalTypeReferenceInConstraint){
+                                error(node, Diagnostics.Illegal_reference_0_in_constraint, typeToString(type));
+                                type = undefined;
+                            }else if (node.typeArguments) {
                                 error(node, Diagnostics.Type_0_is_not_generic, typeToString(type));
                                 type = undefined;
                             }
+
                         }
                     }
                 }
@@ -7153,11 +7161,11 @@ module ts {
                 grammarErrorOnFirstToken(node.expression, Diagnostics.Type_expected);
             }
 
-            checkSourceElement(node.constraint);
             if (produceDiagnostics) {
                 checkTypeParameterHasIllegalReferencesInConstraint(node);
                 checkTypeNameIsReserved(node.name, Diagnostics.Type_parameter_name_cannot_be_0);
             }
+            checkSourceElement(node.constraint);
             // TODO: Check multiple declarations are identical
         }
 

src/compiler/diagnosticInformationMap.generated.ts
@@ -298,6 +298,7 @@ module ts {
         Type_0_is_not_an_array_type: { code: 2461, category: DiagnosticCategory.Error, key: "Type '{0}' is not an array type." },
         A_rest_element_must_be_last_in_an_array_destructuring_pattern: { code: 2462, category: DiagnosticCategory.Error, key: "A rest element must be last in an array destructuring pattern" },
         Duplicate_overload_signature_for_0: { code: 2463, category: DiagnosticCategory.Error, key: "Duplicate overload signature for {0}." },
+        Illegal_reference_0_in_constraint: { code: 2464, category: DiagnosticCategory.Error, key: "Illegal reference {0} in constraint." },
         Import_declaration_0_is_using_private_name_1: { code: 4000, category: DiagnosticCategory.Error, key: "Import declaration '{0}' is using private name '{1}'." },
         Type_parameter_0_of_exported_class_has_or_is_using_private_name_1: { code: 4002, category: DiagnosticCategory.Error, key: "Type parameter '{0}' of exported class has or is using private name '{1}'." },
         Type_parameter_0_of_exported_interface_has_or_is_using_private_name_1: { code: 4004, category: DiagnosticCategory.Error, key: "Type parameter '{0}' of exported interface has or is using private name '{1}'." },