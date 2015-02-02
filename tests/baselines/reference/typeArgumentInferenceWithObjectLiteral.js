34ed45df8e21de17c061ac01933c7d34058480b5
Anders Hejlsberg <andersh@microsoft.com>
2015-02-02 10:26:31 -0800

    Adding more tests

--- tests/baselines/reference/typeArgumentInferenceWithObjectLiteral.js
+++ tests/baselines/reference/typeArgumentInferenceWithObjectLiteral.js
@@ -18,6 +18,23 @@ foo({
     read: () => s
 });
 
+enum E1 { X }
+enum E2 { X }
+
+// Check that we infer from both a.r and b before fixing T in a.w
+
+declare function f1<T, U>(a: { w: (x: T) => U; r: () => T; }, b: T): U;
+
+var v1: number;
+var v1 = f1({ w: x => x, r: () => 0 }, 0);
+var v1 = f1({ w: x => x, r: () => 0 }, E1.X);
+var v1 = f1({ w: x => x, r: () => E1.X }, 0);
+
+var v2: E1;
+var v2 = f1({ w: x => x, r: () => E1.X }, E1.X);
+
+var v3 = f1({ w: x => x, r: () => E1.X }, E2.X);  // Error
+
 
 //// [typeArgumentInferenceWithObjectLiteral.js]
 function foo(x) {
@@ -32,3 +49,18 @@ foo({
     write: function (value) { return s = value; },
     read: function () { return s; }
 });
+var E1;
+(function (E1) {
+    E1[E1["X"] = 0] = "X";
+})(E1 || (E1 = {}));
+var E2;
+(function (E2) {
+    E2[E2["X"] = 0] = "X";
+})(E2 || (E2 = {}));
+var v1;
+var v1 = f1({ w: function (x) { return x; }, r: function () { return 0; } }, 0);
+var v1 = f1({ w: function (x) { return x; }, r: function () { return 0; } }, 0 /* X */);
+var v1 = f1({ w: function (x) { return x; }, r: function () { return 0 /* X */; } }, 0);
+var v2;
+var v2 = f1({ w: function (x) { return x; }, r: function () { return 0 /* X */; } }, 0 /* X */);
+var v3 = f1({ w: function (x) { return x; }, r: function () { return 0 /* X */; } }, 0 /* X */); // Error
--- tests/cases/conformance/expressions/functionCalls/typeArgumentInferenceWithObjectLiteral.ts
+++ tests/cases/conformance/expressions/functionCalls/typeArgumentInferenceWithObjectLiteral.ts
@@ -16,3 +16,20 @@ foo({
     write: value => s = value,
     read: () => s
 });
+
+enum E1 { X }
+enum E2 { X }
+
+// Check that we infer from both a.r and b before fixing T in a.w
+
+declare function f1<T, U>(a: { w: (x: T) => U; r: () => T; }, b: T): U;
+
+var v1: number;
+var v1 = f1({ w: x => x, r: () => 0 }, 0);
+var v1 = f1({ w: x => x, r: () => 0 }, E1.X);
+var v1 = f1({ w: x => x, r: () => E1.X }, 0);
+
+var v2: E1;
+var v2 = f1({ w: x => x, r: () => E1.X }, E1.X);
+
+var v3 = f1({ w: x => x, r: () => E1.X }, E2.X);  // Error
