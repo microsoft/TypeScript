31d54fb7dcc2085158cf71c77c157b2f794549f2
Sheetal Nandi <shkamat@microsoft.com>
2015-02-05 11:59:51 -0800

    Test cases for function overload in default function declaration

---
+++ tests/cases/compiler/es6ExportDefaultFunctionDeclarationOverload.ts
@@ -0,0 +1,7 @@
+// @target: es6
+// @module: commonjs
+// @declaration: true
+export default function b(a: string): string;
+export default function b(a: number): number;
+export default function b(a: any): any {
+}
