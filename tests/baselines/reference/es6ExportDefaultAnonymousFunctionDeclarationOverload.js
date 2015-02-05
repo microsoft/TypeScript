a0e7ffc35e366ffe0464911ea4e87db6a45a0199
Sheetal Nandi <shkamat@microsoft.com>
2015-02-05 12:02:02 -0800

    Anonymous default function declaration overload TODO make this test case run after type check changes

---
+++ tests/baselines/reference/es6ExportDefaultAnonymousFunctionDeclarationOverload.js
@@ -0,0 +1,15 @@
+//// [es6ExportDefaultAnonymousFunctionDeclarationOverload.ts]
+
+//export default function (a: string): string;
+//export default function (a: number): number;
+//export default function (a: any): any {
+//}
+
+//// [es6ExportDefaultAnonymousFunctionDeclarationOverload.js]
+//export default function (a: string): string;
+//export default function (a: number): number;
+//export default function (a: any): any {
+//} 
+
+
+//// [es6ExportDefaultAnonymousFunctionDeclarationOverload.d.ts]
