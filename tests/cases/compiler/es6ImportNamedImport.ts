9fa8bc6fe2e760bc998af622ee5d3c80f8af07fe
Sheetal Nandi <shkamat@microsoft.com>
2015-02-06 09:28:32 -0800

    Test cases for named import resolution - Verify correct resolution - Reports error during resolution

--- tests/cases/compiler/es6ImportNamedImport.ts
+++ tests/cases/compiler/es6ImportNamedImport.ts
@@ -7,6 +7,8 @@ export var x = a;
 export var m = a;
 export var a1 = 10;
 export var x1 = 10;
+export var z1 = 10;
+export var z2 = 10;
 
 // @filename: es6ImportNamedImport_1.ts
 import { } from "es6ImportNamedImport_0";
@@ -17,3 +19,7 @@ import { x as z,  } from "es6ImportNamedImport_0";
 import { m,  } from "es6ImportNamedImport_0";
 import { a1, x1 } from "es6ImportNamedImport_0";
 import { a1 as a11, x1 as x11 } from "es6ImportNamedImport_0";
+import { z1 } from "es6ImportNamedImport_0";
+var z111 = z1;
+import { z2 as z3 } from "es6ImportNamedImport_0";
+var z2 = z3; // z2 shouldn't give redeclare error
