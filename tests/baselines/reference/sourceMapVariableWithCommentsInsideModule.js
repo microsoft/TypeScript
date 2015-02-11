4686fa50a005849c6c9dea0428ae640681bff472
Sheetal Nandi <shkamat@microsoft.com>
2015-02-11 12:12:10 -0800

    Add test case for the issue #1980

---
+++ tests/baselines/reference/sourceMapVariableWithCommentsInsideModule.js
@@ -0,0 +1,17 @@
+//// [sourceMapVariableWithCommentsInsideModule.ts]
+module Q {
+    function P() {
+        // Test this
+        var a = 1;
+    }
+}
+
+//// [sourceMapVariableWithCommentsInsideModule.js]
+var Q;
+(function (Q) {
+    function P() {
+        // Test this
+        var a = 1;
+    }
+})(Q || (Q = {}));
+//# sourceMappingURL=sourceMapVariableWithCommentsInsideModule.js.map
---
+++ tests/cases/compiler/sourceMapVariableWithCommentsInsideModule.ts
@@ -0,0 +1,7 @@
+// @sourcemap: true
+module Q {
+    function P() {
+        // Test this
+        var a = 1;
+    }
+}
