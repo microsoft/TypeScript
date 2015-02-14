ba52d60c7a99223c47477063b26095678912d19b
Vladimir Matveev <vladima@microsoft.com>
2015-02-14 14:23:37 -0800

    try only names generated in current scope with testing if name is unique

--- src/compiler/emitter.ts
+++ src/compiler/emitter.ts
@@ -1696,23 +1696,7 @@ module ts {
 
 
             function isExistingName(location: Node, name: string) {
-                if (!resolver.isUnknownIdentifier(location, name)) {
-                    return true;
-                }
-
-                if (currentScopeNames && hasProperty(currentScopeNames, name)) {
-                    return true;
-                }
-
-                var frame = lastFrame;
-                while (frame) {
-                    if (hasProperty(frame.names, name)) {
-                        return true;
-                    }
-                    frame = frame.previous;
-                }
-
-                return false;
+                return !resolver.isUnknownIdentifier(location, name) || (currentScopeNames && hasProperty(currentScopeNames, name));
             }
 
             function initializeEmitterWithSourceMaps() {
