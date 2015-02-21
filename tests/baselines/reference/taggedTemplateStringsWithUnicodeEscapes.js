f8832598b9ad094774bad6470d2661ed6abeb4cd
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-02-21 14:48:10 +0100

    Add tests for tagged templates

---
+++ tests/baselines/reference/taggedTemplateStringsWithUnicodeEscapes.js
@@ -0,0 +1,15 @@
+//// [taggedTemplateStringsWithUnicodeEscapes.ts]
+function f(...args: any[]) {
+}
+
+f `'\u{1f4a9}'${ " should be converted to " }'\uD83D\uDCA9'`;
+
+//// [taggedTemplateStringsWithUnicodeEscapes.js]
+function f() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i - 0] = arguments[_i];
+    }
+}
+(_a = ["'{1f4a\u0039}'", "'💩'"], _a.raw = ["'\\u{1f4a\u0039}'", "'\\uD83D\\uDCA\u0039'"], f(_a, " should be converted to "));
+var _a;
