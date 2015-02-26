84a22be433f8d0e44e2d98af38d685d214499f45
Jason Freeman <jfreeman@microsoft.com>
2015-02-26 15:58:46 -0800

    Add a few more tests

---
+++ tests/baselines/reference/for-of49.js
@@ -0,0 +1,15 @@
+//// [for-of49.ts]
+var k: string, v: boolean;
+var map = new Map([["", true]]);
+for ([k, ...[v]] of map) {
+    k;
+    v;
+}
+
+//// [for-of49.js]
+var k, v;
+var map = new Map([["", true]]);
+for ([k, ...[v]] of map) {
+    k;
+    v;
+}
