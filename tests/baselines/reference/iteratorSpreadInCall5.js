4a0af76064ae039efd99b47133eb35a21c1cc43f
Jason Freeman <jfreeman@microsoft.com>
2015-03-24 16:52:00 -0700

    Add tests for iterator spread in call

---
+++ tests/baselines/reference/iteratorSpreadInCall5.js
@@ -0,0 +1,56 @@
+//// [iteratorSpreadInCall5.ts]
+foo(...new SymbolIterator, ...new StringIterator);
+
+function foo(...s: (symbol | string)[]) { }
+class SymbolIterator {
+    next() {
+        return {
+            value: Symbol(),
+            done: false
+        };
+    }
+
+    [Symbol.iterator]() {
+        return this;
+    }
+}
+
+class StringIterator {
+    next() {
+        return {
+            value: "",
+            done: false
+        };
+    }
+
+    [Symbol.iterator]() {
+        return this;
+    }
+}
+
+//// [iteratorSpreadInCall5.js]
+foo(...new SymbolIterator, ...new StringIterator);
+function foo(...s) {
+}
+class SymbolIterator {
+    next() {
+        return {
+            value: Symbol(),
+            done: false
+        };
+    }
+    [Symbol.iterator]() {
+        return this;
+    }
+}
+class StringIterator {
+    next() {
+        return {
+            value: "",
+            done: false
+        };
+    }
+    [Symbol.iterator]() {
+        return this;
+    }
+}
---
+++ tests/cases/conformance/es6/spread/iteratorSpreadInCall6.ts
@@ -0,0 +1,29 @@
+//@target: ES6
+foo(...new SymbolIterator, ...new StringIterator);
+
+function foo(...s: (symbol | number)[]) { }
+class SymbolIterator {
+    next() {
+        return {
+            value: Symbol(),
+            done: false
+        };
+    }
+
+    [Symbol.iterator]() {
+        return this;
+    }
+}
+
+class StringIterator {
+    next() {
+        return {
+            value: "",
+            done: false
+        };
+    }
+
+    [Symbol.iterator]() {
+        return this;
+    }
+}
