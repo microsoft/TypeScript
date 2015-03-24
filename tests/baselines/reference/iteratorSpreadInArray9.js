376574d1be89195a34c5b04ce33ecc41282919f2
Jason Freeman <jfreeman@microsoft.com>
2015-03-24 16:33:56 -0700

    Add tests for iterator spread in an array literal

---
+++ tests/baselines/reference/iteratorSpreadInArray9.js
@@ -0,0 +1,29 @@
+//// [iteratorSpreadInArray9.ts]
+var array = [...new SymbolIterator];
+
+class SymbolIterator {
+    next() {
+        return {
+            value: Symbol()
+        };
+    }
+
+    [Symbol.iterator]() {
+        return this;
+    }
+}
+
+//// [iteratorSpreadInArray9.js]
+var array = [
+    ...new SymbolIterator
+];
+class SymbolIterator {
+    next() {
+        return {
+            value: Symbol()
+        };
+    }
+    [Symbol.iterator]() {
+        return this;
+    }
+}
