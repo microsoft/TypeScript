c5f49deb5637ac6b0ae5b89896424f56fc2eae7e
Ron Buckton <rbuckton@microsoft.com>
2015-03-25 18:12:57 -0700

    Minor emit cleanup

--- tests/baselines/reference/decoratorOnClassMethod5.js
+++ tests/baselines/reference/decoratorOnClassMethod5.js
@@ -6,7 +6,6 @@ class C {
 }
 
 //// [decoratorOnClassMethod5.js]
-<<<<<<< HEAD
 var __decorate = this.__decorate || function (decorators, target, key, value) {
     var kind = typeof (arguments.length == 2 ? value = target : value);
     for (var i = decorators.length - 1; i >= 0; --i) {
@@ -26,25 +25,3 @@ class C {
 }
 Object.defineProperty(C.prototype, _a, __decorate([dec(), __metadata('design:type', Function), __metadata('design:paramtypes', []), __metadata('design:returntype', Object)], C.prototype, _a, Object.getOwnPropertyDescriptor(C.prototype, _a)));
 var _a;
-=======
-var __decorate = this.__decorate || (typeof Reflect === "object" && Reflect.decorate) || function (decorators, target, key) {
-    var kind = key == null ? 0 : typeof key == "number" ? 1 : 2, result = target;
-    if (kind == 2) result = Object.getOwnPropertyDescriptor(target, typeof key == "symbol" ? key : key = String(key));
-    for (var i = decorators.length - 1; i >= 0; --i) {
-        var decorator = decorators[i];
-        result = (kind == 0 ? decorator(result) : kind == 1 ? decorator(target, key) : decorator(target, key, result)) || result;
-    }
-    if (kind == 2 && result) Object.defineProperty(target, key, result);
-    if (kind == 0) return result;
-};
-var __metadata = this.__metadata || (typeof Reflect === "object" && Reflect.metadata) || function (metadataKey, metadataValue) { return function() { } };
-var C = (function() {
-    class C {
-        [_a = "method"]() {
-        }
-    }
-    __decorate([dec(), __metadata('design:type', Function), __metadata('design:paramtypes', []), __metadata('design:returntype', Object)], C.prototype, _a);
-    return C;
-    var _a;
-})();
->>>>>>> Updated baselines
