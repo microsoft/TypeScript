d53e0c2ac2c0655cb656b02c774684bd52c5072b
Zev Spitz <shivisi@etrog.net.il>
2015-03-31 12:59:50 +0300

    Added properties/methods for WScript - https://msdn.microsoft.com/en-us/library/2795740w(v=vs.84).aspx per #2540

--- src/lib/scriptHost.d.ts
+++ src/lib/scriptHost.d.ts
@@ -10,10 +10,26 @@ interface ActiveXObject {
 }
 declare var ActiveXObject: ActiveXObject;
 
-interface ITextWriter {
-    Write(s: string): void;
-    WriteLine(s: string): void;
+interface ITextStreamBase {
+    Column: number;
+    Line: number;
     Close(): void;
+}
+
+interface ITextWriter extends ITextStreamBase {
+    Write(s: string): void;
+    WriteBlankLines(intLines: number): void;
+    WriteLine(s: string): void;
+}
+
+interface ITextReader extends ITextStreamBase {
+    Read(characters: number): string;
+    ReadAll(): string;
+    ReadLine(): string;
+    Skip(characters: number): void;
+    SkipLine(): void;
+    AtEndOfLine: boolean;
+    AtEndOfStream: boolean;
 }
 
 declare var WScript: {
@@ -23,4 +39,17 @@ declare var WScript: {
     Arguments: { length: number; Item(n: number): string; };
     ScriptFullName: string;
     Quit(exitCode?: number): number;
+    BuildVersion: number;
+    FullName: string;
+    Interactive: boolean;
+    Name: string;
+    Path: string;
+    ScriptName: string;
+    StdIn: ITextReader;
+    Version: string;
+    ConnectObject(objEventSource: any, strPrefix: string): void;
+    CreateObject(strProgID: string, strPrefix?: string): any;
+    DisconnectObject(obj: any): void;
+    GetObject(strPathname: string, strProgID?: string, strPrefix?: string): any;
+    Sleep(intTime: number): void;
 }
