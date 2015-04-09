c25170558e57cf92a5f36c079519723edc5b24fe
Daniel Rosenwasser <drosen@microsoft.com>
2015-04-09 16:06:26 -0700

    Added test for super method calls in ES6 classes.

---
+++ tests/baselines/reference/emitClassDeclarationWithSuperMethodCall01.js
@@ -0,0 +1,24 @@
+//// [emitClassDeclarationWithSuperMethodCall01.ts]
+
+class Parent {
+    foo() {
+    }
+}
+
+class Foo extends Parent {
+    foo() {
+        var x = () => super.foo();
+    }
+}
+
+//// [emitClassDeclarationWithSuperMethodCall01.js]
+class Parent {
+    foo() {
+    }
+}
+class Foo extends Parent {
+    foo() {
+        var _this = this;
+        var x = () => super.foo();
+    }
+}
