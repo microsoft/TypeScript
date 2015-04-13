522c71a752cdc62f288c5c4d531e907c703d9d05
Zev Spitz <shivisi@etrog.net.il>
2015-04-13 16:23:31 +0300

    Ebynerator, VBArray

--- src/lib/scriptHost.d.ts
+++ src/lib/scriptHost.d.ts
@@ -170,3 +170,64 @@ declare var WScript: {
      */
     Sleep(intTime: number): void;
 };
+
+/*
+ * Allows enumerating over a COM collection, which may not have indexed item access
+ */
+interface Enumerator<T> {
+    /*
+     * Returns true if the current item is the last one in the collection, or the collection is empty, or the current item is undefined
+     */
+    atEnd(): boolean;
+    /*
+     * Returns the current item in the collection
+     */
+    item(): T;
+    /*
+     * Resets the current item in the collection to the first item. If there are no items in the collection, the current item is set to undefined.
+     */
+    moveFirst(): void;
+    /*
+     * Moves the current item to the next item in the collection. If the enumerator is at the end of the collection or the collection is empty, the current item is set to undefined.
+     */
+    moveNext(): void;
+}
+interface EnumeratorConstructor {
+    new <T>(collection: any): Enumerator<T>;
+    new (collection: any): Enumerator<any>;
+}
+declare var Enumerator: EnumeratorConstructor;
+
+/*
+ * Enables reading from a COM safe array, which might have an alternate lower bound, or multiple dimensions
+ */
+interface VBArray<T> {
+    /*
+     * Returns the number of dimensions (1-based).
+     */
+    dimensions(): number;
+    /*
+     * Takes an index for each dimension in the array, and returns the item at the corresponding location.
+     */
+    getItem(dimension1Index: number, ...dimensionNIndexes: number[]): T;
+    /*
+     * Returns the smallest available index for a given dimension
+     * @param dimension 1-based dimension (defaults to 1)
+     */
+    lbound(dimension?: number): number;
+    /*
+     * Returns the largest available index for a given dimension
+     * @param dimension 1-based dimension (defaults to 1)
+     */
+    ubound(dimension?: number): number;
+    /*
+     * Returns a Javascript array with all the elements in the VBArray. If there are multiple dimensions, each successive dimension is appended to the end of the array.
+     * [[1,2,3],[4,5,6]] => [1,2,3,4,5,6]
+     */
+    toArray(): T[];
+}
+interface VBArrayConstructor {
+    new <T>(safeArray: any): VBArray<T>;
+    new (safeArray: any): VBArray<any>;
+}
+declare var VBArray: VBArrayConstructor;
