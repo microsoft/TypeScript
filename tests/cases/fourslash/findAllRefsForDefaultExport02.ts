4927a23d1b11fed34c87954343bca203dfea9428
Daniel Rosenwasser <drosen@microsoft.com>
2015-05-20 15:39:35 -0700

    Verify references count.

--- tests/cases/fourslash/findAllRefsForDefaultExport02.ts
+++ tests/cases/fourslash/findAllRefsForDefaultExport02.ts
@@ -16,6 +16,7 @@ let ranges = test.ranges()
 for (let range of ranges) {
     goTo.position(range.start);
 
+    verify.referencesCountIs(ranges.length);
     for (let expectedReference of ranges) {
         verify.referencesAtPositionContains(expectedReference);
     }
