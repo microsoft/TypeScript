8d135667ab6d2cd6d14611d861bf00266c6b8688
Daniel Rosenwasser <drosen@microsoft.com>
2015-05-21 12:28:02 -0700

    Fixed tests.

--- tests/cases/fourslash/findAllRefsForDefaultExport01.ts
+++ tests/cases/fourslash/findAllRefsForDefaultExport01.ts
@@ -6,10 +6,6 @@
 ////var x: [|DefaultExportedClass|];
 ////
 ////var y = new [|DefaultExportedClass|];
-////
-////namespace [|DefaultExportedClass|] {
-////    var local = 100;
-////}
 
 let ranges = test.ranges()
 for (let range of ranges) {
--- tests/cases/fourslash/renameForDefaultExport03.ts
+++ tests/cases/fourslash/renameForDefaultExport03.ts
@@ -1,8 +1,5 @@
 /// <reference path='fourslash.ts'/>
 
-/////**
-//// *  Commenting [|f|]
-//// */
 ////function /*1*/[|f|]() {
 ////    return 100;
 ////}
@@ -13,6 +10,9 @@
 ////
 ////var y = /*4*/[|f|]();
 ////
+/////**
+//// *  Commenting [|f|]
+//// */
 ////namespace /*5*/[|f|] {
 ////    var local = 100;
 ////}
@@ -22,5 +22,4 @@ for (let marker of markers) {
     goTo.position(marker.position);
 
     verify.renameLocations(/*findInStrings*/ false, /*findInComments*/ true);
-    verify.renameInfoSucceeded("f");
 }
