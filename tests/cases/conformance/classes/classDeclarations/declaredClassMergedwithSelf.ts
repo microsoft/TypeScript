6c98c677146ec32b9c41122daffc6946ef1c599c
Arthur Ozga <aozgaa@umich.edu>
2015-06-01 16:26:41 -0700

    added conformance tests

---
+++ tests/cases/conformance/classes/classDeclarations/declaredClassMergedwithSelf.ts
@@ -0,0 +1,9 @@
+declare class C1 {}
+
+declare class C1 {}
+
+declare class C2 {}
+
+interface C2 {}
+
+declare class C2 {}
---
+++ tests/cases/conformance/classes/classDeclarations/mergedClassInterface.ts
@@ -0,0 +1,11 @@
+declare class C1 {}
+
+interface C1 {}
+
+interface C2 {}
+
+declare class C2 {}
+
+interface C2 {}
+
+interface C2 {}
