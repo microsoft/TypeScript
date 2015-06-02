448e8bf0c7d5adae72cfb0e12bfdae1e2a01bc4d
Daniel Rosenwasser <drosen@microsoft.com>
2015-06-02 15:47:23 -0700

    Added baselines.

---
+++ tests/baselines/reference/asiPreventsParsingAsTypeAlias02.js
@@ -0,0 +1,15 @@
+//// [asiPreventsParsingAsTypeAlias02.ts]
+
+var type;
+var string;
+var Foo;
+
+namespace container {
+    type
+    Foo = string;
+}
+
+//// [asiPreventsParsingAsTypeAlias02.js]
+var type;
+var string;
+var Foo;
