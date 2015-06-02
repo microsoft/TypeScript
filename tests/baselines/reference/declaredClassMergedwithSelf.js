b293da4f9bec122ef3b7761b4d3606661f2f0e03
Arthur Ozga <aozgaa@umich.edu>
2015-06-02 15:26:43 -0700

    updated baselines

--- tests/baselines/reference/declaredClassMergedwithSelf.js
+++ tests/baselines/reference/declaredClassMergedwithSelf.js
@@ -1,4 +1,8 @@
-//// [declaredClassMergedwithSelf.ts]
+//// [tests/cases/conformance/classes/classDeclarations/declaredClassMergedwithSelf.ts] ////
+
+//// [file1.ts]
+
+
 declare class C1 {}
 
 declare class C1 {}
@@ -9,4 +13,14 @@ interface C2 {}
 
 declare class C2 {}
 
-//// [declaredClassMergedwithSelf.js]
+//// [file2.ts]
+
+declare class C3 { }
+
+//// [file3.ts]
+
+declare class C3 { }
+
+//// [file1.js]
+//// [file2.js]
+//// [file3.js]
---
+++ tests/baselines/reference/duplicateIdentifiersAcrossFileBoundaries.js
@@ -0,0 +1,76 @@
+//// [tests/cases/compiler/duplicateIdentifiersAcrossFileBoundaries.ts] ////
+
+//// [file1.ts]
+interface I { }
+class C1 { }
+class C2 { }
+function f() { }
+var v = 3;
+
+class Foo {
+    static x: number;
+}
+
+module N {
+    export module F {
+        var t;
+    }
+}
+
+//// [file2.ts]
+class I { } // error -- cannot merge interface with non-ambient class
+interface C1 { } // error -- cannot merge interface with non-ambient class
+function C2() { } // error -- cannot merge function with non-ambient class
+class f { } // error -- cannot merge function with non-ambient class
+var v = 3;
+
+module Foo {
+    export var x: number; // error for redeclaring var in a different parent
+}
+
+declare module N {
+    export function F(); // no error because function is ambient
+}
+
+
+//// [file1.js]
+var C1 = (function () {
+    function C1() {
+    }
+    return C1;
+})();
+var C2 = (function () {
+    function C2() {
+    }
+    return C2;
+})();
+function f() { }
+var v = 3;
+var Foo = (function () {
+    function Foo() {
+    }
+    return Foo;
+})();
+var N;
+(function (N) {
+    var F;
+    (function (F) {
+        var t;
+    })(F = N.F || (N.F = {}));
+})(N || (N = {}));
+//// [file2.js]
+var I = (function () {
+    function I() {
+    }
+    return I;
+})(); // error -- cannot merge interface with non-ambient class
+function C2() { } // error -- cannot merge function with non-ambient class
+var f = (function () {
+    function f() {
+    }
+    return f;
+})(); // error -- cannot merge function with non-ambient class
+var v = 3;
+var Foo;
+(function (Foo) {
+})(Foo || (Foo = {}));
