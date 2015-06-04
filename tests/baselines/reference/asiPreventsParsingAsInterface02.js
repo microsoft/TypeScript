8af07ee0c0e4d8ea9e33889aa1038ae17dff13ef
Daniel Rosenwasser <drosen@microsoft.com>
2015-06-04 14:03:00 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/asiPreventsParsingAsInterface02.js
@@ -0,0 +1,11 @@
+//// [asiPreventsParsingAsInterface02.ts]
+
+function f(interface: number, I: string) {
+    interface   // This should be the identifier 'interface'
+    I           // This should be the identifier 'I'
+    {}          // This should be a block body
+}
+
+//// [asiPreventsParsingAsInterface02.js]
+function f(interface, I) {
+}
