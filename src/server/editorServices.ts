d8ef02a7f521b2845ac77da0c2b36f5a44df4685
Bill Ticehurst <billti@microsoft.com>
2015-06-05 16:59:04 -0700

    Added non-ts file support to the server

--- src/server/editorServices.ts
+++ src/server/editorServices.ts
@@ -966,7 +966,9 @@ module ts.server {
                 this.setCompilerOptions(opt);
             }
             else {
-                this.setCompilerOptions(ts.getDefaultCompilerOptions());
+                var defaultOpts = ts.getDefaultCompilerOptions();
+                defaultOpts.allowNonTsExtensions = true;
+                this.setCompilerOptions(defaultOpts);
             }
             this.languageService = ts.createLanguageService(this.host, this.documentRegistry);
             this.classifier = ts.createClassifier();
