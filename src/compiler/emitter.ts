26a1ed60171236ea9af4dcc2991537d134eb3164
Vladimir Matveev <vladima@microsoft.com>
2015-06-08 18:31:56 -0700

    move temp declarations out of object literal

--- src/compiler/emitter.ts
+++ src/compiler/emitter.ts
@@ -5327,10 +5327,10 @@ var __param = (this && this.__param) || function (paramIndex, decorator) {
                 emitSetters(exportStarFunction);
                 writeLine();
                 emitExecute(node, startIndex);
-                emitTempDeclarations(/*newLine*/ true)
                 decreaseIndent();
                 writeLine();
                 write("}"); // return
+                emitTempDeclarations(/*newLine*/ true)
             }
 
             function emitSetters(exportStarFunction: string) {
