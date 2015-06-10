8ffafff09b02acf02819b548774469cb25600e16
Сергей Щипанов <breate.by@gmail.com>
2015-06-10 12:59:30 -0700

    Update lib.d.ts
    Add new constructor to formData.

--- bin/lib.d.ts
+++ bin/lib.d.ts
@@ -6736,6 +6736,7 @@ interface FormData {
 declare var FormData: {
     prototype: FormData;
     new(): FormData;
+    new (form?: HTMLFormElement): FormData;
 }
 
 interface GainNode extends AudioNode {
