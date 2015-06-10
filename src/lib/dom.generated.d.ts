13493aba8f3e34cc2306ca98874be31e9e862a5c
matheusmb <matheusmb@outlook.com>
2015-06-10 03:51:37 -0300

    Fix HTMLSelectElement property type "options"

--- src/lib/dom.generated.d.ts
+++ src/lib/dom.generated.d.ts
@@ -6081,7 +6081,7 @@ interface HTMLSelectElement extends HTMLElement {
       * Sets or retrieves the name of the object.
       */
     name: string;
-    options: HTMLSelectElement;
+    options: NodeListOf<HTMLOptionElement>;
     /**
       * When present, marks an element that can't be submitted without a value.
       */
