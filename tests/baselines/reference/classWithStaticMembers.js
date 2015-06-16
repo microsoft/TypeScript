835f91137c47ed08db90d876359cc735212af73d
Anders Hejlsberg <andersh@microsoft.com>
2015-06-16 12:18:31 -0700

    Accepting new baselines

--- tests/baselines/reference/classWithStaticMembers.js
+++ tests/baselines/reference/classWithStaticMembers.js
@@ -23,8 +23,7 @@ var r3 = r.foo;
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var C = (function () {
     function C(a, b) {
--- tests/baselines/reference/es6ClassTest2.js
+++ tests/baselines/reference/es6ClassTest2.js
@@ -162,8 +162,7 @@ var ccwc = new ChildClassWithoutConstructor(1, "s");
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var BasicMonster = (function () {
     function BasicMonster(name, health) {
