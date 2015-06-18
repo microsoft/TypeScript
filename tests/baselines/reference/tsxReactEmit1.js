46eb5216ffc851c4ee214f773872829a3209ab8d
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2015-06-18 14:32:14 -0700

    Missed a few files in baseline-accept

--- tests/baselines/reference/tsxReactEmit1.js
+++ tests/baselines/reference/tsxReactEmit1.js
@@ -68,6 +68,6 @@ var SomeClass = (function () {
     };
     return SomeClass;
 })();
-var whitespace1 = React.createElement("div", null, "      ");
-var whitespace2 = React.createElement("div", null, "  ", p, "    ");
+var whitespace1 = React.createElement("div", null);
+var whitespace2 = React.createElement("div", null, p);
 var whitespace3 = React.createElement("div", null, p);
