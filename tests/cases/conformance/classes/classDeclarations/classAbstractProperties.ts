868138834bdef7434a4b3fd52cfb075115b747ec
Arthur Ozga <aozgaa@umich.edu>
2015-06-18 16:07:10 -0700

    added tests

---
+++ tests/cases/conformance/classes/classDeclarations/classAbstractProperties.ts
@@ -0,0 +1,11 @@
+abstract class A {
+    abstract x : number;
+    public abstract y : number;
+    protected abstract z : number;
+    private abstract w : number;
+    
+    abstract foo_x() : number;
+    public abstract foo_y() : number;
+    protected abstract foo_z() : number;
+    private abstract foo_w() : number;
+}
