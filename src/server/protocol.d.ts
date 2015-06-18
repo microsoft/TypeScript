65a8a5b912648dc7fae31ad5f4bd235936ec509e
Zhengbo Li <zhengbli@microsoft.com>
2015-06-18 14:51:24 -0700

    Add comments

--- src/server/protocol.d.ts
+++ src/server/protocol.d.ts
@@ -855,12 +855,28 @@ declare module ts.server.protocol {
     export interface SignatureHelpResponse extends Response {
         body?: SignatureHelpItems;
     }
-
+    
+    /**
+      * Arguments for geterrForProject request.
+      */
     export interface GeterrForProjectRequestArgs {
+        /**
+          * the file requesting project error list
+          */
         file: string;
+
+        /**
+          * Delay in milliseconds to wait before starting to compute
+          * errors for the files in the file list
+          */
         delay: number;
     }
 
+    /**
+      * GeterrForProjectRequest request; value of command field is 
+      * "geterrForProject". It works similarly with 'Geterr', only 
+      * it request for every file in this project.
+      */
     export interface GeterrForProjectRequest extends Request {
         arguments: GeterrForProjectRequestArgs
     }
