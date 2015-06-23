b159944f0a5eb662988f6f6cabcfc787b72ff294
Daniel Rosenwasser <drosen@microsoft.com>
2015-06-22 17:50:15 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/emptyArrayBindingPatternParameter04.js
@@ -0,0 +1,11 @@
+//// [emptyArrayBindingPatternParameter04.ts]
+
+
+function f([] = [1,2,3,4]) {
+    var x, y, z;
+}
+
+//// [emptyArrayBindingPatternParameter04.js]
+function f(_a) {
+    var x, y, z;
+}
---
+++ tests/baselines/reference/emptyObjectBindingPatternParameter04.js
@@ -0,0 +1,11 @@
+//// [emptyObjectBindingPatternParameter04.ts]
+
+
+function f({} = {a: 1, b: "2", c: true}) {
+    var x, y, z;
+}
+
+//// [emptyObjectBindingPatternParameter04.js]
+function f(_a) {
+    var x, y, z;
+}
