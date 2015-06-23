740fd9c908380147212ecd9a2d77b238e13c7172
Daniel Rosenwasser <drosen@microsoft.com>
2015-06-23 15:05:56 -0700

    Updated baselines.

--- tests/baselines/reference/declarationEmitDestructuring4.js
+++ tests/baselines/reference/declarationEmitDestructuring4.js
@@ -16,12 +16,16 @@ function baz4({} = { x: 10 }) { }
 // we will not make any modification and will emit
 // the similar binding pattern users' have written
 function baz(_a) { }
-function baz1(_a) { }
+function baz1(_a) {
+    var _a = [1, 2, 3];
+}
 function baz2(_a) {
     var _b = (_a === void 0 ? [[1, 2, 3]] : _a)[0];
 }
 function baz3(_a) { }
-function baz4(_a) { }
+function baz4(_a) {
+    var _a = { x: 10 };
+}
 
 
 //// [declarationEmitDestructuring4.d.ts]
--- tests/baselines/reference/emptyObjectBindingPatternParameter04.js
+++ tests/baselines/reference/emptyObjectBindingPatternParameter04.js
@@ -7,5 +7,6 @@ function f({} = {a: 1, b: "2", c: true}) {
 
 //// [emptyObjectBindingPatternParameter04.js]
 function f(_a) {
+    var _a = { a: 1, b: "2", c: true };
     var x, y, z;
 }
