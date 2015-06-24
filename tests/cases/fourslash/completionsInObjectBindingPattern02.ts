c6498d79e57fa8ca018b3b7060b7d81c3c487a5e
Daniel Rosenwasser <drosen@microsoft.com>
2015-06-24 11:28:56 -0400

    Changed ordering of verifications.

--- tests/cases/fourslash/completionsInObjectBindingPattern02.ts
+++ tests/cases/fourslash/completionsInObjectBindingPattern02.ts
@@ -9,5 +9,5 @@
 ////var { property1, /**/ } = foo;
 
 goTo.marker();
-verify.not.completionListContains("property1");
 verify.completionListContains("property2");
+verify.not.completionListContains("property1");
