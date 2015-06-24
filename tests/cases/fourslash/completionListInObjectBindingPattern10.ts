02e01d7afcf03af039f2452e1b242b2aac2c39b3
Daniel Rosenwasser <drosen@microsoft.com>
2015-06-24 17:13:47 -0400

    Added tests.

---
+++ tests/cases/fourslash/completionListInObjectBindingPattern10.ts
@@ -0,0 +1,23 @@
+/// <reference path='fourslash.ts'/>
+
+////interface I {
+////    propertyOfI_1: number;
+////    propertyOfI_2: string;
+////}
+////interface J {
+////    property1: I;
+////    property2: string;
+////}
+////
+////var foo: J[];
+////var [{ property1: { propertyOfI_1, }, /*1*/ }, { /*2*/ }] = foo;
+
+goTo.marker("1");
+verify.completionListContains("property2");
+verify.not.completionListContains("property1");
+verify.not.completionListContains("propertyOfI_2");
+verify.not.completionListContains("propertyOfI_1");
+
+goTo.marker("2");
+verify.completionListContains("property1");
+verify.completionListContains("property2");
