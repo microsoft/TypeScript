03980ccf0c5d014f5ecda360e1ae7c7c5a601eda
Yui T <yuisu@microsoft.com>
2015-06-25 15:39:03 -0700

    Add completion test for class expression

---
+++ tests/cases/fourslash/completionEntryInClassExpressionWithShadowingName.ts
@@ -0,0 +1,22 @@
+﻿///<reference path="fourslash.ts" />
+
+//// class myClass { /*0*/ }
+//// /*1*/
+//// var x = class myClass {
+////    getClassName (){
+////        m/*2*/
+////    }
+////    /*3*/
+//// }
+
+goTo.marker("0");
+verify.completionListContains("myClass", "class myClass", /*documentation*/ undefined, "class");
+
+goTo.marker("1");
+verify.completionListContains("myClass", "class myClass", /*documentation*/ undefined, "class");
+
+goTo.marker("2");
+verify.completionListContains("myClass", "(local class) myClass", /*documentation*/ undefined, "local class");
+
+goTo.marker("3");
+verify.completionListContains("myClass", "(local class) myClass", /*documentation*/ undefined, "local class");
