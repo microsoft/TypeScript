d5710349a29041290d2d2ce9e875e03c18fd79ae
Anders Hejlsberg <andersh@microsoft.com>
2015-06-24 17:53:43 -0700

    Adding tests

---
+++ tests/baselines/reference/intersectionTypeInference.js
@@ -0,0 +1,28 @@
+//// [intersectionTypeInference.ts]
+
+function extend<T, U>(obj1: T, obj2: U): T & U {
+    var result: T & U;
+    obj1 = result;
+    obj2 = result;
+    result = obj1;  // Error
+    result = obj2;  // Error
+    return result;
+}
+
+var x = extend({ a: "hello" }, { b: 42 });
+var s = x.a;
+var n = x.b;
+
+
+//// [intersectionTypeInference.js]
+function extend(obj1, obj2) {
+    var result;
+    obj1 = result;
+    obj2 = result;
+    result = obj1; // Error
+    result = obj2; // Error
+    return result;
+}
+var x = extend({ a: "hello" }, { b: 42 });
+var s = x.a;
+var n = x.b;
