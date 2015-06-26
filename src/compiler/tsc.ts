72050073bc9d6448dc4c7114b53c4e7fcb9b6ed5
Mohamed Hegazy <mhegazy@microsoft.com>
2015-06-26 10:25:58 -0700

    use double quotes

--- src/compiler/tsc.ts
+++ src/compiler/tsc.ts
@@ -192,7 +192,7 @@ namespace ts {
         }
 
         // Firefox has Object.prototype.watch
-        if (commandLine.options.watch && commandLine.options.hasOwnProperty('watch')) {
+        if (commandLine.options.watch && commandLine.options.hasOwnProperty("watch")) {
             if (!sys.watchFile) {
                 reportDiagnostic(createCompilerDiagnostic(Diagnostics.The_current_host_does_not_support_the_0_option, "--watch"));
                 return sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
