9c9e29877d03691abe576d67b1b951aa31c6c71c
Yui T <yuisu@microsoft.com>
2015-06-25 18:21:52 -0700

    Add test for completion in function expression

---
+++ tests/cases/fourslash/completionListInNamedFunctionExpressionWithShadowing.ts
@@ -0,0 +1,19 @@
+﻿///<reference path="fourslash.ts" />
+
+//// function foo() {}
+//// /*0*/
+//// var x = function foo() {
+////    /*1*/
+//// }
+//// var y = function () {
+////    /*2*/
+//// }
+
+goTo.marker("0");
+verify.completionListContains("foo", "function foo(): void", /*documentation*/ undefined, "function");
+
+goTo.marker("1");
+verify.completionListContains("foo", "(local function) foo(): void", /*documentation*/ undefined, "local function");
+
+goTo.marker("2");
+verify.completionListContains("foo", "function foo(): void", /*documentation*/ undefined, "function")
