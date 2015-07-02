3af3177fe3bae91761611fa419aedaaf5842080e
Arthur Ozga <aozgaa@umich.edu>
2015-07-02 10:26:36 -0700

    update baselines

--- tests/baselines/reference/classAbstractMergedDeclaration.js
+++ tests/baselines/reference/classAbstractMergedDeclaration.js
@@ -17,12 +17,28 @@ class CC1 {}
 class CC2 {}
 abstract class CC2 {}
 
+declare abstract class DCI {}
+interface DCI {}
+
+interface DIC {}
+declare abstract class DIC {}
+
+declare abstract class DCC1 {}
+declare class DCC1 {}
+
+declare class DCC2 {}
+declare abstract class DCC2 {}
+
 new CM;
 new MC;
 new CI;
 new IC;
 new CC1;
 new CC2;
+new DCI;
+new DIC;
+new DCC1;
+new DCC2;
 
 //// [classAbstractMergedDeclaration.js]
 var CM = (function () {
@@ -71,3 +87,7 @@ new CI;
 new IC;
 new CC1;
 new CC2;
+new DCI;
+new DIC;
+new DCC1;
+new DCC2;
