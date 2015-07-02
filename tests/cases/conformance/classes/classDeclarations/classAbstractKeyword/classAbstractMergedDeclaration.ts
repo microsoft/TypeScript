c06e5eb8b493e377e44400aaa4307d00524ee4bf
Arthur Ozga <aozgaa@umich.edu>
2015-07-02 10:26:23 -0700

    Update test

--- tests/cases/conformance/classes/classDeclarations/classAbstractKeyword/classAbstractMergedDeclaration.ts
+++ tests/cases/conformance/classes/classDeclarations/classAbstractKeyword/classAbstractMergedDeclaration.ts
@@ -16,9 +16,25 @@ class CC1 {}
 class CC2 {}
 abstract class CC2 {}
 
+declare abstract class DCI {}
+interface DCI {}
+
+interface DIC {}
+declare abstract class DIC {}
+
+declare abstract class DCC1 {}
+declare class DCC1 {}
+
+declare class DCC2 {}
+declare abstract class DCC2 {}
+
 new CM;
 new MC;
 new CI;
 new IC;
 new CC1;
 new CC2;
+new DCI;
+new DIC;
+new DCC1;
+new DCC2;
