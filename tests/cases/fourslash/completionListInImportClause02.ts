97dd855663e48ee6d84e99869fc70c543dba5193
Daniel Rosenwasser <drosen@microsoft.com>
2015-07-06 15:33:49 -0700

    Fix test.

--- tests/cases/fourslash/completionListInImportClause02.ts
+++ tests/cases/fourslash/completionListInImportClause02.ts
@@ -1,10 +1,5 @@
 /// <reference path='fourslash.ts'/>
 
-// @Filename: m1.ts
-////export var foo: number = 1;
-////export function bar() { return 10; }
-////export function baz() { return 10; }
-
 ////declare module "M1" {
 ////    export var V;
 ////}
