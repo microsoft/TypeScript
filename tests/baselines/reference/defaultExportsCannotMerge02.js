17593bf444f7a5b7843e71b02798008dd578c7b5
Daniel Rosenwasser <drosen@microsoft.com>
2015-07-15 17:51:11 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/defaultExportsCannotMerge02.js
@@ -0,0 +1,35 @@
+//// [tests/cases/conformance/es6/modules/defaultExportsCannotMerge02.ts] ////
+
+//// [m1.ts]
+
+export default class foo {
+
+}
+
+export default function bar() {
+
+}
+
+var x = 10;
+export default x;
+
+//// [m2.ts]
+import Entity from "m1"
+
+Entity();
+
+//// [m1.js]
+var foo = (function () {
+    function foo() {
+    }
+    return foo;
+})();
+exports.default = foo;
+function bar() {
+}
+exports.default = bar;
+var x = 10;
+exports.default = x;
+//// [m2.js]
+var m1_1 = require("m1");
+m1_1.default();
---
+++ tests/baselines/reference/defaultExportsCannotMerge03.js
@@ -0,0 +1,27 @@
+//// [tests/cases/conformance/es6/modules/defaultExportsCannotMerge03.ts] ////
+
+//// [m1.ts]
+
+export default function foo() {
+
+}
+
+export default function bar() {
+
+}
+
+//// [m2.ts]
+import Entity from "m1"
+
+Entity();
+
+//// [m1.js]
+function foo() {
+}
+exports.default = foo;
+function bar() {
+}
+exports.default = bar;
+//// [m2.js]
+var m1_1 = require("m1");
+m1_1.default();
