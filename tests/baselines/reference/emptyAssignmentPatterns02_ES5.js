0ec38b759004d6040cd0695f4148840832c71e21
Daniel Rosenwasser <drosen@microsoft.com>
2015-07-16 17:25:00 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/emptyAssignmentPatterns02_ES5.js
@@ -0,0 +1,14 @@
+//// [emptyAssignmentPatterns02_ES5.ts]
+
+var a: any;
+let x, y, z, a1, a2, a3;
+
+({} = { x, y, z } = a);
+([] = [ a1, a2, a3] = a);
+
+//// [emptyAssignmentPatterns02_ES5.js]
+var a;
+var x, y, z, a1, a2, a3;
+(_a = (x = a.x, y = a.y, z = a.z, a), _a);
+(_b = (a1 = a[0], a2 = a[1], a3 = a[2], a), _b);
+var _a, _b;
---
+++ tests/baselines/reference/emptyAssignmentPatterns03_ES5.js
@@ -0,0 +1,12 @@
+//// [emptyAssignmentPatterns03_ES5.ts]
+
+var a: any;
+
+({} = {} = a);
+([] = [] = a);
+
+//// [emptyAssignmentPatterns03_ES5.js]
+var a;
+(_a = (, a), _a);
+(_b = (, a), _b);
+var _a, _b;
