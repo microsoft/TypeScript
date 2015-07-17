53e449c90149fe5929d35c843c87128f386e179d
unknown <tihoanh@microsoft.com>
2015-07-16 23:33:42 -0700

    Allow tsconfig.json to be added to fourslash test.

---
+++ tests/cases/fourslash/server/projectInfo02.ts
@@ -0,0 +1,15 @@
+/// <reference path="../fourslash.ts"/>
+
+// @Filename: a.ts
+////export var test = "test String"
+
+// @Filename: b.ts
+////export var test2 = "test String"
+
+// @Filename: tsconfig.json
+////{ "files": ["a.ts", "b.ts"] }
+
+debugger;
+
+goTo.file("a.ts")
+verify.ProjectInfo(["lib.d.ts", "a.ts", "b.ts"])
