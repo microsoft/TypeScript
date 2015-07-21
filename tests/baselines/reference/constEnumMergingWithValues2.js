43347c79ec1e0cd43551ab183558ec96076e01e4
Vladimir Matveev <vladima@microsoft.com>
2015-07-20 21:49:15 -0700

    treat modules that are merged with values as non-const-enum

---
+++ tests/baselines/reference/constEnumMergingWithValues2.js
@@ -0,0 +1,18 @@
+//// [constEnumMergingWithValues2.ts]
+
+class foo {}
+module foo {
+    const enum E { X }
+}
+
+export = foo
+
+//// [constEnumMergingWithValues2.js]
+define(["require", "exports"], function (require, exports) {
+    var foo = (function () {
+        function foo() {
+        }
+        return foo;
+    })();
+    return foo;
+});
