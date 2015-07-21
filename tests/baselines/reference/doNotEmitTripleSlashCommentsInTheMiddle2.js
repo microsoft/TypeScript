deb6eeebe1dd29be1a3a4afcc0ef1fa83131b710
Yui T <yuisu@microsoft.com>
2015-07-21 09:06:17 -0700

    Add tests

---
+++ tests/baselines/reference/doNotEmitTripleSlashCommentsInTheMiddle2.js
@@ -0,0 +1,14 @@
+//// [tests/cases/compiler/doNotEmitTripleSlashCommentsInTheMiddle2.ts] ////
+
+//// [file0.ts]
+
+
+//// [file1.ts]
+
+/// <reference path="file0.ts" />
+declare var OData: any;
+/// <reference path="file0.ts" />
+
+//// [file0.js]
+//// [file1.js]
+/// <reference path="file0.ts" />
---
+++ tests/cases/compiler/doNotEmitTripleSlashCommentsInTheMiddle2.ts
@@ -0,0 +1,9 @@
+﻿// @comments: false
+
+// @Filename: file0.ts
+
+// @Filename: file1.ts
+
+/// <reference path="file0.ts" />
+declare var OData: any;
+/// <reference path="file0.ts" />
