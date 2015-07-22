2f7a597dcaaf8e877ad972878b63516b43f966e1
Arthur Ozga <aozgaa@umich.edu>
2015-07-21 18:27:01 -0700

    Qualification

--- tests/cases/fourslash/fourslash.ts
+++ tests/cases/fourslash/fourslash.ts
@@ -30,6 +30,7 @@
 // (e.g. 'fs.goTo.eof();')
 
 //---------------------------------------
+// For API editors:
 // When editting this file, and only while editing this file, enable the reference comments
 // and comment out the declarations in this section to get proper type information.
 // Undo these changes before compiling/committing/editing any other fourslash tests.
