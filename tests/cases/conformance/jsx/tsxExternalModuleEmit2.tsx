fc9b10eeb64678e69ff7ce9a229bb6c754874cda
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2015-07-28 13:49:39 -0700

    Correctly identify JSX expressions as identifier parents

---
+++ tests/cases/conformance/jsx/tsxExternalModuleEmit2.tsx
@@ -0,0 +1,14 @@
+//@jsx: react
+//@module: commonjs
+
+//@filename: modules.d.ts
+declare module 'mod' {
+  var y: any;
+  export default y;
+}
+
+//@filename: app.tsx
+import Main from 'mod';
+declare var Foo, React;
+// Should see mod_1['default'] in emit here
+<Foo handler={Main}></Foo>
