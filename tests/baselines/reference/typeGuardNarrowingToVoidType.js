54efca0f7d5cc87f031a653c852a5b23bc63545e
Tingan Ho <tingan87@gmail.com>
2015-07-28 15:48:21 +0800

    Adds void narrowing tests

---
+++ tests/baselines/reference/typeGuardNarrowingToVoidType.js
@@ -0,0 +1,144 @@
+//// [typeGuardNarrowingToVoidType.ts]
+let x: number | string | boolean;
+
+if (typeof x === "number") {
+    x.toPrecision();
+}
+else if (typeof x === "string") {
+    x.charCodeAt(1);
+}
+else if(typeof x === "boolean") {
+    x.valueOf();
+}
+else {
+    x
+}
+
+if(typeof x === "boolean") {
+    x.valueOf();
+}
+else if (typeof x === "number") {
+    x.toPrecision();
+}
+else if (typeof x === "string") {
+    x.charCodeAt(1);
+}
+else if(typeof x === "boolean") {
+    x.valueOf();
+}
+else {
+    x
+}
+
+class A { a: string; }
+class B { b: string; }
+class C { c: string; }
+
+declare function isA(x: any): x is A;
+declare function isB(x: any): x is B;
+declare function isC(x: any): x is C;
+
+let y: A | B | C;
+
+if (isA(y)) {
+    y.a;
+}
+else if(isB(y)){
+    y.b;
+}
+else if(isC(y)) {
+    y.c;
+}
+else{
+    y
+}
+
+if (isB(y)) {
+    y.b;
+}
+else if (isA(y)) {
+    y.a;
+}
+else if(isB(y)){
+    y.b;
+}
+else if(isC(y)) {
+    y.c;
+}
+else{
+    y
+}
+
+
+//// [typeGuardNarrowingToVoidType.js]
+var x;
+if (typeof x === "number") {
+    x.toPrecision();
+}
+else if (typeof x === "string") {
+    x.charCodeAt(1);
+}
+else if (typeof x === "boolean") {
+    x.valueOf();
+}
+else {
+    x;
+}
+if (typeof x === "boolean") {
+    x.valueOf();
+}
+else if (typeof x === "number") {
+    x.toPrecision();
+}
+else if (typeof x === "string") {
+    x.charCodeAt(1);
+}
+else if (typeof x === "boolean") {
+    x.valueOf();
+}
+else {
+    x;
+}
+var A = (function () {
+    function A() {
+    }
+    return A;
+})();
+var B = (function () {
+    function B() {
+    }
+    return B;
+})();
+var C = (function () {
+    function C() {
+    }
+    return C;
+})();
+var y;
+if (isA(y)) {
+    y.a;
+}
+else if (isB(y)) {
+    y.b;
+}
+else if (isC(y)) {
+    y.c;
+}
+else {
+    y;
+}
+if (isB(y)) {
+    y.b;
+}
+else if (isA(y)) {
+    y.a;
+}
+else if (isB(y)) {
+    y.b;
+}
+else if (isC(y)) {
+    y.c;
+}
+else {
+    y;
+}
