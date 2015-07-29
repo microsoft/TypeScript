dff88098a04895da5ceb227a90be1d6069712866
Jason Freeman <jfreeman@microsoft.com>
2015-07-29 15:21:15 -0700

    Add a test

---
+++ tests/baselines/reference/infiniteExpansionCaching.js
@@ -0,0 +1,36 @@
+//// [infiniteExpansionCaching.ts]
+interface A<T> {
+    p: A<C<T>>;
+    s: C<T>;
+}
+
+interface B<U> {
+    p: B<D<U>>;
+    s: D<U>;
+}
+
+interface C<T> {
+    q: T;
+}
+
+interface D<U> {
+    q: U;
+}
+
+var a: A<C<string>>;
+var b: B<D<number>>;
+
+a = b;
+
+var c: C<string>;
+var d: D<number>;
+
+c = d; // Should not be allowed
+
+//// [infiniteExpansionCaching.js]
+var a;
+var b;
+a = b;
+var c;
+var d;
+c = d; // Should not be allowed
---
+++ tests/cases/conformance/types/typeRelationships/recursiveTypes/infiniteExpansionCaching.ts
@@ -0,0 +1,27 @@
+interface A<T> {
+    p: A<C<T>>;
+    s: C<T>;
+}
+
+interface B<U> {
+    p: B<D<U>>;
+    s: D<U>;
+}
+
+interface C<T> {
+    q: T;
+}
+
+interface D<U> {
+    q: U;
+}
+
+var a: A<C<string>>;
+var b: B<D<number>>;
+
+a = b;
+
+var c: C<string>;
+var d: D<number>;
+
+c = d; // Should not be allowed
