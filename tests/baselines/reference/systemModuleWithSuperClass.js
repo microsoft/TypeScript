bfc4dd14da4ee7c3df2ef6dc5fa148530db12ecd
Sheetal Nandi <shkamat@microsoft.com>
2015-07-31 11:54:03 -0700

    Emit the helpers inside the module body Fixes #3655

--- tests/baselines/reference/systemModuleWithSuperClass.js
+++ tests/baselines/reference/systemModuleWithSuperClass.js
@@ -28,12 +28,12 @@ System.register([], function(exports_1) {
     }
 });
 //// [bar.js]
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
 System.register(['./foo'], function(exports_1) {
+    var __extends = (this && this.__extends) || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
     var foo_1;
     var Bar;
     return {
