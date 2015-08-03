42d8328b0c1962e4b21e7a04ba7e7e0c6b5b5627
Sheetal Nandi <shkamat@microsoft.com>
2015-08-03 13:31:56 -0700

    Add testcase for #4101

---
+++ tests/baselines/reference/exportClauseEmit.js
@@ -0,0 +1,16 @@
+//// [exportClauseEmit.ts]
+var str = "Hello";
+// Change str
+str = "Hello World!!!";
+export {str};
+
+//// [exportClauseEmit.js]
+var str = "Hello";
+exports.str = str;
+// Change str
+str = "Hello World!!!";
+
+
+//// [exportClauseEmit.d.ts]
+declare var str: string;
+export { str };
