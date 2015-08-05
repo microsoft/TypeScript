9dfa9c72edd4aa40fada47fb9deb992b29a77303
AbubakerB <abubaker_bashir@hotmail.com>
2015-08-06 00:51:49 +0100

    Add tests for issue #2341

---
+++ tests/cases/conformance/classes/constructorDeclarations/classConstructorAccessibility2.ts
@@ -0,0 +1,49 @@
+class A {
+	private constructor(a: string) // only private access
+	private constructor() { 
+		
+	}
+	
+	static method() {
+		var t1 = new A("test"); // private is accessible in static method
+	}
+	
+	method() {
+		var t1 = new A("1"); // private is accessible in own class
+	}
+}
+
+class A_ext extends A { // Cannot extend private class A
+	method() {
+		var t1 = new A(""); // error - A is private and only accessible in it's own class
+	}
+}
+
+class B {
+	protected constructor() {
+	}
+	
+	method() {
+		var t1 = new B(); // protected is accessible in own class
+	}
+}
+
+class B_ext extends B {
+	method() {
+		var t1 = new B(); // protected is accessible in sub-class
+	}
+}
+
+class C {
+	public constructor(){
+		
+	}
+	methodA() { 
+		var t1 = new C(); // public is accessible anywhere
+	}
+}
+
+// check global scope
+var t1 = new A(""); // error - A is private
+var t2 = new B(); // error - B is protected 
+var t3 = new C();
