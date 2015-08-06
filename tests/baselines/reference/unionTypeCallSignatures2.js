e0cb1603e29cd3357f53f139f7557e955c68a3ec
Anders Hejlsberg <andersh@microsoft.com>
2015-08-06 15:21:53 -0700

    Accepting new baselines

---
+++ tests/baselines/reference/unionTypeCallSignatures2.js
@@ -0,0 +1,32 @@
+//// [unionTypeCallSignatures2.ts]
+interface A {
+    (x: number): number;
+    (x: string, y?: string): boolean;
+    (x: Date): void;
+    <T>(x: T[]): T[];
+}
+
+interface B {
+    (x: number): number;
+    (x: string): string;
+    (x: Date): void;
+    <T>(x: T[]): T[];
+}
+
+interface C {
+    (x: string, ...y: string[]): number;
+    (x: number, s?: string): number;
+    <T>(x: T[]): T[];
+}
+
+var f: A | B | C;
+var n = f(42);             // number
+var s = f("abc");          // boolean | string | number
+var a = f([true, false]);  // boolean[]
+
+
+//// [unionTypeCallSignatures2.js]
+var f;
+var n = f(42); // number
+var s = f("abc"); // boolean | string | number
+var a = f([true, false]); // boolean[]
