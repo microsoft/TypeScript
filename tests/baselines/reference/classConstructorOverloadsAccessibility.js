6d01766acd2aeeac59b0ff936d954a9459a9c770
AbubakerB <abubaker_bashir@hotmail.com>
2015-08-06 01:05:29 +0100

    Accept baselines

---
+++ tests/baselines/reference/classConstructorOverloadsAccessibility.js
@@ -0,0 +1,55 @@
+//// [classConstructorOverloadsAccessibility.ts]
+class A {
+	public constructor(a: boolean) // error
+	protected constructor(a: number) // error
+	private constructor(a: string)
+	private constructor() { 
+		
+	}
+}
+
+class B {
+	protected constructor(a: number) // error
+	constructor(a: string)
+	constructor() { 
+		
+	}
+}
+
+class C {
+	protected constructor(a: number)
+	protected constructor(a: string)
+	protected constructor() { 
+		
+	}
+}
+
+class D {
+	constructor(a: number) // ok
+	constructor(a: string) // ok
+	public constructor() { 
+		
+	}
+}
+
+//// [classConstructorOverloadsAccessibility.js]
+var A = (function () {
+    function A() {
+    }
+    return A;
+})();
+var B = (function () {
+    function B() {
+    }
+    return B;
+})();
+var C = (function () {
+    function C() {
+    }
+    return C;
+})();
+var D = (function () {
+    function D() {
+    }
+    return D;
+})();
