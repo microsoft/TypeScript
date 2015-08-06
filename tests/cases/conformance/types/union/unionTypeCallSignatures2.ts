d317bd1340e94d0711058414fe4fd36cb0c08793
Anders Hejlsberg <andersh@microsoft.com>
2015-08-06 15:21:30 -0700

    Adding/fixing tests

---
+++ tests/cases/conformance/types/union/unionTypeCallSignatures2.ts
@@ -0,0 +1,24 @@
+﻿interface A {
+    (x: number): number;
+    (x: string, y?: string): boolean;
+    (x: Date): void;
+    <T>(x: T[]): T[];
+}
+
+interface B {
+    (x: number): number;
+    (x: string): string;
+    (x: Date): void;
+    <T>(x: T[]): T[];
+}
+
+interface C {
+    (x: string, ...y: string[]): number;
+    (x: number, s?: string): number;
+    <T>(x: T[]): T[];
+}
+
+var f: A | B | C;
+var n = f(42);             // number
+var s = f("abc");          // boolean | string | number
+var a = f([true, false]);  // boolean[]
