0eda412e3e9b9f190d928be96c2254e0139046d8
Sheetal Nandi <shkamat@microsoft.com>
2015-08-10 12:11:05 -0700

    Adds testcase for #4004

---
+++ tests/baselines/reference/amdReexportAmbientVar.js
@@ -0,0 +1,10 @@
+//// [amdReexportAmbientVar.ts]
+/// <amd-dependency path="text!extern_view.html" name="view"/>
+declare var view: string;
+export {view};
+export function test() { }
+
+//// [amdReexportAmbientVar.js]
+exports.view = view;
+function test() { }
+exports.test = test;
---
+++ tests/cases/compiler/amdReexportAmbientVar.ts
@@ -0,0 +1,5 @@
+//@module: commonjs
+/// <amd-dependency path="text!extern_view.html" name="view"/>
+declare var view: string;
+export {view};
+export function test() { }
