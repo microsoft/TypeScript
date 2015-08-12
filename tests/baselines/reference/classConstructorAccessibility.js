5421ac83e5f2f0f97c44b86f2d81c840cb4adfcb
AbubakerB <abubaker_bashir@hotmail.com>
2015-08-12 01:29:14 +0100

    Accept baselines

--- tests/baselines/reference/classConstructorAccessibility.js
+++ tests/baselines/reference/classConstructorAccessibility.js
@@ -1,4 +1,8 @@
 //// [classConstructorAccessibility.ts]
+class B {
+    constructor(public x: number) { }
+}
+
 class C {
     public constructor(public x: number) { }
 }
@@ -11,6 +15,7 @@ class E {
     protected constructor(public x: number) { }
 }
 
+var b = new B(1);
 var c = new C(1);
 var d = new D(1); // error - D is private
 var e = new E(1); // error - E is protected
@@ -28,13 +33,26 @@ module Generic {
         protected constructor(public x: T) { }
     }
 
+    var b = new B(1);
     var c = new C(1);
     var d = new D(1); // error - D is private
     var e = new E(1); // error - E is protected
 }
 
+// make sure signatures are covered.
+let sig: new(x: number) => any;
+sig = B;
+sig = C;
+sig = D; // error - non-public to public
+sig = E; // error - non-public to public
 
 //// [classConstructorAccessibility.js]
+var B = (function () {
+    function B(x) {
+        this.x = x;
+    }
+    return B;
+})();
 var C = (function () {
     function C(x) {
         this.x = x;
@@ -53,6 +71,7 @@ var E = (function () {
     }
     return E;
 })();
+var b = new B(1);
 var c = new C(1);
 var d = new D(1); // error - D is private
 var e = new E(1); // error - E is protected
@@ -76,7 +95,14 @@ var Generic;
         }
         return E;
     })();
+    var b = new B(1);
     var c = new C(1);
     var d = new D(1); // error - D is private
     var e = new E(1); // error - E is protected
 })(Generic || (Generic = {}));
+// make sure signatures are covered.
+var sig;
+sig = B;
+sig = C;
+sig = D; // error - non-public to public
+sig = E; // error - non-public to public
