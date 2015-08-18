bef4e8d58e8ce09bd6629d6e302edcb0f0cf7db0
Yui T <yuisu@microsoft.com>
2015-08-17 22:04:54 -0700

    remove commented test

--- tests/cases/fourslash/completionListInTypeParameterOfTypeAlias1.ts
+++ tests/cases/fourslash/completionListInTypeParameterOfTypeAlias1.ts
@@ -5,13 +5,12 @@
 ////type List4<T> = /*2*/T[];
 ////type List3<T1> = /*3*/;
 
-/*goTo.marker("0");
+goTo.marker("0");
 verify.completionListIsEmpty();
 goTo.marker("1");
 verify.not.completionListIsEmpty();
 goTo.marker("2");
 verify.completionListContains("T");
-*/
 goTo.marker("3");
 verify.not.completionListIsEmpty();
 verify.not.completionListContains("T");
