67f81bc53ceb8fcffee4b09eebfc334d26930a48
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2015-08-20 12:20:28 -0700

    Convert HTML entities to strings (#4125)

---
+++ tests/baselines/reference/tsxReactEmitEntities.js
@@ -0,0 +1,14 @@
+//// [tsxReactEmitEntities.tsx]
+declare module JSX {
+	interface Element { }
+	interface IntrinsicElements {
+		[s: string]: any;
+	}
+}
+declare var React: any;
+
+<div>Dot goes here: &middot; </div>;
+
+
+//// [tsxReactEmitEntities.js]
+React.createElement("div", null, "Dot goes here: · ");
