00d4b4cb501b2015157cc3d1f94afc3bb833213e
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2015-08-21 14:43:14 -0700

    Correctly identify JSX Expressions as Expression parent nodes
    Fixes bug #4404

---
+++ tests/cases/fourslash/tsxRename5.ts
@@ -0,0 +1,21 @@
+/// <reference path='fourslash.ts' />
+
+//@Filename: file.tsx
+//// declare module JSX {
+////     interface Element { }
+////     interface IntrinsicElements {
+////     }
+////     interface ElementAttributesProperty { props }
+//// }
+//// class MyClass {
+////   props: {
+////     name?: string;
+////     size?: number;
+//// }
+//// 
+//// var [|nn|]: string;
+//// var x = <MyClass name={[|n/**/n|]}></MyClass>;
+
+goTo.marker();
+debugger;
+verify.renameLocations(false, false);
