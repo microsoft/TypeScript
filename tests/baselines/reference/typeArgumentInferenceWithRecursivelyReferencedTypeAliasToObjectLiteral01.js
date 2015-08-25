73c4cdca824315be88b8326b8679be42f23cb82e
Daniel Rosenwasser <drosen@microsoft.com>
2015-08-25 15:53:19 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/typeArgumentInferenceWithRecursivelyReferencedTypeAliasToObjectLiteral01.js
@@ -0,0 +1,13 @@
+//// [typeArgumentInferenceWithRecursivelyReferencedTypeAliasToObjectLiteral01.ts]
+type TreeNode = {
+    name: string;
+    parent: TreeNode;
+}
+
+var nodes: TreeNode[];
+nodes.map(n => n.name);
+
+
+//// [typeArgumentInferenceWithRecursivelyReferencedTypeAliasToObjectLiteral01.js]
+var nodes;
+nodes.map(function (n) { return n.name; });
---
+++ tests/baselines/reference/typeArgumentInferenceWithRecursivelyReferencedTypeAliasToObjectLiteral02.js
@@ -0,0 +1,18 @@
+//// [typeArgumentInferenceWithRecursivelyReferencedTypeAliasToObjectLiteral02.ts]
+type TreeNode = {
+    name: string;
+    parent: TreeNode;
+}
+
+type TreeNodeMiddleman = {
+    name: string;
+    parent: TreeNode;
+}
+
+var nodes: TreeNodeMiddleman[];
+nodes.map(n => n.name);
+
+
+//// [typeArgumentInferenceWithRecursivelyReferencedTypeAliasToObjectLiteral02.js]
+var nodes;
+nodes.map(function (n) { return n.name; });
