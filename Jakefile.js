1670ce9664ed914ba2cd7c172caf8fe905261a81
Tingan Ho <tingan87@gmail.com>
2015-08-25 15:43:10 +0800

    Removes project init test

--- Jakefile.js
+++ Jakefile.js
@@ -141,8 +141,7 @@ var harnessSources = harnessCoreSources.concat([
     "session.ts",
     "versionCache.ts",
     "convertToBase64.ts",
-    "transpile.ts",
-    "projectInit.ts"
+    "transpile.ts"
 ].map(function (f) {
     return path.join(unittestsDirectory, f);
 })).concat([
