9b04405e201b61134aaa820e34b83de869296923
Yui T <yuisu@microsoft.com>
2015-08-25 16:53:12 -0700

    Add test and address PR

--- tests/cases/fourslash/completionListInTypeParameterOfClassExpression1.ts
+++ tests/cases/fourslash/completionListInTypeParameterOfClassExpression1.ts
@@ -4,6 +4,7 @@
 ////var C1 = class D</*1*/T> {}
 ////var C3 = class D<T, /*2*/
 ////var C4 = class D<T, /*3*/U>{}
+////var C5 = class D<T extends /*4*/>{}
 
 goTo.marker("0");
 verify.completionListIsEmpty();
@@ -13,3 +14,5 @@ goTo.marker("2");
 verify.completionListIsEmpty();
 goTo.marker("3");
 verify.completionListIsEmpty();
+goTo.marker("4");
+verify.not.completionListIsEmpty();
