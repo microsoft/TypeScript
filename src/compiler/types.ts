d615d21e6d3f3280895b68261764a64f439d22d9
Anders Hejlsberg <andersh@microsoft.com>
2015-08-26 06:41:41 -0700

    Add cache of anonymous object type instantiations to TypeMapper

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -1953,6 +1953,7 @@ namespace ts {
     /* @internal */
     export interface TypeMapper {
         (t: TypeParameter): Type;
+        instantiations?: Type[];    // Cache of instantiations created using this type mapper.
         context?: InferenceContext; // The inference context this mapper was created from.
                                     // Only inference mappers have this set (in createInferenceMapper).
                                     // The identity mapper and regular instantiation mappers do not need it.
