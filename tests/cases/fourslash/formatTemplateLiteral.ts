070353538158b62c7b877cc2db1be4ecfa5e6441
SaschaNaz <saschanaz@outlook.com>
2015-08-28 23:12:30 +0900

    Do not replace but do extend

--- tests/cases/fourslash/formatTemplateLiteral.ts
+++ tests/cases/fourslash/formatTemplateLiteral.ts
@@ -2,10 +2,12 @@
 ////var x = `sadasdasdasdasfegsfd
 /////*1*/rasdesgeryt35t35y35 e4 ergt er 35t 3535 `;
 ////var y = `1${2}/*2*/3`;
-////String.raw`foo`/*3*/
-////String.raw  `bar${3}`/*4*/
+////let z=    `foo`/*3*/
+////let w=  `bar${3}`/*4*/
 ////String.raw
 //// `template`/*5*/
+////String.raw`foo`/*6*/
+////String.raw  `bar${3}`/*7*/
 
 
 goTo.marker("1");
@@ -18,11 +20,17 @@ verify.currentLineContentIs("3`;")
 
 goTo.marker("3");
 edit.insert(";");
-verify.currentLineContentIs("String.raw `foo`;");
+verify.currentLineContentIs("let z = `foo`;"); 
 goTo.marker("4");
 edit.insert(";");
-verify.currentLineContentIs("String.raw `bar${3}`;");
+verify.currentLineContentIs("let w = `bar${3}`;"); 
 
 goTo.marker("5");
 edit.insert(";");
 verify.currentLineContentIs("    `template`;");
+goTo.marker("6");
+edit.insert(";");
+verify.currentLineContentIs("String.raw `foo`;");
+goTo.marker("7");
+edit.insert(";");
+verify.currentLineContentIs("String.raw `bar${3}`;");
