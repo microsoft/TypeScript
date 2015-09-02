906634f0a48e5842d633a9e07178269e47a91bbc
Daniel Rosenwasser <drosen@microsoft.com>
2015-09-01 17:45:47 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/emptyVariableDeclarationBindingPatterns01_ES5.js
@@ -0,0 +1,27 @@
+//// [emptyVariableDeclarationBindingPatterns01_ES5.ts]
+
+(function () {
+    var a: any;
+
+    var {} = a;
+    let {} = a;
+    const {} = a;
+
+    var [] = a;
+    let [] = a;
+    const [] = a;
+})();
+
+//// [emptyVariableDeclarationBindingPatterns01_ES5.js]
+(function () {
+    var a;
+    var ;
+    var ;
+    var ;
+    var ;
+    var ;
+    var ;
+})();
+
+
+//// [emptyVariableDeclarationBindingPatterns01_ES5.d.ts]
---
+++ tests/baselines/reference/emptyVariableDeclarationBindingPatterns01_ES6.js
@@ -0,0 +1,27 @@
+//// [emptyVariableDeclarationBindingPatterns01_ES6.ts]
+
+(function () {
+    var a: any;
+
+    var {} = a;
+    let {} = a;
+    const {} = a;
+
+    var [] = a;
+    let [] = a;
+    const [] = a;
+})();
+
+//// [emptyVariableDeclarationBindingPatterns01_ES6.js]
+(function () {
+    var a;
+    var {  } = a;
+    let {  } = a;
+    const {  } = a;
+    var [] = a;
+    let [] = a;
+    const [] = a;
+})();
+
+
+//// [emptyVariableDeclarationBindingPatterns01_ES6.d.ts]
