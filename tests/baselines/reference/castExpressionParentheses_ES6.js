32d8a22990612e3616068960042803d63b5b3150
vilicvane <i@vilic.info>
2015-09-04 22:15:10 +0800

    Add tests for issue #4603

---
+++ tests/baselines/reference/castExpressionParentheses_ES6.js
@@ -0,0 +1,11 @@
+//// [castExpressionParentheses_ES6.ts]
+
+// parentheses should be omitted
+// numeric literal
+(<any>0o123).foo;
+
+
+//// [castExpressionParentheses_ES6.js]
+// parentheses should be omitted
+// numeric literal
+0o123.foo;
---
+++ tests/cases/compiler/castExpressionParentheses_ES6.ts
@@ -0,0 +1,5 @@
+// @target: es6
+
+// parentheses should be omitted
+// numeric literal
+(<any>0o123).foo;
