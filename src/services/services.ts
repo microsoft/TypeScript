18aab8f92458cbb8e4912834138c9d09d9d4c4cc
Mohamed Hegazy <mhegazy@microsoft.com>
2015-09-08 15:54:46 -0700

    rename variables

--- src/services/services.ts
+++ src/services/services.ts
@@ -3994,24 +3994,24 @@ namespace ts {
 
             if (flags & SymbolFlags.Property) {
                 if (flags & SymbolFlags.SyntheticProperty) {
-                    // If union property is result of union of non method (property/accessors/variables), it is labeled as property
-                    let unionPropertyKind = forEach(typeChecker.getRootSymbols(symbol), rootSymbol => {
+                    // If synthetic property is result of union of non method (property/accessors/variables), it is labeled as property
+                    let syntheticPropertyKind = forEach(typeChecker.getRootSymbols(symbol), rootSymbol => {
                         let rootSymbolFlags = rootSymbol.getFlags();
                         if (rootSymbolFlags & (SymbolFlags.PropertyOrAccessor | SymbolFlags.Variable)) {
                             return ScriptElementKind.memberVariableElement;
                         }
                         Debug.assert(!!(rootSymbolFlags & SymbolFlags.Method));
                     });
-                    if (!unionPropertyKind) {
-                        // If this was union of all methods,
-                        //make sure it has call signatures before we can label it as method
-                        let typeOfUnionProperty = typeChecker.getTypeOfSymbolAtLocation(symbol, location);
-                        if (typeOfUnionProperty.getCallSignatures().length) {
+                    if (!syntheticPropertyKind) {
+                        // If this was union/intersection of all methods,
+                        // make sure it has call signatures before we can label it as method
+                        let typeOfSyntheticProperty = typeChecker.getTypeOfSymbolAtLocation(symbol, location);
+                        if (typeOfSyntheticProperty.getCallSignatures().length) {
                             return ScriptElementKind.memberFunctionElement;
                         }
                         return ScriptElementKind.memberVariableElement;
                     }
-                    return unionPropertyKind;
+                    return syntheticPropertyKind;
                 }
                 return ScriptElementKind.memberVariableElement;
             }
