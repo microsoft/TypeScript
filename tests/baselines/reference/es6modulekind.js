f2b901a3c735a6c30981142a0fa324bdb3206abd
Wesley Wigham <t-weswig@microsoft.com>
2015-09-17 13:14:31 -0700

    reverts 60a120f, reexposes es6 option

---
+++ tests/baselines/reference/es6modulekind.js
@@ -0,0 +1,23 @@
+//// [es6modulekind.ts]
+
+export default class A
+{
+    constructor ()
+    {
+
+    }
+
+    public B()
+    {
+        return 42;
+    }
+}
+
+//// [es6modulekind.js]
+export default class A {
+    constructor() {
+    }
+    B() {
+        return 42;
+    }
+}
