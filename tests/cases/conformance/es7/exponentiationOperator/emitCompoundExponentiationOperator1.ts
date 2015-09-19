04ed89230ed68b59acaacc8ef7ae9b55e3d1ef90
Yui T <yuisu@microsoft.com>
2015-09-18 17:44:19 -0700

    Add tests for exponentiation

---
+++ tests/cases/conformance/es7/exponentiationOperator/emitCompoundExponentiationOperator1.ts
@@ -0,0 +1,20 @@
+﻿var comp: number;
+
+comp **= 1;
+comp **= comp ** comp;
+comp **= comp ** comp ** 2;
+comp **= comp ** comp + 2;
+comp **= comp ** comp - 2;
+comp **= comp ** comp * 2;
+comp **= comp ** comp / 2;
+comp **= comp ** comp % 2;
+comp **= (comp - 2) ** 5;
+comp **= (comp + 2) ** 5;
+comp **= (comp * 2) ** 5;
+comp **= (comp / 2) ** 5;
+comp **= (comp % 2) ** 5;
+comp **= comp ** (5 + 2);
+comp **= comp ** (5 - 2);
+comp **= comp ** (5 * 2);
+comp **= comp ** (5 / 2);
+comp **= comp ** (5 % 2);
---
+++ tests/cases/conformance/es7/exponentiationOperator/emitExponentiationOperatorInTemplateString1.ts
@@ -0,0 +1,10 @@
+﻿var t1 = 10;
+var t2 = 10;
+console.log(`${t1 ** t2}`)
+console.log(`${t1 ** t2 ** t1}`)
+console.log(`${t1 + t2 ** t1}`)
+console.log(`${t1 - t2 ** t1}`)
+console.log(`${t1 ** t2 + t1}`)
+console.log(`${t1 ** t2 - t1}`)
+console.log(`${t1 + t2 ** t2 + t1}`)
+console.log(`${t1 - t2 ** t2 - t1}`)
