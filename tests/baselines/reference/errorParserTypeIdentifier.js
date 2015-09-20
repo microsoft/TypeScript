bcb3883a86dc1798e329330ecd63ae7dee9262de
jbondc <jbondc@openmv.com>
2015-09-19 20:05:54 -0400

    Accept baselines.

---
+++ tests/baselines/reference/errorParserTypeIdentifier.js
@@ -0,0 +1,37 @@
+//// [errorParserTypeIdentifier.ts]
+
+// Union or intersection ambiguity
+function test(x: string | new() => void) { }
+
+let a: string | new () => void;
+let b: string | (new () => void);
+let c: string | new () => void /* not part of error */
+let d: string | new () => void | number;
+let e: string | <T>(...) => void | void;
+
+// Missing identifier or keyword
+let f: TypeModule1.
+
+module TypeModule2 {
+}
+
+let g: x.void;
+let h = (a: ) => {
+}
+Foo<a, , b>();
+
+
+//// [errorParserTypeIdentifier.js]
+// Union or intersection ambiguity
+function test(x) { }
+var a;
+var b;
+var c; /* not part of error */
+var d;
+var e;
+// Missing identifier or keyword
+var f;
+var g = void ;
+var h = function (a) {
+};
+Foo();
