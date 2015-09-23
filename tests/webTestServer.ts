9005546495a17578161920cf3c4697e43ef32b69
Vladimir Matveev <vladima@microsoft.com>
2015-09-22 22:08:49 -0700

    use os dependent default location for Chrome

--- tests/webTestServer.ts
+++ tests/webTestServer.ts
@@ -5,6 +5,7 @@ import fs = require("fs");
 import path = require("path");
 import url = require("url");
 import child_process = require("child_process");
+import os = require("os");
 
 /// Command line processing ///
 
@@ -263,7 +264,19 @@ http.createServer(function (req: http.ServerRequest, res: http.ServerResponse) {
 
 var browserPath: string;
 if ((browser && browser === 'chrome')) {
-    var defaultChromePath = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
+    const platform = os.platform();
+    let defaultChromePath: string;
+    switch(platform) {
+        case "win32": 
+            defaultChromePath = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
+            break;
+        case "linux": 
+            defaultChromePath = "/opt/google/chrome/chrome";
+            break;
+        default:
+            console.log(`Default Chrome location for platform ${platform} is unknown`);
+            break;
+    }
     if (fs.existsSync(defaultChromePath)) {
         browserPath = defaultChromePath;
     } else {
