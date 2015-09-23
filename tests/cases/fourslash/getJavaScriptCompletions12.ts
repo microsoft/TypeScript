d08117b7cd5bea73e80071dec41a736e902845b4
Vladimir Matveev <vladima@microsoft.com>
2015-09-23 16:08:34 -0700

    added test for multiple variable declarations

--- tests/cases/fourslash/getJavaScriptCompletions12.ts
+++ tests/cases/fourslash/getJavaScriptCompletions12.ts
@@ -18,6 +18,11 @@
 ////    }
 ////}
 ////convert(1, "")./*3*/
+/////**
+//// * @param {number} x
+//// */
+////var test1 = function(x) { return x./*4*/ }, test2 = function(a) { return a./*5*/ };
+
 
 goTo.marker("1");
 verify.completionListContains("charCodeAt", /*displayText:*/ undefined, /*documentation*/ undefined, "method");
@@ -25,3 +30,7 @@ goTo.marker("2");
 verify.completionListContains("toExponential", /*displayText:*/ undefined, /*documentation*/ undefined, "method");
 goTo.marker("3");
 verify.completionListContains("toExponential", /*displayText:*/ undefined, /*documentation*/ undefined, "method");
+goTo.marker("4");
+verify.completionListContains("toExponential", /*displayText:*/ undefined, /*documentation*/ undefined, "method");
+goTo.marker("5");
+verify.completionListContains("toExponential", /*displayText:*/ undefined, /*documentation*/ undefined, "warning");
