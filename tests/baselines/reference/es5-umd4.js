2a3173880f68fb7e73ad8f8292b5e52bddd8ac5d
Colin Snover <github.com@zetafleet.com>
2015-09-25 22:22:01 +0000

    Put AMD dependency list inline with define call in UMD modules
    Fixes #4952.

--- tests/baselines/reference/es5-umd4.js
+++ tests/baselines/reference/es5-umd4.js
@@ -17,14 +17,14 @@ export = A;
 
 
 //// [es5-umd4.js]
-(function (deps, factory) {
+(function (factory) {
     if (typeof module === 'object' && typeof module.exports === 'object') {
         var v = factory(require, exports); if (v !== undefined) module.exports = v;
     }
     else if (typeof define === 'function' && define.amd) {
-        define(deps, factory);
+        define(["require", "exports"], factory);
     }
-})(["require", "exports"], function (require, exports) {
+})(function (require, exports) {
     var A = (function () {
         function A() {
         }
--- tests/baselines/reference/es6-umd2.js
+++ tests/baselines/reference/es6-umd2.js
@@ -14,14 +14,14 @@ export class A
 }
 
 //// [es6-umd2.js]
-(function (deps, factory) {
+(function (factory) {
     if (typeof module === 'object' && typeof module.exports === 'object') {
         var v = factory(require, exports); if (v !== undefined) module.exports = v;
     }
     else if (typeof define === 'function' && define.amd) {
-        define(deps, factory);
+        define(["require", "exports"], factory);
     }
-})(["require", "exports"], function (require, exports) {
+})(function (require, exports) {
     class A {
         constructor() {
         }
