8d4b117f07062aa14e081267b1ce4e408ebab06a
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-10-05 15:01:33 -0700

    Update tests that use array slicing

--- tests/baselines/reference/library_ArraySlice.js
+++ tests/baselines/reference/library_ArraySlice.js
@@ -4,8 +4,16 @@ Array.prototype.slice();
 Array.prototype.slice(0);
 Array.prototype.slice(0, 1);
 
+// Array.prototype.slice returns this with zero arguments, allowing tuple slicing
+let t1: [number, number] = [1, 2];
+let t2: [number, number] = t1.slice();
+
+
 //// [library_ArraySlice.js]
 // Array.prototype.slice can have zero, one, or two arguments
 Array.prototype.slice();
 Array.prototype.slice(0);
 Array.prototype.slice(0, 1);
+// Array.prototype.slice returns this with zero arguments, allowing tuple slicing
+var t1 = [1, 2];
+var t2 = t1.slice();
--- tests/cases/compiler/library_ArraySlice.ts
+++ tests/cases/compiler/library_ArraySlice.ts
@@ -2,3 +2,7 @@
 Array.prototype.slice();
 Array.prototype.slice(0);
 Array.prototype.slice(0, 1);
+
+// Array.prototype.slice returns this with zero arguments, allowing tuple slicing
+let t1: [number, number] = [1, 2];
+let t2: [number, number] = t1.slice();
