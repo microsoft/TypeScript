f52b7cc2c5c31decb76c6221c81bda16b6b98c8b
Ron Buckton <rbuckton@microsoft.com>
2015-10-06 15:41:47 -0700

    Removed unnecessary check

--- src/compiler/emitter.ts
+++ src/compiler/emitter.ts
@@ -4785,10 +4785,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, Promi
 
                 // emit name if
                 // - node has a name
-                // - this is default export and target is not ES6 (for ES6 `export default` does not need to be compiled downlevel)
                 // - this is default export with static initializers
-                if ((node.name || (node.flags & NodeFlags.Default && (languageVersion < ScriptTarget.ES6
-                    || staticProperties.length > 0))) && !thisNodeIsDecorated) {
+                if ((node.name || (node.flags & NodeFlags.Default && staticProperties.length > 0)) && !thisNodeIsDecorated) {
                     write(" ");
                     emitDeclarationName(node);
                 }
