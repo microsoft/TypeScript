bf970be0b3de1674f2d6fe41719fea8d9b613ba4
Yui T <yuisu@microsoft.com>
2015-10-05 19:05:14 -0700

    Add baselines for downlevel emit exponentiation and ES7 emit of exponentiation

---
+++ tests/baselines/reference/emitCompoundExponentiationOperator1ES7.js
@@ -0,0 +1,43 @@
+//// [emitCompoundExponentiationOperator1ES7.ts]
+
+var comp: number;
+
+comp **= 1;
+comp **= comp ** comp;
+comp **= comp ** comp ** 2;
+comp **= comp ** comp + 2;
+comp **= comp ** comp - 2;
+comp **= comp ** comp * 2;
+comp **= comp ** comp / 2;
+comp **= comp ** comp % 2;
+comp **= (comp - 2) ** 5;
+comp **= (comp + 2) ** 5;
+comp **= (comp * 2) ** 5;
+comp **= (comp / 2) ** 5;
+comp **= (comp % 2) ** 5;
+comp **= comp ** (5 + 2);
+comp **= comp ** (5 - 2);
+comp **= comp ** (5 * 2);
+comp **= comp ** (5 / 2);
+comp **= comp ** (5 % 2);
+
+//// [emitCompoundExponentiationOperator1ES7.js]
+var comp;
+comp **= 1;
+comp **= comp ** comp;
+comp **= comp ** comp ** 2;
+comp **= comp ** comp + 2;
+comp **= comp ** comp - 2;
+comp **= comp ** comp * 2;
+comp **= comp ** comp / 2;
+comp **= comp ** comp % 2;
+comp **= (comp - 2) ** 5;
+comp **= (comp + 2) ** 5;
+comp **= (comp * 2) ** 5;
+comp **= (comp / 2) ** 5;
+comp **= (comp % 2) ** 5;
+comp **= comp ** (5 + 2);
+comp **= comp ** (5 - 2);
+comp **= comp ** (5 * 2);
+comp **= comp ** (5 / 2);
+comp **= comp ** (5 % 2);
--- tests/baselines/reference/emitExponentiationOperator1.js
+++ tests/baselines/reference/emitExponentiationOperator1.js
@@ -1,12 +1,13 @@
 //// [emitExponentiationOperator1.ts]
 
+1 ** -2;
 1 ** 2;
+(-1) ** 2
 1 ** 2 ** 3;
-1 ** -2 ** 3;
-1 ** -2 ** -3;
--1 ** -2 ** -3;
--(1 ** 2) ** 3;
+1 ** 2 ** -3;
 1 ** -(2 ** 3);
+(-(1 ** 2)) ** 3;
+(-(1 ** 2)) ** -3;
 
 1 ** 2 + 3;
 1 ** 2 - 3;
@@ -32,13 +33,14 @@
 (2 / 3) ** 4;
 
 //// [emitExponentiationOperator1.js]
+Math.pow(1, -2);
 Math.pow(1, 2);
+Math.pow((-1), 2);
 Math.pow(1, Math.pow(2, 3));
-Math.pow(1, -Math.pow(2, 3));
-Math.pow(1, -Math.pow(2, -3));
--Math.pow(1, -Math.pow(2, -3));
--Math.pow((Math.pow(1, 2)), 3);
+Math.pow(1, Math.pow(2, -3));
 Math.pow(1, -(Math.pow(2, 3)));
+Math.pow((-(Math.pow(1, 2))), 3);
+Math.pow((-(Math.pow(1, 2))), -3);
 Math.pow(1, 2) + 3;
 Math.pow(1, 2) - 3;
 Math.pow(1, 2) * 3;
