ff43d464fc74e2617de0f8675961193a2f53a34f
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-10-08 16:36:15 -0700

    Add test case

--- tests/baselines/reference/ambientClassDeclarationWithExtends.js
+++ tests/baselines/reference/ambientClassDeclarationWithExtends.js
@@ -2,5 +2,18 @@
 declare class A { }
 declare class B extends A { }
 
+declare class C {
+    public foo;
+}
+namespace D { var x; }
+declare class D extends C { }
+
+var d: C = new D();
+
 
 //// [ambientClassDeclarationWithExtends.js]
+var D;
+(function (D) {
+    var x;
+})(D || (D = {}));
+var d = new D();
--- tests/cases/compiler/ambientClassDeclarationWithExtends.ts
+++ tests/cases/compiler/ambientClassDeclarationWithExtends.ts
@@ -1,2 +1,10 @@
 declare class A { }
 declare class B extends A { }
+
+declare class C {
+    public foo;
+}
+namespace D { var x; }
+declare class D extends C { }
+
+var d: C = new D();
