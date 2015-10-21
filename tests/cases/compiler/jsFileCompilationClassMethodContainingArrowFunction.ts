6024266d49034cb5e29e41d952ddf616540b0335
Sheetal Nandi <shkamat@microsoft.com>
2015-10-20 17:16:39 -0700

    Some tests to cover transpilation of different syntax

---
+++ tests/cases/compiler/jsFileCompilationClassMethodContainingArrowFunction.ts
@@ -0,0 +1,9 @@
+// @jsExtensions: js
+// @out: out.js
+
+// @FileName: a.js
+class c {
+    method(a) {
+        let x = a => this.method(a);
+    }
+}
---
+++ tests/cases/compiler/jsFileCompilationShortHandProperty.ts
@@ -0,0 +1,12 @@
+// @jsExtensions: js
+// @out: out.js
+
+// @FileName: a.js
+function foo() {
+    var a = 10;
+    var b = "Hello";
+    return {
+        a,
+        b
+    };
+}
