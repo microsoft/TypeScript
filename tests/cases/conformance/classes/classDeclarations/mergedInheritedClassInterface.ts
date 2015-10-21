36ddd022a1f6dc5563cb3dbef8659817f7af0a4a
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-10-21 16:10:46 -0700

    Tests for interfaces merged w/classes
    Both interfaces and classes have base classes/interfaces; all members are
    available on an instance of the merged child.

---
+++ tests/cases/conformance/classes/classDeclarations/mergedInheritedClassInterface.ts
@@ -0,0 +1,27 @@
+interface BaseInterface {
+    required: number;
+    optional?: number;
+}
+
+declare class BaseClass {
+    baseMethod();
+    x2: number;
+}
+
+interface Child extends BaseInterface {
+    x3: number;
+}
+
+declare class Child extends BaseClass {
+    x4: number;
+    method();
+}
+
+// checks if properties actually were merged
+var child : Child;
+child.required;
+child.optional;
+child.x3;
+child.x4;
+child.baseMethod();
+child.method();
