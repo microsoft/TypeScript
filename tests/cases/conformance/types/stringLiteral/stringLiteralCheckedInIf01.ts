6618fd21bfe5e34a9d0014d5a60f8ef97e0b91c8
Daniel Rosenwasser <drosen@microsoft.com>
2015-10-26 15:42:02 -0700

    Added tests for operations that use assignable to/from.

---
+++ tests/cases/conformance/types/stringLiteral/stringLiteralCheckedInIf01.ts
@@ -0,0 +1,17 @@
+﻿
+type S = "a" | "b";
+type T = S[] | S;
+
+function f(foo: T) {
+    if (foo === "a") {
+        return foo;
+    }
+    else if (foo === "b") {
+        return foo;
+    }
+    else { 
+        return (foo as S[])[0];
+    }
+
+    throw new Error("Unreachable code hit.");
+}
---
+++ tests/cases/conformance/types/stringLiteral/stringLiteralCheckedInIf02.ts
@@ -0,0 +1,18 @@
+﻿
+type S = "a" | "b";
+type T = S[] | S;
+
+function isS(t: T): t is S {
+    return t === "a" || t === "b";
+}
+
+function f(foo: T) {
+    if (isS(foo)) {
+        return foo;
+    }
+    else { 
+        return foo[0];
+    }
+
+    throw new Error("Unreachable code hit.");
+}
