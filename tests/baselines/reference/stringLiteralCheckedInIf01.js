5e2314303f89ac4dae495700471c9f6d8f5f3303
Daniel Rosenwasser <drosen@microsoft.com>
2015-10-26 18:49:41 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/stringLiteralCheckedInIf01.js
@@ -0,0 +1,32 @@
+//// [stringLiteralCheckedInIf01.ts]
+
+type S = "a" | "b";
+type T = S[] | S;
+
+function f(foo: T) {
+    if (foo === "a") {
+        return foo;
+    }
+    else if (foo === "b") {
+        return foo;
+    }
+    else { 
+        return (foo as S[])[0];
+    }
+
+    throw new Error("Unreachable code hit.");
+}
+
+//// [stringLiteralCheckedInIf01.js]
+function f(foo) {
+    if (foo === "a") {
+        return foo;
+    }
+    else if (foo === "b") {
+        return foo;
+    }
+    else {
+        return foo[0];
+    }
+    throw new Error("Unreachable code hit.");
+}
