f36b6ab8f94ce66499cb63185da44ca660c0d4e1
Daniel Rosenwasser <drosen@microsoft.com>
2015-10-30 15:30:59 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/for-of58.js
@@ -0,0 +1,23 @@
+//// [for-of58.ts]
+
+var array = [1,2,3];
+var sum = 0;
+
+for (let num of array) {
+    if (sum === 0) {
+        array = [4,5,6]
+    }
+    
+    sum += num;
+}
+
+//// [for-of58.js]
+var array = [1, 2, 3];
+var sum = 0;
+for (var _i = 0; _i < array.length; _i++) {
+    var num = array[_i];
+    if (sum === 0) {
+        array = [4, 5, 6];
+    }
+    sum += num;
+}
