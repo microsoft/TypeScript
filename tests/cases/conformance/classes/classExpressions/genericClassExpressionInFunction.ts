6d683d2a967165717907a3cf43d78bbccb269309
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-11-02 10:44:25 -0800

    Add initial test

---
+++ tests/cases/conformance/classes/classExpressions/genericClassExpressionInFunction.ts
@@ -0,0 +1,13 @@
+﻿class A<T> {
+    genericVar: T
+}
+function B<T>() {
+    // class expression can use T
+    return class extends A<T> { }
+}
+// extends can call B
+class K extends B<number>() {
+    name: string;
+}
+var c = new K();
+c.genericVar = 12;
