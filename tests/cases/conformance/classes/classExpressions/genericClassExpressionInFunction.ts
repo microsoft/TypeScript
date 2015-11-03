9223b02136f0ede6ebeacf0404a7c8c9a2851486
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-11-03 14:42:03 -0800

    Improve test case and add working comparison

--- tests/cases/conformance/classes/classExpressions/genericClassExpressionInFunction.ts
+++ tests/cases/conformance/classes/classExpressions/genericClassExpressionInFunction.ts
@@ -1,13 +1,25 @@
 ﻿class A<T> {
     genericVar: T
 }
-function B<T>() {
+class B3 extends A<number> {
+}
+function B1<U>() {
     // class expression can use T
-    return class extends A<T> { }
+    return class extends A<U> { }
+}
+class B2<V> {
+    anon = class extends A<V> { }
 }
 // extends can call B
-class K extends B<number>() {
+class K extends B1<number>() {
+    namae: string;
+}
+class C extends (new B2<number>().anon) {
     name: string;
 }
-var c = new K();
+var c = new C();
+var k = new K();
+var b3 = new B3();
 c.genericVar = 12;
+k.genericVar = 12;
+b3.genericVar = 12
