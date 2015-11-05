993c2383545021cc769500878a5e5e6847b1f2c7
jied <jied314@mail.sfsu.edu>
2015-11-05 14:02:34 -0800

    struct expression enables and struct body with statemetns not allowed

---
+++ tests/baselines/reference/structBodyWithStatements.js
@@ -0,0 +1,60 @@
+//// [structBodyWithStatements.ts]
+struct C {
+    var x = 1; // error, variable statement
+}
+
+struct C2 {
+    function foo() {} // error, function declaration
+}
+
+var x = 1;
+var y = 2;
+struct C3 {
+    x: number = y + 1; // ok, not a statement (need var in the statement production).
+}
+
+//// [structBodyWithStatements.js]
+var C = (function () {
+    var _C = new TypedObject.StructType({
+    });
+    function _ctor() {
+    }
+    function C() {
+        var obj = new _C();
+        _ctor.call(obj);
+        return obj;
+    }
+    C._TO = _C;
+    return C;
+})();
+var x = 1; // error, variable statement
+var C2 = (function () {
+    var _C2 = new TypedObject.StructType({
+    });
+    function _ctor() {
+    }
+    function C2() {
+        var obj = new _C2();
+        _ctor.call(obj);
+        return obj;
+    }
+    C2._TO = _C2;
+    return C2;
+})();
+function foo() { } // error, function declaration
+var x = 1;
+var y = 2;
+var C3 = (function () {
+    var _C3 = new TypedObject.StructType({
+        x: TypedObject.float64
+    });
+    function _ctor() {
+    }
+    function C3() {
+        var obj = new _C3();
+        _ctor.call(obj);
+        return obj;
+    }
+    C3._TO = _C3;
+    return C3;
+})();
---
+++ tests/baselines/reference/structExpression.js
@@ -0,0 +1,64 @@
+//// [structExpression.ts]
+// struct expression are supported
+
+var x = struct C {
+}
+
+var y = {
+    foo: struct C2 {
+    }
+}
+
+module M {
+    var z = struct C4 {
+    }
+}
+
+//// [structExpression.js]
+// struct expression are supported
+var x = var C = (function () {
+    var _C = new TypedObject.StructType({
+    });
+    function _ctor() {
+    }
+    function C() {
+        var obj = new _C();
+        _ctor.call(obj);
+        return obj;
+    }
+    C._TO = _C;
+    return C;
+})();
+;
+var y = {
+    foo: var C2 = (function () {
+        var _C2 = new TypedObject.StructType({
+        });
+        function _ctor() {
+        }
+        function C2() {
+            var obj = new _C2();
+            _ctor.call(obj);
+            return obj;
+        }
+        C2._TO = _C2;
+        return C2;
+    })();
+};
+var M;
+(function (M) {
+    var z = var C4 = (function () {
+        var _C4 = new TypedObject.StructType({
+        });
+        function _ctor() {
+        }
+        function C4() {
+            var obj = new _C4();
+            _ctor.call(obj);
+            return obj;
+        }
+        C4._TO = _C4;
+        return C4;
+    })();
+    ;
+})(M || (M = {}));
