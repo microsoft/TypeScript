c72861ebd77722230c830ee9b31c929001025967
Jeffrey Morlan <jmmorlan@sonic.net>
2015-11-09 21:05:05 -0800

    Add test case

---
+++ tests/baselines/reference/overloadConsecutiveness.js
@@ -0,0 +1,27 @@
+//// [overloadConsecutiveness.ts]
+// Making sure compiler won't break with declarations that are consecutive in the AST but not consecutive in the source. Syntax errors intentional.
+
+function f1(), function f1();
+function f2(), function f2() {}
+function f3() {}, function f3();
+
+class C {
+	m1(), m1();
+	m2(), m2() {}
+	m3() {}, m3();
+}
+
+
+//// [overloadConsecutiveness.js]
+// Making sure compiler won't break with declarations that are consecutive in the AST but not consecutive in the source. Syntax errors intentional.
+function f2() { }
+function f3() { }
+var C = (function () {
+    function C() {
+    }
+    C.prototype.m1 = ;
+    C.prototype.m2 = ;
+    C.prototype.m2 = function () { };
+    C.prototype.m3 = function () { };
+    return C;
+})();
