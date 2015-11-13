5b29a7e9d9d0d2c82cf980b2c4a4befaa7010ea8
Wesley Wigham <t-weswig@microsoft.com>
2015-11-13 14:41:49 -0800

    accept corrected projects baselines

--- tests/baselines/reference/project/maprootUrlsourcerootUrlModuleMultifolderSpecifyOutputFile/node/bin/test.d.ts
+++ tests/baselines/reference/project/maprootUrlsourcerootUrlModuleMultifolderSpecifyOutputFile/node/bin/test.d.ts
@@ -1,4 +1,4 @@
-declare module "ref/m1" {
+declare module "outputdir_module_multifolder/ref/m1" {
     export var m1_a1: number;
     export class m1_c1 {
         m1_c1_p1: number;
@@ -6,7 +6,7 @@ declare module "ref/m1" {
     export var m1_instance1: m1_c1;
     export function m1_f1(): m1_c1;
 }
-declare module "../outputdir_module_multifolder_ref/m2" {
+declare module "outputdir_module_multifolder_ref/m2" {
     export var m2_a1: number;
     export class m2_c1 {
         m2_c1_p1: number;
--- tests/baselines/reference/project/sourceRootRelativePathModuleMultifolderSpecifyOutputFile/amd/bin/test.js
+++ tests/baselines/reference/project/sourceRootRelativePathModuleMultifolderSpecifyOutputFile/amd/bin/test.js
@@ -1,4 +1,4 @@
-define("ref/m1", ["require", "exports"], function (require, exports) {
+define("outputdir_module_multifolder/ref/m1", ["require", "exports"], function (require, exports) {
     exports.m1_a1 = 10;
     var m1_c1 = (function () {
         function m1_c1() {
@@ -12,7 +12,7 @@ define("ref/m1", ["require", "exports"], function (require, exports) {
     }
     exports.m1_f1 = m1_f1;
 });
-define("../outputdir_module_multifolder_ref/m2", ["require", "exports"], function (require, exports) {
+define("outputdir_module_multifolder_ref/m2", ["require", "exports"], function (require, exports) {
     exports.m2_a1 = 10;
     var m2_c1 = (function () {
         function m2_c1() {
@@ -26,7 +26,7 @@ define("../outputdir_module_multifolder_ref/m2", ["require", "exports"], functio
     }
     exports.m2_f1 = m2_f1;
 });
-define("test", ["require", "exports", "ref/m1", "../outputdir_module_multifolder_ref/m2"], function (require, exports, m1, m2) {
+define("test", ["require", "exports", "outputdir_module_multifolder/ref/m1", "outputdir_module_multifolder_ref/m2"], function (require, exports, m1, m2) {
     exports.a1 = 10;
     var c1 = (function () {
         function c1() {
