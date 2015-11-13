645f2a5d1981b538400f8df5f5ad7e1512d11ede
Yui T <yuisu@microsoft.com>
2015-11-12 18:14:19 -0800

    include version distribution script into the build process

--- Jakefile.js
+++ Jakefile.js
@@ -223,9 +223,12 @@ var builtLocalCompiler = path.join(builtLocalDirectory, compilerFilename);
     * @param generateDeclarations: true to compile using --declaration
     * @param outDir: true to compile using --outDir
     * @param keepComments: false to compile using --removeComments
+    * @param noResolve: specify whether to add ///-references or module import targets to the list of compiled files --noResolve
+    * @param stripInternal: specify whether to emit declarations for code that has an @internal annotation --stripInternal
+    * @param target: a string to indicate ECMAScript target version --target
     * @param callback: a function to execute after the compilation process ends
     */
-function compileFile(outFile, sources, prereqs, prefixes, useBuiltCompiler, noOutFile, generateDeclarations, outDir, preserveConstEnums, keepComments, noResolve, stripInternal, callback) {
+function compileFile(outFile, sources, prereqs, prefixes, useBuiltCompiler, noOutFile, generateDeclarations, outDir, preserveConstEnums, keepComments, noResolve, stripInternal, target, callback) {
     file(outFile, prereqs, function() {
         var compilerPath = useBuiltCompiler ? builtLocalCompiler : LKGCompiler;
         var options = "--noImplicitAny --noEmitOnError --pretty";
@@ -265,6 +268,10 @@ function compileFile(outFile, sources, prereqs, prefixes, useBuiltCompiler, noOu
 
         if (stripInternal) {
             options += " --stripInternal"
+        }
+
+        if (target) {
+            options += " --target " + target;
         }
 
         var cmd = host + " " + compilerPath + " " + options + " ";
@@ -450,6 +457,7 @@ compileFile(servicesFile, servicesSources,[builtLocalDirectory, copyright].conca
             /*keepComments*/ true,
             /*noResolve*/ false,
             /*stripInternal*/ true,
+            /*target*/ "ES3",
             /*callback*/ function () {
                 jake.cpR(servicesFile, nodePackageFile, {silent: true});
 
@@ -492,7 +500,7 @@ task("lssl", [lsslFile]);
 
 // Local target to build the compiler and services
 desc("Builds the full compiler and services");
-task("local", ["generate-diagnostics", "lib", tscFile, servicesFile, nodeDefinitionsFile, serverFile, builtGeneratedDiagnosticMessagesJSON]);
+task("local", ["generate-diagnostics", "lib", tscFile, servicesFile, nodeDefinitionsFile, serverFile, builtGeneratedDiagnosticMessagesJSON, "distribute-version"]);
 
 // Local target to build only tsc.js
 desc("Builds only the compiler");
@@ -875,7 +883,14 @@ var tslintRulesOutFiles = tslintRules.map(function(p) {
 desc("Compiles tslint rules to js");
 task("build-rules", tslintRulesOutFiles);
 tslintRulesFiles.forEach(function(ruleFile, i) {
-    compileFile(tslintRulesOutFiles[i], [ruleFile], [ruleFile], [], /*useBuiltCompiler*/ false, /*noOutFile*/ true, /*generateDeclarations*/ false, path.join(builtLocalDirectory, "tslint")); 
+    compileFile(/*outFile*/ tslintRulesOutFiles[i],
+        /*sources*/ [ruleFile],
+        /*prereqs*/ [ruleFile],
+        /*prefixes*/ [],
+        /*useBuiltCompiler*/ false,
+        /*noOutFile*/ true,
+        /*generateDeclarations*/ false,
+        /*outDir*/ path.join(builtLocalDirectory, "tslint")); 
 });
 
 function getLinterOptions() {
@@ -968,3 +983,36 @@ task("lint-server", ["build-rules"], function() {
         lintWatchFile(lintTargets[i]);
     }
 });
+
+var distributeVersionInputFolder = "scripts/distributeVersion";
+var distributeVersionOutputFolder = path.join(builtLocalDirectory, "distributeVersion");
+var distributeVersionJS = path.join(distributeVersionOutputFolder, "distribute.js");
+var distributeVersionTS = path.join(distributeVersionInputFolder, "distribute.ts");
+
+file(distributeVersionTS);
+
+compileFile(/*outFile*/ distributeVersionJS,
+        /*sources*/ [distributeVersionTS],
+        /*prereqs*/ [],
+        /*prefixes*/ [],
+        /*useBuiltCompiler*/ false,
+        /*noOutFile*/ true,
+        /*generateDeclarations*/ false,
+        /*outDir*/ distributeVersionOutputFolder,
+        /*preserveConstEnums*/ undefined,
+        /*keepComments*/ false,
+        /*noResolve*/ false,
+        /*stripInternal*/ false,
+        /*target*/ "ES2015");
+
+desc("Run version distribution script")
+task("distribute-version", [distributeVersionJS], function() {
+    // Copy version file to the builtLocalDirectory
+    jake.cpR(path.join(distributeVersionInputFolder, "version.json"), distributeVersionOutputFolder);
+    
+    console.log(host + " " + distributeVersionJS);
+    jake.exec([host + " " + distributeVersionJS], {printStdout: true}, function () {
+       console.log("Successfully distribute version");
+       complete();
+    });
+}, {async: true});
---
+++ scripts/distributeVersion/distribute.ts
@@ -0,0 +1,87 @@
+// Update TypeScript Version
+/// <reference path="../../src/harness/external/node.d.ts" />
+"use strict"
+import * as fs from 'fs'
+import * as path from 'path'
+
+const versionFilePath = path.join(__dirname, "version.json");
+
+interface FileInformation {
+	filePath: string,
+	ext: string,
+	content: string
+}
+
+interface Pattern {
+	pattern: RegExp,
+	serialize: (version: string[]) => string
+}
+
+const formats: {[idx: string]: Pattern} = {
+	'src/compiler/program.ts': {
+        pattern: /version\s*=\s*"[0-9]+\.[0-9]+\.[0-9]"/g,
+        serialize: (version: string[]) => {
+			return `version = "${version[0]}.${version[1]}.${version[2]}"`;
+        }
+    },
+	'package.json': {
+		pattern: /"version"\s*:\s*"[0-9]+\.[0-9]+\.[0-9]"/g,
+		serialize: (version: string[]) => {
+			return `"version": "${version[0]}.${version[1]}.${version[2]}"`;
+		}
+	}
+}
+
+
+function getFileExtension(filePath: string): string {
+	let pieces = filePath.split('.');
+	return pieces.length < 2? "": pieces[pieces.length - 1];
+}
+
+function getFileInformation(filePath: string): Promise<FileInformation> {
+	return new Promise<FileInformation>((resolve, reject) => {
+		let result: FileInformation;
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				reject(new Error("getFileInformation error: " + err));
+			}
+
+			resolve({
+				filePath,
+				content: data.toString(),
+				ext: getFileExtension(filePath)
+			});
+		});
+	});
+}
+
+function getVersion(): Promise<string[]> {
+	return new Promise<string[]>((resolve, reject) => {
+		fs.readFile(versionFilePath, 'utf8', function (err, data) {
+			if (err) {
+				reject(new Error("getVersion error: "+ err));
+			}
+			resolve(JSON.parse(data)["TypeScriptVersion"].split("."));
+		});
+	});
+}
+
+async function distributeVersion() {
+	let version = await getVersion();
+
+	for (let filePath in formats) {
+		if (!fs.existsSync(filePath)) {
+			continue;
+		}
+		try {
+			let fileInfo = await getFileInformation(filePath);
+			let format = formats[filePath];
+			fs.writeFileSync(fileInfo.filePath, fileInfo.content.replace(format.pattern, format.serialize(version)));
+			console.log("Wrote version in "+ fileInfo.filePath);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+}
+
+distributeVersion();
