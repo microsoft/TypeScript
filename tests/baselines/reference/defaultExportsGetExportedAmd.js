36607b99b51a2d812ca96da24069756acce44375
Wesley Wigham <t-weswig@microsoft.com>
2015-11-12 19:05:28 -0800

    accept baselines for new tests

---
+++ tests/baselines/reference/defaultExportsGetExportedAmd.js
@@ -0,0 +1,10 @@
+//// [defaultExportsGetExportedAmd.ts]
+export default class Foo {}
+
+
+//// [defaultExportsGetExportedAmd.js]
+define(["require", "exports"], function (require, exports) {
+    class Foo {
+    }
+    exports.default = Foo;
+});
