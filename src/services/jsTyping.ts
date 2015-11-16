f29721fad53a0c503f0eb0b2d72d037c18a798e4
Jason Ramsay <jasonra@microsoft.com>
2015-11-16 11:41:56 -0800

    Update Typings Support - notFound

--- src/services/jsTyping.ts
+++ src/services/jsTyping.ts
@@ -5,21 +5,26 @@
 
 /* @internal */
 namespace ts.JsTyping {
+    interface TsdJsonCacheInfo {
+        path: string;
+        value: any;
+    }
+
     export function discoverTypings(
-        host: ModuleResolutionHost | System, fileNames: string[], cachePath: string, compilerOptions?: CompilerOptions, safeList?: string[], noDevDependencies?: boolean)
+        host: TypeDefinitionResolutionHost | System, fileNames: string[], cachePath: string, compilerOptions?: CompilerOptions, safeList?: string[], noDevDependencies?: boolean)
         : { cachedTypingPaths: string[], newTypings: string[] } {
         let searchDirs: string[] = [];
         let typingNames: string[] = [];
         let cachedTypingPaths: string[] = [];
         let cachedTypingNames: string[] = [];
-        
+
         for (let fileName of fileNames) {
             let dir = ts.getDirectoryPath(ts.normalizePath(fileName));
             if (searchDirs.indexOf(dir) < 0) {
                 searchDirs.push(dir);
             }
         }
-        
+
         for (let searchDir of searchDirs) {
             let packageJsonPath = ts.combinePaths(searchDir, "package.json");
             if (host.fileExists(packageJsonPath)) {
@@ -32,9 +37,13 @@ namespace ts.JsTyping {
         
         let normalizedCachePath = ts.normalizePath(cachePath);
         let typingsPath = ts.combinePaths(normalizedCachePath, "typings");
-        let cacheTsdJsonPath = ts.combinePaths(normalizedCachePath, "tsd.json");
-        if (host.fileExists(cacheTsdJsonPath)) {
-            let cacheTsdJsonDict = JSON.parse(host.readFile(cacheTsdJsonPath));
+        let tsdJsonCacheInfo = tryGetTsdJsonCacheInfo(host, cachePath);
+        if (tsdJsonCacheInfo) {
+            let cacheTsdJsonDict = tsdJsonCacheInfo.value;
+            if (cacheTsdJsonDict.hasOwnProperty("notFound")) {
+                let notFound = cacheTsdJsonDict["notFound"];
+                typingNames = ts.filter(typingNames, i => notFound.indexOf(i) < 0);
+            }
             if (cacheTsdJsonDict.hasOwnProperty("installed")) {
                 for (let cachedTypingPath of Object.keys(cacheTsdJsonDict.installed)) {
                     let cachedTypingName = cachedTypingPath.substr(0, cachedTypingPath.indexOf('/'));
@@ -46,8 +55,44 @@ namespace ts.JsTyping {
             }
         }
         
-        let newTypingNames = typingNames.filter(t => cachedTypingNames.indexOf(t) < 0);
-        
+        let newTypingNames = ts.filter(typingNames, t => cachedTypingNames.indexOf(t) < 0);
         return { cachedTypingPaths, newTypings: newTypingNames };
+    }
+
+    export function updateTypingsConfig(
+        host: TypeDefinitionResolutionHost | System, cachedTypingsPaths: string[], newTypings: string[], cachePath: string, typingsConfigPath: string): void {
+        let tsdJsonCacheInfo = tryGetTsdJsonCacheInfo(host, cachePath);
+        if (tsdJsonCacheInfo) {
+            let notFound: string[] = [];
+            let cacheTsdJsonDict = tsdJsonCacheInfo.value;
+            if (cacheTsdJsonDict.hasOwnProperty("installed")) {
+                let installedTypings = Object.keys(cacheTsdJsonDict.installed);
+                notFound = ts.filter(newTypings, i => !isInstalled(i, installedTypings));
+            }
+            if (cacheTsdJsonDict.hasOwnProperty("notFound")) {
+                notFound = ts.deduplicate<string>(cacheTsdJsonDict["notFound"].concat(notFound));
+            }
+            if (notFound.length > 0) {
+                cacheTsdJsonDict["notFound"] = notFound;
+                host.writeFile(tsdJsonCacheInfo.path, JSON.stringify(cacheTsdJsonDict));
+            }
+        }
+    }
+
+    function tryGetTsdJsonCacheInfo(host: TypeDefinitionResolutionHost | System, cachePath: string): TsdJsonCacheInfo {
+        let cacheTsdJsonPath = ts.combinePaths(ts.normalizePath(cachePath), "tsd.json");
+        if (host.fileExists(cacheTsdJsonPath)) {
+            return { path: cacheTsdJsonPath, value: JSON.parse(host.readFile(cacheTsdJsonPath)) };
+        }
+        return undefined;
+    }
+
+    function isInstalled(typing: string, installedKeys: string[]) {
+        for (let key of installedKeys) {
+            if (key.indexOf(typing + '/') === 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
