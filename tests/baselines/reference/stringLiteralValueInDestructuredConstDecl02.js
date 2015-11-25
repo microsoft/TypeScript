c186a313532a44b39aa82270de05af44a79284eb
Daniel Rosenwasser <drosen@microsoft.com>
2015-11-25 11:30:33 -0800

    Accepted baselines.

---
+++ tests/baselines/reference/stringLiteralValueInDestructuredConstDecl02.js
@@ -0,0 +1,18 @@
+//// [stringLiteralValueInDestructuredConstDecl02.ts]
+
+const {x: {y}} = {
+    x: {
+        y: "foo"
+    } 
+};
+
+var foo2: "foo" = y;
+
+
+//// [stringLiteralValueInDestructuredConstDecl02.js]
+var y = {
+    x: {
+        y: "foo"
+    }
+}.x.y;
+var foo2 = y;
