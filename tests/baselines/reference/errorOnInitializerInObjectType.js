068d10c6bb48d39ef78b2e0dc2119d04fc06eb0b
Nathan Yee <ny.nathan.yee@gmail.com>
2015-11-25 14:31:46 -0800

    Add tests for #5173

---
+++ tests/baselines/reference/errorOnInitializerInObjectType.js
@@ -0,0 +1,12 @@
+//// [errorOnInitializerInObjectType.ts]
+interface Foo {
+    bar: number = 5;
+}
+
+var Foo: {
+    bar: number = 5;
+};
+
+
+//// [errorOnInitializerInObjectType.js]
+var Foo;
---
+++ tests/cases/compiler/errorOnInitializerInObjectType.ts
@@ -0,0 +1,7 @@
+interface Foo {
+    bar: number = 5;
+}
+
+var Foo: {
+    bar: number = 5;
+};
