094d95ea70f317b7168f70566633d79381ec2e2b
Daniel Rosenwasser <drosen@microsoft.com>
2015-11-25 11:59:26 -0800

    Accepted baselines.

---
+++ tests/baselines/reference/stringLiteralValueInConstDecl01.js
@@ -0,0 +1,10 @@
+//// [stringLiteralValueInConstDecl01.ts]
+
+const foo = "foo";
+var bar = foo;
+bar = "bar";
+
+//// [stringLiteralValueInConstDecl01.js]
+var foo = "foo";
+var bar = foo;
+bar = "bar";
