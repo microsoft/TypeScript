cff83c5081d0b3c210e65c4bccbfdde33387eb34
Wesley Wigham <t-weswig@microsoft.com>
2015-12-01 14:05:46 -0800

    Fix #5844 - add many new tests covering named/anonymous default exports

---
+++ tests/baselines/reference/anonymousDefaultExportsAmd.js
@@ -0,0 +1,21 @@
+//// [tests/cases/conformance/es6/moduleExportsAmd/anonymousDefaultExportsAmd.ts] ////
+
+//// [a.ts]
+export default class {}
+
+//// [b.ts]
+export default function() {}
+
+//// [a.js]
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    class default_1 {
+    }
+    exports.default = default_1;
+});
+//// [b.js]
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    function default_1() { }
+    exports.default = default_1;
+});
