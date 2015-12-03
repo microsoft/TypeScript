663df6f42d4a7d4a61c488832258a18d79172923
Mohamed Hegazy <mhegazy@microsoft.com>
2015-12-02 20:41:35 -0800

    Add test

---
+++ tests/baselines/reference/esModuleEmitForDefaultExport.js
@@ -0,0 +1,32 @@
+//// [tests/cases/compiler/esModuleEmitForDefaultExport.ts] ////
+
+//// [a.ts]
+
+class C { }
+export default C;
+
+//// [b.ts]
+var x = 0;
+export default x;
+
+//// [c.ts]
+function f() { }
+export default f;
+
+
+//// [a.js]
+"use strict";
+class C {
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = C;
+//// [b.js]
+"use strict";
+var x = 0;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = x;
+//// [c.js]
+"use strict";
+function f() { }
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = f;
---
+++ tests/cases/compiler/esModuleEmitForDefaultExport.ts
@@ -0,0 +1,14 @@
+// @target: ES6
+// @module: commonjs
+
+// @filename: a.ts
+class C { }
+export default C;
+
+// @filename: b.ts
+var x = 0;
+export default x;
+
+// @filename: c.ts
+function f() { }
+export default f;
