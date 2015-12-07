cb472f217cf201619dc4c4b75e57a74eff2f0cc1
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-12-07 12:55:01 -0800

    Accept baselines

---
+++ tests/baselines/reference/typePredicateUnions.js
@@ -0,0 +1,37 @@
+//// [typePredicateUnions.ts]
+function isA<A>(x: any, y: any): x is A | y is B {
+    return false;
+}
+
+var x: number | number[];
+var y: string | string[];
+var num: number;
+var nums: number[];
+var strOrStrs: string | string[];
+
+if (isA<number>(x, y)) {
+    num = x;
+    strOrStrs = y;
+}
+else {
+    nums = x;
+    strOrStrs = y;
+}
+
+//// [typePredicateUnions.js]
+function isA(x, y) {
+    return false;
+}
+var x;
+var y;
+var num;
+var nums;
+var strOrStrs;
+if (isA(x, y)) {
+    num = x;
+    strOrStrs = y;
+}
+else {
+    nums = x;
+    strOrStrs = y;
+}
