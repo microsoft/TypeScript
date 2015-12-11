59982aba224075cec05a7b365dc11adcc3cc0155
Yui T <yuisu@microsoft.com>
2015-12-10 17:35:10 -0800

    Update testcases

--- tests/cases/fourslash/tsxCompletion11.ts
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path='fourslash.ts' />
-
-//@Filename: file.tsx
-//// var x1 = </**/
-
-goTo.marker();
-verify.memberListCount(0);
--- tests/cases/fourslash/tsxCompletion13.ts
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path='fourslash.ts' />
-
-//@Filename: file.tsx
-//// class MyElement {}
-//// var x1 = <My/**/
-
-goTo.marker("");
-verify.memberListCount(0);
