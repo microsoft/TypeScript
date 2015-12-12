af65e86aaa9cbc28ac9cc767d12f74864de52f28
Yui T <yuisu@microsoft.com>
2015-12-11 19:28:21 -0800

    Add tests

---
+++ tests/cases/fourslash/renameParameterPropertyDeclaration.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path='fourslash.ts'/>
+
+//// class Foo {
+////     constructor(private privateParam: number,
+////         public publicParam: string,
+////         protected protectedParam: boolean) {
+//// 
+////         let localPrivate = privateParam;
+////         this.privateParam += 10;
+//// 
+////         let localPublic = publicParam;
+////         this.publicParam += " Hello!";
+//// 
+////         let localProtected = protectedParam;
+////         this.protectedParam = !this.protectedParam;
+////     }
+//// }
