99722e442cb2e0bd018f133cad04afd29cc8c756
Yui T <yuisu@microsoft.com>
2015-12-12 16:38:11 -0800

    Add tests for binding pattern in parameter property declaration

---
+++ tests/cases/fourslash/renameParameterPropertyDeclaration4.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path='fourslash.ts'/>
+
+//// class Foo {
+////     constructor(protected { |protectedParam| }) {
+////         let myProtectedParam = |protectedParam|;
+////     }
+//// }
+
+let ranges = test.ranges()
+for (let range of ranges) {
+    goTo.position(range.start);
+    verify.renameLocations(/*findInStrings*/ false, /*findInComments*/ false);
+}
