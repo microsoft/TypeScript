638818693729f52cf8aeadb917a539c20a5a8155
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-12-16 11:38:22 -0800

    Accept baselines

--- tests/baselines/reference/implementedPropertyContextualTyping1.js
+++ tests/baselines/reference/implementedPropertyContextualTyping1.js
@@ -2,23 +2,40 @@
 interface Event {
 	time: number
 }
-interface Listener {
+interface Base {
+	superHandle: (e: Event) => number;
+}
+interface Listener extends Base {
 	handle: (e: Event) => void;
 }
+interface Ringer {
+	ring: (times: number) => void;
+}
 
-class Foo implements Listener {
+class Alarm implements Listener, Ringer {
 	handle = e => { 
 		let n: number = e.time;
 	}
+	superHandle = e => {
+		return e.time;
+	}
+	ring = times => {
+		let m: number = times + 1;
+	}
 }
 
-
 //// [implementedPropertyContextualTyping1.js]
-var Foo = (function () {
-    function Foo() {
+var Alarm = (function () {
+    function Alarm() {
         this.handle = function (e) {
             var n = e.time;
         };
+        this.superHandle = function (e) {
+            return e.time;
+        };
+        this.ring = function (times) {
+            var m = times + 1;
+        };
     }
-    return Foo;
+    return Alarm;
 }());
---
+++ tests/baselines/reference/implementedPropertyContextualTyping2.js
@@ -0,0 +1,20 @@
+//// [implementedPropertyContextualTyping2.ts]
+interface Long {
+    length: number;
+}
+
+class Cat implements Long {
+    length = undefined;
+}
+const longCat = new Cat();
+longCat.length = "wat";
+
+//// [implementedPropertyContextualTyping2.js]
+var Cat = (function () {
+    function Cat() {
+        this.length = undefined;
+    }
+    return Cat;
+}());
+var longCat = new Cat();
+longCat.length = "wat";
