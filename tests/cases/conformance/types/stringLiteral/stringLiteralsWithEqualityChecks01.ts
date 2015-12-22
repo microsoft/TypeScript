e452955f053a88de23b76823dd6be829008ce8c9
Daniel Rosenwasser <drosen@microsoft.com>
2015-12-21 17:17:29 -0800

    Amended tests.

--- tests/cases/conformance/types/stringLiteral/stringLiteralsWithEqualityChecks01.ts
+++ tests/cases/conformance/types/stringLiteral/stringLiteralsWithEqualityChecks01.ts
@@ -6,9 +6,17 @@ b = x === y;
 b = "foo" === y
 b = y === "foo";
 b = "foo" === "bar";
+b = "bar" === x;
+b = x === "bar";
+b = y === "bar";
+b = "bar" === y;
 
 b = x !== y;
 b = "foo" !== y
 b = y !== "foo";
 b = "foo" !== "bar";
+b = "bar" !== x;
+b = x !== "bar";
+b = y !== "bar";
+b = "bar" !== y;
 
--- tests/cases/conformance/types/stringLiteral/stringLiteralsWithEqualityChecks03.ts
+++ tests/cases/conformance/types/stringLiteral/stringLiteralsWithEqualityChecks03.ts
@@ -14,9 +14,16 @@ b = x === y;
 b = "foo" === y
 b = y === "foo";
 b = "foo" === "bar";
+b = "bar" === x;
+b = x === "bar";
+b = y === "bar";
+b = "bar" === y;
 
 b = x !== y;
 b = "foo" !== y
 b = y !== "foo";
 b = "foo" !== "bar";
-
+b = "bar" !== x;
+b = x !== "bar";
+b = y !== "bar";
+b = "bar" !== y;
