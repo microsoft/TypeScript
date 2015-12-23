05e5516fc0cd34bbe39f7e2607cffc8e9d63ef26
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2015-12-22 20:02:42 -0800

    Add rename tests

---
+++ tests/cases/fourslash/renameInheritedProperties1.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path='fourslash.ts'/>
+
+//// class class1 extends class1 {
+////    [|propName|]: string;
+//// }
+////
+//// var v: class1;
+//// v.[|propName|];
+
+let ranges = test.ranges();
+verify.assertHasRanges(ranges);
+for (let range of ranges) {
+    goTo.position(range.start);
+    verify.renameLocations(/*findInStrings*/ false, /*findInComments*/ false);
+}
