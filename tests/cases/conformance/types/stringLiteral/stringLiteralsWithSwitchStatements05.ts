2874156da3d912b2d5a92b71538456bdc83e13e1
Daniel Rosenwasser <drosen@microsoft.com>
2015-12-22 17:02:23 -0800

    Added more to test.

--- tests/cases/conformance/types/stringLiteral/stringLiteralsWithSwitchStatements05.ts
+++ tests/cases/conformance/types/stringLiteral/stringLiteralsWithSwitchStatements05.ts
@@ -1,17 +1,26 @@
 ﻿let x: "foo";
 let y: "foo" | "bar"; 
+let z: "bar";
 
 declare function randBool(): boolean;
 
 switch (x) {
     case randBool() ? "foo" : "baz":
         break;
+    case (randBool() ? ("bar") : "baz" ? "bar" : "baz"):
+        break;
     case (("bar")):
         break;
-    case (x, y, "baz"):
+    case (x, y, ("baz")):
         x;
-        break;
-    case (("foo" || "bar")):
         y;
+        break;
+    case (("foo" || ("bar"))):
+        break;
+    case (("bar" || ("baz"))):
+        break;
+    case z || "baz":
+    case "baz" || z:
+        z;
         break;
 }
