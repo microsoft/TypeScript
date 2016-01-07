1798024f4fa76c8fb224386802098e98684b1511
Anders Hejlsberg <andersh@microsoft.com>
2016-01-06 16:13:02 -0800

    Updating tests

--- tests/baselines/reference/for-inStatementsArray.js
+++ tests/baselines/reference/for-inStatementsArray.js
@@ -11,6 +11,33 @@ for (let x in a) {
     let b3 = b[+x];
 }
 
+for (let x in a) {
+    for (let y in a) {
+        for (let z in a) {
+            let a1 = a[x];
+            let a2 = a[y];
+            let a3 = a[z];
+        }
+    }
+}
+
+let i: string;
+let j: string;
+for (i in a) {
+    for (j in b) {
+        let a1 = a[i];
+        let a2 = a[j];
+    }
+}
+
+var s: string;
+for (var s in a) {
+    let a1 = a[s];
+}
+for (s in a) {
+    let a1 = a[s];
+}
+
 
 //// [for-inStatementsArray.js]
 var a;
@@ -22,4 +49,28 @@ for (var x in a) {
     var b1 = b[x];
     var b2 = b[(x)];
     var b3 = b[+x];
+}
+for (var x in a) {
+    for (var y in a) {
+        for (var z in a) {
+            var a1 = a[x];
+            var a2 = a[y];
+            var a3 = a[z];
+        }
+    }
+}
+var i;
+var j;
+for (i in a) {
+    for (j in b) {
+        var a1 = a[i];
+        var a2 = a[j];
+    }
+}
+var s;
+for (var s in a) {
+    var a1 = a[s];
+}
+for (s in a) {
+    var a1 = a[s];
 }
--- tests/cases/conformance/statements/for-inStatements/for-inStatementsArray.ts
+++ tests/cases/conformance/statements/for-inStatements/for-inStatementsArray.ts
@@ -9,3 +9,30 @@ for (let x in a) {
     let b2 = b[(x)];
     let b3 = b[+x];
 }
+
+for (let x in a) {
+    for (let y in a) {
+        for (let z in a) {
+            let a1 = a[x];
+            let a2 = a[y];
+            let a3 = a[z];
+        }
+    }
+}
+
+let i: string;
+let j: string;
+for (i in a) {
+    for (j in b) {
+        let a1 = a[i];
+        let a2 = a[j];
+    }
+}
+
+var s: string;
+for (var s in a) {
+    let a1 = a[s];
+}
+for (s in a) {
+    let a1 = a[s];
+}
