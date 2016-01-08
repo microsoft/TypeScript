259a3cf6f8b6b5cdb4e66db04b3416150a5a68c5
Daniel Rosenwasser <drosen@microsoft.com>
2016-01-08 00:28:29 -0500

    Accepted baselines.

---
+++ tests/baselines/reference/stringLiteralsAssertionsInEqualityComparisons02.js
@@ -0,0 +1,9 @@
+//// [stringLiteralsAssertionsInEqualityComparisons02.ts]
+var a = "foo" === "bar" as "baz";
+var b = "foo" !== ("bar" as "foo");
+var c = "foo" == (<number>"bar");
+
+//// [stringLiteralsAssertionsInEqualityComparisons02.js]
+var a = "foo" === "bar";
+var b = "foo" !== "bar";
+var c = "foo" == "bar";
---
+++ tests/baselines/reference/stringLiteralsAssertionsInSwitchCase02.js
@@ -0,0 +1,15 @@
+//// [stringLiteralsAssertionsInSwitchCase02.ts]
+switch ("foo" as string) {
+    case "bar":
+        break;
+    case (("bar" || "baz")):
+        break;
+}
+
+//// [stringLiteralsAssertionsInSwitchCase02.js]
+switch ("foo") {
+    case "bar":
+        break;
+    case (("bar" || "baz")):
+        break;
+}
