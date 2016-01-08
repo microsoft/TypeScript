99ff4623e6e0ccafe699c079ef686ec0ab8d9c0b
Vladimir Matveev <vladima@microsoft.com>
2016-01-08 10:56:09 -0800

    added baselines

---
+++ tests/baselines/reference/reExportGlobalDeclaration1.js
@@ -0,0 +1,22 @@
+//// [tests/cases/compiler/reExportGlobalDeclaration1.ts] ////
+
+//// [file1.d.ts]
+
+declare var x: number;
+declare var x1: number;
+
+//// [file2.ts]
+export {x, x as y};
+export {x1, x1 as y1};
+
+export {x as z};
+export {x1 as z1};
+
+//// [file2.js]
+"use strict";
+exports.x = x;
+exports.y = x;
+exports.z = x;
+exports.x1 = x1;
+exports.y1 = x1;
+exports.z1 = x1;
---
+++ tests/baselines/reference/reExportGlobalDeclaration3.js
@@ -0,0 +1,26 @@
+//// [tests/cases/compiler/reExportGlobalDeclaration3.ts] ////
+
+//// [file1.d.ts]
+
+declare namespace NS1 {
+    export var foo: number;
+}
+
+declare namespace NS2 {
+    export var foo: number;
+}
+
+//// [file2.ts]
+export {NS1, NS1 as NNS1};
+export {NS2, NS2 as NNS2};
+export {NS1 as NNNS1};
+export {NS2 as NNNS2};
+
+//// [file2.js]
+"use strict";
+exports.NS1 = NS1;
+exports.NNS1 = NS1;
+exports.NNNS1 = NS1;
+exports.NS2 = NS2;
+exports.NNS2 = NS2;
+exports.NNNS2 = NS2;
