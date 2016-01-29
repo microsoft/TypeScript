5fe84781592a08b5294e01a2fbf42d1def07111d
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-01-29 14:46:01 -0800

    Add overloads for Function.apply/call/bind
    The new overloads use this types to specify the return type of these
    functions as well as the type of `thisArg`.

--- src/lib/core.d.ts
+++ src/lib/core.d.ts
@@ -215,14 +215,16 @@ interface Function {
       * @param thisArg The object to be used as the this object.
       * @param argArray A set of arguments to be passed to the function.
       */
-    apply(thisArg: any, argArray?: any): any;
+    apply<T,U>(this: (this: T, ...argArray: any[]) => U, thisArg: T, argArray?: any): U;
+    apply(this: Function, thisArg: any, argArray?: any): any;
 
     /**
       * Calls a method of an object, substituting another object for the current object.
       * @param thisArg The object to be used as the current object.
       * @param argArray A list of arguments to be passed to the method.
       */
-    call(thisArg: any, ...argArray: any[]): any;
+    call<T,U>(this: (this: T, ...argArray: any[]) => U, thisArg: T, ...argArray: any[]): U;
+    call(this: Function, thisArg: any, ...argArray: any[]): any;
 
     /**
       * For a given function, creates a bound function that has the same body as the original function. 
@@ -230,7 +232,8 @@ interface Function {
       * @param thisArg An object to which the this keyword can refer inside the new function.
       * @param argArray A list of arguments to be passed to the new function.
       */
-    bind(thisArg: any, ...argArray: any[]): any;
+    bind<T, U>(this: (this: T, ...argArray: any[]) => U, thisArg: T, ...argArray: any[]): (...argArray: any[]) => U;
+    bind(this: Function, thisArg: any, ...argArray: any[]): any;
 
     prototype: any;
     readonly length: number;
