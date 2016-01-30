0ccc8d022522da475f19f4b2bb35f915d352f900
Daniel Rosenwasser <drosen@microsoft.com>
2016-01-29 17:38:24 -0800

    Accepted baselines.

---
+++ tests/baselines/reference/typePredicateOnVariableDeclaration01.js
@@ -0,0 +1,10 @@
+//// [typePredicateOnVariableDeclaration01.ts]
+
+var x: this is string;
+
+//// [typePredicateOnVariableDeclaration01.js]
+var x;
+
+
+//// [typePredicateOnVariableDeclaration01.d.ts]
+declare var x: this is string;
