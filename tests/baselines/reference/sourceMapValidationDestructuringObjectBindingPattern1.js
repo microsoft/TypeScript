8ce886c4369e0289a61a2a261308e3e2e5872c5e
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-01-29 18:20:17 -0800

    Update baselines

---
+++ tests/baselines/reference/sourceMapValidationDestructuringObjectBindingPattern1.js
@@ -0,0 +1,7 @@
+//// [sourceMapValidationDestructuringObjectBindingPattern1.ts]
+
+var {x} = { x: 20 };
+
+//// [sourceMapValidationDestructuringObjectBindingPattern1.js]
+var x = { x: 20 }.x;
+//# sourceMappingURL=sourceMapValidationDestructuringObjectBindingPattern1.js.map
---
+++ tests/baselines/reference/sourceMapValidationDestructuringObjectBindingPattern2.js
@@ -0,0 +1,9 @@
+//// [sourceMapValidationDestructuringObjectBindingPattern2.ts]
+
+var {x} = { x: 20 };
+var { a, b } = { a: 30, b: 40 };
+
+//// [sourceMapValidationDestructuringObjectBindingPattern2.js]
+var x = { x: 20 }.x;
+var _a = { a: 30, b: 40 }, a = _a.a, b = _a.b;
+//# sourceMappingURL=sourceMapValidationDestructuringObjectBindingPattern2.js.map
