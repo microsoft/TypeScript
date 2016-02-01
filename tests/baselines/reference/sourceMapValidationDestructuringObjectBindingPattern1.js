e1911235a09e05e073557cf6c422be8783dd1242
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-02-01 11:23:03 -0800

    Remove old baselines

--- tests/baselines/reference/sourceMapValidationDestructuringObjectBindingPattern1.js
+++
@@ -1,7 +0,0 @@
-//// [sourceMapValidationDestructuringObjectBindingPattern1.ts]
-
-var {x} = { x: 20 };
-
-//// [sourceMapValidationDestructuringObjectBindingPattern1.js]
-var x = { x: 20 }.x;
-//# sourceMappingURL=sourceMapValidationDestructuringObjectBindingPattern1.js.map
