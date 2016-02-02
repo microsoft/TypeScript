89d6f166b6f7349db87e6ec8e7229178cac134cb
SetTrend <settrend@users.noreply.github.com>
2016-02-02 16:46:38 +0100

    Adapted jakefile syntax to node 0.10

--- Jakefile.js
+++ Jakefile.js
@@ -250,11 +250,11 @@ function compileFile(outFile, sources, prereqs, prefixes, useBuiltCompiler, noOu
         }
 
         if (outDir) {
-            options += ` --outDir "${outDir}"`;
+            options += ' --outDir "' + outDir + '"';
         }
 
         if (!noOutFile) {
-            options += ` --out "${outFile}"`;
+            options += ' --out "' + outFile + '"';
         }
         else {
             options += " --module commonjs"
@@ -265,7 +265,7 @@ function compileFile(outFile, sources, prereqs, prefixes, useBuiltCompiler, noOu
         }
 
         if (useDebugMode) {
-            options += ` -sourcemap -mapRoot "file:///${path.resolve(path.dirname(outFile))}"`;
+            options += ' -sourcemap -mapRoot "file:///' + path.resolve(path.dirname(outFile)) + '"';
         }
 
         if (stripInternal) {
@@ -273,7 +273,8 @@ function compileFile(outFile, sources, prereqs, prefixes, useBuiltCompiler, noOu
         }
 
         var cmd = host + " " + compilerPath + " " + options + " ";
-        cmd = cmd + sources.join(" ");
+        sources = sources.join('" "');
+        cmd = cmd + (sources ? '"' + sources + '"' : sources);
         console.log(cmd + "\n");
 
         var ex = jake.createExec([cmd]);
