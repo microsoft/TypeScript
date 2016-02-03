8d0267c854e2013b93b643280bbd1896c15b1c0a
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2016-02-03 00:09:38 -0800

    Recognize `=` as equivalent to `?` in JSDoc signatures
    Fixes #6811
    
    (cherry picked from commit f7eac73a6f2e72470c581c62857e4ea7263e871e)

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -5758,6 +5758,9 @@ namespace ts {
             function parseJSDocParameter(): ParameterDeclaration {
                 const parameter = <ParameterDeclaration>createNode(SyntaxKind.Parameter);
                 parameter.type = parseJSDocType();
+                if (parseOptional(SyntaxKind.EqualsToken)) {
+                    parameter.questionToken = createNode(SyntaxKind.QuestionToken);
+                }
                 return finishNode(parameter);
             }
 
