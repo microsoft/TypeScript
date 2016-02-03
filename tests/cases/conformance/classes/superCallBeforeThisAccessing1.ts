a5cf7c12bc57e1262592e730b5aae68318ea90b8
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-02-02 16:31:32 -0800

    add conformance tests

---
+++ tests/cases/conformance/classes/superCallBeforeThisAccessing1.ts
@@ -0,0 +1,13 @@
+﻿class Base {
+    constructor(c) { }
+}
+class D extends Base {
+    private _t;
+    constructor() {
+        super(i);
+        var s = {
+            t: this._t
+        }
+        var i = Factory.create(s);
+    }
+}
