d396f6e8c9769365f70d978d511c99823a0eb100
Bill Ticehurst <billti@hotmail.com>
2016-02-03 12:54:48 -0800

    Allow decorators in JavaScript files
    (cherry picked from commit f601e6dd760b667c50298b2c0d334cd7a2d06d52)

--- tests/cases/fourslash/getJavaScriptSemanticDiagnostics21.ts
+++ tests/cases/fourslash/getJavaScriptSemanticDiagnostics21.ts
@@ -4,12 +4,4 @@
 // @Filename: a.js
 //// @internal class C {}
 
-verify.getSemanticDiagnostics(`[
-  {
-    "message": "'decorators' can only be used in a .ts file.",
-    "start": 0,
-    "length": 9,
-    "category": "error",
-    "code": 8017
-  }
-]`);
+verify.getSemanticDiagnostics(`[]`);
