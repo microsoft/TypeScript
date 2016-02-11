f19bd4c1579c5e3003ef801c04283133d6d61935
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-02-11 12:38:21 -0800

    split function into its own file

---
+++ src/lib/es6.function.d.ts
@@ -0,0 +1,19 @@
+interface Function {
+    /**
+      * Returns the name of the function. Function names are read-only and can not be changed.
+      */
+    readonly name: string;
+}
+
+interface GeneratorFunction extends Function { }
+
+interface GeneratorFunctionConstructor {
+    /**
+      * Creates a new Generator function.
+      * @param args A list of arguments the function accepts.
+      */
+    new (...args: string[]): GeneratorFunction;
+    (...args: string[]): GeneratorFunction;
+    readonly prototype: GeneratorFunction;
+}
+declare var GeneratorFunction: GeneratorFunctionConstructor;
