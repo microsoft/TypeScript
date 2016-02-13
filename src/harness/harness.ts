1121098c5649006cbdb3dbeac750795b71ac9ed6
Bill Ticehurst <billti@hotmail.com>
2016-02-12 17:21:51 -0800

    Remove unnecessary option from harness

--- src/harness/harness.ts
+++ src/harness/harness.ts
@@ -896,8 +896,7 @@ namespace Harness {
             { name: "fileName", type: "string" },
             { name: "libFiles", type: "string" },
             { name: "noErrorTruncation", type: "boolean" },
-            { name: "suppressOutputPathCheck", type: "boolean" },
-            { name: "moduleResolution", type: "string"}
+            { name: "suppressOutputPathCheck", type: "boolean" }
         ];
 
         let optionsIndex: ts.Map<ts.CommandLineOption>;
