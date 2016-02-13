3ecc42beb2b2fb95dde32b2b7d764e84c86540c0
Vladimir Matveev <vladima@microsoft.com>
2016-02-12 17:16:50 -0800

    added misssing files

---
+++ tests/baselines/reference/relativeNamesInClassicResolution.js
@@ -0,0 +1,18 @@
+//// [tests/cases/compiler/relativeNamesInClassicResolution.ts] ////
+
+//// [a.ts]
+
+import {x} from "./b"
+
+//// [b.ts]
+export let x = 1;
+
+//// [a.js]
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+});
+//// [b.js]
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.x = 1;
+});
