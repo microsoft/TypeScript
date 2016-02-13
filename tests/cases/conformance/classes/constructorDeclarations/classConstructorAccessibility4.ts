ec7e80e377fae0360bccb30f937adc2693c592ca
AbubakerB <abubaker_bashir@hotmail.com>
2016-02-13 01:10:47 +0000

    Added tests and accept baselines

---
+++ tests/cases/conformance/classes/constructorDeclarations/classConstructorAccessibility4.ts
@@ -0,0 +1,33 @@
+class A {
+    private constructor() { }
+
+    method() {
+        class B {
+            method() {
+                new A(); // OK
+            }
+        }
+
+        class C extends A { // OK
+        }
+    }
+}
+
+var a = new A(); // error
+
+class D {
+    protected constructor() { }
+
+    method() {
+        class E {
+            method() {
+                new D(); // OK
+            }
+        }
+
+        class F extends D { // OK
+        }
+    }
+}
+
+var d = new D(); // error
