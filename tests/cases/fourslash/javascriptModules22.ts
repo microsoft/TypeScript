3ebf0fc383554ddc84dba8f85fb3fd56d68144fe
Bill Ticehurst <billti@hotmail.com>
2016-02-28 18:25:04 -0800

    Fixed default import from export equals
    (cherry picked from commit c4a10cfcdd51f831c3039e305c1c465a85c93b0b)

--- tests/cases/fourslash/javascriptModules22.ts
+++ tests/cases/fourslash/javascriptModules22.ts
@@ -5,9 +5,28 @@
 //// function foo() { return {a: "hello, world"}; }
 //// module.exports = foo();
 
+// @Filename: mod2.js
+//// var x = {name: 'test'};
+//// (function createExport(obj){
+////     module.exports = {
+////         "default": x,
+////         "sausages": {eggs: 2}
+////     };
+//// })();
+
 // @Filename: app.js
 //// import {a} from "./mod"
+//// import def, {sausages} from "./mod2"
 //// a./**/
 
 goTo.marker();
 verify.completionListContains('toString');
+
+edit.backspace(2);
+edit.insert("def.");
+verify.completionListContains("name");
+
+edit.insert("name;\nsausages.");
+verify.completionListContains("eggs");
+edit.insert("eggs;");
+verify.numberOfErrorsInCurrentFile(0);
