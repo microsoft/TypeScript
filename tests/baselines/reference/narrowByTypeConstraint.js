e5f37e4aa0f9c3a678d5155e67df98ed8b522feb
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-03-07 10:46:47 -0800

    Test narrowing constrained type parameters
    Add test and baselines

---
+++ tests/baselines/reference/narrowByTypeConstraint.js
@@ -0,0 +1,19 @@
+//// [narrowByTypeConstraint.ts]
+interface Base { b }
+interface Derived extends Base { d }
+declare function isDerived(b: Base): b is Derived;
+function f<T extends Base>(x: T, y: T) {
+	return isDerived(x) && isDerived(y) && x.d === y.d; 
+}
+function g(z: Base, ka: Base) {
+	return isDerived(z) && isDerived(ka) && z.d === ka.d;
+}
+
+
+//// [narrowByTypeConstraint.js]
+function f(x, y) {
+    return isDerived(x) && isDerived(y) && x.d === y.d;
+}
+function g(z, ka) {
+    return isDerived(z) && isDerived(ka) && z.d === ka.d;
+}
