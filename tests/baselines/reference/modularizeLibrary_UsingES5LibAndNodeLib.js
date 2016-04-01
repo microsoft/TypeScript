17e437b4c7afb5046539bc71cc32d9084b6f57a2
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-04-01 16:55:23 -0700

    Add compiler baseline-tests for using --lib node

---
+++ tests/baselines/reference/modularizeLibrary_UsingES5LibAndNodeLib.js
@@ -0,0 +1,24 @@
+//// [modularizeLibrary_UsingES5LibAndNodeLib.ts]
+
+import fs = require("fs");
+import Console = require("console");
+
+var myLog = new Console.Console(fs.createWriteStream(".\Output"));
+myLog.log("Logging to myLog");
+myLog.trace("show me the trace");
+
+console.log("No Error using node console");
+console.log(`Current Directory ${process.cwd()}`);
+console.error("Report error");
+
+
+//// [modularizeLibrary_UsingES5LibAndNodeLib.js]
+"use strict";
+var fs = require("fs");
+var Console = require("console");
+var myLog = new Console.Console(fs.createWriteStream(".\Output"));
+myLog.log("Logging to myLog");
+myLog.trace("show me the trace");
+console.log("No Error using node console");
+console.log("Current Directory " + process.cwd());
+console.error("Report error");
---
+++ tests/cases/compiler/modularizeLibrary_UsingES5LibAndNodeLib.ts
@@ -0,0 +1,13 @@
+﻿// @lib: es5,node
+// @target: es5
+
+import fs = require("fs");
+import Console = require("console");
+
+var myLog = new Console.Console(fs.createWriteStream(".\Output"));
+myLog.log("Logging to myLog");
+myLog.trace("show me the trace");
+
+console.log("No Error using node console");
+console.log(`Current Directory ${process.cwd()}`);
+console.error("Report error");
