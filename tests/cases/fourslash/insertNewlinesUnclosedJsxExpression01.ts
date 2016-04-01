127218b5ba0de64e9bf50550884e758594d5905c
Daniel Rosenwasser <drosen@microsoft.com>
2016-04-01 13:39:32 -0700

    Added test case that causes debug failure.

---
+++ tests/cases/fourslash/insertNewlinesUnclosedJsxExpression01.ts
@@ -0,0 +1,13 @@
+/// <reference path="fourslash.ts" />
+
+//@Filename: file.tsx
+////var x = <div>/**/
+
+goTo.marker();
+edit.insertLine("");
+edit.insertLine("");
+edit.insertLine("");
+
+format.document();
+
+edit.insertLine("</div>");
