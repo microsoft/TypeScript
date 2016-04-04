592a4b9fb3613b64574b7004307c053e355269c9
Sheetal Nandi <shkamat@microsoft.com>
2016-04-04 13:35:42 -0700

    Emit "declare" for temp default var name only if declaration file emit context allows Fixes #7807

--- tests/baselines/reference/declarationEmitDefaultExportWithTempVarNameWithBundling.js
+++ tests/baselines/reference/declarationEmitDefaultExportWithTempVarNameWithBundling.js
@@ -17,22 +17,6 @@ System.register("pi", [], function(exports_1, context_1) {
 
 //// [app.d.ts]
 declare module "pi" {
-    declare var _default: number;
+    var _default: number;
     export default _default;
 }
-
-
-//// [DtsFileErrors]
-
-
-app.d.ts(2,5): error TS1038: A 'declare' modifier cannot be used in an already ambient context.
-
-
-==== app.d.ts (1 errors) ====
-    declare module "pi" {
-        declare var _default: number;
-        ~~~~~~~
-!!! error TS1038: A 'declare' modifier cannot be used in an already ambient context.
-        export default _default;
-    }
-    
