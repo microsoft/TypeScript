75ef6772bf07feea611c5b8a48d6f2da84d86ae7
Sheetal Nandi <shkamat@microsoft.com>
2016-04-04 13:34:05 -0700

    Test cases for when default temp var is inserted for export default in the declaration file

---
+++ tests/baselines/reference/declarationEmitDefaultExportWithTempVarNameWithBundling.js
@@ -0,0 +1,38 @@
+//// [pi.ts]
+
+export default 3.14159;
+
+//// [app.js]
+System.register("pi", [], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("default",3.14159);
+        }
+    }
+});
+
+
+//// [app.d.ts]
+declare module "pi" {
+    declare var _default: number;
+    export default _default;
+}
+
+
+//// [DtsFileErrors]
+
+
+app.d.ts(2,5): error TS1038: A 'declare' modifier cannot be used in an already ambient context.
+
+
+==== app.d.ts (1 errors) ====
+    declare module "pi" {
+        declare var _default: number;
+        ~~~~~~~
+!!! error TS1038: A 'declare' modifier cannot be used in an already ambient context.
+        export default _default;
+    }
+    
