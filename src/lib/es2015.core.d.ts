b7be760369dc6cad766a21436996bbacef8d1310
Yui T <yuisu@microsoft.com>
2016-04-05 07:38:40 -0700

    Move propertyKey definition to es2015.core

--- src/lib/es2015.core.d.ts
+++ src/lib/es2015.core.d.ts
@@ -1,3 +1,5 @@
+declare type PropertyKey = string | number | symbol;
+
 interface Array<T> {
     /** 
       * Returns the value of the first element in the array where predicate is true, and undefined 
