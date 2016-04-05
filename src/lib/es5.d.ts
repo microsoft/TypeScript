988063e9cf1cf066383e609845d267901923a882
Yui <yuit@users.noreply.github.com>
2016-04-05 11:08:26 -0700

    Move PropertyKey from es5 to es2015.core
    * Move propertyKey definition to es2015.core
    
    * Update baselines to reflect how one will use --lib

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1247,8 +1247,6 @@ interface TypedPropertyDescriptor<T> {
     set?: (value: T) => void;
 }
 
-declare type PropertyKey = string | number | symbol;
-
 declare type ClassDecorator = <TFunction extends Function>(target: TFunction) => TFunction | void;
 declare type PropertyDecorator = (target: Object, propertyKey: string | symbol) => void;
 declare type MethodDecorator = <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>) => TypedPropertyDescriptor<T> | void;
