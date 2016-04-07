dbaa6e163c5c4576f1c02a344456215c53dea1a7
Daniel Rosenwasser <drosen@microsoft.com>
2016-04-07 00:42:04 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/useStrictLikePrologueString01.js
@@ -0,0 +1,13 @@
+//// [useStrictLikePrologueString01.ts]
+
+"hey!"
+" use strict "
+export function f() {   
+}
+
+//// [useStrictLikePrologueString01.js]
+"hey!";
+" use strict ";
+function f() {
+}
+exports.f = f;
