50d570b12c1a19ab01c19d71c8dd39e5bdf0a2d3
Daniel Rosenwasser <drosen@microsoft.com>
2016-04-08 12:34:25 -0700

    Changed emit to use 'Object.prototype.hasOwnProperty'.

--- src/compiler/emitter.ts
+++ src/compiler/emitter.ts
@@ -347,9 +347,10 @@ var __extends = (this && this.__extends) || function (d, b) {
 
         const assignHelper = `
 var __assign = (this && this.__assign) || Object.assign || function(t) {
-    for (var i = 1; i < arguments.length; i++) {
-        var s = arguments[i];
-        for (var p in s) if (s.hasOwnProperty(p)) t[p] = s[p];
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
     }
     return t;
 };`;
