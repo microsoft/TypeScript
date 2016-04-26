b6fdd36984e3c8374788d2780990284625768a86
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-04-26 16:58:50 -0700

    Add tests and baselines

---
+++ tests/baselines/reference/commentsOnRequireStatement.js
@@ -0,0 +1,36 @@
+//// [tests/cases/compiler/commentsOnRequireStatement.ts] ////
+
+//// [0.ts]
+
+export var subject = 10;
+
+//// [1.ts]
+export var subject1 = 10;
+
+//// [2.ts]
+/* tslint:disable:no-unused-variable */
+// Subject imported before Observable to bypass circular dependency issue since
+// Subject extends Observable and Observable references Subject in it's
+// definition
+export {subject} from './0';
+/* tslint:enable:no-unused-variable */
+export {subject1} from './1';
+
+
+//// [0.js]
+"use strict";
+exports.subject = 10;
+//// [1.js]
+"use strict";
+exports.subject1 = 10;
+//// [2.js]
+"use strict";
+/* tslint:disable:no-unused-variable */
+// Subject imported before Observable to bypass circular dependency issue since
+// Subject extends Observable and Observable references Subject in it's
+// definition
+var _0_1 = require("./0");
+exports.subject = _0_1.subject;
+/* tslint:enable:no-unused-variable */
+var _1_1 = require("./1");
+exports.subject1 = _1_1.subject1;
---
+++ tests/cases/compiler/commentsOnRequireStatement.ts
@@ -0,0 +1,17 @@
+﻿// @target: es5
+// @module: commonjs
+
+// @Filename: 0.ts
+export var subject = 10;
+
+// @Filename: 1.ts
+export var subject1 = 10;
+
+// @Filename: 2.ts
+/* tslint:disable:no-unused-variable */
+// Subject imported before Observable to bypass circular dependency issue since
+// Subject extends Observable and Observable references Subject in it's
+// definition
+export {subject} from './0';
+/* tslint:enable:no-unused-variable */
+export {subject1} from './1';
