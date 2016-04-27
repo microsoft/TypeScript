252f18f9d77788f003c69784b03e7669297cf002
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-04-26 17:01:40 -0700

    Update baselines

--- tests/baselines/reference/commentsOnRequireStatement.js
+++ tests/baselines/reference/commentsOnRequireStatement.js
@@ -8,12 +8,12 @@ export var subject = 10;
 export var subject1 = 10;
 
 //// [2.ts]
-/* tslint:disable:no-unused-variable */
-// Subject imported before Observable to bypass circular dependency issue since
-// Subject extends Observable and Observable references Subject in it's
-// definition
+/* blah0 */
+// blah 
+// blah 
+// blah 
 export {subject} from './0';
-/* tslint:enable:no-unused-variable */
+/* blah1 */
 export {subject1} from './1';
 
 
@@ -25,12 +25,12 @@ exports.subject = 10;
 exports.subject1 = 10;
 //// [2.js]
 "use strict";
-/* tslint:disable:no-unused-variable */
-// Subject imported before Observable to bypass circular dependency issue since
-// Subject extends Observable and Observable references Subject in it's
-// definition
+/* blah0 */
+// blah 
+// blah 
+// blah 
 var _0_1 = require("./0");
 exports.subject = _0_1.subject;
-/* tslint:enable:no-unused-variable */
+/* blah1 */
 var _1_1 = require("./1");
 exports.subject1 = _1_1.subject1;
