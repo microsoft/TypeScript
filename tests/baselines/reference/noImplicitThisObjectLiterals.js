8d45a73f126b6c5779bb7ee07ae6923bfe9dbe8c
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-04-29 16:42:51 -0700

    Add --noImplicitThis test for object literals

---
+++ tests/baselines/reference/noImplicitThisObjectLiterals.js
@@ -0,0 +1,22 @@
+//// [noImplicitThisObjectLiterals.ts]
+let o = {
+    d: this, // error, this: any
+    m() {
+        return this.d.length; // error, this: any
+    },
+    f: function() {
+        return this.d.length; // error, this: any
+    }
+}
+
+
+//// [noImplicitThisObjectLiterals.js]
+var o = {
+    d: this,
+    m: function () {
+        return this.d.length; // error, this: any
+    },
+    f: function () {
+        return this.d.length; // error, this: any
+    }
+};
