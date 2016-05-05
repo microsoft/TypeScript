673fa41418f425b4218ee80a812a43c4595c8d2c
Yui <yuit@users.noreply.github.com>
2016-05-05 11:10:00 -0700

    Fix 8407: Allow (weak)map & (weak)set constructor when targeting ES5 using ES6 library (#8451)
    * Move iterabl related stuffs off from es2015.collection
    
    * Move symbol.wellknown stuffs off from es2015.collection
    
    * Add map & set constructor support when targeting ES5
    
    * Address PR: move symbol.iterator to es2015.iterable
    
    * Move Symbol.Iterator into es2015.iterable
    
    * Update baselines

--- src/lib/es2015.collection.d.ts
+++ src/lib/es2015.collection.d.ts
@@ -10,7 +10,7 @@ interface Map<K, V> {
 
 interface MapConstructor {
     new (): Map<any, any>;
-    new <K, V>(): Map<K, V>;
+    new <K, V>(entries?: [K, V][]): Map<K, V>;
     readonly prototype: Map<any, any>;
 }
 declare var Map: MapConstructor;
@@ -26,7 +26,7 @@ interface WeakMap<K, V> {
 
 interface WeakMapConstructor {
     new (): WeakMap<any, any>;
-    new <K, V>(): WeakMap<K, V>;
+    new <K, V>(entries?: [K, V][]): WeakMap<K, V>;
     readonly prototype: WeakMap<any, any>;
 }
 declare var WeakMap: WeakMapConstructor;
@@ -35,20 +35,14 @@ interface Set<T> {
     add(value: T): Set<T>;
     clear(): void;
     delete(value: T): boolean;
-    entries(): IterableIterator<[T, T]>;
     forEach(callbackfn: (value: T, index: T, set: Set<T>) => void, thisArg?: any): void;
     has(value: T): boolean;
-    keys(): IterableIterator<T>;
     readonly size: number;
-    values(): IterableIterator<T>;
-    [Symbol.iterator]():IterableIterator<T>;
-    readonly [Symbol.toStringTag]: "Set";
 }
 
 interface SetConstructor {
     new (): Set<any>;
-    new <T>(): Set<T>;
-    new <T>(iterable: Iterable<T>): Set<T>;
+    new <T>(values?: T[]): Set<T>;
     readonly prototype: Set<any>;
 }
 declare var Set: SetConstructor;
@@ -58,13 +52,12 @@ interface WeakSet<T> {
     clear(): void;
     delete(value: T): boolean;
     has(value: T): boolean;
-    readonly [Symbol.toStringTag]: "WeakSet";
+
 }
 
 interface WeakSetConstructor {
     new (): WeakSet<any>;
-    new <T>(): WeakSet<T>;
-    new <T>(iterable: Iterable<T>): WeakSet<T>;
+    new <T>(values?: T[]): WeakSet<T>;
     readonly prototype: WeakSet<any>;
 }
 declare var WeakSet: WeakSetConstructor;
