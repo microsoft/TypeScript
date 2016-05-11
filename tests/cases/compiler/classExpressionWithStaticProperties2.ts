569f39a559068ae1734e981b43c576cdf9abfefb
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-05-10 17:23:25 -0700

    Add tests and Update baselines
    Update tests and baselines
    
    Update tests and baselines
    
    Update tests for downlevel emit
    
    Add tests and update baselines
    
    Add tests and baselines
    
    Update baselines
    
    Update baselines

--- tests/cases/compiler/classExpressionWithStaticProperties2.ts
+++ tests/cases/compiler/classExpressionWithStaticProperties2.ts
@@ -1,1 +1,9 @@
-var v = class C { static a = 1; static b };
+//@target: es5
+var v = class C {
+    static a = 1;
+    static b
+    static c = {
+        x: "hi"
+    }
+    static d = C.c.x + " world";
+ };
