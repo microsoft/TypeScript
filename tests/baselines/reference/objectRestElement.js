56a439f6fda61a97ec157d1563a120eef8ace63e
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-05-11 10:02:33 -0700

    Accept object rest element baselines
    Note that the emitted javascript is still wrong.

---
+++ tests/baselines/reference/objectRestElement.js
@@ -0,0 +1,12 @@
+//// [objectRestElement.ts]
+let o = { a: 1, b: 'no' };
+var { ...clone } = o;
+var { a, ...justB } = o;
+var { a, b, ...empty } = o;
+
+
+//// [objectRestElement.js]
+var o = { a: 1, b: 'no' };
+var clone = o.clone;
+var a = o.a, justB = o.justB;
+var a = o.a, b = o.b, empty = o.empty;
---
+++ tests/baselines/reference/objectRestElementNegative.js
@@ -0,0 +1,8 @@
+//// [objectRestElementNegative.ts]
+let o = { a: 1, b: 'no' };
+var { ...mustBeLast, a } = o;
+
+
+//// [objectRestElementNegative.js]
+var o = { a: 1, b: 'no' };
+var mustBeLast = o.mustBeLast, a = o.a;
