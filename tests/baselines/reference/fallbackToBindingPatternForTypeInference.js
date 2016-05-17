70d8def398d9dfefa4d049152a3d801611aa2ff5
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-05-17 09:33:55 -0700

    Add test that infers 'number' from pattern

--- tests/baselines/reference/fallbackToBindingPatternForTypeInference.js
+++ tests/baselines/reference/fallbackToBindingPatternForTypeInference.js
@@ -4,6 +4,7 @@ trans(({a}) => a);
 trans(([b,c]) => 'foo');
 trans(({d: [e,f]}) => 'foo');
 trans(([{g},{h}]) => 'foo');
+trans(({a, b = 10}) => a);
 
 
 //// [fallbackToBindingPatternForTypeInference.js]
@@ -22,4 +23,8 @@ trans(function (_a) {
 trans(function (_a) {
     var g = _a[0].g, h = _a[1].h;
     return 'foo';
+});
+trans(function (_a) {
+    var a = _a.a, _b = _a.b, b = _b === void 0 ? 10 : _b;
+    return a;
 });
--- tests/cases/compiler/fallbackToBindingPatternForTypeInference.ts
+++ tests/cases/compiler/fallbackToBindingPatternForTypeInference.ts
@@ -3,3 +3,4 @@ trans(({a}) => a);
 trans(([b,c]) => 'foo');
 trans(({d: [e,f]}) => 'foo');
 trans(([{g},{h}]) => 'foo');
+trans(({a, b = 10}) => a);
