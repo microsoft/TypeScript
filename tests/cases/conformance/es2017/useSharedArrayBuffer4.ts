70c390bbbfb246cb6188edf1e8fc178c7dfd9fe6
Yuichi Nukiyama <oscar.wilde84@hotmail.co.jp>
2016-05-23 23:13:38 +0900

    Revert "Add SharedArrayBuffer"
    This reverts commit b096f78f51a6301ed716122d50f6031bc12a4d14.

--- tests/cases/conformance/es2017/useSharedArrayBuffer4.ts
+++
@@ -1,8 +0,0 @@
-// @target: es6
-// @lib: es2017
-
-var foge = new SharedArrayBuffer(1024);
-var bar = foge.slice(1, 10);
-var species = foge[Symbol.species];
-var stringTag = foge[Symbol.toStringTag];
-var len = foge.byteLength;
