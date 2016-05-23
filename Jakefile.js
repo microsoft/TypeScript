b096f78f51a6301ed716122d50f6031bc12a4d14
Yuichi Nukiyama <oscar.wilde84@hotmail.co.jp>
2016-05-23 23:11:21 +0900

    Add SharedArrayBuffer

--- Jakefile.js
+++ Jakefile.js
@@ -187,7 +187,10 @@ var es2016LibrarySourceMap = es2016LibrarySource.map(function (source) {
     return { target: "lib." + source, sources: ["header.d.ts", source] };
 });
 
-var es2017LibrarySource = ["es2017.object.d.ts"];
+var es2017LibrarySource = [
+    "es2017.object.d.ts",
+    "es2017.sharedmemory.d.ts"
+];
 
 var es2017LibrarySourceMap = es2017LibrarySource.map(function (source) {
     return { target: "lib." + source, sources: ["header.d.ts", source] };
