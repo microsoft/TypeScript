44493583ce90fbd8e859c3d377157efa29ed8add
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-05-25 09:02:43 -0700

    Change tests name

--- tests/baselines/reference/classExpressionWithStaticProperties3_ES6.js
+++ tests/baselines/reference/classExpressionWithStaticProperties3_ES6.js
@@ -1,23 +1,23 @@
-//// [classExpressionWithStaticPropertiesES64.ts]
+//// [classExpressionWithStaticProperties3_ES6.ts]
 declare var console: any;
 const arr: any[] = [];
 for (let i = 0; i < 3; i++) {
-    arr.push((class C {
+    arr.push(class C {
         static x = i;
         static y = () => C.x * 2;
-    }), i);
+    });
 }
 arr.forEach(C => console.log(C.y()));
 
-//// [classExpressionWithStaticPropertiesES64.js]
+//// [classExpressionWithStaticProperties3_ES6.js]
 const arr = [];
 for (let i = 0; i < 3; i++) {
-    arr.push(((() => {
+    arr.push((() => {
         let _a = class C {
             };
         _a.x = i;
         _a.y = () => C.x * 2;
         return _a;
-    })()), i);
+    })());
 }
 arr.forEach(C => console.log(C.y()));
