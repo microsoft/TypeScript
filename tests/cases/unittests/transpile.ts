99b3167635c182ebb03983cd42d272956cbaadf2
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-06-01 17:27:47 -0700

    Disable using null in unittest as we use null to signal to not generate baselines

--- tests/cases/unittests/transpile.ts
+++ tests/cases/unittests/transpile.ts
@@ -64,7 +64,9 @@ namespace ts {
                 it("Correct errors for " + justName, () => {
                     Harness.Baseline.runBaseline("Correct errors", justName.replace(/\.tsx?$/, ".errors.txt"), () => {
                         if (transpileResult.diagnostics.length === 0) {
-                            return undefined;
+                            /* tslint:disable:no-null-keyword */
+                            return null;
+                            /* tslint:enable:no-null-keyword */
                         }
 
                         return Harness.Compiler.getErrorBaseline(toBeCompiled, transpileResult.diagnostics);
@@ -75,7 +77,9 @@ namespace ts {
                     it("Correct errors (old transpile) for " + justName, () => {
                         Harness.Baseline.runBaseline("Correct errors", justName.replace(/\.tsx?$/, ".oldTranspile.errors.txt"), () => {
                             if (oldTranspileDiagnostics.length === 0) {
-                                return undefined;
+                                /* tslint:disable:no-null-keyword */
+                                return null;
+                                /* tslint:enable:no-null-keyword */
                             }
 
                             return Harness.Compiler.getErrorBaseline(toBeCompiled, oldTranspileDiagnostics);
