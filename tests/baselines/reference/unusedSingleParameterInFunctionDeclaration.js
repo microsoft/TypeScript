e17ed58a27bcd16516273c4eb981cef546bb4e59
Sarangan Rajamanickam <sarajama@microsoft.com>
2016-06-03 15:07:58 -0700

    Compiler Test Cases

---
+++ tests/baselines/reference/unusedSingleParameterInFunctionDeclaration.js
@@ -0,0 +1,10 @@
+//// [unusedSingleParameterInFunctionDeclaration.ts]
+
+function greeter(person: string) {
+    var unused = 20;
+}
+
+//// [unusedSingleParameterInFunctionDeclaration.js]
+function greeter(person) {
+    var unused = 20;
+}
