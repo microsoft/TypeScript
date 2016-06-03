8b0974a77e6317891fc11259dd06f721b621be5d
Andy Hanson <anhans@microsoft.com>
2016-06-03 09:29:21 -0700

    Update tests

--- tests/baselines/reference/trailingCommasInFunctionParametersAndArguments.js
+++ tests/baselines/reference/trailingCommasInFunctionParametersAndArguments.js
@@ -3,7 +3,18 @@ function f1(x,) {}
 
 f1(1,);
 
+function f2(...args,) {}
+
+f2(...[],);
+
 
 //// [trailingCommasInFunctionParametersAndArguments.js]
 function f1(x) { }
 f1(1);
+function f2() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i - 0] = arguments[_i];
+    }
+}
+f2.apply(void 0, []);
--- tests/cases/conformance/es7/trailingCommasInFunctionParametersAndArguments.ts
+++ tests/cases/conformance/es7/trailingCommasInFunctionParametersAndArguments.ts
@@ -1,3 +1,7 @@
 function f1(x,) {}
 
 f1(1,);
+
+function f2(...args,) {}
+
+f2(...[],);
