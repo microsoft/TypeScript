b4f12144d2c9571aa48db44cfc3778c3e721bec9
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-06-07 08:17:28 -0700

    Remove incorrectly added tests

--- tests/cases/compiler/inferFromConstraints.ts
+++
@@ -1,9 +0,0 @@
-interface Ctor<T> {
-    new(): T;
-}
-// declare function create1<T>(ctor: Ctor<T>): T;
-declare function create2<T, C extends Ctor<T>>(ctor: C): T;
-
-class A { a: number }
-// let a1 = create1(A).a; // a: A --> OK
-let a2 = create2(A).a; // a: {} --> Should be A
--- tests/cases/conformance/types/contextualTypes/methodDeclarations/contextuallyTypedObjectLiteralReturnType.ts
+++
@@ -1,9 +0,0 @@
-// @noImplicitAny: true
-function id<T>(x: T): T { return x };
-// Correct: type of fnWrapper is (y: number) => { y: number }
-var fn = function(y: number) { return { y } };
-var fnWrapper = id(fn);
-// Incorrect: type of inlineWrapper is (z: number) => any 
-var inlineWrapper = id(function(z: number) { return { z } });
-let error1: number = fnWrapper(12).y.length; // should error
-let error2: number = inlineWrapper(12).z.length; // should error
