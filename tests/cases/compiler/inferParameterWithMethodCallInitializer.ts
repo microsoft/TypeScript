9fac99e6922912916c92689483babd38854d8d1f
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-06-08 14:23:42 -0700

    Add test case
    The test passes in 1.8 and fails in master.

---
+++ tests/cases/compiler/inferParameterWithMethodCallInitializer.ts
@@ -0,0 +1,12 @@
+// @noImplicitAny: true
+function getNumber(): number {
+    return 1;
+}
+class Example {
+    getNumber(): number {
+        return 1;
+    }
+    doSomething(a = this.getNumber()): typeof a {
+        return a;
+    }
+}
