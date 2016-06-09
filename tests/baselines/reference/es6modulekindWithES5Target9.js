475c1c24fc4e380de0b585da0a2608989976aa91
Mohamed Hegazy <mhegazy@microsoft.com>
2016-06-08 17:48:37 -0700

    Add more tests

---
+++ tests/baselines/reference/es6modulekindWithES5Target9.js
@@ -0,0 +1,24 @@
+//// [es6modulekindWithES5Target9.ts]
+
+import d from "mod";
+
+import {a} from "mod";
+
+import * as M from "mod";
+
+export {a};
+
+export * from "mod";
+
+export {b} from "mod"
+
+export default d;
+
+
+//// [es6modulekindWithES5Target9.js]
+import d from "mod";
+import { a } from "mod";
+export { a };
+export * from "mod";
+export { b } from "mod";
+export default d;
---
+++ tests/cases/compiler/es6modulekindWithES5Target9.ts
@@ -0,0 +1,16 @@
+// @target: es5
+// @module: es2015
+
+import d from "mod";
+
+import {a} from "mod";
+
+import * as M from "mod";
+
+export {a};
+
+export * from "mod";
+
+export {b} from "mod"
+
+export default d;
