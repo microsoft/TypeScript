0a1c4c60da9609155ba65dcfc5932ce748c3c3ba
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2016-06-14 11:36:14 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/emptyTuplesTypeAssertion01.js
@@ -0,0 +1,27 @@
+//// [emptyTuplesTypeAssertion01.ts]
+
+let x = <[]>[];
+let y = x[0];
+
+//// [emptyTuplesTypeAssertion01.js]
+var x = [];
+var y = x[0];
+
+
+//// [emptyTuplesTypeAssertion01.d.ts]
+declare let x: [];
+declare let y: never;
+
+
+//// [DtsFileErrors]
+
+
+tests/cases/conformance/types/tuple/emptyTuples/emptyTuplesTypeAssertion01.d.ts(1,16): error TS1122: A tuple type element list cannot be empty.
+
+
+==== tests/cases/conformance/types/tuple/emptyTuples/emptyTuplesTypeAssertion01.d.ts (1 errors) ====
+    declare let x: [];
+                   ~~
+!!! error TS1122: A tuple type element list cannot be empty.
+    declare let y: never;
+    
---
+++ tests/baselines/reference/emptyTuplesTypeAssertion02.js
@@ -0,0 +1,27 @@
+//// [emptyTuplesTypeAssertion02.ts]
+
+let x = [] as [];
+let y = x[0];
+
+//// [emptyTuplesTypeAssertion02.js]
+var x = [];
+var y = x[0];
+
+
+//// [emptyTuplesTypeAssertion02.d.ts]
+declare let x: [];
+declare let y: never;
+
+
+//// [DtsFileErrors]
+
+
+tests/cases/conformance/types/tuple/emptyTuples/emptyTuplesTypeAssertion02.d.ts(1,16): error TS1122: A tuple type element list cannot be empty.
+
+
+==== tests/cases/conformance/types/tuple/emptyTuples/emptyTuplesTypeAssertion02.d.ts (1 errors) ====
+    declare let x: [];
+                   ~~
+!!! error TS1122: A tuple type element list cannot be empty.
+    declare let y: never;
+    
