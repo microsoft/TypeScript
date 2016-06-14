82b385f33122eeebdd266d851ebd479020838847
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2016-06-14 15:15:42 -0700

    Accepted baselines.

--- tests/baselines/reference/emptyTuplesTypeAssertion02.js
+++ tests/baselines/reference/emptyTuplesTypeAssertion02.js
@@ -11,17 +11,3 @@ var y = x[0];
 //// [emptyTuplesTypeAssertion02.d.ts]
 declare let x: [];
 declare let y: never;
-
-
-//// [DtsFileErrors]
-
-
-tests/cases/conformance/types/tuple/emptyTuples/emptyTuplesTypeAssertion02.d.ts(1,16): error TS1122: A tuple type element list cannot be empty.
-
-
-==== tests/cases/conformance/types/tuple/emptyTuples/emptyTuplesTypeAssertion02.d.ts (1 errors) ====
-    declare let x: [];
-                   ~~
-!!! error TS1122: A tuple type element list cannot be empty.
-    declare let y: never;
-    
