97a3d904a053aa65c0703bdda28cec4de7189a2b
Sarangan Rajamanickam <sarajama@microsoft.com>
2016-06-23 17:45:13 -0700

    Added test for another scenario involving type parameters

---
+++ tests/cases/fourslash/unusedTypeParametersInLambda4.ts
@@ -0,0 +1,15 @@
+/// <reference path='fourslash.ts' />
+
+// @noUnusedLocals: true
+//// class A<T> {
+////    public x: T;
+//// }
+//// var y: new <T,U>(a:T)=>void;
+
+verify.codeFixAtPosition(`
+class A<T> {
+   public x: T;
+}
+var y: new <T>(a:T)=>void;
+`);
+
