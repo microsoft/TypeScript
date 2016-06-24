cc4a5c7e9769160a5902412f41a539b5ab218103
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-06-24 13:18:34 -0700

    Add tets and update baselines

--- tests/cases/conformance/jsx/tsxUnionTypeComponent2.tsx
+++ tests/cases/conformance/jsx/tsxUnionTypeComponent2.tsx
@@ -5,9 +5,9 @@
 
 import React = require('react');
 
-type Invalid1 = React.ComponentClass<any> | string;
+type Invalid1 = React.ComponentClass<any> | number;
 
-const X: Invalid1 = "Should fail to construct";
+const X: Invalid1 = 1;
 
 <X />;
 
