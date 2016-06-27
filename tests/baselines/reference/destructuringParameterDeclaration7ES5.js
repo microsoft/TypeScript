a7cdd38f922d76f6a5122e961f0d2ef221e0f69b
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-06-27 15:57:48 -0700

    Add tests and baselines

---
+++ tests/baselines/reference/destructuringParameterDeclaration7ES5.js
@@ -0,0 +1,14 @@
+//// [destructuringParameterDeclaration7ES5.ts]
+
+interface ISomething {
+    foo: string,
+    bar: string
+}
+
+function foo({}, {foo, bar}: ISomething) {}
+
+
+//// [destructuringParameterDeclaration7ES5.js]
+function foo(_a, _b) {
+    var foo = _b.foo, bar = _b.bar;
+}
