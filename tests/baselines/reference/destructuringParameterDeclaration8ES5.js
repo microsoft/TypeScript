5cf7cf29807eb4a3ffacc423e8adab35240a76cd
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-06-29 11:31:27 -0700

    Add tests and baselines

---
+++ tests/baselines/reference/destructuringParameterDeclaration8ES5.js
@@ -0,0 +1,36 @@
+//// [destructuringParameterDeclaration8ES5.ts]
+
+function one({}, {foo, bar}) {
+  // ...
+}
+
+function two([], [a,b]) {}
+
+// should be an error
+one(undefined, { foo: 'foo', bar: 'bar' });  
+
+one(undefined, undefined);
+
+one(null,  { foo: 'foo', bar: 'bar' });
+
+one(null, null);
+
+two(undefined, undefined);
+
+two(null, null);
+
+//// [destructuringParameterDeclaration8ES5.js]
+function one(_a, _b) {
+    var foo = _b.foo, bar = _b.bar;
+    // ...
+}
+function two(_a, _b) {
+    var a = _b[0], b = _b[1];
+}
+// should be an error
+one(undefined, { foo: 'foo', bar: 'bar' });
+one(undefined, undefined);
+one(null, { foo: 'foo', bar: 'bar' });
+one(null, null);
+two(undefined, undefined);
+two(null, null);
