5a45c44eb789f52ceb1aa0e23a230ecb599bfb08
Bill Ticehurst <billti@hotmail.com>
2016-06-29 17:04:42 -0700

    Dont load JavaScript if types packages are present

--- tests/baselines/reference/project/nodeModulesMaxDepthIncreased/amd/maxDepthIncreased/root.js
+++ tests/baselines/reference/project/nodeModulesMaxDepthIncreased/amd/maxDepthIncreased/root.js
@@ -1,6 +1,8 @@
-define(["require", "exports", "m1"], function (require, exports, m1) {
+define(["require", "exports", "m1", "m4"], function (require, exports, m1, m4) {
     "use strict";
     m1.f1("test");
     m1.f2.a = 10;
-    m1.f2.person.age = "10"; // Error: Should be number
+    m1.f2.person.age = "10"; // Should error if loaded the .js files correctly
+    var r1 = m4.test.charAt(2); // Should error if correctly not using the .js file but using @types info
+    var r2 = 3 + m4.foo; // Should be OK if correctly using the @types .d.ts file
 });
--- tests/cases/projects/NodeModulesSearch/maxDepthIncreased/root.ts
+++ tests/cases/projects/NodeModulesSearch/maxDepthIncreased/root.ts
@@ -1,4 +1,10 @@
 import * as m1 from "m1";
+import * as m4 from "m4";
+
 m1.f1("test");
 m1.f2.a = 10;
-m1.f2.person.age = "10"; // Error: Should be number
+
+m1.f2.person.age = "10";    // Should error if loaded the .js files correctly
+let r1 = m4.test.charAt(2); // Should error if correctly not using the .js file but using @types info
+
+let r2 = 3 + m4.foo; // Should be OK if correctly using the @types .d.ts file
