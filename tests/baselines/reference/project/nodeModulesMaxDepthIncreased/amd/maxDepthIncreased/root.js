5e4f13f342a75ec8f7cf65cb669bec9d6e6c5581
Bill Ticehurst <billti@hotmail.com>
2016-06-30 16:36:39 -0700

    Removed one error to avoid full path issues

--- tests/baselines/reference/project/nodeModulesMaxDepthIncreased/amd/maxDepthIncreased/root.js
+++ tests/baselines/reference/project/nodeModulesMaxDepthIncreased/amd/maxDepthIncreased/root.js
@@ -3,6 +3,5 @@ define(["require", "exports", "m1", "m4"], function (require, exports, m1, m4) {
     m1.f1("test");
     m1.f2.a = 10;
     m1.f2.person.age = "10"; // Should error if loaded the .js files correctly
-    var r1 = m4.test.charAt(2); // Should error if correctly not using the .js file but using @types info
     var r2 = 3 + m4.foo; // Should be OK if correctly using the @types .d.ts file
 });
