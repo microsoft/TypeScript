2dc84a8c4b5ecdb09fea7acc2c3bfa2712e741de
Bill Ticehurst <billti@hotmail.com>
2016-06-30 16:36:39 -0700

    Removed one error to avoid full path issues
    (cherry picked from commit 5e4f13f342a75ec8f7cf65cb669bec9d6e6c5581)

--- tests/cases/projects/NodeModulesSearch/maxDepthIncreased/root.ts
+++ tests/cases/projects/NodeModulesSearch/maxDepthIncreased/root.ts
@@ -5,6 +5,5 @@ m1.f1("test");
 m1.f2.a = 10;
 
 m1.f2.person.age = "10";    // Should error if loaded the .js files correctly
-let r1 = m4.test.charAt(2); // Should error if correctly not using the .js file but using @types info
 
 let r2 = 3 + m4.foo; // Should be OK if correctly using the @types .d.ts file
