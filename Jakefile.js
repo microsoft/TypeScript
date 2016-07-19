4a3efe4b0fd1395a93b4b7a0785dc5f68c4718d1
Raghav Katyal <rakatyal@microsoft.com>
2016-07-19 11:45:57 -0700

    Jakefile entry

--- Jakefile.js
+++ Jakefile.js
@@ -994,6 +994,7 @@ var tslintRules = [
     "noInOperatorRule",
     "noIncrementDecrementRule",
     "objectLiteralSurroundingSpaceRule",
+    "enforceConsecutiveFunctionOverloadRule",
 ];
 var tslintRulesFiles = tslintRules.map(function(p) {
     return path.join(tslintRuleDir, p + ".ts");
