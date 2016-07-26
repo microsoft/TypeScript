a69108b76b49452fdc3821f0958464a813626995
Wesley Wigham <t-weswig@microsoft.com>
2016-07-26 10:32:24 -0700

    Add the word `Rule` to Jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -990,7 +990,7 @@ var tslintRules = [
     "noInOperatorRule",
     "noIncrementDecrementRule",
     "objectLiteralSurroundingSpaceRule",
-    "noTypeAssertionWhitespace"
+    "noTypeAssertionWhitespaceRule"
 ];
 var tslintRulesFiles = tslintRules.map(function(p) {
     return path.join(tslintRuleDir, p + ".ts");
