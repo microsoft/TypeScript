6fbd79b70954682f8a131a4899f8a227237e91f5
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-07-27 13:25:16 -0700

    Update baselines to be current

--- tests/baselines/reference/jsdocStringLiteral.js
+++ tests/baselines/reference/jsdocStringLiteral.js
@@ -1,16 +1,22 @@
 //// [in.js]
 /**
- * @param {'literal'} input
+ * @param {'literal'} p1
+ * @param {"literal"} p2
+ * @param {'literal' | 'other'} p3
+ * @param {'literal' | number} p4
  */
-function f(input) {
-    return input + '.';
+function f(p1, p2, p3, p4) {
+    return p1 + p2 + p3 + p4 + '.';
 }
 
 
 //// [out.js]
 /**
- * @param {'literal'} input
+ * @param {'literal'} p1
+ * @param {"literal"} p2
+ * @param {'literal' | 'other'} p3
+ * @param {'literal' | number} p4
  */
-function f(input) {
-    return input + '.';
+function f(p1, p2, p3, p4) {
+    return p1 + p2 + p3 + p4 + '.';
 }
