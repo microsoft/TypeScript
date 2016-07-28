ef53c24f320fef3e11a89431a88b8a449c77d26f
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-07-27 17:02:39 -0700

    wip - add tests

---
+++ tests/cases/fourslash/findAllRefsForUMDModuleAlias1.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path='fourslash.ts' />
+
+// @filename: 0.d.ts
+//// export function doThing(): string;
+//// export function doTheOtherThing(): void;
+
+//// export as namespace [|myLib|];
+
+// @filename: 1.ts
+
+//// /// <reference path="0.d.ts" />
+//// [|myLib|].doThing();
