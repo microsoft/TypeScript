e661bbee79fc464e9932badac8e0edb59f160cda
Ron Buckton <rbuckton@microsoft.com>
2016-08-02 17:58:49 -0700

    Fixed and accepted new baselines.

--- tests/baselines/reference/es5-importHelpersAsyncFunctions.js
+++ tests/baselines/reference/es5-importHelpersAsyncFunctions.js
@@ -38,7 +38,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (sent[0] === 1) throw sent[1]; return sent[1]; }, trys: [], stack: [] }, sent, f;
+    var _ = { label: 0, sent: function() { if (sent[0] === 1) throw sent[1]; return sent[1]; }, trys: [], stack: [] }, sent, star, f;
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
         while (1) {
@@ -48,9 +48,17 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
                 case 2: return { value: op[1], done: true };
             }
             try {
-                switch (f = 1, op[0]) {
+                f = 1;
+                if (star) {
+                    var v = star[["next", "throw", "return"][op[0]]];
+                    if (v && !(v = v.call(star, op[1])).done) return v;
+                    if (v) op = [0, v.value];
+                    star = void 0; continue;
+                }
+                switch (op[0]) {
                     case 0: case 1: sent = op; break;
                     case 4: return _.label++, { value: op[1], done: false };
+                    case 5: _.label++, star = op[1], op = [0]; continue;
                     case 7: op = _.stack.pop(), _.trys.pop(); continue;
                     default:
                         var r = _.trys.length > 0 && _.trys[_.trys.length - 1];
@@ -64,8 +72,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
                 }
                 op = body.call(thisArg, _);
             }
-            catch (e) { op = [6, e]; }
-            finally { f = 0, sent = void 0; }
+            catch (e) { op = [6, e], star = void 0; }
+            finally { f = 0, sent = v = void 0; }
         }
     }
     return {
--- tests/baselines/reference/templateStringWithEmbeddedYieldKeyword.js
+++ tests/baselines/reference/templateStringWithEmbeddedYieldKeyword.js
@@ -6,7 +6,61 @@ function* gen {
 
 
 //// [templateStringWithEmbeddedYieldKeyword.js]
-function* gen() {
-    // Once this is supported, yield *must* be parenthesized.
-    var x = "abc" + (yield 10) + "def";
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (sent[0] === 1) throw sent[1]; return sent[1]; }, trys: [], stack: [] }, sent, star, f;
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (1) {
+            if (_.done) switch (op[0]) {
+                case 0: return { value: void 0, done: true };
+                case 1: case 6: throw op[1];
+                case 2: return { value: op[1], done: true };
+            }
+            try {
+                f = 1;
+                if (star) {
+                    var v = star[["next", "throw", "return"][op[0]]];
+                    if (v && !(v = v.call(star, op[1])).done) return v;
+                    if (v) op = [0, v.value];
+                    star = void 0; continue;
+                }
+                switch (op[0]) {
+                    case 0: case 1: sent = op; break;
+                    case 4: return _.label++, { value: op[1], done: false };
+                    case 5: _.label++, star = op[1], op = [0]; continue;
+                    case 7: op = _.stack.pop(), _.trys.pop(); continue;
+                    default:
+                        var r = _.trys.length > 0 && _.trys[_.trys.length - 1];
+                        if (!r && (op[0] === 6 || op[0] === 2)) { _.done = 1; continue; }
+                        if (op[0] === 3 && (!r || (op[1] > r[0] && op[1] < r[3]))) { _.label = op[1]; break; }
+                        if (op[0] === 6 && _.label < r[1]) { _.label = r[1], sent = op; break; }
+                        if (r && _.label < r[2]) { _.label = r[2], _.stack.push(op); break; }
+                        if (r[2]) { _.stack.pop(); }
+                        _.trys.pop();
+                        continue;
+                }
+                op = body.call(thisArg, _);
+            }
+            catch (e) { op = [6, e], star = void 0; }
+            finally { f = 0, sent = v = void 0; }
+        }
+    }
+    return {
+        next: function (v) { return step([0, v]); },
+        "throw": function (v) { return step([1, v]); },
+        "return": function (v) { return step([2, v]); }
+    };
+};
+function gen() {
+    var x, _a;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = "abc";
+                return [4 /*yield*/, 10];
+            case 1:
+                x = _a + (_b.sent()) + "def";
+                return [2 /*return*/];
+        }
+    });
 }
