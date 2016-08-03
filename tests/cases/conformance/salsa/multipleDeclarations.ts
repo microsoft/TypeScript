72057500b5220b28e637a6a31acb86f4f4273393
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-08-03 16:10:14 -0700

    Test that declares conflicting method first

--- tests/cases/conformance/salsa/multipleDeclarations.ts
+++ tests/cases/conformance/salsa/multipleDeclarations.ts
@@ -1,14 +1,12 @@
 // @filename: input.js
 // @out: output.js
 // @allowJs: true
-
 function C() {
     this.m = null;
 }
 C.prototype.m = function() {
     this.nothing();
 }
-
 class X {
     constructor() {
         this.m = this.m.bind(this);
@@ -23,3 +21,17 @@ let x = new X();
 X.prototype.mistake = false;
 x.m();
 x.mistake;
+class Y {
+    mistake() {
+    }
+    m() {
+    }
+    constructor() {
+        this.m = this.m.bind(this);
+        this.mistake = 'even more nonsense';
+    }
+}
+Y.prototype.mistake = true;
+let y = new Y();
+y.m();
+y.mistake();
