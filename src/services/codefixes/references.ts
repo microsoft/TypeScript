e0b73c4a4a65f3c98a1b99ce5a981a1f89d44283
Paul van Brenk <paul.van.brenk@microsoft.com>
2016-08-05 16:44:43 -0700

    Clean up

--- src/services/codefixes/references.ts
+++ src/services/codefixes/references.ts
@@ -1,6 +1,3 @@
 ///<reference path='..\services.ts' />
 ///<reference path='codeFixProvider.ts' />
 ///<reference path='superFixes.ts' />
-///<reference path='unusedIdentifierFixes.ts' />
-///<reference path='changeExtendsToImplementsFix.ts' />
-///<reference path='interfaceFixes.ts' />
