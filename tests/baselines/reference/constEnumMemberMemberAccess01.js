26ae45501025d5da4fedd67107292bd11fdf3895
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2016-08-09 14:40:48 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/constEnumMemberMemberAccess01.js
@@ -0,0 +1,9 @@
+//// [constEnumMemberMemberAccess01.ts]
+const enum Foo {
+    A
+}
+
+Foo.A["toString"]();
+
+//// [constEnumMemberMemberAccess01.js]
+0 /* A */["toString"]();
---
+++ tests/baselines/reference/constEnumMemberPropertyAccess01.js
@@ -0,0 +1,9 @@
+//// [constEnumMemberPropertyAccess01.ts]
+const enum Foo {
+    A
+}
+
+Foo.A.toString();
+
+//// [constEnumMemberPropertyAccess01.js]
+0 /* A */..toString();
