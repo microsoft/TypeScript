0168ab2051bc9a364f8f148296cf71848fc4bba9
Wesley Wigham <wwigham@gmail.com>
2016-08-19 13:34:06 -0700

    Check return code paths on getters (#10102)
    * Check return paths on getters
    
    * Remove TODO comment

---
+++ tests/baselines/reference/getterControlFlowStrictNull.js
@@ -0,0 +1,47 @@
+//// [getterControlFlowStrictNull.ts]
+class A {
+   a(): string | null {
+        if (Math.random() > 0.5) {
+            return '';
+        }
+
+        // it does error here as expected
+    }
+}
+class B {
+    get a(): string | null {
+        if (Math.random() > 0.5) {
+            return '';
+        }
+
+        // it should error here because it returns undefined
+    }
+}
+
+//// [getterControlFlowStrictNull.js]
+var A = (function () {
+    function A() {
+    }
+    A.prototype.a = function () {
+        if (Math.random() > 0.5) {
+            return '';
+        }
+        // it does error here as expected
+    };
+    return A;
+}());
+var B = (function () {
+    function B() {
+    }
+    Object.defineProperty(B.prototype, "a", {
+        get: function () {
+            if (Math.random() > 0.5) {
+                return '';
+            }
+            // it should error here because it returns undefined
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return B;
+}());
