03182bfec58d29973df46b0c95074995b14c768d
Justin Bay <justin.bay@outlook.com>
2016-08-24 21:08:50 -0400

    Update error message + refactor

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -872,7 +872,7 @@ namespace ts {
                     if (!errorLocation ||
                         !checkAndReportErrorForMissingPrefix(errorLocation, name, nameArg) &&
                         !checkAndReportErrorForExtendingInterface(errorLocation) &&
-                        !checkAndReportErrorForUsingTypeAsValue(errorLocation, name, meaning, nameNotFoundMessage, nameArg)) {
+                        !checkAndReportErrorForUsingTypeAsValue(errorLocation, name, meaning)) {
                         error(errorLocation, nameNotFoundMessage, typeof nameArg === "string" ? nameArg : declarationNameToString(nameArg));
                     }
                 }
@@ -982,28 +982,15 @@ namespace ts {
             }
         }
 
-        function checkAndReportErrorForUsingTypeAsValue(errorLocation: Node, name: string, meaning: SymbolFlags, nameNotFoundMessage: DiagnosticMessage, nameArg: string | Identifier): boolean {
-            const strictlyValueMeanings = SymbolFlags.Value & ~SymbolFlags.Type;
-            const strictlyTypeMeanings = SymbolFlags.Type & ~SymbolFlags.Value;
-
-            if (!(meaning & strictlyValueMeanings) || meaning & SymbolFlags.NamespaceModule) {
-                return false;
-            }
-
-            const nameAsType = resolveName(errorLocation, name, strictlyTypeMeanings, nameNotFoundMessage, nameArg);
-            if (!nameAsType) {
-                return false;
-            }
-
-            if (nameAsType.flags & SymbolFlags.Alias) {
-                const resolvedSymbol = resolveAlias(nameAsType);
-                if (resolvedSymbol.flags & SymbolFlags.NamespaceModule) {
-                    return false;
+        function checkAndReportErrorForUsingTypeAsValue(errorLocation: Node, name: string, meaning: SymbolFlags): boolean {
+            if (meaning & (SymbolFlags.Value & ~SymbolFlags.NamespaceModule)) {
+                const symbol = resolveSymbol(resolveName(errorLocation, name, SymbolFlags.Type & ~SymbolFlags.Value, /*nameNotFoundMessage*/undefined, /*nameArg*/ undefined));
+                if (symbol && !(symbol.flags & SymbolFlags.NamespaceModule)) {
+                    error(errorLocation, Diagnostics._0_only_refers_to_a_type_but_is_being_used_as_a_value_here, name);
+                    return true;
                 }
             }
-
-            error(errorLocation, Diagnostics.Cannot_find_name_0_A_type_exists_with_this_name_but_no_value, name);
-            return true;
+            return false;
         }
 
         function checkResolvedBlockScopedVariable(result: Symbol, errorLocation: Node): void {
