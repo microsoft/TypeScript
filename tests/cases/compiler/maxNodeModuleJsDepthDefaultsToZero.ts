751b0a65dec5ddecf8a4d53942776e32b88115bc
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-08-25 10:25:17 -0700

    Clean up maxNodeModuleJsDepth test

--- tests/cases/compiler/maxNodeModuleJsDepthDefaultsToZero.ts
+++ tests/cases/compiler/maxNodeModuleJsDepthDefaultsToZero.ts
@@ -4,7 +4,7 @@
 // @traceResolution: true
 // @noEmit: true
 
-// @filename: c:/root/tsconfig.json
+// @filename: tsconfig.json
 {
     "compileOnSave": true,
     "compilerOptions": {
@@ -14,23 +14,23 @@
     },
     "exclude": [ "node_modules" ]
 }
-// @filename: c:/root/node_modules/shortid/node_modules/z/index.js
+// @filename: node_modules/shortid/node_modules/z/index.js
 // z will not be found because maxNodeModulesJsDepth: 0
 module.exports = { z: 'no' };
 
-// @filename: c:/root/node_modules/shortid/index.js
+// @filename: node_modules/shortid/index.js
 var z = require('z');
 var y = { y: 'foo' };
 module.exports = y;
 
-// @filename: c:/root/typings/index.d.ts
+// @filename: typings/index.d.ts
 declare module "shortid" {
     export var x: number;
 }
 
-// @filename: c:/root/index.ts
-/// <reference path="c:/root/typings/index.d.ts" />
+// @filename: index.ts
+/// <reference path="typings/index.d.ts" />
 import * as foo from "shortid";
 foo.x // found in index.d.ts
-foo.y // ignored from shortid/index.ts
+foo.y // ignored from shortid/index.js
 
