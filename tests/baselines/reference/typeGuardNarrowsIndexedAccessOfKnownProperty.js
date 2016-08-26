d05e354a7092cc1d8c8d14f68d3cebe501d826eb
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-08-26 10:07:50 -0700

    Add baselines, which I missed

---
+++ tests/baselines/reference/typeGuardNarrowsIndexedAccessOfKnownProperty.js
@@ -0,0 +1,42 @@
+//// [typeGuardNarrowsIndexedAccessOfKnownProperty.ts]
+interface Square {
+    kind: "square";
+    size: number;
+}
+
+interface Rectangle {
+    kind: "rectangle";
+    width: number;
+    height: number;
+}
+
+interface Circle {
+    kind: "circle";
+    radius: number;
+}
+
+type Shape = Square | Rectangle | Circle;
+
+function area(s: Shape) {
+    // In the following switch statement, the type of s is narrowed in each case clause
+    // according to the value of the discriminant property, thus allowing the other properties
+    // of that variant to be accessed without a type assertion.
+    switch (s['kind']) {
+        case "square": return s.size * s.size;
+        case "rectangle": return s.width * s.height;
+        case "circle": return Math.PI * s.radius * s.radius;
+    }
+}
+
+
+//// [typeGuardNarrowsIndexedAccessOfKnownProperty.js]
+function area(s) {
+    // In the following switch statement, the type of s is narrowed in each case clause
+    // according to the value of the discriminant property, thus allowing the other properties
+    // of that variant to be accessed without a type assertion.
+    switch (s['kind']) {
+        case "square": return s.size * s.size;
+        case "rectangle": return s.width * s.height;
+        case "circle": return Math.PI * s.radius * s.radius;
+    }
+}
