4eaee735644b2a013b542ada4b50351277f38937
Omer Sheikh <ojsheikh@gmail.com>
2016-08-29 01:07:10 +0500

    Add test for invalid property access in unions

---
+++ tests/baselines/reference/unionPropertyExistance.js
@@ -0,0 +1,44 @@
+//// [unionPropertyExistance.ts]
+interface A {
+    inAll: string;
+    notInB: string;
+    notInC: string;
+}
+
+interface B {
+    inAll: boolean;
+    onlyInB: number;
+    notInC: string;
+}
+
+interface C {
+    inAll: number;
+    notInB: string;
+}
+
+type AB = A | B;
+type ABC = C | AB;
+
+var ab: AB;
+var abc: ABC;
+
+ab.onlyInB;
+
+ab.notInC; // Ok
+abc.notInC;
+ab.notInB;
+abc.notInB;
+
+abc.inAll; // Ok
+abc.inNone;
+
+//// [unionPropertyExistance.js]
+var ab;
+var abc;
+ab.onlyInB;
+ab.notInC; // Ok
+abc.notInC;
+ab.notInB;
+abc.notInB;
+abc.inAll; // Ok
+abc.inNone;
---
+++ tests/cases/compiler/unionPropertyExistance.ts
@@ -0,0 +1,32 @@
+interface A {
+    inAll: string;
+    notInB: string;
+    notInC: string;
+}
+
+interface B {
+    inAll: boolean;
+    onlyInB: number;
+    notInC: string;
+}
+
+interface C {
+    inAll: number;
+    notInB: string;
+}
+
+type AB = A | B;
+type ABC = C | AB;
+
+var ab: AB;
+var abc: ABC;
+
+ab.onlyInB;
+
+ab.notInC; // Ok
+abc.notInC;
+ab.notInB;
+abc.notInB;
+
+abc.inAll; // Ok
+abc.inNone;
