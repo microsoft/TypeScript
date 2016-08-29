5e4465acd6f12958f7a8ad573cdc73c0ba0fe4cc
Andy Hanson <anhans@microsoft.com>
2016-08-29 07:28:14 -0700

    Treat variable declaration as top-level if it has an important child.

---
+++ tests/cases/fourslash/navigationBarFunctionIndirectlyInVariableDeclaration.ts
@@ -0,0 +1,46 @@
+/// <reference path="fourslash.ts"/>
+
+////var a = {
+////    propA: function() {}
+////};
+////var b;
+////b = {
+////    propB: function() {}
+////};
+
+verify.navigationBar([
+  {
+    "text": "<global>",
+    "kind": "script",
+    "childItems": [
+      {
+        "text": "a",
+        "kind": "var"
+      },
+      {
+        "text": "b",
+        "kind": "var"
+      },
+      {
+        "text": "propB",
+        "kind": "function"
+      }
+    ]
+  },
+  {
+    "text": "a",
+    "kind": "var",
+    "childItems": [
+      {
+        "text": "propA",
+        "kind": "function"
+      }
+    ],
+    "indent": 1
+  },
+  {
+    "text": "propB",
+    "kind": "function",
+    "indent": 1
+  }
+]);
