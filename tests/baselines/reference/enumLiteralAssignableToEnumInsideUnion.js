083f3426d145837a11f23744f21bd30be861aca4
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-09-08 14:43:17 -0700

    Test that enum literal is assignable to an enum in a union

---
+++ tests/baselines/reference/enumLiteralAssignableToEnumInsideUnion.js
@@ -0,0 +1,32 @@
+//// [enumLiteralAssignableToEnumInsideUnion.ts]
+module X {
+    export enum Foo {
+        A, B
+    }
+}
+module Y {
+    export enum Foo {
+        A, B
+    }
+}
+const y: X.Foo | boolean = Y.Foo.A;
+
+
+//// [enumLiteralAssignableToEnumInsideUnion.js]
+var X;
+(function (X) {
+    (function (Foo) {
+        Foo[Foo["A"] = 0] = "A";
+        Foo[Foo["B"] = 1] = "B";
+    })(X.Foo || (X.Foo = {}));
+    var Foo = X.Foo;
+})(X || (X = {}));
+var Y;
+(function (Y) {
+    (function (Foo) {
+        Foo[Foo["A"] = 0] = "A";
+        Foo[Foo["B"] = 1] = "B";
+    })(Y.Foo || (Y.Foo = {}));
+    var Foo = Y.Foo;
+})(Y || (Y = {}));
+var y = Y.Foo.A;
