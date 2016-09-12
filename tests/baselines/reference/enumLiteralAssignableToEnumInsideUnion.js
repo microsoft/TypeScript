eb933f9567e97dc49d016e31059176def50fbfee
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-09-12 10:05:48 -0700

    More tests of enum assignability
    1. Numeric literal <-> enum literal assignability
    2. Computed enum <-> union enum assignability
    3. Also rebaseline error reporting of existing enum cases.

--- tests/baselines/reference/enumLiteralAssignableToEnumInsideUnion.js
+++ tests/baselines/reference/enumLiteralAssignableToEnumInsideUnion.js
@@ -9,7 +9,23 @@ module Y {
         A, B
     }
 }
-const y: X.Foo | boolean = Y.Foo.A;
+module Z {
+    export enum Foo {
+        A = 1 << 1,
+        B = 1 << 2,
+    }
+}
+module Ka {
+    export enum Foo {
+        A = 1 << 10,
+        B = 1 << 11,
+    }
+}
+const e1: X.Foo | boolean = Z.Foo.A; // ok
+const e2: X.Foo.A | X.Foo.B | boolean = Z.Foo.A; // ok, X.Foo is equivalent to X.Foo.A | X.Foo.B
+const e3: X.Foo.B | boolean = Z.Foo.A; // not legal
+const e4: X.Foo.A | boolean = Z.Foo.A; // not legal either because Z.Foo is computed and Z.Foo.A is not necessarily assignable to X.Foo.A
+const e5: Ka.Foo | boolean = Z.Foo.A; // ok
 
 
 //// [enumLiteralAssignableToEnumInsideUnion.js]
@@ -29,4 +45,24 @@ var Y;
     })(Y.Foo || (Y.Foo = {}));
     var Foo = Y.Foo;
 })(Y || (Y = {}));
-var y = Y.Foo.A;
+var Z;
+(function (Z) {
+    (function (Foo) {
+        Foo[Foo["A"] = 2] = "A";
+        Foo[Foo["B"] = 4] = "B";
+    })(Z.Foo || (Z.Foo = {}));
+    var Foo = Z.Foo;
+})(Z || (Z = {}));
+var Ka;
+(function (Ka) {
+    (function (Foo) {
+        Foo[Foo["A"] = 1024] = "A";
+        Foo[Foo["B"] = 2048] = "B";
+    })(Ka.Foo || (Ka.Foo = {}));
+    var Foo = Ka.Foo;
+})(Ka || (Ka = {}));
+var e1 = Z.Foo.A; // ok
+var e2 = Z.Foo.A; // ok, X.Foo is equivalent to X.Foo.A | X.Foo.B
+var e3 = Z.Foo.A; // not legal
+var e4 = Z.Foo.A; // not legal either because Z.Foo is computed and Z.Foo.A is not necessarily assignable to X.Foo.A
+var e5 = Z.Foo.A; // ok
--- tests/cases/compiler/enumLiteralAssignableToEnumInsideUnion.ts
+++ tests/cases/compiler/enumLiteralAssignableToEnumInsideUnion.ts
@@ -8,4 +8,20 @@ module Y {
         A, B
     }
 }
-const y: X.Foo | boolean = Y.Foo.A;
+module Z {
+    export enum Foo {
+        A = 1 << 1,
+        B = 1 << 2,
+    }
+}
+module Ka {
+    export enum Foo {
+        A = 1 << 10,
+        B = 1 << 11,
+    }
+}
+const e1: X.Foo | boolean = Z.Foo.A; // ok
+const e2: X.Foo.A | X.Foo.B | boolean = Z.Foo.A; // ok, X.Foo is equivalent to X.Foo.A | X.Foo.B
+const e3: X.Foo.B | boolean = Z.Foo.A; // not legal
+const e4: X.Foo.A | boolean = Z.Foo.A; // not legal either because Z.Foo is computed and Z.Foo.A is not necessarily assignable to X.Foo.A
+const e5: Ka.Foo | boolean = Z.Foo.A; // ok
