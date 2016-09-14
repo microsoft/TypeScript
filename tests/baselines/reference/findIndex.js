6aa2427cded86b7dad8a58afab6806a75cf8fc85
Rostislav Galimsky <rostgal@gmail.com>
2016-09-14 20:05:57 +0300

    add test for issue 10843

---
+++ tests/baselines/reference/findIndex.js
@@ -0,0 +1,10 @@
+//// [findIndex.ts]
+
+[].findIndex(function(elem, i, arr) {
+    return true;
+});
+
+//// [findIndex.js]
+[].findIndex(function (elem, i, arr) {
+    return true;
+});
---
+++ tests/cases/conformance/es6/arrayMethods/findIndex.ts
@@ -0,0 +1,5 @@
+//@target: ES6
+
+[].findIndex(function(elem, i, arr) {
+    return true;
+});
