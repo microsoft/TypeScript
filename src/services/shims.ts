3f2e3919b4a5193d68bc0027700dd0e2a28eeddb
Vladimir Matveev <vladima@microsoft.com>
2016-09-27 23:36:19 -0700

    revert versions

--- src/services/shims.ts
+++ src/services/shims.ts
@@ -1256,6 +1256,6 @@ namespace TypeScript.Services {
 // TODO: it should be moved into a namespace though.
 
 /* @internal */
-const toolsVersion = "2.0";
+const toolsVersion = "2.1";
 
 /* tslint:enable:no-unused-variable */
