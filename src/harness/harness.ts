860dd237083836c2a870c83c0c6d09deb16bdb82
Andy Hanson <anhans@microsoft.com>
2016-10-04 09:04:23 -0700

    Lint whitespace around "="

--- src/harness/harness.ts
+++ src/harness/harness.ts
@@ -925,7 +925,7 @@ namespace Harness {
         export const defaultLibFileName = "lib.d.ts";
         export const es2015DefaultLibFileName = "lib.es2015.d.ts";
 
-        const libFileNameSourceFileMap=  ts.createMap<ts.SourceFile>({
+        const libFileNameSourceFileMap = ts.createMap<ts.SourceFile>({
             [defaultLibFileName]: createSourceFileAndAssertInvariants(defaultLibFileName, IO.readFile(libFolder + "lib.es5.d.ts"), /*languageVersion*/ ts.ScriptTarget.Latest)
         });
 
