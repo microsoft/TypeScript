d48a3b19e54ee540f90a37f14f59b332acd796fd
Mohamed Hegazy <mhegazy@microsoft.com>
2016-10-05 21:15:58 -0700

    Update Version

--- src/compiler/program.ts
+++ src/compiler/program.ts
@@ -5,7 +5,7 @@
 namespace ts {
     /** The version of the TypeScript compiler release */
 
-    export const version = "2.0.5";
+    export const version = "2.0.6";
 
     const emptyArray: any[] = [];
 
