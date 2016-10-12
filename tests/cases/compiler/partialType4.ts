5a0ba57342ea1e785cf69aa91a93fa378c2c53ab
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2016-10-12 16:21:56 -0700

    Update test for --d

--- tests/cases/compiler/partialType4.ts
+++ tests/cases/compiler/partialType4.ts
@@ -1,8 +1,12 @@
 // @strictNullChecks: true
+<<<<<<< 4c6bf657250405beb9bb7edaf3bd10d3db165690
 <<<<<<< d147616ccc57b7c9f6418074c8edffd3ee258961
 // @declaration: true
 =======
 >>>>>>> Partial Types (#11233)
+=======
+// @declaration: true
+>>>>>>> Update test for --d
 
 interface State1 {
     name: string;
