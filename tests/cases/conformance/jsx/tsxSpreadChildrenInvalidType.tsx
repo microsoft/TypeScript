d29c78e7189fcbcc13bd54f88d52948ddf65ff9f
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-10-13 09:28:39 -0700

    Add spread syntax tests for JsxExpressions
    And baselines

---
+++ tests/cases/conformance/jsx/tsxSpreadChildrenInvalidType.tsx
@@ -0,0 +1,26 @@
+// @jsx: react
+declare module JSX {
+	interface Element { }
+	interface IntrinsicElements {
+		[s: string]: any;
+	}
+}
+declare var React: any;
+
+interface TodoProp {
+    id: number;
+    todo: string;
+}
+interface TodoListProps {
+    todos: TodoProp[];
+}
+function Todo(prop: { key: number, todo: string }) {
+    return <div>{prop.key.toString() + prop.todo}</div>;
+}
+function TodoList({ todos }: TodoListProps) {
+    return <div>
+        {...<Todo key={todos[0].id} todo={todos[0].todo} />}
+    </div>;
+}
+let x: TodoListProps;
+    <TodoList {...x}/>
