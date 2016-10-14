73dd1d248feab50b42e24215379b1232a7ac8a9b
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-10-14 15:49:22 -0700

    Test object rest w/destructuring properties

--- tests/baselines/reference/objectRest.js
+++ tests/baselines/reference/objectRest.js
@@ -5,6 +5,7 @@ var { a, ...justB } = o;
 var { a, b: renamed, ...empty } = o;
 var { ['b']: renamed, ...justA } = o;
 var { 'b': renamed, ...justA } = o;
+var { b: { '0': n, '1': oooo }, ...justA } = o;
 
 let o2 = { c: 'terrible idea?', d: 'yes' };
 var { d: renamed, ...d } = o2;
@@ -26,6 +27,7 @@ var a = o.a, justB = __rest(o, ["a"]);
 var a = o.a, renamed = o.b, empty = __rest(o, ["a", "b"]);
 var _a = 'b', renamed = o[_a], justA = __rest(o, ["b"]);
 var renamed = o["b"], justA = __rest(o, ["b"]);
+var _b = o.b, n = _b["0"], oooo = _b["1"], justA = __rest(o, ["b"]);
 var o2 = { c: 'terrible idea?', d: 'yes' };
 var renamed = o2.d, d = __rest(o2, ["d"]);
 function cloneAgain(_a) {
--- tests/cases/conformance/es6/destructuring/objectRest.ts
+++ tests/cases/conformance/es6/destructuring/objectRest.ts
@@ -4,6 +4,7 @@ var { a, ...justB } = o;
 var { a, b: renamed, ...empty } = o;
 var { ['b']: renamed, ...justA } = o;
 var { 'b': renamed, ...justA } = o;
+var { b: { '0': n, '1': oooo }, ...justA } = o;
 
 let o2 = { c: 'terrible idea?', d: 'yes' };
 var { d: renamed, ...d } = o2;
