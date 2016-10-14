4d147e268859672d0243d73f0ac5e462f792422d
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-10-14 13:05:05 -0700

    Re-add rest tests

---
+++ tests/cases/conformance/es6/destructuring/objectRest.ts
@@ -0,0 +1,10 @@
+let o = { a: 1, b: 'no' }
+var { ...clone } = o;
+var { a, ...justB } = o;
+var { a, b: renamed, ...empty } = o;
+
+let o2 = { c: 'terrible idea?', d: 'yes' };
+var { d: renamed, ...d } = o2;
+
+function cloneAgain({ ...clone }: { a: number, b: string }): void {
+}
