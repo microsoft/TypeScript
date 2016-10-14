8437b223f5354e2321cbd28497c6d89ba1d0588c
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-10-14 13:05:18 -0700

    Update baselines

---
+++ tests/baselines/reference/objectRest.js
@@ -0,0 +1,29 @@
+//// [objectRest.ts]
+let o = { a: 1, b: 'no' }
+var { ...clone } = o;
+var { a, ...justB } = o;
+var { a, b: renamed, ...empty } = o;
+
+let o2 = { c: 'terrible idea?', d: 'yes' };
+var { d: renamed, ...d } = o2;
+
+function cloneAgain({ ...clone }: { a: number, b: string }): void {
+}
+
+
+//// [objectRest.js]
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && !e.indexOf(p))
+        t[p] = s[p];
+    return t;
+};
+var o = { a: 1, b: 'no' };
+var clone = __rest(o, []);
+var a = o.a, justB = __rest(o, ["a"]);
+var a = o.a, renamed = o.b, empty = __rest(o, ["a", "b"]);
+var o2 = { c: 'terrible idea?', d: 'yes' };
+var renamed = o2.d, d = __rest(o2, ["d"]);
+function cloneAgain(_a) {
+    var clone = __rest(_a, []);
+}
---
+++ tests/baselines/reference/objectRestNegative.js
@@ -0,0 +1,19 @@
+//// [objectRestNegative.ts]
+let o = { a: 1, b: 'no' };
+var { ...mustBeLast, a } = o;
+function stillMustBeLast({ ...mustBeLast, a }: { a: number, b: string }): void {
+}
+
+
+//// [objectRestNegative.js]
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && !e.indexOf(p))
+        t[p] = s[p];
+    return t;
+};
+var o = { a: 1, b: 'no' };
+var mustBeLast = __rest(o, ["a"]), a = o.a;
+function stillMustBeLast(_a) {
+    var mustBeLast = __rest(_a, ["a"]), a = _a.a;
+}
