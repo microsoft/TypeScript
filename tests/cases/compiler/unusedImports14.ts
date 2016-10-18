17cf4357adbb363ba796a810150fef28d8bac10e
Sheetal Nandi <shkamat@microsoft.com>
2016-10-18 12:24:41 -0700

    Add testcase when error is reported about unused react

---
+++ tests/cases/compiler/unusedImports14.ts
@@ -0,0 +1,23 @@
+//@noUnusedLocals:true
+//@noUnusedParameters:true
+//@module: commonjs
+//@jsx: react
+
+// @filename: foo.tsx
+import React = require("react");
+
+export const FooComponent = <div></div>
+
+// @filename: node_modules/@types/react/index.d.ts
+export = React;
+export as namespace React;
+
+declare namespace React {
+    function createClass<P, S>(spec);
+}
+declare global {
+    namespace JSX {
+    }
+}
+
+
