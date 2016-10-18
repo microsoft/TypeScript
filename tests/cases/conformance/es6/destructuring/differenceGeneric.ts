96d836e3acb776905eaabfd48543f9b974bea9c2
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-10-18 15:58:56 -0700

    Test instantiation and apparent type of difference
    Apparent type doesn't work yet, though, so no baselines yet.

--- tests/cases/conformance/es6/destructuring/differenceGeneric.ts
+++ tests/cases/conformance/es6/destructuring/differenceGeneric.ts
@@ -1,14 +1,27 @@
 interface Gen {
-    x: any;
+    x: number;
 }
 interface Gen2 {
     parent: Gen;
-    millenial: any;
+    millenial: string;
 }
-function cloneAgain<T extends Gen & Gen2>(t: T): T {
+function cloneAgain<T extends Gen & Gen2>(t: T): T - Gen {
     let y: Gen;
     let rest: T - Gen;
     let rest1: T - Gen - Gen2;
     var { x, ...rest2 } = t;
-    return t;
+    rest.parent;
+    rest.millenial;
+    rest2.parent;
+    rest2.millenial;
+    return rest2;
 }
+interface Gen3 extends Gen2 {
+    x: number;
+    w: boolean;
+}
+let gen3: Gen3;
+let rested = cloneAgain(gen3);
+rested.parent;
+rested.millenial;
+rested.w;
