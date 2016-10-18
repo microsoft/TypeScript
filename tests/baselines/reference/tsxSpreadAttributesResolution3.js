a7c405ea9d27d97be57f95af826445e75b39a191
Yui T <yuisu@microsoft.com>
2016-10-18 13:05:22 -0700

    Update baselines: using spread-type in React.Component

---
+++ tests/baselines/reference/tsxSpreadAttributesResolution3.js
@@ -0,0 +1,49 @@
+//// [file.tsx]
+
+import React = require('react');
+
+interface PoisonedProp {
+    x: string;
+    y: number;
+}
+
+class Poisoned extends React.Component<PoisonedProp, {}> {
+    render() {
+        return <div>Hello</div>;
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+}
+
+const obj = {
+    x: "hello world",
+    y: 2
+};
+
+// OK
+let p = <Poisoned {...obj} />;
+let y = <Poisoned x="hi" y={2} />;
+
+//// [file.jsx]
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var React = require("react");
+var Poisoned = (function (_super) {
+    __extends(Poisoned, _super);
+    function Poisoned() {
+        return _super.apply(this, arguments) || this;
+    }
+    Poisoned.prototype.render = function () {
+        return <div>Hello</div>;
+    };
+    return Poisoned;
+}(React.Component));
+var obj = {
+    x: "hello world",
+    y: 2
+};
+// OK
+var p = <Poisoned {...obj}/>;
+var y = <Poisoned x="hi" y={2}/>;
