2cb95fcd8246f43d5e322981ef330cebcfcf05c5
Yui T <yuisu@microsoft.com>
2016-10-18 13:05:10 -0700

    Add tests: using spread-type in React.Component

---
+++ tests/cases/conformance/jsx/tsxSpreadAttributesResolution6.tsx
@@ -0,0 +1,22 @@
+// @filename: file.tsx
+// @jsx: preserve
+// @noLib: true
+// @libFiles: react.d.ts,lib.d.ts
+
+import React = require('react');
+
+type TextProps = { editable: false }
+               | { editable: true, onEdit: (newText: string) => void };
+
+class TextComponent extends React.Component<TextProps, {}> {
+    render() {
+        return <span>Some Text..</span>;
+    }
+}
+
+// Error
+let x = <TextComponent editable={true} />
+
+const textProps: TextProps = {
+    editable: false
+};
---
+++ tests/cases/conformance/jsx/tsxSpreadAttributesResolution7.tsx
@@ -0,0 +1,29 @@
+// @filename: file.tsx
+// @jsx: preserve
+// @noLib: true
+// @libFiles: react.d.ts,lib.d.ts
+
+import React = require('react');
+
+type TextProps = { editable: false }
+               | { editable: true, onEdit: (newText: string) => void };
+
+class TextComponent extends React.Component<TextProps, {}> {
+    render() {
+        return <span>Some Text..</span>;
+    }
+}
+
+// OK
+const textPropsFalse: TextProps = {
+    editable: false
+};
+
+let y1 = <TextComponent {...textPropsFalse} />
+
+const textPropsTrue: TextProps = {
+    editable: true,
+    onEdit: () => {}
+};
+
+let y2 = <TextComponent {...textPropsTrue} />
