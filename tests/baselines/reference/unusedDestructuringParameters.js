1e32b6742ecb2dd172de6fc0950991a05649509d
Sheetal Nandi <shkamat@microsoft.com>
2016-10-24 10:58:51 -0700

    Add test case for destructured unused parameter

---
+++ tests/baselines/reference/unusedDestructuringParameters.js
@@ -0,0 +1,15 @@
+//// [unusedDestructuringParameters.ts]
+const f = ([a]) => { };
+f([1]);
+const f2 = ({a}) => { };
+f2({ a: 10 });
+
+//// [unusedDestructuringParameters.js]
+var f = function (_a) {
+    var a = _a[0];
+};
+f([1]);
+var f2 = function (_a) {
+    var a = _a.a;
+};
+f2({ a: 10 });
---
+++ tests/cases/compiler/unusedDestructuringParameters.ts
@@ -0,0 +1,5 @@
+//@noUnusedParameters: true
+const f = ([a]) => { };
+f([1]);
+const f2 = ({a}) => { };
+f2({ a: 10 });
