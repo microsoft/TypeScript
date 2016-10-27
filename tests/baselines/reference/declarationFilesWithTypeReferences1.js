1b57cba3e24f6dff3905de4c593affa42321fd97
Vladimir Matveev <vladima@microsoft.com>
2016-10-26 17:03:28 -0700

    pass proper value for current directory when compiling .d.ts files

--- tests/baselines/reference/declarationFilesWithTypeReferences1.js
+++ tests/baselines/reference/declarationFilesWithTypeReferences1.js
@@ -20,21 +20,3 @@ function foo() {
 
 //// [app.d.ts]
 declare function foo(): Error;
-
-
-//// [DtsFileErrors]
-
-
-error TS2688: Cannot find type definition file for 'node'.
-
-
-!!! error TS2688: Cannot find type definition file for 'node'.
-==== /node_modules/@types/node/index.d.ts (0 errors) ====
-    
-    interface Error {
-        stack2: string;
-    }
-    
-==== /app.d.ts (0 errors) ====
-    declare function foo(): Error;
-    
--- tests/baselines/reference/declarationFilesWithTypeReferences2.js
+++ tests/baselines/reference/declarationFilesWithTypeReferences2.js
@@ -21,22 +21,3 @@ function foo() {
 //// [app.d.ts]
 /// <reference types="node" />
 declare function foo(): Error2;
-
-
-//// [DtsFileErrors]
-
-
-error TS2688: Cannot find type definition file for 'node'.
-
-
-!!! error TS2688: Cannot find type definition file for 'node'.
-==== /node_modules/@types/node/index.d.ts (0 errors) ====
-    
-    interface Error2 {
-        stack2: string;
-    }
-    
-==== /app.d.ts (0 errors) ====
-    /// <reference types="node" />
-    declare function foo(): Error2;
-    
