6d760cfe255639e3f6d740f2602f9187bf8505ab
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-10-28 14:59:04 -0700

    Add and split object rest destructuring tests

--- tests/cases/conformance/types/difference/objectRest.ts
+++ tests/cases/conformance/types/difference/objectRest.ts
@@ -10,18 +10,6 @@ var { b: { '0': n, '1': oooo }, ...justA } = o;
 
 let o2 = { c: 'terrible idea?', d: 'yes' };
 var { d: renamed, ...d } = o2;
-function cloneAgain({ a, ...clone }: { a: number, b: string }): void {
-}
-let array: { x: number, y: string }[];
-for (let { x, ...restOf } of array) {
-    [x, restOf];
-}
-let xx: number;
-let rrestOff: { y: string };
-for ({ x: xx, ...rrestOff } of array ) {
-    [xx, rrestOff];
-}
-for (const norest of array.map(a => ({ ...a }))) {
-    [norest.x, norest.y];
-    // x is now a string. who knows why.
-}
+
+let complex: { x: { z, ka }, y: string };
+let { x: { z, ...nested }, ...rest } = complex;
---
+++ tests/cases/conformance/types/difference/objectRestParameter.ts
@@ -0,0 +1,7 @@
+function cloneAgain({ a, ...clone }: { a: number, b: string }): void {
+}
+
+declare function suddenly(f: (a: { x: { z, ka }, y: string }) => void);
+suddenly(({ x: a, ...rest }) => rest.y);
+suddenly(({ x: { z, ...nested }, ...rest }) => rest.y + nested.ka);
+
