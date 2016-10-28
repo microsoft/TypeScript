7758e6dc9d8b1d7b284395ce09fda87a0b31a185
Arthur Ozga <arozga@microsoft.com>
2016-10-27 18:39:49 -0700

    Rename Most Tests

---
+++ tests/cases/fourslash/codeFixClassExtendsAbstractPrivateNumber.ts
@@ -0,0 +1,16 @@
+/// <reference path='fourslash.ts' />
+
+//// abstract class A {
+////    private abstract x: number;
+//// }
+////
+//// class C extends A {[|
+//// |]}
+
+// We don't know how to fix this problem. We can:
+// 1) Make x protected, and then insert.
+// 2) Make x private, and then insert.
+// 3) Make x not abstract.
+// So we offer no fixes for now.
+// TODO: (arozga) change this behavior.
+verify.not.codeFixAvailable();
--- tests/cases/fourslash/codeFixObjectInterfaceMissingArrayNumber.ts
+++ tests/cases/fourslash/codeFixObjectInterfaceMissingArrayNumber.ts
@@ -1,17 +1,12 @@
 /// <reference path='fourslash.ts' />
 
 //// interface I1 {
-////     x:T;
-////     f1();
+////     x:Array<Number>;
 //// }
-////
-//// class T {}
-////
 ////
 //// var x: I1 ={[|
-////     |]f1(){}
-//// }
+//// |]}
 
 verify.not.codeFixAvailable();
-// verify.codeFixAtPosition(`x : null,
+// verify.codeFixAtPosition(`x : null 
 // `);
