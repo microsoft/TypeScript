36b60dc21702de2c12bf35995504e6e7b3336974
Zhengbo Li <Zhengbo Li>
2016-10-31 16:37:09 -0700

    fix comment

--- src/compiler/program.ts
+++ src/compiler/program.ts
@@ -1539,7 +1539,7 @@ namespace ts {
                     if (filesByName.contains(emitFilePath)) {
                         let chain: DiagnosticMessageChain;
                         if (!options.configFilePath) {
-                            // The program is from either inferred project or 
+                            // The program is from either an inferred project or an external project
                             chain = chainDiagnosticMessages(/*details*/ undefined, Diagnostics.Adding_a_tsconfig_json_file_will_help_organizing_projects_with_both_TypeScript_and_JavaScript_files_Learn_more_at_https_Colon_Slash_Slashaka_ms_Slashtsconfig);
                         }
                         chain = chainDiagnosticMessages(chain, Diagnostics.Cannot_write_file_0_because_it_would_overwrite_input_file, emitFileName);
