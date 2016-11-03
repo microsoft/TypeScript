14f8b9990f7307484ab317c6b9016d50a26db1c7
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-03 13:33:16 -0700

    Update objectRestAssignment test
    Missed previously, just got the baselines

--- tests/cases/conformance/types/rest/objectRestAssignment.ts
+++ tests/cases/conformance/types/rest/objectRestAssignment.ts
@@ -1,7 +1,6 @@
-let x;
-let ka;
-let nested;
-let other;
-let rest;
+let ka: any;
+let nested: { ki };
+let other: number;
+let rest: { };
 let complex: { x: { ka, ki }, y: number };
 ({x: { ka, ...nested }, y: other, ...rest} = complex);
