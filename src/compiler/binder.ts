1f270437211306beddbfbe510778bc430072f03d
Vladimir Matveev <vladima@microsoft.com>
2016-11-04 15:23:47 -0700

    add missing bind calls to properly set parent on token nodes

--- src/compiler/binder.ts
+++ src/compiler/binder.ts
@@ -1234,9 +1234,11 @@ namespace ts {
             const postExpressionLabel = createBranchLabel();
             bindCondition(node.condition, trueLabel, falseLabel);
             currentFlow = finishFlowLabel(trueLabel);
+            bind(node.questionToken);
             bind(node.whenTrue);
             addAntecedent(postExpressionLabel, currentFlow);
             currentFlow = finishFlowLabel(falseLabel);
+            bind(node.colonToken);
             bind(node.whenFalse);
             addAntecedent(postExpressionLabel, currentFlow);
             currentFlow = finishFlowLabel(postExpressionLabel);
