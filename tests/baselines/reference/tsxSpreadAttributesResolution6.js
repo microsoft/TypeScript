2c15eab5676627e187b82395da5a872267d9f051
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-11-08 10:39:43 -0800

    Update conformance tests baselines

---
+++ tests/baselines/reference/tsxSpreadAttributesResolution6.js
@@ -0,0 +1,43 @@
+//// [file.tsx]
+
+import React = require('react');
+
+type TextProps = { editable: false }
+               | { editable: true, onEdit: (newText: string) => void };
+
+class TextComponent extends React.Component<TextProps, {}> {
+    render() {
+        return <span>Some Text..</span>;
+    }
+}
+
+// Error
+let x = <TextComponent editable={true} />
+
+const textProps: TextProps = {
+    editable: false
+};
+
+//// [file.jsx]
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var React = require("react");
+var TextComponent = (function (_super) {
+    __extends(TextComponent, _super);
+    function TextComponent() {
+        return _super.apply(this, arguments) || this;
+    }
+    TextComponent.prototype.render = function () {
+        return <span>Some Text..</span>;
+    };
+    return TextComponent;
+}(React.Component));
+// Error
+var x = <TextComponent editable={true}/>;
+var textProps = {
+    editable: false
+};
