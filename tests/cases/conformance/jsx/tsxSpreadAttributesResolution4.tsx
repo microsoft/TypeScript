aea7d0957834adedbec661a2019169c65966be58
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-11-08 09:32:26 -0800

    Add tests for using spread attributes resolution in JSX stateful component

---
+++ tests/cases/conformance/jsx/tsxSpreadAttributesResolution4.tsx
@@ -0,0 +1,25 @@
+// @filename: file.tsx
+// @jsx: preserve
+// @noLib: true
+// @libFiles: react.d.ts,lib.d.ts
+
+import React = require('react');
+
+interface PoisonedProp {
+    x: string;
+    y: 2;
+}
+
+class Poisoned extends React.Component<PoisonedProp, {}> {
+    render() {
+        return <div>Hello</div>;
+    }
+}
+
+const obj: PoisonedProp = {
+    x: "hello world",
+    y: 2
+};
+
+// OK
+let p = <Poisoned {...obj} />;
