bb4227fdc42bf9856aa7695d60b7942bf1679549
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-11-08 10:39:43 -0800

    Update conformance tests baselines

--- tests/baselines/reference/tsxAttributeResolution3.js
+++ tests/baselines/reference/tsxAttributeResolution3.js
@@ -35,7 +35,7 @@ var obj5 = { x: 32, y: 32 };
 var obj6 = { x: 'ok', y: 32, extra: 100 };
 <test1 {...obj6} />
 
-// Error
+// OK (spread override)
 var obj7 = { x: 'foo' };
 <test1 x={32} {...obj7} />
 
@@ -59,6 +59,6 @@ var obj5 = { x: 32, y: 32 };
 // OK
 var obj6 = { x: 'ok', y: 32, extra: 100 };
 <test1 {...obj6}/>;
-// Error
+// OK (spread override)
 var obj7 = { x: 'foo' };
 <test1 x={32} {...obj7}/>;
