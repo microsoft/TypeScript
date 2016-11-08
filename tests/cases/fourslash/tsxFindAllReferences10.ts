3d19782b1c52ff62e094b8e8f7d69087647dd6fc
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-11-08 11:07:17 -0800

    Add language service tests

---
+++ tests/cases/fourslash/tsxFindAllReferences10.ts
@@ -0,0 +1,33 @@
+/// <reference path='fourslash.ts' />
+
+//@Filename: file.tsx
+// @jsx: preserve
+// @noLib: true
+
+//// declare module JSX {
+////     interface Element { }
+////     interface IntrinsicElements {
+////     }
+////     interface ElementAttributesProperty { props; }
+//// }
+//// interface ClickableProps {
+////     children?: string;
+////     className?: string;
+//// }
+//// interface ButtonProps extends ClickableProps {
+////     [|onClick|](event?: React.MouseEvent<HTMLButtonElement>): void;
+//// }
+//// interface LinkProps extends ClickableProps {
+////     goTo: string;
+//// }
+//// declare function MainButton(buttonProps: ButtonProps): JSX.Element;
+//// declare function MainButton(linkProps: LinkProps): JSX.Element;
+//// declare function MainButton(props: ButtonProps | LinkProps): JSX.Element;
+//// let opt = <MainButton />;
+//// let opt = <MainButton children="chidlren" />;
+//// let opt = <MainButton [|onClick|]={()=>{}} />;
+//// let opt = <MainButton [|onClick|]={()=>{}} ignore-prop />;
+//// let opt = <MainButton goTo="goTo" />;
+//// let opt = <MainButton wrong />;
+
+verify.rangesReferenceEachOther();
