01969475cf576b4f476beeb5aca648e743eb5cd2
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-09 10:51:56 -0800

    Improve nested destructuring test for ESNext emit
    Now with object destructuring inside array destructuring inside object
    destructuring! Each with their own array/object rest!
    
    Also updates baselines.

--- tests/baselines/reference/objectRestAssignment.js
+++ tests/baselines/reference/objectRestAssignment.js
@@ -6,6 +6,13 @@ let rest: { };
 let complex: { x: { ka, ki }, y: number };
 ({x: { ka, ...nested }, y: other, ...rest} = complex);
 
+// should be:
+let overEmit: { a: { ka: string, x: string }[], b: { z: string, ki: string, ku: string }, ke: string, ko: string };
+
+// var _g = overEmit.a, [_h, ...y] = _g, nested2 = __rest(_h, []), _j = overEmit.b, { z } = _j, c = __rest(_j, ["z"]), rest2 = __rest(overEmit, ["a", "b"]);
+var { a: [{ ...nested2 }, ...y], b: { z, ...c }, ...rest2 } = overEmit;
+({ a: [{ ...nested2 }, ...y], b: { z, ...c }, ...rest2 } = overEmit);
+
 
 //// [objectRestAssignment.js]
 var __rest = (this && this.__rest) || function (s, e) {
@@ -14,10 +21,15 @@ var __rest = (this && this.__rest) || function (s, e) {
         t[p] = s[p];
     return t;
 };
-var ka;
-var nested;
-var other;
-var rest;
-var complex;
-(_a = complex.x, (ka = _a.ka, _a), nested = __rest(_a, ["ka"]), (other = complex.y, complex), rest = __rest(complex, ["x", "y"]), complex);
-var _a;
+let ka;
+let nested;
+let other;
+let rest;
+let complex;
+(_a = complex.x, { ka } = _a, nested = __rest(_a, ["ka"]), { y: other } = complex, rest = __rest(complex, ["x", "y"]), complex);
+// should be:
+let overEmit;
+// var _g = overEmit.a, [_h, ...y] = _g, nested2 = __rest(_h, []), _j = overEmit.b, { z } = _j, c = __rest(_j, ["z"]), rest2 = __rest(overEmit, ["a", "b"]);
+var _b = overEmit.a, [_c, ...y] = _b, nested2 = __rest(_c, []), _d = overEmit.b, { z } = _d, c = __rest(_d, ["z"]), rest2 = __rest(overEmit, ["a", "b"]);
+(_e = overEmit.a, [_f, ...y] = _e, nested2 = __rest(_f, []), _g = overEmit.b, { z } = _g, c = __rest(_g, ["z"]), rest2 = __rest(overEmit, ["a", "b"]), overEmit);
+var _a, _e, _f, _g;
