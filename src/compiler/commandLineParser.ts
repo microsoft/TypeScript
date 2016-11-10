9a8e1bf8a62936bb6acf7d2d8f16de7f466a1be4
Cotton Hou <himcotton@gmail.com>
2016-11-11 04:43:51 +0800

    Add ES2017 string padding (#12152)
    * add es2017.string.d.ts for String.prototype.{padStart,padEnd}
    
    * append es2017.string.d.ts into es2017.d.ts
    
    * add es2017.string into commandLineParser
    
    * append es2017.string into error message for unit tests of commandLineParser
    
    * append es2017.string into Gulpfile
    
    * append es2017.string into Jakefile

--- src/compiler/commandLineParser.ts
+++ src/compiler/commandLineParser.ts
@@ -434,7 +434,8 @@ namespace ts {
                     "es2015.symbol.wellknown": "lib.es2015.symbol.wellknown.d.ts",
                     "es2016.array.include": "lib.es2016.array.include.d.ts",
                     "es2017.object": "lib.es2017.object.d.ts",
-                    "es2017.sharedmemory": "lib.es2017.sharedmemory.d.ts"
+                    "es2017.sharedmemory": "lib.es2017.sharedmemory.d.ts",
+                    "es2017.string": "lib.es2017.string.d.ts",
                 }),
             },
             description: Diagnostics.Specify_library_files_to_be_included_in_the_compilation_Colon
