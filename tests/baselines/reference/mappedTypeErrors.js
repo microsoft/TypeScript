5028a44e562c27636c6ad98addc410925e002709
Anders Hejlsberg <andersh@microsoft.com>
2016-11-11 11:00:24 -0800

    Accept new baselines

---
+++ tests/baselines/reference/mappedTypeErrors.js
@@ -0,0 +1,121 @@
+//// [mappedTypeErrors.ts]
+
+type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
+
+type Readonly<T> = {
+    readonly [P in keyof T]: T[P];
+};
+
+type Pick<T, K extends keyof T> = {
+    [P in K]: T[P];
+}
+
+type Record<K extends string | number, T> = {
+    [_ in K]: T;
+}
+
+interface Shape {
+    name: string;
+    width: number;
+    height: number;
+    visible: boolean;
+}
+
+interface Named {
+    name: string;
+}
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+type T00 = { [P in P]: string };  // Error
+type T01 = { [P in Date]: number };  // Error
+type T02 = Record<Date, number>;  // Error
+
+type T10 = Pick<Shape, "name">;
+type T11 = Pick<Shape, "foo">;  // Error
+type T12 = Pick<Shape, "name" | "foo">;  // Error
+type T13 = Pick<Shape, keyof Named>;
+type T14 = Pick<Shape, keyof Point>;  // Error
+type T15 = Pick<Shape, never>;
+type T16 = Pick<Shape, undefined>;
+
+function f1<T>(x: T) {
+    let y: Pick<Shape, T>;  // Error
+}
+
+function f2<T extends string | number>(x: T) {
+    let y: Pick<Shape, T>;  // Error
+}
+
+function f3<T extends keyof Shape>(x: T) {
+    let y: Pick<Shape, T>;
+}
+
+function f4<T extends keyof Named>(x: T) {
+    let y: Pick<Shape, T>;
+}
+
+//// [mappedTypeErrors.js]
+function f1(x) {
+    var y; // Error
+}
+function f2(x) {
+    var y; // Error
+}
+function f3(x) {
+    var y;
+}
+function f4(x) {
+    var y;
+}
+
+
+//// [mappedTypeErrors.d.ts]
+declare type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
+declare type Readonly<T> = {
+    readonly [P in keyof T]: T[P];
+};
+declare type Pick<T, K extends keyof T> = {
+    [P in K]: T[P];
+};
+declare type Record<K extends string | number, T> = {
+    [_ in K]: T;
+};
+interface Shape {
+    name: string;
+    width: number;
+    height: number;
+    visible: boolean;
+}
+interface Named {
+    name: string;
+}
+interface Point {
+    x: number;
+    y: number;
+}
+declare type T00 = {
+    [P in P]: string;
+};
+declare type T01 = {
+    [P in Date]: number;
+};
+declare type T02 = Record<Date, number>;
+declare type T10 = Pick<Shape, "name">;
+declare type T11 = Pick<Shape, "foo">;
+declare type T12 = Pick<Shape, "name" | "foo">;
+declare type T13 = Pick<Shape, keyof Named>;
+declare type T14 = Pick<Shape, keyof Point>;
+declare type T15 = Pick<Shape, never>;
+declare type T16 = Pick<Shape, undefined>;
+declare function f1<T>(x: T): void;
+declare function f2<T extends string | number>(x: T): void;
+declare function f3<T extends keyof Shape>(x: T): void;
+declare function f4<T extends keyof Named>(x: T): void;
--- tests/baselines/reference/mappedTypes1.js
+++ tests/baselines/reference/mappedTypes1.js
@@ -25,6 +25,15 @@ type T36 = { [P in keyof void]: void };
 type T37 = { [P in keyof symbol]: void };
 type T38 = { [P in keyof never]: void };
 
+type T40 = { [P in string]: void };
+type T41 = { [P in number]: void };
+type T42 = { [P in string | number]: void };
+type T43 = { [P in "a" | "b" | 0 | 1]: void };
+type T44 = { [P in "a" | "b" | "0" | "1"]: void };
+type T45 = { [P in "a" | "b" | "0" | "1" | 0 | 1]: void };
+type T46 = { [P in number | "a" | "b" | 0 | 1]: void };
+type T47 = { [P in string | number | "a" | "b" | 0 | 1]: void };
+
 declare function f1<T1>(): { [P in keyof T1]: void };
 declare function f2<T1 extends string>(): { [P in keyof T1]: void };
 declare function f3<T1 extends number>(): { [P in keyof T1]: void };
@@ -101,6 +110,30 @@ declare type T37 = {
 };
 declare type T38 = {
     [P in keyof never]: void;
+};
+declare type T40 = {
+    [P in string]: void;
+};
+declare type T41 = {
+    [P in number]: void;
+};
+declare type T42 = {
+    [P in string | number]: void;
+};
+declare type T43 = {
+    [P in "a" | "b" | 0 | 1]: void;
+};
+declare type T44 = {
+    [P in "a" | "b" | "0" | "1"]: void;
+};
+declare type T45 = {
+    [P in "a" | "b" | "0" | "1" | 0 | 1]: void;
+};
+declare type T46 = {
+    [P in number | "a" | "b" | 0 | 1]: void;
+};
+declare type T47 = {
+    [P in string | number | "a" | "b" | 0 | 1]: void;
 };
 declare function f1<T1>(): {
     [P in keyof T1]: void;
