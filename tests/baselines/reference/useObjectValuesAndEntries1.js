5599d05252c3453714fa9d093f6db794cca93bb9
Ethan Resnick <ethan.resnick@gmail.com>
2016-11-14 20:51:28 -0500

    Object.entries() types: fixup
    1. Special case array. Because array instances are typed as
    implementing an interface that lists all their public methods, `keyof
    Array` returns all those method names as keys…even though they’re not
    actually enumerable at runtime.
    
    2. Intersect keyof T with string, because `keyof T` can return `number
    | string` in some cases, whereas the entries’ keys will always be
    strings.

--- tests/baselines/reference/useObjectValuesAndEntries1.js
+++ tests/baselines/reference/useObjectValuesAndEntries1.js
@@ -6,10 +6,15 @@ for (var x of Object.values(o)) {
     let y = x;
 }
 
-var entries = Object.entries(o);  // <-- entries: ['a' | 'b', number][]
+var entries = Object.entries(o);  // <-- entries: [('a' & string) | ('b' & string), number][]
 var entries1 = Object.entries(1); // <-- entries: [string, any][]
-var entries2 = Object.entries({a: true, b: 2}) // ['a' | 'b', number | boolean][]
-var entries3 = Object.entries({}) // [never, any][]
+var entries2 = Object.entries({a: true, b: 2}) // [('a' & string) | ('b' & string), number | boolean][]
+var entries3 = Object.entries({}) // [string, any][]
+var entries4 = Object.entries([1, 2, 3, 4]); // [string, number][]
+
+// type below should be [string | (string & number), any] NOT [string | number, any]
+var x2: { [index: string]: any } = {1: 2};
+var entries5 = Object.entries(x2);
 
 
 //// [useObjectValuesAndEntries1.js]
@@ -18,7 +23,11 @@ for (var _i = 0, _a = Object.values(o); _i < _a.length; _i++) {
     var x = _a[_i];
     var y = x;
 }
-var entries = Object.entries(o); // <-- entries: ['a' | 'b', number][]
+var entries = Object.entries(o); // <-- entries: [('a' & string) | ('b' & string), number][]
 var entries1 = Object.entries(1); // <-- entries: [string, any][]
-var entries2 = Object.entries({ a: true, b: 2 }); // ['a' | 'b', number | boolean][]
-var entries3 = Object.entries({}); // [never, any][]
+var entries2 = Object.entries({ a: true, b: 2 }); // [('a' & string) | ('b' & string), number | boolean][]
+var entries3 = Object.entries({}); // [string, any][]
+var entries4 = Object.entries([1, 2, 3, 4]); // [string, number][]
+// type below should be [string | (string & number), any] NOT [string | number, any]
+var x2 = { 1: 2 };
+var entries5 = Object.entries(x2);
