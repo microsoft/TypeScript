17e1e152947db8e25e5914f3e97b507a0e97351e
Mohamed Hegazy <mhegazy@microsoft.com>
2016-11-15 11:53:01 -0800

    Make sure version is public

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -1,11 +1,13 @@
 ﻿/// <reference path="types.ts"/>
 /// <reference path="performance.ts" />
 
+namespace ts {
+    /** The version of the TypeScript compiler release */
+    export const version = "2.0.10";
+}
+
 /* @internal */
 namespace ts {
-
-    export const version = "2.0.10";
-
     /**
      * Ternary values are defined such that
      * x & y is False if either x or y is False.
