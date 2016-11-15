c011b36d2ecfb2713b61a5e0bb366d2ba55a7a35
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-15 11:21:23 -0800

    Update baseline of newly merged test

--- tests/baselines/reference/objectRest2.js
+++ tests/baselines/reference/objectRest2.js
@@ -20,6 +20,9 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
         s = arguments[i];
         for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
             t[p] = s[p];
+        if (typeof Object.getOwnPropertySymbols === "function")
+            for (var i = 0, s = Object.getOwnPropertySymbols(s); i < s.length; i++)
+                t[i] = s[i];
     }
     return t;
 };
