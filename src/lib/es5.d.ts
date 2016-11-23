60395565e6fb9767aa6a73a0086238793bb7df58
Mohamed Hegazy <mhegazy@microsoft.com>
2016-11-23 09:48:25 -0800

    Handel call and construct signatures

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -186,6 +186,18 @@ interface ObjectConstructor {
       * Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
       * @param o Object on which to lock the attributes.
       */
+    freeze<T extends (...args: any[]) => any>(f: T): T;
+
+    /**
+      * Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+      * @param o Object on which to lock the attributes.
+      */
+    freeze<T extends new (...args: any[]) => any>(c: T): T;
+
+    /**
+      * Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+      * @param o Object on which to lock the attributes.
+      */
     freeze<T>(f: (...args: any[]) => T): (...args: any[]) => T;
 
     /**
