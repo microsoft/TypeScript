e18901a61ef165fa5c1d73047c32a84403d53c55
Yuichi Nukiyama <oscar.wilde84@hotmail.co.jp>
2016-11-26 08:03:42 +0900

    Revert "remove extra files"
    This reverts commit b293257640e608c09055e033e8b3b7b8dda342cb.

---
+++ tests/baselines/reference/assigningFromObjecToAnythingElse.js
@@ -0,0 +1,18 @@
+//// [assigningFromObjecToAnythingElse.ts]
+var x: Object;
+var y: RegExp;
+y = x;
+
+var a: String = Object.create<Object>("");
+var c: String = Object.create<Number>(1);
+
+var w: Error = new Object();
+
+
+//// [assigningFromObjecToAnythingElse.js]
+var x;
+var y;
+y = x;
+var a = Object.create("");
+var c = Object.create(1);
+var w = new Object();
