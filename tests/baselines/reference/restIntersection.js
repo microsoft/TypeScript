e4f0a50a7cf9f212d7102756e336472af89d2fa7
Mohamed Hegazy <mhegazy@microsoft.com>
2016-11-29 12:29:40 -0800

    Update test name

--- tests/baselines/reference/restIntersection.js
+++ tests/baselines/reference/restIntersection.js
@@ -1,17 +1,11 @@
-//// [restIntersectionOrIntersection.ts]
+//// [restIntersection.ts]
 var intersection: { x: number, y: number } & { w: string, z: string };
-var union: { a: number, c: boolean } | { a: string, b: string };
-
 
 var rest1: { y: number, w: string, z: string };
 var {x, ...rest1 } = intersection;
 
-var rest2: { c: boolean } | { b: string };
-var {a, ...rest2 } = union;
 
-
-
-//// [restIntersectionOrIntersection.js]
+//// [restIntersection.js]
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -22,8 +16,5 @@ var __rest = (this && this.__rest) || function (s, e) {
     return t;
 };
 var intersection;
-var union;
 var rest1;
 var x = intersection.x, rest1 = __rest(intersection, ["x"]);
-var rest2;
-var a = union.a, rest2 = __rest(union, ["a"]);
