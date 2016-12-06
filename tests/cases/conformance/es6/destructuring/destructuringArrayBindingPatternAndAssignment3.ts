4efa61cf80aa66512a892f8db40bfa1aee1c0fe7
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-12-06 13:05:52 -0800

    More tests for binding elements referencing previous elements

--- tests/cases/conformance/es6/destructuring/destructuringArrayBindingPatternAndAssignment3.ts
+++ tests/cases/conformance/es6/destructuring/destructuringArrayBindingPatternAndAssignment3.ts
@@ -1,4 +1,10 @@
 const [a, b = a] = [1]; // ok
-const [a, b = a, c = c] = [1]; // error for c
-const [a, b = a, c = d, d = a] = [1]; // error for c
+const [c, d = c, e = e] = [1]; // error for e = e
+const [f, g = f, h = i, i = f] = [1]; // error for h = i
 
+(function ([a, b = a]) { // ok
+})([1]);
+(function ([c, d = c, e = e]) { // error for e = e
+})([1]);
+(function ([f, g = f, h = i, i = f]) { // error for h = i
+})([1])
