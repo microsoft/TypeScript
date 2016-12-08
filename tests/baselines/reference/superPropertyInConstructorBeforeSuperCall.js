074f2b7636102003acfaa54cdc9a4b86dbf5e50c
vvakame <vvakame+dev@gmail.com>
2016-12-08 23:02:59 +0900

    catch up

--- tests/baselines/reference/superPropertyInConstructorBeforeSuperCall.js
+++ tests/baselines/reference/superPropertyInConstructorBeforeSuperCall.js
@@ -16,8 +16,12 @@ class C2 extends B {
 }
 
 //// [superPropertyInConstructorBeforeSuperCall.js]
+var __extendStatics = (this && this.__extendStatics) ||
+    Object.setPrototypeOf ||
+    ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+    function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
 var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    __extendStatics(d, b);
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
