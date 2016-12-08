96c14de26a83eaac7cfdfeb1ed3b4f6734cf9e54
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-12-08 10:25:21 -0800

    More tests:default-valued parameter+strictNullChecks

--- tests/cases/compiler/defaultParameterAddsUndefinedWithStrictNullChecks.ts
+++ tests/cases/compiler/defaultParameterAddsUndefinedWithStrictNullChecks.ts
@@ -1,3 +1,9 @@
 // @strictNullChecks: true
 function f(addUndefined1 = "J", addUndefined2?: number) {
+    return addUndefined1.length + (addUndefined2 || 0);
 }
+function g(addUndefined = "J", addDefined: number) {
+    return addUndefined.length + addDefined;
+}
+let total = f() + f('a', 1) + f('b') + f(undefined, 2);
+total = g('c', 3) + g(undefined, 4);
