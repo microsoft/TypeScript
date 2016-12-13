b0f42f04eea70f4912fd85ef8d48cafda31e3107
Bill Ticehurst <billti@hotmail.com>
2016-12-12 18:20:09 -0800

    Fix __awaiter helper for IE8 compat

--- src/compiler/transformers/es2017.ts
+++ src/compiler/transformers/es2017.ts
@@ -494,7 +494,7 @@ namespace ts {
                     function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
                     function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
                     function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-                    step((generator = generator.apply(thisArg, _arguments)).next());
+                    step((generator = generator.apply(thisArg, _arguments || [])).next());
                 });
             };`
     };
