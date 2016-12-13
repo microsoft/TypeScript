e9814f8a2f22d2e728f71822b2f0f63b67eae029
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-12-13 13:55:13 -0800

    Test:spreading any doesn't set flags on anyType
    And update baselines

---
+++ tests/baselines/reference/explicitAnyAfterSpreadNoImplicitAnyError.js
@@ -0,0 +1,16 @@
+//// [explicitAnyAfterSpreadNoImplicitAnyError.ts]
+({ a: [], ...(null as any) });
+let x: any;
+
+
+//// [explicitAnyAfterSpreadNoImplicitAnyError.js]
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+(__assign({ a: [] }, null));
+var x;
