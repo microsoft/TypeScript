1f79741732f5bf517c1fd232ca9036815f3764bb
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-12-20 03:14:50 +0900

    needless newline

--- src/services/classifier.ts
+++ src/services/classifier.ts
@@ -605,7 +605,6 @@ namespace ts {
         Debug.assert(classifications.spans.length % 3 === 0);
         const dense = classifications.spans;
         const result: ClassifiedSpan[] = [];
-
         for (let i = 0; i < dense.length; i += 3) {
             result.push({
                 textSpan: createTextSpan(dense[i], dense[i + 1]),
