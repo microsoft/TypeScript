1c080d11e6445398ff34b50995f5f9aa6acf2d1d
Andy Hanson <anhans@microsoft.com>
2017-01-20 07:20:23 -0800

    Set ScriptElementKind for enum members to "enum member" instead of "const"

--- src/services/types.ts
+++ src/services/types.ts
@@ -701,8 +701,7 @@ namespace ts {
 
         /** enum E */
         export const enumElement = "enum";
-        // TODO: GH#9983
-        export const enumMemberElement = "const";
+        export const enumMemberElement = "enum member";
 
         /**
          * Inside module and script only
