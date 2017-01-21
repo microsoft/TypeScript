3686811d1ace0d7c085a71f192964384accc3526
rdosanjh <me@rajdeep.io>
2017-01-21 23:15:05 +0000

    better error message for mistaken import

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -1140,7 +1140,7 @@ namespace ts {
         }
 
         function getTargetOfImportClause(node: ImportClause): Symbol {
-            const moduleSymbol = resolveExternalModuleName(node, (<ImportDeclaration>node.parent).moduleSpecifier);
+            const moduleSymbol = resolveExternalModuleName(node, (<ImportDeclaration>node.parent).moduleSpecifier);    
 
             if (moduleSymbol) {
                 const exportDefaultSymbol = isShorthandAmbientModuleSymbol(moduleSymbol) ?
@@ -1151,6 +1151,9 @@ namespace ts {
 
                 if (!exportDefaultSymbol && !allowSyntheticDefaultImports) {
                     error(node.name, Diagnostics.Module_0_has_no_default_export, symbolToString(moduleSymbol));
+                    if(!moduleSymbol.exports[node.symbol.name]) {
+                        error(node, Diagnostics.Did_you_mean_to_import_the_named_import_0_by_using_curly_braces, symbolToString(node.symbol))
+                    }
                 }
                 else if (!exportDefaultSymbol && allowSyntheticDefaultImports) {
                     return resolveExternalModuleSymbol(moduleSymbol) || resolveSymbol(moduleSymbol);
@@ -1243,6 +1246,9 @@ namespace ts {
                         symbolFromModule || symbolFromVariable;
                     if (!symbol) {
                         error(name, Diagnostics.Module_0_has_no_exported_member_1, getFullyQualifiedName(moduleSymbol), declarationNameToString(name));
+                    }
+                    if(!symbol && moduleSymbol.exports["default"]){
+                        error(name, Diagnostics.Did_you_mean_to_import_the_default_export_with_no_curly_braces, getFullyQualifiedName(moduleSymbol), declarationNameToString(name));
                     }
                     return symbol;
                 }
