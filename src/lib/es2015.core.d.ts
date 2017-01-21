23323ac31a4eb5fb2f273571532db7caa8324959
falsandtru <falsandtru@users.noreply.github.com>
2017-01-21 18:55:50 +0900

    Use Partial types for Object.assign

--- src/lib/es2015.core.d.ts
+++ src/lib/es2015.core.d.ts
@@ -276,6 +276,14 @@ interface ObjectConstructor {
       * Copy the values of all of the enumerable own properties from one or more source objects to a
       * target object. Returns the target object.
       * @param target The target object to copy to.
+      * @param sources One or more source objects from which to copy properties
+      */
+    assign<T>(target: Partial<T>, ...sources: Partial<T>[]): T;
+
+    /**
+      * Copy the values of all of the enumerable own properties from one or more source objects to a
+      * target object. Returns the target object.
+      * @param target The target object to copy to.
       * @param source The source object from which to copy properties.
       */
     assign<T, U>(target: T, source: U): T & U;
