34b4dbb26000d726600c4565ccb75cbc4eb53d06
Anders Hejlsberg <andersh@microsoft.com>
2017-01-30 15:47:18 -0800

    Add regression test

--- tests/cases/conformance/types/keyof/keyofAndIndexedAccess.ts
+++ tests/cases/conformance/types/keyof/keyofAndIndexedAccess.ts
@@ -517,3 +517,13 @@ class B extends A<{ x: number}> {
 		p.x;
 	}
 }
+
+// Repro from #13749
+
+class Form<T> {
+    private childFormFactories: {[K in keyof T]: (v: T[K]) => Form<T[K]>}
+
+    public set<K extends keyof T>(prop: K, value: T[K]) {
+        this.childFormFactories[prop](value)
+    }
+}
