125dd57a757612367bcf495ed3070bb932ae75ed
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-02-08 13:28:23 -0800

    Fix assigned type of assignment nested in literals
    Fixes #12946

---
+++ tests/baselines/reference/assignmentNestedInLiterals.js
@@ -0,0 +1,13 @@
+//// [assignmentNestedInLiterals.ts]
+var target, x, y;
+target = [x = 1, y = x];
+
+var aegis, a, b;
+aegis = { x: a = 1, y: b = a };
+
+
+//// [assignmentNestedInLiterals.js]
+var target, x, y;
+target = [x = 1, y = x];
+var aegis, a, b;
+aegis = { x: a = 1, y: b = a };
---
+++ tests/cases/compiler/assignmentNestedInLiterals.ts
@@ -0,0 +1,6 @@
+// @noImplicitAny: true
+var target, x, y;
+target = [x = 1, y = x];
+
+var aegis, a, b;
+aegis = { x: a = 1, y: b = a };
