11533787ffc875f865cf0dcfd2231ebf366a608b
gcnew <gcnew@abv.bg>
2017-02-18 02:10:36 +0200

    Added tests

---
+++ tests/baselines/reference/literalAssertions.js
@@ -0,0 +1,41 @@
+//// [literalAssertions.ts]
+0 as 0; // OK
+0 as 1; // Error
+
+false as (false | string); // OK
+false as (true | string);  // Error
+false as (boolean | string);  // OK
+
+'hello' as 'str'; // Error
+'hello' as 'hello'; // OK
+'hello' as ('str' | 123); // Error
+'hello' as ('hello' | 123); // OK
+'hello' as ('str' & 'hello'); // OK
+'hello' as ('str' | 'hello'); // OK
+'hello' as (1 | 2 | string); // OK
+'hello' as ('str' & { _brand: any }); // Error
+'hello' as ('hello' & { _brand: any }); // OK
+'hello' as ('str' & { _brand: any } | 1); // Error
+'hello' as ('hello' & { _brand: any } | 1); // OK
+
+('string' as string as 'literal'); // OK
+
+
+//// [literalAssertions.js]
+0; // OK
+0; // Error
+false; // OK
+false; // Error
+false; // OK
+'hello'; // Error
+'hello'; // OK
+'hello'; // Error
+'hello'; // OK
+'hello'; // OK
+'hello'; // OK
+'hello'; // OK
+'hello'; // Error
+'hello'; // OK
+'hello'; // Error
+'hello'; // OK
+'string'; // OK
---
+++ tests/cases/conformance/expressions/typeAssertions/literalAssertions.ts
@@ -0,0 +1,20 @@
+0 as 0; // OK
+0 as 1; // Error
+
+false as (false | string); // OK
+false as (true | string);  // Error
+false as (boolean | string);  // OK
+
+'hello' as 'str'; // Error
+'hello' as 'hello'; // OK
+'hello' as ('str' | 123); // Error
+'hello' as ('hello' | 123); // OK
+'hello' as ('str' & 'hello'); // OK
+'hello' as ('str' | 'hello'); // OK
+'hello' as (1 | 2 | string); // OK
+'hello' as ('str' & { _brand: any }); // Error
+'hello' as ('hello' & { _brand: any }); // OK
+'hello' as ('str' & { _brand: any } | 1); // Error
+'hello' as ('hello' & { _brand: any } | 1); // OK
+
+('string' as string as 'literal'); // OK
