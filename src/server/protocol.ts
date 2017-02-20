bb86e9e291a9a592af2f2ce43ebbe0eb946b4a18
Jason Jarrett <jason@elegantcode.com>
2017-02-19 22:14:39 -0800

    Update protocol.ts

--- src/server/protocol.ts
+++ src/server/protocol.ts
@@ -820,7 +820,7 @@ namespace ts.server.protocol {
      * Represents a file in external project.
      * External project is project whose set of files, compilation options and open\close state
      * is maintained by the client (i.e. if all this data come from .csproj file in Visual Studio).
-     * External project will exist even if all files in it are closed and should be closed explicity.
+     * External project will exist even if all files in it are closed and should be closed explicitly.
      * If external project includes one or more tsconfig.json/jsconfig.json files then tsserver will
      * create configured project for every config file but will maintain a link that these projects were created
      * as a result of opening external project so they should be removed once external project is closed.
