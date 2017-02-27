baef2aa2c17c59669038525e95010581ac273fe1
Oleg Mihailik <mihailik@gmail.com>
2017-02-27 01:05:56 +0000

    Baseline accept

--- tests/cases/compiler/evalAfter0.ts
+++ tests/cases/compiler/evalAfter0.ts
@@ -1,3 +1,4 @@
 (0,eval)("10"); // fine: special case for eval
 
-(0,alert)("10"); // error: no side effect left of comma (suspect of missing method name or something)
+declare var eva;
+(0,eva)("10"); // error: no side effect left of comma (suspect of missing method name or something)
