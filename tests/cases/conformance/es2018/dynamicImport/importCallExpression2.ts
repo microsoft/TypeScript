26f9a52e9252a6c4a1d65df4e2a5a0d187dbab24
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-03-13 17:07:13 -0700

    Add initial tests

---
+++ tests/cases/conformance/es2018/dynamicImport/importCallExpression2.ts
@@ -0,0 +1,10 @@
+﻿// @module: es2018
+// @lib: es2015
+// @filename: 0.ts
+export function foo(){}
+
+// @filename: 1.ts
+import * as Zero from "./0"  // Should preserve ES2015 module syntax
+import("./0");
+var p1 = import("./0");
+export default p1;
