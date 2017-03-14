fd9fb8f9bce4828fc657cfb36a3a47c3283a583c
Mohamed Hegazy <mhegazy@microsoft.com>
2017-03-13 22:49:54 -0700

    Support static properties

--- src/services/codefixes/fixAddMissingMember.ts
+++ src/services/codefixes/fixAddMissingMember.ts
@@ -1,4 +1,4 @@
-/* @internal */
+﻿/* @internal */
 namespace ts.codefix {
     registerCodeFix({
         errorCodes: [Diagnostics.Property_0_does_not_exist_on_type_1.code],
@@ -13,13 +13,7 @@ namespace ts.codefix {
         // this.missing = 1;
         //      ^^^^^^^
         const token = getTokenAtPosition(sourceFile, start);
-
         if (token.kind != SyntaxKind.Identifier) {
-            return undefined;
-        }
-
-        const classDeclaration = getContainingClass(token);
-        if (!classDeclaration) {
             return undefined;
         }
 
@@ -27,8 +21,19 @@ namespace ts.codefix {
             return undefined;
         }
 
-        return isInJavaScriptFile(sourceFile) ? getActionsForAddMissingMemberInJavaScriptFile() : getActionsForAddMissingMemberInTypeScriptFile();
+        const classMemberDeclaration = getThisContainer(token, /*includeArrowFunctions*/ false);
+        if (!isClassElement(classMemberDeclaration)) {
+            return undefined;
+        }
 
+        const classDeclaration = <ClassLikeDeclaration>classMemberDeclaration.parent;
+        if (!classDeclaration || !isClassLike(classDeclaration)) {
+            return undefined;
+        }
+
+        const isStatic = hasModifier(getThisContainer(token, /*includeArrowFunctions*/ false), ModifierFlags.Static);
+
+        return isInJavaScriptFile(sourceFile) ? getActionsForAddMissingMemberInJavaScriptFile() : getActionsForAddMissingMemberInTypeScriptFile();
 
         function getActionsForAddMissingMemberInTypeScriptFile(): CodeAction[] | undefined {
             let typeString = "any";
@@ -43,47 +48,71 @@ namespace ts.codefix {
 
             const startPos = classDeclaration.members.pos;
 
-            return [{
+            const actions = [{
                 description: formatStringFromArgs(getLocaleSpecificMessage(Diagnostics.Add_declaration_for_missing_property_0), [token.getText()]),
                 changes: [{
                     fileName: sourceFile.fileName,
                     textChanges: [{
                         span: { start: startPos, length: 0 },
-                        newText: `${token.getFullText(sourceFile)}: ${typeString};`
-                    }]
-                }]
-            },
-            {
-                description: formatStringFromArgs(getLocaleSpecificMessage(Diagnostics.Add_index_signature_for_missing_property_0), [token.getText()]),
-                changes: [{
-                    fileName: sourceFile.fileName,
-                    textChanges: [{
-                        span: { start: startPos, length: 0 },
-                        newText: `[name: string]: ${typeString};`
+                        newText: `${isStatic ? "static " : ""}${token.getFullText(sourceFile)}: ${typeString};`
                     }]
                 }]
             }];
+
+            if (!isStatic) {
+                actions.push({
+                    description: formatStringFromArgs(getLocaleSpecificMessage(Diagnostics.Add_index_signature_for_missing_property_0), [token.getText()]),
+                    changes: [{
+                        fileName: sourceFile.fileName,
+                        textChanges: [{
+                            span: { start: startPos, length: 0 },
+                            newText: `[x: string]: ${typeString};`
+                        }]
+                    }]
+                });
+            }
+
+            return actions;
         }
 
         function getActionsForAddMissingMemberInJavaScriptFile(): CodeAction[] | undefined {
-            const classConstructor = getFirstConstructorWithBody(classDeclaration);
-            if (!classConstructor) {
-                return undefined;
-            }
-
             const memberName = token.getText();
-            const startPos = classConstructor.body.getEnd() - 1;
 
-            return [{
-                description: formatStringFromArgs(getLocaleSpecificMessage(Diagnostics.Initialize_property_0_in_the_constructor), [memberName]),
-                changes: [{
-                    fileName: sourceFile.fileName,
-                    textChanges: [{
-                        span: { start: startPos, length: 0 },
-                        newText: `this.${memberName} = undefined;`
+            if (isStatic) {
+                if (classDeclaration.kind === SyntaxKind.ClassExpression) {
+                    return undefined;
+                }
+
+                const className = classDeclaration.name.getText();
+
+                return [{
+                    description: formatStringFromArgs(getLocaleSpecificMessage(Diagnostics.Initialize_static_property_0), [memberName]),
+                    changes: [{
+                        fileName: sourceFile.fileName,
+                        textChanges: [{
+                            span: { start: classDeclaration.getEnd(), length: 0 },
+                            newText: `${context.newLineCharacter}${className}.${memberName} = undefined;${context.newLineCharacter}`
+                        }]
                     }]
-                }]
-            }];
+                }];
+            }
+            else {
+                const classConstructor = getFirstConstructorWithBody(classDeclaration);
+                if (!classConstructor) {
+                    return undefined;
+                }
+
+                return [{
+                    description: formatStringFromArgs(getLocaleSpecificMessage(Diagnostics.Initialize_property_0_in_the_constructor), [memberName]),
+                    changes: [{
+                        fileName: sourceFile.fileName,
+                        textChanges: [{
+                            span: { start: classConstructor.body.getEnd() - 1, length: 0 },
+                            newText: `this.${memberName} = undefined;${context.newLineCharacter}`
+                        }]
+                    }]
+                }];
+            }
         }
     }
 }
