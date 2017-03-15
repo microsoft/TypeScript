db878adea31c025328276cdba365f578ba9acd7b
cedvdb <cedvandenbosch@gmail.com>
2017-03-15 03:15:58 +0100

    changed forEach comment

--- src/lib/dom.iterable.d.ts
+++ src/lib/dom.iterable.d.ts
@@ -10,9 +10,11 @@ interface NodeList {
       * Returns an list of values in the list
       */
     entries(): IterableIterator<Node>;
-    /** 
-      * Executes a provided function once per NodeList element.
-      */  
+    /**
+      * Performs the specified action for each node in an list.
+      * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the list.
+      * @param thisArg  An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+      */
     forEach(callbackfn: (value: Node, index: number, listObj: NodeList) => void, thisArg?: any): void;
     /** 
       * Returns an list of keys in the list
@@ -33,9 +35,11 @@ interface NodeListOf<TNode extends Node> {
       */
     entries(): IterableIterator<TNode>;
 
-    /** 
-      * Executes a provided function once per NodeList element.
-      */  
+    /**
+      * Performs the specified action for each node in an list.
+      * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the list.
+      * @param thisArg  An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+      */
     forEach(callbackfn: (value: TNode, index: number, listObj: NodeListOf<TNode>) => void, thisArg?: any): void;
     /** 
       * Returns an list of keys in the list
