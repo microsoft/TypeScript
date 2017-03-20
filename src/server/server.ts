0c1642a8b778e9f117beb341b3b0ba20f4c5ca7d
Mine Starks <mineyalc@microsoft.com>
2017-03-20 16:07:20 -0700

    Allow specifying location of typingSafeList.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -219,6 +219,7 @@ namespace ts.server {
             host: ServerHost,
             eventPort: number,
             readonly globalTypingsCacheLocation: string,
+            readonly typingSafeListLocation: string,
             private newLine: string) {
             this.throttledOperations = new ThrottledOperations(host);
             if (eventPort) {
@@ -259,6 +260,9 @@ namespace ts.server {
             }
             if (this.logger.loggingEnabled() && this.logger.getLogFileName()) {
                 args.push(Arguments.LogFile, combinePaths(getDirectoryPath(normalizeSlashes(this.logger.getLogFileName())), `ti-${process.pid}.log`));
+            }
+            if (this.typingSafeListLocation) {
+                args.push(Arguments.TypingSafeListLocation, this.typingSafeListLocation);
             }
             const execArgv: string[] = [];
             {
@@ -378,11 +382,12 @@ namespace ts.server {
             useSingleInferredProject: boolean,
             disableAutomaticTypingAcquisition: boolean,
             globalTypingsCacheLocation: string,
+            typingSafeListLocation: string,
             telemetryEnabled: boolean,
             logger: server.Logger) {
                 const typingsInstaller = disableAutomaticTypingAcquisition
                     ? undefined
-                    : new NodeTypingsInstaller(telemetryEnabled, logger, host, installerEventPort, globalTypingsCacheLocation, host.newLine);
+                    : new NodeTypingsInstaller(telemetryEnabled, logger, host, installerEventPort, globalTypingsCacheLocation, typingSafeListLocation, host.newLine);
 
                 super(
                     host,
@@ -728,6 +733,8 @@ namespace ts.server {
     if (localeStr) {
         validateLocaleAndSetLanguage(localeStr, sys);
     }
+    
+    const typingSafeListLocation = findArgument("--typingSafeListLocation");
 
     const useSingleInferredProject = hasArgument("--useSingleInferredProject");
     const disableAutomaticTypingAcquisition = hasArgument("--disableAutomaticTypingAcquisition");
@@ -741,6 +748,7 @@ namespace ts.server {
         useSingleInferredProject,
         disableAutomaticTypingAcquisition,
         getGlobalTypingsCacheLocation(),
+        typingSafeListLocation,
         telemetryEnabled,
         logger);
     process.on("uncaughtException", function (err: Error) {
