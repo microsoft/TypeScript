61f199e5f25b1d443e0c674a9f1eb086f9ebde7e
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-03-24 15:32:16 -0700

    Update tests

--- tests/cases/conformance/es2018/dynamicImport/importCallExpressionReturnPromiseOfAny.ts
+++ tests/cases/conformance/es2018/dynamicImport/importCallExpressionReturnPromiseOfAny.ts
@@ -1,7 +1,11 @@
 // @module: commonjs
 // @target: es6
-// @noImplicitAny: false
+// @noImplicitAny: true
+// @filename: defaultPath.ts
+export class C {}
 
+// @filename: 1.ts
+import * as defaultModule from "./defaultPath";
 declare function getSpecifier(): string;
 declare var whatToLoad: boolean;
 declare const directory: string;
@@ -9,14 +13,15 @@ declare const moduleFile: number;
 
 import(`${directory}\${moduleFile}`);
 import(getSpecifier());
-var p1 = import(getSpecifier());
-const p2 = import(whatToLoad ? getSpecifier() : "defaulPath")
+var p1: Promise<any> = import(getSpecifier());
+var p11: Promise<typeof defaultModule> = import(getSpecifier());
+const p2 = import(whatToLoad ? getSpecifier() : "defaulPath") as Promise<typeof defaultModule>;
 p1.then(zero => {
     return zero.foo();  // ok, zero is any
 });
 
 let j: string;
-var p3 = import(j=getSpecifier());
+var p3: Promise<typeof defaultModule> = import(j=getSpecifier());
 
 function * loadModule(directories: string[]) {
     for (const directory of directories) {
