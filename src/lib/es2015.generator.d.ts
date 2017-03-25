a065331d694a9864c074833316d9eef2ea098f0f
IgorNovozhilov <maildrakon@gmail.com>
2017-03-25 17:26:18 +0300

    [object Generator]
    25.3 Generator Objects
    http://www.ecma-international.org/ecma-262/6.0/#sec-generator-objects
    ```
    C:\Users> node
    > GF = function* (){}
    [Function: GF]
    > GF.constructor.name
    'GeneratorFunction'
    > G = GF()
    {}
    > G + ''
    '[object Generator]'
    ```

--- src/lib/es2015.generator.d.ts
+++ src/lib/es2015.generator.d.ts
@@ -1,4 +1,13 @@
-interface GeneratorFunction extends Function { }
+interface Generator extends Iterator<any> { }
+
+interface GeneratorFunction {
+    /**
+     * Creates a new Generator object.
+     * @param args A list of arguments the function accepts.
+     */
+    new (...args: any[]): Generator;
+    (...args: any[]): Generator;
+}
 
 interface GeneratorFunctionConstructor {
     /**
