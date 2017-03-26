ee2a909f54cae61621d5a4cf52ff217d8e817d86
IgorNovozhilov <maildrakon@gmail.com>
2017-03-26 17:08:27 +0300

    ["Two small changes"]

--- src/lib/es2015.generator.d.ts
+++ src/lib/es2015.generator.d.ts
@@ -6,7 +6,23 @@ interface GeneratorFunction {
      * @param args A list of arguments the function accepts.
      */
     new (...args: any[]): Generator;
+    /**
+     * Creates a new Generator object.
+     * @param args A list of arguments the function accepts.
+     */
     (...args: any[]): Generator;
+    /**
+     * The length of the arguments.
+     */
+    readonly length: number;
+    /**
+      * Returns the name of the function.
+      */
+    readonly name: string;
+    /**
+      * A reference to the prototype.
+      */
+    readonly prototype: Generator;
 }
 
 interface GeneratorFunctionConstructor {
@@ -15,7 +31,22 @@ interface GeneratorFunctionConstructor {
       * @param args A list of arguments the function accepts.
       */
     new (...args: string[]): GeneratorFunction;
+    /**
+      * Creates a new Generator function.
+      * @param args A list of arguments the function accepts.
+      */
     (...args: string[]): GeneratorFunction;
+    /**
+     * The length of the arguments.
+     */
+    readonly length: number;
+    /**
+      * Returns the name of the function.
+      */
+    readonly name: string;
+    /**
+      * A reference to the prototype.
+      */
     readonly prototype: GeneratorFunction;
 }
 declare var GeneratorFunction: GeneratorFunctionConstructor;
