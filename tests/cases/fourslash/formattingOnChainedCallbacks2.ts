5270c337226f98a0092b2bc7de66a050771f4803
Zhengbo Li <zhengbli@microsoft.com>
2017-03-30 16:36:26 -0700

    Add test

---
+++ tests/cases/fourslash/formattingOnChainedCallbacks2.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path='fourslash.ts' />
+
+////Promise
+////    .then(
+////    /*then1cb1*/cb,
+////    /*then1cb2*/cb2,
+////    /*then1*/)
+////    .then(
+////    /*then2cb1*/cb,
+////    /*then2cb2*/cb2,
+////    /*then2*/)
+////    .then();
+
+format.document();
+verify.currentFileContentIs("");
+// goTo.marker('then1cb1');
+// verify.indentationIs(8);
+// goTo.marker('then1cb2');
+// verify.indentationIs(8);
+// goTo.marker('then1');
+// verify.indentationIs(4);
+
+// goTo.marker('then2cb1');
+// verify.indentationIs(8);
+// goTo.marker('then2cb2');
+// verify.indentationIs(8);
+// goTo.marker('then2');
+// verify.indentationIs(4);
