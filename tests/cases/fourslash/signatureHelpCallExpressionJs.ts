e06b3ecbcaa7dca3bb3a70e226a07338de6c9c69
rbuckton <rbuckton@chronicles.org>
2017-03-30 13:02:39 -0700

    Added additional test verification

--- tests/cases/fourslash/signatureHelpCallExpressionJs.ts
+++ tests/cases/fourslash/signatureHelpCallExpressionJs.ts
@@ -6,6 +6,7 @@
 // @Filename: main.js
 ////function fnTest() { arguments; }
 ////fnTest(/*1*/);
+////fnTest(1, 2, 3);
 
 goTo.marker('1');
 verify.signatureHelpCountIs(1);
@@ -13,3 +14,4 @@ verify.currentSignatureParameterCountIs(1);
 verify.currentSignatureHelpIs('fnTest(...args: any[]): void');
 verify.currentParameterHelpArgumentNameIs('args');
 verify.currentParameterSpanIs("...args: any[]");
+verify.numberOfErrorsInCurrentFile(0);
