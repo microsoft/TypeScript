33524bd18c313b9fb0f2a47eda6e78733c7d4495
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-04-05 10:26:28 -0700

    Update test and baseline

--- tests/baselines/reference/emitAccessExpressionOfCastedObjectLiteralExpressionES5.js
+++
@@ -1,7 +0,0 @@
-//// [emitAccessExpressionOfCastedObjectLiteralExpressionES5.ts]
-(x) => ({ "1": "one", "2": "two" } as { [key: string]: string })[x];
-(x) => ({ "1": "one", "2": "two" } as { [key: string]: string }).x;
-
-//// [emitAccessExpressionOfCastedObjectLiteralExpressionES5.js]
-(function (x) { return ({ "1": "one", "2": "two" })[x]; });
-(function (x) { return ({ "1": "one", "2": "two" }).x; });
