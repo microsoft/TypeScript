04c71f097136493a2bc42b6e7fabcf69c0e25e24
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-04-05 13:48:35 -0700

    Include dom into default library files

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -4202,12 +4202,13 @@ namespace ts {
     export function getDefaultLibFileName(options: CompilerOptions): string {
         switch (options.target) {
             case ScriptTarget.ESNext:
+                return "lib.esnext.full.d.ts";
             case ScriptTarget.ES2017:
-                return "lib.es2017.d.ts";
+                return "lib.es2017.full.d.ts";
             case ScriptTarget.ES2016:
-                return "lib.es2016.d.ts";
+                return "lib.es2016.full.d.ts";
             case ScriptTarget.ES2015:
-                return "lib.es6.d.ts";
+                return "lib.es6.d.ts";  // We don't use lib.es2015.full.d.ts due to breaking change.
             default:
                 return "lib.d.ts";
         }
