5cfc41c2a080b8c2a6e4d47bd87074dd7d7fd721
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-04-05 17:22:52 +0900

    TEMPORARILY add ArrayBufferView on dom lib
    This is a temporary change to provide ArrayBufferView type and solve PR
    deadlock before TSJS-lib-generator PR merge.

--- src/lib/dom.generated.d.ts
+++ src/lib/dom.generated.d.ts
@@ -14940,3 +14940,4 @@ type IDBValidKey = number | string | Date | IDBArrayKey;
 type BufferSource = ArrayBuffer | ArrayBufferView;
 type MouseWheelEvent = WheelEvent;
 type ScrollRestoration = "auto" | "manual";
+type ArrayBufferView = Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView;
