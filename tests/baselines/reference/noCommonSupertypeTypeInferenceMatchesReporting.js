862fa455bfeee02cf280d5fa424ef2f9eaa79b97
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-04-17 08:28:10 -0700

    Test widening inference candidates for err reports
    Add a test to show that error reporting for type inference widens the
    same way that type inference proper does. Previously it asserted.
    
    Also update other baselines with the widened type.

---
+++ tests/baselines/reference/noCommonSupertypeTypeInferenceMatchesReporting.js
@@ -0,0 +1,14 @@
+//// [noCommonSupertypeTypeInferenceMatchesReporting.ts]
+// Fixes #15116, which asserted on line 5 but not 6
+declare function f<T>(a: T, b: T): boolean;
+function g(gut: { n: 12 | undefined }) {
+    f(gut.n, 12); // error, no common supertype
+    f(12, gut.n); // error, no common supertype
+}
+
+
+//// [noCommonSupertypeTypeInferenceMatchesReporting.js]
+function g(gut) {
+    f(gut.n, 12); // error, no common supertype
+    f(12, gut.n); // error, no common supertype
+}
