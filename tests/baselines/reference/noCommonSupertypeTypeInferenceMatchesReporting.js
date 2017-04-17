806739524e351aa68f1946237502df78e41a6eb3
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-04-17 13:43:21 -0700

    Update test comments

--- tests/baselines/reference/noCommonSupertypeTypeInferenceMatchesReporting.js
+++ tests/baselines/reference/noCommonSupertypeTypeInferenceMatchesReporting.js
@@ -2,13 +2,13 @@
 // Fixes #15116, which asserted on line 5 but not 6
 declare function f<T>(a: T, b: T): boolean;
 function g(gut: { n: 12 | undefined }) {
-    f(gut.n, 12); // error, no common supertype
-    f(12, gut.n); // error, no common supertype
+    f(gut.n, 12); // ok, T = number | undefined
+    f(12, gut.n); // ok, T = number | undefined
 }
 
 
 //// [noCommonSupertypeTypeInferenceMatchesReporting.js]
 function g(gut) {
-    f(gut.n, 12); // error, no common supertype
-    f(12, gut.n); // error, no common supertype
+    f(gut.n, 12); // ok, T = number | undefined
+    f(12, gut.n); // ok, T = number | undefined
 }
