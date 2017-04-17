b7c416cdeb093a6dfbc00ed807d839d849e6c9de
Mohamed Hegazy <mhegazy@microsoft.com>
2017-04-17 16:26:49 -0700

    Check if container has locals before looking up properties

--- src/compiler/binder.ts
+++ src/compiler/binder.ts
@@ -2401,7 +2401,7 @@ namespace ts {
         }
 
         function lookupSymbolForName(name: string) {
-            return (container.symbol && container.symbol.exports && container.symbol.exports.get(name)) || container.locals.get(name);
+            return (container.symbol && container.symbol.exports && container.symbol.exports.get(name)) || (container.locals && container.locals.get(name));
         }
 
         function bindPropertyAssignment(functionName: string, propertyAccessExpression: PropertyAccessExpression, isPrototypeProperty: boolean) {
