502803ae748ce30c90d38f6ef1e4283c5da52fb3
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-04-17 13:51:41 -0700

    Update test too.
    Previous commit mistakenly only updated baselines

--- tests/cases/compiler/noCommonSupertypeTypeInferenceMatchesReporting.ts
+++ tests/cases/compiler/noCommonSupertypeTypeInferenceMatchesReporting.ts
@@ -2,6 +2,6 @@
 // Fixes #15116, which asserted on line 5 but not 6
 declare function f<T>(a: T, b: T): boolean;
 function g(gut: { n: 12 | undefined }) {
-    f(gut.n, 12); // error, no common supertype
-    f(12, gut.n); // error, no common supertype
+    f(gut.n, 12); // ok, T = number | undefined
+    f(12, gut.n); // ok, T = number | undefined
 }
