ab7d5ee8c7465f1ba9f350a168d93f0c276e8897
Herrington Darkholme <nonamesheep1@gmail.com>
2017-04-21 09:38:55 +0800

    add missing methods

--- src/lib/dom.iterable.d.ts
+++ src/lib/dom.iterable.d.ts
@@ -73,6 +73,18 @@ interface NodeListOf<TNode extends Node> {
 
 interface URLSearchParams {
     /**
+     * Returns an array of key, value pairs for every entry in the search params
+     */
+    entries(): IterableIterator<[string, string]>;
+    /**
+     * Returns a list of keys in the search params
+     */
+    keys(): IterableIterator<string>;
+    /**
+     * Returns a list of values in the search params
+     */
+    values(): IterableIterator<string>;
+    /**
      * iterate over key/value pairs
      */
     [Symbol.iterator](): IterableIterator<[string, string]>;
