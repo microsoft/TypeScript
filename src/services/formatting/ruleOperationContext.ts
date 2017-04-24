dcab32875de2a5f33f0b166ae97f84930e1fb5e4
Jason Ramsay <jasonra@microsoft.com>
2017-04-24 15:06:51 -0700

    RulesProvider performance improvements

--- src/services/formatting/ruleOperationContext.ts
+++ src/services/formatting/ruleOperationContext.ts
@@ -4,20 +4,27 @@
 namespace ts.formatting {
 
     export class RuleOperationContext {
-        private customContextChecks: { (context: FormattingContext): boolean; }[];
-
-        constructor(...funcs: { (context: FormattingContext): boolean; }[]) {
-            this.customContextChecks = funcs;
+        constructor(private optionName: string, private checkApplyRuleOperation: { (optionName: string, options: FormatCodeSettings): boolean }, private customContextChecks: { (context: FormattingContext): boolean; }[]) {
         }
 
-        static Any: RuleOperationContext = new RuleOperationContext();
+        static Any: RuleOperationContext = new RuleOperationContext(undefined, undefined, []);
 
+        static Create1(...funcs: { (context: FormattingContext): boolean; }[]) {
+            return new RuleOperationContext(undefined, undefined, funcs);
+        }
+        static Create2(optionName: string, checkApplyRuleOperation: { (optionName: string, options: FormatCodeSettings): boolean }, ...funcs: { (context: FormattingContext): boolean; }[]) {
+            return new RuleOperationContext(optionName, checkApplyRuleOperation, funcs);
+        }
 
         public IsAny(): boolean {
             return this === RuleOperationContext.Any;
         }
 
-        public  InContext(context: FormattingContext): boolean {
+        public InContext(context: FormattingContext): boolean {
+            if (this.checkApplyRuleOperation && !this.checkApplyRuleOperation(this.optionName, context.options)) {
+                return false;
+            }
+
             if (this.IsAny()) {
                 return true;
             }
