a8111692d8426c6fa76dd57b8369be17b124adb5
Michael Maier <maier1michael@gmail.com>
2017-05-04 09:00:00 +0200

    update array filter signatures with type guard

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1240,7 +1240,9 @@ interface Array<T> {
      * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function one time for each element in the array.
      * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
      */
-    filter<S extends T>(callbackfn: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[];
+    filter<S extends T>(callbackfn: (this: void, value: T, index: number, array: T[]) => value is S): S[];
+    filter<S extends T>(callbackfn: (this: void, value: T, index: number, array: T[]) => value is S, thisArg: undefined): S[];
+    filter<S extends T, Z>(callbackfn: (this: Z, value: T, index: number, array: T[]) => value is S, thisArg: Z): S[];
     /**
       * Returns the elements of an array that meet the condition specified in a callback function.
       * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function one time for each element in the array.
