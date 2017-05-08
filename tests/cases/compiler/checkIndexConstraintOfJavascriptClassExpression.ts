5ad2ced0c345141272ea101656d1b0ba45411884
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-05-08 11:03:41 -0700

    Update test with trickier case from #15616

--- tests/cases/compiler/checkIndexConstraintOfJavascriptClassExpression.ts
+++ tests/cases/compiler/checkIndexConstraintOfJavascriptClassExpression.ts
@@ -1,10 +1,18 @@
 // @Filename: weird.js
 // @allowJs: true
+// @checkJs: true
+// @strict: true
+// @noEmit: true
 // @out: foo.js
 someFunction(function(BaseClass) {
+        'use strict';
 	class Hello extends BaseClass {
+                const DEFAULT_MESSAGE = "nop!";
 		constructor() {
 			this.foo = "bar";
 		}
+                _render(error) {
+                    const message = error.message || DEFAULT_MESSAGE;
+                }
 	}
 });
