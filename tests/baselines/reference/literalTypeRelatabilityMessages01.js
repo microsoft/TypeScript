0002d9553bf67fd31710c1f7fe0359a6db4f98b1
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-05-12 01:38:17 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/literalTypeRelatabilityMessages01.js
@@ -0,0 +1,29 @@
+//// [literalTypeRelatabilityMessages01.ts]
+declare let a: false | 0;
+declare let b: true | 1 | "hello";
+
+function nope() {
+    if (Math.random() < 0.5) {
+        a = b;
+    }
+    else {
+        b = a;
+    }
+}
+
+a === b;
+
+b === a;
+
+
+//// [literalTypeRelatabilityMessages01.js]
+function nope() {
+    if (Math.random() < 0.5) {
+        a = b;
+    }
+    else {
+        b = a;
+    }
+}
+a === b;
+b === a;
