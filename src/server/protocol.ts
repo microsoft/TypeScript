ca748d6c02fb7cbd208d6b2ad9aaa43e4b1b8c66
Mohamed Hegazy <mhegazy@microsoft.com>
2017-05-12 14:48:13 -0700

    Add insertSpaceAfterTypeAssertion to the server protocol

--- src/server/protocol.ts
+++ src/server/protocol.ts
@@ -2267,6 +2267,7 @@ namespace ts.server.protocol {
         insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces?: boolean;
         insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces?: boolean;
         insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces?: boolean;
+        insertSpaceAfterTypeAssertion?: boolean;
         insertSpaceBeforeFunctionParenthesis?: boolean;
         placeOpenBraceOnNewLineForFunctions?: boolean;
         placeOpenBraceOnNewLineForControlBlocks?: boolean;
