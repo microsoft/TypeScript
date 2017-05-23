bd422e3a524e1fb8c975e660e6a70cb8446a33a8
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-05-23 16:11:23 -0700

    Add tests and update baselines

---
+++ tests/cases/conformance/salsa/jsDocTypes2.ts
@@ -0,0 +1,23 @@
+﻿// @allowJS: true
+// @suppressOutputPathCheck: true
+
+// @filename: 0.js
+// @ts-check
+/** @type {*} */
+var anyT = 2;
+
+/** @type {?} */
+var anyT1 = 2;
+anyT1 = "hi";
+
+/** @type {Function} */
+const x = (a) => a + 1;
+x(1);
+
+/** @type {function (number)} */
+const x1 = (a) => a + 1;
+x1(0);
+
+/** @type {function (number): number} */
+const x2 = (a) => a + 1;
+x2(0);
