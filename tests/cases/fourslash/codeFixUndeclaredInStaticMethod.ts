6742f9dcd8ddabdfbbdb9df98f6ef65a45ba042d
Arthur Ozga <aozgaa-ms@outlook.com>
2017-06-01 11:17:24 -0700

    remove comment

--- tests/cases/fourslash/codeFixUndeclaredInStaticMethod.ts
+++ tests/cases/fourslash/codeFixUndeclaredInStaticMethod.ts
@@ -24,21 +24,3 @@ verify.rangeIs(`
         throw new Error("Method not implemented.");
     }
 `);
-
-// class A {
-//     static prop2: string;
-//     static prop1: number;
-//     static m2(arg0: any, arg1: any): any {
-//         throw new Error("Method not implemented.");
-//     }
-//     static m1(arg0: any, arg1: any, arg2: any): any {
-//         throw new Error("Method not implemented.");
-//     }
-//     static foo0() {
-//         this.m1(1,2,3);
-//         A.m2(1,2);
-//         this.prop1 = 10;
-//         A.prop2 = "asdf";
-//     }
-// }
-
