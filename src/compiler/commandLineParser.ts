6e49237d31435c9aea8ac6f8e8aa4fdad353eaab
t_ <t-mrt@users.noreply.github.com>
2017-06-02 05:43:44 +0900

    Remove trailing whitespace from tsconfig.json (#16197)
    * Remove trailing whitespace from tsconfig.json
    
    * Simplify

--- src/compiler/commandLineParser.ts
+++ src/compiler/commandLineParser.ts
@@ -1042,7 +1042,7 @@ namespace ts {
             for (let i = 0; i < nameColumn.length; i++) {
                 const optionName = nameColumn[i];
                 const description = descriptionColumn[i];
-                result.push(tab + tab + optionName + makePadding(marginLength - optionName.length + 2) + description);
+                result.push(optionName && `${tab}${tab}${optionName}${ description && (makePadding(marginLength - optionName.length + 2) + description)}`);
             }
             if (configurations.files && configurations.files.length) {
                 result.push(`${tab}},`);
