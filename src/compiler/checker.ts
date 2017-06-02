c2056c0579dbc84f234cc359068e84ab48500518
Yui T <yuisu@microsoft.com>
2017-06-02 16:13:32 -0700

    Address minor error messages

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -16368,7 +16368,7 @@ namespace ts {
             const promiseType = createPromiseType(promisedType);
             if (promiseType === emptyObjectType) {
                 error(func, isImportCall(func) ?
-                    Diagnostics.A_dynamic_import_call_return_a_Promise_Make_sure_you_have_a_declaration_for_Promise_or_include_ES2015_in_your_lib_option :
+                    Diagnostics.A_dynamic_import_call_returns_a_Promise_Make_sure_you_have_a_declaration_for_Promise_or_include_ES2015_in_your_lib_option :
                     Diagnostics.An_async_function_or_method_must_return_a_Promise_Make_sure_you_have_a_declaration_for_Promise_or_include_ES2015_in_your_lib_option);
                 return unknownType;
             }
