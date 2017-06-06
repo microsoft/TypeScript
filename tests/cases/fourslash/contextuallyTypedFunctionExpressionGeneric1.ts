34710a33b5a8f18f1cdc9fa9ec728968e6e16c1e
Anders Hejlsberg <andersh@microsoft.com>
2017-06-06 13:35:29 -0700

    Fix fourslash tests

--- tests/cases/fourslash/contextuallyTypedFunctionExpressionGeneric1.ts
+++ tests/cases/fourslash/contextuallyTypedFunctionExpressionGeneric1.ts
@@ -1,13 +1,13 @@
 /// <reference path='fourslash.ts'/>
 
-interface Comparable<T> {
-   compareTo(other: T): T;
-}
-interface Comparer {
-   <T extends Comparable<T>>(x: T, y: T): T;
-}
+////interface Comparable<T> {
+////   compareTo(other: T): T;
+////}
+////interface Comparer {
+////   <T extends Comparable<T>>(x: T, y: T): T;
+////}
 
-//var max2: Comparer = (x/*1*/x, y/*2*/y) => { return x/*3*/x.compareTo(y/*4*/y) };
+////var max2: Comparer = (x/*1*/x, y/*2*/y) => { return x/*3*/x.compareTo(y/*4*/y) };
 
 verify.quickInfos({
     1: "(parameter) xx: T extends Comparable<T>",
