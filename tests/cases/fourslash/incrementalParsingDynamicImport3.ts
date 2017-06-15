f66ad0345978a0db576b3df7665d85fa02f9089c
Yui T <yuisu@microsoft.com>
2017-06-14 21:41:19 -0700

    Add tests

---
+++ tests/cases/fourslash/incrementalParsingDynamicImport3.ts
@@ -0,0 +1,14 @@
+/// <reference path="fourslash.ts"/>
+
+// @lib: es2015
+
+// @Filename: ./foo.ts
+//// export function bar() { return 1; }
+
+// @Filename: ./0.ts
+//// var x = import/*1*/
+
+verify.numberOfErrorsInCurrentFile(0);
+goTo.marker("1");
+edit.insert("(");
+verify.numberOfErrorsInCurrentFile(2);
