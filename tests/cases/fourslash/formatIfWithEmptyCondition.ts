37d4116ae0e69f31425cbd560e0fdf2ad61d10b6
Arthur Ozga <aozgaa-ms@outlook.com>
2017-06-19 14:22:56 -0700

    rename test

--- tests/cases/fourslash/formatIfWithEmptyCondition.ts
+++ tests/cases/fourslash/formatIfWithEmptyCondition.ts
@@ -1,16 +1,11 @@
 /// <reference path="fourslash.ts"/>
 
-//// if (true) {
-//// }
 //// if () {
 //// }
 
 format.setOption("PlaceOpenBraceOnNewLineForControlBlocks", true);
 format.document();
 verify.currentFileContentIs(
-`if (true)
-{
-}
-if ()
+`if ()
 {
 }`);
