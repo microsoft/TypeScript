f9592b6479ba79a7fa4e05199591c90ad0cda262
Arthur Ozga <aozgaa-ms@outlook.com>
2017-06-22 11:17:38 -0700

    fix and add test

--- tests/cases/fourslash/formatOnEnterOpenBraceRemoveNewLine.ts
+++ tests/cases/fourslash/formatOnEnterOpenBraceRemoveNewLine.ts
@@ -3,12 +3,8 @@
 //// if(true)
 //// /**/
 
-
 format.setOption("PlaceOpenBraceOnNewLineForControlBlocks", false);
 goTo.marker("");
 edit.insert("{");
-// TODO: figure out how to get rid of 3 extra spaces on second last line.
 verify.currentFileContentIs(
     `if (true) {`);
-
-    
