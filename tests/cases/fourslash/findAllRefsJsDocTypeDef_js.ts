b748d1942cac884961c662c64d73d1b2eacf7e4a
Andy Hanson <anhans@microsoft.com>
2017-06-23 12:49:44 -0700

    Fix scope of @typedef references

---
+++ tests/cases/fourslash/findAllRefsJsDocTypeDef_js.ts
@@ -0,0 +1,21 @@
+/// <reference path='fourslash.ts'/>
+
+// Tests that the scope of @typedef is not just the node immediately below it.
+
+// @allowJs: true
+
+// @Filename: /a.js
+/////** @typedef {number} [|{| "isWriteAccess": true, "isDefinition": true |}T|] */
+////
+/////**
+//// * @return {[|T|]}
+//// */
+////function f(obj) { return 0; }
+////
+/////**
+//// * @return {[|T|]}
+//// */
+////function f2(obj) { return 0; }
+
+const [r0, r1, r2] = test.ranges();
+verify.singleReferenceGroup("type T = number");
