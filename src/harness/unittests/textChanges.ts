de0836744c1f038d29e228c5b9c6a7db31856972
t_ <t.us.mrt@gmail.com>
2017-06-29 10:52:03 +0900

    keep whitespace

--- src/harness/unittests/textChanges.ts
+++ src/harness/unittests/textChanges.ts
@@ -100,9 +100,9 @@ namespace ts {
 
         {
             const text = `
-namespace M
+namespace M 
 {
-    namespace M2
+    namespace M2 
     {
         function foo() {
             // comment 1
@@ -550,7 +550,7 @@ const x = 1;`;
         }
         {
             const text = `
-const x = 1,
+const x = 1, 
     y = 2;`;
             runSingleFileTest("insertNodeInListAfter6", noop, text, /*validateNodes*/ false, (sourceFile, changeTracker) => {
                 changeTracker.insertNodeInListAfter(sourceFile, findChild("x", sourceFile), createVariableDeclaration("z", /*type*/ undefined, createLiteral(1)));
@@ -561,7 +561,7 @@ const x = 1,
         }
         {
             const text = `
-const /*x*/ x = 1,
+const /*x*/ x = 1, 
     /*y*/ y = 2;`;
             runSingleFileTest("insertNodeInListAfter8", noop, text, /*validateNodes*/ false, (sourceFile, changeTracker) => {
                 changeTracker.insertNodeInListAfter(sourceFile, findChild("x", sourceFile), createVariableDeclaration("z", /*type*/ undefined, createLiteral(1)));
