67faecc32ce54b10fe102d747226f358a8804ee8
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-06-30 11:52:00 -0700

    remove BOM

--- src/harness/rwcRunner.ts
+++ src/harness/rwcRunner.ts
@@ -1,4 +1,4 @@
-﻿/// <reference path="harness.ts"/>
+/// <reference path="harness.ts"/>
 /// <reference path="runnerbase.ts" />
 /// <reference path="loggedIO.ts" />
 /// <reference path="..\compiler\commandLineParser.ts"/>
