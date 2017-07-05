1ea9972951ad0af8adcd2cd8044dfb76dde13907
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-07-05 16:05:03 -0700

    Add validation to fourslash test

--- tests/cases/fourslash/extract-method1.ts
+++ tests/cases/fourslash/extract-method1.ts
@@ -14,6 +14,20 @@
 
 goTo.select('start', 'end')
 verify.refactorAvailable('Extract Method');
-debug.printCurrentFileState();
 edit.applyRefactor('Extract Method', "scope_0");
-debug.printCurrentFileState();
+verify.currentFileContentIs(
+`class Foo {
+    someMethod(m: number) {
+        this.newFunction(m);
+        var q = 10;
+        return q;
+    }
+
+    private newFunction(m: number) {
+        var x = m;
+        x = x * 3;
+        var y = 30;
+        var z = y + x;
+        console.log(z);
+    }
+}`);
