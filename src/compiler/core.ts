81f8151e3a28e03b1478d608d26b52861c193263
Andy <anhans@microsoft.com>
2017-07-07 10:22:59 -0700

    Use 'push' and 'pop' methods instead of using array.length (#16979)

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -1733,7 +1733,7 @@ namespace ts {
         if (directoryComponents.length > 1 && lastOrUndefined(directoryComponents) === "") {
             // If the directory path given was of type test/cases/ then we really need components of directory to be only till its name
             // that is  ["test", "cases", ""] needs to be actually ["test", "cases"]
-            directoryComponents.length--;
+            directoryComponents.pop();
         }
 
         // Find the component that differs
