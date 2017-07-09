ae533551c29fe4fd9f8dfa214400ff44a85124b9
Filipe Silva <filipematossilva@gmail.com>
2017-07-09 18:50:45 +0100

    Allow visitors to return undefined
    While implementing `ts.Visitor`, it is possible to return `undefined` in order to drop a node. However, the typings do not reflect this and only allow to return `Node | Node []`.
    
    This PR extends the typings to allow `undefined` as well.

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -4350,7 +4350,7 @@ namespace ts {
      */
     export type Visitor = (node: Node) => VisitResult<Node>;
 
-    export type VisitResult<T extends Node> = T | T[];
+    export type VisitResult<T extends Node> = T | T[] | undefined;
 
     export interface Printer {
         /**
