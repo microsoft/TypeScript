48d9012989b2bd4c9b8a73037d7058cdcb538bfc
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-07-13 11:34:56 -0700

    Update baselines

---
+++ tests/baselines/reference/jsdocInTypescript2.js
@@ -0,0 +1,10 @@
+//// [jsdocInTypescript2.ts]
+// parse error (blocks grammar errors from checker)
+function parse1(n: number=) { }
+
+
+//// [jsdocInTypescript2.js]
+// parse error (blocks grammar errors from checker)
+function parse1(n) {
+    if (n === void 0) { n = ; }
+}
