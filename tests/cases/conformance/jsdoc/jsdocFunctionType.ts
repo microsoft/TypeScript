91633cde5f6b06d7e55a3d7d428efac4f2aa48a7
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-07-13 11:33:12 -0700

    Test JSDoc parsing using TS parser

---
+++ tests/cases/conformance/jsdoc/jsdocFunctionType.ts
@@ -0,0 +1,35 @@
+// @allowJs: true
+// @checkJs: true
+// @noEmit: true
+// @noImplicitAny: true
+
+// @Filename: functions.js
+
+/**
+ * @param {function(this: string, number): number} c is just passing on through
+ * @return {function(this: string, number): number}
+ */
+function id1(c) {
+    return c
+}
+
+var x = id1(function (n) { return this.length + n });
+
+/**
+ * @param {function(new: { length: number }, number): number} c is just passing on through
+ * @return {function(new: { length: number }, number): number}
+ */
+function id2(c) {
+    return c
+}
+
+class C {
+    /** @param {number} n */
+    constructor(n) {
+        this.length = n;
+    }
+}
+
+var y = id2(C);
+var z = new y(12);
+z.length;
