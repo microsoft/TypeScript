b94a32b45f21d6eb0d2b53a527c33d83b7018215
John J. Glynn <jjg1914@rit.edu>
2017-07-21 21:12:02 -0400

    Add Path overload for CanvasRenderingContext2D
    https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fill

--- lib/lib.d.ts
+++ lib/lib.d.ts
@@ -6446,6 +6446,7 @@ interface CanvasRenderingContext2D extends Object, CanvasPathMethods {
     drawImage(image: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement | ImageBitmap, dstX: number, dstY: number, dstW: number, dstH: number): void;
     drawImage(image: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement | ImageBitmap, srcX: number, srcY: number, srcW: number, srcH: number, dstX: number, dstY: number, dstW: number, dstH: number): void;
     fill(fillRule?: CanvasFillRule): void;
+    fill(path: Path2D, fillRule?: CanvasFillRule): void;
     fillRect(x: number, y: number, w: number, h: number): void;
     fillText(text: string, x: number, y: number, maxWidth?: number): void;
     getImageData(sx: number, sy: number, sw: number, sh: number): ImageData;
