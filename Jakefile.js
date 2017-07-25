fca698a847296a83a05ff23999b664ca9b52b50d
Wesley Wigham <wewigham@microsoft.com>
2017-07-25 14:12:45 -0700

    Actually let you disable colors with jake

--- Jakefile.js
+++ Jakefile.js
@@ -802,6 +802,13 @@ function deleteTemporaryProjectOutput() {
     }
 }
 
+const boolLookup = {
+    "true": true,
+    "1": true,
+    "false": false,
+    "0": false
+};
+
 function runConsoleTests(defaultReporter, runInParallel) {
     var dirty = process.env.dirty;
     if (!dirty) {
@@ -840,7 +847,7 @@ function runConsoleTests(defaultReporter, runInParallel) {
         testTimeout = 800000;
     }
 
-    var colors = process.env.colors || process.env.color || true;
+    var colors = boolLookup[process.env.colors] !== undefined ? boolLookup[process.env.colors] : boolLookup[process.env.color] !== undefined ? boolLookup[process.env.color] : true;
     var reporter = process.env.reporter || process.env.r || defaultReporter;
     var bail = process.env.bail || process.env.b;
     var lintFlag = process.env.lint !== 'false';
