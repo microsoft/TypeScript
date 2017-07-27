2b2bb171807a866b517538ff6de3cca5b18e7282
Andy Hanson <anhans@microsoft.com>
2017-07-27 11:37:32 -0700

    Make exportSymbol public

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -2905,7 +2905,7 @@ namespace ts {
         /* @internal */ id?: number;            // Unique id (used to look up SymbolLinks)
         /* @internal */ mergeId?: number;       // Merge id (used to look up merged symbol)
         /* @internal */ parent?: Symbol;        // Parent symbol
-        /* @internal */ exportSymbol?: Symbol;  // Exported symbol associated with this symbol
+        exportSymbol?: Symbol;                  // Exported symbol associated with this symbol
         /* @internal */ constEnumOnlyModule?: boolean; // True if module contains only const enums or other modules with only const enums
         /* @internal */ isReferenced?: boolean; // True if the symbol is referenced elsewhere
         /* @internal */ isReplaceableByMethod?: boolean; // Can this Javascript class property be replaced by a method symbol?
