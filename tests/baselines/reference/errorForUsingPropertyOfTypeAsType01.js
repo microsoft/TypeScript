3205ca68c07d5bb770e92839ad4706d4c9e248a3
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-07-27 14:56:04 -0700

    Updated baselines.

--- tests/baselines/reference/errorForUsingPropertyOfTypeAsType01.js
+++ tests/baselines/reference/errorForUsingPropertyOfTypeAsType01.js
@@ -5,6 +5,7 @@ namespace Test1 {
     }
 
     var x: Foo.bar = "";
+    var y: Test1.Foo.bar = "";
 }
 
 namespace Test2 {
@@ -13,6 +14,7 @@ namespace Test2 {
     }
 
     var x: Foo.bar = "";
+    var y: Test2.Foo.bar = "";
 }
 
 namespace Test3 {
@@ -21,6 +23,7 @@ namespace Test3 {
     }
 
     var x: Foo.bar = "";
+    var y: Test3.Foo.bar = "";
 }
 
 namespace Test4 {
@@ -28,6 +31,7 @@ namespace Test4 {
                     | { bar: string }
 
     var x: Foo.bar = "";
+    var y: Test4.Foo.bar = "";
 }
 
 namespace Test5 {
@@ -35,12 +39,16 @@ namespace Test5 {
                     | { wat: string }
 
     var x: Foo.bar = "";
+    var y: Test5.Foo.bar = "";
 }
+
+import lol = Test5.Foo.
 
 //// [errorForUsingPropertyOfTypeAsType01.js]
 var Test1;
 (function (Test1) {
     var x = "";
+    var y = "";
 })(Test1 || (Test1 = {}));
 var Test2;
 (function (Test2) {
@@ -51,16 +59,21 @@ var Test2;
     }());
     Test2.Foo = Foo;
     var x = "";
+    var y = "";
 })(Test2 || (Test2 = {}));
 var Test3;
 (function (Test3) {
     var x = "";
+    var y = "";
 })(Test3 || (Test3 = {}));
 var Test4;
 (function (Test4) {
     var x = "";
+    var y = "";
 })(Test4 || (Test4 = {}));
 var Test5;
 (function (Test5) {
     var x = "";
+    var y = "";
 })(Test5 || (Test5 = {}));
+var lol = Test5.Foo.;
