2ff333c71c2fa880e92b15ab20b3c75e4f0a9cb7
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-07-28 09:21:45 -0700

    Update LKG.

--- lib/tsserverlibrary.d.ts
+++ lib/tsserverlibrary.d.ts
@@ -2340,7 +2340,7 @@ declare namespace ts {
     }
 }
 declare namespace ts {
-    const version = "2.4.1";
+    const version = "2.4.2";
 }
 declare function setTimeout(handler: (...args: any[]) => void, timeout: number): any;
 declare function clearTimeout(handle: any): void;
--- lib/typescriptServices.d.ts
+++ lib/typescriptServices.d.ts
@@ -2594,7 +2594,7 @@ declare namespace ts {
 }
 declare namespace ts {
     /** The version of the TypeScript compiler release */
-    const version = "2.4.1";
+    const version = "2.4.2";
 }
 declare function setTimeout(handler: (...args: any[]) => void, timeout: number): any;
 declare function clearTimeout(handle: any): void;
