da64b3c45fd9d18b6b02543856d00e697690daea
Andy Hanson <anhans@microsoft.com>
2017-07-28 15:46:24 -0700

    Handle setter too

--- tests/cases/fourslash/findAllRefsDestructureGetter.ts
+++ tests/cases/fourslash/findAllRefsDestructureGetter.ts
@@ -2,13 +2,21 @@
 
 ////class Test {
 ////    get [|{| "isDefinition": true, "isWriteAccess": true |}x|]() { return 0; }
+////
+////    set [|{| "isDefinition": true, "isWriteAccess": true |}y|](a: number) {}
 ////}
-////const { [|{| "isDefinition": true, "isWriteAccess": true |}x|] } = new Test();
-////[|x|];
+////const { [|{| "isDefinition": true, "isWriteAccess": true |}x|], [|{| "isDefinition": true, "isWriteAccess": true |}y|] } = new Test();
+////[|x|]; [|y|];
 
-const [r0, r1, r2] = test.ranges();
-verify.referenceGroups(r0, [{ definition: "(property) Test.x: number", ranges: [r0, r1, r2] }]);
-verify.referenceGroups([r1, r2], [
-    { definition: "(property) Test.x: number", ranges: [r0] },
-    { definition: "const x: number", ranges: [r1, r2] }
+const [x0, y0, x1, y1, x2, y2] = test.ranges();
+verify.referenceGroups(x0, [{ definition: "(property) Test.x: number", ranges: [x0, x1, x2] }]);
+verify.referenceGroups([x1, x2], [
+    { definition: "(property) Test.x: number", ranges: [x0] },
+    { definition: "const x: number", ranges: [x1, x2] },
+]);
+
+verify.referenceGroups(y0, [{ definition: "(property) Test.y: number", ranges: [y0, y1, y2] }]);
+verify.referenceGroups([y1, y2], [
+    { definition: "(property) Test.y: number", ranges: [y0] },
+    { definition: "const y: number", ranges: [y1, y2] },
 ]);
