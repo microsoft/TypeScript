053b708bf136295af812a97fb93b1554285d60bf
Tingan Ho <tingan87@gmail.com>
2017-08-02 17:53:46 +0200

    Accept baseline

--- tests/baselines/reference/tryStatements.js
+++ tests/baselines/reference/tryStatements.js
@@ -2,13 +2,20 @@
 function fn() {
     try { } catch { }
 
+    try { } catch { 
+        try { } catch { 
+            try { } catch { }
+        }
+        try { } catch { }
+    }
+
     try { } catch (x) { var x: any; }
 
     try { } finally { }
 
     try { } catch { } finally { }
 
-    try { } catch(z) { } finally { }
+    try { } catch (z) { } finally { }
 }
 
 //// [tryStatements.js]
@@ -16,13 +23,23 @@ function fn() {
     try { }
     catch (_a) { }
     try { }
+    catch (_b) {
+        try { }
+        catch (_c) {
+            try { }
+            catch (_d) { }
+        }
+        try { }
+        catch (_e) { }
+    }
+    try { }
     catch (x) {
         var x;
     }
     try { }
     finally { }
     try { }
-    catch (_b) { }
+    catch (_f) { }
     finally { }
     try { }
     catch (z) { }
