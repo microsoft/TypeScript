0296eef9d3d7b6277f25b303f92d34abaf4a426b
Andy Hanson <anhans@microsoft.com>
2017-08-08 10:25:05 -0700

    Convert two arrays to readonly

--- src/server/session.ts
+++ src/server/session.ts
@@ -259,8 +259,8 @@ namespace ts.server {
         eventHandler?: ProjectServiceEventHandler;
         throttleWaitMilliseconds?: number;
 
-        globalPlugins?: string[];
-        pluginProbeLocations?: string[];
+        globalPlugins?: ReadonlyArray<string>;
+        pluginProbeLocations?: ReadonlyArray<string>;
         allowLocalPluginLoads?: boolean;
     }
 
