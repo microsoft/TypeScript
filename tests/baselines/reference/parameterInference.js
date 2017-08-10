a5d65429836b90057378ab605107fd341fdf99ef
Asad Saeeduddin <masaeedu@gmail.com>
2017-08-10 04:28:58 -0400

    Accept baselines for new test

---
+++ tests/baselines/reference/parameterInference.js
@@ -0,0 +1,45 @@
+//// [parameterInference.ts]
+// CASE 1
+function foo(s) {
+    Math.sqrt(s)
+}
+// => function foo(s: number): void
+
+// CASE 2
+declare function swapNumberString(n: string): number;
+declare function swapNumberString(n: number): string;
+
+function subs(s) {
+  return swapNumberString(s);
+}
+// => function subs(s: string): number
+// NOTE: Still broken, needs to deal with overloads. Should have been inferred as:
+// => (s: string) => number & (s: number) => string
+
+// CASE 3
+function f(x: number){
+   return x;
+}
+
+function g(x){ return f(x); };
+// => function g(x: number): number
+
+
+//// [parameterInference.js]
+// CASE 1
+function foo(s) {
+    Math.sqrt(s);
+}
+function subs(s) {
+    return swapNumberString(s);
+}
+// => function subs(s: string): number
+// NOTE: Still broken, needs to deal with overloads. Should have been inferred as:
+// => (s: string) => number & (s: number) => string
+// CASE 3
+function f(x) {
+    return x;
+}
+function g(x) { return f(x); }
+;
+// => function g(x: number): number
