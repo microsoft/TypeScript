8a1a12485675dfcc5cb15eff79762a9adbcbc90a
Mine Starks <mineyalc@microsoft.com>
2017-08-09 17:33:25 -0700

    session.ts, services.ts, types.ts: Add formattingOptions parameter to getCompletionEntryDetails

--- src/server/session.ts
+++ src/server/session.ts
@@ -1201,9 +1201,10 @@ namespace ts.server {
             const { file, project } = this.getFileAndProject(args);
             const scriptInfo = project.getScriptInfoForNormalizedPath(file);
             const position = this.getPosition(args, scriptInfo);
-            
+            const formattingOptions = project.projectService.getFormatCodeOptions(file);
+
             return mapDefined(args.entryNames, entryName => {
-                const details = project.getLanguageService().getCompletionEntryDetails(file, position, entryName);
+                const details = project.getLanguageService().getCompletionEntryDetails(file, position, entryName, formattingOptions);
                 if (details) {
                     const mappedCodeActions = map(details.codeActions, action => this.mapCodeAction(action, scriptInfo));
                     return { ...details, codeActions: mappedCodeActions };
--- src/services/services.ts
+++ src/services/services.ts
@@ -1373,8 +1373,9 @@ namespace ts {
             return Completions.getCompletionsAtPosition(host, program.getTypeChecker(), log, program.getCompilerOptions(), getValidSourceFile(fileName), position);
         }
 
-        function getCompletionEntryDetails(fileName: string, position: number, entryName: string): CompletionEntryDetails {
+        function getCompletionEntryDetails(fileName: string, position: number, entryName: string, formattingOptions?: FormatCodeSettings): CompletionEntryDetails {
             synchronizeHostData();
+            formattingOptions;
             return Completions.getCompletionEntryDetails(program.getTypeChecker(), log, program.getCompilerOptions(), getValidSourceFile(fileName), position, entryName);
         }
 
