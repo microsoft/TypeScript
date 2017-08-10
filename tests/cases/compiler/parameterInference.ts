9bec8c16bc9deabbf8f488833c923c2c20896849
Asad Saeeduddin <masaeedu@gmail.com>
2017-08-10 03:57:20 -0400

    Update testcases

--- tests/cases/compiler/parameterInference.ts
+++ tests/cases/compiler/parameterInference.ts
@@ -2,21 +2,23 @@
 function foo(s) {
     Math.sqrt(s)
 }
+// => function foo(s: number): void
 
 // CASE 2
 declare function swapNumberString(n: string): number;
 declare function swapNumberString(n: number): string;
 
-// Should have identical signature to swapNumberString
 function subs(s) {
   return swapNumberString(s);
 }
+// => function subs(s: string): number
+// NOTE: Still broken, needs to deal with overloads. Should have been inferred as:
+// => (s: string) => number & (s: number) => string
 
 // CASE 3
 function f(x: number){
    return x;
 }
 
-function g(x){ return x};
-
-function h(x){ return f(x); };
+function g(x){ return f(x); };
+// => function g(x: number): number
