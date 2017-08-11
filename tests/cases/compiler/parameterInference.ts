aff19fc5df58066e15fcee179204c3f3928315d2
Asad Saeeduddin <masaeedu@gmail.com>
2017-08-11 12:49:34 -0400

    Add comments to describe test cases

--- tests/cases/compiler/parameterInference.ts
+++ tests/cases/compiler/parameterInference.ts
@@ -1,10 +1,10 @@
-// CASE 1
+// CASE 1 - Propagation of parameter types from invocations
 function foo(s) {
     Math.sqrt(s)
 }
 // => function foo(s: number): void
 
-// CASE 2
+// CASE 2 - Inference from invocation of overloaded functions
 declare function swapNumberString(n: string): number;
 declare function swapNumberString(n: number): string;
 
@@ -15,7 +15,7 @@ function subs(s) {
 // NOTE: Still broken, needs to deal with overloads. Should have been inferred as:
 // => (s: string) => number & (s: number) => string
 
-// CASE 3
+// CASE 3 - Propagation of explicitly annotated parameter types
 function f3(x: number){
    return x;
 }
@@ -23,7 +23,7 @@ function f3(x: number){
 function g3(x){ return f3(x); };
 // => function g3(x: number): number
 
-// CASE 4
+// CASE 4 - Inference from invocation sites within function expressions passed as arguments 
 declare function f4(g: Function)
 function g4(x) {
   f4(() => {
