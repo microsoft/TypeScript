9bb191585123618e0319716b9ea51c7dd49a2f4a
Ron Buckton <ron.buckton@microsoft.com>
2017-08-11 16:15:28 -0700

    PR feedback

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -16344,7 +16344,7 @@ namespace ts {
         }
 
         function getJavaScriptClassType(symbol: Symbol): Type | undefined {
-            if (symbol && isDeclarationOfFunctionOrClassExpression(symbol)) {
+            if (isDeclarationOfFunctionOrClassExpression(symbol)) {
                 symbol = getSymbolOfNode((<VariableDeclaration>symbol.valueDeclaration).initializer);
             }
             if (isJavaScriptConstructor(symbol.valueDeclaration)) {
