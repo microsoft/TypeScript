b8e0dedac03c0049e7e8500a93f99b8f383c75d4
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-08-17 12:40:10 -0700

    Fix #17069 and #15371
    1. `T[K]` now correctly produces `number` when
       `K extends string, T extends Record<K, number>`.
    2. `T[K]` no longer allows any type to be assigned to it when
       `T extends object, K extends keyof T`.
    
    Previously both of these cases failed in
    getConstraintOfIndexedAccessType because both bases followed `K`'s base
    constraint to `string` and then incorrectly produced `any` for types
    (like `object`) with no string index signature. In (1), this produced an
    error in checkBinaryLikeExpression`. In (2), this failed to produce an
    error in `checkTypeRelatedTo`.

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -5912,7 +5912,13 @@ namespace ts {
                 return transformed;
             }
             const baseObjectType = getBaseConstraintOfType(type.objectType);
-            const baseIndexType = getBaseConstraintOfType(type.indexType);
+            const keepTypeParameterForMappedType = baseObjectType && getObjectFlags(baseObjectType) & ObjectFlags.Mapped && type.indexType.flags & TypeFlags.TypeParameter;
+            const baseIndexType = !keepTypeParameterForMappedType && getBaseConstraintOfType(type.indexType);
+            if (baseObjectType && baseIndexType === stringType && !getIndexInfoOfType(baseObjectType, IndexKind.String)) {
+                // getIndexedAccessType returns `any` for X[string] where X doesn't have an index signature.
+                // instead, return undefined so that the indexed access check fails
+                return undefined;
+            }
             return baseObjectType || baseIndexType ? getIndexedAccessType(baseObjectType || type.objectType, baseIndexType || type.indexType) : undefined;
         }
 
@@ -5962,8 +5968,9 @@ namespace ts {
             function computeBaseConstraint(t: Type): Type {
                 if (t.flags & TypeFlags.TypeParameter) {
                     const constraint = getConstraintFromTypeParameter(<TypeParameter>t);
-                    return (<TypeParameter>t).isThisType ? constraint :
-                        constraint ? getBaseConstraint(constraint) : undefined;
+                    return ((t as TypeParameter).isThisType || !constraint || getObjectFlags(constraint) & ObjectFlags.Mapped) ?
+                        constraint :
+                        getBaseConstraint(constraint);
                 }
                 if (t.flags & TypeFlags.UnionOrIntersection) {
                     const types = (<UnionOrIntersectionType>t).types;
@@ -9290,7 +9297,7 @@ namespace ts {
                 }
                 else if (target.flags & TypeFlags.IndexedAccess) {
                     // A type S is related to a type T[K] if S is related to A[K], where K is string-like and
-                    // A is the apparent type of S.
+                    // A is the apparent type of T.
                     const constraint = getConstraintOfType(<IndexedAccessType>target);
                     if (constraint) {
                         if (result = isRelatedTo(source, constraint, reportErrors)) {
