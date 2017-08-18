345622d22d3466bba17a6a33454e6bde63ef6431
Henry Mercer <henry.mercer@me.com>
2017-08-18 23:53:20 +0100

    Add test that demonstrates bug

---
+++ tests/baselines/reference/propertyAccessOnEmptyObjectLiteral.js
@@ -0,0 +1,12 @@
+//// [propertyAccessOnEmptyObjectLiteral.ts]
+class A { }
+
+(<A>{}).toString();
+
+//// [propertyAccessOnEmptyObjectLiteral.js]
+var A = /** @class */ (function () {
+    function A() {
+    }
+    return A;
+}());
+({}).toString();
---
+++ tests/cases/compiler/propertyAccessOnEmptyObjectLiteral.ts
@@ -0,0 +1,3 @@
+class A { }
+
+(<A>{}).toString();
