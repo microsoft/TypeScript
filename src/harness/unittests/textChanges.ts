f2144490833a49e8657e13967957d9dedc0faf69
andy-ms <anhans@microsoft.com>
2017-08-18 17:25:33 -0700

    Rename lint rules

--- src/harness/unittests/textChanges.ts
+++ src/harness/unittests/textChanges.ts
@@ -100,9 +100,9 @@ namespace ts {
 
         {
             const text = `
-namespace M 
+namespace M
 {
-    namespace M2 
+    namespace M2
     {
         function foo() {
             // comment 1
@@ -130,7 +130,7 @@ namespace M
                         /*typeParameters*/ undefined,
                         /*parameters*/ emptyArray,
                         /*type*/ createKeywordTypeNode(SyntaxKind.AnyKeyword),
-                        /*body */ createBlock(statements)
+                        /*body*/ createBlock(statements)
                 );
 
                 changeTracker.insertNodeBefore(sourceFile, /*before*/findChild("M2", sourceFile), newFunction, { suffix: newLineCharacter });
@@ -550,7 +550,7 @@ const x = 1;`;
         }
         {
             const text = `
-const x = 1, 
+const x = 1,
     y = 2;`;
             runSingleFileTest("insertNodeInListAfter6", noop, text, /*validateNodes*/ false, (sourceFile, changeTracker) => {
                 changeTracker.insertNodeInListAfter(sourceFile, findChild("x", sourceFile), createVariableDeclaration("z", /*type*/ undefined, createLiteral(1)));
@@ -561,7 +561,7 @@ const x = 1,
         }
         {
             const text = `
-const /*x*/ x = 1, 
+const /*x*/ x = 1,
     /*y*/ y = 2;`;
             runSingleFileTest("insertNodeInListAfter8", noop, text, /*validateNodes*/ false, (sourceFile, changeTracker) => {
                 changeTracker.insertNodeInListAfter(sourceFile, findChild("x", sourceFile), createVariableDeclaration("z", /*type*/ undefined, createLiteral(1)));
