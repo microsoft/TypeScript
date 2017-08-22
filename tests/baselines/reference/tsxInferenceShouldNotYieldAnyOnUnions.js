308ac714aa8870e3e2c9fe5e9a749f0ed7b24bcd
Wesley Wigham <wewigham@microsoft.com>
2017-08-22 13:43:33 -0700

    Rename type

--- tests/baselines/reference/tsxInferenceShouldNotYieldAnyOnUnions.js
+++ tests/baselines/reference/tsxInferenceShouldNotYieldAnyOnUnions.js
@@ -13,28 +13,28 @@ interface PropsWithConvert<T> extends PropsBase<T> {
     convert: (t: T) => string;
 }
 
-function InferenceShouldNotProduceAny<T>(props: Props<T>): JSX.Element {
+function ShouldInferFromData<T>(props: Props<T>): JSX.Element {
     return <div />;
 }
 
 // Sanity check: function call equivalent versions work fine
-InferenceShouldNotProduceAny({ data: "1" });
-InferenceShouldNotProduceAny({ data: "1", convert: n => "" + n });
-InferenceShouldNotProduceAny({ data: 2, convert: n => "" + n });
+ShouldInferFromData({ data: "1" });
+ShouldInferFromData({ data: "1", convert: n => "" + n });
+ShouldInferFromData({ data: 2, convert: n => "" + n });
 
 
-const f1 = <InferenceShouldNotProduceAny data={"1"} />;
-const f2 = <InferenceShouldNotProduceAny data={"1"} convert={n => "" + n} />;
-const f3 = <InferenceShouldNotProduceAny data={2} convert={n => "" + n} />;
+const f1 = <ShouldInferFromData data={"1"} />;
+const f2 = <ShouldInferFromData data={"1"} convert={n => "" + n} />;
+const f3 = <ShouldInferFromData data={2} convert={n => "" + n} />;
 
 //// [index.jsx]
-function InferenceShouldNotProduceAny(props) {
+function ShouldInferFromData(props) {
     return <div />;
 }
 // Sanity check: function call equivalent versions work fine
-InferenceShouldNotProduceAny({ data: "1" });
-InferenceShouldNotProduceAny({ data: "1", convert: function (n) { return "" + n; } });
-InferenceShouldNotProduceAny({ data: 2, convert: function (n) { return "" + n; } });
-var f1 = <InferenceShouldNotProduceAny data={"1"}/>;
-var f2 = <InferenceShouldNotProduceAny data={"1"} convert={function (n) { return "" + n; }}/>;
-var f3 = <InferenceShouldNotProduceAny data={2} convert={function (n) { return "" + n; }}/>;
+ShouldInferFromData({ data: "1" });
+ShouldInferFromData({ data: "1", convert: function (n) { return "" + n; } });
+ShouldInferFromData({ data: 2, convert: function (n) { return "" + n; } });
+var f1 = <ShouldInferFromData data={"1"}/>;
+var f2 = <ShouldInferFromData data={"1"} convert={function (n) { return "" + n; }}/>;
+var f3 = <ShouldInferFromData data={2} convert={function (n) { return "" + n; }}/>;
--- tests/cases/compiler/tsxInferenceShouldNotYieldAnyOnUnions.tsx
+++ tests/cases/compiler/tsxInferenceShouldNotYieldAnyOnUnions.tsx
@@ -14,16 +14,16 @@ interface PropsWithConvert<T> extends PropsBase<T> {
     convert: (t: T) => string;
 }
 
-function InferenceShouldNotProduceAny<T>(props: Props<T>): JSX.Element {
+function ShouldInferFromData<T>(props: Props<T>): JSX.Element {
     return <div />;
 }
 
 // Sanity check: function call equivalent versions work fine
-InferenceShouldNotProduceAny({ data: "1" });
-InferenceShouldNotProduceAny({ data: "1", convert: n => "" + n });
-InferenceShouldNotProduceAny({ data: 2, convert: n => "" + n });
+ShouldInferFromData({ data: "1" });
+ShouldInferFromData({ data: "1", convert: n => "" + n });
+ShouldInferFromData({ data: 2, convert: n => "" + n });
 
 
-const f1 = <InferenceShouldNotProduceAny data={"1"} />;
-const f2 = <InferenceShouldNotProduceAny data={"1"} convert={n => "" + n} />;
-const f3 = <InferenceShouldNotProduceAny data={2} convert={n => "" + n} />;
+const f1 = <ShouldInferFromData data={"1"} />;
+const f2 = <ShouldInferFromData data={"1"} convert={n => "" + n} />;
+const f3 = <ShouldInferFromData data={2} convert={n => "" + n} />;
