a39ae1fab712d321ffdc50ee02089fad5091f9bf
Wesley Wigham <wwigham@gmail.com>
2017-08-24 23:58:20 -0700

    Fix crash when attempting to merge an import with a local declaration (#18032) (#18034)
    * There should be no crash when attempting to merge an import with a local declaration
    
    * Show symbol has actually merged within the module

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -19126,6 +19126,8 @@ namespace ts {
                             : DeclarationSpaces.ExportNamespace;
                     case SyntaxKind.ClassDeclaration:
                     case SyntaxKind.EnumDeclaration:
+                    // A NamespaceImport declares an Alias, which is allowed to merge with other values within the module
+                    case SyntaxKind.NamespaceImport:
                         return DeclarationSpaces.ExportType | DeclarationSpaces.ExportValue;
                     case SyntaxKind.ImportEqualsDeclaration:
                         let result = DeclarationSpaces.None;
