5e91bbdc3c16171c4b89cdda19a4b96844540cdd
Wesley Wigham <wewigham@microsoft.com>
2017-09-05 13:04:38 -0700

    Corrected test

--- tests/cases/compiler/jsdocTypecastNoTypeNoCrash.ts
+++ tests/cases/compiler/jsdocTypecastNoTypeNoCrash.ts
@@ -2,11 +2,4 @@
 // @outDir: ./out
 // @filename: index.js
 function Foo() {}
-const x = /* @type */(Foo);
-/* @type */(function() {})();
-
-const y = /* @type {} */(Foo);
-/* @type {} */(function() {})();
-
-const z = /* @type { */(Foo);
-/* @type { */(function() {})();
+const a = /* @type string */(Foo);
