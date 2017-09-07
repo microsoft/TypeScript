fb5e8c611083c92f9744847957d2014d65025e6b
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-09-07 16:37:13 -0700

    Fix forEachChild's visit of JSDocTypedefTag
    Also remove JSDocTypeLiteral.jsdocTypeTag, which made no sense since it
    was only useful when storing information for its parent `@typedef` tag.

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -2211,7 +2211,6 @@ namespace ts {
     export interface JSDocTypeLiteral extends JSDocType {
         kind: SyntaxKind.JSDocTypeLiteral;
         jsDocPropertyTags?: ReadonlyArray<JSDocPropertyLikeTag>;
-        jsDocTypeTag?: JSDocTypeTag;
         /** If true, then this type literal represents an *array* of its type. */
         isArrayType?: boolean;
     }
