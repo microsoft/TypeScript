5afbbcfebd8d58dd777862751db3fa3db6d2ddaa
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-09-21 10:37:53 -0700

    Add more tests for convert-jsdoc-to-ts-types

---
+++ tests/cases/fourslash/convertJSDocToTypes6.ts
@@ -0,0 +1,15 @@
+/// <reference path='fourslash.ts' />
+
+////declare class C {
+////    /** @type {number | null} */
+////    /*1*/p;
+////}
+
+// NOTE: The duplicated comment is unintentional but needs a serious fix in trivia handling
+verify.applicableRefactorAvailableAtMarker('1');
+verify.fileAfterApplyingRefactorAtMarker('1',
+`declare class C {
+    /** @type {number | null} */
+    /** @type {number | null} */
+    p: number | null;
+}`, 'Convert to Typescript type', 'convert');
