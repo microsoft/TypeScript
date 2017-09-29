95d405216bcf4a9f97a06dd80bead89032816b5d
Andy Hanson <anhans@microsoft.com>
2017-09-29 14:39:02 -0700

    Add test for #17217

---
+++ tests/cases/fourslash/findAllRefsJsDocParam.ts
@@ -0,0 +1,6 @@
+/// <reference path='fourslash.ts' />
+
+/////** @param [|s|] */
+////const x = [|{| "isWriteAccess": true, "isDefinition": true |}s|] => [|s|];
+
+verify.singleReferenceGroup("(parameter) s: any");
