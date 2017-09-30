a23d1bf3ea14a42aa8bac5268881b57fddaf8c8f
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-09-30 00:06:20 -0700

    Updated helper for marginally better minification.

--- src/compiler/transformers/es2015.ts
+++ src/compiler/transformers/es2015.ts
@@ -4093,9 +4093,11 @@ namespace ts {
         text: `
             var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
                 if (Object.defineProperty) {
-                    return Object.defineProperty(cooked, "raw", { value: raw });
+                    Object.defineProperty(cooked, "raw", { value: raw });
                 }
-                cooked.raw = raw;
+                else {
+                    cooked.raw = raw;
+                }
                 return cooked;
             };`
     };
