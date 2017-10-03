babe3cb9311ee423f4b8421b06f30380ca22c922
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-10-03 00:35:29 -0700

    Flatten the '__makeTemplateObject' helper to use less vertical screen real estate.

--- src/compiler/transformers/es2015.ts
+++ src/compiler/transformers/es2015.ts
@@ -4092,12 +4092,7 @@ namespace ts {
         priority: 0,
         text: `
             var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-                if (Object.defineProperty) {
-                    Object.defineProperty(cooked, "raw", { value: raw });
-                }
-                else {
-                    cooked.raw = raw;
-                }
+                if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
                 return cooked;
             };`
     };
