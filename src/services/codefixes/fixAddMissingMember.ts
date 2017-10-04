4cf289e1a57b7ff32d73efc7d9b36b932df926ec
Andrew Casey <andrew.casey@microsoft.com>
2017-10-04 11:26:41 -0700

    Fix whitespace around inserted property initializer
    Fixes #18741

--- src/services/codefixes/fixAddMissingMember.ts
+++ src/services/codefixes/fixAddMissingMember.ts
@@ -112,11 +112,11 @@ namespace ts.codefix {
                     createIdentifier("undefined")));
 
                 const propertyInitializationChangeTracker = textChanges.ChangeTracker.fromContext(context);
-                propertyInitializationChangeTracker.insertNodeAt(
+                propertyInitializationChangeTracker.insertNodeBefore(
                     classDeclarationSourceFile,
-                    classConstructor.body.getEnd() - 1,
+                    classConstructor.body.getLastToken(),
                     propertyInitialization,
-                    { prefix: context.newLineCharacter, suffix: context.newLineCharacter });
+                    { suffix: context.newLineCharacter });
 
                 const initializeAction = {
                     description: formatStringFromArgs(getLocaleSpecificMessage(Diagnostics.Initialize_property_0_in_the_constructor), [tokenName]),
