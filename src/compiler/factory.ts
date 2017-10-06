5c9f8c56d95eee6bc897c8dfb64c398bf0e273fd
Andrew Casey <andrew.casey@microsoft.com>
2017-10-06 10:20:12 -0700

    Mark getSynthesizedDeepClone @internal

--- src/compiler/factory.ts
+++ src/compiler/factory.ts
@@ -74,6 +74,7 @@ namespace ts {
     /**
      * Creates a deep, memberwise clone of a node with no source map location.
      */
+    /* @internal */
     export function getSynthesizedDeepClone<T extends Node>(node: T | undefined): T | undefined {
         return node
             ? getSynthesizedClone(visitEachChild(node, child => getSynthesizedDeepClone(child), nullTransformationContext))
