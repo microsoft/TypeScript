dca6e33ac7ba655af0d27a9d2e9555424c9cc67e
Ron Buckton <ron.buckton@microsoft.com>
2017-10-10 10:03:18 -0700

    baseline updates

--- tests/baselines/reference/importCallExpressionInExportEqualsCJS.js
+++ tests/baselines/reference/importCallExpressionInExportEqualsCJS.js
@@ -14,5 +14,5 @@ module.exports = 42;
 //// [index.js]
 "use strict";
 module.exports = async function () {
-    const something = await Promise.resolve().then(function () { return require("./something"); });
+    const something = await Promise.resolve().then(() => require("./something"));
 };
