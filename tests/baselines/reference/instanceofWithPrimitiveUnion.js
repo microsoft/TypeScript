36df62dc2aab94820f52413c4c6e2763dd91fff2
Charles Pierce <cpierce.grad@gmail.com>
2017-10-09 22:31:18 -0700

    Error when LHS of instanceof is Union of Primitives #18519

---
+++ tests/baselines/reference/instanceofWithPrimitiveUnion.js
@@ -0,0 +1,14 @@
+//// [instanceofWithPrimitiveUnion.ts]
+function foo(x: number | string) {
+    if (x instanceof Object) {
+        x;
+    }
+}
+
+
+//// [instanceofWithPrimitiveUnion.js]
+function foo(x) {
+    if (x instanceof Object) {
+        x;
+    }
+}
---
+++ tests/cases/compiler/instanceofWithPrimitiveUnion.ts
@@ -0,0 +1,5 @@
+function foo(x: number | string) {
+    if (x instanceof Object) {
+        x;
+    }
+}
