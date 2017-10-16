9563246993fdc4d323db60179a8bfd84a943cd70
Ron Buckton <ron.buckton@microsoft.com>
2017-10-16 14:26:16 -0700

    Do not reduce subtypes of awaited union type

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -19680,7 +19680,7 @@ namespace ts {
                     return undefined;
                 }
 
-                return typeAsAwaitable.awaitedTypeOfType = getUnionType(types, /*subtypeReduction*/ true);
+                return typeAsAwaitable.awaitedTypeOfType = getUnionType(types);
             }
 
             const promisedType = getPromisedTypeOfPromise(type);
