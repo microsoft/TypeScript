7a2fe5a0a7196d38840d9eaaebb789d4fbd0ea2a
Wesley Wigham <wewigham@microsoft.com>
2017-10-18 13:18:07 -0700

    make getCurrentDirectory required

--- tests/baselines/reference/api/typescript.d.ts
+++ tests/baselines/reference/api/typescript.d.ts
@@ -3883,6 +3883,7 @@ declare namespace ts {
         getScriptSnapshot(fileName: string): IScriptSnapshot | undefined;
         getLocalizedDiagnosticMessages?(): any;
         getCancellationToken?(): HostCancellationToken;
+        getCurrentDirectory(): string;
         getDefaultLibFileName(options: CompilerOptions): string;
         log?(s: string): void;
         trace?(s: string): void;
