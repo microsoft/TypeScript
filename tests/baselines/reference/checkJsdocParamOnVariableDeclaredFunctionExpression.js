0fedfc5c67203405a11970c997550830dc492573
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-10-19 09:12:49 -0700

    Test:jsdoc @param errors on vardecls/assignments

---
+++ tests/baselines/reference/checkJsdocParamOnVariableDeclaredFunctionExpression.js
@@ -0,0 +1,26 @@
+//// [0.js]
+// @ts-check
+/**
+ * @param {number=} n
+ * @param {string} [s]
+ */
+var x = function foo(n, s) {}
+var y;
+/**
+ * @param {boolean!} b
+ */
+y = function bar(b) {}
+
+
+//// [0.js]
+// @ts-check
+/**
+ * @param {number=} n
+ * @param {string} [s]
+ */
+var x = function foo(n, s) { };
+var y;
+/**
+ * @param {boolean!} b
+ */
+y = function bar(b) { };
