8a292e35147e287cb18057689923295d79391dea
Mohamed Hegazy <mhegazy@microsoft.com>
2017-10-19 15:37:38 -0700

    Update version (#19353)
    * Update version
    
    * Accept baselines after version change

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -2672,7 +2672,7 @@ declare namespace ts {
     }
 }
 declare namespace ts {
-    const versionMajorMinor = "2.6";
+    const versionMajorMinor = "2.7";
     /** The version of the TypeScript compiler release */
     const version: string;
 }
