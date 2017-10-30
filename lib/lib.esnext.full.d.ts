6f1cdf3887cde4403b45343549a3d325b33e7a5a
Wesley Wigham <wewigham@microsoft.com>
2017-10-30 15:26:03 -0700

    Remove duplicate type

--- lib/lib.esnext.full.d.ts
+++ lib/lib.esnext.full.d.ts
@@ -21,9 +21,6 @@ and limitations under the License.
 /// <reference path="lib.es2017.d.ts" />
 /// <reference path="lib.esnext.asynciterable.d.ts" />
 
-
-declare type PropertyKey = string | number | symbol;
-
 interface Array<T> {
     /**
      * Returns the value of the first element in the array where predicate is true, and undefined
