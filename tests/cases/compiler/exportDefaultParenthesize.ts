899f719025705d817cff378e9e7b2190a196ebbf
Wesley Wigham <wewigham@microsoft.com>
2017-10-30 17:14:59 -0700

    Handle commalist and comma cases

--- tests/cases/compiler/exportDefaultParenthesize.ts
+++ tests/cases/compiler/exportDefaultParenthesize.ts
@@ -2,6 +2,33 @@
 // @module: esnext
 // @filename: commalist.ts
 export default {
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+    ['foo'+'']: 42,
+};
+
+// @filename: comma.ts
+export default {
     ['foo']: 42
 };
 
