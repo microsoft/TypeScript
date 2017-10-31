d1902348ee88e79df8280a90586cd092737acb58
Wesley Wigham <wewigham@microsoft.com>
2017-10-30 17:07:07 -0700

    Revert 6f1cdf3

--- lib/lib.esnext.full.d.ts
+++ lib/lib.esnext.full.d.ts
@@ -21,6 +21,9 @@ and limitations under the License.
 /// <reference path="lib.es2017.d.ts" />
 /// <reference path="lib.esnext.asynciterable.d.ts" />
 
+
+declare type PropertyKey = string | number | symbol;
+
 interface Array<T> {
     /**
      * Returns the value of the first element in the array where predicate is true, and undefined
