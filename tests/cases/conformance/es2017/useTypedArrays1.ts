b3a625703318a90e96605d51671e89d709308985
Eugene Timokhov <timocov@gmail.com>
2017-11-04 11:08:00 +0300

    Added empty constructors to TypedArrays from es2017 (#19680)

---
+++ tests/cases/conformance/es2017/useTypedArrays1.ts
@@ -0,0 +1,12 @@
+// @target: es5
+// @lib: es5,es2017.typedarrays
+
+var int8Array = new Int8Array();
+var uint8Array = new Uint8Array();
+var uint8ClampedArray = new Uint8ClampedArray();
+var int16Array = new Int16Array();
+var uint16Array = new Uint16Array();
+var int32Array = new Int32Array();
+var uint32Array = new Uint32Array();
+var float32Array = new Float32Array();
+var float64Array = new Float64Array();
