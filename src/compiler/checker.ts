76d4a6e64e88cce7ceed428f9cedcd3275c63c39
Andy Hanson <anhans@microsoft.com>
2017-11-07 07:29:23 -0800

    Fix undefined error for diagnostic for instantiating an abstract class

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -16725,7 +16725,13 @@ namespace ts {
                 // only the class declaration node will have the Abstract flag set.
                 const valueDecl = expressionType.symbol && getClassLikeDeclarationOfSymbol(expressionType.symbol);
                 if (valueDecl && hasModifier(valueDecl, ModifierFlags.Abstract)) {
-                    error(node, Diagnostics.Cannot_create_an_instance_of_the_abstract_class_0, declarationNameToString(getNameOfDeclaration(valueDecl)));
+                    const name = getNameOfDeclaration(valueDecl);
+                    if (name) {
+                        error(node, Diagnostics.Cannot_create_an_instance_of_the_abstract_class_0, declarationNameToString(name));
+                    }
+                    else {
+                        error(node, Diagnostics.Cannot_create_an_instance_of_an_abstract_class);
+                    }
                     return resolveErrorCall(node);
                 }
 
