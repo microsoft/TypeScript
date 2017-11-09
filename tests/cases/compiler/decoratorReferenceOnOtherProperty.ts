80f68776334766679b5aba2835db9e084e44125a
Wesley Wigham <wewigham@microsoft.com>
2017-11-08 18:44:19 -0800

    Add restarg test

--- tests/cases/compiler/decoratorReferenceOnOtherProperty.ts
+++ tests/cases/compiler/decoratorReferenceOnOtherProperty.ts
@@ -13,3 +13,13 @@ class Bar {
   yoha(@foo yoha, bar: Yoha) {}
   //                   ^^^^
 }
+
+// @filename: index2.ts
+import {Yoha} from './yoha';
+
+function foo(...args: any[]) {}
+
+class Bar {
+  yoha(@foo yoha, ...bar: Yoha[]) {}
+  //                      ^^^^
+}
