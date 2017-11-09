235356e6ffb9d9ad365758ab46ee245ec6df08d3
Wesley Wigham <wwigham@gmail.com>
2017-11-08 18:15:23 -0800

    Handle synthetic nodes correctly as namespace identifiers in system transform (#19623)
    * Handle synthetic nodes correctly as namespace identifiers in system transform
    
    * Add ref to issue in comment
    
    * Lock newline for ci

--- src/harness/unittests/transform.ts
+++ src/harness/unittests/transform.ts
@@ -192,6 +192,38 @@ namespace ts {
                 };
             }
         });
+
+        // https://github.com/Microsoft/TypeScript/issues/19618
+        testBaseline("transformAddImportStar", () => {
+            return ts.transpileModule("", {
+                transformers: {
+                    before: [transformAddImportStar],
+                },
+                compilerOptions: {
+                    target: ts.ScriptTarget.ES5,
+                    module: ts.ModuleKind.System,
+                    newLine: NewLineKind.CarriageReturnLineFeed,
+                }
+            }).outputText;
+
+            function transformAddImportStar(_context: ts.TransformationContext) {
+                return (sourceFile: ts.SourceFile): ts.SourceFile => {
+                    return visitNode(sourceFile);
+                };
+                function visitNode(sf: ts.SourceFile) {
+                    // produce `import * as i0 from './comp';
+                    const importStar = ts.createImportDeclaration(
+                        /*decorators*/ undefined,
+                        /*modifiers*/ undefined,
+                        /*importClause*/ ts.createImportClause(
+                            /*name*/ undefined,
+                            ts.createNamespaceImport(ts.createIdentifier("i0"))
+                        ),
+                        /*moduleSpecifier*/ ts.createLiteral("./comp1"));
+                    return ts.updateSourceFileNode(sf, [importStar]);
+                }
+            }
+        });
     });
 }
 
