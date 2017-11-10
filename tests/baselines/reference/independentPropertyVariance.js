d15926d9c7bdef8efc4c671914777304695e316f
Anders Hejlsberg <andersh@microsoft.com>
2017-11-10 13:13:32 -0800

    Add test

---
+++ tests/baselines/reference/independentPropertyVariance.js
@@ -0,0 +1,13 @@
+//// [independentPropertyVariance.ts]
+// Verify that properties can vary idependently in comparable relationship
+
+declare const x: { a: 1, b: string };
+declare const y: { a: number, b: 'a' };
+
+x === y;
+
+
+//// [independentPropertyVariance.js]
+"use strict";
+// Verify that properties can vary idependently in comparable relationship
+x === y;
