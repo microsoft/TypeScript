05c05258c3cfd8047482ddc462e7decdd75c25e2
Andy Hanson <anhans@microsoft.com>
2017-11-10 12:54:58 -0800

    Support arbitrary prototype property assignments in navigation bar

--- src/services/utilities.ts
+++ src/services/utilities.ts
@@ -364,7 +364,7 @@ namespace ts {
                         const rightKind = getNodeKind(right);
                         return rightKind === ScriptElementKind.unknown ? ScriptElementKind.constElement : rightKind;
                     case SpecialPropertyAssignmentKind.PrototypeProperty:
-                        return ScriptElementKind.memberFunctionElement; // instance method
+                        return isFunctionExpression(right) ? ScriptElementKind.memberFunctionElement : ScriptElementKind.memberVariableElement;
                     case SpecialPropertyAssignmentKind.ThisProperty:
                         return ScriptElementKind.memberVariableElement; // property
                     case SpecialPropertyAssignmentKind.Property:
