18962c1ed976ed74243989e401345a057c8f57bf
Wesley Wigham <wewigham@microsoft.com>
2017-11-17 14:07:30 -0800

    Add deeply destructured array to test

--- tests/cases/compiler/destructuredDeclarationEmit.ts
+++ tests/cases/compiler/destructuredDeclarationEmit.ts
@@ -1,10 +1,14 @@
 // @declaration: true
 // @filename: foo.ts
 const foo = { bar: 'hello', bat: 'world', bam: { bork: { bar: 'a', baz: 'b' } } };
-export { foo };
+const arr: [0, 1, 2, ['a', 'b', 'c', [{def: 'def'}, {sec: 'sec'}]]] = [0, 1, 2, ['a', 'b', 'c', [{def: 'def'}, {sec: 'sec'}]]];
+export { foo, arr };
 // @filename: index.ts
-import { foo } from './foo';
-export { foo };
+import { foo, arr } from './foo';
+export { foo, arr };
 
 const { bar: baz, bat, bam: { bork: { bar: ibar, baz: ibaz } } } = foo;
 export { baz, ibaz };
+
+const [ , one, , [, bee, , [, {sec} ]]] = arr;
+export { one, bee, sec };
