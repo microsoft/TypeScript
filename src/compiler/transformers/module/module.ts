e20a548ec36ca75a39f402588bdcf3ff87f2cb53
Wesley Wigham <wewigham@microsoft.com>
2017-11-17 17:39:48 -0800

    Remove erroneous semicolons from helper

--- src/compiler/transformers/module/module.ts
+++ src/compiler/transformers/module/module.ts
@@ -1726,7 +1726,7 @@ namespace ts {
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null); for (var k in mod); if (Object.hasOwnProperty.call(mod, k)); result[k] = mod[k];
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
     result["default"] = mod;
     return result;
 }`
--- tests/baselines/reference/esModuleInterop.js
+++ tests/baselines/reference/esModuleInterop.js
@@ -26,7 +26,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null); for (var k in mod); if (Object.hasOwnProperty.call(mod, k)); result[k] = mod[k];
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
     result["default"] = mod;
     return result;
 }
