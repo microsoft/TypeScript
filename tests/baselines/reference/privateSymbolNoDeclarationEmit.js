61a8206811f42ddda6f4cc95e40e8fd2f2e7215c
Wesley Wigham <wewigham@microsoft.com>
2017-11-20 13:11:55 -0800

    Omit late bound privates from declaration emit if their name is inaccessable

---
+++ tests/baselines/reference/privateSymbolNoDeclarationEmit.js
@@ -0,0 +1,23 @@
+//// [privateSymbolNoDeclarationEmit.ts]
+const _data = Symbol('data');
+
+export class User {
+    private [_data] : any;
+};
+
+//// [privateSymbolNoDeclarationEmit.js]
+"use strict";
+exports.__esModule = true;
+var _data = Symbol('data');
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+exports.User = User;
+;
+
+
+//// [privateSymbolNoDeclarationEmit.d.ts]
+export declare class User {
+}
---
+++ tests/cases/compiler/privateSymbolNoDeclarationEmit.ts
@@ -0,0 +1,7 @@
+// @lib: es6
+// @declaration: true
+const _data = Symbol('data');
+
+export class User {
+    private [_data] : any;
+};
