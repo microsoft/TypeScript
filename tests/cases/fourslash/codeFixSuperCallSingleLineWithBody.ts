8596de4c160bc734d2b60139158c7ef94f9758ea
Charles Pierce <cpierce.grad@gmail.com>
2017-12-01 18:30:45 -0800

    Correct super() Code Fix Formatting #20073

--- tests/cases/fourslash/codeFixSuperCallSingleLineWithBody.ts
+++ tests/cases/fourslash/codeFixSuperCallSingleLineWithBody.ts
@@ -3,10 +3,10 @@
 ////class Base{
 ////}
 ////class C extends Base{
-////    constructor() [|{
-////    }|]
+////    constructor() [|{ const t = 0; }|]
 ////}
 // TODO: GH#18445
 verify.rangeAfterCodeFix(`{\r
         super();\r
+        const t = 0;\r
     }`, /*includeWhitespace*/ true);
