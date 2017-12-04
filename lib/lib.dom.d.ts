2249fb403a5e32fa96d5e5a8c4b94e687a9dc663
Marius Schulz <marius.schulz@me.com>
2017-12-04 23:08:18 +0100

    Fix type declaration for "setSelectionRange" API
    The `start` and `end` parameters aren't optional. Also, the `direction` parameter only accepts one of three string values, which are now modeled as a union of string literal types.
    
    More information: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/setSelectionRange

--- lib/lib.dom.d.ts
+++ lib/lib.dom.d.ts
@@ -5516,7 +5516,7 @@ interface HTMLInputElement extends HTMLElement {
      * @param start The offset into the text field for the start of the selection.
      * @param end The offset into the text field for the end of the selection.
      */
-    setSelectionRange(start?: number, end?: number, direction?: string): void;
+    setSelectionRange(start: number, end: number, direction?: "forward" | "backward" | "none"): void;
     /**
      * Decrements a range input control's value by the value given by the Step attribute. If the optional parameter is used, it will decrement the input control's step value multiplied by the parameter's value.
      * @param n Value to decrement the value by.
@@ -6979,7 +6979,7 @@ interface HTMLTextAreaElement extends HTMLElement {
      * @param start The offset into the text field for the start of the selection.
      * @param end The offset into the text field for the end of the selection.
      */
-    setSelectionRange(start: number, end: number): void;
+    setSelectionRange(start: number, end: number, direction?: "forward" | "backward" | "none"): void;
     addEventListener<K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLTextAreaElement, ev: HTMLElementEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
     addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
     removeEventListener<K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLTextAreaElement, ev: HTMLElementEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
