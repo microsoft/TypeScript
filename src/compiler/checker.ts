988f9ac641128cc3b201c0399332ca24fffa4bae
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2017-12-04 13:29:51 -0800

    Fill "empty" structured type marker with early symbols
    The same way that getResolvedMembersOrExportsOfSymbol does. That
    functionality may not be needed anymore, in fact.

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -6171,7 +6171,8 @@ namespace ts {
 
         function resolveStructuredTypeMembers(type: StructuredType): ResolvedType {
             if (!(<ResolvedType>type).members) {
-                setStructuredTypeMembers(type, emptySymbols, emptyArray, emptyArray, undefined, undefined);
+                const earlySymbols = (type.symbol && type.symbol.members) || emptySymbols;
+                setStructuredTypeMembers(type, earlySymbols, emptyArray, emptyArray, undefined, undefined);
                 if (type.flags & TypeFlags.Object) {
                     if ((<ObjectType>type).objectFlags & ObjectFlags.Reference) {
                         resolveTypeReferenceMembers(<TypeReference>type);
