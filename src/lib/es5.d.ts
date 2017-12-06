d383f18d72efd2537ff646065b1ce6b09ef5f8cd
Alex Khomchenko <akhomchenko@gmail.com>
2017-12-06 22:53:42 +0100

    fix docs for Math.ceil and Math.floor (#16900)

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -550,7 +550,7 @@ interface Math {
       */
     atan2(y: number, x: number): number;
     /**
-      * Returns the smallest number greater than or equal to its numeric argument.
+      * Returns the smallest integer greater than or equal to its numeric argument.
       * @param x A numeric expression.
       */
     ceil(x: number): number;
@@ -565,7 +565,7 @@ interface Math {
       */
     exp(x: number): number;
     /**
-      * Returns the greatest number less than or equal to its numeric argument.
+      * Returns the greatest integer less than or equal to its numeric argument.
       * @param x A numeric expression.
       */
     floor(x: number): number;
