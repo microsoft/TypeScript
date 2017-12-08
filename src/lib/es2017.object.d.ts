8a045b7029513674af891ee3619f294434e43876
Andy Hanson <anhans@microsoft.com>
2017-12-08 09:31:22 -0800

    Use mapped type and constrain 'T extends object'

--- src/lib/es2017.object.d.ts
+++ src/lib/es2017.object.d.ts
@@ -3,25 +3,14 @@ interface ObjectConstructor {
      * Returns an array of values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    values<T>(o: { [s: string]: T } | { [n: number]: T } | boolean | number | string | symbol): T[];
+    values<T extends object>(o: T): T[keyof T][];
 
-    /**
-     * Returns an array of values of the enumerable properties of an object
-     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
-     */
-    values(o: {}): any[];
 
     /**
      * Returns an array of key/values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    entries<T>(o: { [s: string]: T } | { [n: number]: T } | boolean | number | string | symbol): [string, T][];
-
-    /**
-     * Returns an array of key/values of the enumerable properties of an object
-     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
-     */
-    entries(o: {}): [string, any][];
+    entries<T extends object>(o: T): [string, T[keyof T]][];
 
     /**
      * Returns an object containing all own property descriptors of an object
