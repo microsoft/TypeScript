54908f63453e4f16a185a376e67be8efa34b1681
Josh Goldberg <josh@fullscreenmario.com>
2017-12-09 14:41:47 -0800

    Allowed trailing commas in type parameter/argument lists
    This change is only one source file and one error file... there must be something I'm missing!?

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -25545,10 +25545,6 @@ namespace ts {
         }
 
         function checkGrammarTypeParameterList(typeParameters: NodeArray<TypeParameterDeclaration>, file: SourceFile): boolean {
-            if (checkGrammarForDisallowedTrailingComma(typeParameters)) {
-                return true;
-            }
-
             if (typeParameters && typeParameters.length === 0) {
                 const start = typeParameters.pos - "<".length;
                 const end = skipTrivia(file.text, typeParameters.end) + ">".length;
