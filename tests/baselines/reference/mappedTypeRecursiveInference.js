808abb6833d5615ca49fec1ed0754204d36c021e
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2017-12-11 12:32:54 -0800

    Update baselines

--- tests/baselines/reference/mappedTypeRecursiveInference.js
+++ tests/baselines/reference/mappedTypeRecursiveInference.js
@@ -1,15 +1,15 @@
 //// [mappedTypeRecursiveInference.ts]
-// interface A { a: A }
-// declare let a: A;
+interface A { a: A }
+declare let a: A;
 type Deep<T> = { [K in keyof T]: Deep<T[K]> }
 declare function foo<T>(deep: Deep<T>): T;
-// const out = foo(a);
+const out = foo(a);
 
 let xhr: XMLHttpRequest;
 const out2 = foo(xhr);
 
 
 //// [mappedTypeRecursiveInference.js]
-// const out = foo(a);
+var out = foo(a);
 var xhr;
 var out2 = foo(xhr);
