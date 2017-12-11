d021bce0fb2e51c4fd512ff1e9b6cc84c1a31094
Andy Hanson <anhans@microsoft.com>
2017-12-11 09:11:28 -0800

    Always check a return expression in a generator

---
+++ tests/baselines/reference/generatorReturnExpressionIsChecked.js
@@ -0,0 +1,10 @@
+//// [generatorReturnExpressionIsChecked.ts]
+function* f(): Iterator<number> {
+    return invalid;
+}
+
+
+//// [generatorReturnExpressionIsChecked.js]
+function* f() {
+    return invalid;
+}
