4bc0d85d29f6533f07368b93f7002b8f179b71bd
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2017-12-12 08:37:34 -0800

    Test:spread `object` retains lefthandside
    1. Make sure that spreading `object` doesn't lose everything to the left.
    2. Update baselines to show that `{ ...object }` is now `{}`.

---
+++ tests/baselines/reference/spreadNonPrimitive.js
@@ -0,0 +1,15 @@
+//// [spreadNonPrimitive.ts]
+declare let o: object;
+const x: { a: number, b: number } = { a: 1, ...o, b: 2 };
+
+
+//// [spreadNonPrimitive.js]
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var x = __assign({ a: 1 }, o, { b: 2 });
