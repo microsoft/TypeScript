4c7ddcd7e0dbea2ed9a064e0035cbd3a6a359cd7
Wesley Wigham <wwigham@gmail.com>
2017-12-13 13:04:10 -0800

    Trim test a bit

--- tests/baselines/reference/declarationEmitAliasExportStar.js
+++ tests/baselines/reference/declarationEmitAliasExportStar.js
@@ -1,21 +1,14 @@
 //// [tests/cases/compiler/declarationEmitAliasExportStar.ts] ////
 
-//// [thingA.ts]
-export interface ThingA { }
 //// [thingB.ts]
 export interface ThingB { }
 //// [things.ts]
-export { ThingA } from "./thingA";
 export * from "./thingB";
 //// [index.ts]
 import * as things from "./things";
-export const thing1 = (param: things.ThingA) => null;
 export const thing2 = (param: things.ThingB) => null;
 
 
-//// [thingA.js]
-"use strict";
-exports.__esModule = true;
 //// [thingB.js]
 "use strict";
 exports.__esModule = true;
@@ -25,20 +18,14 @@ exports.__esModule = true;
 //// [index.js]
 "use strict";
 exports.__esModule = true;
-exports.thing1 = function (param) { return null; };
 exports.thing2 = function (param) { return null; };
 
 
-//// [thingA.d.ts]
-export interface ThingA {
-}
 //// [thingB.d.ts]
 export interface ThingB {
 }
 //// [things.d.ts]
-export { ThingA } from "./thingA";
 export * from "./thingB";
 //// [index.d.ts]
 import * as things from "./things";
-export declare const thing1: (param: things.ThingA) => any;
 export declare const thing2: (param: things.ThingB) => any;
--- tests/cases/compiler/declarationEmitAliasExportStar.ts
+++ tests/cases/compiler/declarationEmitAliasExportStar.ts
@@ -1,12 +1,8 @@
 // @declaration: true
-// @filename: thingA.ts
-export interface ThingA { }
 // @filename: thingB.ts
 export interface ThingB { }
 // @filename: things.ts
-export { ThingA } from "./thingA";
 export * from "./thingB";
 // @filename: index.ts
 import * as things from "./things";
-export const thing1 = (param: things.ThingA) => null;
 export const thing2 = (param: things.ThingB) => null;
