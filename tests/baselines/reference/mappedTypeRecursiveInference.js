dd941e5665a8dd5742217fd255e7fc560001f967
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2017-12-14 15:11:49 -0800

    Add more tests of deferred mapped types

--- tests/baselines/reference/mappedTypeRecursiveInference.js
+++ tests/baselines/reference/mappedTypeRecursiveInference.js
@@ -4,12 +4,22 @@ declare let a: A;
 type Deep<T> = { [K in keyof T]: Deep<T[K]> }
 declare function foo<T>(deep: Deep<T>): T;
 const out = foo(a);
+out.a
+out.a.a
+out.a.a.a.a.a.a.a
 
 let xhr: XMLHttpRequest;
 const out2 = foo(xhr);
+out2.responseXML
+out2.responseXML.activeElement.className.length
 
 
 //// [mappedTypeRecursiveInference.js]
 var out = foo(a);
+out.a;
+out.a.a;
+out.a.a.a.a.a.a.a;
 var xhr;
 var out2 = foo(xhr);
+out2.responseXML;
+out2.responseXML.activeElement.className.length;
