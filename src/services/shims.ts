9330346e1a2ffc77f14043f2bcf9e4e1a09b028d
Andy Hanson <anhans@microsoft.com>
2017-12-20 07:38:44 -0800

    type-operator-spacing: Just check for presence of space, not double-space

--- src/services/shims.ts
+++ src/services/shims.ts
@@ -141,7 +141,6 @@ namespace ts {
         getEncodedSemanticClassifications(fileName: string, start: number, length: number): string;
 
         getCompletionsAtPosition(fileName: string, position: number, options: GetCompletionsAtPositionOptions | undefined): string;
-        // tslint:disable-next-line type-operator-spacing (false positive)
         getCompletionEntryDetails(fileName: string, position: number, entryName: string, options: string/*Services.FormatCodeOptions*/ | undefined, source: string | undefined): string;
 
         getQuickInfoAtPosition(fileName: string, position: number): string;
@@ -907,7 +906,6 @@ namespace ts {
         }
 
         /** Get a string based representation of a completion list entry details */
-        // tslint:disable-next-line type-operator-spacing (false positive)
         public getCompletionEntryDetails(fileName: string, position: number, entryName: string, options: string/*Services.FormatCodeOptions*/ | undefined, source: string | undefined) {
             return this.forwardJSONCall(
                 `getCompletionEntryDetails('${fileName}', ${position}, '${entryName}')`,
