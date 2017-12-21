477635bef6573f78aa76751f9154e078fe953897
Matt Bierner <matb@microsoft.com>
2017-12-21 11:13:44 -0800

    Add method test

--- tests/cases/fourslash/completionsOptionalKindModifier.ts
+++ tests/cases/fourslash/completionsOptionalKindModifier.ts
@@ -1,9 +1,10 @@
 /// <reference path="fourslash.ts" />
 
-////interface A { a?: number; };
+////interface A { a?: number; method?(): number; };
 ////function f(x: A) {
-////x./**/;
+////x./*a*/;
 ////}
 
-goTo.marker();
+goTo.marker("a");
 verify.completionListContains("a", /* text */ undefined, /* documentation */ undefined, { kindModifiers: "optional" });
+verify.completionListContains("method", /* text */ undefined, /* documentation */ undefined, { kindModifiers: "optional" });
