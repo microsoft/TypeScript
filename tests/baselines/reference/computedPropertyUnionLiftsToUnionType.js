6bec7533a34c742ee8ab7b81e7df17b4110ac127
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-01-05 16:23:20 -0800

    More computed property cases + Update baselines

--- tests/baselines/reference/computedPropertyUnionLiftsToUnionType.js
+++ tests/baselines/reference/computedPropertyUnionLiftsToUnionType.js
@@ -1,45 +1,85 @@
 //// [computedPropertyUnionLiftsToUnionType.ts]
 declare var ab: 'a' | 'b';
 declare var cd: 'c' | 'd';
-// More cases:
-//   add spreads
-//   other literal types: number, boolean, enum (string and number)
-//   multiple unions
-//   union, spread (with union inside), union
-//   methods and other stuff that would get mangled by spread (since I use spread internally)
-const x: { a: string } | { b: string } = { [ab]: 'hi' }
-const y: { a: string, c: string } | { a: string, d: string } | { b: string, c: string } | { b: string, d: string } =
-    { [ab]: 'hi', [cd]: 'there' }
+declare var onetwo: 1 | 2;
+enum Alphabet {
+    Aleph,
+    Bet,
+}
+declare var alphabet: Alphabet;
 
-//   in destructuring???!!
-/*
-declare let o: { [t]: string }
+const x: { a: string } | { b: string } = { [ab]: 'hi' }
+//   multiple unions
+const y: { a: string, m: number, c: string }
+    | { a: string, m: number, d: string }
+    | { b: string, m: number, c: string }
+    | { b: string, m: number, d: string } = { [ab]: 'hi', m: 1, [cd]: 'there' }
+//   union, spread (with union inside), union
+const s: { a: string, c: string } | { b: string, c: string } = { [ab]: 'hi', ...{ c: 'no' }}
+const sd: { a: string } | { b: string } = { [ab]: 'hi', ...{ a: 'no' }}
+const sn: { a: string, c: string }
+    | { a: string, d: string }
+    | { b: string, c: string }
+    | { b: string, d: string } = { [ab]: 'hi', ...{ [cd]: 'no' }}
+// methods
+const m: { a: string, m(): number, p: number } | { b: string, m(): number, p: number } =
+    { [ab]: 'hi', m() { return 1 }, get p() { return 2 } }
+//   other literal types: number, enum (string and number)
+const n: { "1": string } | { "2": string } = { [onetwo]: 'hi' }
+const e: { "0": string } | { "1": string } = { [alphabet]: 'hi' }
+
+//   destructuring
 declare let u: { a: string } | { b: string }
-const { [t]: doo } = o
-const { [t]: duo } = u
-var t: 'a' | 'b' = doo
-var t: 'a' | 'b' = duo
-*/
+({ [ab]: du } = u) // implicit any error
+var du: any
+declare let sig: { [s: string]: string }
+({ [ab]: ds } = sig) // fine, comes from index signature
+var ds: string
+
+var duo: any
+var dso: string
+var { [ab]: duo } = u   // implicit any error (or similar to the singleton one)
+var { [ab]: dso } = sig // fine
+
+// number index signatures
+declare let sin: { [n: number]: number }
+var dn: number
+({ [onetwo]: dn } = sin) // fine, from index signature
+var dno: number
+var { [onetwo]: dno } = sin // fine, from index signature
+
+
 
 
 
 
 //// [computedPropertyUnionLiftsToUnionType.js]
-// More cases:
-//   add spreads
-//   other literal types: number, boolean, enum (string and number)
+var Alphabet;
+(function (Alphabet) {
+    Alphabet[Alphabet["Aleph"] = 0] = "Aleph";
+    Alphabet[Alphabet["Bet"] = 1] = "Bet";
+})(Alphabet || (Alphabet = {}));
+const x = { [ab]: 'hi' };
 //   multiple unions
+const y = { [ab]: 'hi', m: 1, [cd]: 'there' };
 //   union, spread (with union inside), union
-//   methods and other stuff that would get mangled by spread (since I use spread internally)
-var x = (_a = {}, _a[ab] = 'hi', _a);
-var y = (_b = {}, _b[ab] = 'hi', _b[cd] = 'there', _b);
-var _a, _b;
-//   in destructuring???!!
-/*
-declare let o: { [t]: string }
-declare let u: { a: string } | { b: string }
-const { [t]: doo } = o
-const { [t]: duo } = u
-var t: 'a' | 'b' = doo
-var t: 'a' | 'b' = duo
-*/
+const s = Object.assign({ [ab]: 'hi' }, { c: 'no' });
+const sd = Object.assign({ [ab]: 'hi' }, { a: 'no' });
+const sn = Object.assign({ [ab]: 'hi' }, { [cd]: 'no' });
+// methods
+const m = { [ab]: 'hi', m() { return 1; }, get p() { return 2; } };
+//   other literal types: number, enum (string and number)
+const n = { [onetwo]: 'hi' };
+const e = { [alphabet]: 'hi' };
+({ [ab]: du } = u); // implicit any error
+var du;
+({ [ab]: ds } = sig); // fine, comes from index signature
+var ds;
+var duo;
+var dso;
+var { [ab]: duo } = u; // implicit any error (or similar to the singleton one)
+var { [ab]: dso } = sig; // fine
+var dn;
+({ [onetwo]: dn } = sin); // fine, from index signature
+var dno;
+var { [onetwo]: dno } = sin; // fine, from index signature
--- tests/cases/conformance/es6/computedProperties/computedPropertyUnionLiftsToUnionType.ts
+++ tests/cases/conformance/es6/computedProperties/computedPropertyUnionLiftsToUnionType.ts
@@ -1,25 +1,54 @@
+// @noImplicitAny: true
+// @target: es6
 declare var ab: 'a' | 'b';
 declare var cd: 'c' | 'd';
-// More cases:
-//   add spreads
-//   other literal types: number, boolean, enum (string and number)
-//   multiple unions
-//   union, spread (with union inside), union
-//   methods and other stuff that would get mangled by spread (since I use spread internally)
+declare var onetwo: 1 | 2;
+enum Alphabet {
+    Aleph,
+    Bet,
+}
+declare var alphabet: Alphabet;
+
 const x: { a: string } | { b: string } = { [ab]: 'hi' }
+//   multiple unions
 const y: { a: string, m: number, c: string }
     | { a: string, m: number, d: string }
     | { b: string, m: number, c: string }
     | { b: string, m: number, d: string } = { [ab]: 'hi', m: 1, [cd]: 'there' }
+//   union, spread (with union inside), union
+const s: { a: string, c: string } | { b: string, c: string } = { [ab]: 'hi', ...{ c: 'no' }}
+const sd: { a: string } | { b: string } = { [ab]: 'hi', ...{ a: 'no' }}
+const sn: { a: string, c: string }
+    | { a: string, d: string }
+    | { b: string, c: string }
+    | { b: string, d: string } = { [ab]: 'hi', ...{ [cd]: 'no' }}
+// methods
+const m: { a: string, m(): number, p: number } | { b: string, m(): number, p: number } =
+    { [ab]: 'hi', m() { return 1 }, get p() { return 2 } }
+//   other literal types: number, enum (string and number)
+const n: { "1": string } | { "2": string } = { [onetwo]: 'hi' }
+const e: { "0": string } | { "1": string } = { [alphabet]: 'hi' }
 
-//   in destructuring???!!
-/*
-declare let o: { [t]: string }
+//   destructuring
 declare let u: { a: string } | { b: string }
-const { [t]: doo } = o
-const { [t]: duo } = u
-var t: 'a' | 'b' = doo
-var t: 'a' | 'b' = duo
-*/
+({ [ab]: du } = u) // implicit any error
+var du: any
+declare let sig: { [s: string]: string }
+({ [ab]: ds } = sig) // fine, comes from index signature
+var ds: string
+
+var duo: any
+var dso: string
+var { [ab]: duo } = u   // implicit any error (or similar to the singleton one)
+var { [ab]: dso } = sig // fine
+
+// number index signatures
+declare let sin: { [n: number]: number }
+var dn: number
+({ [onetwo]: dn } = sin) // fine, from index signature
+var dno: number
+var { [onetwo]: dno } = sin // fine, from index signature
+
+
 
 
