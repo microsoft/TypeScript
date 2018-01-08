4bb3c902250c659580537c6f8f86f979d77e321d
Andy Hanson <anhans@microsoft.com>
2018-01-08 12:40:21 -0800

    Revert everything but change to parameter types

--- src/lib/es2017.object.d.ts
+++ src/lib/es2017.object.d.ts
@@ -3,14 +3,25 @@ interface ObjectConstructor {
      * Returns an array of values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    values<T extends object>(o: T): T[keyof T][];
+    values<T>(o: { [s: string]: T } | { [n: number]: T }): T[];
 
+    /**
+     * Returns an array of values of the enumerable properties of an object
+     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+     */
+    values(o: {}): any[];
 
     /**
      * Returns an array of key/values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    entries<T extends object>(o: T): [string, T[keyof T]][];
+    entries<T>(o: { [s: string]: T } | { [n: number]: T }): [string, T][];
+
+    /**
+     * Returns an array of key/values of the enumerable properties of an object
+     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+     */
+    entries(o: {}): [string, any][];
 
     /**
      * Returns an object containing all own property descriptors of an object
