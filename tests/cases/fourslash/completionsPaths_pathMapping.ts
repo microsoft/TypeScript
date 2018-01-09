9aa99b90c72917347e781d146af577090c0b584d
Andy <anhans@microsoft.com>
2018-01-09 13:15:30 -0800

    Include directories in completions for path mapping (#21103)

--- tests/cases/fourslash/completionsPaths_pathMapping.ts
+++ tests/cases/fourslash/completionsPaths_pathMapping.ts
@@ -3,6 +3,9 @@
 // @Filename: /src/b.ts
 ////export const x = 0;
 
+// @Filename: /src/dir/x.ts
+/////export const x = 0;
+
 // @Filename: /src/a.ts
 ////import {} from "foo/[|/**/|]";
 
@@ -17,4 +20,8 @@
 ////}
 
 const [replacementSpan] = test.ranges();
-verify.completionsAt("", [{ name: "a", replacementSpan }, { name: "b", replacementSpan }]);
+verify.completionsAt("", [
+    { name: "a", replacementSpan },
+    { name: "b", replacementSpan },
+    { name: "dir", replacementSpan },
+]);
