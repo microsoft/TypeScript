15e916ed0e17948dda6ee95328e20992ba6dd858
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-01-08 19:02:33 -0500

    only one codefix

--- tests/cases/fourslash/codeFixAwaitInSyncFunction2.ts
+++ tests/cases/fourslash/codeFixAwaitInSyncFunction2.ts
@@ -2,14 +2,12 @@
 
 ////const f = function() {
 ////    await Promise.resolve();
-////    await Promise.resolve();
 ////}
 
 verify.codeFix({
     description: "Convert to async",
     newFileContent:
 `const f = async function() {
-    await Promise.resolve();
     await Promise.resolve();
 }`,
 });
