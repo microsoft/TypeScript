ff102da9d1a77bd148d7c713ad109e65edfcba65
Andrew Casey <andrew.casey@microsoft.com>
2018-01-10 15:12:00 -0800

    Refine explanatory comment

--- tests/cases/fourslash/completionListInImportClause05.ts
+++ tests/cases/fourslash/completionListInImportClause05.ts
@@ -1,7 +1,7 @@
 /// <reference path='fourslash.ts' />
 
 // @Filename: app.ts
-//// import * as A from "[|/*1*/|]";
+////import * as A from "[|/*1*/|]";
 
 // @Filename: /node_modules/@types/a__b/index.d.ts
 ////declare module "@e/f" { function fun(): string; }
@@ -9,10 +9,8 @@
 // @Filename: /node_modules/@types/c__d/index.d.ts
 ////export declare let x: number;
 
-// NOTE: When performing completion, the "current directory" appears to be "/",
-// which is well above "." (i.e. the directory containing "app.ts").  This issue
-// is specific to the virtual file system, so just work around it by putting the
-// node modules folder in "/", rather than ".".
+// NOTE: The node_modules folder is in "/", rather than ".", because it requires
+// less scaffolding to mock.  In particular, "/" is where we look for type roots.
 
 const [replacementSpan] = test.ranges();
 verify.completionsAt("1", [
