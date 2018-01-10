8275bed33fd75dcc2f1c7ebcd1e7ba08b3cbb9f5
Andrew Casey <andrew.casey@microsoft.com>
2018-01-10 13:57:06 -0800

    Add comment explaining test-only workaround

--- tests/cases/fourslash/completionListInImportClause05.ts
+++ tests/cases/fourslash/completionListInImportClause05.ts
@@ -9,6 +9,11 @@
 // @Filename: /node_modules/@types/c__d/index.d.ts
 ////export declare let x: number;
 
+// NOTE: When performing completion, the "current directory" appears to be "/",
+// which is well above "." (i.e. the directory containing "app.ts").  This issue
+// is specific to the virtual file system, so just work around it by putting the
+// node modules folder in "/", rather than ".".
+
 const [replacementSpan] = test.ranges();
 verify.completionsAt("1", [
     { name: "@a/b", replacementSpan },
