baf31ec52e500bd646090f62fcb4b6112bb09645
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-01-11 16:30:05 -0800

    Test Diff and Omit

---
+++ tests/baselines/reference/indexedAccessRetainsIndexSignature.js
@@ -0,0 +1,10 @@
+//// [indexedAccessRetainsIndexSignature.ts]
+type Diff<T extends string, U extends string> =
+    ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T]
+type Omit<U, K extends keyof U> = Pick<U, Diff<keyof U, K>>
+
+
+type O = Omit<{ a: number, b: string }, 'a'>
+
+
+//// [indexedAccessRetainsIndexSignature.js]
