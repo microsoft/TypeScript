ec3765130812677e1be7b3d9909ee312e82ac663
Andy <anhans@microsoft.com>
2018-01-17 14:22:58 -0800

    Use packageId for suggestion to install `@types/packageName` (#21241)

--- tests/baselines/reference/untypedModuleImport_noImplicitAny_relativePath.js
+++ tests/baselines/reference/untypedModuleImport_noImplicitAny_relativePath.js
@@ -1,15 +1,13 @@
-//// [tests/cases/conformance/moduleResolution/untypedModuleImport_noImplicitAny.ts] ////
+//// [tests/cases/conformance/moduleResolution/untypedModuleImport_noImplicitAny_relativePath.ts] ////
 
 //// [package.json]
-// This tests that `--noImplicitAny` disables untyped modules.
-
 { "name": "foo", "version": "1.2.3" }
 
 //// [index.js]
 This file is not processed.
 
 //// [a.ts]
-import * as foo from "foo";
+import * as foo from "./node_modules/foo";
 
 
 //// [a.js]
