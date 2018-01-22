18e1ac030613bd15a55156bd95e16b5002e99ffd
Stephan Ginthör <26004708+Lazarus535@users.noreply.github.com>
2018-01-22 09:52:58 +0100

    Fixes #17080
    Changes are in src/compiler.checker.ts only
    The second arguments to the function "removeOptionalityFromDeclaredType" has been changed from "getRootDeclaration(declaration)" to "declaration".

---
+++ tests/cases/compiler/optionalParameterInDestructuringWithInitializer.ts
@@ -0,0 +1,39 @@
+// @strictNullChecks: true
+// https://github.com/Microsoft/TypeScript/issues/17080
+function f(a:number,b:number) {
+}
+
+function func1( {a, b}: {a: number, b?: number} = {a: 1, b: 2} ) {
+  f(a, b)
+  // error
+}
+
+function func2( {a, b = 3}: {a: number, b?:number} = {a: 1,b: 2} ) {
+  f(a, b)
+  // no error
+}
+
+function func3( {a, b}: {a: number, b?: number} = {a: 1} ) {
+  f(a,b)
+  // error
+}
+
+function func4( {a: {b, c}, d}: {a: {b: number,c?: number},d: number} = {a: {b: 1,c: 2},d: 3} ) {
+  f(b,c)
+  // error
+}
+
+function func5({a: {b, c = 4}, d}: {a: {b: number,c?: number},d: number} = {a: {b: 1,c: 2},d: 3} ) {
+  f(b, c)
+  // no error
+}
+
+function func6( {a: {b, c} = {b: 4, c: 5}, d}: {a: {b: number, c?: number}, d: number} = {a: {b: 1,c: 2}, d: 3} ) {
+  f(b, c)
+  // error
+}
+
+function func7( {a: {b, c = 6} = {b: 4, c: 5}, d}: {a: {b: number, c?: number}, d: number} = {a: {b: 1, c: 2}, d: 3} ) {
+  f(b, c)
+  // no error
+}
