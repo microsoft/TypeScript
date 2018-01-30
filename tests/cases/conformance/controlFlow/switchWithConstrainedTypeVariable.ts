926f2e52ed5cf2b5b63aea01ad5609677e64a494
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-01-30 10:18:55 -0800

    Test:narrowing in switch w/constrained type variable

--- tests/cases/conformance/controlFlow/switchWithConstrainedTypeVariable.ts
+++ tests/cases/conformance/controlFlow/switchWithConstrainedTypeVariable.ts
@@ -12,3 +12,19 @@ function function1<T extends 'a' | 'b'>(key: T) {
       break;
   }
 }
+
+// #20375
+
+declare var n: never;
+declare function never(never: never): never;
+function f<T extends 'a' | 'b' | 'c'>(t: T): void {
+    switch (t) {
+        // in a/b/c branches, assignment should fail as t narrows to a/b/c
+        // in default branch, assignment should be fine
+        case 'a': n = t; break;
+        case 'b': n = t; break;
+        case 'c': n = t; break;
+        default: n = t; break;
+    }
+}
+
