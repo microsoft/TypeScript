23fb403c4b5e66332b12665a4e8e962950b4fe21
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-01-31 15:03:36 -0800

    Test:narrowing by property assignment in obj literal

---
+++ tests/baselines/reference/controlFlowObjectLiteralDeclaration.js
@@ -0,0 +1,14 @@
+//// [controlFlowObjectLiteralDeclaration.ts]
+type Foo = {
+  bar?: number[];
+}
+
+let f2: Foo = { bar: [] };
+f2.bar.push(0)
+
+
+
+//// [controlFlowObjectLiteralDeclaration.js]
+"use strict";
+var f2 = { bar: [] };
+f2.bar.push(0);
---
+++ tests/cases/conformance/controlFlow/controlFlowObjectLiteralDeclaration.ts
@@ -0,0 +1,8 @@
+// @strict: true
+type Foo = {
+  bar?: number[];
+}
+
+let f2: Foo = { bar: [] };
+f2.bar.push(0)
+
