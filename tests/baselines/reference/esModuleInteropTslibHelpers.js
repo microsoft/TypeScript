50d2ad82f9b3fdfea054acca18665093721ddb2a
Wesley Wigham <wewigham@microsoft.com>
2018-02-02 10:19:00 -0800

    Add tests for other forms that trigger import helpers

--- tests/baselines/reference/esModuleInteropTslibHelpers.js
+++ tests/baselines/reference/esModuleInteropTslibHelpers.js
@@ -6,6 +6,18 @@ declare module "path";
 import path from "path";
 path.resolve("", "../");
 export class Foo { }
+//// [file2.ts]
+import * as path from "path";
+path.resolve("", "../");
+export class Foo2 { }
+//// [file3.ts]
+import {default as resolve} from "path";
+resolve("", "../");
+export class Foo3 { }
+//// [file4.ts]
+import {Bar, default as resolve} from "path";
+resolve("", "../");
+export { Bar }
 
 //// [file.js]
 "use strict";
@@ -19,3 +31,34 @@ var Foo = /** @class */ (function () {
     return Foo;
 }());
 exports.Foo = Foo;
+//// [file2.js]
+"use strict";
+exports.__esModule = true;
+var tslib_1 = require("tslib");
+var path = tslib_1.__importStar(require("path"));
+path.resolve("", "../");
+var Foo2 = /** @class */ (function () {
+    function Foo2() {
+    }
+    return Foo2;
+}());
+exports.Foo2 = Foo2;
+//// [file3.js]
+"use strict";
+exports.__esModule = true;
+var tslib_1 = require("tslib");
+var path_1 = tslib_1.__importDefault(require("path"));
+path_1["default"]("", "../");
+var Foo3 = /** @class */ (function () {
+    function Foo3() {
+    }
+    return Foo3;
+}());
+exports.Foo3 = Foo3;
+//// [file4.js]
+"use strict";
+exports.__esModule = true;
+var tslib_1 = require("tslib");
+var path_1 = tslib_1.__importStar(require("path"));
+exports.Bar = path_1.Bar;
+path_1["default"]("", "../");
--- tests/cases/compiler/esModuleInteropTslibHelpers.ts
+++ tests/cases/compiler/esModuleInteropTslibHelpers.ts
@@ -7,3 +7,15 @@ declare module "path";
 import path from "path";
 path.resolve("", "../");
 export class Foo { }
+// @filename: file2.ts
+import * as path from "path";
+path.resolve("", "../");
+export class Foo2 { }
+// @filename: file3.ts
+import {default as resolve} from "path";
+resolve("", "../");
+export class Foo3 { }
+// @filename: file4.ts
+import {Bar, default as resolve} from "path";
+resolve("", "../");
+export { Bar }
