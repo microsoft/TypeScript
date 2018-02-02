a5a99fbda4eb9a961e3780a706ac98baa9538442
Andy Hanson <anhans@microsoft.com>
2018-02-02 09:27:41 -0800

    Detect and prevent creation of bad Identifier

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -5891,7 +5891,7 @@ namespace ts {
 
     /** True if node is of a kind that may contain comment text. */
     export function isJSDocCommentContainingNode(node: Node): boolean {
-        return node.kind === SyntaxKind.JSDocComment || isJSDocTag(node);
+        return node.kind === SyntaxKind.JSDocComment || isJSDocTag(node) || isJSDocTypeLiteral(node);
     }
 
     // TODO: determine what this does before making it public.
