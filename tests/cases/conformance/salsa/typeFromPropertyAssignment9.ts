a51bce0ab5227c00b804c25fa5f2651a534aabe8
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-02-07 11:32:20 -0800

    Test:basic `var x = x || {}` support in JS

---
+++ tests/cases/conformance/salsa/typeFromPropertyAssignment9.ts
@@ -0,0 +1,17 @@
+// @noEmit: true
+// @allowJs: true
+// @checkJs: true
+// @target: es6
+// @Filename: a.js
+
+// TODO: JSDoc would provide a contextual type, so ... I should test that
+// TODO: Try initializer of function or class I guess (though classes aren't context sensitive)
+var my = my || {};
+my.m = function() {
+    return 1;
+}
+my.p = {};
+my.n = 1;
+my.m
+my.p
+my.n
