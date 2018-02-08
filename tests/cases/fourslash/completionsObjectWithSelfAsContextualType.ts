685a314ad707343c5cded3ca0adee602eec4905c
Andy Hanson <anhans@microsoft.com>
2018-02-08 09:04:02 -0800

    fix typo

--- tests/cases/fourslash/completionsObjectWithSelfAsContextualType.ts
+++ tests/cases/fourslash/completionsObjectWithSelfAsContextualType.ts
@@ -1,6 +1,6 @@
 /// <reference path="fourslash.ts" />
 
-// Test that we don't use the contexual type `{ x: any }` from the inferred type argument to `id`,
+// Test that we don't use the contextual type `{ x: any }` from the inferred type argument to `id`,
 // since that comes from the object itself.
 
 ////declare function id<T>(a: T): void;
