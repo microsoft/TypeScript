b19c50e70d712758476e959a6f37313f1e8a7450
Andy Hanson <anhans@microsoft.com>
2018-02-08 14:42:13 -0800

    Fix bug: Don't skip emitting JSX attributes

--- src/compiler/emitter.ts
+++ src/compiler/emitter.ts
@@ -2201,10 +2201,7 @@ namespace ts {
             writePunctuation("<");
             emitJsxTagName(node.tagName);
             writeSpace();
-            // We are checking here so we won't re-enter the emiting pipeline and emit extra sourcemap
-            if (node.attributes.properties && node.attributes.properties.length > 0) {
-                emit(node.attributes);
-            }
+            emit(node.attributes);
             writePunctuation("/>");
         }
 
@@ -2219,11 +2216,10 @@ namespace ts {
 
             if (isJsxOpeningElement(node)) {
                 emitJsxTagName(node.tagName);
-                // We are checking here so we won't re-enter the emitting pipeline and emit extra sourcemap
                 if (node.attributes.properties && node.attributes.properties.length > 0) {
                     writeSpace();
-                    emit(node.attributes);
                 }
+                emit(node.attributes);
             }
 
             writePunctuation(">");
