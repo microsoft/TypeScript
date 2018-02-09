849d9abdce71d6ef2a52e29d234e0a18b6217d30
xiongmao86 <superkeen@sina.cn>
2018-02-10 06:52:55 +0800

    Add new test case from the issue.

---
+++ tests/baselines/reference/assigningIncompatibleConstructor.js
@@ -0,0 +1,11 @@
+//// [assigningIncompatibleConstructor.ts]
+class Foo { constructor(a: string) { } }
+let FooConstructor: { new(): Foo } = Foo;
+
+//// [assigningIncompatibleConstructor.js]
+var Foo = /** @class */ (function () {
+    function Foo(a) {
+    }
+    return Foo;
+}());
+var FooConstructor = Foo;
