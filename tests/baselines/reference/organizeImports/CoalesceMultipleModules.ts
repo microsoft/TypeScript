fee1df34ce7cea5d76832284d542efa198e7509f
Andrew Casey <andrew.casey@microsoft.com>
2018-02-14 13:52:48 -0800

    Implement ts.OrganizeImports.removeUnusedImports
    TODO: Still need to add support for organizing imports in ambient
    modules

--- tests/baselines/reference/organizeImports/CoalesceMultipleModules.ts
+++ tests/baselines/reference/organizeImports/CoalesceMultipleModules.ts
@@ -4,8 +4,10 @@ import { d } from "lib1";
 import { b } from "lib1";
 import { c } from "lib2";
 import { a } from "lib2";
+a + b + c + d;
 
 // ==ORGANIZED==
 
 import { b, d } from "lib1";
 import { a, c } from "lib2";
+a + b + c + d;
