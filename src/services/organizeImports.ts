7a313947880ba57b0628b78c158ba9210b614d89
Andrew Casey <andrew.casey@microsoft.com>
2018-02-15 15:06:34 -0800

    Group imports before sorting and coalescing

--- src/services/organizeImports.ts
+++ src/services/organizeImports.ts
@@ -14,11 +14,15 @@ namespace ts.OrganizeImports {
             return [];
         }
 
-        const oldValidImportDecls = oldImportDecls.filter(importDecl => getExternalModuleName(importDecl.moduleSpecifier));
-        const oldInvalidImportDecls = oldImportDecls.filter(importDecl => !getExternalModuleName(importDecl.moduleSpecifier));
+        const oldImportGroups = group(oldImportDecls, importDecl => getExternalModuleName(importDecl.moduleSpecifier));
 
-        // All of the new ImportDeclarations in the file, in sorted order.
-        const newImportDecls = coalesceImports(sortImports(removeUnusedImports(oldValidImportDecls))).concat(oldInvalidImportDecls);
+        const sortedImportGroups = stableSort(oldImportGroups, (group1, group2) =>
+            compareModuleSpecifiers(group1[0].moduleSpecifier, group2[0].moduleSpecifier));
+
+        const newImportDecls = flatMap(sortedImportGroups, importGroup =>
+            getExternalModuleName(importGroup[0].moduleSpecifier)
+                ? coalesceImports(removeUnusedImports(importGroup))
+                : importGroup);
 
         const changeTracker = textChanges.ChangeTracker.fromContext({ host, formatContext });
 
@@ -47,131 +51,82 @@ namespace ts.OrganizeImports {
         return oldImports; // TODO (https://github.com/Microsoft/TypeScript/issues/10020)
     }
 
-    /* @internal */ // Internal for testing
-    export function sortImports(oldImports: ReadonlyArray<ImportDeclaration>) {
-        return stableSort(oldImports, (import1, import2) => {
-            const name1 = getExternalModuleName(import1.moduleSpecifier);
-            const name2 = getExternalModuleName(import2.moduleSpecifier);
-            Debug.assert(name1 !== undefined);
-            Debug.assert(name2 !== undefined);
-            return compareBooleans(isExternalModuleNameRelative(name1), isExternalModuleNameRelative(name2)) ||
-                compareStringsCaseSensitive(name1, name2);
-        });
-    }
-
     function getExternalModuleName(specifier: Expression) {
         return isStringLiteral(specifier) || isNoSubstitutionTemplateLiteral(specifier)
             ? specifier.text
             : undefined;
     }
 
-    /**
-     * @param sortedImports a non-empty list of ImportDeclarations, sorted by module name.
-     */
-    function groupSortedImports(sortedImports: ReadonlyArray<ImportDeclaration>): ReadonlyArray<ReadonlyArray<ImportDeclaration>> {
-        Debug.assert(length(sortedImports) > 0);
-
-        const groups: ImportDeclaration[][] = [];
-
-        let groupName: string | undefined = getExternalModuleName(sortedImports[0].moduleSpecifier);
-        Debug.assert(groupName !== undefined);
-        let group: ImportDeclaration[] = [];
-
-        for (const importDeclaration of sortedImports) {
-            const moduleName = getExternalModuleName(importDeclaration.moduleSpecifier);
-            Debug.assert(moduleName !== undefined);
-            if (moduleName === groupName) {
-                group.push(importDeclaration);
-            }
-            else if (group.length) {
-                groups.push(group);
-
-                groupName = moduleName;
-                group = [importDeclaration];
-            }
-        }
-
-        if (group.length) {
-            groups.push(group);
-        }
-
-        return groups;
-    }
-
     /* @internal */ // Internal for testing
     /**
-     * @param sortedImports a list of ImportDeclarations, sorted by module name.
+     * @param importGroup a list of ImportDeclarations, all with the same module name.
      */
-    export function coalesceImports(sortedImports: ReadonlyArray<ImportDeclaration>) {
-        if (sortedImports.length === 0) {
-            return sortedImports;
+    export function coalesceImports(importGroup: ReadonlyArray<ImportDeclaration>) {
+        if (importGroup.length === 0) {
+            return importGroup;
         }
+
+        const { importWithoutClause, defaultImports, namespaceImports, namedImports } = getImportParts(importGroup);
 
         const coalescedImports: ImportDeclaration[] = [];
 
-        const groupedImports = groupSortedImports(sortedImports);
-        for (const importGroup of groupedImports) {
-
-            const { importWithoutClause, defaultImports, namespaceImports, namedImports } = getImportParts(importGroup);
-
-            if (importWithoutClause) {
-                coalescedImports.push(importWithoutClause);
-            }
-
-            // Normally, we don't combine default and namespace imports, but it would be silly to
-            // produce two import declarations in this special case.
-            if (defaultImports.length === 1 && namespaceImports.length === 1 && namedImports.length === 0) {
-                // Add the namespace import to the existing default ImportDeclaration.
-                const defaultImportClause = defaultImports[0].parent as ImportClause;
-                coalescedImports.push(
-                    updateImportDeclarationAndClause(defaultImportClause, defaultImportClause.name, namespaceImports[0]));
-
-                continue;
-            }
-
-            const sortedNamespaceImports = stableSort(namespaceImports, (n1, n2) => compareIdentifiers(n1.name, n2.name));
-
-            for (const namespaceImport of sortedNamespaceImports) {
-                // Drop the name, if any
-                coalescedImports.push(
-                    updateImportDeclarationAndClause(namespaceImport.parent, /*name*/ undefined, namespaceImport));
-            }
-
-            if (defaultImports.length === 0 && namedImports.length === 0) {
-                continue;
-            }
-
-            let newDefaultImport: Identifier | undefined;
-            const newImportSpecifiers: ImportSpecifier[] = [];
-            if (defaultImports.length === 1) {
-                newDefaultImport = defaultImports[0];
-            }
-            else {
-                for (const defaultImport of defaultImports) {
-                    newImportSpecifiers.push(
-                        createImportSpecifier(createIdentifier("default"), defaultImport));
-                }
-            }
-
-            newImportSpecifiers.push(...flatMap(namedImports, n => n.elements));
-
-            const sortedImportSpecifiers = stableSort(newImportSpecifiers, (s1, s2) =>
-                compareIdentifiers(s1.propertyName || s1.name, s2.propertyName || s2.name) ||
-                compareIdentifiers(s1.name, s2.name));
-
-            const importClause = defaultImports.length > 0
-                ? defaultImports[0].parent as ImportClause
-                : namedImports[0].parent;
-
-            const newNamedImports = sortedImportSpecifiers.length === 0
-                ? undefined
-                : namedImports.length === 0
-                    ? createNamedImports(sortedImportSpecifiers)
-                    : updateNamedImports(namedImports[0], sortedImportSpecifiers);
-
-            coalescedImports.push(
-                updateImportDeclarationAndClause(importClause, newDefaultImport, newNamedImports));
+        if (importWithoutClause) {
+            coalescedImports.push(importWithoutClause);
         }
+
+        // Normally, we don't combine default and namespace imports, but it would be silly to
+        // produce two import declarations in this special case.
+        if (defaultImports.length === 1 && namespaceImports.length === 1 && namedImports.length === 0) {
+            // Add the namespace import to the existing default ImportDeclaration.
+            const defaultImportClause = defaultImports[0].parent as ImportClause;
+            coalescedImports.push(
+                updateImportDeclarationAndClause(defaultImportClause, defaultImportClause.name, namespaceImports[0]));
+
+            return coalescedImports;
+        }
+
+        const sortedNamespaceImports = stableSort(namespaceImports, (n1, n2) => compareIdentifiers(n1.name, n2.name));
+
+        for (const namespaceImport of sortedNamespaceImports) {
+            // Drop the name, if any
+            coalescedImports.push(
+                updateImportDeclarationAndClause(namespaceImport.parent, /*name*/ undefined, namespaceImport));
+        }
+
+        if (defaultImports.length === 0 && namedImports.length === 0) {
+            return coalescedImports;
+        }
+
+        let newDefaultImport: Identifier | undefined;
+        const newImportSpecifiers: ImportSpecifier[] = [];
+        if (defaultImports.length === 1) {
+            newDefaultImport = defaultImports[0];
+        }
+        else {
+            for (const defaultImport of defaultImports) {
+                newImportSpecifiers.push(
+                    createImportSpecifier(createIdentifier("default"), defaultImport));
+            }
+        }
+
+        newImportSpecifiers.push(...flatMap(namedImports, n => n.elements));
+
+        const sortedImportSpecifiers = stableSort(newImportSpecifiers, (s1, s2) =>
+            compareIdentifiers(s1.propertyName || s1.name, s2.propertyName || s2.name) ||
+            compareIdentifiers(s1.name, s2.name));
+
+        const importClause = defaultImports.length > 0
+            ? defaultImports[0].parent as ImportClause
+            : namedImports[0].parent;
+
+        const newNamedImports = sortedImportSpecifiers.length === 0
+            ? undefined
+            : namedImports.length === 0
+                ? createNamedImports(sortedImportSpecifiers)
+                : updateNamedImports(namedImports[0], sortedImportSpecifiers);
+
+        coalescedImports.push(
+            updateImportDeclarationAndClause(importClause, newDefaultImport, newNamedImports));
 
         return coalescedImports;
 
@@ -230,5 +185,14 @@ namespace ts.OrganizeImports {
                 updateImportClause(importClause, name, namedBindings),
                 importDeclaration.moduleSpecifier);
         }
+    }
+
+    /* internal */ // Exported for testing
+    export function compareModuleSpecifiers(m1: Expression, m2: Expression) {
+        const name1 = getExternalModuleName(m1);
+        const name2 = getExternalModuleName(m2);
+        return compareBooleans(name1 === undefined, name2 === undefined) ||
+            compareBooleans(isExternalModuleNameRelative(name1), isExternalModuleNameRelative(name2)) ||
+            compareStringsCaseSensitive(name1, name2);
     }
 }
