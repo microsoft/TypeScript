cc0489f3003318968fad4bc96e1f82b36037dc64
Josh Goldberg <josh@fullscreenmario.com>
2018-02-17 13:00:19 -0500

    Improved error diagnostics for misplaced commas and semicolons in class expressions
    If there's a comma or semicolon after a decorator in a class expression, it's useful to have a more specific error message than "declaration expected" as it's easy (even common) to accidentally place a semicolon after the decorator.

---
+++ tests/baselines/reference/decoratorWithOrWithoutSemicolon.js
@@ -0,0 +1,36 @@
+//// [decoratorWithOrWithoutSemicolon.ts]
+function decorated(): Function {
+    return (target: any): any => target;
+}
+
+class Member { }
+
+class Example {
+    @decorated()
+    public readonly member: Member;
+
+    @decorated();
+    public readonly memberSemicolon: Member;
+}
+
+
+//// [decoratorWithOrWithoutSemicolon.js]
+function decorated() {
+    return function (target) { return target; };
+}
+var Member = /** @class */ (function () {
+    function Member() {
+    }
+    return Member;
+}());
+var Example = /** @class */ (function () {
+    function Example() {
+    }
+    __decorate([
+        decorated()
+    ], Example.prototype, "member");
+    __decorate([
+        decorated()
+    ], Example.prototype, "");
+    return Example;
+}());
