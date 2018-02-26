0b1e21794d627f87c8e8484afdaa225433bc00cf
Andy <anhans@microsoft.com>
2018-02-26 14:55:26 -0800

    fourslash diagnostics tests: use objects instead of strings (#22193)

--- tests/cases/fourslash/getJavaScriptSyntacticDiagnostics1.ts
+++ tests/cases/fourslash/getJavaScriptSyntacticDiagnostics1.ts
@@ -4,12 +4,12 @@
 // @Filename: a.js
 //// import a = b;
 
-verify.getSyntacticDiagnostics(`[
+verify.getSyntacticDiagnostics([
   {
-    "message": "'import ... =' can only be used in a .ts file.",
-    "start": 0,
-    "length": 13,
-    "category": "error",
-    "code": 8002
+    message: "'import ... =' can only be used in a .ts file.",
+    start: 0,
+    length: 13,
+    category: "error",
+    code: 8002
   }
-]`);
+]);
