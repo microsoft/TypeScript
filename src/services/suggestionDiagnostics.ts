f53adf8030db1306519d5ffdaf40794eadf77c77
Andy Hanson <anhans@microsoft.com>
2018-02-28 14:07:22 -0800

    Make convertFunctionToEs6Class a codefix

--- src/services/suggestionDiagnostics.ts
+++ src/services/suggestionDiagnostics.ts
@@ -1,8 +1,28 @@
 /* @internal */
 namespace ts {
     export function computeSuggestionDiagnostics(sourceFile: SourceFile): Diagnostic[] {
-        return sourceFile.commonJsModuleIndicator
-            ? [createDiagnosticForNode(sourceFile.commonJsModuleIndicator, Diagnostics.File_is_a_CommonJS_module_it_may_be_converted_to_an_ES6_module)]
-            : emptyArray;
+        const diags: Diagnostic[] = [];
+
+        if (sourceFile.commonJsModuleIndicator) {
+            diags.push(createDiagnosticForNode(sourceFile.commonJsModuleIndicator, Diagnostics.File_is_a_CommonJS_module_it_may_be_converted_to_an_ES6_module));
+        }
+
+        function check(node: Node) {
+            switch (node.kind) {
+                case SyntaxKind.FunctionDeclaration:
+                case SyntaxKind.FunctionExpression:
+                    if (isInJavaScriptFile(node)) {
+                        const symbol = node.symbol;
+                        if (symbol.members && (symbol.members.size > 0)) {
+                            diags.push(createDiagnosticForNode(isVariableDeclaration(node.parent) ? node.parent.name : node, Diagnostics.Function_may_be_converted_to_a_class));
+                        }
+                    }
+                    break;
+            }
+            node.forEachChild(check);
+        }
+        check(sourceFile);
+
+        return diags;
     }
 }
--- tests/cases/fourslash/convertFunctionToEs6Class2.ts
+++ tests/cases/fourslash/convertFunctionToEs6Class2.ts
@@ -2,18 +2,18 @@
 
 // @allowNonTsExtensions: true
 // @Filename: test123.js
-//// [|var /*1*/foo = function() { };
-//// /*2*/foo.prototype.instanceMethod1 = function() { return "this is name"; };
-//// /*3*/foo.prototype.instanceMethod2 = () => { return "this is name"; };
-//// /*4*/foo.instanceProp1 = "hello";
-//// /*5*/foo.instanceProp2 = undefined;
-//// /*6*/foo.staticProp = "world";
-//// /*7*/foo.staticMethod1 = function() { return "this is static name"; };
-//// /*8*/foo.staticMethod2 = () => "this is static name";|]
+////var foo = function() { };
+////foo.prototype.instanceMethod1 = function() { return "this is name"; };
+////foo.prototype.instanceMethod2 = () => { return "this is name"; };
+////foo.instanceProp1 = "hello";
+////foo.instanceProp2 = undefined;
+////foo.staticProp = "world";
+////foo.staticMethod1 = function() { return "this is static name"; };
+////foo.staticMethod2 = () => "this is static name";
 
-
-['1', '2', '3', '4', '5', '6', '7', '8'].forEach(m => verify.applicableRefactorAvailableAtMarker(m));
-verify.fileAfterApplyingRefactorAtMarker('4',
+verify.codeFix({
+    description: "Convert function to an ES2015 class",
+    newFileContent:
 `class foo {
     constructor() { }
     instanceMethod1() { return "this is name"; }
@@ -24,4 +24,5 @@ verify.fileAfterApplyingRefactorAtMarker('4',
 foo.instanceProp1 = "hello";
 foo.instanceProp2 = undefined;
 foo.staticProp = "world";
-`, 'Convert to ES2015 class', 'convert');
+`,
+});
