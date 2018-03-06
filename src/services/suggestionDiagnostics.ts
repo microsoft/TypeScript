0e9d248918eb369e01d96095df6057969fc360f7
Andy Hanson <anhans@microsoft.com>
2018-03-06 07:25:46 -0800

    Compute isJsFile once at top

--- src/services/suggestionDiagnostics.ts
+++ src/services/suggestionDiagnostics.ts
@@ -9,11 +9,13 @@ namespace ts {
             diags.push(createDiagnosticForNode(sourceFile.commonJsModuleIndicator, Diagnostics.File_is_a_CommonJS_module_it_may_be_converted_to_an_ES6_module));
         }
 
+        const isJsFile = isSourceFileJavaScript(sourceFile);
+
         function check(node: Node) {
             switch (node.kind) {
                 case SyntaxKind.FunctionDeclaration:
                 case SyntaxKind.FunctionExpression:
-                    if (isInJavaScriptFile(node)) {
+                    if (isJsFile) {
                         const symbol = node.symbol;
                         if (symbol.members && (symbol.members.size > 0)) {
                             diags.push(createDiagnosticForNode(isVariableDeclaration(node.parent) ? node.parent.name : node, Diagnostics.This_constructor_function_may_be_converted_to_a_class_declaration));
@@ -22,7 +24,7 @@ namespace ts {
                     break;
             }
 
-            if (!isInJavaScriptFile(node) && codefix.parameterShouldGetTypeFromJSDoc(node)) {
+            if (!isJsFile && codefix.parameterShouldGetTypeFromJSDoc(node)) {
                 diags.push(createDiagnosticForNode(node.name || node, Diagnostics.JSDoc_types_may_be_moved_to_TypeScript_types));
             }
 
