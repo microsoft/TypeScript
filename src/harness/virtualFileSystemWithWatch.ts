eb0955ce571ad757811f93d868acdeba7bfe17d2
Sheetal Nandi <shkamat@microsoft.com>
2018-03-09 13:46:55 -0800

    Enable the commented out test and convert folder entries to sorted list

--- src/harness/virtualFileSystemWithWatch.ts
+++ src/harness/virtualFileSystemWithWatch.ts
@@ -88,7 +88,7 @@ interface Array<T> {}`
     }
 
     interface Folder extends FSEntry {
-        entries: FSEntry[];
+        entries: SortedArray<FSEntry>;
     }
 
     interface SymLink extends FSEntry {
@@ -512,7 +512,7 @@ interface Array<T> {}`
         }
 
         private addFileOrFolderInFolder(folder: Folder, fileOrDirectory: File | Folder | SymLink, ignoreWatch?: boolean) {
-            folder.entries.push(fileOrDirectory);
+            insertSorted(folder.entries, fileOrDirectory, (a, b) => compareStringsCaseSensitive(getBaseFileName(a.path), getBaseFileName(b.path)));
             folder.modifiedTime = new Date();
             this.fs.set(fileOrDirectory.path, fileOrDirectory);
 
@@ -622,7 +622,7 @@ interface Array<T> {}`
 
         private toFolder(path: string): Folder {
             const folder = this.toFsEntry(path) as Folder;
-            folder.entries = [];
+            folder.entries = [] as SortedArray<FSEntry>;
             return folder;
         }
 
