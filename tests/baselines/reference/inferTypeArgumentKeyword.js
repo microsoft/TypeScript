aecc82381c6e57883ea7e6b418367284ee249413
Wesley Wigham <wewigham@microsoft.com>
2018-03-09 16:55:51 -0800

    Slightly more tests

--- tests/baselines/reference/inferTypeArgumentKeyword.js
+++ tests/baselines/reference/inferTypeArgumentKeyword.js
@@ -2,19 +2,39 @@
 declare function foo<A, B, C>(x: A, y: B, z: { z: C }): A & B & C;
 
 // good
-foo<infer A, {x: string}, A>({y: 12}, {x: "yes"}, {z: {y: 12}});
+var a = foo<infer A, {x: string}, A>({y: 12}, {x: "yes"}, {z: {y: 12}});
 
 // error on 3rd arg
-foo<infer A, {x: string}, A>({y: 12}, {x: "yes"}, {z: 12});
+var b = foo<infer A, {x: string}, A>({y: 12}, {x: "yes"}, {z: 12});
 
 // error on first arg
-foo<A, {x: string}, infer A>({y: 12}, {x: "yes"}, {z: 12});
+var c = foo<A, {x: string}, infer A>({y: 12}, {x: "yes"}, {z: 12});
 
+type Ob<T> = {y: T};
+// good
+var d = foo<Ob<infer A>, {x: string}, A>({y: 12}, {x: "yes"}, {z: 12});
+
+// error on 3rd arg
+var e = foo<Ob<infer A>, {x: string}, A>({y: 12}, {x: "yes"}, {z: "no"});
+
+// good
+var e = foo<{y: A}, {x: string}, Ob<infer A>>({y: 12}, {x: "yes"}, {z: { y: 12 }});
+
+// error on 1st arg
+var f = foo<{y: A}, {x: string}, Ob<infer A>>({y: "no"}, {x: "yes"}, {z: { y: 12 }});
 
 //// [inferTypeArgumentKeyword.js]
 // good
-foo({ y: 12 }, { x: "yes" }, { z: { y: 12 } });
+var a = foo({ y: 12 }, { x: "yes" }, { z: { y: 12 } });
 // error on 3rd arg
-foo({ y: 12 }, { x: "yes" }, { z: 12 });
+var b = foo({ y: 12 }, { x: "yes" }, { z: 12 });
 // error on first arg
-foo({ y: 12 }, { x: "yes" }, { z: 12 });
+var c = foo({ y: 12 }, { x: "yes" }, { z: 12 });
+// good
+var d = foo({ y: 12 }, { x: "yes" }, { z: 12 });
+// error on 3rd arg
+var e = foo({ y: 12 }, { x: "yes" }, { z: "no" });
+// good
+var e = foo({ y: 12 }, { x: "yes" }, { z: { y: 12 } });
+// error on 1st arg
+var f = foo({ y: "no" }, { x: "yes" }, { z: { y: 12 } });
--- tests/cases/conformance/types/typeParameters/typeArgumentLists/inferTypeArgumentKeyword.ts
+++ tests/cases/conformance/types/typeParameters/typeArgumentLists/inferTypeArgumentKeyword.ts
@@ -9,5 +9,15 @@ var b = foo<infer A, {x: string}, A>({y: 12}, {x: "yes"}, {z: 12});
 // error on first arg
 var c = foo<A, {x: string}, infer A>({y: 12}, {x: "yes"}, {z: 12});
 
+type Ob<T> = {y: T};
 // good
-var d = foo<{y: infer A}, {x: string}, A>({y: 12}, {x: "yes"}, {z: 12});
+var d = foo<Ob<infer A>, {x: string}, A>({y: 12}, {x: "yes"}, {z: 12});
+
+// error on 3rd arg
+var e = foo<Ob<infer A>, {x: string}, A>({y: 12}, {x: "yes"}, {z: "no"});
+
+// good
+var e = foo<{y: A}, {x: string}, Ob<infer A>>({y: 12}, {x: "yes"}, {z: { y: 12 }});
+
+// error on 1st arg
+var f = foo<{y: A}, {x: string}, Ob<infer A>>({y: "no"}, {x: "yes"}, {z: { y: 12 }});
