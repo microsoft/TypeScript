f190842d131646c07c82b36573f058e259c3eda8
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-03-13 10:44:02 -0700

    Fix crash after `var = require('x')` type resolution (#22452)
    * Harden var x=require name resolution+update chrome baseline
    
    Chrome-devtools crashed after the new `var = require('x')` resolution
    because it forgot to check whether the declaration had an initializer.
    
    * Update chrome-devtools-frontend baseline

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -2051,6 +2051,7 @@ namespace ts {
                     }
                     if (namespace.valueDeclaration &&
                         isVariableDeclaration(namespace.valueDeclaration) &&
+                        namespace.valueDeclaration.initializer &&
                         isCommonJsRequire(namespace.valueDeclaration.initializer)) {
                         const moduleName = (namespace.valueDeclaration.initializer as CallExpression).arguments[0] as StringLiteral;
                         const moduleSym = resolveExternalModuleName(moduleName, moduleName);
