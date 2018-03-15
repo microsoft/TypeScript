58b3c16b47260a1f998df4b36520aae1905ba282
Andy <anhans@microsoft.com>
2018-03-15 08:13:16 -0700

    Don't add external module completions in a pure commonjs file (#22583)

--- tests/cases/fourslash/completionsImport_require.ts
+++ tests/cases/fourslash/completionsImport_require.ts
@@ -14,20 +14,8 @@
 ////fo/*c*/
 
 goTo.marker("b");
-verify.completionListContains({ name: "foo", source: "/a" }, "const foo: 0", "", "const", /*spanIndex*/ undefined, /*hasAction*/ true, {
-    includeExternalModuleExports: true,
-    sourceDisplay: "./a",
-});
-
-verify.applyCodeActionFromCompletion("b", {
-    name: "foo",
-    source: "/a",
-    description: `Import 'foo' from module "./a"`,
-    newFileContent: `import { foo } from "./a";
-
-const a = require("./a");
-fo`,
-});
+// Doesn't activate for commonjs-only module
+verify.not.completionListContains({ name: "foo", source: "/a" });
 
 goTo.marker("c");
 verify.completionListContains({ name: "foo", source: "/a" }, "const foo: 0", "", "const", /*spanIndex*/ undefined, /*hasAction*/ true, {
