c8fa6d1544a12c951b267b3cc4bf78c72a8874fe
Karol Janyst <lapkom@gmail.com>
2018-03-21 01:15:28 +0900

    Add types for new Intl.getCanonicalLocales method

---
+++ src/lib/es2019.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="lib.es2018.d.ts" />
+/// <reference path="lib.es2019.intl.d.ts" />
---
+++ src/lib/es2019.intl.d.ts
@@ -0,0 +1,3 @@
+declare namespace Intl {
+    function getCanonicalLocales(locales: string | string[]): string[];
+}
