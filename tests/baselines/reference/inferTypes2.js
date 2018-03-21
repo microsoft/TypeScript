09da98b18556d4eb021085af22f4452f4d219e6b
Anders Hejlsberg <andersh@microsoft.com>
2018-03-21 10:03:57 -0700

    Accept new baselines

---
+++ tests/baselines/reference/inferTypes2.js
@@ -0,0 +1,22 @@
+//// [inferTypes2.ts]
+// Repro from #22755
+
+export declare function foo<T>(obj: T): T extends () => infer P ? P : never;
+export function bar<T>(obj: T) {
+    return foo(obj);
+}
+
+
+//// [inferTypes2.js]
+"use strict";
+// Repro from #22755
+exports.__esModule = true;
+function bar(obj) {
+    return foo(obj);
+}
+exports.bar = bar;
+
+
+//// [inferTypes2.d.ts]
+export declare function foo<T>(obj: T): T extends () => infer P ? P : never;
+export declare function bar<T>(obj: T): T extends () => infer P ? P : never;
