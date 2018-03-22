3b83af8755cb441b38b05c076d40825c5cfe76d4
Wesley Wigham <wewigham@microsoft.com>
2018-03-22 14:57:42 -0700

    Break up conditional a bit

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -15392,7 +15392,10 @@ namespace ts {
             if (hasSpreadAnyType) {
                 return anyType;
             }
-            return typeToIntersect && spread !== emptyObjectType ? getIntersectionType([typeToIntersect, spread]) : (typeToIntersect || (spread === emptyObjectType ? createJsxAttributesType() : spread));
+            if (typeToIntersect && spread !== emptyObjectType) {
+                return getIntersectionType([typeToIntersect, spread]);
+            }
+            return typeToIntersect || (spread === emptyObjectType ? createJsxAttributesType() : spread);
 
             /**
              * Create anonymous type from given attributes symbol table.
