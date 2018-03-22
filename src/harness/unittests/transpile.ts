29cdf671aed0d02448c71d36d8b372f05b0efd07
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-03-23 03:46:38 +0500

    Add unit test for transpile with emit the pipeline operator

--- src/harness/unittests/transpile.ts
+++ src/harness/unittests/transpile.ts
@@ -192,6 +192,20 @@ var x = 0;`, {
                 }
             });
 
+        transpilesCorrectly("Transpile with emit the pipeline operator",
+            `var inc = (x: number) => x + 1;\n` +
+            `var res = 10 |> inc;\n`, {
+                options: {
+                    compilerOptions: {
+                        module: ModuleKind.CommonJS,
+                        newLine: NewLineKind.LineFeed,
+                        noEmitHelpers: true,
+                        experimentalPipelineOperator: true,
+                        target: ScriptTarget.ES5,
+                    }
+                }
+            });
+
         transpilesCorrectly("Supports backslashes in file name", "var x", {
             options: { fileName: "a\\b.ts" }
         });
