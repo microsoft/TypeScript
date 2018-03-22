b51d7dea63b3ec7e799d55721354d445d5dab410
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-03-23 04:05:28 +0500

    Add "pipelineOperator" test

---
+++ tests/baselines/reference/pipelineOperator.js
@@ -0,0 +1,10 @@
+//// [pipelineOperator.ts]
+var inc = (x: number) => x + 1;
+
+var res = 10 |> inc;
+
+
+//// [pipelineOperator.js]
+var inc = function (x) { return x + 1; };
+var res = (_ref_1 = 10, inc(_ref_1));
+var _ref_1;
