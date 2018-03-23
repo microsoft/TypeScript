0955531c73328c32009881572f7520c9e1fa5000
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-03-23 05:21:46 +0500

    Add "pipelineOperatorWithEvaluation" test

---
+++ tests/baselines/reference/pipelineOperatorWithEvaluation.js
@@ -0,0 +1,28 @@
+//// [pipelineOperatorWithEvaluation.ts]
+var obj = {
+  _prop: 0,
+
+  get prop() {
+    return this._prop = 1;
+  },
+
+  get method() {
+    return (v: number) => v + 1;
+  }
+};
+
+var result = obj.prop |> obj.method;
+
+
+//// [pipelineOperatorWithEvaluation.js]
+var obj = {
+    _prop: 0,
+    get prop() {
+        return this._prop = 1;
+    },
+    get method() {
+        return (v) => v + 1;
+    }
+};
+var result = (_ref_1 = obj.prop, obj.method(_ref_1));
+var _ref_1;
