6786d7473d8c5794c9b1a40651cd3e5edb51c67b
Arthur Ozga <arozga@microsoft.com>
2018-03-26 16:53:20 -0700

    fix test

--- tests/cases/fourslash/formattingObjectLiteralOpenCurlySingleLine.ts
+++ tests/cases/fourslash/formattingObjectLiteralOpenCurlySingleLine.ts
@@ -2,22 +2,22 @@
 
 //// 
 //// let obj1 =
-////     { x: 10 };
+//// { x: 10 };
 //// 
 //// let obj2 =
 ////     // leading trivia
-////     { y: 10 };
+//// { y: 10 };
 //// 
 
 format.document();
 verify.currentFileContentIs(
 `
 let obj1 =
-{ x: 10 };
+    { x: 10 };
 
 let obj2 =
     // leading trivia
-{ y: 10 };
+    { y: 10 };
 `
 );
 
