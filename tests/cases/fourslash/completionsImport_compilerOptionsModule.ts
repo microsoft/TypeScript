d88b884273ca93bd326289b84390bc2423df30f6
Andy Hanson <anhans@microsoft.com>
2018-03-30 15:16:37 -0700

    Even smarter shouldOfferImportCompletions

--- tests/cases/fourslash/completionsImport_compilerOptionsModule.ts
+++ tests/cases/fourslash/completionsImport_compilerOptionsModule.ts
@@ -3,7 +3,7 @@
 // @allowJs: true
 // @module: commonjs
 
-// @Filename: /a.ts
+// @Filename: /node_modules/a/index.d.ts
 ////export const foo = 0;
 
 // @Filename: /b.js
@@ -11,20 +11,30 @@
 ////fo/*b*/
 
 // @Filename: /c.js
-////const x = 0;/*c*/
+////const x = 0;/*c*/ // Off for JS files (unless a non-declaration external module exists in the project)
+
+// @Filename: /c2.ts
+////const x = 0;/*c2*/
 
 // @Filename: /d.js
 ////const a = import("./a"); // Does not make this an external module
 ////fo/*d*/
 
-goTo.marker("b");
-verify.completionListContains("x");
-verify.not.completionListContains({ name: "foo", source: "/a" });
+// @Filename: /d2.ts
+////const a = import("./a"); // Does not make this an external module
+////fo/*d2*/
 
-for (const marker of ["c", "d"]) {
+for (const marker of ["b", "c", "d"]) {
     goTo.marker(marker);
-    verify.completionListContains({ name: "foo", source: "/a" }, "const foo: 0", "", "const", /*spanIndex*/ undefined, /*hasAction*/ true, {
+    verify.not.completionListContains({ name: "foo", source: "/node_modules/a/index" }, undefined, undefined, undefined, undefined, undefined, {
+        includeCompletionsForModuleExports: true
+    });
+}
+
+for (const marker of ["c2", "d2"]) {
+    goTo.marker(marker);
+    verify.completionListContains({ name: "foo", source: "/node_modules/a/index" }, "const foo: 0", "", "const", /*spanIndex*/ undefined, /*hasAction*/ true, {
         includeCompletionsForModuleExports: true,
-        sourceDisplay: "./a",
+        sourceDisplay: "a",
     });
 }
