ad533fe1b3a57af8e3763df51a095ec5e2fd6ae0
Andy Hanson <anhans@microsoft.com>
2018-03-30 08:45:21 -0700

    findAllReferences: Share code between populateSearchSymbolSet and getRelatedSymbol

--- tests/cases/fourslash/findAllRefsObjectBindingElementPropertyName07.ts
+++ tests/cases/fourslash/findAllRefsObjectBindingElementPropertyName07.ts
@@ -4,10 +4,4 @@
 ////
 ////p, [{ [|{| "isWriteAccess": true, "isDefinition": true |}a|]: p, b }] = [{ [|{| "isWriteAccess": true, "isDefinition": true |}a|]: 10, b: true }];
 
-const ranges = test.ranges();
-const [r0, r1] = ranges;
-verify.referenceGroups(r0, [{ definition: "(property) a: any", ranges }]);
-verify.referenceGroups(r1, [
-    { definition: "(property) a: any", ranges: [r0] },
-    { definition: "(property) a: number", ranges: [r1] }
-]);
+verify.singleReferenceGroup("(property) a: any");
