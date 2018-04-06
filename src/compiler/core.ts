b8f6dbfe9455d399c01896492529f3af3b57a3b3
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-04-06 11:04:48 +0200

    declare ts.versionMajorMinor as string

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -4,7 +4,7 @@
 namespace ts {
     // WARNING: The script `configureNightly.ts` uses a regexp to parse out these values.
     // If changing the text in this section, be sure to test `configureNightly` too.
-    export const versionMajorMinor = "2.9";
+    export const versionMajorMinor: string = "2.9";
     /** The version of the TypeScript compiler release */
     export const version = `${versionMajorMinor}.0-dev`;
 }
