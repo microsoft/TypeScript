9673d20ee38e515e2d301a2fb98b9b6972ad697e
Andy Hanson <anhans@microsoft.com>
2018-04-06 15:01:06 -0700

    Update rename tests

--- tests/cases/fourslash/renameDestructuringAssignmentNestedInFor2.ts
+++ tests/cases/fourslash/renameDestructuringAssignmentNestedInFor2.ts
@@ -3,16 +3,20 @@
 ////interface MultiRobot {
 ////    name: string;
 ////    skills: {
-////        primary: string;
+////        [|primary|]: string;
 ////        secondary: string;
 ////    };
 ////}
 ////let multiRobot: MultiRobot, [|primary|]: string;
-////for ({ skills: { primary: primaryA, secondary: secondaryA } } = multiRobot, i = 0; i < 1; i++) {
+////for ({ skills: { [|primary|]: primaryA, secondary: secondaryA } } = multiRobot, i = 0; i < 1; i++) {
 ////    console.log(primaryA);
 ////}
 ////for ({ skills: { [|primary|], secondary } } = multiRobot, i = 0; i < 1; i++) {
 ////    console.log([|primary|]);
 ////}
 
-verify.rangesAreRenameLocations();
+const ranges = test.ranges();
+const [r0, r1, r2, r3, r4] = ranges;
+verify.renameLocations([r0, r2], [r0, r2, r3]);
+verify.renameLocations([r1, r4], [r1, r3, r4]);
+verify.renameLocations(r3, ranges);
