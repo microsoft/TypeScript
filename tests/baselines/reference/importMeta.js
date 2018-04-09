9b8670cf9208c2681548b2859957297d268535cc
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-04-09 15:28:37 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/importMeta.js
@@ -0,0 +1,48 @@
+//// [tests/cases/conformance/es2019/importMeta/importMeta.ts] ////
+
+//// [example.ts]
+// Adapted from https://github.com/tc39/proposal-import-meta/tree/c3902a9ffe2e69a7ac42c19d7ea74cbdcea9b7fb#example
+(async () => {
+  const response = await fetch(new URL("../hamsters.jpg", import.meta.url).toString());
+  const blob = await response.blob();
+
+  const size = import.meta.scriptElement.dataset.size || 300;
+
+  const image = new Image();
+  image.src = URL.createObjectURL(blob);
+  image.width = image.height = size;
+
+  document.body.appendChild(image);
+})();
+
+//// [moduleLookingFile01.ts]
+export let x = import.meta;
+export let y = import.metal;
+export let z = import.import.import.malkovich;
+
+//// [scriptLookingFile01.ts]
+let globalA = import.meta;
+let globalB = import.metal;
+let globalC = import.import.import.malkovich;
+
+
+//// [example.js]
+// Adapted from https://github.com/tc39/proposal-import-meta/tree/c3902a9ffe2e69a7ac42c19d7ea74cbdcea9b7fb#example
+(async () => {
+    const response = await fetch(new URL("../hamsters.jpg", meta.url).toString());
+    const blob = await response.blob();
+    const size = , meta, scriptElement, dataset, size;
+     || 300;
+    const image = new Image();
+    image.src = URL.createObjectURL(blob);
+    image.width = image.height = size;
+    document.body.appendChild(image);
+})();
+//// [moduleLookingFile01.js]
+export let x = , meta;
+export let y = , metal;
+export let z = , malkovich;
+//// [scriptLookingFile01.js]
+let globalA = , meta;
+let globalB = , metal;
+let globalC = , malkovich;
