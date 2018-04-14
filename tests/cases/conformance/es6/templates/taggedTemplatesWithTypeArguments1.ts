b409888cbcce4b0c393d9fa7b255905b5a6efcad
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-04-13 23:01:24 -0700

    Added tests.

---
+++ tests/cases/conformance/es6/templates/taggedTemplatesWithTypeArguments1.ts
@@ -0,0 +1,44 @@
+// @target: esnext
+
+declare function f<T>(strs: TemplateStringsArray, ...callbacks: Array<(x: T) => any>): void;
+
+interface Stuff {
+    x: number;
+    y: string;
+    z: boolean;
+}
+
+export const a = f<Stuff> `
+    hello
+    ${stuff => stuff.x}
+    brave
+    ${stuff => stuff.y}
+    world
+    ${stuff => stuff.z}
+`;
+
+declare function g<Input, T, U, V>(
+    strs: TemplateStringsArray,
+    t: (i: Input) => T, u: (i: Input) => U, v: (i: Input) => V): T | U | V;
+
+export const b = g<Stuff, number, string, boolean> `
+    hello
+    ${stuff => stuff.x}
+    brave
+    ${stuff => stuff.y}
+    world
+    ${stuff => stuff.z}
+`;
+
+declare let obj: {
+    prop: <T>(strs: TemplateStringsArray, x: (input: T) => T) => {
+        returnedObjProp: {
+            lastOne: T
+        }
+    }
+}
+
+export const c = obj["prop"]<Stuff> `${(input) => { ...input }}`
+c.returnedProp.x;
+c.returnedProp.y;
+c.returnedProp.z;
