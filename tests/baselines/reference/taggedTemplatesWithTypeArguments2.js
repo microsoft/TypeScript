2510c19fbd64baba32d9b4c656c9902163fb10d7
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-04-15 15:59:23 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/taggedTemplatesWithTypeArguments2.js
@@ -0,0 +1,25 @@
+//// [taggedTemplatesWithTypeArguments2.ts]
+export interface SomethingTaggable {
+    <T>(t: TemplateStringsArray, ...args: T[]): SomethingNewable;
+}
+
+export interface SomethingNewable {
+    new <T>(...args: T[]): any;
+}
+
+declare const tag: SomethingTaggable;
+
+const a = new tag `${100} ${200}`<string>("hello", "world");
+
+const b = new tag<number> `${"hello"} ${"world"}`(100, 200);
+
+const c = new tag<number> `${100} ${200}`<string>("hello", "world");
+
+const d = new tag<number> `${"hello"} ${"world"}`<string>(100, 200);
+
+
+//// [taggedTemplatesWithTypeArguments2.js]
+const a = new tag `${100} ${200}`("hello", "world");
+const b = new tag(`${"hello"} ${"world"}`(100, 200));
+const c = new tag(`${100} ${200}`("hello", "world"));
+const d = new tag(`${"hello"} ${"world"}`(100, 200));
