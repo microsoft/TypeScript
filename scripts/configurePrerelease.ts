4288e1d5a603692710f6d7ce4c4a66ececf37232
Wesley Wigham <wewigham@microsoft.com>
2018-04-17 12:15:58 -0700

    Remove alias

--- scripts/configurePrerelease.ts
+++ scripts/configurePrerelease.ts
@@ -1,7 +1,7 @@
 /// <reference types="node"/>
 import { normalize } from "path";
 import assert = require("assert");
-import { readFileSync as readFile, writeFileSync as writeFile } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 const args = process.argv.slice(2);
 
 
@@ -28,14 +28,14 @@ function main(): void {
 
     // Acquire the version from the package.json file and modify it appropriately.
     const packageJsonFilePath = normalize(args[1]);
-    const packageJsonValue: PackageJson = JSON.parse(readFile(packageJsonFilePath).toString());
+    const packageJsonValue: PackageJson = JSON.parse(readFileSync(packageJsonFilePath).toString());
 
     const { majorMinor, patch } = parsePackageJsonVersion(packageJsonValue.version);
     const prereleasePatch = getPrereleasePatch(tag, patch);
 
     // Acquire and modify the source file that exposes the version string.
     const tsFilePath = normalize(args[2]);
-    const tsFileContents = readFile(tsFilePath).toString();
+    const tsFileContents = readFileSync(tsFilePath).toString();
     const modifiedTsFileContents = updateTsFile(tsFilePath, tsFileContents, majorMinor, patch, prereleasePatch);
 
     // Ensure we are actually changing something - the user probably wants to know that the update failed.
@@ -48,8 +48,8 @@ function main(): void {
     // Finally write the changes to disk.
     // Modify the package.json structure
     packageJsonValue.version = `${majorMinor}.${prereleasePatch}`;
-    writeFile(packageJsonFilePath, JSON.stringify(packageJsonValue, /*replacer:*/ undefined, /*space:*/ 4))
-    writeFile(tsFilePath, modifiedTsFileContents);
+    writeFileSync(packageJsonFilePath, JSON.stringify(packageJsonValue, /*replacer:*/ undefined, /*space:*/ 4))
+    writeFileSync(tsFilePath, modifiedTsFileContents);
 }
 
 function updateTsFile(tsFilePath: string, tsFileContents: string, majorMinor: string, patch: string, nightlyPatch: string): string {
