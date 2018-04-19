87bb96d7b24f6cb27dafddde6de80476d3a4dd5a
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-04-19 15:15:53 -0700

    Accepted baselines.

--- tests/baselines/reference/taggedTemplatesWithTypeArguments2.js
+++ tests/baselines/reference/taggedTemplatesWithTypeArguments2.js
@@ -28,6 +28,16 @@ const d = new tag<number> `${"hello"} ${"world"}`<string>(100, 200);
 const e = new tag<number>
 `hello`();
 
+class SomeBase<A, B, C> {
+    a!: A; b!: B; c!: C;
+}
+
+class SomeDerived<T> extends SomeBase<number, string, T> {
+    constructor() {
+        super<number, string, T> `hello world`;
+    }
+}
+
 //// [taggedTemplatesWithTypeArguments2.js]
 const a = new tag `${100} ${200}`("hello", "world");
 const b = new tag `${"hello"} ${"world"}`(100, 200);
@@ -42,3 +52,10 @@ const d = (new tag `${"hello"} ${"world"}`)(100, 200);
  * ```
  */
 const e = new tag `hello`();
+class SomeBase {
+}
+class SomeDerived extends SomeBase {
+    constructor() {
+        super. `hello world`;
+    }
+}
