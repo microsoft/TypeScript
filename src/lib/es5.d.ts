a55febda779bd9c24b1d3b454860a40b351c158e
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-04-24 12:21:36 -0700

    Make 'ImportMeta' more minimal.

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -505,8 +505,13 @@ interface TemplateStringsArray extends ReadonlyArray<string> {
     readonly raw: ReadonlyArray<string>;
 }
 
+/**
+ * The type of `import.meta`.
+ * 
+ * If you need to declare that a given property exists on `import.meta`,
+ * this type may be augmented via interface merging.
+ */
 interface ImportMeta {
-    [propertyName: string]: any;
 }
 
 interface Math {
