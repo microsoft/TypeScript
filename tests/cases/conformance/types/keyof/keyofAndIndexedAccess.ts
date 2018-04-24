57ef618120f9f7463bf9cf0edcbf8be793287e59
Anders Hejlsberg <andersh@microsoft.com>
2018-04-24 10:02:47 -0700

    Add regression test

--- tests/cases/conformance/types/keyof/keyofAndIndexedAccess.ts
+++ tests/cases/conformance/types/keyof/keyofAndIndexedAccess.ts
@@ -567,3 +567,12 @@ function f3<T, K extends Extract<keyof T, string>>(t: T, k: K, tk: T[K]): void {
 type Predicates<TaggedRecord> = {
   [T in keyof TaggedRecord]: (variant: TaggedRecord[keyof TaggedRecord]) => variant is TaggedRecord[T]
 }
+
+// Repros from #23592
+
+type Example<T extends { [K in keyof T]: { prop: any } }> = { [K in keyof T]: T[K]["prop"] };
+type Result = Example<{ a: { prop: string }; b: { prop: number } }>;
+
+type Helper2<T> = { [K in keyof T]: Extract<T[K], { prop: any }> };
+type Example2<T> = { [K in keyof Helper2<T>]: Helper2<T>[K]["prop"] };
+type Result2 = Example2<{ 1: { prop: string }; 2: { prop: number } }>;
