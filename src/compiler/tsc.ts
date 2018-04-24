0900823aed40cf01a9517e1ba2d54ac83a75d39d
Mohamed Hegazy <mhegazy@microsoft.com>
2018-04-24 13:59:09 -0700

    Fix one more use of sys.getEnvironmentVariable

--- src/compiler/tsc.ts
+++ src/compiler/tsc.ts
@@ -396,7 +396,7 @@ if (ts.Debug.isDebugging) {
     ts.Debug.enableDebugInfo();
 }
 
-if (ts.sys.tryEnableSourceMapsForHost && /^development$/i.test(ts.sys.getEnvironmentVariable("NODE_ENV")!)) {
+if (ts.sys.tryEnableSourceMapsForHost && /^development$/i.test(ts.sys.getEnvironmentVariable("NODE_ENV"))) {
     ts.sys.tryEnableSourceMapsForHost();
 }
 
