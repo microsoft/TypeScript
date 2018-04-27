9ab6fd4c15075104ea4e8bdb9ea0ca94ae557d80
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-04-27 14:25:59 -0700

    Remove readonly from object rest properties
    Works the same as removing it from object spread properties

---
+++ tests/cases/conformance/types/rest/objectRestReadonly.ts
@@ -0,0 +1,16 @@
+// #23374
+type ObjType = {
+  foo: string
+  baz: string
+  quux: string
+}
+
+const obj: Readonly<ObjType> = {
+  foo: 'bar',
+  baz: 'qux',
+  quux: 'quuz',
+}
+
+const { foo, ...rest } = obj
+
+delete rest.baz
