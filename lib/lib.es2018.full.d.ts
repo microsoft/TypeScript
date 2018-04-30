cc0c82c01492b7cdb4f57c08074158216c9a7314
Derek P Sifford <dereksifford@gmail.com>
2018-04-30 15:17:00 -0400

    add lib.es2018.intl + fix lib.es2017.intl
    closes #23691

--- lib/lib.es2018.full.d.ts
+++ lib/lib.es2018.full.d.ts
@@ -21,6 +21,7 @@ and limitations under the License.
 /// <reference path="lib.es2017.d.ts" />
 /// <reference path="lib.es2018.promise.d.ts" />
 /// <reference path="lib.es2018.regexp.d.ts" />
+/// <reference path="lib.es2018.intl.d.ts" />
 
 /////////////////////////////
 /// DOM APIs
