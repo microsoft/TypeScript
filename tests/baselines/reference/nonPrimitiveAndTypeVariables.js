0066b027724a31657cddff003d3bde45d2b3014b
Anders Hejlsberg <andersh@microsoft.com>
2018-05-01 13:06:19 -0700

    Accept new baselines

--- tests/baselines/reference/nonPrimitiveAndTypeVariables.js
+++ tests/baselines/reference/nonPrimitiveAndTypeVariables.js
@@ -4,8 +4,8 @@
 type A<T, V> = { [P in keyof T]: T[P] extends V ? 1 : 0; };
 type B<T, V> = { [P in keyof T]: T[P] extends V | object ? 1 : 0; };
 
-type a = A<{ a: 0 | 1 }, 0>; // { a: 0; }
-type b = B<{ a: 0 | 1 }, 0>; // { a: 0; }
+let a: A<{ a: 0 | 1 }, 0> = { a: 0 };
+let b: B<{ a: 0 | 1 }, 0> = { a: 0 };
 
 function foo<T, U>(x: T) {
     let a: object = x;  // Error
@@ -16,6 +16,8 @@ function foo<T, U>(x: T) {
 //// [nonPrimitiveAndTypeVariables.js]
 "use strict";
 // Repros from #23800
+var a = { a: 0 };
+var b = { a: 0 };
 function foo(x) {
     var a = x; // Error
     var b = x; // Error
