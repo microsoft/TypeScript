4c933aef9a5c86aecafa6c1f3bafaf960a49c02e
Anders Hejlsberg <andersh@microsoft.com>
2018-05-01 13:05:49 -0700

    Check that test cases produce expected types

--- tests/cases/conformance/types/nonPrimitive/nonPrimitiveAndTypeVariables.ts
+++ tests/cases/conformance/types/nonPrimitive/nonPrimitiveAndTypeVariables.ts
@@ -5,8 +5,8 @@
 type A<T, V> = { [P in keyof T]: T[P] extends V ? 1 : 0; };
 type B<T, V> = { [P in keyof T]: T[P] extends V | object ? 1 : 0; };
 
-type a = A<{ a: 0 | 1 }, 0>; // { a: 0; }
-type b = B<{ a: 0 | 1 }, 0>; // { a: 0; }
+let a: A<{ a: 0 | 1 }, 0> = { a: 0 };
+let b: B<{ a: 0 | 1 }, 0> = { a: 0 };
 
 function foo<T, U>(x: T) {
     let a: object = x;  // Error
