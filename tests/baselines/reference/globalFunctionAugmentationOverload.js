cc869ce4ee85026c3acf7619ce68da47573ede98
Wesley Wigham <wewigham@microsoft.com>
2018-05-03 10:53:25 -0700

    Remove unneeded test parts from repro

--- tests/baselines/reference/globalFunctionAugmentationOverload.js
+++ tests/baselines/reference/globalFunctionAugmentationOverload.js
@@ -2,7 +2,6 @@
 
 //// [mod.d.ts]
 declare function expect(spy: Function): void;
-declare function expect<T>(actual: ArrayLike<T>): void;
 declare module "mod" {
     class mod {}
     export = mod;
@@ -13,8 +12,6 @@ import "mod";
 declare global {
     function expect(element: string): void;
 }
-//// [index.d.ts]
-declare function expect(spy: Function): void;
 
 //// [mine.js]
 "use strict";
--- tests/cases/compiler/globalFunctionAugmentationOverload.ts
+++ tests/cases/compiler/globalFunctionAugmentationOverload.ts
@@ -1,6 +1,5 @@
 // @filename: mod.d.ts
 declare function expect(spy: Function): void;
-declare function expect<T>(actual: ArrayLike<T>): void;
 declare module "mod" {
     class mod {}
     export = mod;
@@ -11,5 +10,3 @@ import "mod";
 declare global {
     function expect(element: string): void;
 }
-// @filename: index.d.ts
-declare function expect(spy: Function): void;
