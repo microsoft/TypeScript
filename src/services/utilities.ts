df9e2621d277a3eec4e824e7065d9440d66be3b4
Andy <anhans@microsoft.com>
2018-05-04 13:32:40 -0700

    Fix find-all-refs for import("mod").Foo where Foo is a typedef (#23881)
    * Fix find-all-refs for import("mod").Foo where Foo is a typedef
    
    * Add Debug.assertNever message
    
    * Ensure SemanticMeaning is matched

--- src/services/utilities.ts
+++ src/services/utilities.ts
@@ -180,6 +180,8 @@ namespace ts {
         switch (node.parent.kind) {
             case SyntaxKind.TypeReference:
                 return true;
+            case SyntaxKind.ImportType:
+                return !(node.parent as ImportTypeNode).isTypeOf;
             case SyntaxKind.ExpressionWithTypeArguments:
                 return !isExpressionWithTypeArgumentsInClassExtendsClause(<ExpressionWithTypeArguments>node.parent);
         }
