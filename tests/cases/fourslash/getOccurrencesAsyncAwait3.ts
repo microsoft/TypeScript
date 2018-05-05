34323b69fbfbe445c701c015b8a4112e489b25e0
Zzzen <843968788@qq.com>
2018-05-05 22:51:16 +0800

    Document highlights on async/await keywords should highlight other occurrences in the same body fix #22944

---
+++ tests/cases/fourslash/getOccurrencesAsyncAwait3.ts
@@ -0,0 +1,13 @@
+/// <reference path='fourslash.ts' />
+
+// Not valid TS ('await' expression is only allowed within an async function.)
+
+////a/**/wait 100;
+////async function f() {
+////    await 300;
+////}
+
+verify.rangesAreOccurrences(false);
+
+goTo.marker();
+verify.occurrencesAtPositionCount(0);
