4eb1b823bc988562bdf08a4be161a729e61c9c43
rflorian <rflorian@users.noreply.github.com>
2018-05-10 15:02:11 +0200

    Added reference test case and diagnostics message

---
+++ tests/baselines/reference/arityMismatchError.js
@@ -0,0 +1,8 @@
+//// [arityMismatchError.ts]
+declare function f(x: number): number;
+declare function f(x: number, y: number, z: number): number;
+f(1, 2);
+
+
+//// [arityMismatchError.js]
+f(1, 2);
---
+++ tests/cases/compiler/arityMismatchError.ts
@@ -0,0 +1,3 @@
+declare function f(x: number): number;
+declare function f(x: number, y: number, z: number): number;
+f(1, 2);
