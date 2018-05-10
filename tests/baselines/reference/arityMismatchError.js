f0562017654d26f831218e11bd4d72f87669600b
rflorian <rflorian@users.noreply.github.com>
2018-05-10 19:23:32 +0200

    Cleaned up baseline tracking the now renamed arity check test

--- tests/baselines/reference/arityMismatchError.js
+++
@@ -1,8 +0,0 @@
-//// [functionParameterArityMismatch.ts]
-declare function f(x: number): number;
-declare function f(x: number, y: number, z: number): number;
-f(1, 2);
-
-
-//// [arityMismatchError.js]
-f(1, 2);
