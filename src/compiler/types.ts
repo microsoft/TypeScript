e885b3975a6742cb7a22131fbc7e7aa72b288132
Andy Hanson <anhans@microsoft.com>
2018-05-10 13:51:52 -0700

    Fix comment

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -538,7 +538,7 @@ namespace ts {
         Abstract =           1 << 7,  // Class/Method/ConstructSignature
         Async =              1 << 8,  // Property/Method/Function
         Default =            1 << 9,  // Function/Class (export default declaration)
-        Const =              1 << 11, // Variable declaration
+        Const =              1 << 11, // Const enum
         HasComputedFlags =   1 << 29, // Modifier flags have been computed
 
         AccessibilityModifier = Public | Private | Protected,
