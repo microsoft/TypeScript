e45c949c8e5ffe905420714aa667dca829f5fc07
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-05-22 16:22:37 -0700

    Fix to issue 6154 - Overriding a method with a property in the derived class should not cause a compiler error

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -24430,7 +24430,7 @@ namespace ts {
                             continue;
                         }
 
-                        if (isPrototypeProperty(base) && isPrototypeProperty(derived) || base.flags & SymbolFlags.PropertyOrAccessor && derived.flags & SymbolFlags.PropertyOrAccessor) {
+                        if (isPrototypeProperty(base) || base.flags & SymbolFlags.PropertyOrAccessor && derived.flags & SymbolFlags.PropertyOrAccessor) {
                             // method is overridden with method or property/accessor is overridden with property/accessor - correct case
                             continue;
                         }
