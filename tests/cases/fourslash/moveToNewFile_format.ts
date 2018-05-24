a7b7b3c2adf9ea5cc769292f18154d1e6d9b8b92
Andy <anhans@microsoft.com>
2018-05-24 11:35:02 -0700

    moveToNewFile: Format the new file (#24379) (#24382)

---
+++ tests/cases/fourslash/moveToNewFile_format.ts
@@ -0,0 +1,21 @@
+/// <reference path='fourslash.ts' />
+
+// @Filename: /a.ts
+////[|function f() {
+////	const x = 0;
+////}|]
+
+var copy = format.copyFormatOptions();
+copy.ConvertTabsToSpaces = false;
+format.setFormatOptions(copy);
+
+verify.moveToNewFile({
+    newFileContents: {
+        "/a.ts":
+``,
+        "/f.ts":
+`function f() {
+	const x = 0;
+}`,
+    },
+});
