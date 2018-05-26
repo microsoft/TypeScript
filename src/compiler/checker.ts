b20925ad40838217cdc0c4d1224966a84ba72e2a
Anders Hejlsberg <andersh@microsoft.com>
2018-05-26 12:09:18 -0700

    'null' and 'undefined' are bottom types in non-strictNullChecks mode

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -8592,6 +8592,9 @@ namespace ts {
             if (includes & TypeFlags.Any) {
                 return includes & TypeFlags.Wildcard ? wildcardType : anyType;
             }
+            if (!strictNullChecks && includes & TypeFlags.Nullable) {
+                return includes & TypeFlags.Undefined ? undefinedType : nullType;
+            }
             if (includes & TypeFlags.String && includes & TypeFlags.StringLiteral ||
                 includes & TypeFlags.Number && includes & TypeFlags.NumberLiteral ||
                 includes & TypeFlags.ESSymbol && includes & TypeFlags.UniqueESSymbol) {
