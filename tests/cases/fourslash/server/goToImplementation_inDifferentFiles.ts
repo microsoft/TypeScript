9d42ab983d12780eb2a2e8e90dc63e532a36b462
Andy <anhans@microsoft.com>
2018-05-31 13:32:02 -0700

    implementations: Use declaration name for the span (#24537)
    * implementations: Use declaration name for the span
    
    * Always get name in nodeEntry

--- tests/cases/fourslash/server/goToImplementation_inDifferentFiles.ts
+++ tests/cases/fourslash/server/goToImplementation_inDifferentFiles.ts
@@ -3,13 +3,13 @@
 // @Filename: /bar.ts
 ////import {Foo} from './foo'
 ////
-////[|class A implements Foo {
+////class [|A|] implements Foo {
 ////    func() {}
-////}|]
+////}
 ////
-////[|class B implements Foo {
+////class [|B|] implements Foo {
 ////    func() {}
-////}|]
+////}
 
 // @Filename: /foo.ts
 ////export interface /**/Foo {
