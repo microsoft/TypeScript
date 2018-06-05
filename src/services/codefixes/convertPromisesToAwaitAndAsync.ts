69795c88422025defb8df46eabc14b0e14705144
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-06-04 19:05:50 -0700

    Try block is added

--- src/services/codefixes/convertPromisesToAwaitAndAsync.ts
+++ src/services/codefixes/convertPromisesToAwaitAndAsync.ts
@@ -11,28 +11,35 @@ namespace ts.codefix {
         getAllCodeActions: context => codeFixAll(context, errorCodes, (changes, err) => convertToAsyncAwait(changes, err.file!, err.start, context.program.getTypeChecker())),
     });
     function convertToAsyncAwait(changes: textChanges.ChangeTracker, sourceFile: SourceFile, position: number, checker: TypeChecker): void {
-        const oldNode = checker.getSymbolAtLocation(getTokenAtPosition(sourceFile, position, false)).valueDeclaration;
-        const newModifier:Modifier = createModifier(SyntaxKind.AsyncKeyword);
-        let newNode = <FunctionDeclaration>oldNode;
-        newNode.modifiers ? newNode.modifiers.concat([newModifier]) : newNode.modifiers = createNodeArray([newModifier]);
-        changes.insertModifierBefore(sourceFile, SyntaxKind.AsyncKeyword, oldNode)
-        refactorDotThen(sourceFile, changes, newNode);
+        const funcToConvert = checker.getSymbolAtLocation(getTokenAtPosition(sourceFile, position, false)).valueDeclaration;
+        changes.insertModifierBefore(sourceFile, SyntaxKind.AsyncKeyword, funcToConvert)
+
+        //maybe find the statement line with the promise call so we can put that in the .then
+        let tryBlock:FunctionBody = refactorDotThen(sourceFile, changes, funcToConvert)
+        if(tryBlock && (<FunctionDeclaration>funcToConvert).body.statements.length > 0){
+            changes.insertNodeBefore(sourceFile, (<FunctionDeclaration>funcToConvert).body.statements[0],  createTry(tryBlock, undefined, undefined));
+        }
+        
     }
    
-    function refactorDotThen(sourceFile: SourceFile, changes: textChanges.ChangeTracker, node: Node){
+    function refactorDotThen(sourceFile: SourceFile, changes: textChanges.ChangeTracker, node: Node): FunctionBody{
         switch(node.kind){
             case SyntaxKind.PropertyAccessExpression:
-                if((<PropertyAccessExpression>node).name.text === "then"){
+                if((<PropertyAccessExpression>node).name.text === "then" /*and the return type of the function called is a promise!!*/){
                     let newParNode = createAwait(<PropertyAccessExpression>node.parent);
-                    //getChildren().concat(node.parent.getChildren())
                     changes.replaceNode(sourceFile, node.parent, newParNode)
-                    return;
+                    //node.parent.arguments[0].body is the try block
+                    let parNode = node.parent as CallExpression; 
+                    return parNode.arguments.length > 0 && isFunctionLikeDeclaration(parNode.arguments[0]) ? (<FunctionBody>(<FunctionLikeDeclaration>parNode.arguments[0]).body) : null;
                 }
                 break;
         }
 
         for( let child of node.getChildren() ){
-            refactorDotThen(sourceFile, changes, child);
+            let ret = refactorDotThen(sourceFile, changes, child);
+            if(ret){
+                return ret;
+            }
         }
     }
 
