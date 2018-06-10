8bc92e4a6363804436dba0dc37b33b1d862019bd
Markus Johnsson <markus.johnsson@infviz.com>
2018-06-10 19:58:32 +0200

    Update test to reflect new behavior

--- tests/cases/fourslash/codeFixAddMissingMember9.ts
+++ tests/cases/fourslash/codeFixAddMissingMember9.ts
@@ -10,15 +10,16 @@
 
 verify.codeFixAll({
     fixId: "addMissingMember",
+    fixAllDescription: "Add all missing members",
     newFileContent:
 `class C {
-    y(x: number, arg1: string, z: boolean): any {
-        throw new Error("Method not implemented.");
-    }
     z: boolean = true;
     method() {
         const x = 0;
         this.y(x, "a", this.z);
+    }
+    y(x: number, arg1: string, z: boolean): any {
+        throw new Error("Method not implemented.");
     }
 }`,
 });
