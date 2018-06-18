57fd9ebe97b3f80121042ffb92eaa873e09e6d67
kingwl <kingwenlu@gmail.com>
2018-06-18 20:50:26 +0800

    add more testcase

--- tests/baselines/reference/unnecessaryReferenceChecksWithPropertySignature.js
+++ tests/baselines/reference/unnecessaryReferenceChecksWithPropertySignature.js
@@ -4,13 +4,32 @@ interface C {
   [C.b]: void;
 }
 class C {
+  [C.c] = 1;
   static readonly a = Symbol();
   static readonly b = Symbol();
+  static readonly c = Symbol();
 }
 
+class D {
+  [D.a] = 0;
+  static a = "a";
+}
 
 //// [unnecessaryReferenceChecksWithPropertySignature.js]
+var _a, _b;
 class C {
+    constructor() {
+        this[_a] = 1;
+    }
 }
+_a = C.c;
 C.a = Symbol();
 C.b = Symbol();
+C.c = Symbol();
+class D {
+    constructor() {
+        this[_b] = 0;
+    }
+}
+_b = D.a;
+D.a = "a";
--- tests/cases/compiler/unnecessaryReferenceChecksWithPropertySignature.ts
+++ tests/cases/compiler/unnecessaryReferenceChecksWithPropertySignature.ts
@@ -5,6 +5,13 @@ interface C {
   [C.b]: void;
 }
 class C {
+  [C.c] = 1;
   static readonly a = Symbol();
   static readonly b = Symbol();
+  static readonly c = Symbol();
+}
+
+class D {
+  [D.a] = 0;
+  static a = "a";
 }
