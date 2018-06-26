ae602ca19b2643580d993534537a263cab60256f
Wesley Wigham <wewigham@microsoft.com>
2018-06-26 11:33:17 -0700

    Also skip sourcemappy comments like sourceURL

--- src/services/services.ts
+++ src/services/services.ts
@@ -1511,7 +1511,7 @@ namespace ts {
 
         // Sometimes tools can sometimes see the following line as a source mapping url comment, so we mangle it a bit (the [M])
         const sourceMapCommentRegExp = /^\/\/[@#] source[M]appingURL=(.+)$/;
-        const whitespaceRegExp = /^[\s]*$/;
+        const whitespaceOrMapCommentRegExp = /^\s*(\/\/[@#] .*)?$/;
         const base64UrlRegExp = /^data:(?:application\/json(?:;charset=[uU][tT][fF]-8);base64,([A-Za-z0-9+\/=]+)$)?/;
         function scanForSourcemapURL(fileName: string) {
             const mappedFile = sourcemappedFileCache.get(toPath(fileName, currentDirectory, getCanonicalFileName));
@@ -1525,8 +1525,8 @@ namespace ts {
                 if (comment) {
                     return comment[1];
                 }
-                // If we see a nonwhitespace line, break, to avoid scanning up the entire file
-                else if (!lineText.match(whitespaceRegExp)) {
+                // If we see a nonwhitespace/map comment-like line, break, to avoid scanning up the entire file
+                else if (!lineText.match(whitespaceOrMapCommentRegExp)) {
                     break;
                 }
             }
