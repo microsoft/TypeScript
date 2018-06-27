89de9c19789616f6324ad50254df41b8f21e17dd
王文璐 <kingwenlu@gmail.com>
2018-06-27 17:29:37 +0800

    check more case for empty binding patten

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -26317,15 +26317,25 @@ namespace ts {
             // For a binding pattern, validate the initializer and exit
             if (isBindingPattern(node.name)) {
                 // Don't validate for-in initializer as it is already an error
+                const widenedType = getWidenedTypeForVariableLikeDeclaration(node);
                 if (node.initializer && node.parent.parent.kind !== SyntaxKind.ForInStatement) {
                     const initializerType = checkExpressionCached(node.initializer);
                     if (strictNullChecks && node.name.elements.length === 0) {
                         checkNonNullNonVoidType(initializerType, node);
                     }
                     else {
-                        checkTypeAssignableToAndOptionallyElaborate(initializerType, getWidenedTypeForVariableLikeDeclaration(node), node, node.initializer);
+                        checkTypeAssignableToAndOptionallyElaborate(initializerType, widenedType, node, node.initializer);
                     }
                     checkParameterInitializer(node);
+                }
+                // check the binding pattern with empty elements
+                if (node.name.elements.length === 0) {
+                    if (isArrayBindingPattern(node.name)) {
+                        checkIteratedTypeOrElementType(widenedType,node, /* allowStringInput */ false, /* allowAsyncIterables */ false);
+                    }
+                    else if(strictNullChecks) {
+                        checkNonNullType(widenedType, node);
+                    }
                 }
                 return;
             }
