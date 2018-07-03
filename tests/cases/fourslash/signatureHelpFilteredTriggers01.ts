3e38693425dd4fd732ceb04051b346deea4fad17
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-07-03 13:01:55 -0700

    Amend test for comments, other string types.

--- tests/cases/fourslash/signatureHelpFilteredTriggers01.ts
+++ tests/cases/fourslash/signatureHelpFilteredTriggers01.ts
@@ -4,20 +4,28 @@
 ////  throw null;
 ////}
 ////
-////foo("/**/")
+////foo("/*1*/");
+////foo('/*2*/');
+////foo(` ${100}/*3*/`);
+////foo(/* /*4*/ */);
+////foo(
+////    ///*5*/
+////);
 
-goTo.marker();
-for (const triggerCharacter of ["<", "(", ","]) {
-    edit.insert(triggerCharacter);
-    verify.noSignatureHelpForTriggerReason({
-        kind: "characterTyped",
-        triggerCharacter,
-    });
-    verify.signatureHelpPresentForTriggerReason({
-        kind: "retrigger",
-        triggerCharacter,
-    });
-    edit.backspace();
+for (const marker of test.markers()) {
+    goTo.marker(marker);
+    for (const triggerCharacter of ["<", "(", ","]) {
+        edit.insert(triggerCharacter);
+        verify.noSignatureHelpForTriggerReason({
+            kind: "characterTyped",
+            triggerCharacter,
+        });
+        verify.signatureHelpPresentForTriggerReason({
+            kind: "retrigger",
+            triggerCharacter,
+        });
+        edit.backspace();
+    }
+    verify.signatureHelpPresentForTriggerReason(/*triggerReason*/ undefined);
+    verify.signatureHelpPresentForTriggerReason({ kind: "invoked" });
 }
-verify.signatureHelpPresentForTriggerReason(/*triggerReason*/ undefined);
-verify.signatureHelpPresentForTriggerReason({ kind: "invoked" });
