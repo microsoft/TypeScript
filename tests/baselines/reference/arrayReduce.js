fa6353644f798e42ac96f9ceee012e3fc1eb30d2
Andy Hanson <anhans@microsoft.com>
2018-07-05 14:57:09 -0700

    Add tests and update baselines

--- tests/baselines/reference/arrayReduce.js
+++ tests/baselines/reference/arrayReduce.js
@@ -6,6 +6,13 @@ function toStrings(arr: ReadonlyArray<object>): string[] {
 	}, [] as string[]);
 }
 
+interface Operation { readonly kind: "add" | "subtract"; readonly value: number; }
+function add(arr: ReadonlyArray<Operation>): Operation {
+	return arr.reduce(({ value }, { kind, value: v }) => {
+		return { kind: "add", value: kind === "add" ? value + v : value - v };
+	}, { kind: "add", value: 5 });
+}
+
 
 //// [arrayReduce.js]
 function toStrings(arr) {
@@ -13,4 +20,11 @@ function toStrings(arr) {
         acc.push(obj.toString());
         return acc;
     }, []);
+}
+function add(arr) {
+    return arr.reduce(function (_a, _b) {
+        var value = _a.value;
+        var kind = _b.kind, v = _b.value;
+        return { kind: "add", value: kind === "add" ? value + v : value - v };
+    }, { kind: "add", value: 5 });
 }
--- tests/cases/compiler/arrayReduce.ts
+++ tests/cases/compiler/arrayReduce.ts
@@ -4,3 +4,10 @@ function toStrings(arr: ReadonlyArray<object>): string[] {
 		return acc;
 	}, [] as string[]);
 }
+
+interface Operation { readonly kind: "add" | "subtract"; readonly value: number; }
+function add(arr: ReadonlyArray<Operation>): Operation {
+	return arr.reduce(({ value }, { kind, value: v }) => {
+		return { kind: "add", value: kind === "add" ? value + v : value - v };
+	}, { kind: "add", value: 5 });
+}
