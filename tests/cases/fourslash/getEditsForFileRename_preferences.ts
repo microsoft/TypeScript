19a6164bd1ff213efa22a859e2149a17a4d997ea
Andy Hanson <anhans@microsoft.com>
2018-07-17 15:47:22 -0700

    Test quotePreference

--- tests/cases/fourslash/getEditsForFileRename_preferences.ts
+++ tests/cases/fourslash/getEditsForFileRename_preferences.ts
@@ -5,6 +5,7 @@
 
 // @Filename: /dir/b.ts
 ////import {} from "dir/a";
+////import {} from 'dir/a';
 
 // @Filename: /tsconfig.json
 ////{ "compilerOptions": { "baseUrl": "." } }
@@ -13,9 +14,13 @@ verify.getEditsForFileRename({
     oldPath: "/dir/a.ts",
     newPath: "/dir/a1.ts",
     newFileContents: {
-        "/dir/b.ts": 'import {} from "dir/a1";',
+        "/dir/b.ts":
+`import {} from "dir/a1";
+import {} from 'dir/a1';`,
     },
     preferences: {
         importModuleSpecifierPreference: "non-relative",
+        // No effect because we are changing existing imports, which already have quotes
+        quotePreference: "single",
     },
 });
