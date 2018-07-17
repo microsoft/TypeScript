991cb42c92cf13baf391bd0027c1b77949bda109
aleph-naught2tog <maggie@8thlight.com>
2018-07-17 00:52:01 -0500

    Includes baseline files

---
+++ tests/baselines/reference/abstractGenerator.js
@@ -0,0 +1,12 @@
+//// [abstractGenerator.ts]
+abstract class Passes {
+  abstract *foo();
+}
+
+
+//// [abstractGenerator.js]
+var Passes = /** @class */ (function () {
+    function Passes() {
+    }
+    return Passes;
+}());
