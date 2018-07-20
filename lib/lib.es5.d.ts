0176533cab78fdd6653336bbaee7d20d5603cd07
Eduard Rastoropov <eduard.rastoropov@gmail.com>
2018-07-20 20:48:24 +0300

    Adjusted Object.keys definition
    Object.values has 2 type definitions: https://github.com/Microsoft/TypeScript/blob/master/lib/lib.es2017.object.d.ts
    This commit returns the current type definition of Object.keys, adding it in addition to the proposed one in attempt to resolve the "non-successful checks" reported by the bot.

--- lib/lib.es5.d.ts
+++ lib/lib.es5.d.ts
@@ -259,6 +259,12 @@ interface ObjectConstructor {
       * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
       */
     keys<o>(o: {}): (keyof o)[];
+
+    /**
+      * Returns the names of the enumerable properties and methods of an object.
+      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+      */
+    keys(o: {}): string[];
 }
 
 /**
