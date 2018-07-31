d65f0ffbb1c1d9edcf88c574dfd0a0048c7533c4
Wesley Wigham <wwigham@gmail.com>
2018-07-31 13:53:28 -0700

    Fix decorated accessor emit (#26016)

--- src/compiler/transformers/ts.ts
+++ src/compiler/transformers/ts.ts
@@ -1750,6 +1750,12 @@ namespace ts {
         type SerializedEntityNameAsExpression = Identifier | BinaryExpression | PropertyAccessExpression;
         type SerializedTypeNode = SerializedEntityNameAsExpression | VoidExpression | ConditionalExpression;
 
+        function getAccessorTypeNode(node: AccessorDeclaration) {
+            const accessors = resolver.getAllAccessorDeclarations(node);
+            return accessors.setAccessor && getSetAccessorTypeAnnotationNode(accessors.setAccessor)
+                || accessors.getAccessor && getEffectiveReturnTypeNode(accessors.getAccessor);
+        }
+
         /**
          * Serializes the type of a node for use with decorator type metadata.
          *
@@ -1759,10 +1765,10 @@ namespace ts {
             switch (node.kind) {
                 case SyntaxKind.PropertyDeclaration:
                 case SyntaxKind.Parameter:
-                case SyntaxKind.GetAccessor:
                     return serializeTypeNode((<PropertyDeclaration | ParameterDeclaration | GetAccessorDeclaration>node).type);
                 case SyntaxKind.SetAccessor:
-                    return serializeTypeNode(getSetAccessorTypeAnnotationNode(<SetAccessorDeclaration>node));
+                case SyntaxKind.GetAccessor:
+                    return serializeTypeNode(getAccessorTypeNode(node as AccessorDeclaration));
                 case SyntaxKind.ClassDeclaration:
                 case SyntaxKind.ClassExpression:
                 case SyntaxKind.MethodDeclaration:
