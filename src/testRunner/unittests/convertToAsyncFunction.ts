2d21376ef121f3c02350666ee02d21c43b9dead2
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-08-08 15:46:09 -0700

    More tests and baselines

--- src/testRunner/unittests/convertToAsyncFunction.ts
+++ src/testRunner/unittests/convertToAsyncFunction.ts
@@ -460,6 +460,8 @@ function [#|f|](): Promise<void>{
 }`);
     _testConvertToAsyncFunction("convertToAsyncFunction_basicWithComments", `
 function [#|f|](): Promise<void>{
+    /* Note - some of these comments are removed during the refactor. This is not ideal. */
+
     // a 
     /*b*/ return /*c*/ fetch( /*d*/ 'https://typescriptlang.org' /*e*/).then( /*f*/ result /*g*/ => { /*h*/ console.log(/*i*/ result /*j*/) /*k*/}/*l*/);
     // m
