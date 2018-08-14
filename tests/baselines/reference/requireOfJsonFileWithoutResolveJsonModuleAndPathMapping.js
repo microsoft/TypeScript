aa8f1e5b6accfa7c9bb80432c82b2c48f2317542
Sheetal Nandi <shkamat@microsoft.com>
2018-08-14 15:46:51 -0700

    Test when module resolution because of path mapping takes to json file Test for #26402

---
+++ tests/baselines/reference/requireOfJsonFileWithoutResolveJsonModuleAndPathMapping.js
@@ -0,0 +1,14 @@
+//// [tests/cases/compiler/requireOfJsonFileWithoutResolveJsonModuleAndPathMapping.ts] ////
+
+//// [foobar.json]
+{ "a": 10 }
+
+//// [a.ts]
+import foobar from "foo/bar/foobar.json";
+
+
+//// [/bin/node_modules/foo/bar/foobar.json]
+{ "a": 10 }
+//// [/bin/a.js]
+"use strict";
+exports.__esModule = true;
