1c732e3b2a27e7b82f2c312bb000df555db74c21
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-08-17 15:58:44 -0700

    Don't create expando object literals in TS
    Previously, getWidenedTypedFromJSPropertyAssignment was not called for
    Typescript code. Since property assignments on functions, it is. That
    meant that property assignments would incorrectly create a JS container
    for empty object literals in a property assignment, even in Typescript:
    
    ```ts
    const one = () => 1
    one.p = {}
    one.p.q = {} // should not work in Typescript!
    ```
    
    Now empty object literals never create expando objects in Typescript,
    because getJSExpandoObjectType requires the declaration to be in a JS
    file.

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -4786,7 +4786,7 @@ namespace ts {
         }
 
         function getJSExpandoObjectType(decl: Node, symbol: Symbol, init: Expression | undefined): Type | undefined {
-            if (!init || !isObjectLiteralExpression(init) || init.properties.length) {
+            if (!isInJavaScriptFile(decl) || !init || !isObjectLiteralExpression(init) || init.properties.length) {
                 return undefined;
             }
             const exports = createSymbolTable();
