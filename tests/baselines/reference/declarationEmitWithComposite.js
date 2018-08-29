38a85cfbf47cf1b96077bbdb13cbbfc23cd0aa89
Sheetal Nandi <shkamat@microsoft.com>
2018-08-29 15:47:52 -0700

    Add test to verify #26669 where declaration output is incorrect when declaration flag is not set explicitly in options

---
+++ tests/baselines/reference/declarationEmitWithComposite.js
@@ -0,0 +1,14 @@
+//// [test.ts]
+interface Foo {
+    x: number;
+}
+export default Foo;
+
+
+//// [/foo/out/test.js]
+"use strict";
+exports.__esModule = true;
+
+
+//// [/foo/out/test.d.ts]
+export default Foo;
