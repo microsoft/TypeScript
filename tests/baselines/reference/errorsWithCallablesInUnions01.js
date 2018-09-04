289ae3cca66b79e39a3f60f3bf029462c350fe39
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-09-04 15:47:04 -0700

    Accepted baseleines.

---
+++ tests/baselines/reference/errorsWithCallablesInUnions01.js
@@ -0,0 +1,17 @@
+//// [errorsWithCallablesInUnions01.ts]
+interface IDirectiveLinkFn<TScope> {
+    (scope: TScope): void;
+}
+
+interface IDirectivePrePost<TScope> {
+    pre?: IDirectiveLinkFn<TScope>;
+    post?: IDirectiveLinkFn<TScope>;
+}
+
+export let blah: IDirectiveLinkFn<number> | IDirectivePrePost<number> = (x: string) => {}
+
+
+//// [errorsWithCallablesInUnions01.js]
+"use strict";
+exports.__esModule = true;
+exports.blah = function (x) { };
