8ca438d393cfa63ac991e330df2a88f750280a85
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-09-17 15:42:43 -0700

    Improve comment

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -3670,7 +3670,7 @@ namespace ts {
     export interface NodeLinks {
         flags: NodeCheckFlags;           // Set of flags specific to Node
         resolvedType?: Type;              // Cached type of type node
-        resolvedEnumType?: Type;          // UGH
+        resolvedEnumType?: Type;          // Cached constraint type from enum jsdoc tag
         resolvedSignature?: Signature;    // Cached signature of signature node or call expression
         resolvedSignatures?: Map<Signature[]>;   // Cached signatures of jsx node
         resolvedSymbol?: Symbol;          // Cached name resolution result
