0f5d62630b7faa5b0820a8859bd5159d9472366e
Andy Hanson <anhans@microsoft.com>
2018-09-17 11:26:35 -0700

    Use this.toPath

--- src/server/editorServices.ts
+++ src/server/editorServices.ts
@@ -1884,7 +1884,7 @@ namespace ts.server {
             const path = toNormalizedPath(uncheckedFileName);
             const info = this.getScriptInfoForNormalizedPath(path);
             if (info) return info;
-            const configProject = this.configuredProjects.get(this.toCanonicalFileName(path));
+            const configProject = this.configuredProjects.get(this.toPath(uncheckedFileName));
             return configProject && configProject.getCompilerOptions().configFile;
         }
 
