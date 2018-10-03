9bdd6a3b555c353e0a23968dc08a4ce57668d920
Andy <anhans@microsoft.com>
2018-10-03 11:44:16 -0700

    Support loading "index.d.ts" using "typesVersions" without "types", "typings", or "main" (#27514)
    * Support loading "index.d.ts" using "typesVersions" without "types", "typings", or "main"
    
    * Update baseline

---
+++ tests/baselines/reference/typesVersions.justIndex.js
@@ -0,0 +1,19 @@
+//// [tests/cases/conformance/moduleResolution/typesVersions.justIndex.ts] ////
+
+//// [package.json]
+{
+    "typesVersions": {
+        ">=3.1.0-0": { "*" : ["ts3.1/*"] }
+    }
+}
+
+//// [index.d.ts]
+export const a = 0;
+
+//// [user.ts]
+import { a } from "a";
+
+
+//// [user.js]
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
