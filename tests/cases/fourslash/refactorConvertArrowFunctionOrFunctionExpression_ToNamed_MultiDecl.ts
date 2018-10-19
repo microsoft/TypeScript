152e3621756f22c424508fec61316d2d6e27076a
BigAru <arooran@indikon.ch>
2018-10-19 08:08:57 +0200

    adapt one test case

--- tests/cases/fourslash/refactorConvertArrowFunctionOrFunctionExpression_ToNamed_MultiDecl.ts
+++ tests/cases/fourslash/refactorConvertArrowFunctionOrFunctionExpression_ToNamed_MultiDecl.ts
@@ -1,13 +1,13 @@
 /// <reference path='fourslash.ts' />
 
-//// let foo, bar = /*x*/(/*y*/) => 1 + 1;
+//// let foo, bar = /*x*/(/*y*/) => 1 + 1, magicNo;
 
 goTo.select("x", "y");
 edit.applyRefactor({
     refactorName: "Convert arrow function or function expression",
     actionName: "Convert to named function",
     actionDescription: "Convert to named function",
-    newContent: `let foo;
+    newContent: `let foo, magicNo;
 function bar() {
     return 1 + 1;
 }
