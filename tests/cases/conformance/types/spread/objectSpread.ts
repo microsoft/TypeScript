2ed627aaf12b3270c4e21fa8db4d92058c545625
Anders Hejlsberg <andersh@microsoft.com>
2018-10-30 16:33:14 -0700

    Update tests

--- tests/cases/conformance/types/spread/objectSpread.ts
+++ tests/cases/conformance/types/spread/objectSpread.ts
@@ -122,6 +122,20 @@ let shortCutted: { a: number, b: string } = { ...o, a }
 let spreadNonPrimitive = { ...<object>{}};
 
 // generic spreads
+
+function f<T, U>(t: T, u: U) {
+    return { ...t, ...u, id: 'id' };
+}
+
+let exclusive: { id: string, a: number, b: string, c: string, d: boolean } =
+    f({ a: 1, b: 'yes' }, { c: 'no', d: false })
+let overlap: { id: string, a: number, b: string } =
+    f({ a: 1 }, { a: 2, b: 'extra' })
+let overlapConflict: { id:string, a: string } =
+    f({ a: 1 }, { a: 'mismatch' })
+let overwriteId: { id: string, a: number, c: number, d: string } =
+    f({ a: 1, id: true }, { c: 1, d: 'no' })
+
 function genericSpread<T, U>(t: T, u: U, v: T | U, w: T | { s: string }, obj: { x: number }) {
     let x01 = { ...t };
     let x02 = { ...t, ...t };
