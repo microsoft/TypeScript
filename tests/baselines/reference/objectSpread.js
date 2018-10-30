0f308f50744da64bce435c29e3ef522c82b41a07
Anders Hejlsberg <andersh@microsoft.com>
2018-10-30 08:18:25 -0700

    Accept new baselines

--- tests/baselines/reference/objectSpread.js
+++ tests/baselines/reference/objectSpread.js
@@ -120,6 +120,27 @@ let shortCutted: { a: number, b: string } = { ...o, a }
 // non primitive
 let spreadNonPrimitive = { ...<object>{}};
 
+// generic spreads
+function genericSpread<T, U>(t: T, u: U, v: T | U, w: T | { s: string }, obj: { x: number }) {
+    let x01 = { ...t };
+    let x02 = { ...t, ...t };
+    let x03 = { ...t, ...u };
+    let x04 = { ...u, ...t };
+    let x05 = { a: 5, b: 'hi', ...t };
+    let x06 = { ...t, a: 5, b: 'hi' };
+    let x07 = { a: 5, b: 'hi', ...t, c: true, ...obj };
+    let x09 = { a: 5, ...t, b: 'hi', c: true, ...obj };
+    let x10 = { a: 5, ...t, b: 'hi', ...u, ...obj };
+    let x11 = { ...v };
+    let x12 = { ...v, ...obj };
+    let x13 = { ...w };
+    let x14 = { ...w, ...obj };
+    let x15 = { ...t, ...v };
+    let x16 = { ...t, ...w };
+    let x17 = { ...t, ...w, ...obj };
+    let x18 = { ...t, ...v, ...w };
+}
+
 
 //// [objectSpread.js]
 var __assign = (this && this.__assign) || function () {
@@ -214,3 +235,23 @@ var a = 12;
 var shortCutted = __assign({}, o, { a: a });
 // non primitive
 var spreadNonPrimitive = __assign({}, {});
+// generic spreads
+function genericSpread(t, u, v, w, obj) {
+    var x01 = __assign({}, t);
+    var x02 = __assign({}, t, t);
+    var x03 = __assign({}, t, u);
+    var x04 = __assign({}, u, t);
+    var x05 = __assign({ a: 5, b: 'hi' }, t);
+    var x06 = __assign({}, t, { a: 5, b: 'hi' });
+    var x07 = __assign({ a: 5, b: 'hi' }, t, { c: true }, obj);
+    var x09 = __assign({ a: 5 }, t, { b: 'hi', c: true }, obj);
+    var x10 = __assign({ a: 5 }, t, { b: 'hi' }, u, obj);
+    var x11 = __assign({}, v);
+    var x12 = __assign({}, v, obj);
+    var x13 = __assign({}, w);
+    var x14 = __assign({}, w, obj);
+    var x15 = __assign({}, t, v);
+    var x16 = __assign({}, t, w);
+    var x17 = __assign({}, t, w, obj);
+    var x18 = __assign({}, t, v, w);
+}
