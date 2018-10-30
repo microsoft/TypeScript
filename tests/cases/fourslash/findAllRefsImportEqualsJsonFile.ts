d3e3e42148253ea79dea5ac733d3d7ea75fd9571
Andy Hanson <anhans@microsoft.com>
2018-10-30 08:57:58 -0700

    Add json test

---
+++ tests/cases/fourslash/findAllRefsImportEqualsJsonFile.ts
@@ -0,0 +1,24 @@
+/// <reference path='fourslash.ts' />
+
+// @allowJs: true
+// @checkJs: true
+// @resolveJsonModule: true
+
+// @Filename: /a.ts
+////import [|{| "isWriteAccess": true, "isDefinition": true |}j|] = require("[|./j.json|]");
+////[|j|];
+
+// @Filename: /b.js
+////const [|{| "isWriteAccess": true, "isDefinition": true |}j|] = require("[|./j.json|]");
+////[|j|];
+
+// @Filename: /j.json
+////[|{ "x": 0 }|]
+
+verify.noErrors();
+
+const [r0, r1, r2, r3, r4, r5, r6] = test.ranges();
+verify.singleReferenceGroup('import j = require("./j.json")', [r0, r2]);
+verify.referenceGroups([r1, r4], [{ definition: 'module "/j"', ranges: [r1, r4, r6] }]);
+verify.singleReferenceGroup('const j: {\n    "x": number;\n}', [r3, r5]);
+verify.referenceGroups(r6, undefined);
