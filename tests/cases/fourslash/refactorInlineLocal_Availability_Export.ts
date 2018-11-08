89d17713e664e0794d0573c72a7f5c4fe4e6fb04
Giovanni Heilmann <giovanni.heilmann@gmail.com>
2018-11-08 16:43:11 +0100

    Add refactoring availability tests

---
+++ tests/cases/fourslash/refactorInlineLocal_Availability_Export.ts
@@ -0,0 +1,12 @@
+/// <reference path='fourslash.ts' />
+
+//// export const /*z*a/*y*/ = 42-3;
+//// const b = 2 * /*x*/a/*w*/;
+
+goTo.select("z", "y");
+verify.not.refactorAvailable("Inline local", "Inline all");
+verify.not.refactorAvailable("Inline local", "Inline here");
+
+goTo.select("x", "w");
+verify.not.refactorAvailable("Inline local", "Inline all");
+verify.not.refactorAvailable("Inline local", "Inline here");
