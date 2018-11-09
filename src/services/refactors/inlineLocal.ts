6f1f0e1c9d705f6729002fcb0ecda117491d38f0
Giovanni Heilmann <giovanni.heilmann@gmail.com>
2018-11-09 16:04:20 +0100

    Fix style

--- src/services/refactors/inlineLocal.ts
+++ src/services/refactors/inlineLocal.ts
@@ -23,7 +23,6 @@ namespace ts.refactor.inlineLocal {
         const info = getLocalInfo(file, program, startPosition);
         if (!info) return undefined;
         const { selectedUsage } = info;
-        // inline all is always be available
         const refactorInfo = {
             name: refactorName,
             description: refactorDescription,
@@ -32,7 +31,6 @@ namespace ts.refactor.inlineLocal {
                 description: inlineAllActionDescription
             }]
         };
-        // inline here is only available if usage is selected
         if (selectedUsage) {
             refactorInfo.actions.push({
                 name: inlineHereActionName,
@@ -44,24 +42,21 @@ namespace ts.refactor.inlineLocal {
 
     function getLocalInfo(file: SourceFile, program: Program, startPosition: number): Info | undefined {
         const token = getTokenAtPosition(file, startPosition);
-        const parent = token.parent;
+        const maybeDeclaration = token.parent;
         const checker = program.getTypeChecker();
-        if (isLocalVariable(parent)) {
-            return createInfo(checker, parent);
+        if (isLocalVariable(maybeDeclaration)) {
+            return createInfo(checker, maybeDeclaration);
         }
-        else if (isVariableDeclarationList(parent) && parent.declarations.length === 1 && isVariableStatement(parent.parent)) {
-            return undefined;
-        }
-        else if (isIdentifier(token)) {
+        if (isIdentifier(token)) {
             const symbol = checker.getSymbolAtLocation(token);
             if (!symbol) return undefined;
             const declaration = symbol.valueDeclaration;
             if (isLocalVariable(declaration)) {
                 return createInfo(checker, declaration, token);
             }
-            else return undefined;
+            return undefined;
         }
-        else return undefined;
+        return undefined;
     }
 
     function isLocalVariable(parent: Node): parent is VariableDeclaration {
@@ -69,26 +64,31 @@ namespace ts.refactor.inlineLocal {
     }
 
     function createInfo(checker: TypeChecker, declaration: VariableDeclaration, token?: Identifier): Info | undefined {
-        const identifier = declaration.name;
-        let hasErrors = false;
-        const usages = getReferencesInScope(ts.getEnclosingBlockScopeContainer(identifier), identifier, checker);
-        if (!declaration.initializer) hasErrors = true;
-        if (containsProhibitedModifiers(declaration.parent.parent.modifiers)) hasErrors = true;
-        ts.forEach(usages, usage => {
-            if (isAssigned(usage)) hasErrors = true;
-        });
-        return !hasErrors ? {
+        const name = declaration.name;
+        const usages = getReferencesInScope(ts.getEnclosingBlockScopeContainer(name), name, checker);
+        return canInline(declaration, usages) ? {
             declaration,
             usages,
             selectedUsage: token ? token : undefined
         } : undefined;
     }
 
-    function isAssigned(usage: Identifier) {
+    function canInline(declaration: VariableDeclaration, usages: ReadonlyArray<Identifier>): boolean {
+        let hasErrors = false;
+        if (!declaration.initializer) hasErrors = true;
+        if (containsProhibitedModifiers(declaration.parent.parent.modifiers)) hasErrors = true;
+        ts.forEach(usages, usage => {
+            if (isAssigned(usage)) hasErrors = true;
+        });
+        return !hasErrors;
+    }
+
+    function isAssigned(usage: Identifier): boolean {
         type AssignExpr = AssignmentExpression<AssignmentOperatorToken>;
-        const assignment: AssignExpr = findAncestor(usage, ancestor => isAssignmentExpression(ancestor)) as AssignExpr;
-        if (assignment && assignment.left === usage) return true;
-        else return false;
+        const assignment: AssignExpr = findAncestor(
+            usage,
+            ancestor => isAssignmentExpression(ancestor)) as AssignExpr;
+        return assignment && assignment.left === usage;
     }
 
     function containsProhibitedModifiers(modifiers?: NodeArray<Modifier>): boolean {
@@ -106,61 +106,68 @@ namespace ts.refactor.inlineLocal {
                 edits.push(...getInlineAllEdits(context, declaration, usages));
                 break;
             case inlineHereActionName:
-            edits.push(...getInlineHereEdits(context, declaration, usages, selectedUsage!));
+                edits.push(...getInlineHereEdits(context, declaration, usages, selectedUsage!));
                 break;
             default:
-                return undefined;
+                return Debug.fail("invalid action");
         }
-
         return { edits };
     }
 
-    function getInlineAllEdits(context: RefactorContext, declaration: VariableDeclaration, usages: ReadonlyArray<Identifier>): ReadonlyArray<FileTextChanges> {
+    function getInlineAllEdits(
+        context: RefactorContext,
+        declaration: VariableDeclaration,
+        usages: ReadonlyArray<Identifier>): ReadonlyArray<FileTextChanges> {
         const { file } = context;
         return textChanges.ChangeTracker.with(context, t => {
             ts.forEach(usages, oldNode => {
                 const { initializer } = declaration;
-                const expression = parenthesize(oldNode, initializer!);
+                makeIdUnique(initializer!); // since there is no node-copying function
+                const expression = parenthesizeIfNecessary(oldNode, initializer!);
                 t.replaceNode(file, oldNode, expression);
             });
             t.delete(file, declaration);
         });
     }
 
-    function getInlineHereEdits(context: RefactorContext, declaration: VariableDeclaration, usages: ReadonlyArray<Identifier>, selectedUsage: Identifier): ReadonlyArray<FileTextChanges> {
+    function getInlineHereEdits(context: RefactorContext,
+        declaration: VariableDeclaration,
+        usages: ReadonlyArray<Identifier>,
+        selectedUsage: Identifier): ReadonlyArray<FileTextChanges> {
         const { file } = context;
         return textChanges.ChangeTracker.with(context, t => {
             const { initializer } = declaration;
-            makeIdUnique(initializer!);
-            const node = selectedUsage;
-            const expression = parenthesize(node, initializer!);
-            t.replaceNode(file, node, expression);
+            const expression = parenthesizeIfNecessary(selectedUsage, initializer!);
+            t.replaceNode(file, selectedUsage, expression);
             if (usages.length === 1) t.delete(file, declaration);
         });
     }
 
-    function makeIdUnique(node: Node) {
+    function makeIdUnique(node: Node): void {
         node.id = undefined;
         getNodeId(node);
     }
 
-    function parenthesize(node: Identifier, expression: Expression) {
-        const parent = node.parent;
+    function parenthesizeIfNecessary(target: Node, expression: Expression): Expression {
+        const parent = target.parent;
         if (isUnaryExpression(parent)) return createParen(expression);
         if (isBinaryExpression(parent)) {
-            const isLeft = node === parent.left;
-            return parenthesizeBinaryOperand(parent.operatorToken.kind, expression, isLeft, parent.left);
+            return parenthesizeBinaryOperand(
+                parent.operatorToken.kind,
+                expression,
+                target === parent.left,
+                parent.left);
         }
         return expression;
     }
 
-    function getReferencesInScope(scope: Node, node: Node, checker: TypeChecker): ReadonlyArray<Identifier> {
+    function getReferencesInScope(scope: Node, target: Node, checker: TypeChecker): ReadonlyArray<Identifier> {
         const nodes: Node[] = [];
         function getNodes(node: Node) {
             ts.forEachChild(node, n => { nodes.push(n); getNodes(n); });
         }
         getNodes(scope);
-        const symbol = checker.getSymbolAtLocation(node);
-        return nodes.filter(n => isIdentifier(n) && n.id !== node.id && checker.getSymbolAtLocation(n) === symbol) as Identifier[];
+        const symbol = checker.getSymbolAtLocation(target);
+        return nodes.filter(n => checker.getSymbolAtLocation(n) === symbol && n.id !== target.id) as Identifier[];
     }
 }
