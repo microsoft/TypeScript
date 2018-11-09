1649f91bb496c5bf141adcf7cb9d86269a251007
Giovanni Heilmann <giovanni.heilmann@gmail.com>
2018-11-09 11:06:29 +0100

    Split up test cases

---
+++ tests/cases/fourslash/refactorInlineLocal_Simple_Usage_InlineHere.ts
@@ -0,0 +1,12 @@
+/// <reference path='fourslash.ts' />
+
+//// const /*z*/a/*y*/ = 42 - 3;
+//// const b = 2 * /*x*/a/*w*/;
+
+goTo.select("x", "w");
+edit.applyRefactor({
+    refactorName: "Inline local",
+    actionName: "Inline here",
+    actionDescription: "Inline here",
+    newContent: `const b = 2 * (42 - 3);`
+});
