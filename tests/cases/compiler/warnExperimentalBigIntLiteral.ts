e4bb1973ed97023546d983c1b07f22c335610e8a
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-11-12 14:05:01 -0800

    Fixed up flags on test cases and stray newline.

--- tests/cases/compiler/warnExperimentalBigIntLiteral.ts
+++ tests/cases/compiler/warnExperimentalBigIntLiteral.ts
@@ -5,4 +5,3 @@ let bigintType: bigint; // should not error
 let bigintLiteralType: 123n; // should not error when used as type
 let bigintNegativeLiteralType: -123n; // should not error when used as type
 const bigintNumber = 123n * 0b1111n + 0o444n * 0x7fn; // each literal should error
-
