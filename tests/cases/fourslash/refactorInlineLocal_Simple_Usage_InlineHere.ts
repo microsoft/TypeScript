b99292ccea2cb5eea825e09080cd4a2f5d9956e8
Giovanni Heilmann <giovanni.heilmann@gmail.com>
2018-11-14 12:48:46 +0100

    Correct simple testcases

--- tests/cases/fourslash/refactorInlineLocal_Simple_Usage_InlineHere.ts
+++ tests/cases/fourslash/refactorInlineLocal_Simple_Usage_InlineHere.ts
@@ -1,6 +1,6 @@
 /// <reference path='fourslash.ts' />
 
-//// const /*z*/a/*y*/ = 42 - 3;
+//// const /*z*/a/*y*/ = 42;
 //// const b = 2 * /*x*/a/*w*/;
 
 goTo.select("x", "w");
@@ -8,5 +8,5 @@ edit.applyRefactor({
     refactorName: "Inline local",
     actionName: "Inline here",
     actionDescription: "Inline here",
-    newContent: `const b = 2 * (42 - 3);`
+    newContent: `const b = 2 * 42;`
 });
