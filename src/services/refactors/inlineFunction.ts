d44fec3c752eb5947350803a94aa15745652cb74
Giovanni Heilmann <giovanni.heilmann@gmail.com>
2018-11-16 21:40:14 +0100

    Write getInfo

--- src/services/refactors/inlineFunction.ts
+++ src/services/refactors/inlineFunction.ts
@@ -13,14 +13,14 @@ namespace ts.refactor.inlineFunction {
     registerRefactor(refactorName, { getEditsForAction, getAvailableActions });
 
     interface Info {
-        readonly declaration: VariableDeclaration;
+        readonly declaration: InlineableFunction;
         readonly usages: ReadonlyArray<Identifier>;
         readonly selectedUsage: Identifier | undefined;
     }
 
     function getAvailableActions(context: RefactorContext): ReadonlyArray<ApplicableRefactorInfo> {
-        const { file, program, startPosition } = context;
-        const info = getInfo(file, program, startPosition);
+        const { program, file, startPosition } = context;
+        const info = getInfo(program, file, startPosition);
         if (!info) return emptyArray;
         const { selectedUsage } = info;
         const refactorInfo = {
@@ -40,11 +40,53 @@ namespace ts.refactor.inlineFunction {
         return [refactorInfo];
     }
 
-    function getInfo(file: SourceFile, program: Program, startPosition: number): Info | undefined {
+    function getInfo(program: Program, file: SourceFile, startPosition: number): Info | undefined {
+        const token = getTokenAtPosition(file, startPosition);
+        const checker = program.getTypeChecker();
+        const maybeFunc = getContainingFunction(token);
+        isCallExpression(token);
+        if (isIdentifier(token)) {
+            if (isNameOfFunctionDeclaration(token)) {
+                return createInfo(checker, <InlineableFunction>token.parent);
+            }
+            const symbol = checker.getSymbolAtLocation(token);
+            if (!symbol) return undefined;
+            const declaration = symbol.valueDeclaration;
+            if (!isInlineableFunction(declaration)) return undefined;
+            return createInfo(checker, declaration, token);
+        }
         return undefined;
+    }
+
+    function createInfo(checker: TypeChecker, declaration: InlineableFunction, token?: Identifier): Info | undefined {
+        const usages = inlineLocal.getReferencesInScope(
+            getEnclosingBlockScopeContainer(declaration),
+            declaration.name!,
+            checker);
+        return canInline(declaration, usages) ? {
+            declaration,
+            usages,
+            selectedUsage: token ? token : undefined
+        } : undefined;
+    }
+
+    function canInline(declaration: InlineableFunction, usages: ReadonlyArray<Identifier>): boolean {
+        let hasErrors = false;
+        if (containsProhibitedModifiers(declaration.parent.parent.modifiers)) hasErrors = true;
+        return !hasErrors;
+    }
+
+    function containsProhibitedModifiers(modifiers?: NodeArray<Modifier>): boolean {
+        return !!modifiers && !!modifiers.find(mod => mod.kind === SyntaxKind.ExportKeyword || mod.kind === SyntaxKind.PrivateKeyword);
     }
 
     function getEditsForAction(context: RefactorContext, actionName: string): RefactorEditInfo | undefined {
         return undefined;
+    }
+
+    type InlineableFunction = FunctionDeclaration | MethodDeclaration;
+
+    function isInlineableFunction(node: Node): node is InlineableFunction {
+        return node.kind === SyntaxKind.FunctionDeclaration || node.kind === SyntaxKind.MethodDeclaration;
     }
 }
