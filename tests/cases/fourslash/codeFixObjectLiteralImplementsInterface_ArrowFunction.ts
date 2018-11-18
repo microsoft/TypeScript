21edb5f34f9fae9a14278c4196705494fff50438
BigAru <arooran@indikon.ch>
2018-11-18 13:29:50 +0100

    add test for arrowFunction

---
+++ tests/cases/fourslash/codeFixObjectLiteralImplementsInterface_ArrowFunction.ts
@@ -0,0 +1,19 @@
+/// <reference path='fourslash.ts' />
+
+//// interface foo {
+////     bar: (x: boolean, y: number) => boolean;
+//// }
+//// let n: foo = { }
+
+verify.codeFix({
+    description: "Implement interface 'foo'",
+    newFileContent:
+`interface foo {
+    bar: (x: boolean, y: number) => boolean;
+}
+let n: foo = {
+    bar(): (x: boolean, y: number) => boolean {
+        throw new Error("Method not implemented.");
+    },
+}`,
+});
