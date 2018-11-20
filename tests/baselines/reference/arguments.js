94d7e3039341a812e18e64610f7ce1413241b66a
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-11-20 22:01:34 +0100

    MethodSignature doesn't have 'arguments' (#28625)
    Fixes: #28622

--- tests/baselines/reference/arguments.js
+++ tests/baselines/reference/arguments.js
@@ -3,6 +3,14 @@ function f() {
     var x=arguments[12];
 }
 
+interface I {
+    method(args: typeof arguments): void;
+    fn: (args: typeof arguments) => void;
+    (args: typeof arguments): void;
+    new (args: typeof arguments): void;
+    construct: new (args: typeof arguments) => void;
+}
+
 //// [arguments.js]
 function f() {
     var x = arguments[12];
