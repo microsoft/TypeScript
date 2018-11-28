500a8f7cda1dace787c063c077cc5a2350159dd5
BigAru <arooran@indikon.ch>
2018-11-28 08:11:24 +0100

    add testcase for generic function

---
+++ tests/cases/fourslash/codeFixObjectLiteralImplementsInterface_PropertyGenericFunction.ts
@@ -0,0 +1,19 @@
+/// <reference path='fourslash.ts' />
+
+//// interface foo {
+////     bar<T>(x: number, y: T): boolean;
+//// }
+//// let n: foo = { }
+
+verify.codeFix({
+    description: "Implement interface 'foo'",
+    newFileContent:
+`interface foo {
+    bar<T>(x: number, y: T): boolean;
+}
+let n: foo = {
+    bar<T>(x: number, y: T): boolean {
+        throw new Error("Function not implemented.");
+    },
+}`,
+});
