b44ee8ef4b5f3775c451b7aad90d53e8a284bdd0
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-11-30 14:59:43 -0800

    Added tests for types with overlap across a single property name.

--- tests/cases/compiler/errorsOnUnionsOfOverlappingObjects01.ts
+++ tests/cases/compiler/errorsOnUnionsOfOverlappingObjects01.ts
@@ -27,3 +27,21 @@ declare function h(x: Foo | Bar | Other): any;
 
 h(x);
 h({ a: '', b: '' })
+
+interface CatDog { cat: any, dog: any }
+interface ManBearPig { man: any, bear: any, pig: any }
+interface Platypus { platypus: any }
+
+type ExoticAnimal =
+    | CatDog
+    | ManBearPig
+    | Platypus;
+
+declare function addToZoo(animal: ExoticAnimal): void;
+
+addToZoo({ dog: "Barky McBarkface" });
+addToZoo({ man: "Manny", bear: "Coffee" });
+
+const manBeer = { man: "Manny", beer: "Coffee" };
+addToZoo({ man: "Manny", beer: "Coffee" });
+addToZoo(manBeer);
