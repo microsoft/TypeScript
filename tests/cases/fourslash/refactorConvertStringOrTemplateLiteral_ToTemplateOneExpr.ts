7620615212eadbb407768247cee6263c4047fdfe
BigAru <arooran@indikon.ch>
2018-12-05 06:00:28 +0100

    add test cases

---
+++ tests/cases/fourslash/refactorConvertStringOrTemplateLiteral_ToTemplateOneExpr.ts
@@ -0,0 +1,14 @@
+/// <reference path='fourslash.ts' />
+
+//// const age = 42
+//// const foo = "/*x*/f/*y*/oobar is " + age + " years old"
+
+goTo.select("x", "y");
+edit.applyRefactor({
+    refactorName: "Convert string concatenation or template literal",
+    actionName: "Convert to template literal",
+    actionDescription: "Convert to template literal",
+    newContent: 
+`const age = 42
+const foo = \`foobar is \${ age } years old\``,
+});
