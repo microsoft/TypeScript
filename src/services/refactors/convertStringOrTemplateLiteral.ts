03f0f88101ce3c5afff0457a7305219446cc8a35
BigAru <arooran@indikon.ch>
2018-12-05 09:14:23 +0100

    add diagnostic messages

--- src/services/refactors/convertStringOrTemplateLiteral.ts
+++ src/services/refactors/convertStringOrTemplateLiteral.ts
@@ -1,13 +1,13 @@
 /* @internal */
 namespace ts.refactor.convertStringOrTemplateLiteral {
     const refactorName = "Convert string concatenation or template literal";
-    const refactorDescription = "Convert string concatenation or template literal";
-    const toTemplateLiteralActionName = "Convert to template literal";
-    const toStringConcatenationActionName = "Convert to string concatenation";
-    const toTemplateLiteralDescription = "Convert to template literal";
-    const toStringConcatenationDescription = "Convert to string concatenation";
+    // const toTemplateLiteralActionName = "Convert to template literal";
+    // const toStringConcatenationActionName = "Convert to string concatenation";
 
-    // TODO convert Description to DiagnosticMsg
+    // const refactorDescription = getLocaleSpecificMessage(Diagnostics.Convert_string_concatenation_or_template_literal);
+    // const toTemplateLiteralDescription = getLocaleSpecificMessage(Diagnostics.Convert_to_template_literal);
+    // const toStringConcatenationDescription = getLocaleSpecificMessage(Diagnostics.Convert_to_string_concatenation);
+
     registerRefactor(refactorName, { getEditsForAction, getAvailableActions });
 
     function getAvailableActions(context: RefactorContext): ReadonlyArray<ApplicableRefactorInfo> {
@@ -17,7 +17,7 @@ namespace ts.refactor.convertStringOrTemplateLiteral {
     }
 
     function getEditsForAction(context: RefactorContext, actionName: string): RefactorEditInfo | undefined {
-        const { file, startPosition } = context; file; startPosition;
+        const { file, startPosition } = context; file; startPosition; actionName;
         return undefined;
     }
 
