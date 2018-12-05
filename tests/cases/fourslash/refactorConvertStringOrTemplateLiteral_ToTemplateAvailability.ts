1046eb6bdc89b6ef49741627379981f4fbc0f261
BigAru <arooran@indikon.ch>
2018-12-05 08:59:18 +0100

    add visibility tests

---
+++ tests/cases/fourslash/refactorConvertStringOrTemplateLiteral_ToTemplateAvailability.ts
@@ -0,0 +1,30 @@
+/// <reference path='fourslash.ts' />
+
+//// const age = 22
+//// const name = "Eddy"
+//// const /*z*/f/*y*/oo = /*x*/"/*w*/M/*v*/r/*u*/ " /*t*/+/*s*/ /*r*/n/*q*/ame + " is " + /*p*/a/*o*/ge + " years old"
+
+goTo.select("z", "y");
+verify.not.refactorAvailable("Convert string concatenation or template literal", "Convert to string concatenation")
+verify.not.refactorAvailable("Convert string concatenation or template literal", "Convert to template literal")
+
+goTo.select("x", "w");
+verify.not.refactorAvailable("Convert string concatenation or template literal", "Convert to string concatenation")
+verify.refactorAvailable("Convert string concatenation or template literal", "Convert to template literal")
+
+goTo.select("v", "u");
+verify.not.refactorAvailable("Convert string concatenation or template literal", "Convert to string concatenation")
+verify.refactorAvailable("Convert string concatenation or template literal", "Convert to template literal")
+
+goTo.select("t", "s");
+verify.not.refactorAvailable("Convert string concatenation or template literal", "Convert to string concatenation")
+verify.refactorAvailable("Convert string concatenation or template literal", "Convert to template literal")
+
+goTo.select("r", "q");
+verify.not.refactorAvailable("Convert string concatenation or template literal", "Convert to string concatenation")
+verify.refactorAvailable("Convert string concatenation or template literal", "Convert to template literal")
+
+goTo.select("p", "o");
+verify.not.refactorAvailable("Convert string concatenation or template literal", "Convert to string concatenation")
+verify.refactorAvailable("Convert string concatenation or template literal", "Convert to template literal")
+
