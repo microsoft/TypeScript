174816fc2608adece8fd1f831bb3b9054e22835d
Josh Goldberg <josh@fullscreenmario.com>
2018-12-04 17:59:20 -0800

    Added error for IdentifierStart immediately after a NumericLiteral
    Fixes #4702.

---
+++ tests/baselines/reference/identifierStartAfterNumericLiteral.js
@@ -0,0 +1,35 @@
+//// [identifierStartAfterNumericLiteral.ts]
+let valueIn = 3in[null];
+
+3a[null]
+3e[null]
+3in[null]
+3en[null]
+1a
+1e
+1e9
+1ee
+1n
+2n2
+2na
+
+
+//// [identifierStartAfterNumericLiteral.js]
+var valueIn = 3 in [null];
+3;
+a[null];
+3e[null];
+3 in [null];
+3e;
+n[null];
+1;
+a;
+1e;
+1e9;
+1e;
+e;
+1n;
+2n;
+2;
+2n;
+a;
---
+++ tests/cases/compiler/identifierStartAfterNumericLiteral.ts
@@ -0,0 +1,13 @@
+let valueIn = 3in[null];
+
+3a[null]
+3e[null]
+3in[null]
+3en[null]
+1a
+1e
+1e9
+1ee
+1n
+2n2
+2na
