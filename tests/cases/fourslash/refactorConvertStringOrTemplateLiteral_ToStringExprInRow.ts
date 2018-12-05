949262330a2da2482da265ce653d346721d7d164
BigAru <arooran@indikon.ch>
2018-12-05 08:30:22 +0100

    add test cases

---
+++ tests/cases/fourslash/refactorConvertStringOrTemplateLiteral_ToStringExprInRow.ts
@@ -0,0 +1,12 @@
+/// <reference path='fourslash.ts' />
+
+//// const foo = `/*x*/f/*y*/oobar is ${ 42 }${ 6 } years old`
+
+goTo.select("x", "y");
+edit.applyRefactor({
+    refactorName: "Convert string concatenation or template literal",
+    actionName: "Convert to string concatenation",
+    actionDescription: "Convert to string concatenation",
+    newContent: 
+`const foo = "foobar is " + 42 + 6 + " years old"`,
+});
---
+++ tests/cases/fourslash/refactorConvertStringOrTemplateLiteral_ToStringOneExpr.ts
@@ -0,0 +1,14 @@
+/// <reference path='fourslash.ts' />
+
+//// const age = 42
+//// const foo = `/*x*/f/*y*/oobar is ${ age } years old`
+
+goTo.select("x", "y");
+edit.applyRefactor({
+    refactorName: "Convert string concatenation or template literal",
+    actionName: "Convert to string concatenation",
+    actionDescription: "Convert to string concatenation",
+    newContent: 
+`const age = 42
+const foo = "foobar is " + age + " years old"`,
+});
