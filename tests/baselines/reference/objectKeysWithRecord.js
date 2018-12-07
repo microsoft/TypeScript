146ed2fec5b8fced61567b7e9c2b441fdbabdab6
Christophe Hurpeau <christophe@hurpeau.com>
2018-12-07 13:22:03 +0100

    test: jake accept baseline

---
+++ tests/baselines/reference/objectKeysWithRecord.js
@@ -0,0 +1,13 @@
+//// [objectKeysWithRecord.ts]
+type K = 'foo' | 'bar'
+const record: Record<K, boolean> = { foo: true, bar: false };
+
+Object.keys(record).forEach((key: K) => {
+
+});
+
+
+//// [objectKeysWithRecord.js]
+var record = { foo: true, bar: false };
+Object.keys(record).forEach(function (key) {
+});
