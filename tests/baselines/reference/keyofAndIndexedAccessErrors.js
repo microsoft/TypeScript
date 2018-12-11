02b13a916a07cbd54e3132ea703e0be2035a1866
Anders Hejlsberg <andersh@microsoft.com>
2018-12-11 12:12:57 -0800

    Accept new baselines

--- tests/baselines/reference/keyofAndIndexedAccessErrors.js
+++ tests/baselines/reference/keyofAndIndexedAccessErrors.js
@@ -143,6 +143,22 @@ function test1<T extends Record<string, any>, K extends keyof T>(t: T, k: K) {
     t[k] = [10, 20];  // Error
 }
 
+// Repro from #28839
+
+function f30<T, K extends keyof T>() {
+    let x: Partial<Record<keyof T, string>>[K] = "hello";
+}
+
+// We simplify indexed accesses applied to mapped types up to five levels deep
+
+function f31<T, K extends keyof T>() {
+    let x: Partial<Partial<Partial<Partial<Record<keyof T, string>>>>>[K] = "hello";
+}
+
+function f32<T, K extends keyof T>() {
+    let x: Partial<Partial<Partial<Partial<Partial<Record<keyof T, string>>>>>>[K] = "hello";
+}
+
 
 //// [keyofAndIndexedAccessErrors.js]
 var Shape = /** @class */ (function () {
@@ -214,4 +230,15 @@ function test1(t, k) {
     t[k] = 42; // Error
     t[k] = "hello"; // Error
     t[k] = [10, 20]; // Error
+}
+// Repro from #28839
+function f30() {
+    var x = "hello";
+}
+// We simplify indexed accesses applied to mapped types up to five levels deep
+function f31() {
+    var x = "hello";
+}
+function f32() {
+    var x = "hello";
 }
