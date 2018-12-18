678117711578bdfb1623cc2442f4f57da3da3ac6
ECrownofFire <ECrownofFire@gmail.com>
2018-12-18 05:10:53 -0500

    Add basic operator test for ranges

---
+++ tests/baselines/reference/rangeTypeBasicOperators.js
@@ -0,0 +1,33 @@
+//// [rangeTypeBasicOperators.ts]
+declare var a: (> 42);
+declare var b: (<= 3);
+
+let a1 = a + b;
+let a2 = a - b;
+let a3 = a * b;
+let a4 = a / b;
+let a5 = a % b;
+let a6 = a & b;
+let a7 = a | b;
+let a8 = a ^ b;
+let a9 = a << b;
+let a10 = a >> b;
+let a11 = a >>> b;
+let a12 = ~a;
+let a13 = -a;
+
+
+//// [rangeTypeBasicOperators.js]
+var a1 = a + b;
+var a2 = a - b;
+var a3 = a * b;
+var a4 = a / b;
+var a5 = a % b;
+var a6 = a & b;
+var a7 = a | b;
+var a8 = a ^ b;
+var a9 = a << b;
+var a10 = a >> b;
+var a11 = a >>> b;
+var a12 = ~a;
+var a13 = -a;
---
+++ tests/cases/compiler/rangeTypeBasicOperators.ts
@@ -0,0 +1,16 @@
+declare var a: (> 42);
+declare var b: (<= 3);
+
+let a1 = a + b;
+let a2 = a - b;
+let a3 = a * b;
+let a4 = a / b;
+let a5 = a % b;
+let a6 = a & b;
+let a7 = a | b;
+let a8 = a ^ b;
+let a9 = a << b;
+let a10 = a >> b;
+let a11 = a >>> b;
+let a12 = ~a;
+let a13 = -a;
