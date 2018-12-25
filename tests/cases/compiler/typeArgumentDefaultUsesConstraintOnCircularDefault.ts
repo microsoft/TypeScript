b2dc1b3d3154e6bcde50800dafbd559240bf173e
Anders Hejlsberg <andersh@microsoft.com>
2018-12-25 08:22:11 -1000

    Add regression test

--- tests/cases/compiler/typeArgumentDefaultUsesConstraintOnCircularDefault.ts
+++ tests/cases/compiler/typeArgumentDefaultUsesConstraintOnCircularDefault.ts
@@ -3,3 +3,9 @@ type Test<T extends string = T> = { value: T };
 let zz: Test = { foo: "abc" };  // should error on comparison with Test<string>
 
 let zzy: Test = { value: {} };  // should error
+
+// Simplified repro from #28873
+
+class C1<T extends C1 = any> {}  // Error, circular constraint
+
+class C2<T extends C2<any> = any> {}
