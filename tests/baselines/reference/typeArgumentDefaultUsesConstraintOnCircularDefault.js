ebb66dffa257c3ce70aded3871874306eb45e602
Anders Hejlsberg <andersh@microsoft.com>
2018-12-25 08:22:21 -1000

    Accept new baselines

--- tests/baselines/reference/typeArgumentDefaultUsesConstraintOnCircularDefault.js
+++ tests/baselines/reference/typeArgumentDefaultUsesConstraintOnCircularDefault.js
@@ -5,7 +5,24 @@ let zz: Test = { foo: "abc" };  // should error on comparison with Test<string>
 
 let zzy: Test = { value: {} };  // should error
 
+// Simplified repro from #28873
+
+class C1<T extends C1 = any> {}  // Error, circular constraint
+
+class C2<T extends C2<any> = any> {}
+
 
 //// [typeArgumentDefaultUsesConstraintOnCircularDefault.js]
 var zz = { foo: "abc" }; // should error on comparison with Test<string>
 var zzy = { value: {} }; // should error
+// Simplified repro from #28873
+var C1 = /** @class */ (function () {
+    function C1() {
+    }
+    return C1;
+}()); // Error, circular constraint
+var C2 = /** @class */ (function () {
+    function C2() {
+    }
+    return C2;
+}());
