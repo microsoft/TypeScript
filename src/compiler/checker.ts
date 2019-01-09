80558a4428305f335d8a224e0b0db8def823b93f
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-01-09 08:55:04 -0800

    Restore former behaviour of implicitThis errors
    I left the noImplicitThis rule for captured use of global this in an
    arrow function, even though technically it isn't `any` any more --
    it's typeof globalThis.  However, you should still use some other method
    to access globals inside an arrow, because captured-global-this is super
    confusing there.

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -16709,17 +16709,19 @@ namespace ts {
             }
 
             const type = tryGetThisTypeAt(node, container);
-            if (!type && noImplicitThis) {
-                // With noImplicitThis, functions may not reference 'this' if it has type 'any'
-                const diag = error(
-                    node,
-                    capturedByArrowFunction && container.kind === SyntaxKind.SourceFile ?
-                        Diagnostics.The_containing_arrow_function_captures_the_global_value_of_this_which_implicitly_has_type_any :
-                        Diagnostics.this_implicitly_has_type_any_because_it_does_not_have_a_type_annotation);
-                if (!isSourceFile(container)) {
-                    const outsideThis = tryGetThisTypeAt(container);
-                    if (outsideThis) {
-                        addRelatedInfo(diag, createDiagnosticForNode(container, Diagnostics.An_outer_value_of_this_is_shadowed_by_this_container));
+            const globalThisType = getGlobalThisType();
+            if (noImplicitThis) {
+                if (type === globalThisType && capturedByArrowFunction) {
+                    error(node, Diagnostics.The_containing_arrow_function_captures_the_global_value_of_this);
+                }
+                if (!type) {
+                    // With noImplicitThis, functions may not reference 'this' if it has type 'any'
+                    const diag = error(node, Diagnostics.this_implicitly_has_type_any_because_it_does_not_have_a_type_annotation);
+                    if (!isSourceFile(container)) {
+                        const outsideThis = tryGetThisTypeAt(container);
+                        if (outsideThis && outsideThis !== globalThisType) {
+                            addRelatedInfo(diag, createDiagnosticForNode(container, Diagnostics.An_outer_value_of_this_is_shadowed_by_this_container));
+                        }
                     }
                 }
             }
