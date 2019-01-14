f0ebf90364635bb94e7a789ceb34aa868ab70e17
Wesley Wigham <wewigham@microsoft.com>
2019-01-14 13:04:45 -0800

    Save & recalculate declre flag modifier on late printed statements

--- src/compiler/transformers/declarations.ts
+++ src/compiler/transformers/declarations.ts
@@ -634,7 +634,10 @@ namespace ts {
                 if (!isLateVisibilityPaintedStatement(i)) {
                     return Debug.fail(`Late replaced statement was found which is not handled by the declaration transformer!: ${(ts as any).SyntaxKind ? (ts as any).SyntaxKind[(i as any).kind] : (i as any).kind}`);
                 }
+                const priorNeedsDeclare = needsDeclare;
+                needsDeclare = i.parent && isSourceFile(i.parent) && !(isExternalModule(i.parent) && isBundledEmit);
                 const result = transformTopLevelDeclaration(i, /*privateDeclaration*/ true);
+                needsDeclare = priorNeedsDeclare;
                 lateStatementReplacementMap.set("" + getOriginalNodeId(i), result);
             }
 
