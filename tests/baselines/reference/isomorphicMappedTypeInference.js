262e3c1ae294cef6e1075fa53a4d60481c700494
Anders Hejlsberg <andersh@microsoft.com>
2019-02-06 15:41:51 -0800

    Accept new baselines

--- tests/baselines/reference/isomorphicMappedTypeInference.js
+++ tests/baselines/reference/isomorphicMappedTypeInference.js
@@ -151,6 +151,30 @@ let o = {a: 5, b: 7};
 foo(o, {b: 9});
 o = foo(o, {b: 9});
 
+// Inferring to { [P in K]: X }, where K extends keyof T, produces same inferences as
+// inferring to { [P in keyof T]: X }.
+
+declare function f20<T, K extends keyof T>(obj: Pick<T, K>): T;
+declare function f21<T, K extends keyof T>(obj: Pick<T, K>): K;
+declare function f22<T, K extends keyof T>(obj: Boxified<Pick<T, K>>): T;
+
+let x0 = f20({ foo: 42, bar: "hello" });
+let x1 = f21({ foo: 42, bar: "hello" });
+let x2 = f22({ foo: { value: 42} , bar: { value: "hello" } });
+
+// Repro from #29765
+
+function getProps<T, K extends keyof T>(obj: T, list: K[]): Pick<T, K> {
+    return {} as any;
+}
+
+const myAny: any = {};
+
+const o1 = getProps(myAny, ['foo', 'bar']);
+
+const o2: { foo: any; bar: any } = getProps(myAny, ['foo', 'bar']);
+
+
 //// [isomorphicMappedTypeInference.js]
 function box(x) {
     return { value: x };
@@ -255,6 +279,16 @@ var foo = function (object, partial) { return object; };
 var o = { a: 5, b: 7 };
 foo(o, { b: 9 });
 o = foo(o, { b: 9 });
+var x0 = f20({ foo: 42, bar: "hello" });
+var x1 = f21({ foo: 42, bar: "hello" });
+var x2 = f22({ foo: { value: 42 }, bar: { value: "hello" } });
+// Repro from #29765
+function getProps(obj, list) {
+    return {};
+}
+var myAny = {};
+var o1 = getProps(myAny, ['foo', 'bar']);
+var o2 = getProps(myAny, ['foo', 'bar']);
 
 
 //// [isomorphicMappedTypeInference.d.ts]
@@ -322,4 +356,23 @@ declare const foo: <T>(object: T, partial: Partial<T>) => T;
 declare let o: {
     a: number;
     b: number;
+};
+declare function f20<T, K extends keyof T>(obj: Pick<T, K>): T;
+declare function f21<T, K extends keyof T>(obj: Pick<T, K>): K;
+declare function f22<T, K extends keyof T>(obj: Boxified<Pick<T, K>>): T;
+declare let x0: {
+    foo: number;
+    bar: string;
+};
+declare let x1: "foo" | "bar";
+declare let x2: {
+    foo: number;
+    bar: string;
+};
+declare function getProps<T, K extends keyof T>(obj: T, list: K[]): Pick<T, K>;
+declare const myAny: any;
+declare const o1: Pick<any, "foo" | "bar">;
+declare const o2: {
+    foo: any;
+    bar: any;
 };
