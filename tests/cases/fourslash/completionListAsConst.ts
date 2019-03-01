feb11485a0808145921e063e9677cbf288b21216
André Rodrigues <andrefrodrigues21@gmail.com>
2019-03-01 16:44:42 +0000

    Add test cases to issues

---
+++ tests/cases/fourslash/completionListAsConst.ts
@@ -0,0 +1,5 @@
+/// <reference path='fourslash.ts'/>
+
+////1 as con/*1*/
+
+verify.completions({marker:"1", includes:"const"});
---
+++ tests/cases/fourslash/completionListReadonlyType.ts
@@ -0,0 +1,5 @@
+/// <reference path='fourslash.ts'/>
+
+////type T = read/*1*/
+
+verify.completions({marker:"1", includes:"readonly"});
