a60a6f311ff53dd46292710f333b5a9f8244dbea
Nicolas DUBIEN <github@dubien.org>
2019-03-05 13:03:57 +0100

    Better typings of Object.keys when keys coming from enum

--- lib/lib.es5.d.ts
+++ lib/lib.es5.d.ts
@@ -258,6 +258,12 @@ interface ObjectConstructor {
       * Returns the names of the enumerable properties and methods of an object.
       * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
       */
+    keys<EnumType extends string>(o: { [K in EnumType]: any }): EnumType[];
+
+    /**
+      * Returns the names of the enumerable properties and methods of an object.
+      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+      */
     keys(o: {}): string[];
 }
 
