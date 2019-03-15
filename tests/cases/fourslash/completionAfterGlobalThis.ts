42c65dd39ac321ee00886fe579e14d6b0f03bcbd
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-03-15 16:53:05 -0700

    Add missing completions to test

--- tests/cases/fourslash/completionAfterGlobalThis.ts
+++ tests/cases/fourslash/completionAfterGlobalThis.ts
@@ -3,4 +3,11 @@
 ////globalThis./**/
 
 // TODO: This is right except for globalThis and undefined
-verify.completions({ marker: "", exact: completion.globalsVars });
+verify.completions({
+    marker: "",
+    exact: [
+        { name: "globalThis", kind: "module" },
+        ...completion.globalsVars,
+        { name: "undefined", kind: "var" }
+    ]
+});
