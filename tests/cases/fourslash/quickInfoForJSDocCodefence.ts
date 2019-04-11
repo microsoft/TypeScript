9df2f88372b6330a6a1043cbb90d9d3ea383b946
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-04-11 14:43:53 -0700

    Scan backticks in jsdoc as a single token less often
    Previously, matching backticks in jsdoc would always be scanned as one
    token to aid parsing incorrect jsdoc that uses backticks for parameter
    names:
    
    ``js
    /** @param {string} `nope`
     * @param {number} `not needed`
     */
    ```
    
    However, this is wrong for code fences, which use triple backticks. This
    fix parses a single backtick as a single token if it's immediately
    followed by another backtick or by a newline. It retains the
    questionable tokenisation of backticks-as-pairs in other cases, however.
    A better fix might be to have the parser ignore backticks in jsdoc
    instead.

---
+++ tests/cases/fourslash/quickInfoForJSDocCodefence.ts
@@ -0,0 +1,12 @@
+/// <reference path='fourslash.ts' />
+
+/////**
+//// * @example
+//// * ```
+//// * 1 + 2
+//// * ```
+//// */
+////function fo/*1*/o() {
+////    return '2';
+////}
+verify.baselineQuickInfo();
