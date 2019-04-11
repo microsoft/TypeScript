dfdffe34df24ce7dde67e3516a55e53c689fb2dd
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-04-11 14:49:11 -0700

    Add test case

--- tests/cases/fourslash/quickInfoForJSDocCodefence.ts
+++ tests/cases/fourslash/quickInfoForJSDocCodefence.ts
@@ -9,4 +9,13 @@
 ////function fo/*1*/o() {
 ////    return '2';
 ////}
+/////**
+//// * @example
+//// * ``
+//// * 1 + 2
+//// * `
+//// */
+////function bo/*2*/o() {
+////    return '2';
+////}
 verify.baselineQuickInfo();
