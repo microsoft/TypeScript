8c5688431d3761e57c5f134d54a89e7fb539bb8c
lvlei03 <lvlei03@baidu.com>
2019-04-14 18:46:42 +0800

    Uniform code indentation style

--- lib/lib.es5.d.ts
+++ lib/lib.es5.d.ts
@@ -1998,13 +1998,13 @@ interface Uint8Array {
     every(callbackfn: (value: number, index: number, array: Uint8Array) => boolean, thisArg?: any): boolean;
 
     /**
-    	* Returns the this object after filling the section identified by start and end with value
-    	* @param value value to fill array section with
-    	* @param start index to start filling the array at. If start is negative, it is treated as
-    	* length+start where length is the length of the array.
-    	* @param end index to stop filling the array at. If end is negative, it is treated as
-    	* length+end.
-    	*/
+      * Returns the this object after filling the section identified by start and end with value
+      * @param value value to fill array section with
+      * @param start index to start filling the array at. If start is negative, it is treated as
+      * length+start where length is the length of the array.
+      * @param end index to stop filling the array at. If end is negative, it is treated as
+      * length+end.
+      */
     fill(value: number, start?: number, end?: number): this;
 
     /**
