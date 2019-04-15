32caaf60c0f53a258e3a371bd164a51e15b3201b
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-04-15 22:21:59 +0900

    add some comments

--- src/lib/es2019.object.d.ts
+++ src/lib/es2019.object.d.ts
@@ -1,5 +1,9 @@
 /// <reference lib="es2015.iterable" />
 
 interface ObjectConstructor {
+    /**
+     * Returns an object created by key-value entries for properties and methods
+     * @param entries An iterable object that contains key-value entries for properties and methods.
+     */
     fromEntries(entries: Iterable<[string | symbol, any]>): any;
 }
