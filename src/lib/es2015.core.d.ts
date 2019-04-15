efb263ef262b43d989f117d78b1ba3da92d336ee
keilyn3d <keilyn2005@hotmail.com>
2019-04-15 02:54:41 -0300

    Improve intellisense inside Object.assign
    Many times We use Object.assign to set the properties of some existing type, but intellisense don't work, with this change We can have intellisense inside the secondary parameter of Object.assign
    
    Example:
    var div = document.createElement("div");
    Object.assign(div.style, {
        color: "red" // Here we can have intellisense with all the properties of the first element type
    });

--- src/lib/es2015.core.d.ts
+++ src/lib/es2015.core.d.ts
@@ -263,7 +263,7 @@ interface ObjectConstructor {
      * @param target The target object to copy to.
      * @param source The source object from which to copy properties.
      */
-    assign<T, U>(target: T, source: U): T & U;
+    assign<T, U | T>(target: T, source: U): T & U;
 
     /**
      * Copy the values of all of the enumerable own properties from one or more source objects to a
