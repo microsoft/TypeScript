519d0e603d8f493ebe8f6e26286aa3fa38f13877
Anders Hejlsberg <andersh@microsoft.com>
2019-04-16 07:25:41 -1000

    Merge pull request #30963 from Microsoft/fixObjectFlagsPropagation
    Fix object flags propagation

--- tests/cases/compiler/genericFunctionInference2.ts
+++ tests/cases/compiler/genericFunctionInference2.ts
@@ -13,3 +13,18 @@ const myReducer1: Reducer<MyState> = combineReducers({
 const myReducer2 = combineReducers({
     combined: combineReducers({ foo }),
 });
+
+// Repro from #30942
+
+declare function withH<T, U>(handlerCreators: HandleCreatorsFactory<T, U>): U;
+
+type Props = { out: number }
+
+type HandleCreatorsFactory<T, U> = (initialProps: T) => { [P in keyof U]: (props: T) => U[P] };
+
+const enhancer4 = withH((props: Props) => ({
+    onChange: (props) => (e: any) => {},
+    onSubmit: (props) => (e: any) => {},
+}));
+
+enhancer4.onChange(null);
