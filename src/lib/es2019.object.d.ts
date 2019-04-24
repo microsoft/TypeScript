9bc4d6a7b93fac579bf227dcbf0cf4ccab6b8724
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-04-24 10:43:11 +0900

    apply suggested changes

--- src/lib/es2019.object.d.ts
+++ src/lib/es2019.object.d.ts
@@ -5,5 +5,5 @@ interface ObjectConstructor {
      * Returns an object created by key-value entries for properties and methods
      * @param entries An iterable object that contains key-value entries for properties and methods.
      */
-    fromEntries(entries: Iterable<[string | symbol, any]>): any;
+    fromEntries<T = any>(entries: Iterable<[PropertyKey, T]>): { [k in PropertyKey]: T };
 }
