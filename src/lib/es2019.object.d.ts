42626bd0ef833fb2dc5fe5c98879a0cdfc345035
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-04-25 14:37:07 +0900

    add readonly and general any

--- src/lib/es2019.object.d.ts
+++ src/lib/es2019.object.d.ts
@@ -5,5 +5,11 @@ interface ObjectConstructor {
      * Returns an object created by key-value entries for properties and methods
      * @param entries An iterable object that contains key-value entries for properties and methods.
      */
-    fromEntries<T = any>(entries: Iterable<[PropertyKey, T]>): { [k in PropertyKey]: T };
+    fromEntries<T = any>(entries: Iterable<readonly [PropertyKey, T]>): { [k in PropertyKey]: T };
+
+    /**
+     * Returns an object created by key-value entries for properties and methods
+     * @param entries An iterable object that contains key-value entries for properties and methods.
+     */
+    fromEntries(entries: Iterable<readonly any[]>): any;
 }
