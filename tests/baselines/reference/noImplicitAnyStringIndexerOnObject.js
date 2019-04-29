b66fea6e4c9fdf608311305daff0a531fc5a7d05
Collins Abitekaniza <abtcolns@gmail.com>
2019-04-30 02:09:06 +0300

    add tests for noImplicitAny indexing on Object

--- tests/baselines/reference/noImplicitAnyStringIndexerOnObject.js
+++ tests/baselines/reference/noImplicitAnyStringIndexerOnObject.js
@@ -1,7 +1,43 @@
 //// [noImplicitAnyStringIndexerOnObject.ts]
-var x = {}["hello"];
-var y: string = { '': 'foo' }[''];
+var a = {}["hello"];
+var b: string = { '': 'foo' }[''];
+
+// Should give suggestion 'c.get'
+var c = {
+  get: (key: string) => 'foobar'
+};
+c['hello'];
+
+// Should give suggestion 'd.set'
+var d = {
+  set: (key: string) => 'foobar'
+};
+d['hello'];
+
+// Should give suggestion 'e.get or e.set'
+var e = {
+  set: (key: string) => 'foobar',
+  get: (key: string) => 'foobar'
+};
+e['hello'];
+
 
 //// [noImplicitAnyStringIndexerOnObject.js]
-var x = {}["hello"];
-var y = { '': 'foo' }[''];
+var a = {}["hello"];
+var b = { '': 'foo' }[''];
+// Should give suggestion 'c.get'
+var c = {
+    get: function (key) { return 'foobar'; }
+};
+c['hello'];
+// Should give suggestion 'd.set'
+var d = {
+    set: function (key) { return 'foobar'; }
+};
+d['hello'];
+// Should give suggestion 'e.get or e.set'
+var e = {
+    set: function (key) { return 'foobar'; },
+    get: function (key) { return 'foobar'; }
+};
+e['hello'];
--- tests/cases/compiler/noImplicitAnyStringIndexerOnObject.ts
+++ tests/cases/compiler/noImplicitAnyStringIndexerOnObject.ts
@@ -1,4 +1,23 @@
 // @noimplicitany: true
 
-var x = {}["hello"];
-var y: string = { '': 'foo' }[''];
+var a = {}["hello"];
+var b: string = { '': 'foo' }[''];
+
+// Should give suggestion 'c.get'
+var c = {
+  get: (key: string) => 'foobar'
+};
+c['hello'];
+
+// Should give suggestion 'd.set'
+var d = {
+  set: (key: string) => 'foobar'
+};
+d['hello'];
+
+// Should give suggestion 'e.get or e.set'
+var e = {
+  set: (key: string) => 'foobar',
+  get: (key: string) => 'foobar'
+};
+e['hello'];
