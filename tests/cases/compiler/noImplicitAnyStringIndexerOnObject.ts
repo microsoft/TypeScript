a481be729ac183b74f186dbb82310f642f323fe9
Collins Abitekaniza <abtcolns@gmail.com>
2019-04-30 02:09:17 +0300

    remove comments regarding error messages

--- tests/cases/compiler/noImplicitAnyStringIndexerOnObject.ts
+++ tests/cases/compiler/noImplicitAnyStringIndexerOnObject.ts
@@ -3,19 +3,16 @@
 var a = {}["hello"];
 var b: string = { '': 'foo' }[''];
 
-// Should give suggestion 'c.get'
 var c = {
   get: (key: string) => 'foobar'
 };
 c['hello'];
 
-// Should give suggestion 'd.set'
 var d = {
   set: (key: string) => 'foobar'
 };
 d['hello'];
 
-// Should give suggestion 'e.get or e.set'
 var e = {
   set: (key: string) => 'foobar',
   get: (key: string) => 'foobar'
