80773caac16ca40744839843a46e6e0b601264f9
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2019-04-30 19:00:55 +0530

    Accept new baselines

---
+++ tests/baselines/reference/keyofNonNullable.js
@@ -0,0 +1,20 @@
+//// [keyofNonNullable.ts]
+function f<X, Y extends X>(keyofX: keyof X, keyofY: keyof Y) {
+    keyofY = keyofX;
+    keyofX = keyofY;
+}
+
+
+function f1<T>(keyofT: keyof T, keyofNonNullT: keyof NonNullable<T>) {
+    keyofT = keyofNonNullT
+}
+
+
+//// [keyofNonNullable.js]
+function f(keyofX, keyofY) {
+    keyofY = keyofX;
+    keyofX = keyofY;
+}
+function f1(keyofT, keyofNonNullT) {
+    keyofT = keyofNonNullT;
+}
