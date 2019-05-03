4b08941ca633c767e280e578a3bf62b7fe0a1002
Collins Abitekaniza <abtcolns@gmail.com>
2019-05-03 17:40:03 +0300

    refactor baseline tests for import/export name collision

--- tests/baselines/reference/mergeWithImportedType.js
+++ tests/baselines/reference/mergeWithImportedType.js
@@ -5,7 +5,6 @@ export enum E {X}
 
 //// [f2.ts]
 import {E} from "./f1";
-// partial revert of https://github.com/Microsoft/TypeScript/pull/7583 to prevent breaking changes
 export type E = E;
 
 //// [f1.js]
