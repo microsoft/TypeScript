60923c53fb2b67e7cb569ec0b8e7e0e791e9c3d9
Wesley Wigham <wewigham@microsoft.com>
2019-05-15 17:36:34 -0700

    Accept baselines for expansion spans and new locations of symbol spans

---
+++ tests/baselines/reference/deepReadonlyAssignabilityError.js
@@ -0,0 +1,31 @@
+//// [deepReadonlyAssignabilityError.ts]
+type DeepReadonly<T> = {
+    readonly [K in keyof T]: DeepReadonly<T[K]>;
+}
+
+declare function f2<T>(x: DeepReadonly<T>): (x: T) => void;
+
+/**
+ * This produces a function whose argument type is a deeply recursive reverse mapped type
+ */
+const result = f2({ x: { y: { z: { a: { b: { c: 12 } } } } } });
+
+result({
+    x: {
+        y: {
+            
+        }
+    }
+});
+
+
+//// [deepReadonlyAssignabilityError.js]
+/**
+ * This produces a function whose argument type is a deeply recursive reverse mapped type
+ */
+var result = f2({ x: { y: { z: { a: { b: { c: 12 } } } } } });
+result({
+    x: {
+        y: {}
+    }
+});
