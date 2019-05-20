0fcb933496e48d1d7abf539607e2dc3c568a07df
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-05-20 14:58:46 -0700

    Allow JS with isolated modules
    Previously legacy JS code was not allowed; it was required to use ES6
    module syntax. Unfortunately, the check happens after parsing but before
    binding, and the commonjs module indicator isn't set until binding
    because it's not syntactically simple like the ES6 module indicator,
    which is set during parsing.
    
    So I decided that JS should be allowed during isolatedModules
    unconditionally. We're not going to be transforming it anyway.

---
+++ tests/cases/compiler/commonJsIsolatedModules.ts
@@ -0,0 +1,12 @@
+// @Filename: tsconfig.json
+{
+  "compilerOptions": {
+    "allowJs": true,
+    "noEmit": true,
+    "isolatedModules": true,
+  }
+}
+
+// @Filename: index.js
+module.exports = {}
+var x = 1
