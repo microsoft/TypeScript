ccf38f65671f5e59bb3c4093693767c11e2df3ae
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-05-21 13:12:20 -0700

    Add this-parameter workaround to Array.filter
    Allows anys.filter(Boolean) to once again return any[], not unknown[].

---
+++ tests/cases/compiler/booleanFilterAnyArray.ts
@@ -0,0 +1,17 @@
+interface Bullean { }
+interface BulleanConstructor {
+    new(v1?: any): Bullean;
+    <T>(v2?: T): v2 is T;
+}
+
+interface Ari<T> {
+    filter<S extends T>(cb1: (value: T) => value is S): Ari<S>;
+    filter(cb2: (value: T) => unknown): Ari<T>;
+}
+declare var Bullean: BulleanConstructor;
+declare let anys: Ari<any>;
+var xs: Ari<any>;
+var xs = anys.filter(Bullean)
+
+declare let realanys: any[];
+var ys = realanys.filter(Boolean)
