9f6791a5abef5d9e8161de6c7bc545a65fa3fe7c
Andrew Branch <andrew@wheream.io>
2019-05-22 15:03:17 -0700

    Error when writing to readonly tuple in rest element range

--- tests/cases/conformance/types/tuple/readonlyArraysAndTuples.ts
+++ tests/cases/conformance/types/tuple/readonlyArraysAndTuples.ts
@@ -30,8 +30,10 @@ function f1(ma: string[], ra: readonly string[], mt: [string, string], rt: reado
 }
 
 declare var v: readonly[number, number, ...number[]];
-v[0] = 1; // Error
-v[1] = 1; // Error
-v[2] = 1; // Error
-v[0 + 1] = 1; // Error
-v[0 + 2] = 1; // Error
+v[0] = 1;        // Error
+v[1] = 1;        // Error
+v[2] = 1;        // Error
+delete v[2];     // Error
+v[0 + 1] = 1;    // Error
+v[0 + 2] = 1;    // Error
+delete v[0 + 1]; // Error
