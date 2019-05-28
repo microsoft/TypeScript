cf7b2d4ae91c4f27ba9ae7137ddf9a407815e590
Wesley Wigham <wewigham@microsoft.com>
2019-05-28 13:48:46 -0700

    Bring #31486 and #31586 into `release-3.5` (#31634)
    * Defer union or intersection property type normalization (#31486)
    
    * Defer union or intersection property type normalization
    
    * Accept moved span
    
    * Update user baselines (#31615)
    
    * Cache widened types (#31586)
    
    * Cache widened types
    
    * Fix lint
    
    * Accept LKG

---
+++ tests/cases/compiler/reactTagNameComponentWithPropsNoOOM.tsx
@@ -0,0 +1,13 @@
+// @jsx: react
+// @strict: true
+/// <reference path="/.lib/react16.d.ts" />
+
+import * as React from "react";
+declare const Tag: keyof React.ReactHTML;
+
+const classes = "";
+const rest: {} = {};
+const children: any[] = [];
+<Tag className={classes} {...rest}>
+{children}
+</Tag>
