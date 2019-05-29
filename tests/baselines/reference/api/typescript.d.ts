c5c869f673044d7e0daa27e194103409251d89dd
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2019-05-29 10:20:49 -0700

    Accepted baselines

--- tests/baselines/reference/api/typescript.d.ts
+++ tests/baselines/reference/api/typescript.d.ts
@@ -14,7 +14,7 @@ and limitations under the License.
 ***************************************************************************** */
 
 declare namespace ts {
-    const versionMajorMinor = "3.5";
+    const versionMajorMinor = "3.6";
     /** The version of the TypeScript compiler release */
     const version: string;
 }
