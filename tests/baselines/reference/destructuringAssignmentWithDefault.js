48d343b9852acd673c38ad0c43a93aada3935e79
Anders Hejlsberg <andersh@microsoft.com>
2019-06-01 10:37:02 -0700

    Accept new baselines

--- tests/baselines/reference/destructuringAssignmentWithDefault.js
+++ tests/baselines/reference/destructuringAssignmentWithDefault.js
@@ -8,11 +8,27 @@ let x = 0;
 function f1(options?: { color?: string, width?: number }) {
     let { color, width } = options || {};
     ({ color, width } = options || {});
+    let x1 = (options || {}).color;
+    let x2 = (options || {})["color"];
 }
 
 function f2(options?: [string?, number?]) {
     let [str, num] = options || [];
     [str, num] = options || [];
+    let x1 = (options || {})[0];
+}
+
+function f3(options?: { color: string, width: number }) {
+    let { color, width } = options || {};
+    ({ color, width } = options || {});
+    let x1 = (options || {}).color;
+    let x2 = (options || {})["color"];
+}
+
+function f4(options?: [string, number]) {
+    let [str, num] = options || [];
+    [str, num] = options || [];
+    let x1 = (options || {})[0];
 }
 
 
@@ -26,9 +42,25 @@ function f1(options) {
     var _a;
     var _b = options || {}, color = _b.color, width = _b.width;
     (_a = options || {}, color = _a.color, width = _a.width);
+    var x1 = (options || {}).color;
+    var x2 = (options || {})["color"];
 }
 function f2(options) {
     var _a;
     var _b = options || [], str = _b[0], num = _b[1];
     _a = options || [], str = _a[0], num = _a[1];
+    var x1 = (options || {})[0];
+}
+function f3(options) {
+    var _a;
+    var _b = options || {}, color = _b.color, width = _b.width;
+    (_a = options || {}, color = _a.color, width = _a.width);
+    var x1 = (options || {}).color;
+    var x2 = (options || {})["color"];
+}
+function f4(options) {
+    var _a;
+    var _b = options || [], str = _b[0], num = _b[1];
+    _a = options || [], str = _a[0], num = _a[1];
+    var x1 = (options || {})[0];
 }
