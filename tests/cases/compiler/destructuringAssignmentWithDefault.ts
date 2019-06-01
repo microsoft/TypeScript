86040e069932260ede34c400160a44034c262f6b
Anders Hejlsberg <andersh@microsoft.com>
2019-06-01 10:36:53 -0700

    Add more tests

--- tests/cases/compiler/destructuringAssignmentWithDefault.ts
+++ tests/cases/compiler/destructuringAssignmentWithDefault.ts
@@ -8,9 +8,25 @@ let x = 0;
 function f1(options?: { color?: string, width?: number }) {
     let { color, width } = options || {};
     ({ color, width } = options || {});
+    let x1 = (options || {}).color;
+    let x2 = (options || {})["color"];
 }
 
 function f2(options?: [string?, number?]) {
     let [str, num] = options || [];
     [str, num] = options || [];
+    let x1 = (options || {})[0];
+}
+
+function f3(options?: { color: string, width: number }) {
+    let { color, width } = options || {};
+    ({ color, width } = options || {});
+    let x1 = (options || {}).color;
+    let x2 = (options || {})["color"];
+}
+
+function f4(options?: [string, number]) {
+    let [str, num] = options || [];
+    [str, num] = options || [];
+    let x1 = (options || {})[0];
 }
