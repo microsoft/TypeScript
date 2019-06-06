64de998356c16cec75eaaab52e8778d868b159b3
Sheetal Nandi <shkamat@microsoft.com>
2019-06-06 09:00:57 -0700

    More tests

--- tests/cases/fourslash/findAllRefsEnumAsNamespace.ts
+++ tests/cases/fourslash/findAllRefsEnumAsNamespace.ts
@@ -1,6 +1,6 @@
 /// <reference path='fourslash.ts' />
 
-////enum [|{| "isWriteAccess": true, "isDefinition": true |}E|] { A }
+////[|enum [|{| "isWriteAccess": true, "isDefinition": true, "declarationRangeIndex": 0 |}E|] { A }|]
 ////let e: [|E|].A;
 
-verify.singleReferenceGroup("enum E");
+verify.singleReferenceGroup("enum E", "E");
