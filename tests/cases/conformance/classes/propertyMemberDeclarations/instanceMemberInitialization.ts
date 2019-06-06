19f01a2984b33bb7e77758840c4377a2dec279ae
Andrew Branch <andrew@wheream.io>
2019-06-06 13:55:48 -0700

    Add test for regression

--- tests/cases/conformance/classes/propertyMemberDeclarations/instanceMemberInitialization.ts
+++ tests/cases/conformance/classes/propertyMemberDeclarations/instanceMemberInitialization.ts
@@ -6,3 +6,9 @@ var c = new C();
 c.x = 3;
 var c2 = new C();
 var r = c.x === c2.x;
+
+// #31792
+export class MyMap<K, V> {
+	constructor(private readonly Map_: typeof Map = Map) {}
+	private readonly store = new this.Map_<K, V>();
+}
