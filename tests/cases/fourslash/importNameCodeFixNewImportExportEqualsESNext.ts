2ca96c1783e5bef10295b8d83e0ccf3e291c6c70
Andrew Branch <andrew@wheream.io>
2019-06-27 11:42:13 -0700

    Add failing tests

---
+++ tests/cases/fourslash/importNameCodeFixNewImportExportEqualsESNext.ts
@@ -0,0 +1,19 @@
+/// <reference path="fourslash.ts" />
+
+// @Module: esnext
+
+// @Filename: /foo.d.ts
+////declare module "foo" {
+////  const _default: number;
+////  export = _default;
+////}
+
+// @Filename: /index.ts
+////foo/**/
+
+goTo.marker('');
+verify.getAndApplyCodeFix(2304, 0);
+verify.currentFileContentIs(
+`import * as foo from './a';
+
+foo`);
