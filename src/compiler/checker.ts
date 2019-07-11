9e34a81e2b87b7d6ece85bf1f35b31c39d120595
Andrew Branch <andrew@wheream.io>
2019-07-11 10:16:56 -0700

    Fix regression of generic T assignability to Partial<T>

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -13443,7 +13443,8 @@ namespace ts {
                             if (includeOptional
                                     ? !(filteredByApplicability!.flags & TypeFlags.Never)
                                     : isRelatedTo(targetConstraint, sourceKeys)) {
-                                const indexingType = filteredByApplicability || getTypeParameterFromMappedType(target);
+                                const typeParameter = getTypeParameterFromMappedType(target);
+                                const indexingType = filteredByApplicability ? getIntersectionType([filteredByApplicability, typeParameter]) : typeParameter;
                                 const indexedAccessType = getIndexedAccessType(source, indexingType);
                                 const templateType = getTemplateTypeFromMappedType(target);
                                 if (result = isRelatedTo(indexedAccessType, templateType, reportErrors)) {
