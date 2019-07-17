387c917765793773a6f7184bab84e6f5956f44fc
Andrew Branch <andrewbranch@users.noreply.github.com>
2019-07-17 14:02:18 -0700

    Revert "Proposal: If there’s a package.json, only auto-import things in it, more or less (#31893)" (#32448)
    This reverts commit 60a1b1dc1a93ca792cf12bb0432cf7bc134c3ad1.

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -7485,7 +7485,7 @@ namespace ts {
     export function getDirectoryPath(path: Path): Path;
     /**
      * Returns the path except for its basename. Semantics align with NodeJS's `path.dirname`
-     * except that we support URLs as well.
+     * except that we support URL's as well.
      *
      * ```ts
      * getDirectoryPath("/path/to/file.ext") === "/path/to"
