68ae4382bd3bfa667e1d15e65043b34a19dd5c3e
Alexander T <alexander.tarasyuk@outlook.com>
2019-07-18 12:23:22 +0300

    jsdoc/check-alignment

--- src/lib/es2020.string.d.ts
+++ src/lib/es2020.string.d.ts
@@ -2,9 +2,9 @@
 
 interface String {
     /**
-      * Matches a string with a regular expression, and returns an iterable of matches
-      * containing the results of that search.
-      * @param regexp A variable name or string literal containing the regular expression pattern and flags.
-      */
+     * Matches a string with a regular expression, and returns an iterable of matches
+     * containing the results of that search.
+     * @param regexp A variable name or string literal containing the regular expression pattern and flags.
+     */
     matchAll(regexp: RegExp): IterableIterator<RegExpMatchArray>;
 }
