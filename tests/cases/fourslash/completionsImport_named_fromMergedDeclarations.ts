46f605ce2b740fe1a685135edc331d016e300669
Andrew Branch <andrew@wheream.io>
2019-07-30 17:23:55 -0700

    Handle merged symbols through cache

---
+++ tests/cases/fourslash/completionsImport_named_fromMergedDeclarations.ts
@@ -0,0 +1,39 @@
+/// <reference path="fourslash.ts" />
+
+// @module: esnext
+
+// @Filename: /a.ts
+////declare module "m" {
+////    export class M {}
+////}
+
+// @Filename: /b.ts
+////declare module "m" {
+////    export interface M {}
+////}
+
+// @Filename: /c.ts
+/////**/
+
+verify.completions({
+  marker: "",
+  includes: {
+      name: "M",
+      source: "m",
+      sourceDisplay: "m",
+      text: "class M\ninterface M",
+      kind: "class",
+      kindModifiers: "export,declare",
+      hasAction: true,
+      sortText: completion.SortText.AutoImportSuggestions
+  },
+  preferences: { includeCompletionsForModuleExports: true },
+});
+verify.applyCodeActionFromCompletion("", {
+  name: "M",
+  source: "m",
+  description: `Import 'M' from module "m"`,
+  newFileContent: `import { M } from "m";
+
+`,
+});
