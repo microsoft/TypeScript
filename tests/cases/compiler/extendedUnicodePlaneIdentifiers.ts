e9fd11e5f4a689079fa1ed5434d8b3ff7c0ccaab
Wesley Wigham <wewigham@microsoft.com>
2019-08-05 13:20:25 -0700

    Adjust isIdentifierText to skip multiple characters when a code point is multiple chars long

--- tests/cases/compiler/extendedUnicodePlaneIdentifiers.ts
+++ tests/cases/compiler/extendedUnicodePlaneIdentifiers.ts
@@ -2,3 +2,11 @@
 const 𝑚 = 4;
 const 𝑀 = 5;
 console.log(𝑀 + 𝑚); // 9
+
+// lower 8 bits look like 'a'
+const ၡ = 6;
+console.log(ၡ ** ၡ);
+
+// lower 8 bits aren't a valid unicode character
+const ဒ = 7;
+console.log(ဒ ** ဒ);
