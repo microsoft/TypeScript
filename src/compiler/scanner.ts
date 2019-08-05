bbd9f5c6e1c684d32b905f214897068c28006401
Wesley Wigham <wewigham@microsoft.com>
2019-08-05 13:04:54 -0700

    Add error message for keywords with escapes in them

--- src/compiler/scanner.ts
+++ src/compiler/scanner.ts
@@ -1376,6 +1376,9 @@ namespace ts {
                 if (ch >= CharacterCodes.a && ch <= CharacterCodes.z) {
                     const keyword = textToKeyword.get(tokenValue);
                     if (keyword !== undefined) {
+                        if (tokenFlags & TokenFlags.ExtendedUnicodeEscape || "" + tokenValue !== text.substring(tokenPos, pos)) {
+                            error(Diagnostics.Keyword_must_not_contain_escaped_characters, tokenPos, pos - tokenPos);
+                        }
                         return token = keyword;
                     }
                 }
