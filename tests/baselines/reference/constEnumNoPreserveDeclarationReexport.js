692ab2618b3a26660cb45f7d3c5a076b5c59949f
Wesley Wigham <wewigham@microsoft.com>
2019-08-05 14:47:42 -0700

    Limit change to declarations and preserveConstEnums mode

---
+++ tests/baselines/reference/constEnumNoPreserveDeclarationReexport.js
@@ -0,0 +1,28 @@
+//// [tests/cases/compiler/constEnumNoPreserveDeclarationReexport.ts] ////
+
+//// [ConstEnum.d.ts]
+export const enum MyConstEnum {
+    Foo,
+    Bar
+}
+//// [ImportExport.d.ts]
+import { MyConstEnum } from './ConstEnum';
+export default MyConstEnum;
+//// [ReExport.d.ts]
+export { MyConstEnum as default } from './ConstEnum';
+//// [usages.ts]
+import {MyConstEnum} from "./ConstEnum";
+import AlsoEnum from "./ImportExport";
+import StillEnum from "./ReExport";
+
+MyConstEnum.Foo;
+AlsoEnum.Foo;
+StillEnum.Foo;
+
+
+//// [usages.js]
+"use strict";
+exports.__esModule = true;
+0 /* Foo */;
+0 /* Foo */;
+0 /* Foo */;
