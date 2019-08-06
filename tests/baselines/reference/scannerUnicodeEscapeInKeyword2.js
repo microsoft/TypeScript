8b4f2322ee14313edd1bbac2fcae52bd44c1f8a9
Wesley Wigham <wewigham@microsoft.com>
2019-08-06 15:12:26 -0700

    Add tests for extended escapes

--- tests/baselines/reference/scannerUnicodeEscapeInKeyword2.js
+++ tests/baselines/reference/scannerUnicodeEscapeInKeyword2.js
@@ -1,4 +1,6 @@
-//// [scannerUnicodeEscapeInKeyword2.ts]
+//// [tests/cases/conformance/scanner/ecmascript5/scannerUnicodeEscapeInKeyword2.ts] ////
+
+//// [file1.ts]
 var \u0061wait = 12; // ok
 async function main() {
     \u0061wait 12; // not ok
@@ -13,13 +15,42 @@ type typ\u0065 = 12; // ok
 
 typ\u0065 notok = 0; // not ok
 
+export {};
+//// [file2.ts]
+\u{0076}ar x = "hello"; // not ok
 
-//// [scannerUnicodeEscapeInKeyword2.js]
+var \u{0061}wait = 12; // ok
+async function main() {
+    \u{0061}wait 12; // not ok
+}
+
+var \u{0079}ield = 12; // ok
+function *gen() {
+    \u{0079}ield 12; //not ok
+}
+
+type typ\u{0065} = 12; // ok
+
+typ\u{0065} notok = 0; // not ok
+
+export {};
+
+//// [file1.js]
 var \u0061wait = 12; // ok
 async function main() {
     await 12; // not ok
 }
 var \u0079ield = 12; // ok
+function* gen() {
+    yield 12; //not ok
+}
+//// [file2.js]
+var x = "hello"; // not ok
+var \u{0061}wait = 12; // ok
+async function main() {
+    await 12; // not ok
+}
+var \u{0079}ield = 12; // ok
 function* gen() {
     yield 12; //not ok
 }
--- tests/cases/conformance/scanner/ecmascript5/scannerUnicodeEscapeInKeyword2.ts
+++ tests/cases/conformance/scanner/ecmascript5/scannerUnicodeEscapeInKeyword2.ts
@@ -1,4 +1,5 @@
 // @target: esnext
+// @filename: file1.ts
 var \u0061wait = 12; // ok
 async function main() {
     \u0061wait 12; // not ok
@@ -12,3 +13,24 @@ function *gen() {
 type typ\u0065 = 12; // ok
 
 typ\u0065 notok = 0; // not ok
+
+export {};
+// @filename: file2.ts
+
+\u{0076}ar x = "hello"; // not ok
+
+var \u{0061}wait = 12; // ok
+async function main() {
+    \u{0061}wait 12; // not ok
+}
+
+var \u{0079}ield = 12; // ok
+function *gen() {
+    \u{0079}ield 12; //not ok
+}
+
+type typ\u{0065} = 12; // ok
+
+typ\u{0065} notok = 0; // not ok
+
+export {};
