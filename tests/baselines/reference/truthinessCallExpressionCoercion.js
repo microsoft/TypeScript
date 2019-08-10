130615ad320a617acbae40f6a5bf11751115616a
Justin Bay <justin.bay@outlook.com>
2019-08-10 13:50:14 -0400

    Flag non-nullable values with call expressions in `if` statements as errors

---
+++ tests/baselines/reference/truthinessCallExpressionCoercion.js
@@ -0,0 +1,94 @@
+//// [truthinessCallExpressionCoercion.ts]
+function func() { return Math.random() > 0.5; }
+
+if (func) { // error
+}
+
+function onlyErrorsWhenNonNullable(required: () => boolean, optional?: () => boolean) {
+    if (required) { // error
+    }
+
+    if (required()) { // ok
+    }
+
+    if (optional) { // ok
+    }
+}
+
+function checksPropertyAndElementAccess() {
+    const x = {
+        foo: {
+            bar() { }
+        }
+    }
+
+    if (x.foo.bar) { // error
+    }
+    
+    if (x.foo['bar']) { // error
+    }
+}
+
+function maybeBoolean(param: boolean | (() => boolean)) {
+    if (param) { // ok
+    }
+}
+
+class Foo {
+    maybeIsUser?: () => boolean;
+
+    isUser() {
+        return true;
+    }
+
+    test() {
+        if (this.isUser) { // error
+        }
+
+        if (this.maybeIsUser) { // ok
+        }
+    }
+}
+
+
+//// [truthinessCallExpressionCoercion.js]
+function func() { return Math.random() > 0.5; }
+if (func) { // error
+}
+function onlyErrorsWhenNonNullable(required, optional) {
+    if (required) { // error
+    }
+    if (required()) { // ok
+    }
+    if (optional) { // ok
+    }
+}
+function checksPropertyAndElementAccess() {
+    var x = {
+        foo: {
+            bar: function () { }
+        }
+    };
+    if (x.foo.bar) { // error
+    }
+    if (x.foo['bar']) { // error
+    }
+}
+function maybeBoolean(param) {
+    if (param) { // ok
+    }
+}
+var Foo = /** @class */ (function () {
+    function Foo() {
+    }
+    Foo.prototype.isUser = function () {
+        return true;
+    };
+    Foo.prototype.test = function () {
+        if (this.isUser) { // error
+        }
+        if (this.maybeIsUser) { // ok
+        }
+    };
+    return Foo;
+}());
