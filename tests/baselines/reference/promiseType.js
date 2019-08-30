9b0a55abb8b30dd127d83b0e7d433c09adcb178e
Jack Bates <jack@nottheoilrig.com>
2019-08-30 09:38:13 -0700

    Add tests

--- tests/baselines/reference/promiseType.js
+++ tests/baselines/reference/promiseType.js
@@ -218,6 +218,8 @@ const pc7 = p.then(() => Promise.reject("1"), () => {throw 1});
 const pc8 = p.then(() => Promise.reject("1"), () => Promise.resolve(1));
 const pc9 = p.then(() => Promise.reject("1"), () => Promise.reject(1));
 
+Promise.resolve(undefined as Promise<string> | number);
+
 
 //// [promiseType.js]
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
@@ -440,3 +442,4 @@ const pc6 = p.then(() => Promise.reject("1"), () => { });
 const pc7 = p.then(() => Promise.reject("1"), () => { throw 1; });
 const pc8 = p.then(() => Promise.reject("1"), () => Promise.resolve(1));
 const pc9 = p.then(() => Promise.reject("1"), () => Promise.reject(1));
+Promise.resolve(undefined);
