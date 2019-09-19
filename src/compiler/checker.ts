58e17462045651933fce591f1579f519b2750f4c
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-09-19 07:40:34 -0700

    Ignore this-property assignments

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -29519,8 +29519,10 @@ namespace ts {
                     if (basePropertyFlags && derivedPropertyFlags) {
                         // property/accessor is overridden with property/accessor
                         if (baseDeclarationFlags & ModifierFlags.Abstract
-                            || getObjectFlags(getTargetType(baseType)) & ObjectFlags.Interface) {
+                            || getObjectFlags(getTargetType(baseType)) & ObjectFlags.Interface
+                            || derived.valueDeclaration && isBinaryExpression(derived.valueDeclaration)) {
                             // when the base property is abstract or from an interface, base/derived flags don't need to match
+                            // same when the derived property is from an assignment
                             continue;
                         }
                         if (basePropertyFlags !== SymbolFlags.Property && derivedPropertyFlags === SymbolFlags.Property) {
