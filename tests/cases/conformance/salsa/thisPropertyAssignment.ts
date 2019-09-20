bd6602c002183c921a24f58e9946c2bcc427af3f
Andrew Branch <andrew@wheream.io>
2019-09-20 16:11:25 -0700

    Add test for this property assignment

---
+++ tests/cases/conformance/salsa/thisPropertyAssignment.ts
@@ -0,0 +1,17 @@
+// @noEmit: true
+// @checkJs: true
+// @allowJs: true
+// @strict: true
+// @Filename: a.js
+
+// This test is asserting that a single property/element access
+// on `this` is a special assignment declaration, but chaining
+// off that does not create additional declarations. I’m not sure
+// if it needs to be that way in JavaScript; the test simply
+// ensures no accidental changes were introduced while allowing
+// element access assignments to create declarations.
+
+this.x = {};
+this.x.y = {};
+this["x"] = {};
+this["x"]["y"] = {};
