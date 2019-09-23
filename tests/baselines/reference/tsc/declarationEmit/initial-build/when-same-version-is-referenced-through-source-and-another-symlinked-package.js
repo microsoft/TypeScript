c67c68e1490aa7248b44fea0aaa189ce13ca430b
Sheetal Nandi <shkamat@microsoft.com>
2019-09-23 13:15:40 -0700

    Sort the paths for module specifier by closeness to importing file path Fixes #32970

--- tests/baselines/reference/tsc/declarationEmit/initial-build/when-same-version-is-referenced-through-source-and-another-symlinked-package.js
+++ tests/baselines/reference/tsc/declarationEmit/initial-build/when-same-version-is-referenced-through-source-and-another-symlinked-package.js
@@ -1,13 +1,20 @@
 //// [/lib/initial-buildOutput.txt]
 /lib/tsc -p plugin-one --listFiles
-plugin-one/action.ts(4,14): error TS2742: The inferred type of 'actions' cannot be named without a reference to 'plugin-two/node_modules/typescript-fsa'. This is likely not portable. A type annotation is necessary.
 /lib/lib.d.ts
 /plugin-one/node_modules/typescript-fsa/index.d.ts
 /plugin-one/action.ts
 /plugin-two/node_modules/typescript-fsa/index.d.ts -> /plugin-one/node_modules/typescript-fsa/index.d.ts
 /plugin-two/index.d.ts
 /plugin-one/index.ts
-exitCode:: 1
+exitCode:: 0
+
+
+//// [/plugin-one/action.d.ts]
+export declare const actions: {
+    featureOne: import("typescript-fsa").ActionCreator<{
+        route: string;
+    }>;
+};
 
 
 //// [/plugin-one/action.js]
