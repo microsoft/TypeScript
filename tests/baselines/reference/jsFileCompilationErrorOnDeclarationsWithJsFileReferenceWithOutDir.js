08b105c2f898a49204687faa1d6864550eb7fdd8
Wesley Wigham <wewigham@microsoft.com>
2019-09-25 13:28:51 -0700

    Update comment

--- tests/baselines/reference/jsFileCompilationErrorOnDeclarationsWithJsFileReferenceWithOutDir.js
+++ tests/baselines/reference/jsFileCompilationErrorOnDeclarationsWithJsFileReferenceWithOutDir.js
@@ -6,7 +6,6 @@ class c {
 
 //// [b.ts]
 /// <reference path="c.js"/>
-// b.d.ts should have c.js as the reference path since we dont emit declarations for js files
 function foo() {
 }
 
@@ -25,7 +24,6 @@ function bar() {
 }
 //// [b.js]
 /// <reference path="c.js"/>
-// b.d.ts should have c.js as the reference path since we dont emit declarations for js files
 function foo() {
 }
 
