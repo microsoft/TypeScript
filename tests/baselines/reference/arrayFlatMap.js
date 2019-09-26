307822396425a5d1996691d773a4145ecb43781e
Jack Bates <jack@nottheoilrig.com>
2019-09-26 16:04:39 -0700

    Add tests

--- tests/baselines/reference/arrayFlatMap.js
+++ tests/baselines/reference/arrayFlatMap.js
@@ -4,9 +4,17 @@ const readonlyArray: ReadonlyArray<number> = [];
 array.flatMap((): ReadonlyArray<number> => []); // ok
 readonlyArray.flatMap((): ReadonlyArray<number> => []); // ok
 
+// #19535
+
+const [x] = [""].flatMap(undefined as () => string[] | string[][]);
+x == "";
+
 
 //// [arrayFlatMap.js]
 var array = [];
 var readonlyArray = [];
 array.flatMap(function () { return []; }); // ok
 readonlyArray.flatMap(function () { return []; }); // ok
+// #19535
+var x = [""].flatMap(undefined)[0];
+x == "";
--- tests/cases/compiler/arrayFlatMap.ts
+++ tests/cases/compiler/arrayFlatMap.ts
@@ -4,3 +4,8 @@ const array: number[] = [];
 const readonlyArray: ReadonlyArray<number> = [];
 array.flatMap((): ReadonlyArray<number> => []); // ok
 readonlyArray.flatMap((): ReadonlyArray<number> => []); // ok
+
+// #19535
+
+const [x] = [""].flatMap(undefined as () => string[] | string[][]);
+x == "";
