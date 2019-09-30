8b63c160b6218526e3941573ad9c01f3427596ed
Matts966 <Matts966@users.noreply.github.com>
2019-09-30 18:23:58 +0900

    Emit comment and newline only once a initizlier

---
+++ tests/baselines/reference/variableDeclarationWithTrailingComment.js
@@ -0,0 +1,9 @@
+//// [variableDeclarationWithTrailingComment.ts]
+for (let i = 0; i <= 10; ++i) {
+  let _a, _b; /// 123
+}
+
+//// [variableDeclarationWithTrailingComment.js]
+for (var i = 0; i <= 10; ++i) {
+    var _a = void 0, _b = void 0; /// 123
+}
