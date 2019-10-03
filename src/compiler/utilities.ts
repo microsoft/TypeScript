b7575eba8f555f7482e0d6ce787db05dd8ac76e6
Andrew Branch <andrew@wheream.io>
2019-10-03 08:55:14 -0500

    Detect unbindable exports before setting CommonJS module indicator

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -2174,9 +2174,11 @@ namespace ts {
                 nextToLast = nextToLast.expression as Exclude<BindableStaticNameExpression, Identifier>;
             }
             const id = nextToLast.expression;
-            if (id.escapedText === "exports" ||
-                id.escapedText === "module" && getElementOrPropertyAccessName(nextToLast) === "exports") {
-                // exports.name = expr OR module.exports.name = expr
+            if ((id.escapedText === "exports" ||
+                id.escapedText === "module" && getElementOrPropertyAccessName(nextToLast) === "exports") &&
+                // ExportsProperty does not support late binding; it must have a static name
+                isBindableStaticAccessExpression(lhs)) {
+                // exports.name = expr OR module.exports.name = expr OR exports["name"] = expr ...
                 return AssignmentDeclarationKind.ExportsProperty;
             }
             // F.G...x = expr
