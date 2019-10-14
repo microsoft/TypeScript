47bc70aa99933c997bfe7c5b680e80dec0622b76
Macide Celik <macide.35cc@gmail.com>
2019-10-14 21:38:55 +0300

    Delete es2019.d.ts

--- src/lib/es2019.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference lib="es2018" />
-/// <reference lib="es2019.array" />
-/// <reference lib="es2019.object" />
-/// <reference lib="es2019.string" />
-/// <reference lib="es2019.symbol" />
