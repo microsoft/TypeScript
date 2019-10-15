4600d1bd961a1951310c5ed581eb78bc4cc303d1
Sheetal Nandi <shkamat@microsoft.com>
2019-10-15 11:42:04 -0700

    Add test for rename on file with case change that fails Test for #25460

--- src/testRunner/unittests/tsserver/forceConsistentCasingInFileNames.ts
+++ src/testRunner/unittests/tsserver/forceConsistentCasingInFileNames.ts
@@ -41,5 +41,104 @@ namespace ts.projectSystem {
             const diagnostics = configuredProjectAt(projectService, 0).getLanguageService().getCompilerOptionsDiagnostics();
             assert.deepEqual(diagnostics, []);
         });
+
+        it("works when renaming file with different casing", () => {
+            const loggerFile: File = {
+                path: `${projectRoot}/Logger.ts`,
+                content: `export class logger { }`
+            };
+            const anotherFile: File = {
+                path: `${projectRoot}/another.ts`,
+                content: `import { logger } from "./Logger"; new logger();`
+            };
+            const tsconfig: File = {
+                path: `${projectRoot}/tsconfig.json`,
+                content: JSON.stringify({
+                    compilerOptions: { forceConsistentCasingInFileNames: true }
+                })
+            };
+
+            const host = createServerHost([loggerFile, anotherFile, tsconfig, libFile, tsconfig]);
+            const session = createSession(host, { canUseEvents: true, logger: projectSystem.createLoggerWritingToConsole() });
+            session.executeCommandSeq<protocol.UpdateOpenRequest>({
+                command: protocol.CommandTypes.UpdateOpen,
+                arguments: {
+                    openFiles: [{
+                        file: loggerFile.path,
+                        fileContent: loggerFile.content,
+                        projectRootPath: projectRoot
+                    }]
+                }
+            });
+            const service = session.getProjectService();
+            checkNumberOfProjects(service, { configuredProjects: 1 });
+            const project = service.configuredProjects.get(tsconfig.path)!;
+            checkProjectActualFiles(project, [loggerFile.path, anotherFile.path, libFile.path, tsconfig.path]);
+            verifyGetErrRequest({
+                host,
+                session,
+                expected: [
+                    { file: loggerFile.path, syntax: [], semantic: [], suggestion: [] }
+                ]
+            });
+
+            const newLoggerPath = loggerFile.path.toLowerCase();
+            host.renameFile(loggerFile.path, newLoggerPath);
+            session.executeCommandSeq<protocol.UpdateOpenRequest>({
+                command: protocol.CommandTypes.UpdateOpen,
+                arguments: {
+                    closedFiles: [
+                        loggerFile.path
+                    ]
+                }
+            });
+            session.executeCommandSeq<protocol.UpdateOpenRequest>({
+                command: protocol.CommandTypes.UpdateOpen,
+                arguments: {
+                    openFiles: [{
+                        file: newLoggerPath,
+                        fileContent: loggerFile.content,
+                        projectRootPath: projectRoot
+                    }]
+                }
+            });
+
+            // Apply edits for rename
+            session.executeCommandSeq<protocol.UpdateOpenRequest>({
+                command: protocol.CommandTypes.UpdateOpen,
+                arguments: {
+                    openFiles: [{
+                        file: anotherFile.path,
+                        fileContent: anotherFile.content,
+                        projectRootPath: projectRoot
+                    }]
+                }
+            });
+            session.executeCommandSeq<protocol.UpdateOpenRequest>({
+                command: protocol.CommandTypes.UpdateOpen,
+                arguments: {
+                    changedFiles: [{
+                        fileName: anotherFile.path,
+                        textChanges: [{
+                            newText: "./logger",
+                            ...projectSystem.protocolTextSpanFromSubstring(
+                                anotherFile.content,
+                                "./Logger"
+                            )
+                        }]
+                    }]
+                }
+            });
+
+            // Check errors in both files
+            verifyGetErrRequest({
+                host,
+                session,
+                expected: [
+                    { file: newLoggerPath, syntax: [], semantic: [], suggestion: [] },
+                    { file: anotherFile.path, syntax: [], semantic: [], suggestion: [] }
+                ]
+            });
+        });
     });
 }
