25db0b0bf06ead2501778558611f7b273890d2b1
Sheetal Nandi <shkamat@microsoft.com>
2019-10-16 11:20:14 -0700

    Merge baselining and tsc logic into one

--- src/testRunner/unittests/tsc/helpers.ts
+++ src/testRunner/unittests/tsc/helpers.ts
@@ -3,167 +3,6 @@ namespace ts {
         writtenFiles: Map<true>;
         baseLine(): void;
     };
-    function executeCommandLine(sys: TscCompileSystem, commandLineArgs: readonly string[]) {
-        if (isBuild(commandLineArgs)) {
-            return performBuild(sys, commandLineArgs.slice(1));
-        }
-
-        const reportDiagnostic = createDiagnosticReporter(sys);
-        const commandLine = parseCommandLine(commandLineArgs, path => sys.readFile(path));
-        if (commandLine.options.build) {
-            reportDiagnostic(createCompilerDiagnostic(Diagnostics.Option_build_must_be_the_first_command_line_argument));
-            return sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
-        }
-
-        if (commandLine.errors.length > 0) {
-            commandLine.errors.forEach(reportDiagnostic);
-            return sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
-        }
-
-        let configFileName: string | undefined;
-        if (commandLine.options.project) {
-            if (commandLine.fileNames.length !== 0) {
-                reportDiagnostic(createCompilerDiagnostic(Diagnostics.Option_project_cannot_be_mixed_with_source_files_on_a_command_line));
-                return sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
-            }
-
-            const fileOrDirectory = normalizePath(commandLine.options.project);
-            if (!fileOrDirectory /* current directory "." */ || sys.directoryExists(fileOrDirectory)) {
-                configFileName = combinePaths(fileOrDirectory, "tsconfig.json");
-                if (!sys.fileExists(configFileName)) {
-                    reportDiagnostic(createCompilerDiagnostic(Diagnostics.Cannot_find_a_tsconfig_json_file_at_the_specified_directory_Colon_0, commandLine.options.project));
-                    return sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
-                }
-            }
-            else {
-                configFileName = fileOrDirectory;
-                if (!sys.fileExists(configFileName)) {
-                    reportDiagnostic(createCompilerDiagnostic(Diagnostics.The_specified_path_does_not_exist_Colon_0, commandLine.options.project));
-                    return sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
-                }
-            }
-        }
-        else if (commandLine.fileNames.length === 0) {
-            const searchPath = normalizePath(sys.getCurrentDirectory());
-            configFileName = findConfigFile(searchPath, sys.fileExists);
-        }
-
-        Debug.assert(commandLine.fileNames.length !== 0 || !!configFileName);
-
-        const currentDirectory = sys.getCurrentDirectory();
-        const getCanonicalFileName = createGetCanonicalFileName(sys.useCaseSensitiveFileNames);
-        const commandLineOptions = convertToOptionsWithAbsolutePaths(
-            commandLine.options,
-            fileName => toPath(fileName, currentDirectory, getCanonicalFileName)
-        );
-
-        if (configFileName) {
-            const configParseResult = Debug.assertDefined(parseConfigFileWithSystem(configFileName, commandLineOptions, sys, reportDiagnostic));
-            if (isIncrementalCompilation(configParseResult.options)) {
-                performIncrementalCompilation(sys, configParseResult);
-            }
-            else {
-                performCompilation(sys, configParseResult);
-            }
-        }
-        else {
-            if (isIncrementalCompilation(commandLine.options)) {
-                performIncrementalCompilation(sys, {
-                    ...commandLine,
-                    options: commandLineOptions
-                });
-            }
-            else {
-                performCompilation(sys, {
-                    ...commandLine,
-                    options: commandLineOptions
-                });
-            }
-        }
-    }
-
-    function createReportErrorSummary(sys: TscCompileSystem, options: CompilerOptions): ReportEmitErrorSummary | undefined {
-        return options.pretty ?
-            errorCount => sys.write(getErrorSummaryText(errorCount, sys.newLine)) :
-            undefined;
-    }
-
-    function performCompilation(sys: TscCompileSystem, config: ParsedCommandLine) {
-        const { fileNames, options, projectReferences } = config;
-        const reportDiagnostic = createDiagnosticReporter(sys, options.pretty);
-        const host = createCompilerHostWorker(options, /*setParentPos*/ undefined, sys);
-        fakes.patchHostForBuildInfoReadWrite(host);
-        const currentDirectory = host.getCurrentDirectory();
-        const getCanonicalFileName = createGetCanonicalFileName(host.useCaseSensitiveFileNames());
-        changeCompilerHostLikeToUseCache(host, fileName => toPath(fileName, currentDirectory, getCanonicalFileName));
-        const program = createProgram({
-            rootNames: fileNames,
-            options,
-            projectReferences,
-            host,
-            configFileParsingDiagnostics: getConfigFileParsingDiagnostics(config)
-        });
-        const exitStatus = emitFilesAndReportErrorsAndGetExitStatus(
-            program,
-            reportDiagnostic,
-            s => sys.write(s + sys.newLine),
-            createReportErrorSummary(sys, options)
-        );
-        baselineBuildInfo([config], sys.vfs, sys.writtenFiles);
-        return sys.exit(exitStatus);
-    }
-
-    function performIncrementalCompilation(sys: TscCompileSystem, config: ParsedCommandLine) {
-        const reportDiagnostic = createDiagnosticReporter(sys, config.options.pretty);
-        const { options, fileNames, projectReferences } = config;
-        const host = createIncrementalCompilerHost(options, sys);
-        fakes.patchHostForBuildInfoReadWrite(host);
-        const exitCode = ts.performIncrementalCompilation({
-            host,
-            system: sys,
-            rootNames: fileNames,
-            options,
-            configFileParsingDiagnostics: getConfigFileParsingDiagnostics(config),
-            projectReferences,
-            reportDiagnostic,
-            reportErrorSummary: createReportErrorSummary(sys, options),
-        });
-        baselineBuildInfo([config], sys.vfs, sys.writtenFiles);
-        return sys.exit(exitCode);
-    }
-
-    function performBuild(sys: TscCompileSystem, args: string[]) {
-        const { buildOptions, projects, errors } = parseBuildCommand(args);
-        const reportDiagnostic = createDiagnosticReporter(sys, buildOptions.pretty);
-
-        if (errors.length > 0) {
-            errors.forEach(reportDiagnostic);
-            return sys.exit(ExitStatus.DiagnosticsPresent_OutputsSkipped);
-        }
-
-        Debug.assert(projects.length !== 0);
-
-        const buildHost = createSolutionBuilderHost(
-            sys,
-            /*createProgram*/ undefined,
-            reportDiagnostic,
-            createBuilderStatusReporter(sys, buildOptions.pretty),
-            createReportErrorSummary(sys, buildOptions)
-        );
-        fakes.patchSolutionBuilderHost(buildHost, sys);
-        const builder = createSolutionBuilder(buildHost, projects, buildOptions);
-        const exitCode = buildOptions.clean ? builder.clean() : builder.build();
-        baselineBuildInfo(builder.getAllParsedConfigs(), sys.vfs, sys.writtenFiles);
-        return sys.exit(exitCode);
-    }
-
-    function isBuild(commandLineArgs: readonly string[]) {
-        if (commandLineArgs.length > 0 && commandLineArgs[0].charCodeAt(0) === CharacterCodes.minus) {
-            const firstOption = commandLineArgs[0].slice(commandLineArgs[0].charCodeAt(1) === CharacterCodes.minus ? 2 : 1).toLowerCase();
-            return firstOption === "build" || firstOption === "b";
-        }
-        return false;
-    }
 
     export enum BuildKind {
         Initial = "initial-build",
@@ -221,7 +60,16 @@ namespace ts {
 
         sys.write(`${sys.getExecutingFilePath()} ${commandLineArgs.join(" ")}\n`);
         sys.exit = exitCode => sys.exitCode = exitCode;
-        executeCommandLine(sys, commandLineArgs);
+        ts.executeCommandLine(
+            sys,
+            {
+                onCompilerHostCreate: host => fakes.patchHostForBuildInfoReadWrite(host),
+                onCompilationComplete: config => baselineBuildInfo([config], sys.vfs, sys.writtenFiles),
+                onSolutionBuilderHostCreate: host => fakes.patchSolutionBuilderHost(host, sys),
+                onSolutionBuildComplete: configs => baselineBuildInfo(configs, sys.vfs, sys.writtenFiles),
+            },
+            commandLineArgs,
+        );
         sys.write(`exitCode:: ExitStatus.${ExitStatus[sys.exitCode as ExitStatus]}\n`);
         if (baselineReadFileCalls) {
             sys.write(`readFiles:: ${JSON.stringify(actualReadFileMap, /*replacer*/ undefined, " ")} `);
