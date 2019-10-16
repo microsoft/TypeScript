f136e717f8b03baeafc72f8eddef25ef3034db3c
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2019-10-16 11:17:23 -0700

    Added test case.

---
+++ tests/cases/compiler/errorForObjectLiteralAgainstTypeWithIndexer01.ts
@@ -0,0 +1,19 @@
+// @strict: true
+
+type PlainObjectItem = string | number | boolean | null
+
+interface PlainObject {
+    [key: string]: PlainObjectItem | PlainObjectItem[] | PlainObject
+}
+
+const hoge: PlainObject = {
+    key1: 'hoge',
+    key2: {
+        key21: 'aaa',
+        key22: false,
+        key23: {
+            key231: [1,2,3,4,5],
+            date: new Date()
+        }
+    }
+}
