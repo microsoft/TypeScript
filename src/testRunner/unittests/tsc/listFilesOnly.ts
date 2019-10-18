7275e9ca93e634cb0e2d8e7f57bd3c00c72d9d93
Andrew Casey <andrew.casey@microsoft.com>
2019-10-17 17:22:47 -0700

    Delete redundant test

--- src/testRunner/unittests/tsc/listFilesOnly.ts
+++ src/testRunner/unittests/tsc/listFilesOnly.ts
@@ -2,17 +2,7 @@ namespace ts {
     describe("unittests:: tsc:: listFilesOnly::", () => {
         verifyTsc({
             scenario: "listFilesOnly",
-            subScenario: "before watch",
-            fs: () => loadProjectFromFiles({
-                "/src/test.ts": utils.dedent`
-                        export const x = 1;`,
-            }),
-            commandLineArgs: ["/src/test.ts", "--listFilesOnly", "--watch"]
-        });
-
-        verifyTsc({
-            scenario: "listFilesOnly",
-            subScenario: "after watch",
+            subScenario: "combined with watch",
             fs: () => loadProjectFromFiles({
                 "/src/test.ts": utils.dedent`
                         export const x = 1;`,
--- tests/baselines/reference/tsc/listFilesOnly/initial-build/before-watch.js
+++
@@ -1,6 +0,0 @@
-//// [/lib/initial-buildOutput.txt]
-/lib/tsc /src/test.ts --listFilesOnly --watch
-error TS6370: Options 'watch' and 'listFilesOnly' cannot be combined.
-exitCode:: ExitStatus.DiagnosticsPresent_OutputsSkipped
-
-
