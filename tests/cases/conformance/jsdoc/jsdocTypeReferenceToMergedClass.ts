056a656937944f9f7366f630d278b2ececa39cd4
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-10-24 08:37:10 -0700

    Restore delayed merge check to getTypeFromJSDocValueReference
    This is needed when a function merges with a prototype assignment. The
    resulting *merged* symbol is a constructor function marked with
    SymbolFlags.Class. However, the merge doesn't happen until
    getTypeOfFuncClassEnumModule is called, which, in the
    getTypeReferenceType code path, doesn't happen until
    getTypeFromJSDocValueReference. That means the check for
    SymbolFlags.Class is missed.
    
    Previously, getTypeFromJSDocValueReference had a weird check
    `symbol !== getTypeOfSymbol(symbol).symbol`, which, if true, ran
    getTypeReferenceType again on `getTypeOfSymbol(symbol).symbol`. For
    JS "aliases", this had the effect of dereferencing the alias, and for
    function-prototype merges, this had the effect of ... just trying again
    after the merge had happened.
    
    This is a confusing way to run things. getTypeReferenceType should
    instead detect a function-prototype merge, cause it to happen, and
    *then* run the rest of its code instead of relying on try-again logic at
    the very end. However, for the RC, I want to fix this code by restoring
    the old check, with an additional check to make sure that #33106 doesn't
    break again:
    
    ```ts
    const valueType = getTypeOfSymbol(symbol)
    symbol !== valueType.symbol && getMergedSymbol(symbol) === valueType.symbol
    ```
    
    I'll work on the real fix afterwards and put it into 3.8.

---
+++ tests/cases/conformance/jsdoc/jsdocTypeReferenceToMergedClass.ts
@@ -0,0 +1,16 @@
+// @noEmit: true
+// @allowJs: true
+// @checkJs: true
+
+// @Filename: test.js
+/** @param {Workspace.Project} p */
+function demo(p) {
+    p.isServiceProject()
+}
+// @Filename: mod1.js
+// Note: mod1.js needs to appear second to trigger the bug
+var Workspace = {}
+Workspace.Project = function wp() { }
+Workspace.Project.prototype = {
+  isServiceProject() {}
+}
