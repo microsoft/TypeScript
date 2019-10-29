ea803620ecf3c76f012dda07ef7c13b6c8a6cf48
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-10-29 13:56:25 -0700

    Update comments in test

--- tests/baselines/reference/commonTypeIntersection.js
+++ tests/baselines/reference/commonTypeIntersection.js
@@ -1,10 +1,10 @@
 //// [commonTypeIntersection.ts]
 declare let x1: { __typename?: 'TypeTwo' } & { a: boolean };
-let y1: { __typename?: 'TypeOne' } & { a: boolean} = x1;  // No error!
+let y1: { __typename?: 'TypeOne' } & { a: boolean} = x1;  // should error here
 declare let x2: { __typename?: 'TypeTwo' } & string;
-let y2: { __typename?: 'TypeOne' } & string = x2;  // No error!
+let y2: { __typename?: 'TypeOne' } & string = x2;  // should error here
 
 
 //// [commonTypeIntersection.js]
-var y1 = x1; // No error!
-var y2 = x2; // No error!
+var y1 = x1; // should error here
+var y2 = x2; // should error here
