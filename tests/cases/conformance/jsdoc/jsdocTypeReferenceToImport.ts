b713df83bc96d061bcb9a1d9a020384a09dc1ef7
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-10-29 10:43:45 -0700

    resolve require with entity name postfix
    For example, `require("x").c`. This is the value equivalent of
    `import("x").a.b.c`, but the syntax tree is not as nicely designed for
    this purpose.
    
    Fixes #34802

---
+++ tests/cases/conformance/jsdoc/jsdocTypeReferenceToImport.ts
@@ -0,0 +1,16 @@
+// @Filename: jsdocTypeReferenceToImport.js
+// @noEmit: true
+// @allowJs: true
+// @checkJs: true
+
+const C = require('./ex').C;
+/** @type {C} c */
+var c = new C()
+c.start
+c.end
+
+// @Filename: ex.d.ts
+export class C {
+    start: number
+    end: number
+}
