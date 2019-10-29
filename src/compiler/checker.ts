7d77ecbcff52db885fe7b936891ac793c4b7714a
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-10-29 14:56:33 -0700

    JSDoc type reference understands require with entity name (#34804)
    * resolve require with entity name postfix
    
    For example, `require("x").c`. This is the value equivalent of
    `import("x").a.b.c`, but the syntax tree is not as nicely designed for
    this purpose.
    
    Fixes #34802
    
    * Add bug number to test
    
    * Add optional chain test

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -10759,14 +10759,17 @@ namespace ts {
             let typeType = valueType;
             if (symbol.valueDeclaration) {
                 const decl = getRootDeclaration(symbol.valueDeclaration);
-                const isRequireAlias = isVariableDeclaration(decl)
-                    && decl.initializer
-                    && isCallExpression(decl.initializer)
-                    && isRequireCall(decl.initializer, /*requireStringLiteralLikeArgument*/ true)
-                    && valueType.symbol;
-                const isImportType = node.kind === SyntaxKind.ImportType;
+                let isRequireAlias = false;
+                if (isVariableDeclaration(decl) && decl.initializer) {
+                    let expr = decl.initializer;
+                    // skip past entity names, eg `require("x").a.b.c`
+                    while (isPropertyAccessExpression(expr)) {
+                        expr = expr.expression;
+                    }
+                    isRequireAlias = isCallExpression(expr) && isRequireCall(expr, /*requireStringLiteralLikeArgument*/ true) && !!valueType.symbol;
+                }
                 const isDelayedMergeClass = symbol !== valueType.symbol && getMergedSymbol(symbol) === valueType.symbol;
-                if (isRequireAlias || isImportType || isDelayedMergeClass) {
+                if (isRequireAlias || node.kind === SyntaxKind.ImportType || isDelayedMergeClass) {
                     typeType = getTypeReferenceType(node, valueType.symbol);
                 }
             }
