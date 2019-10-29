b65bbc57d127abbbefd99de57df36114e702f779
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-10-29 12:53:50 -0700

    Add optional chain test

--- tests/cases/conformance/jsdoc/jsdocTypeReferenceToImport.ts
+++ tests/cases/conformance/jsdoc/jsdocTypeReferenceToImport.ts
@@ -5,10 +5,16 @@
 // @checkJs: true
 
 const C = require('./ex').C;
+const D = require('./ex')?.C;
 /** @type {C} c */
 var c = new C()
 c.start
 c.end
+
+/** @type {D} c */
+var d = new D()
+d.start
+d.end
 
 // @Filename: ex.d.ts
 export class C {
