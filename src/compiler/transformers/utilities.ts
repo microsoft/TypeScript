8ae5a8cfcedaa840f8764ad1d3e58522115837a5
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-11-12 10:15:30 -0800

    useDefineForClassFields skips emit of ambient properties
    Previously:
    
    ```ts
    class C {
      declare p
    }
    ```
    
    would incorrectly emit
    
    ```js
    class C {
        constructor() {
            Object.defineProperty(this, "p", {
                enumerable: true,
                configurable: true,
                writable: true,
                value: void 0
            });
        }
    }
    ```
    
    when useDefineForClassFields was turned on (for targets <ESNext).

--- src/compiler/transformers/utilities.ts
+++ src/compiler/transformers/utilities.ts
@@ -314,7 +314,7 @@ namespace ts {
      */
     function isInitializedOrStaticProperty(member: ClassElement, requireInitializer: boolean, isStatic: boolean) {
         return isPropertyDeclaration(member)
-            && (!!member.initializer || !requireInitializer)
+            && (!!member.initializer || !requireInitializer && !(getOriginalNode(member).flags & NodeFlags.Ambient))
             && hasStaticModifier(member) === isStatic;
     }
 
