d44234e91295a5e635154aca077336a38d8d405b
Jack Bates <jack@nottheoilrig.com>
2019-11-19 15:20:54 -0700

    Fix test broken since #14053 and #16368

--- tests/baselines/reference/promiseTypeInference.js
+++ tests/baselines/reference/promiseTypeInference.js
@@ -1,11 +1,11 @@
 //// [promiseTypeInference.ts]
-declare class Promise<T> {
-    then<U>(success?: (value: T) => Promise<U>): Promise<U>;
+declare class MyPromise<T> {
+    then<U>(success?: (value: T) => MyPromise<U>): MyPromise<U>;
 }
 interface IPromise<T> {
     then<U>(success?: (value: T) => IPromise<U>): IPromise<U>;
 }
-declare function load(name: string): Promise<string>;
+declare function load(name: string): MyPromise<string>;
 declare function convert(s: string): IPromise<number>;
 
 var $$x = load("something").then(s => convert(s));
