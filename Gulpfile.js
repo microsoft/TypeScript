77b14f6085c262ff9a019c70135db793aec6aece
Orta Therox <orta.therox@gmail.com>
2019-11-19 12:53:43 -0500

    Don't have a different CI linter from in-app linting

--- Gulpfile.js
+++ Gulpfile.js
@@ -346,8 +346,6 @@ const lintFoldEnd = async () => { if (fold.isTravis()) console.log(fold.end("lin
 
 /** @type { (folder: string) => { (): Promise<any>; displayName?: string } } */
 const eslint = (folder) => async () => {
-    const ESLINTRC_CI = ".eslintrc.ci.json";
-    const isCIEnv = cmdLineOptions.ci || process.env.CI === "true";
 
     const args = [
         "node_modules/eslint/bin/eslint",
@@ -358,13 +356,6 @@ const eslint = (folder) => async () => {
         "--ext", ".ts",
     ];
 
-    if (
-        isCIEnv &&
-        fs.existsSync(path.resolve(folder, ESLINTRC_CI))
-    ) {
-        args.push("--config", path.resolve(folder, ESLINTRC_CI));
-    }
-
     if (cmdLineOptions.fix) {
         args.push("--fix");
     }
@@ -373,7 +364,7 @@ const eslint = (folder) => async () => {
 
     log(`Linting: ${args.join(" ")}`);
     return exec(process.execPath, args);
-}
+};
 
 const lintScripts = eslint("scripts");
 lintScripts.displayName = "lint-scripts";
