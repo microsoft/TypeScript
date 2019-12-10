50603eda5cd5a8956849f3f217232e5253aec360
Wesley Wigham <wewigham@microsoft.com>
2019-12-09 16:48:00 -0800

    Add ts namespace reference to tsc, since it now doesnt have one

--- src/tsc/tsc.ts
+++ src/tsc/tsc.ts
@@ -1,3 +1,4 @@
+namespace ts {} // empty ts module so the module migration script knows this file depends on the `ts` project namespace
 // This file actually uses arguments passed on commandline and executes it
 ts.executeCommandLine(
     ts.sys,
