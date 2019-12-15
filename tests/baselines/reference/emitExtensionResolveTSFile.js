24449749e1bbaca7950d96c03befeb452be10f4f
Jack Works <jackworks@protonmail.com>
2019-12-15 15:05:41 +0800

    test: add test for deno case import

---
+++ tests/baselines/reference/emitExtensionResolveTSFile.js
@@ -0,0 +1,21 @@
+//// [tests/cases/compiler/emitExtensionResolveTSFile.ts] ////
+
+//// [0.ts]
+// For the deno case
+export default 0
+
+//// [1.ts]
+import num from './0.ts'
+num + 1
+
+
+//// [0.ts]
+"use strict";
+exports.__esModule = true;
+// For the deno case
+exports["default"] = 0;
+//// [1.ts]
+"use strict";
+exports.__esModule = true;
+var _0_ts_1 = require("./0.ts");
+_0_ts_1["default"] + 1;
