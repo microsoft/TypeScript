05a7cbe2bbeec596cd70019a9bf1d4913ee28720
Jack Works <jackworks@protonmail.com>
2019-12-15 15:37:39 +0800

    test: add test case for extension name !== emitExtension

--- tests/baselines/reference/emitExtensionUnknownExtension.js
+++ tests/baselines/reference/emitExtensionUnknownExtension.js
@@ -1,10 +1,10 @@
-//// [tests/cases/compiler/emitExtensionRejectResolveTSFile.ts] ////
+//// [tests/cases/compiler/emitExtensionUnknownExtension.ts] ////
 
 //// [0.ts]
 export default 0
 
 //// [1.ts]
-import num from './0.ts'
+import num from './0.cjs'
 num + 1
 
 
@@ -15,5 +15,5 @@ exports["default"] = 0;
 //// [1.mjs]
 "use strict";
 exports.__esModule = true;
-var _0_ts_1 = require("./0.ts");
-_0_ts_1["default"] + 1;
+var _0_cjs_1 = require("./0.cjs");
+_0_cjs_1["default"] + 1;
--- tests/cases/compiler/emitExtensionUnknownExtension.ts
+++ tests/cases/compiler/emitExtensionUnknownExtension.ts
@@ -1,9 +1,8 @@
 // @emitExtension: .mjs
-// @outDir: ../out/
 
 // @Filename: 0.ts
 export default 0
 
 // @Filename: 1.ts
-import num from './0.ts'
+import num from './0.cjs'
 num + 1
