d293fa92d81404bb893f470ed8f66fdfe03e675f
Sheetal Nandi <shkamat@microsoft.com>
2019-12-17 15:31:29 -0800

    Add baseline for #35468

--- src/testRunner/unittests/tsbuild/watchMode.ts
+++ src/testRunner/unittests/tsbuild/watchMode.ts
@@ -1246,4 +1246,44 @@ const a = {
             ]
         });
     });
+
+    describe("unittests:: tsbuild:: watchMode:: with reexport when referenced project reexports definitions from another file", () => {
+        verifyTscWatch({
+            scenario: "reexport",
+            subScenario: "Reports errors correctly",
+            commandLineArgs: ["-b", "-w", "-verbose", "src"],
+            sys: () => createWatchedSystem(
+                [
+                    ...[
+                        "src/tsconfig.json",
+                        "src/main/tsconfig.json", "src/main/index.ts",
+                        "src/pure/tsconfig.json", "src/pure/index.ts", "src/pure/session.ts"
+                    ]
+                        .map(f => getFileFromProject("reexport", f)),
+                    { path: libFile.path, content: libContent }
+                ],
+                { currentDirectory: `${projectsLocation}/reexport` }
+            ),
+            changes: [
+                sys => {
+                    const content = sys.readFile(`${projectsLocation}/reexport/src/pure/session.ts`)!;
+                    sys.writeFile(`${projectsLocation}/reexport/src/pure/session.ts`, content.replace("// ", ""));
+                    sys.checkTimeoutQueueLengthAndRun(1); // build src/pure
+                    sys.checkTimeoutQueueLengthAndRun(1); // build src/main
+                    sys.checkTimeoutQueueLengthAndRun(1); // build src
+                    sys.checkTimeoutQueueLength(0);
+                    return "Introduce error";
+                },
+                sys => {
+                    const content = sys.readFile(`${projectsLocation}/reexport/src/pure/session.ts`)!;
+                    sys.writeFile(`${projectsLocation}/reexport/src/pure/session.ts`, content.replace("bar: ", "// bar: "));
+                    sys.checkTimeoutQueueLengthAndRun(1); // build src/pure
+                    sys.checkTimeoutQueueLengthAndRun(1); // build src/main
+                    sys.checkTimeoutQueueLengthAndRun(1); // build src
+                    sys.checkTimeoutQueueLength(0);
+                    return "Fix error";
+                }
+            ]
+        });
+    });
 }
