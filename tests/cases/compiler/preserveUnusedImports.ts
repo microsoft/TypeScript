0c2df8ebdc93bcadb98991404ef5cdb822006a0e
Andrew Branch <andrew@wheream.io>
2019-12-20 15:11:53 -0600

    Add test for preserveUnusedImports option

---
+++ tests/cases/compiler/preserveUnusedImports.ts
@@ -0,0 +1,13 @@
+// @preserveUnusedImports: true
+
+// @Filename: a.ts
+export type A = {};
+
+// @Filename: b.ts
+export class B {}
+
+// @Filename: c.ts
+import { A } from './a';
+import { B } from './b';
+
+let b: B;
