cef62f9c5963fd920f82b165513a462a0b68b04f
Max Heiber <max.heiber@gmail.com>
2019-12-23 12:38:30 +0000

    make PrivateIdentifier not a PropertyNameLiteral
    Signed-off-by: Max Heiber <max.heiber@gmail.com>

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -2947,11 +2947,10 @@ namespace ts {
         }
     }
 
-    export type PropertyNameLiteral = Identifier | PrivateIdentifier | StringLiteralLike | NumericLiteral;
+    export type PropertyNameLiteral = Identifier | StringLiteralLike | NumericLiteral;
     export function isPropertyNameLiteral(node: Node): node is PropertyNameLiteral {
         switch (node.kind) {
             case SyntaxKind.Identifier:
-            case SyntaxKind.PrivateIdentifier:
             case SyntaxKind.StringLiteral:
             case SyntaxKind.NoSubstitutionTemplateLiteral:
             case SyntaxKind.NumericLiteral:
