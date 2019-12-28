7ecf11689f3bac215a2f6dbdbb45a63189259340
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-12-28 08:30:00 +0100

    fix(lib/es5): Preserve `Omit` in output

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1463,7 +1463,9 @@ type Extract<T, U> = T extends U ? T : never;
 /**
  * Construct a type with the properties of T except for those in type K.
  */
-type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+type Omit<T, K extends keyof any> = {
+    [P in Exclude<keyof T, K>]: T[P];
+};
 
 /**
  * Exclude null and undefined from T
