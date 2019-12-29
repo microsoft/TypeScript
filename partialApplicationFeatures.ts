34d7ee9de02ed7b99f3905fd698cc8783474768e
Eero Tanskanen <pokute@draconus.com>
2019-12-29 23:55:40 +0200

    Partial application - Notes and test file.

---
+++ partialApplicationFeatures.ts
@@ -0,0 +1,14 @@
+const square = Math.pow(?, 2);
+// const fiveSquared = square(5);
+
+// const cube = (a) => Math.pow(a, 3);
+// const fourCubed = cube(4);
+
+// const add = (a, b) => a + b;
+// const add5 = add(?, 3);
+
+
+// console.log(10 |> add5);
+// console.log(20 |> add(?, 2));
+
+// afterWait();
