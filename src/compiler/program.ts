06f81594c3564af29a5dc309c3875f19e170ae9e
Josh Goldberg <me@joshuakgoldberg.com>
2020-01-05 19:05:58 -0500

    Clarified file-not-found error for reference paths without extensions

--- src/compiler/program.ts
+++ src/compiler/program.ts
@@ -2249,7 +2249,7 @@ namespace ts {
                 }
 
                 const sourceFileWithAddedExtension = forEach(supportedExtensions, extension => getSourceFile(fileName + extension));
-                if (fail && !sourceFileWithAddedExtension) fail(Diagnostics.File_0_not_found, fileName + Extension.Ts);
+                if (fail && !sourceFileWithAddedExtension) fail(Diagnostics.No_file_under_0_found_with_any_supported_extension, fileName);
                 return sourceFileWithAddedExtension;
             }
         }
