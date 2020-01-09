49d21bf0c6c6fd8bd77f6d08e71a32910a8b3bb6
kingwl <kingwenlu@gmail.com>
2020-01-10 00:24:33 +0800

    fix semi

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -3546,7 +3546,6 @@ namespace ts {
          * e.g. it specifies `kind: "a"` and obj has `kind: "b"`.
          */
         /* @internal */ isTypeInvalidDueToUnionDiscriminant(contextualType: Type, obj: ObjectLiteralExpression | JsxAttributes): boolean;
-        /* @internal */ isTypeAssignableTo(type1: Type, type2: Type): boolean;
         /**
          * For a union, will include a property if it's defined in *any* of the member types.
          * So for `{ a } | { b }`, this will include both `a` and `b`.
--- tests/cases/fourslash/codeFixSurmiseReturnValue_all1.ts
+++ tests/cases/fourslash/codeFixSurmiseReturnValue_all1.ts
@@ -67,55 +67,55 @@ verify.codeFixAll({
 
 function foo1 (_a: () => number ) { }
 foo1(() => {
-    return 1;
+    return 1
 })
 function foo2 (_a: () => A) { }
 foo2(() => {
-    return { bar: '1' };
+    return { bar: '1' }
 })
 foo2(() => {
-    return { bar: '1' };
+    return { bar: '1' }
 })
 function foo3 (_a: () => A | number) { }
 foo3(() => {
-    return 1;
+    return 1
 })
 foo3(() => {
-    return { bar: '1' };
+    return { bar: '1' }
 })
 
 function bar1 (): number {
-    return 1;
+    return 1
 }
 function bar2 (): A {
-    return { bar: '1' };
+    return { bar: '1' }
 }
 function bar3 (): A {
-    return { bar: '1' };
+    return { bar: '1' }
 }
 function bar4 (): A | number {
-    return 1;
+    return 1
 }
 function bar5(): A | number {
-    return { bar: '1' };
+    return { bar: '1' }
 }
 const baz1: () => number = () => {
-    return 1;
+    return 1
 }
 const baz2: () => A = () => {
-    return { bar: '1' };
+    return { bar: '1' }
 }
 const baz3: () => A = () => {
-    return { bar: '1' };
+    return { bar: '1' }
 }
 const baz4: ((() => number) | (() => A)) = () => {
-    return 1;
+    return 1
 }
 const baz5: ((() => number) | (() => A)) = () => {
-    return { bar: '1' };
+    return { bar: '1' }
 }
 
 const test: { a: () => A } = { a: () => {
-    return { bar: '1' };
+    return { bar: '1' }
 } }`,
 });
