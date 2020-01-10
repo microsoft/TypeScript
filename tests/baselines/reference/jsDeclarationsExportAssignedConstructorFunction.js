add1d04786ed34bb9b236057234bc8205ba40955
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-01-10 15:06:43 -0800

    add simple test case

---
+++ tests/baselines/reference/jsDeclarationsExportAssignedConstructorFunction.js
@@ -0,0 +1,24 @@
+//// [jsDeclarationsExportAssignedConstructorFunction.js]
+module.exports.MyClass = function() {
+    this.x = 1
+}
+module.exports.MyClass.prototype = {
+    a: function() {
+        this.b = 2
+    }
+}
+
+
+//// [jsDeclarationsExportAssignedConstructorFunction.js]
+module.exports.MyClass = function () {
+    this.x = 1;
+};
+module.exports.MyClass.prototype = {
+    a: function () {
+        this.b = 2;
+    }
+};
+
+
+//// [jsDeclarationsExportAssignedConstructorFunction.d.ts]
+export {};
