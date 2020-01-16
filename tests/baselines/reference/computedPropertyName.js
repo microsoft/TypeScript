8b4c2355df58bcfe0a8df89fc22b45d7ed7ee14b
Andrew Branch <andrew@wheream.io>
2020-01-16 09:41:36 -0800

    Commit missing baselines

--- tests/baselines/reference/computedPropertyName.js
+++ tests/baselines/reference/computedPropertyName.js
@@ -10,7 +10,58 @@ interface Component {
   [onInit]?(): void;
 }
 
+type T = {
+  [onInit]: any;
+}
+
+const o = {
+  [onInit]: 0 // Error
+};
+
+class C {
+  [onInit]: any; // Error (because class fields)
+}
+
+class D {
+  [onInit] = 0; // Error
+}
+
+class E {
+  [onInit]() {} // Error
+}
+
+abstract class F {
+  abstract [onInit](): void;
+}
+
+class G {
+  declare [onInit]: any;
+}
+
+declare class H {
+  [onInit]: any;
+}
+
 
 //// [framework-hooks.js]
 export const onInit = Symbol("onInit");
 //// [component.js]
+var _a;
+const o = {
+    [onInit]: 0 // Error
+};
+class C {
+}
+class D {
+    constructor() {
+        this[_a] = 0; // Error
+    }
+}
+_a = onInit;
+class E {
+    [onInit]() { } // Error
+}
+class F {
+}
+class G {
+}
