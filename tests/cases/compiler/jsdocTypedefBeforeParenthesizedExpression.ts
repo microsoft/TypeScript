1fb4f72fc0e8d82dbb75317c49c225af4b845f5e
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2020-01-16 21:23:47 +0100

    don't parse JSDoc on ExpressionStatement if it starts with ParenthesizedExpression

---
+++ tests/cases/compiler/jsdocTypedefBeforeParenthesizedExpression.ts
@@ -0,0 +1,14 @@
+// @allowJs: true
+// @checkJs: true
+// @noEmit: true
+
+// @filename: test.js
+;
+/** @typedef {object} NotADuplicateIdentifier */
+
+(2 * 2);
+
+/** @typedef {object} AlsoNotADuplicate */
+
+(2 * 2) + 1;
+
