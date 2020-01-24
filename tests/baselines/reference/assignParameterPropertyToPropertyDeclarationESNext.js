09f409cfcb75df76803702664cec2b67046a4dde
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-01-24 14:04:37 -0800

    improve test

--- tests/baselines/reference/assignParameterPropertyToPropertyDeclarationESNext.js
+++ tests/baselines/reference/assignParameterPropertyToPropertyDeclarationESNext.js
@@ -1,11 +1,15 @@
 //// [assignParameterPropertyToPropertyDeclarationESNext.ts]
 class C {
+    qux = this.bar // should error
     bar = this.foo // should error
+    quiz = this.bar // ok
     m1() {
         this.foo // ok
     }
     constructor(private foo: string) {}
+    quim = this.baz // should error
     baz = this.foo; // should error
+    quid = this.baz // ok
     m2() {
         this.foo // ok
     }
@@ -15,14 +19,18 @@ class C {
 //// [assignParameterPropertyToPropertyDeclarationESNext.js]
 class C {
     foo;
+    qux = this.bar; // should error
     bar = this.foo; // should error
+    quiz = this.bar; // ok
     m1() {
         this.foo; // ok
     }
     constructor(foo) {
         this.foo = foo;
     }
+    quim = this.baz; // should error
     baz = this.foo; // should error
+    quid = this.baz; // ok
     m2() {
         this.foo; // ok
     }
