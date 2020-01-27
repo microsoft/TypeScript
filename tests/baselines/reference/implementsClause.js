8c74b61cf87dd14de8e144a0df942bcc2b3903f4
Andrew Branch <andrew@wheream.io>
2020-01-27 15:09:57 -0800

    Add more complex test

--- tests/baselines/reference/implementsClause.js
+++ tests/baselines/reference/implementsClause.js
@@ -3,11 +3,22 @@
 //// [types.ts]
 export interface Component {}
 
+//// [ns.ts]
+import type * as types from './types';
+export { types };
+
 //// [index.ts]
 import type * as types from './types';
+import * as nestedNamespace from './ns';
+
 class C implements types.Component {}
+class D implements nestedNamespace.types.Component {}
+
 
 //// [types.js]
+"use strict";
+exports.__esModule = true;
+//// [ns.js]
 "use strict";
 exports.__esModule = true;
 //// [index.js]
@@ -17,4 +28,9 @@ var C = /** @class */ (function () {
     function C() {
     }
     return C;
+}());
+var D = /** @class */ (function () {
+    function D() {
+    }
+    return D;
 }());
