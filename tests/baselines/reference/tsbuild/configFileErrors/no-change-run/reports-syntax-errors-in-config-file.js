4c378c09dcf8f14d36d6ea982ee45535cf75fdc3
Sheetal Nandi <shkamat@microsoft.com>
2020-01-30 11:18:06 -0800

    Report config file parsing diagnostics correctly with tsc --b (#36520)
    * Refactor the test
    
    * Add tests for syntax errors in tsconfig not being reported
    
    * Report config file parsing diagnostics correctly
    Fixes #36515
    
    * Fix errors in existing tests for unintended tsconfig parse errors
    
    * Fix lint

---
+++ tests/baselines/reference/tsbuild/configFileErrors/no-change-run/reports-syntax-errors-in-config-file.js
@@ -0,0 +1,6 @@
+//// [/lib/no-change-runOutput.txt]
+/lib/tsc --b /src/tsconfig.json
+src/tsconfig.json(7,9): error TS1005: ',' expected.
+exitCode:: ExitStatus.DiagnosticsPresent_OutputsSkipped
+
+
