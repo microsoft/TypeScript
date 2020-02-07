1e48cbe2c92d4e0338179f1ac512ae3bbb6f5c40
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-02-07 08:35:40 -0800

    Fix jsdoc comment parsing initial state (#36661)
    * Fix jsdoc comment parsing initial state
    
    Jsdoc comment parsing can be invoked in two modes:
    
    1. top-level parsing, for comments not inside a tag.
    2. tag parsing, for comment that occur after the semantic parts of a
    tag.
    
    Top-level parsing skips an initial * because it assumes that it is starting
    at the very beginning of a JSDoc comment. Tag parsing does not.
    
    The two modes are distinguished by an optional second parameter named
    `margin`. When `margin` is provided, it provides an initial indent used
    for comment alignment.
    
    Previously, the check for `margin` was a truthy check `if (margin)`.
    This check incorrectly treats `margin=""` the same as
    `margin=undefined`.
    
    This PR changes the check to `if (margin !== undefined)`, which
    correctly treats `margin=""` the same as `margin="    "`.
    
    * Fixes for broken tests
    
    1. Use SawAsterisk start state.
    2. @template needs to skip asterisk in addition to whitespace while
    parsing type parameter names.
    
    * undo code move

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -7017,10 +7017,12 @@ namespace ts {
                         comments.push(text);
                         indent += text.length;
                     }
-                    if (initialMargin) {
+                    if (initialMargin !== undefined) {
                         // jump straight to saving comments if there is some initial indentation
-                        pushComment(initialMargin);
-                        state = JSDocState.SavingComments;
+                        if (initialMargin !== "") {
+                            pushComment(initialMargin);
+                        }
+                        state = JSDocState.SawAsterisk;
                     }
                     let tok = token() as JSDocSyntaxKind;
                     loop: while (true) {
@@ -7558,7 +7560,7 @@ namespace ts {
                         const typeParameter = <TypeParameterDeclaration>createNode(SyntaxKind.TypeParameter);
                         typeParameter.name = parseJSDocIdentifierName(Diagnostics.Unexpected_token_A_type_parameter_name_was_expected_without_curly_braces);
                         finishNode(typeParameter);
-                        skipWhitespace();
+                        skipWhitespaceOrAsterisk();
                         typeParameters.push(typeParameter);
                     } while (parseOptionalJsdoc(SyntaxKind.CommaToken));
 
