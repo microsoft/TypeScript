5cae3bdaa95b743a1134374174b26370aa49f830
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-02-11 09:13:53 -0800

    add tests

--- tests/baselines/reference/spreadOverwritesPropertyStrict.js
+++ tests/baselines/reference/spreadOverwritesPropertyStrict.js
@@ -4,6 +4,8 @@ declare var abq: { a: number, b?: number };
 var unused1 = { b: 1, ...ab } // error
 var unused2 = { ...ab, ...ab } // ok, overwritten error doesn't apply to spreads
 var unused3 = { b: 1, ...abq } // ok, abq might have b: undefined
+var unused4 = { ...ab, b: 1 } // ok, we don't care that b in ab is overwritten
+var unused5 = { ...abq, b: 1 } // ok
 function g(obj: { x: number | undefined }) {
     return { x: 1, ...obj }; // ok, obj might have x: undefined
 }
@@ -31,6 +33,8 @@ var __assign = (this && this.__assign) || function () {
 var unused1 = __assign({ b: 1 }, ab); // error
 var unused2 = __assign(__assign({}, ab), ab); // ok, overwritten error doesn't apply to spreads
 var unused3 = __assign({ b: 1 }, abq); // ok, abq might have b: undefined
+var unused4 = __assign(__assign({}, ab), { b: 1 }); // ok, we don't care that b in ab is overwritten
+var unused5 = __assign(__assign({}, abq), { b: 1 }); // ok
 function g(obj) {
     return __assign({ x: 1 }, obj); // ok, obj might have x: undefined
 }
--- tests/cases/conformance/types/spread/spreadOverwritesPropertyStrict.ts
+++ tests/cases/conformance/types/spread/spreadOverwritesPropertyStrict.ts
@@ -4,6 +4,8 @@ declare var abq: { a: number, b?: number };
 var unused1 = { b: 1, ...ab } // error
 var unused2 = { ...ab, ...ab } // ok, overwritten error doesn't apply to spreads
 var unused3 = { b: 1, ...abq } // ok, abq might have b: undefined
+var unused4 = { ...ab, b: 1 } // ok, we don't care that b in ab is overwritten
+var unused5 = { ...abq, b: 1 } // ok
 function g(obj: { x: number | undefined }) {
     return { x: 1, ...obj }; // ok, obj might have x: undefined
 }
