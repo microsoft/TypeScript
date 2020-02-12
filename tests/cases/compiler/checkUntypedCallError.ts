582f5e0bd31ff0d2b6e58a1371736f878d18a936
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-02-11 16:28:02 -0800

    Update baselines and remove new test
    It was redundant with the old tests

--- tests/cases/compiler/checkUntypedCallError.ts
+++
@@ -1,8 +0,0 @@
-function greeting(username: string, x: () => void) {
-    x();
-    return `Hello, ${username}!`;
-}
-greeting(12, () => {       // error
-    greeting('hi', () => 12)  // ok
-    greeting(12, () => 12) // error
-});
