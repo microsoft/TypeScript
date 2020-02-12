700c3509b2c60e1c7b61616323dfe13fd3f9873f
Sam Denty <samddenty@gmail.com>
2020-02-12 18:56:33 +0000

    Add support for .filter(Boolean)

--- lib/lib.es5.d.ts
+++ lib/lib.es5.d.ts
@@ -1334,6 +1334,12 @@ interface Array<T> {
      * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function one time for each element in the array.
      * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
      */
+    filter(callbackfn: BooleanConstructor): Exclude<T, false | 0 | "" | null | undefined>[];
+    /**
+     * Returns the elements of an array that meet the condition specified in a callback function.
+     * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+     */
     filter<S extends T>(callbackfn: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[];
     /**
      * Returns the elements of an array that meet the condition specified in a callback function.
