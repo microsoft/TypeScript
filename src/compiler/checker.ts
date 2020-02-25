59d90d44b09a853b4debfebc6e1dbb9ebfbaceb8
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2020-02-25 21:49:23 +0000

    Cherry-pick PR #37021 into release-3.8
    Component commits:
    864c2d43c7 Check for undefined `source.symbol`
    Fixes #37014

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -16153,6 +16153,7 @@ namespace ts {
                 if (unmatchedProperty.valueDeclaration
                     && isNamedDeclaration(unmatchedProperty.valueDeclaration)
                     && isPrivateIdentifier(unmatchedProperty.valueDeclaration.name)
+                    && source.symbol
                     && source.symbol.flags & SymbolFlags.Class) {
                     const privateIdentifierDescription = unmatchedProperty.valueDeclaration.name.escapedText;
                     const symbolTableKey = getSymbolNameForPrivateIdentifier(source.symbol, privateIdentifierDescription);
