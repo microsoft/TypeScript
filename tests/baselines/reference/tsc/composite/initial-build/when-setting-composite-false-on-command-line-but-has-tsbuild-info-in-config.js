dc6cbc368c6714633cf22874c3995f7cbbaf735f
Sheetal Nandi <shkamat@microsoft.com>
2020-02-26 15:26:26 -0800

    Allow --composite false or --composite null on the command line (#36997)
    * Add tests for specifying composite as command line option
    
    * Allow passing --composite false on commandline
    
    * Add test to verify tsc --composite false from command line
    
    * Handle "undefined" as option value to be set to undefined for that option
    
    * Support "null" as option to be converted to undefined which is normally end result from our config file as well
    
    * Support null as option for any tsconfig only option as well, and dont support undefined
    
    * Fix public api test case
    
    * Validates objects instead of stringify result
    
    * Add composite true to base source

---
+++ tests/baselines/reference/tsc/composite/initial-build/when-setting-composite-false-on-command-line-but-has-tsbuild-info-in-config.js
@@ -0,0 +1,12 @@
+//// [/lib/initial-buildOutput.txt]
+/lib/tsc --composite false --p src/project
+src/project/tsconfig.json(6,9): error TS5069: Option 'tsBuildInfoFile' cannot be specified without specifying option 'incremental' or option 'composite'.
+exitCode:: ExitStatus.DiagnosticsPresent_OutputsGenerated
+
+
+//// [/src/project/src/main.js]
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.x = 10;
+
+
