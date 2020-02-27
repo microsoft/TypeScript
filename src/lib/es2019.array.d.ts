1b31861d5b361df055db8072de93945f736e6477
Bannerets <comonoid@protonmail.com>
2020-02-27 03:48:06 +0000

    Use more meaningful names

--- src/lib/es2019.array.d.ts
+++ src/lib/es2019.array.d.ts
@@ -1,9 +1,9 @@
-type Flat<A, D extends number> = {
-    '1': A,
-    '0': A extends ReadonlyArray<infer T>
-        ? Flat<T, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][D]>
-        : A
-}[D extends -1 ? '1' : '0']
+type Flat<Arr, Depth extends number> = {
+    "done": Arr,
+    "recur": Arr extends ReadonlyArray<infer InnerArr>
+        ? Flat<InnerArr, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][Depth]>
+        : Arr
+}[Depth extends -1 ? "done" : "recur"];
 
 interface ReadonlyArray<T> {
 
