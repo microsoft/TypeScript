afac48c0122fe11024c92a418c78930d07cf3da8
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-03-06 15:54:20 -0800

    do not handle private identifiers

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -16504,7 +16504,7 @@ namespace ts {
                             }
                             result &= related;
                         }
-                        else {
+                        else if (!isPrivateIdentifierPropertyDeclaration(targetProp.valueDeclaration)) {
                             Debug.assert(!!(targetProp.flags & SymbolFlags.Optional));
                             let sourcePropType, diagnostic;
                             if (isTupleType(source)) {
