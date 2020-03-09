6abd5a134005db8ed53dd56e3a33382690caaaef
Wesley Wigham <wwigham@gmail.com>
2020-03-09 15:13:15 -0700

    Ensure computed property names are always checked

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -22576,6 +22576,15 @@ namespace ts {
             let hasComputedNumberProperty = false;
             propertiesTable = createSymbolTable();
 
+            // Spreads may cause an early bail; ensure computed names are always checked (this is cached)
+            // As otherwise they may not be checked until exports for the type at this position are retrieved,
+            // which may never occur.
+            for (const elem of node.properties) {
+                if (elem.name && isComputedPropertyName(elem.name) && !isWellKnownSymbolSyntactically(elem.name)) {
+                    checkComputedPropertyName(elem.name);
+                }
+            }
+
             let offset = 0;
             for (let i = 0; i < node.properties.length; i++) {
                 const memberDecl = node.properties[i];
