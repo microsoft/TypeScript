9fe87af85dc849a9735dbc89f3f65e560d486804
Wenlu Wang <kingwenlu@gmail.com>
2020-03-11 15:59:43 +0800

    Update src/lib/esnext.promise.d.ts
    Co-Authored-By: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

--- src/lib/esnext.promise.d.ts
+++ src/lib/esnext.promise.d.ts
@@ -15,7 +15,7 @@ declare var AggregateError: AggregateErrorConstructor;
  */
 interface PromiseConstructor {
     /**
-     * The any function returns a promise that is fulfilled by the first given promise to be fulfilled, or rejected with an array of rejection reasons if all of the given promises are rejected. It resolves all elements of the passed iterable to promises as it runs this algorithm.
+     * The any function returns a promise that is fulfilled by the first given promise to be fulfilled, or rejected with an AggregateError containing an array of rejection reasons if all of the given promises are rejected. It resolves all elements of the passed iterable to promises as it runs this algorithm.
      * @param values An array or iterable of Promises.
      * @returns A new Promise.
      */
