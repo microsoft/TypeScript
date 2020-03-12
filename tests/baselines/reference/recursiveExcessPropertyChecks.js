0caa0d8ec20b48f365ba375af711c148e2d857a0
Anders Hejlsberg <andersh@microsoft.com>
2020-03-11 21:31:00 -0700

    Add regression test

---
+++ tests/baselines/reference/recursiveExcessPropertyChecks.js
@@ -0,0 +1,23 @@
+//// [recursiveExcessPropertyChecks.ts]
+// Repro from #35804
+
+interface ITreeItem {    
+  Parent?: this;
+}
+
+type NodeWithId = ITreeItem & { Id?: number };
+
+function getMaxId(items: NodeWithId[]) {
+}
+
+const nodes = [] as ITreeItem[];
+getMaxId(nodes);
+
+
+//// [recursiveExcessPropertyChecks.js]
+"use strict";
+// Repro from #35804
+function getMaxId(items) {
+}
+var nodes = [];
+getMaxId(nodes);
