5b2ef8730bbede87c313140745322a4ab989f861
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2020-03-12 14:50:34 -0700

    Remove 'path' from assert call
    Fixes #37369

--- src/services/services.ts
+++ src/services/services.ts
@@ -1359,7 +1359,7 @@ namespace ts {
                         // We do not support the scenario where a host can modify a registered
                         // file's script kind, i.e. in one project some file is treated as ".ts"
                         // and in another as ".js"
-                        Debug.assertEqual(hostFileInformation.scriptKind, oldSourceFile.scriptKind, "Registered script kind should match new script kind.", path);
+                        Debug.assertEqual(hostFileInformation.scriptKind, oldSourceFile.scriptKind, "Registered script kind should match new script kind.");
 
                         return documentRegistry.updateDocumentWithKey(fileName, path, newSettings, documentRegistryBucketKey, hostFileInformation.scriptSnapshot, hostFileInformation.version, hostFileInformation.scriptKind);
                     }
