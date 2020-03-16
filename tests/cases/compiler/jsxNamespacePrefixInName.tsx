7000ca7519f895f7c7f8d5a8db8481b676dd0212
tomjw64 <tom.jw64@gmail.com>
2020-03-16 14:54:31 -0500

    Support xml namespace prefix for JSX elements and attributes
    Just as with the `-` character, `:` is now also treated specially in JSX
    element and attribute names, but is only allowed a single time, and not
    at the beginning or end of the name, as is specified in the JSX spec.
    All tests in jsxInvalidEsprimaTestSuite still fail, but for slightly
    different reasons now. Two lines in jsxEsprimaFbTestSuite were
    uncommented as they included elements with namespaces, and they now pass
    without error.

---
+++ tests/cases/compiler/jsxNamespacePrefixInName.tsx
@@ -0,0 +1,21 @@
+// @jsx: preserve
+
+var justElement1 = <a:element />;
+var justElement2 = <a:element></a:element>;
+var justElement3 = <a:element attr={"value"}></a:element>;
+var justElement4 = <a:element>{"text"}</a:element>;
+var justElement5 = <a:element attr={"value"}>{"text"}</a:element>;
+
+var tooManySeparators1 = <a:ele:ment />;
+var tooManySeparators2 = <a:ele:ment></a:ele:ment>;
+var tooManySeparators3 = <a:ele:ment attr={"value"}></a:ele:ment>;
+var tooManySeparators4 = <a:ele:ment>{"text"}</a:ele:ment>;
+var tooManySeparators5 = <a:ele:ment attr={"value"}>{"text"}</a:ele:ment>;
+
+var justAttribute1 = <element a:attr={"value"} />;
+var justAttribute2 = <element a:attr={"value"}></element>;
+var justAttribute3 = <element a:attr={"value"}>{"text"}</element>;
+
+var both1 = <a:element a:attr={"value"} />;
+var both2 = <a:element k:attr={"value"}></a:element>;
+var both3 = <a:element a:attr={"value"}>{"text"}</a:element>;
