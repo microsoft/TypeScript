0aa2e2783c42dd48e7d3085e1a612ac410416ec1
Alexander T <alexander.tarasyuk@outlook.com>
2020-03-18 18:54:17 +0200

    feat(37409): add fix all quick fix to fixInvalidJsxCharacters (#37436)

--- tests/cases/fourslash/codeFixInvalidJsxCharacters10.ts
+++ tests/cases/fourslash/codeFixInvalidJsxCharacters10.ts
@@ -6,13 +6,8 @@
 //// let foo = <div>></div>;
 
 verify.codeFix({
+    index: 0,
     description: ts.Diagnostics.Wrap_invalid_character_in_an_expression_container.message,
     newFileContent: `let foo = <div>{">"}</div>;`,
-    index: 0,
-});
-
-verify.codeFix({
-    description: ts.Diagnostics.Convert_invalid_character_to_its_html_entity_code.message,
-    newFileContent: `let foo = <div>&gt;</div>;`,
-    index: 1,
+    preferences: { quotePreference: "double" }
 });
