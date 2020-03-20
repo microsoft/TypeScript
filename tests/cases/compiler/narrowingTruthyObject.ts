1380811a34570579ca3ffd3ee10691d72d05492e
Anders Hejlsberg <andersh@microsoft.com>
2020-03-20 16:08:16 -0700

    Add tests

---
+++ tests/cases/compiler/narrowingTruthyObject.ts
@@ -0,0 +1,25 @@
+// @strict: true
+
+function foo(x: unknown, b: boolean) {
+    if (typeof x === 'object') {
+        x.toString();
+    }
+    if (typeof x === 'object' && x) {
+        x.toString();
+    }
+    if (x && typeof x === 'object') {
+        x.toString();
+    }
+    if (b && x && typeof x === 'object') {
+        x.toString();
+    }
+    if (x && b && typeof x === 'object') {
+        x.toString();
+    }
+}
+
+// Repro from #36870
+
+function f1(x: unknown): any {
+    return x && typeof x === 'object' && x.hasOwnProperty('x');
+}
