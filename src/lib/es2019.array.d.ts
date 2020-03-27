c92d25f93b4a189cb370b505d61c67511fda8824
Bannerets <comonoid@protonmail.com>
2020-03-27 01:19:58 +0000

    Rename 'Flat' to 'FlatArray'

--- src/lib/es2019.array.d.ts
+++ src/lib/es2019.array.d.ts
@@ -1,7 +1,7 @@
-type Flat<Arr, Depth extends number> = {
+type FlatArray<Arr, Depth extends number> = {
     "done": Arr,
     "recur": Arr extends ReadonlyArray<infer InnerArr>
-        ? Flat<InnerArr, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][Depth]>
+        ? FlatArray<InnerArr, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][Depth]>
         : Arr
 }[Depth extends -1 ? "done" : "recur"];
 
@@ -32,7 +32,7 @@ interface ReadonlyArray<T> {
     flat<A, D extends number = 1>(
         this: A,
         depth?: D
-    ): Flat<A, D>[]
+    ): FlatArray<A, D>[]
   }
 
 interface Array<T> {
@@ -61,5 +61,5 @@ interface Array<T> {
     flat<A, D extends number = 1>(
         this: A,
         depth?: D
-    ): Flat<A, D>[]
+    ): FlatArray<A, D>[]
 }
