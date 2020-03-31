0164dab968654c23ce498909a6770b78aa97df85
Anders Hejlsberg <andersh@microsoft.com>
2020-03-31 16:04:25 -0700

    Add tests

---
+++ tests/cases/compiler/intersectionNotAssignableToConstituents.ts
@@ -0,0 +1,62 @@
+// @strict: true
+// @target: esnext
+
+class A { private x: unknown }
+class B { private x: unknown }
+
+function f1(node: A | B) {
+  if (node instanceof A || node instanceof A) {
+    node;  // A
+  }
+  else {
+    node;  // B
+  }
+  node;  // A | B
+}
+
+function f2(a: A, b: B, c: A & B) {
+  a = b;  // Error
+  b = a;  // Error
+  a = c;  // Error (conflicting private fields)
+  b = c;  // Error (conflicting private fields)
+}
+
+// Repro from #37659
+
+abstract class ViewNode { }
+abstract class ViewRefNode extends ViewNode { }
+abstract class ViewRefFileNode extends ViewRefNode { }
+
+class CommitFileNode extends ViewRefFileNode {
+  private _id: any;
+}
+
+class ResultsFileNode extends ViewRefFileNode {
+  private _id: any;
+}
+
+class StashFileNode extends CommitFileNode { 
+  private _id2: any;
+}
+
+class StatusFileNode extends ViewNode {
+  private _id: any;
+}
+
+class Foo {
+  private async foo(node: CommitFileNode | ResultsFileNode | StashFileNode) {
+		if (
+			!(node instanceof CommitFileNode) &&
+			!(node instanceof StashFileNode) &&
+			!(node instanceof ResultsFileNode)
+		) {
+			return;
+		}
+
+		await this.bar(node);
+	}
+
+  private async bar(node: CommitFileNode | ResultsFileNode | StashFileNode | StatusFileNode, options?: {}) {
+    return Promise.resolve(undefined);
+  }
+}
