6ad12e00211cb21138c8b52bca1106787687b74e
jeffy-g <hirotom1107@gmail.com>
2020-04-02 22:32:54 +0900

    regex

--- tests/cases/fourslash/completionsJsdocTag.ts
+++ tests/cases/fourslash/completionsJsdocTag.ts
@@ -5,14 +5,55 @@
 //// * /**/
 //// */
 
+
+// jsdoc tags are listed when there is more than one whitespace after "*"
 /////**
-//// +/*1*/
+//// * /*1*/
 //// */
-////function a0(s: string) {}
+////
+/////**
+//// * link to {/*2*/
+//// */
 ////
 
-/////**@/*2*/ */
-////const some = 0;
+// before the fix, jsdoc tag names was listed (but no longer appears
+// jsdoc tag names are still listed after the fix.
+// however this behavior does not by getCompletionData.insideComment.hasDocComment clause
+/////**@/*3*/ */
+////
+/////**
+//// * @type {@/*4*/
+//// */
+////
+/////**
+//// *@/*5*/
+//// */
+////
+
+// before the fix, jsdoc tags was listed but no longer appears
+/////**
+//// +/*6*/
+//// */
+////
+/////**
+//// */*7*/
+//// */
+////
+
+// jsdoc tag names will be listed
+/////**
+//// * @/*8*/
+//// */
+////
+
+// before the fix, jsdoc tag names was listed but no longer appears
+/////**
+//// +@/*9*/
+//// */
+////
+/////**
+//// * ### jsdoc @/*10*/
+//// */
 ////
 
 verify.completions({ marker: "", includes: { name: "@property", text: "@property", kind: "keyword" } });
@@ -21,69 +62,41 @@ verify.completions({ marker: "", includes: { name: "@property", text: "@property
 //
 // test for src/services/completions.ts#getCompletionData.insideComment.hasDocComment (#37546)
 //
-goTo.marker("2");
-// line 1: [/**@|c|]
-// before the fix, jsdoc tag names was listed (but no longer appears
-// jsdoc tag names are still listed after the fix.
-// however this behavior does not by getCompletionData.insideComment.hasDocComment clause
-verify.completions({
-    triggerCharacter: "@",
-    includes: ["abstract", "access"]
-});
-// line 1: [/**@|c|] -> [/**|c|]
-// before the fix, jsdoc tags was listed but no longer appears
-edit.backspace(1);
-verify.completions({
-    exact: []
-});
-
-goTo.marker("1");
-// line 2: [ +|c|]
-verify.completions({
-    // marker: "1",
-    exact: []
-});
-// line 2: [ +|c|] -> [ +@|c|]
-// before the fix, jsdoc tag names was listed but no longer appears
-edit.insert("@");
-verify.completions({
-    // marker: "1", // marker is invalid
-    triggerCharacter: "@",
-    exact: []
-});
-
-// line 2: [ +@|c|] -> [ * ### jsdoc @|c|]
-// before the fix, jsdoc tag names was listed but no longer appears
-edit.replaceLine(1, " * ### jsdoc @");
-verify.completions({
-    triggerCharacter: "@",
-    exact: []
-});
-
-// line 2: [ +@|c|] -> [ *|c|]
-// before the fix, jsdoc tags was listed but no longer appears
-edit.replaceLine(1, " *");
-verify.completions({
-    exact: []
-});
-
-// line 2: [ *|c|] -> [ *@|c|]
-// this behavior does not by getCompletionData.insideComment.hasDocComment clause
-edit.insert("@");
-verify.completions({
-    triggerCharacter: "@",
-    includes: ["abstract", "access"]
-});
-// line 2: [ *@|c|] -> [ * |c|]
-// jsdoc tags are listed when there is more than one whitespace after "*"
-edit.replaceLine(1, " * ");
-verify.completions({
-    includes: ["@abstract", "@access"]
-});
-// line 2: [ * |c|] -> [ * @|c|]
-// jsdoc tag names will be listed
-edit.insert("@");
-verify.completions({
-    triggerCharacter: "@",
-    includes: ["abstract", "access"]
+test.markerNames().forEach(marker => {
+    if (marker) {
+        const n = +marker;
+        switch (n) {
+            case 1:
+            // case 2:
+                verify.completions({ marker, includes: ["@abstract", "@access"] });
+                break;
+            case 3:
+            // case 4:
+            case 5:
+                verify.completions({
+                    marker,
+                    triggerCharacter: "@",
+                    includes: ["abstract", "access"]
+                });
+                break;
+            case 6: case 7:
+                verify.completions({ marker, exact: [] });
+                break;
+            case 8:
+                verify.completions({
+                    marker,
+                    triggerCharacter: "@",
+                    includes: ["abstract", "access"]
+                });
+                break;
+            case 9: case 10:
+                verify.completions({
+                    marker,
+                    triggerCharacter: "@",
+                    exact: []
+                });
+                break;
+            default: break;
+        }
+    }
 });
