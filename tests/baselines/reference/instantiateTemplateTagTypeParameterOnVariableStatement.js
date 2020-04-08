f31b5a278f03b2e2d2713ed64230f853ae6b4545
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-04-08 15:38:40 -0700

    fix build break by updating missed baseline

--- tests/baselines/reference/instantiateTemplateTagTypeParameterOnVariableStatement.js
+++ tests/baselines/reference/instantiateTemplateTagTypeParameterOnVariableStatement.js
@@ -16,6 +16,11 @@ var text3 = seq(text1)(text2);
 
 
 //// [instantiateTemplateTagTypeParameterOnVariableStatement.d.ts]
+/**
+ * @template T
+ * @param {T} a
+ * @returns {(b: T) => T}
+ */
 declare function seq<T>(a: T): (b: T) => T;
 declare const text1: "hello";
 declare const text2: "world";
