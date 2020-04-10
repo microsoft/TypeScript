1b50be1720fbf7a0d39e41331968bdcb1f13ead0
Ayaz Hafiz <ayazhafiz@lyft.com>
2020-04-10 10:34:54 -0700

    fixup! Make `Symbol#valueDeclaration` optional in public API

--- scripts/overwrite.ts
+++ scripts/overwrite.ts
@@ -1,24 +1,24 @@
-import * as ts from 'typescript';
+import * as ts from "typescript";
 
 export function overwriteOptional(data: string): string {
-    const sourceFile = ts.createSourceFile("input.d.ts", data, ts.ScriptTarget.Latest, true);
-    const fixes: Array<{insertAt: number, remove: [number, number]}> = [];
+    const sourceFile = ts.createSourceFile("input.d.ts", data, ts.ScriptTarget.Latest, /*setParentNodes*/ true);
+    const fixes: {insertAt: number, remove: [number, number]}[] = [];
     sourceFile.forEachChild(visit);
 
     let result = data;
     while (fixes.length > 0) {
         const {insertAt, remove} = fixes.pop()!;
-        result = result.substring(0, remove[0]) + result.substring(remove[1], insertAt) + '?' + result.substring(insertAt);
+        result = result.substring(0, remove[0]) + result.substring(remove[1], insertAt) + "?" + result.substring(insertAt);
     }
     return result;
 
     function visit(node: ts.Node) {
         if (isTraversable(node)) {
             const trivia = node.getFullText().substr(0, node.getLeadingTriviaWidth());
-            if (trivia.indexOf('@optional') >= 0 && hasName(node)) {
+            if (trivia.indexOf("@optional") >= 0 && hasName(node)) {
                 fixes.push({
                   insertAt: node.name.getEnd(),
-                  remove: [node.getStart(sourceFile, true), node.getStart()],  // jsDoc
+                  remove: [node.getStart(sourceFile, /* includeJsDocComment */ true), node.getStart()],
                 });
             }
             node.forEachChild(visit);
@@ -48,7 +48,7 @@ function isTraversable(node: ts.Node) {
     return false;
 }
 
-function hasName(node: ts.Node) : node is ts.Node&{name: ts.Identifier} {
+function hasName(node: ts.Node): node is ts.Node & {name: ts.Identifier} {
     switch (node.kind) {
         case ts.SyntaxKind.PropertySignature:
             return true;
