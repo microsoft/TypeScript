4f4fa2af535b2b56fa9450451d494cd1267b4196
kingwl <kingwenlu@gmail.com>
2020-04-14 00:42:39 +0800

    Add more tests

--- tests/baselines/reference/deleteExpressionMustBeOptional(strict=true).js
+++ tests/baselines/reference/deleteExpressionMustBeOptional(strict=true).js
@@ -11,7 +11,17 @@ interface Foo {
     i: never
 }
 
+interface AA {
+    [s: string]: number
+}
+
+type BB = {
+    [P in keyof any]: number
+}
+
 declare const f: Foo
+declare const a: AA
+declare const b: BB
 
 delete f.a
 delete f.b
@@ -23,6 +33,12 @@ delete f.g
 delete f.h
 delete f.i
 delete f.j
+
+delete a.a
+delete a.b
+
+delete b.a
+delete b.b
 
 //// [deleteExpressionMustBeOptional.js]
 "use strict";
@@ -36,3 +52,7 @@ delete f.g;
 delete f.h;
 delete f.i;
 delete f.j;
+delete a.a;
+delete a.b;
+delete b.a;
+delete b.b;
