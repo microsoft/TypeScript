fd470ae655f98db2b7b300061f38d0ec010a3e9b
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-04-14 11:01:44 -0700

    Fix instantiated check for imports
    Currently, the instantiated check for namespaces assumes that an
    `import` that doesn't use the `export import` syntax is uninstantiated.
    This is not correct when the import is exported.
    
    This PR searchs siblings and descendants for exports of the symbol bound
    by the import.
    
    This is less efficient. I'm thinking about how to improve that.
    
    Fixes #35385

--- src/compiler/binder.ts
+++ src/compiler/binder.ts
@@ -48,12 +48,15 @@ namespace ts {
                 }
                 break;
             // 3. non-exported import declarations
-            case SyntaxKind.ImportDeclaration:
             case SyntaxKind.ImportEqualsDeclaration:
-                if (!(hasModifier(node, ModifierFlags.Export))) {
-                    return ModuleInstanceState.NonInstantiated;
+                if (isExportedInBlock(node as ImportEqualsDeclaration)) {
+                    return ModuleInstanceState.Instantiated;
                 }
-                break;
+                // FALLTHROUGH
+            case SyntaxKind.ImportDeclaration:
+                return hasModifier(node, ModifierFlags.Export) ?
+                    ModuleInstanceState.Instantiated :
+                    ModuleInstanceState.NonInstantiated;
             // 4. Export alias declarations pointing at only uninstantiated modules or things uninstantiated modules contain
             case SyntaxKind.ExportDeclaration:
                 const exportDeclaration = node as ExportDeclaration;
@@ -104,6 +107,43 @@ namespace ts {
                 }
         }
         return ModuleInstanceState.Instantiated;
+    }
+
+    function isExportedInBlock(node: ImportEqualsDeclaration) {
+        const root = findAncestor(node, (n): n is ModuleDeclaration | SourceFile => isModuleDeclaration(n) || isSourceFile(n));
+        if (!root) {
+            return false;
+        }
+        else if (isSourceFile(root)) {
+            return isExportedInBlockWorker(node, root.statements);
+        }
+        else {
+            return root.body && isModuleBlock(root.body) && isExportedInBlockWorker(node, root.body.statements);
+        }
+    }
+
+    function isExportedInBlockWorker(node: ImportEqualsDeclaration, statements: readonly Statement[]): boolean {
+        for (const statement of statements) {
+            switch (statement.kind) {
+                case SyntaxKind.ModuleDeclaration:
+                    const decl = statement as ModuleDeclaration;
+                    return !!decl.body && isModuleBlock(decl.body) && isExportedInBlockWorker(node, decl.body.statements);
+                case SyntaxKind.Block:
+                    // recur on statements
+                    return isExportedInBlockWorker(node, (statement as Block).statements);
+                case SyntaxKind.ExportDeclaration:
+                    // check exportClause.elements
+                    if (isExportDeclaration(statement) && !statement.moduleSpecifier && statement.exportClause && isNamedExports(statement.exportClause)) {
+                        for (const spec of statement.exportClause.elements) {
+                            if (idText(node.name) === idText(spec.propertyName || spec.name)) {
+                                return true;
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+        return false;
     }
 
     function getModuleInstanceStateForAliasTarget(specifier: ExportSpecifier, visited: Map<ModuleInstanceState | undefined>) {
---
+++ tests/cases/conformance/internalModules/importDeclarations/reexportedImportDeclarations.ts
@@ -0,0 +1,21 @@
+// @target: es2015
+declare class C {
+    method(): any;
+}
+
+declare namespace ns1 {
+    export { C as A1 };
+}
+declare namespace ns2 {
+    import A2 = ns1.A1;
+    namespace ns21 {
+        export { A2 as A3 };
+    }
+}
+declare namespace ns3 {
+    import A4 = ns1.A1;
+    export { A4 as A5 };
+}
+
+export class C2 extends ns2.ns21.A3 {}
+export class C3 extends ns3.A5 {}
