ec6f191e811bced616290496c4eeaac9f11a9e95
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-04-22 09:30:38 -0700

    Filter undefined only in binding patterns in params
    initialiser. But this is only correct when the initialiser is for a
    parameter. For example:
    
    ```ts
    declare let x: { s: string } | undefined;
    const { s } = x;
    ```
    
    This PR removes undefined from the type of a binding pattern only when
    the binding pattern's parent is a parameter. This fixes the regression
    from 3.8. However, it's still not the ideal fix; we should be able to
    use control flow to solve this problem. Consider:
    
    ```ts
    const { s }: { s: string } | undefined = { s: 'hi' }
    declare function f({ s }: { s: string } | undefined = { s: 'hi' }): void
    ```
    
    Neither line should have an error, but the first does in 3.8 and after
    this change.

--- tests/cases/compiler/contextualTypeForInitalizedVariablesFiltersUndefined.ts
+++ tests/cases/compiler/contextualTypeForInitalizedVariablesFiltersUndefined.ts
@@ -3,3 +3,6 @@ const fInferred = ({ a = 0 } = {}) => a;
 // const fInferred: ({ a }?: { a?: number; }) => number
 
 const fAnnotated: typeof fInferred = ({ a = 0 } = {}) => a;
+
+declare var t: { s: string } | undefined;
+const { s } = t;
