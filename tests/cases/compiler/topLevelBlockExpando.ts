ee1ae84c94e8d719f701c9af6b30f9a4355208f8
orta therox <orta.therox@gmail.com>
2020-04-22 12:39:03 -0400

    Adds JS tests to validate the JS parsing also works

--- tests/cases/compiler/topLevelBlockExpando.ts
+++ tests/cases/compiler/topLevelBlockExpando.ts
@@ -1,4 +1,12 @@
 // https://github.com/microsoft/TypeScript/issues/31972
+
+// @allowJs: true
+// @noEmit: true
+// @checkJs: true
+
+// @filename: check.ts
+
+// https://github.com/microsoft/TypeScript/issues/31972
 interface Person {
   first: string;
   last: string;
@@ -9,4 +17,33 @@ interface Person {
   dice.first = 'Rando';  
   dice.last = 'Calrissian';
   const diceP: Person = dice;  
+}
+
+// @filename: check.js
+
+// Creates a type { first:string, last: string }
+/**
+ * @typedef {Object} Human - creates a new type named 'SpecialType'
+ * @property {string} first - a string property of SpecialType
+ * @property {string} last - a number property of SpecialType
+ */
+
+/**
+ * @param {Human} param used as a validation tool
+ */
+function doHumanThings(param) {}
+
+const dice = () => Math.floor(Math.random() * 6);
+dice.first = 'Rando';
+dice.last = 'Calrissian';
+
+doHumanThings(dice)
+
+// but inside a block... you can't call iut a human
+{
+  const dice = () => Math.floor(Math.random() * 6);
+  dice.first = 'Rando';
+  dice.last = 'Calrissian'; 
+  
+  doHumanThings(dice)
 }
