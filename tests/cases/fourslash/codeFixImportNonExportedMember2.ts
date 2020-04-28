7f7ab7ed19695f6f8cb88ffa2279a79b229e70fa
forfun <834352945@qq.com>
2020-04-28 15:24:16 +0800

    modify testcase

--- tests/cases/fourslash/codeFixImportNonExportedMember2.ts
+++ tests/cases/fourslash/codeFixImportNonExportedMember2.ts
@@ -1,25 +1,31 @@
 /// <reference path='fourslash.ts' />
+
 // @Filename: /a.ts
 ////let a = 1, b = 2, c = 3;
-////let d = 4;
 ////export function whatever() {
 ////}
 
 // @Filename: /b.ts
-////import { a, d } from "./a"
+////let d = 4;
+////export function whatever2() {
+////}
 
-goTo.file("/b.ts");
+// @Filename: /c.ts
+////import { a } from "./a"
+////import { d } from "./b"
+
+goTo.file("/c.ts");
 verify.codeFixAvailable([
     { description: `Export 'a' from module './a'` },
-    { description: `Export 'd' from module './a'` },
+    { description: `Export 'd' from module './b'` },
     { description: `Remove import from './a'` },
+    { description: `Remove import from './b'` },
 ]);
 verify.codeFix({
     index: 0,
     description: `Export 'a' from module './a'`,
     newFileContent: {
         '/a.ts': `let a = 1, b = 2, c = 3;
-let d = 4;
 export function whatever() {
 }
 
@@ -30,11 +36,10 @@ export { a };
 
 verify.codeFix({
     index: 1,
-    description: `Export 'd' from module './a'`,
+    description: `Export 'd' from module './b'`,
     newFileContent: {
-        '/a.ts': `let a = 1, b = 2, c = 3;
-export let d = 4;
-export function whatever() {
+        '/b.ts': `export let d = 4;
+export function whatever2() {
 }`
     }
 });
