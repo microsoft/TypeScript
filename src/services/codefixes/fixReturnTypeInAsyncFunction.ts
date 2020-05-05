23b9875c1be4434177c5c853d98e31107fc74aee
Daniel <Daniel.Rosenwasser@microsoft.com>
2020-05-05 01:20:31 +0000

    Use returnTypeNode as the enclosingDeclaration.

--- src/services/codefixes/fixReturnTypeInAsyncFunction.ts
+++ src/services/codefixes/fixReturnTypeInAsyncFunction.ts
@@ -52,7 +52,7 @@ namespace ts.codefix {
 
         const returnType = checker.getTypeFromTypeNode(returnTypeNode);
         const promisedType = checker.getAwaitedType(returnType) || checker.getVoidType();
-        const promisedTypeNode = checker.typeToTypeNode(promisedType, /*enclosingDeclaration*/ func, /*flags*/ undefined);
+        const promisedTypeNode = checker.typeToTypeNode(promisedType, /*enclosingDeclaration*/ returnTypeNode, /*flags*/ undefined);
         if (promisedTypeNode) {
             return { returnTypeNode, returnType, promisedTypeNode, promisedType };
         }
