e9867a73532d7235cfc57210f0f59bdcd4967f5e
Daniel <Daniel.Rosenwasser@microsoft.com>
2020-05-11 22:14:45 +0000

    Add and use the 'intersperse' helper function.

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -130,6 +130,22 @@ namespace ts {
     }
 
     /**
+     * Creates a new array with `element` interspersed in between each element of `input`
+     * if there is more than 1 value in `input`. Otherwise, returns the existing array.
+     */
+    export function intersperse<T>(input: T[], element: T): T[] {
+        if (input.length <= 1) {
+            return input;
+        }
+        const result: T[] = [];
+        for (let i = 0, n = input.length; i < n; i++) {
+            if (i) result.push(element);
+            result.push(input[i]);
+        }
+        return result;
+    }
+
+    /**
      * Iterates through `array` by index and performs the callback on each element of array until the callback
      * returns a falsey value, then returns false.
      * If no such value is found, the callback is applied to each element of array and `true` is returned.
