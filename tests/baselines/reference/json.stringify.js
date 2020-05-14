f26c0113674d554986c65ac717066dc896449954
Pieter Raubenheimer <pieter@wavana.com>
2020-05-14 17:52:04 +0100

    Fix return type for JSON.stringify

--- tests/baselines/reference/json.stringify.js
+++ tests/baselines/reference/json.stringify.js
@@ -1,15 +1,40 @@
 //// [json.stringify.ts]
+var replacer = (k) => undefined;
 var value = null;
 JSON.stringify(value, undefined, 2);
 JSON.stringify(value, null, 2);
 JSON.stringify(value, ["a", 1], 2);
-JSON.stringify(value, (k) => undefined, 2);
+JSON.stringify(value, replacer, 2);
 JSON.stringify(value, undefined, 2);
+JSON.stringify(undefined);
+JSON.stringify(undefined, replacer);
+JSON.stringify(() => '');
+JSON.stringify(() => '', replacer);
+JSON.stringify({});
+JSON.stringify({}, replacer);
+JSON.stringify(new Object());
+JSON.stringify(new Object(), replacer);
+var anyValue: any;
+JSON.stringify(anyValue);
+JSON.stringify(anyValue, replacer);
+
 
 //// [json.stringify.js]
+var replacer = function (k) { return undefined; };
 var value = null;
 JSON.stringify(value, undefined, 2);
 JSON.stringify(value, null, 2);
 JSON.stringify(value, ["a", 1], 2);
-JSON.stringify(value, function (k) { return undefined; }, 2);
+JSON.stringify(value, replacer, 2);
 JSON.stringify(value, undefined, 2);
+JSON.stringify(undefined);
+JSON.stringify(undefined, replacer);
+JSON.stringify(function () { return ''; });
+JSON.stringify(function () { return ''; }, replacer);
+JSON.stringify({});
+JSON.stringify({}, replacer);
+JSON.stringify(new Object());
+JSON.stringify(new Object(), replacer);
+var anyValue;
+JSON.stringify(anyValue);
+JSON.stringify(anyValue, replacer);
--- tests/cases/compiler/json.stringify.ts
+++ tests/cases/compiler/json.stringify.ts
@@ -1,8 +1,20 @@
 // @strictNullChecks: true
 
+var replacer = (k) => undefined;
 var value = null;
 JSON.stringify(value, undefined, 2);
 JSON.stringify(value, null, 2);
 JSON.stringify(value, ["a", 1], 2);
-JSON.stringify(value, (k) => undefined, 2);
+JSON.stringify(value, replacer, 2);
 JSON.stringify(value, undefined, 2);
+JSON.stringify(undefined);
+JSON.stringify(undefined, replacer);
+JSON.stringify(() => '');
+JSON.stringify(() => '', replacer);
+JSON.stringify({});
+JSON.stringify({}, replacer);
+JSON.stringify(new Object());
+JSON.stringify(new Object(), replacer);
+var anyValue: any;
+JSON.stringify(anyValue);
+JSON.stringify(anyValue, replacer);
