57e856b0b42f9e4aa6a3d999e9cbdad7458115d3
Jesse Trinity <jetrinit@microsoft.com>
2020-05-14 11:31:50 -0700

    convert export tests

---
+++ tests/cases/fourslash/refactorConvertExport_defaultToNamedValidSpans.ts
@@ -0,0 +1,9 @@
+/// <reference path='fourslash.ts' />
+
+/////*a1*/ex/*a2*/port def/*a3*//*b3*/ault functio/*b2*/n f() {}/*b1*/
+
+// verify that the refactor is offered for full, partial, and empty spans.
+for (const m of ["1", "2", "3"]) {
+    goTo.select("a" + m, "b" + m);
+    verify.refactorAvailable("Convert export", "Convert default export to named export");
+}
---
+++ tests/cases/fourslash/refactorConvertExport_namedToDefaultValidSpans.ts
@@ -0,0 +1,9 @@
+/// <reference path='fourslash.ts' />
+
+/////*a1*/ex/*a2*/port f/*a3*//*b3*/unctio/*b2*/n f() {}/*b1*/
+
+// verify that the refactor is offered for full, partial, and empty spans.
+for (const m of ["1", "2", "3"]) {
+    goTo.select("a" + m, "b" + m);
+    verify.refactorAvailable("Convert export", "Convert named export to default export");
+}
