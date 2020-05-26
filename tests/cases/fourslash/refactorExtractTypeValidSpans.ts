df8ff659c5dbcdd00e718ac6e2f3954fe8448548
Jesse Trinity <jetrinit@microsoft.com>
2020-05-26 13:12:02 -0700

    extract type test

---
+++ tests/cases/fourslash/refactorExtractTypeValidSpans.ts
@@ -0,0 +1,12 @@
+/// <reference path='fourslash.ts' />
+
+//// var x: /*1a*/{ a?:/*2a*/ number, b?: string/*2b*//*3a*//*3b*/ }/*1b*/ = { };
+
+// Only offer refactor for cursor position if explicitly requested
+goTo.select("3a", "3b");
+verify.refactorNotAvailableForTriggerReason("implicit", "Extract type");
+
+for (const m of ["1", "2", "3"]) {
+    goTo.select(m + "a", m + "b");
+    verify.refactorAvailableForTriggerReason("invoked", "Extract type");
+}
