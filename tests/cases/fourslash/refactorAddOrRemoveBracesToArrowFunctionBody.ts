673a86833aa070f94f6ec4c245f3ccb8309d36f7
Jesse Trinity <jetrinit@microsoft.com>
2020-05-26 17:51:00 -0700

    update test names

--- tests/cases/fourslash/refactorAddOrRemoveBracesToArrowFunctionBody.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path='fourslash.ts' />
-
-//// const a = (a: number) => { return/*a*//*b*/ a; };
-
-// an invoked refactor request for a cursor in the body should return a refactor
-goTo.select("a", "b");
-verify.refactorAvailableForTriggerReason("invoked","Add or remove braces in an arrow function", "Remove braces from arrow function");
