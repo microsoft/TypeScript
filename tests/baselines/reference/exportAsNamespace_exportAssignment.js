b36b803cddc29e3c5f3950dfe6187beda8be4ac8
Wesley Wigham <wewigham@microsoft.com>
2020-06-01 14:52:24 -0700

    Skip default when initially iterating exports in __importStar, same as __exportStar (#38808)
    * Skip default when iterating exports in __importStar, same as __exportStar
    
    * Accept baselines

--- tests/baselines/reference/exportAsNamespace_exportAssignment.js
+++ tests/baselines/reference/exportAsNamespace_exportAssignment.js
@@ -27,7 +27,7 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
 };
