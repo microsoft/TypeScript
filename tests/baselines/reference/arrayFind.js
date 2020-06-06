b74b61568ea6235f92faba498451f1282df8b480
Andrei Alecu <andrei.alecu@vecttor.co>
2020-06-06 14:49:51 +0300

    baseline-accept for arrayFind tests

--- tests/baselines/reference/arrayFind.js
+++ tests/baselines/reference/arrayFind.js
@@ -10,6 +10,13 @@ const foundNumber: number | undefined = arrayOfStringsNumbersAndBooleans.find(is
 const readonlyArrayOfStringsNumbersAndBooleans = arrayOfStringsNumbersAndBooleans as ReadonlyArray<string | number | boolean>;
 const readonlyFoundNumber: number | undefined = readonlyArrayOfStringsNumbersAndBooleans.find(isNumber);
 
+// test for #38959
+interface Foo { kind: "foo"; foo: string; }
+interface Bar { kind: "bar"; bar: string; }
+type FooBar = Foo | Bar;
+const fooBar: FooBar[] = [];
+const foundBar: Bar | undefined = fooBar.find<Bar>((t) => t.kind === "bar" && t.bar === "value");
+
 
 //// [arrayFind.js]
 // test fix for #18112, type guard predicates should narrow returned element
@@ -20,3 +27,5 @@ var arrayOfStringsNumbersAndBooleans = ["string", false, 0, "strung", 1, true];
 var foundNumber = arrayOfStringsNumbersAndBooleans.find(isNumber);
 var readonlyArrayOfStringsNumbersAndBooleans = arrayOfStringsNumbersAndBooleans;
 var readonlyFoundNumber = readonlyArrayOfStringsNumbersAndBooleans.find(isNumber);
+var fooBar = [];
+var foundBar = fooBar.find(function (t) { return t.kind === "bar" && t.bar === "value"; });
