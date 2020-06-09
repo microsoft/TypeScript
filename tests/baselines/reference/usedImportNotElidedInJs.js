1e3b8b9b07d9493142a66961e458f600e776a797
Andrew Branch <andrew@wheream.io>
2020-06-09 15:54:28 -0700

    Add baseline

---
+++ tests/baselines/reference/usedImportNotElidedInJs.js
@@ -0,0 +1,10 @@
+//// [test.js]
+import * as moment from 'moment';
+import rollupMoment__default from 'moment';
+export const moment = rollupMoment__default || moment;
+
+
+//// [test.js]
+import * as moment from 'moment';
+import rollupMoment__default from 'moment';
+export const moment = rollupMoment__default || moment;
