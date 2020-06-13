a30a4300257160335b3b1a339fb9eee5dcb42248
kingwl <kingwenlu@gmail.com>
2020-06-13 21:38:16 +0800

    fix crash

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -4361,7 +4361,7 @@ namespace ts {
     }
 
     export function hasEffectiveModifierFlagsFromSymbol(symbol: Symbol, flags: ModifierFlags): boolean {
-        return symbol.declarations?.some(decl => !!getSelectedEffectiveModifierFlags(decl, flags));
+        return !!symbol.declarations?.some(decl => !!getSelectedEffectiveModifierFlags(decl, flags));
     }
 
     function getModifierFlagsWorker(node: Node, includeJSDoc: boolean): ModifierFlags {
