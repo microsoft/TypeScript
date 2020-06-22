78ad1d73fb67dbada9ae94a36a3fe0a0460af2a2
Orta <orta.therox+github@gmail.com>
2020-06-22 15:06:26 -0400

    Update src/harness/fourslashImpl.ts
    Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

--- src/harness/fourslashImpl.ts
+++ src/harness/fourslashImpl.ts
@@ -2487,8 +2487,7 @@ namespace FourSlash {
             const typeIdx = getTokenTypeFromClassification(classification) || 0;
             const modSet = getTokenModifierFromClassification(classification);
 
-            const tokenClassifiction = [tokenTypes[typeIdx], ...tokenModifiers.filter((_, i) => modSet & 1 << i)].join(".");
-            return tokenClassifiction;
+            return [tokenTypes[typeIdx], ...tokenModifiers.filter((_, i) => modSet & 1 << i)].join(".");
         }
 
         private verifyClassifications(expected: { classificationType: string | number, text?: string; textSpan?: TextSpan }[], actual: ts.ClassifiedSpan[], sourceFileText: string) {
