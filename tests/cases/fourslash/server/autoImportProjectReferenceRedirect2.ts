5020ac018e8075b3bc98945438862fc3e08be939
Andrew Branch <andrew@wheream.io>
2020-06-25 15:46:25 -0700

    Add paths test

--- tests/cases/fourslash/server/autoImportProjectReferenceRedirect2.ts
+++ tests/cases/fourslash/server/autoImportProjectReferenceRedirect2.ts
@@ -1,12 +1,7 @@
 /// <reference path="../fourslash.ts" />
 
-// @link: /packages/a -> /node_modules/a
-
 // @Filename: /packages/a/tsconfig.json
 //// { "compilerOptions": { "module": "commonjs", "composite": true, "outDir": "dist", "rootDir": "src" } }
-
-// @Filename: /packages/a/package.json
-//// { "name": "a", "main": "dist/index.js", "typings": "dist/index.d.ts" }
 
 // @Filename: /packages/a/src/index.ts
 //// import "./a";
@@ -15,9 +10,20 @@
 // @Filename: /packages/a/src/a.ts
 //// export class A {}
 
-
 // @Filename: /packages/b/tsconfig.json
-//// { "compilerOptions": { "module": "commonjs", "outDir": "dist", "rootDir": "src" }, "references": [{ "path": "../a" }] }
+//// {
+////   "compilerOptions": {
+////     "module": "commonjs",
+////     "outDir": "dist",
+////     "rootDir": "src",
+////     "baseUrl": ".",
+////     "paths": {
+////       "a": ["../a/src/index"],
+////       "a/*": ["../a/*"]
+////     }
+////   },
+////   "references": [{ "path": "../a" }]
+//// }
 
 // @Filename: /packages/b/src/util.ts
 //// import {} from "a";
