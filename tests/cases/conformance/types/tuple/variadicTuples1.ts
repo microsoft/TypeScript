357946ccbb244f656b4527503fb7924ce7c379d8
Anders Hejlsberg <andersh@microsoft.com>
2020-06-26 13:35:23 -0700

    Add tests

--- tests/cases/conformance/types/tuple/variadicTuples1.ts
+++ tests/cases/conformance/types/tuple/variadicTuples1.ts
@@ -334,3 +334,22 @@ call('hello', 32, (a, b) => 42);
 // Requires [starting-fixed-part, ...rest-part, ending-fixed-part] tuple structure
 
 call(...sa, (...x) => 42);
+
+// No inference to ending optional elements (except with identical structure)
+
+declare function f20<T extends unknown[] = []>(args: [...T, number?]): T;
+
+function f21<U extends string[]>(args: [...U, number?]) {
+    let v1 = f20(args);  // U
+    let v2 = f20(["foo", "bar"]);  // []
+    let v3 = f20(["foo", 42]);  // []
+}
+
+declare function f22<T extends unknown[] = []>(args: [...T, number]): T;
+declare function f22<T extends unknown[] = []>(args: [...T]): T;
+
+function f23<U extends string[]>(args: [...U, number]) {
+    let v1 = f22(args);  // U
+    let v2 = f22(["foo", "bar"]);  // [string, string]
+    let v3 = f22(["foo", 42]);  // [string]
+}
