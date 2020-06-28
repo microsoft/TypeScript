180c9c8277851a3178d0fe17a529afc46f0ecb85
ShuiRuTian <158983297@qq.com>
2020-06-28 18:57:16 +0800

    draft of draft

---
+++ NoNameSpace/index.ts
@@ -0,0 +1,14 @@
+/**
+ * steps to remove namespace
+ * 1. remove namespaces in all ts files in src/*.
+ * 2. use add all missing imports to auto-fix
+ */
+
+ /**
+  * Details about step1:
+  * 1. ts.A.B 
+  * 1. ts.A.B
+  * 2. namespace merge like partial keyword in c#
+  * 3.
+  */
+
