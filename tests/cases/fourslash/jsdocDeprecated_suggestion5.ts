84bad5e676d6d29831c76dd7893f64fc9270ccb7
kingwl <kingwenlu@gmail.com>
2020-07-03 01:20:53 +0800

    Use combined node flags

---
+++ tests/cases/fourslash/jsdocDeprecated_suggestion5.ts
@@ -0,0 +1,46 @@
+// @Filename: a.tsx
+//// /** @deprecated */
+//// type Props = {}
+
+//// /** @deprecated */
+//// const Component = (props: [|Props|]) => props && <div />;
+
+//// <[|Component|] old="old" new="new" />
+
+//// /** @deprecated */
+//// type Options = {}
+
+//// /** @deprecated */
+//// const deprecatedFunction = (options: [|Options|]) => { options }
+
+//// [|deprecatedFunction|]({});
+
+goTo.file('a.tsx')
+const ranges = test.ranges();
+
+verify.getSuggestionDiagnostics([
+    {
+        message: "'Props' is deprecated",
+        code: 6385,
+        range: ranges[0],
+        reportsDeprecated: true,
+    },
+    {
+        message: "'Component' is deprecated",
+        code: 6385,
+        range: ranges[1],
+        reportsDeprecated: true
+    },
+    {
+        message: "'Options' is deprecated",
+        code: 6385,
+        range: ranges[2],
+        reportsDeprecated: true,
+    },
+    {
+        message: "'deprecatedFunction' is deprecated",
+        code: 6385,
+        range: ranges[3],
+        reportsDeprecated: true,
+    }
+])
