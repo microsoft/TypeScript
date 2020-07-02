b5d4013c86fbccc86fc2f4f46448169181cc48c4
ShuiRuTian <158983297@qq.com>
2020-07-02 21:40:22 +0800

    format

--- src/services/findAllReferences.ts
+++ src/services/findAllReferences.ts
@@ -2059,7 +2059,7 @@ namespace ts.FindAllReferences {
                 };
             };
             const allowBaseTypes = (rootSymbol: Symbol) =>
-            !every(search.parents, parent => !explicitlyInheritsFrom(rootSymbol.parent!, parent, state.inheritsFromCache, checker));
+                !every(search.parents, parent => !explicitlyInheritsFrom(rootSymbol.parent!, parent, state.inheritsFromCache, checker));
 
             return forEachRelatedSymbol(referenceSymbol, referenceLocation, checker, /*isForRenamePopulateSearchSymbolSet*/ false,
                 /*onlyIncludeBindingElementAtReferenceLocation*/ state.options.use !== FindReferencesUse.Rename || !!state.options.providePrefixAndSuffixTextForRename,
