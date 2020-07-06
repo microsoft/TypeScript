2d2e7a046f6ac35fa34dbf39d2a500bf21123dd5
ShuiRuTian <158983297@qq.com>
2020-07-06 22:11:05 +0800

    add comment.

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -149,17 +149,16 @@ namespace ts {
      * Iterates through `array` by index and performs the callback on each element of array until the callback
      * returns a falsey value, then returns false.
      * If no such value is found, the callback is applied to each element of array and `true` is returned.
+     * If `array` is undefined, return false.
      */
     export function every<T>(array: readonly T[] | undefined, callback: (element: T, index: number) => boolean): boolean {
-        if (array) {
-            for (let i = 0; i < array.length; i++) {
-                if (!callback(array[i], i)) {
-                    return false;
-                }
-            }
-        }
-        if(!array){
+        if (!array) {
             return false;
+        }
+        for (let i = 0; i < array.length; i++) {
+            if (!callback(array[i], i)) {
+                return false;
+            }
         }
         return true;
     }
