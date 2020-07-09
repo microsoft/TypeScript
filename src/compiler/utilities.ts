52372c36f113a7099ae8b03cb91b49647883d6e0
Alexander T <alexander.tarasyuk@outlook.com>
2020-07-09 20:24:21 +0300

    fix(39524): convert access property suggestion to string

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -4755,7 +4755,7 @@ namespace ts {
         if (isPropertyAccessExpression(expr)) {
             const baseStr = tryGetPropertyAccessOrIdentifierToString(expr.expression);
             if (baseStr !== undefined) {
-                return baseStr + "." + expr.name;
+                return baseStr + "." + entityNameToString(expr.name);
             }
         }
         else if (isIdentifier(expr)) {
