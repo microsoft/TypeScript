2eb311132e75a9a6a0ef9a0f03f050983bedc257
Anders Hejlsberg <andersh@microsoft.com>
2020-07-13 15:02:04 -0700

    Check assignable-to before definitely-assignable-to

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -15013,7 +15013,7 @@ namespace ts {
         }
 
         function isTypeDefinitelyAssignableTo(source: Type, target: Type): boolean {
-            return isTypeRelatedTo(source, target, definitelyAssignableRelation);
+            return isTypeAssignableTo(source, target) && isTypeRelatedTo(source, target, definitelyAssignableRelation);
         }
 
         // An object type S is considered to be derived from an object type T if
