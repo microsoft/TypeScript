40e993867447088ae3415c501cea650812918482
Sheetal Nandi <shkamat@microsoft.com>
2020-07-24 13:30:35 -0700

    Handle unresolved baseType when trying to get completions for static member Fixes  #38067

--- src/services/completions.ts
+++ src/services/completions.ts
@@ -2012,7 +2012,10 @@ namespace ts.Completions {
                 // List of property symbols of base type that are not private and already implemented
                 const baseSymbols = flatMap(getAllSuperTypeNodes(decl), baseTypeNode => {
                     const type = typeChecker.getTypeAtLocation(baseTypeNode);
-                    return type && typeChecker.getPropertiesOfType(classElementModifierFlags & ModifierFlags.Static ? typeChecker.getTypeOfSymbolAtLocation(type.symbol, decl) : type);
+                    return type &&
+                        classElementModifierFlags & ModifierFlags.Static ?
+                        type.symbol && typeChecker.getPropertiesOfType(typeChecker.getTypeOfSymbolAtLocation(type.symbol, decl)) :
+                        typeChecker.getPropertiesOfType(type);
                 });
                 symbols = filterClassMembersList(baseSymbols, decl.members, classElementModifierFlags);
             }
