c89574cdf1846690e8362e83f4b3e06f13d2cc2b
kingwl <kingwenlu@gmail.com>
2020-07-27 16:04:26 +0800

    add baseline

---
+++ tests/baselines/reference/seeTag1.js
@@ -0,0 +1,38 @@
+//// [seeTag1.ts]
+interface Foo {
+    foo: string
+}
+
+namespace NS {
+    export interface Bar {
+        baz: Foo
+    }
+}
+
+/** @see {Foo} foooo*/
+const a = ""
+
+/** @see {NS.Bar} ns.bar*/
+const b = ""
+
+
+//// [seeTag1.js]
+/** @see {Foo} foooo*/
+var a = "";
+/** @see {NS.Bar} ns.bar*/
+var b = "";
+
+
+//// [seeTag1.d.ts]
+interface Foo {
+    foo: string;
+}
+declare namespace NS {
+    interface Bar {
+        baz: Foo;
+    }
+}
+/** @see {Foo} foooo*/
+declare const a = "";
+/** @see {NS.Bar} ns.bar*/
+declare const b = "";
