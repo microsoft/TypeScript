8d85576d748384286d27f800f1a25f5dcb94ddcb
Andrew Branch <andrew@wheream.io>
2020-07-31 15:53:37 -0700

    Fix other test

--- src/testRunner/unittests/tsserver/typingsInstaller.ts
+++ src/testRunner/unittests/tsserver/typingsInstaller.ts
@@ -130,7 +130,7 @@ namespace ts.projectSystem {
             })();
 
             const projectService = createProjectService(host, { useSingleInferredProject: true, typingsInstaller: installer });
-            projectService.setHostConfiguration({ preferences: { includePackageJsonAutoImports: "none" } });
+            projectService.setHostConfiguration({ preferences: { includePackageJsonAutoImports: "off" } });
             projectService.openClientFile(file1.path);
 
             checkNumberOfProjects(projectService, { configuredProjects: 1 });
