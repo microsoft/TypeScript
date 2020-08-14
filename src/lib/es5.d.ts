d3d867e59509fae76c53868741ef609df006e9e2
Ariel Sosnovsky <asosnovsky@cam-c02x11a4htd.ca.deloitte.com>
2020-08-14 16:44:34 -0400

    adjusted typing for Object.keys

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -238,7 +238,7 @@ interface ObjectConstructor {
      * Returns the names of the enumerable string properties and methods of an object.
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    keys(o: object): string[];
+    keys<O extends object = object, K extends keyof O = keyof O>(o: O): K[];
 }
 
 /**
