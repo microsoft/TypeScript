f3e89375924c9b25e7355a2c955dc00116591c00
Kyle Hensel <k.y.l.e@outlook.co.nz>
2020-08-14 20:42:58 +1200

    don't emit unused ts-expect-error in unchecked js files

--- src/compiler/program.ts
+++ src/compiler/program.ts
@@ -1757,9 +1757,17 @@ namespace ts {
                 return flatDiagnostics;
             }
 
+            const isCheckJs = isCheckJsEnabledForFile(sourceFile, options);
+            const isJs = sourceFile.scriptKind === ScriptKind.JS || sourceFile.scriptKind === ScriptKind.JSX;
+            const isTsNoCheck = sourceFile.checkJsDirective?.enabled === false;
+
             const { diagnostics, directives } = getDiagnosticsWithPrecedingDirectives(sourceFile, sourceFile.commentDirectives, flatDiagnostics);
 
             for (const errorExpectation of directives.getUnusedExpectations()) {
+                // don't report "unused ts-expect-error" in JS files that aren't being type-checked
+                // and also not in files with // @ts-nocheck
+                if (isTsNoCheck || (isJs && !isCheckJs)) continue;
+
                 diagnostics.push(createDiagnosticForRange(sourceFile, errorExpectation.range, Diagnostics.Unused_ts_expect_error_directive));
             }
 
