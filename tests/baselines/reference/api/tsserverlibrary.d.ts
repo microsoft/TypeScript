dd7dbe0e87ca743820ec2a0d419eba429c7d8e82
Andrew Branch <andrew@wheream.io>
2020-09-01 15:25:19 -0700

    Baseline

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -5965,7 +5965,7 @@ declare namespace ts {
         isGlobalCompletion: boolean;
         isMemberCompletion: boolean;
         /**
-         * In the absence of `CompletionEntry["replacementSpan"]`, the editor may choose whether to use
+         * In the absence of `CompletionEntry["replacementSpan"], the editor may choose whether to use
          * this span or its default one. If `CompletionEntry["replacementSpan"]` is defined, that span
          * must be used to commit that completion entry.
          */
@@ -8109,7 +8109,7 @@ declare namespace ts.server.protocol {
         readonly isMemberCompletion: boolean;
         readonly isNewIdentifierLocation: boolean;
         /**
-         * In the absence of `CompletionEntry["replacementSpan"], the editor may choose whether to use
+         * In the absence of `CompletionEntry["replacementSpan"]`, the editor may choose whether to use
          * this span or its default one. If `CompletionEntry["replacementSpan"]` is defined, that span
          * must be used to commit that completion entry.
          */
