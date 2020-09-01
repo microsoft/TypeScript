96ec48ddb17f29b36992b93dd7c967ac8f91cf4e
kingwl <kingwenlu@gmail.com>
2020-09-01 16:56:36 +0800

    wip

--- tests/cases/fourslash/jsDocSee3.ts
+++ tests/cases/fourslash/jsDocSee3.ts
@@ -1,14 +1,12 @@
 ///<reference path="fourslash.ts" />
 
-//// interface [|/*def1*/A|] { }
-//// namespace [|/*def2*/A|] { }
-//// const [|/*def3*/A|] = 1;
-
-//// /**
-////  * @see {/*use1*/[|A|]}
-////  */
-//// const v = 2;
-
+//// function foo ([|/*def1*/a|]: string) {
+////     /**
+////      * @see {/*use1*/[|a|]}
+////      */
+////     function bar (a: string) {
+////     }
+//// }
 
 goTo.marker("use1");
-verify.goToDefinitionIs(["def1", "def2", "def3"]);
+verify.goToDefinitionIs(["def1"]);
