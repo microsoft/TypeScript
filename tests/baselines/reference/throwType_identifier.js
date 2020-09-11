daac7e799964bdb14c4e227a71915c35fc061ead
Jack Works <jackworks@protonmail.com>
2020-09-11 17:54:37 +0800

    test: add test for identifier use case

---
+++ tests/baselines/reference/throwType_identifier.js
@@ -0,0 +1,9 @@
+//// [throwType_identifier.ts]
+declare const name: throw 'please use window.name'
+console.log(name, 123, window.name)
+export {}
+
+//// [throwType_identifier.js]
+"use strict";
+exports.__esModule = true;
+console.log(name, 123, window.name);
---
+++ tests/cases/compiler/throwType_identifier.ts
@@ -0,0 +1,3 @@
+declare const name: throw 'please use window.name'
+console.log(name, 123, window.name)
+export {}
