10b240cde3ab3bfe95fa09275760389c082a4a80
Wesley Wigham <wewigham@microsoft.com>
2020-09-22 21:21:13 -0700

    Allow an infer type node to resolve its own name (#40483)

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -1954,6 +1954,15 @@ namespace ts {
                             }
                         }
                         break;
+                    case SyntaxKind.InferType:
+                        if (meaning & SymbolFlags.TypeParameter) {
+                            const parameterName = (<InferTypeNode>location).typeParameter.name;
+                            if (parameterName && name === parameterName.escapedText) {
+                                result = (<InferTypeNode>location).typeParameter.symbol;
+                                break loop;
+                            }
+                        }
+                        break;
                 }
                 if (isSelfReferenceLocation(location)) {
                     lastSelfReferenceLocation = location;
