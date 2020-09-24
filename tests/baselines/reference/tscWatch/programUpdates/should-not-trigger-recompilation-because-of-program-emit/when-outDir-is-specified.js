4f9713c51579d0a5a5d0ce35b8a205eac6041726
Sheetal Nandi <shkamat@microsoft.com>
2020-09-24 13:08:57 -0700

    Fix unnecessary new lines in output represented incorretly in the baseline (#40751)

--- tests/baselines/reference/tscWatch/programUpdates/should-not-trigger-recompilation-because-of-program-emit/when-outDir-is-specified.js
+++ tests/baselines/reference/tscWatch/programUpdates/should-not-trigger-recompilation-because-of-program-emit/when-outDir-is-specified.js
@@ -97,7 +97,6 @@ Output::
 >> Screen clear
 [[90m12:00:25 AM[0m] Starting compilation in watch mode...
 
-
 [[90m12:00:36 AM[0m] Found 0 errors. Watching for file changes.
 
 
@@ -194,7 +193,6 @@ export const y = 10;
 Output::
 >> Screen clear
 [[90m12:00:39 AM[0m] File change detected. Starting incremental compilation...
-
 
 [[90m12:00:42 AM[0m] Found 0 errors. Watching for file changes.
 
