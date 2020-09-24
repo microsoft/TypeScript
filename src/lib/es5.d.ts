bcd4ce80e4a60a1ce23d8eef27844e01b253524c
jieniu$ <jienius@outlook.com>
2020-09-23 18:29:03 -0600

    Fix typo in JSON.stringify doc

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1047,7 +1047,7 @@ interface JSON {
     /**
      * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
      * @param value A JavaScript value, usually an object or array, to be converted.
-     * @param replacer An array of strings and numbers that acts as a approved list for selecting the object properties that will be stringified.
+     * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
     stringify(value: any, replacer?: (number | string)[] | null, space?: string | number): string;
