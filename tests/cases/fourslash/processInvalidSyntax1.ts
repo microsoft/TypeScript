7e131e19bde5249a2242ead68d8a4f859875f33a
Andrew Branch <andrew@wheream.io>
2020-09-25 14:57:09 -0700

    Add test

---
+++ tests/cases/fourslash/processInvalidSyntax1.ts
@@ -0,0 +1,13 @@
+/// <reference path="fourslash.ts" />
+
+// @allowJs: true
+
+// @Filename: unicode.js
+//// obj.𝒜 ;
+
+// @Filename: forof.js
+//// for (obj/**/.prop of arr) {
+//// 
+//// }
+
+verify.baselineRename("", {});
