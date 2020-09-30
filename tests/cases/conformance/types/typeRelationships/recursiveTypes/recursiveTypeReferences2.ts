b93d96afa60b4c641748167cf8d205b0a48840f0
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-09-30 16:48:18 -0700

    add test case from #40234

--- tests/cases/conformance/types/typeRelationships/recursiveTypes/recursiveTypeReferences2.ts
+++ tests/cases/conformance/types/typeRelationships/recursiveTypes/recursiveTypeReferences2.ts
@@ -5,3 +5,25 @@
 /** @typedef {ReadonlyArray<Json>} JsonArray */
 /** @typedef {{ readonly [key: string]: Json }} JsonRecord */
 /** @typedef {boolean | number | string | null | JsonRecord | JsonArray | readonly []} Json */
+
+/**
+ * @template T
+ * @typedef {{
+  $A: {
+    [K in keyof T]?: XMLObject<T[K]>[]
+  },
+  $O: {
+    [K in keyof T]?: {
+      $$?: Record<string, string>
+    } & (T[K] extends string ? {$:string} : XMLObject<T[K]>)
+  },
+  $$?: Record<string, string>,
+  } & {
+  [K in keyof T]?: (
+    T[K] extends string ? string
+      : XMLObject<T[K]>
+  )
+}} XMLObject<T> */
+
+/** @type {XMLObject<{foo:string}>} */
+const p = {};
