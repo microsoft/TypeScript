64ce9c564a2593a4c7f92c07054a65b22e0ad590
Matt Bidewell <matthewbidewell@gmail.com>
2020-10-01 20:04:08 +0100

    optional input

--- lib/lib.es2020.bigint.d.ts
+++ lib/lib.es2020.bigint.d.ts
@@ -35,7 +35,7 @@ interface BigInt {
 }
 
 interface BigIntConstructor {
-    (value: string | number | bigint | boolean | object): bigint;
+    (value?: string | number | bigint | boolean | object): bigint;
     readonly prototype: BigInt;
 
     /**
