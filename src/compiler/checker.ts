449cdd70b496da97fdeab1be0bdd5459a0c63444
Anders Hejlsberg <andersh@microsoft.com>
2020-10-01 11:50:39 -0700

    Propagate wildcard types in template literal type construction

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -13552,6 +13552,9 @@ namespace ts {
                     mapType(types[unionIndex], t => getTemplateLiteralType(texts, replaceElement(types, unionIndex, t))) :
                     errorType;
             }
+            if (contains(types, wildcardType)) {
+                return wildcardType;
+            }
             const newTypes: Type[] = [];
             const newTexts: string[] = [];
             let text = texts[0];
