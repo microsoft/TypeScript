440892caadd1bab75032f854c3ad623e0c810539
Wesley Wigham <wwigham@gmail.com>
2020-10-01 03:20:46 -0700

    Add extensions to jsx automatically added import, per babel change

--- tests/baselines/reference/jsxJsxsCjsTransformChildren(jsx=react-jsxdev).js
+++ tests/baselines/reference/jsxJsxsCjsTransformChildren(jsx=react-jsxdev).js
@@ -8,7 +8,7 @@ export {};
 //// [jsxJsxsCjsTransformChildren.js]
 "use strict";
 exports.__esModule = true;
-var jsx_dev_runtime_1 = require("react/jsx-dev-runtime");
+var jsx_dev_runtime_js_1 = require("react/jsx-dev-runtime.js");
 var _jsxFileName = "tests/cases/conformance/jsx/jsxs/jsxJsxsCjsTransformChildren.tsx";
 /// <reference path="react16.d.ts" />
-var a = jsx_dev_runtime_1.jsxDEV("div", { children: "text" }, void 0, false, { fileName: _jsxFileName, lineNumber: 2, columnNumber: 10 }, this);
+var a = jsx_dev_runtime_js_1.jsxDEV("div", { children: "text" }, void 0, false, { fileName: _jsxFileName, lineNumber: 2, columnNumber: 10 }, this);
