918edf6cb095f5d5f47cd0d13e186b0b55c31a31
Alexander T <alexander.tarasyuk@outlook.com>
2020-10-03 00:04:33 +0300

    fix(40901): skip checking custom arguments name in a constructor

---
+++ tests/baselines/reference/argumentsReferenceInMethod3_Js.js
@@ -0,0 +1,47 @@
+//// [a.js]
+class A {
+	get arguments() {
+		return { bar: {} };
+	}
+}
+
+class B extends A {
+	/**
+	 * @param {object} [foo={}]
+	 */
+	m(foo = {}) {
+		/**
+		 * @type object
+		 */
+		this.x = foo;
+
+		/**
+		 * @type object
+		 */
+		this.y = super.arguments.bar;
+	}
+}
+
+
+
+
+//// [a.d.ts]
+declare class A {
+    get arguments(): {
+        bar: {};
+    };
+}
+declare class B extends A {
+    /**
+     * @param {object} [foo={}]
+     */
+    m(foo?: object): void;
+    /**
+     * @type object
+     */
+    x: object;
+    /**
+     * @type object
+     */
+    y: object;
+}
