197a67bf43079ec2af7f86587f6cea163b2fb8a9
Richmond Liew <richmond.liew.97@gmail.com>
2020-10-06 16:36:44 -0400

    Fixes #35735

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -18053,7 +18053,7 @@ namespace ts {
                 const requireOptionalProperties = (relation === subtypeRelation || relation === strictSubtypeRelation) && !isObjectLiteralType(source) && !isEmptyArrayLiteralType(source) && !isTupleType(source);
                 const unmatchedProperty = getUnmatchedProperty(source, target, requireOptionalProperties, /*matchDiscriminantProperties*/ false);
                 if (unmatchedProperty) {
-                    if (reportErrors) {
+                    if (reportErrors && !typeOnlyHasCallOrConstructSignatures(source)) {
                         reportUnmatchedProperty(source, target, unmatchedProperty, requireOptionalProperties);
                     }
                     return Ternary.False;
@@ -18207,6 +18207,16 @@ namespace ts {
                     }
                 }
                 return result;
+            }
+
+            function typeOnlyHasCallOrConstructSignatures(type: Type): boolean {
+                const typeCallSignatures = getSignaturesOfStructuredType(type, SignatureKind.Call);
+                const typeConstructSignatures = getSignaturesOfStructuredType(type, SignatureKind.Construct);
+                const typeProperties = getPropertiesOfObjectType(type);
+                if ((typeCallSignatures.length || typeConstructSignatures.length) && !typeProperties.length) {
+                    return true;
+                }
+                return false;
             }
 
             function reportIncompatibleCallSignatureReturn(siga: Signature, sigb: Signature) {
@@ -18390,7 +18400,7 @@ namespace ts {
                 findBestTypeForInvokable(source, target) ||
                 findMostOverlappyType(source, target);
         }
-
+        
         function discriminateTypeByDiscriminableItems(target: UnionType, discriminators: [() => Type, __String][], related: (source: Type, target: Type) => boolean | Ternary, defaultValue?: undefined, skipPartial?: boolean): Type | undefined;
         function discriminateTypeByDiscriminableItems(target: UnionType, discriminators: [() => Type, __String][], related: (source: Type, target: Type) => boolean | Ternary, defaultValue: Type, skipPartial?: boolean): Type;
         function discriminateTypeByDiscriminableItems(target: UnionType, discriminators: [() => Type, __String][], related: (source: Type, target: Type) => boolean | Ternary, defaultValue?: Type, skipPartial?: boolean) {
