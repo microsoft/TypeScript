b31be85169578d416b9c3ebfd51930d54feec397
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2020-10-09 15:25:23 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/propertyElementAccessOnVoidNoImplicitAnyFalse01.js
@@ -0,0 +1,26 @@
+//// [propertyElementAccessOnVoidNoImplicitAnyFalse01.ts]
+let result = [1, 2, 3, 4].forEach(x => {
+    console.log(x);
+});
+
+if (Math.random()) {
+    result["hello"] = "foo";
+    result.world = "foo";
+}
+else {
+    console.log(result["hello"]);
+    console.log(result.world);
+}
+
+//// [propertyElementAccessOnVoidNoImplicitAnyFalse01.js]
+var result = [1, 2, 3, 4].forEach(function (x) {
+    console.log(x);
+});
+if (Math.random()) {
+    result["hello"] = "foo";
+    result.world = "foo";
+}
+else {
+    console.log(result["hello"]);
+    console.log(result.world);
+}
