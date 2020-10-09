3252050cc2ea95ec9c1ede39823224722a58cf94
Omar Diab <me@omardiab.com>
2020-10-09 20:38:42 +0900

    use generics in map constructor

--- src/lib/es2015.collection.d.ts
+++ src/lib/es2015.collection.d.ts
@@ -9,8 +9,7 @@ interface Map<K, V> {
 }
 
 interface MapConstructor {
-    new(): Map<any, any>;
-    new<K, V>(entries?: readonly (readonly [K, V])[] | null): Map<K, V>;
+    new<K = any, V = any>(entries?: ReadonlyArray<[K, V]> | null): Map<K, V>;
     readonly prototype: Map<any, any>;
 }
 declare var Map: MapConstructor;
