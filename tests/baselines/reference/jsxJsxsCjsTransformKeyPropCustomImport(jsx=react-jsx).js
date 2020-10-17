43433eb2f1b1269da6cae855f259d00b742c37cf
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-10-17 15:20:39 +0200

    Add tests related to the key prop when using new JSX transform

---
+++ tests/baselines/reference/jsxJsxsCjsTransformKeyPropCustomImport(jsx=react-jsx).js
@@ -0,0 +1,28 @@
+//// [jsxJsxsCjsTransformKeyPropCustomImport.tsx]
+/// <reference path="/.lib/react16.d.ts" />
+const props = { answer: 42 }
+const a = <div key="foo" {...props}>text</div>;
+const b = <div {...props} key="bar">text</div>;
+
+export {};
+
+
+//// [jsxJsxsCjsTransformKeyPropCustomImport.js]
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+exports.__esModule = true;
+var jsx_runtime_js_1 = require("preact/jsx-runtime.js");
+/// <reference path="react16.d.ts" />
+var props = { answer: 42 };
+var a = jsx_runtime_js_1.jsx("div", __assign({}, props, { children: "text" }), "foo");
+var b = React.createElement("div", __assign({}, props, { key: "bar" }), "text");
