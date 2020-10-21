6dde1621cb0f113d4a2006fa17861e2c19661405
Wesley Wigham <wwigham@gmail.com>
2020-10-21 12:31:29 -0700

    Track control flow for comma expressions in call expressions

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -20720,7 +20720,8 @@ namespace ts {
                 case SyntaxKind.NonNullExpression:
                     return isMatchingReference(source, (target as NonNullExpression | ParenthesizedExpression).expression);
                 case SyntaxKind.BinaryExpression:
-                    return isAssignmentExpression(target) && isMatchingReference(source, target.left);
+                    return (isAssignmentExpression(target) && isMatchingReference(source, target.left)) ||
+                        (isBinaryExpression(target) && target.operatorToken.kind === SyntaxKind.CommaToken && isMatchingReference(source, target.right));
             }
             switch (source.kind) {
                 case SyntaxKind.Identifier:
