8a152915415b943feea9f1f7699f3e52207b2963
Ron Buckton <ron.buckton@microsoft.com>
2020-10-28 16:30:48 -0700

    Update control flow for correct destructuring evaluation order

---
+++ tests/cases/conformance/controlFlow/controlFlowAssignmentPatternOrder.ts
@@ -0,0 +1,28 @@
+// @target: esnext
+// @noEmit: true
+
+// https://github.com/microsoft/TypeScript/pull/41094#issuecomment-716044363
+{
+    let a: 0 | 1 = 0;
+    let b: 0 | 1 | 9;
+    [{ [(a = 1)]: b } = [9, a] as const] = [];
+    const bb: 0 = b;
+}
+{
+    let a: 0 | 1 = 1;
+    let b: 0 | 1 | 9;
+    [{ [a]: b } = [9, a = 0] as const] = [];
+    const bb: 9 = b;
+}
+{
+    let a: 0 | 1 = 0;
+    let b: 0 | 1 | 8 | 9;
+    [{ [(a = 1)]: b } = [9, a] as const] = [[9, 8] as const];
+    const bb: 0 | 8 = b;
+}
+{
+    let a: 0 | 1 = 1;
+    let b: 0 | 1 | 8 | 9;
+    [{ [a]: b } = [a = 0, 9] as const] = [[8, 9] as const];
+    const bb: 0 | 8 = b;
+}
