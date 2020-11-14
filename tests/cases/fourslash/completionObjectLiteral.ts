78bd6a6f609141f0677d9bae4d693f447f117f49
orange4glace <orange4glace@gmail.com>
2020-11-14 12:12:29 +0900

    fix: fourslash

--- tests/cases/fourslash/completionObjectLiteral.ts
+++ tests/cases/fourslash/completionObjectLiteral.ts
@@ -1,20 +1,20 @@
 /// <reference path="fourslash.ts"/>
 
-const variableOne = 1;
-const variableTwo = 2;
-const objAny: any = {
-  v/*1*/
-}
-const objNone = {
-  v/*2*/
-}
+//// const variableOne = 1;
+//// const variableTwo = 2;
+//// const objAny: any = {
+////   v/*1*/
+//// }
+//// const objNone = {
+////   v/*2*/
+//// }
 
-interface Typed {
-  variableThree: number;
-}
-const typed: Typed = {
-  v/*3*/
-}
+//// interface Typed {
+////   variableThree: number;
+//// }
+//// const typed: Typed = {
+////   v/*3*/
+//// }
 
 verify.completions(
     { marker: ["1", "2"], includes: ["variableOne", "variableTwo"]},
