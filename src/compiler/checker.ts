b58054077bbdd9bca1739731990ed454500e5aa4
Andrew Branch <andrew@wheream.io>
2020-11-19 16:29:23 -0800

    Make bad error talk words more good for Daniel. Fixes #41603

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -36544,7 +36544,7 @@ namespace ts {
                 if (canConvertImportDeclarationToTypeOnly(statement) || canConvertImportEqualsDeclarationToTypeOnly(statement)) {
                     error(
                         statement,
-                        Diagnostics.This_import_is_never_used_as_a_value_and_must_use_import_type_because_the_importsNotUsedAsValues_is_set_to_error);
+                        Diagnostics.This_import_is_never_used_as_a_value_and_must_use_import_type_because_importsNotUsedAsValues_is_set_to_error);
                 }
             }
         }
