3a903b8a283910ca6fdcc4989a0d4b0311b137fb
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2020-11-26 21:19:15 +0200

    fix(41653): convert arrow function to function expression that used as destructure initializer

---
+++ tests/baselines/reference/destructuringObjectBindingPatternAndAssignment6(target=esnext).js
@@ -0,0 +1,17 @@
+//// [destructuringObjectBindingPatternAndAssignment6.ts]
+const a = "a";
+const b = "b";
+
+const { [a]: aVal, [b]: bVal } = (() => {
+	return { [a]: 1, [b]: 1 };
+})();
+console.log(aVal, bVal);
+
+
+//// [destructuringObjectBindingPatternAndAssignment6.js]
+const a = "a";
+const b = "b";
+const { [a]: aVal, [b]: bVal } = (() => {
+    return { [a]: 1, [b]: 1 };
+})();
+console.log(aVal, bVal);
