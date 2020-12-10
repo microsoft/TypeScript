035c7ca90504dd31a8a0f65659c366ba0e3e8cc7
Andrew Branch <andrewbranch@users.noreply.github.com>
2020-12-10 10:17:28 -0800

    Elide export assignment when it does not resolve to a value (#41904)
    * Only mark aliases that resolve to values referenced
    
    * Update other affected baselines
    
    * Remove redundant check

--- tests/baselines/reference/tsbuild/demo/initial-build/in-master-branch-with-everything-setup-correctly-and-reports-no-error.js
+++ tests/baselines/reference/tsbuild/demo/initial-build/in-master-branch-with-everything-setup-correctly-and-reports-no-error.js
@@ -215,7 +215,6 @@ export { createDog, Dog };
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createDog = void 0;
-var animal_1 = require("./animal");
 var dog_1 = require("./dog");
 Object.defineProperty(exports, "createDog", { enumerable: true, get: function () { return dog_1.createDog; } });
 
--- tests/baselines/reference/tsbuild/watchMode/demo/updates-with-circular-reference.js
+++ tests/baselines/reference/tsbuild/watchMode/demo/updates-with-circular-reference.js
@@ -387,7 +387,6 @@ export default interface Animal {
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createDog = void 0;
-var animal_1 = require("./animal");
 var dog_1 = require("./dog");
 Object.defineProperty(exports, "createDog", { enumerable: true, get: function () { return dog_1.createDog; } });
 
