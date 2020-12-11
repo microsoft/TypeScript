5abc3631a9d38dbca19b0914c2ebf157b50885ae
Chris West (Faux) <git@goeswhere.com>
2020-12-11 11:46:36 +0000

    chore: failing test for const enums and isolatedModules

---
+++ tests/baselines/reference/isolatedModulesImportConstEnum.js
@@ -0,0 +1,24 @@
+//// [tests/cases/compiler/isolatedModulesImportConstEnum.ts] ////
+
+//// [file1.ts]
+import { Foo } from './file2';
+console.log(Foo.BAR);
+
+//// [file2.ts]
+export const enum Foo {
+  BAR,
+}
+
+
+//// [file2.js]
+"use strict";
+exports.__esModule = true;
+exports.Foo = void 0;
+var Foo;
+(function (Foo) {
+    Foo[Foo["BAR"] = 0] = "BAR";
+})(Foo = exports.Foo || (exports.Foo = {}));
+//// [file1.js]
+"use strict";
+exports.__esModule = true;
+console.log(file2_1.Foo.BAR);
