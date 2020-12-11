3842657cbab14aa078a5a7a0f3a38cf66768ccdc
Andrew Branch <andrew@wheream.io>
2020-12-11 14:44:39 -0800

    Add test for <reference types="..." />

--- tests/cases/fourslash/findAllReferencesTripleSlash.ts
+++ tests/cases/fourslash/findAllReferencesTripleSlash.ts
@@ -2,13 +2,18 @@
 
 // @checkJs: true
 
+// @Filename: /node_modules/@types/globals/index.d.ts
+//// declare const someAmbientGlobal: unknown;
+
 // @Filename: /a.ts
-//// /// <reference path="b.ts/**/" />
+//// /// <reference path="b.ts/*1*/" />
+//// /// <reference types="globals/*2*/" />
 
 // @Filename: /b.ts
 //// console.log("b.ts");
 
 // @Filename: /c.js
 //// require("./b");
+//// require("globals");
 
-verify.baselineFindAllReferences("");
+verify.baselineFindAllReferences("1", "2");
