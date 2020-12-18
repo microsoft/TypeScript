718b3d445b3674362f9fb126cb66a901751f3b44
Anders Hejlsberg <andersh@microsoft.com>
2020-12-18 07:27:37 -1000

    Add regression test

---
+++ tests/baselines/reference/inferenceFromIncompleteSource.js
@@ -0,0 +1,18 @@
+//// [inferenceFromIncompleteSource.ts]
+// Repro from #42030
+
+interface ListProps<T, K extends keyof T> {
+  items: T[];
+  itemKey: K;
+  prop: number;
+}
+
+declare const Component: <T, K extends keyof T>(x: ListProps<T, K>) => void;
+
+Component({items: [{name:' string'}], itemKey: 'name' });
+
+
+//// [inferenceFromIncompleteSource.js]
+"use strict";
+// Repro from #42030
+Component({ items: [{ name: ' string' }], itemKey: 'name' });
