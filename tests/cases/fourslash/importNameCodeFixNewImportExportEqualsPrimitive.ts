570493f845d2f39a9ab60ea38bdbc28cf505d514
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2020-12-19 12:51:59 +0200

    use codeFixAvailable instead of codeFix

--- tests/cases/fourslash/importNameCodeFixNewImportExportEqualsPrimitive.ts
+++ tests/cases/fourslash/importNameCodeFixNewImportExportEqualsPrimitive.ts
@@ -6,14 +6,6 @@
 ////declare var x: number;
 ////export = x;
 
-verify.codeFix({
-    index: 0,
-    description: [ts.Diagnostics.Add_missing_function_declaration_0.message, "valueOf"],
-    newFileContent:
-`valueOf();
-
-function valueOf() {
-    throw new Error("Function not implemented.");
-}
-`
-});
+verify.codeFixAvailable([
+    { description: "Add missing function declaration 'valueOf'" }
+]);
--- tests/cases/fourslash/incompleteFunctionCallCodefix3.ts
+++ tests/cases/fourslash/incompleteFunctionCallCodefix3.ts
@@ -3,14 +3,6 @@
 // @noImplicitAny: true
 //// function ...q) {}} f(10);
 
-verify.codeFix({
-    index: 0,
-    description: [ts.Diagnostics.Add_missing_function_declaration_0.message, "f"],
-    newFileContent:
-`function ...q) {}} f(10);
-
-function f(arg0: number) {
-    throw new Error("Function not implemented.");
-}
-`
-});
+verify.codeFixAvailable([
+    { description: "Add missing function declaration 'f'" }
+]);
