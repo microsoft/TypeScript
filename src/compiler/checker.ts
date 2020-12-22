3442151f9d579e1d73a9719706a723ad84ffb671
Song Gao <158983297@qq.com>
2020-12-22 17:16:30 +0800

    completion for return const assertion. (#41475)

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -24423,10 +24423,7 @@ namespace ts {
             return undefined;
 
             function tryFindWhenConstTypeReference(node: Expression) {
-                if(isCallLikeExpression(node.parent)){
-                    return getContextualTypeForArgument(node.parent, node);
-                }
-                return undefined;
+                return getContextualType(node);
             }
         }
 
