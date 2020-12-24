da8c72ed21b76a1abb54e80a5f34f72492eb6221
Luke Bennett <LukeeeeBennettPlus@gmail.com>
2020-12-24 14:39:30 +0000

    test: accept additional baselines for opt chain

--- tests/baselines/reference/noPropertyAccessFromIndexSignature1.js
+++ tests/baselines/reference/noPropertyAccessFromIndexSignature1.js
@@ -56,8 +56,8 @@ c["foo"];
 c.bar;
 c["bar"];
 // optional access property
-d === null || d === void 0 ? void 0 : d.foo;
-d === null || d === void 0 ? void 0 : d["foo"];
+d !== null && d !== void 0 && d.foo;
+d !== null && d !== void 0 && d["foo"];
 // optional access index signature
-d === null || d === void 0 ? void 0 : d.bar;
-d === null || d === void 0 ? void 0 : d["bar"];
+d !== null && d !== void 0 && d.bar;
+d !== null && d !== void 0 && d["bar"];
--- tests/baselines/reference/optionalChainingInArrow.js
+++ tests/baselines/reference/optionalChainingInArrow.js
@@ -7,7 +7,10 @@ const test = (names: string[]) =>
 
 //// [optionalChainingInArrow.js]
 // https://github.com/microsoft/TypeScript/issues/41814
-var test = function (names) {
-    // single-line comment
-    return names === null || names === void 0 ? void 0 : names.filter(function (x) { return x; });
-};
+var test = function (names) { return 
+// single-line comment
+names === null || 
+// single-line comment
+names === void 0 ? void 0 : 
+// single-line comment
+names.filter(function (x) { return x; }); };
