ff132566b0eb9bffabce66d75b00b93e2f053de4
Andrew Branch <andrew@wheream.io>
2021-01-14 13:55:06 -0800

    Add failing test

---
+++ tests/cases/fourslash/formatSelectionSingleProperty.ts
@@ -0,0 +1,17 @@
+/// <reference path="fourslash.ts" />
+
+//// console.log({
+//// }, {
+//// /*1*/    a: 1,
+//// /*2*/    b: 2
+//// })
+
+format.selection("1", "2");
+verify.currentFileContentIs(
+`console.log({
+}, {
+    a: 1,
+    b: 2
+})`);
+
+// verify.formatDocumentChangesNothing();
