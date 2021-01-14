7e0aa9898d6c141034b176f1578227d6a2f199d8
pvarga-dni <58729567+pvarga-dni@users.noreply.github.com>
2021-01-14 22:12:47 +0000

    Update es5.d.ts

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -524,7 +524,7 @@ interface Number {
      * Returns a string representation of an object.
      * @param radix Specifies a radix for converting numeric values to strings. This value is only used for numbers.
      */
-    toString(radix?: number): string;
+    toString(radix?: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36): string;
 
     /**
      * Returns a string representing a number in fixed-point notation.
