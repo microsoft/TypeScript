71d4ee5d605110fe4f761c27cfcb592add69e9af
Wesley Wigham <wewigham@microsoft.com>
2021-01-21 13:06:56 -0800

    Preserve alias information where possible when looking up union and intersection base constraints (#42430)

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -11209,11 +11209,21 @@ namespace ts {
                 if (t.flags & TypeFlags.UnionOrIntersection) {
                     const types = (<UnionOrIntersectionType>t).types;
                     const baseTypes: Type[] = [];
+                    let different = false;
                     for (const type of types) {
                         const baseType = getBaseConstraint(type);
                         if (baseType) {
+                            if (baseType !== type) {
+                                different = true;
+                            }
                             baseTypes.push(baseType);
                         }
+                        else {
+                            different = true;
+                        }
+                    }
+                    if (!different) {
+                        return t;
                     }
                     return t.flags & TypeFlags.Union && baseTypes.length === types.length ? getUnionType(baseTypes) :
                         t.flags & TypeFlags.Intersection && baseTypes.length ? getIntersectionType(baseTypes) :
