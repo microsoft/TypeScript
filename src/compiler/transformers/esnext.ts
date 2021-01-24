63d944c4e037ed2495d15591e905b30db1850ce7
Jack Works <jackworks@protonmail.com>
2021-01-24 14:56:00 +0800

    feat: add do expr transform

--- src/compiler/transformers/esnext.ts
+++ src/compiler/transformers/esnext.ts
@@ -91,8 +91,37 @@ namespace ts {
         }
 
         function transformDoExpression(expr: DoExpression): VisitResult<Node> {
-            // TODO:
-            return factory.createImmediatelyInvokedArrowFunction(expr.block.statements);
+            const hasAsync = expr.transformFlags & TransformFlags.ContainsAwait;
+            const hasYield = expr.transformFlags & TransformFlags.ContainsYield;
+            const temp = context.factory.createTempVariable(context.hoistVariableDeclaration);
+            function do_visit(node: Node): VisitResult<Node> {
+                const isFinallyBlock = isBlock(node) && isTryStatement(node.parent) && node === node.parent.finallyBlock;
+                if (isFinallyBlock) return visitEachChild(node, visitor, context);
+                if (isExpressionStatement(node) && !(isFunctionLike(node) || isClassLike(node) || isNamespaceBody(node))) {
+                    return factory.createExpressionStatement(
+                        factory.createAssignment(temp, visitEachChild(node.expression, visitor, context))
+                    );
+                }
+                if (isTryStatement(node) || isSwitchStatement(node) || isIfStatement(node)) {
+                    return [factory.createAssignment(temp, factory.createVoidZero()), visitEachChild(node, do_visit, context)];
+                }
+                return visitEachChild(node, do_visit, context);
+            }
+            const block = visitEachChild(expr.block, do_visit, context);
+            const star = factory.createToken(SyntaxKind.AsteriskToken);
+            const f = factory.createFunctionExpression(
+                hasAsync ? [factory.createModifier(SyntaxKind.AsyncKeyword)] : undefined,
+                hasYield ? star : undefined,
+                /* name */ undefined, /* typeParam */ undefined, /** param */[], /** type */ undefined,
+                block
+            );
+            // f.call(this)
+            const call = factory.createCallExpression(
+                factory.createPropertyAccessExpression(f, "call"), /** typeArgs */ undefined, [factory.createThis()]
+            );
+            // yield* f.call(this)
+            const executor = hasYield ? factory.createYieldExpression(star, call) : hasAsync ? factory.createAwaitExpression(call) : call;
+            return factory.createCommaListExpression([executor, temp]);
         }
     }
 }
