8ca874aa413fabd854af6b0889054a7fffba3d33
Andrew Branch <andrew@wheream.io>
2021-01-26 15:16:57 -0800

    Accept baselines

--- tests/baselines/reference/controlFlowOptionalChain2.js
+++ tests/baselines/reference/controlFlowOptionalChain2.js
@@ -18,9 +18,27 @@ function funcTwo(arg: A | B | undefined) {
   arg?.name;
 }
 
+function funcThree(arg: A | B | null) {
+  if (arg?.type === 'B') {
+    arg; // `B`
+    return;
+  }
+
+  arg;
+  arg?.name;
+}
+
 
 //// [controlFlowOptionalChain2.js]
 function funcTwo(arg) {
+    if ((arg === null || arg === void 0 ? void 0 : arg.type) === 'B') {
+        arg; // `B`
+        return;
+    }
+    arg;
+    arg === null || arg === void 0 ? void 0 : arg.name;
+}
+function funcThree(arg) {
     if ((arg === null || arg === void 0 ? void 0 : arg.type) === 'B') {
         arg; // `B`
         return;
