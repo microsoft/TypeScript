a1b59cd4fb4649ee00581cc3a8f28d2fe9625055
Kubilay Kahveci <kahvecikubilay@gmail.com>
2021-01-28 12:22:30 +0000

    accept baseline
    Signed-off-by: Kubilay Kahveci <kahvecikubilay@gmail.com>

--- tests/baselines/reference/privateNameAccessors.js
+++ tests/baselines/reference/privateNameAccessors.js
@@ -16,15 +16,29 @@ class A1 {
 
 //// [privateNameAccessors.js]
 "use strict";
-var _A1_prop, _A1_prop_1, _A1_roProp;
+var __classPrivateAccessorSet = (this && this.__classPrivateAccessorSet) || function (receiver, instances, fn, value) {
+    if (!instances.has(receiver)) {
+        throw new TypeError("attempted to set private accessor on non-instance");
+    }
+    return fn.call(receiver, value);
+};
+var __classPrivateReadonly = (this && this.__classPrivateReadonly) || function () {
+    throw new TypeError("private element is not writable");
+};
+var __classPrivateAccessorGet = (this && this.__classPrivateAccessorGet) || function (receiver, instances, fn) {
+    if (!instances.has(receiver)) {
+        throw new TypeError("attempted to get private accessor on non-instance");
+    }
+    return fn.call(receiver);
+};
+var _A1_prop_get, _A1_prop_set, _A1_roProp_get, _A1_instances;
 class A1 {
     constructor(name) {
-        this. = "";
-        this. = ""; // Error
-        console.log(this.);
-        console.log(this.);
+        _A1_instances.add(this);
+        __classPrivateAccessorSet(this, _A1_instances, _A1_prop_set, "");
+        __classPrivateReadonly(this, ""); // Error
+        console.log(__classPrivateAccessorGet(this, _A1_instances, _A1_prop_get));
+        console.log(__classPrivateAccessorGet(this, _A1_instances, _A1_roProp_get));
     }
-    get () { return ""; }
-    set (param) { }
-    get () { return ""; }
 }
+_A1_instances = new WeakSet(), _A1_prop_get = function _A1_prop_get() { return ""; }, _A1_prop_set = function _A1_prop_set(param) { }, _A1_roProp_get = function _A1_roProp_get() { return ""; };
--- tests/baselines/reference/privateNameDuplicateField.js
+++ tests/baselines/reference/privateNameDuplicateField.js
@@ -36,43 +36,44 @@ class A6 {
 
 //// [privateNameDuplicateField.js]
 "use strict";
-var _foo, _foo_1, _foo_2, _foo_3, _foo_4, _foo_5, _foo_6, _foo_7, _foo_8, _foo_9, _foo_10, _foo_11, _foo_12;
+var _A_foo, _A_foo_1, _A2_foo, _A2_foo_1, _A2_instances, _A3_foo, _A3_foo_get, _A3_instances, _A4_foo, _A4_foo_1, _A4_instances, _A5_foo, _A5_foo_get, _A5_instances, _A6_foo, _A6_foo_1, _A6_foo_get, _A6_instances;
 class A {
     constructor() {
-        _foo_1.set(this, "foo");
-        _foo_1.set(this, "foo");
+        _A_foo_1.set(this, "foo");
+        _A_foo_1.set(this, "foo");
     }
 }
-_foo = new WeakMap(), _foo_1 = new WeakMap();
+_A_foo = new WeakMap(), _A_foo_1 = new WeakMap();
 class A2 {
     constructor() {
-        _foo_3.set(this, "foo");
+        _A2_instances.add(this);
+        this.#foo = "foo";
     }
-    () { }
 }
-_foo_2 = new WeakMap(), _foo_3 = new WeakMap();
+_A2_foo = new WeakMap(), _A2_instances = new WeakSet(), _A2_foo_1 = function _A2_foo_1() { };
 class A3 {
     constructor() {
-        _foo_5.set(this, "foo");
+        _A3_instances.add(this);
+        this.#foo = "foo";
     }
-    get () { return ""; }
 }
-_foo_4 = new WeakMap(), _foo_5 = new WeakMap();
+_A3_foo = new WeakMap(), _A3_instances = new WeakSet(), _A3_foo_get = function _A3_foo_get() { return ""; };
 class A4 {
-    () { return ""; }
-    () { return ""; }
+    constructor() {
+        _A4_instances.add(this);
+    }
 }
-_foo_6 = new WeakMap(), _foo_7 = new WeakMap();
+_A4_instances = new WeakSet(), _A4_foo_1 = function _A4_foo_1() { return ""; }, _A4_foo_1 = function _A4_foo_1() { return ""; };
 class A5 {
-    () { return ""; }
-    get () { return ""; }
+    constructor() {
+        _A5_instances.add(this);
+    }
 }
-_foo_8 = new WeakMap(), _foo_9 = new WeakMap();
+_A5_instances = new WeakSet(), _A5_foo_get = function _A5_foo_get() { return ""; };
 class A6 {
     constructor() {
-        _foo_12.set(this, "foo");
+        _A6_instances.add(this);
+        this.#foo = "foo";
     }
-    () { return ""; }
-    get () { return ""; }
 }
-_foo_10 = new WeakMap(), _foo_11 = new WeakMap(), _foo_12 = new WeakMap();
+_A6_foo = new WeakMap(), _A6_instances = new WeakSet(), _A6_foo_get = function _A6_foo_get() { return ""; };
