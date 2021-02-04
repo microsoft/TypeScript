0862c3c6ddea99d3bf1aad288d78d9b274017ba1
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-02-03 16:35:43 -0800

    Accessors can override synthetic properties
    This is the simplest fix -- I'm going to check whether it's worthwhile
    to make it stricter next.
    
    Fixes #41347

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -36244,8 +36244,9 @@ namespace ts {
                         // property/accessor is overridden with property/accessor
                         if (baseDeclarationFlags & ModifierFlags.Abstract && !(base.valueDeclaration && isPropertyDeclaration(base.valueDeclaration) && base.valueDeclaration.initializer)
                             || base.valueDeclaration && base.valueDeclaration.parent.kind === SyntaxKind.InterfaceDeclaration
+                            || getCheckFlags(base) & CheckFlags.Synthetic
                             || derived.valueDeclaration && isBinaryExpression(derived.valueDeclaration)) {
-                            // when the base property is abstract or from an interface, base/derived flags don't need to match
+                            // when the base property is abstract, from an interface or from an intersection, base/derived flags don't need to match
                             // same when the derived property is from an assignment
                             continue;
                         }
