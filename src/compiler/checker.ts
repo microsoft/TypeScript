ed2681639c5ee5a0cff792dee80acecf574f8e85
Eli Barzilay <eli@barzilay.org>
2021-02-11 15:22:06 -0500

    Avoid getting undefined `callSignatures`/`constructSignatures` in `getPropertyOfType`
    e350c357 (#40228) introduced a subtle bug: it switched the flags to an
    alias, dropping `SymbolFlags.Property` --- and that makes
    `symbolIsValue()` get to the `resolveAlias(symbol)` call, which leads to
    `getPropertyOfType()` with`resolved.callSignatures`+`constructSignatures`
    being `undefined`.  So initialize them in `setStructuredTypeMembers`
    before calling `getNamedMembers()`.
    
    Fixes #42350

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -3846,13 +3846,16 @@ namespace ts {
         }
 
         function setStructuredTypeMembers(type: StructuredType, members: SymbolTable, callSignatures: readonly Signature[], constructSignatures: readonly Signature[], stringIndexInfo: IndexInfo | undefined, numberIndexInfo: IndexInfo | undefined): ResolvedType {
-            (<ResolvedType>type).members = members;
-            (<ResolvedType>type).properties = members === emptySymbols ? emptyArray : getNamedMembers(members);
-            (<ResolvedType>type).callSignatures = callSignatures;
-            (<ResolvedType>type).constructSignatures = constructSignatures;
-            (<ResolvedType>type).stringIndexInfo = stringIndexInfo;
-            (<ResolvedType>type).numberIndexInfo = numberIndexInfo;
-            return <ResolvedType>type;
+            const resolved = <ResolvedType>type;
+            resolved.members = members;
+            resolved.properties = emptyArray;
+            resolved.callSignatures = callSignatures;
+            resolved.constructSignatures = constructSignatures;
+            resolved.stringIndexInfo = stringIndexInfo;
+            resolved.numberIndexInfo = numberIndexInfo;
+            // This can loop back to getPropertyOfType() which would crash if `callSignatures` & `constructSignatures` are not initialized.
+            if (members !== emptySymbols) resolved.properties = getNamedMembers(members);
+            return resolved;
         }
 
         function createAnonymousType(symbol: Symbol | undefined, members: SymbolTable, callSignatures: readonly Signature[], constructSignatures: readonly Signature[], stringIndexInfo: IndexInfo | undefined, numberIndexInfo: IndexInfo | undefined): ResolvedType {
