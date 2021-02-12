251793e82f0911de41b3fd3f9524239385dc32e5
Nathan Shively-Sanders <nathansa@microsoft.com>
2021-02-12 18:13:36 +0000

    Cherry-pick PR #42626 into release-4.2
    Component commits:
    214ef0c750 No did-you-mean-to-call error on casts
    I chose to do the ad-hoc check rather than yet another tree walk.
    
    1. It's faster to run and easier to read.
    2. This error came from looking at real code. It happened twice, so I
    think the best estimate for other uses that happened zero times is in
    fact zero.
    3. I couldn't think of other places to put the cast, given the
    restrictions on `testedNode` just before the new code.
    
    1d347785de Merge branch 'master' into no-did-you-mean-to-call-error-on-casts

---
+++ tests/cases/compiler/truthinessCallExpressionCoercion3.ts
@@ -0,0 +1,13 @@
+// @strictNullChecks: true
+// @lib: esnext,dom
+
+// from #41640, based on an example in ant-design
+interface I {
+    always(): void
+}
+
+function f(result: unknown) {
+    if ((result as I).always) {
+        return result
+    }
+}
