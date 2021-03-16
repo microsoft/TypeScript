63c63467376dc14bbb57342ad9a4e62d2c14a607
Andrew Branch <andrew@wheream.io>
2021-03-16 09:30:02 -0700

    Add React repro to test

--- tests/baselines/reference/excessiveStackDepthFlatArray.js
+++ tests/baselines/reference/excessiveStackDepthFlatArray.js
@@ -1,4 +1,4 @@
-//// [excessiveStackDepthFlatArray.ts]
+//// [index.tsx]
 interface MiddlewareArray<T> extends Array<T> {}
 declare function configureStore(options: { middleware: MiddlewareArray<any> }): void;
 
@@ -7,8 +7,40 @@ configureStore({
   middleware: [...defaultMiddleware], // Should not error
 });
 
+declare namespace React {
+  type DetailedHTMLProps<E extends HTMLAttributes<T>, T> = E;
+  interface HTMLAttributes<T> {
+    children?: ReactNode;
+  }
+  type ReactNode = ReactChild | ReactFragment | boolean | null | undefined;
+  type ReactText = string | number;
+  type ReactChild = ReactText;
+  type ReactFragment = {} | ReactNodeArray;
+  interface ReactNodeArray extends Array<ReactNode> {}
+}
+declare namespace JSX {
+  interface IntrinsicElements {
+    ul: React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>;
+    li: React.DetailedHTMLProps<React.HTMLAttributes<HTMLLIElement>, HTMLLIElement>;
+  }
+}
+declare var React: any;
 
-//// [excessiveStackDepthFlatArray.js]
+const Component = () => {
+  const categories = ['Fruit', 'Vegetables'];
+
+  return (
+    <ul>
+      <li>All</li>
+      {categories.map((category) => (
+        <li key={category}>{category}</li> // Error about 'key' only
+      ))}
+    </ul>
+  );
+};
+
+
+//// [index.js]
 var __spreadArray = (this && this.__spreadArray) || function (to, from) {
     for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
         to[j] = from[i];
@@ -17,3 +49,10 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from) {
 configureStore({
     middleware: __spreadArray([], defaultMiddleware)
 });
+var Component = function () {
+    var categories = ['Fruit', 'Vegetables'];
+    return (React.createElement("ul", null,
+        React.createElement("li", null, "All"),
+        categories.map(function (category) { return (React.createElement("li", { key: category }, category) // Error about 'key' only
+        ); })));
+};
