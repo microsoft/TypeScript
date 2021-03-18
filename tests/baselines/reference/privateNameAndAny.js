c461b921d93d171a49f3a38b4b6cb41c015d7fd3
Kubilay Kahveci <kahvecikubilay@gmail.com>
2021-03-18 15:24:22 +0000

    do not clone the receiver (#57)

--- tests/baselines/reference/privateNameAndAny.js
+++ tests/baselines/reference/privateNameAndAny.js
@@ -41,11 +41,13 @@ class A {
     }
     method(thing) {
         __classPrivateFieldGet(thing, _A_foo, "f"); // OK
-        __classPrivateFieldGet(thing, _a, "m", _A_m).call(// OK
+        __classPrivateFieldGet(// OK
+        thing, _a, "m", _A_m).call(// OK
         thing);
         __classPrivateFieldGet(thing, _a, "f", _A_baz);
         thing.; // Error
-        __classPrivateFieldGet(thing, _A_foo, "f").call(// Error
+        __classPrivateFieldGet(// Error
+        thing, _A_foo, "f").call(// Error
         thing);
     }
     methodU(thing) {
--- tests/baselines/reference/privateNameNestedMethodAccess.js
+++ tests/baselines/reference/privateNameNestedMethodAccess.js
@@ -43,7 +43,8 @@ class C {
                     _D_instances.add(this);
                     __classPrivateFieldGet(new C(), _C_foo, "f");
                     __classPrivateFieldGet(new C(), _D_instances, "m", _D_bar); // Error
-                    __classPrivateFieldGet(new C(), _C_instances, "a", _C_baz_get);
+                    __classPrivateFieldGet(// Error
+                    new C(), _C_instances, "a", _C_baz_get);
                     __classPrivateFieldGet(new D(), _D_instances, "m", _D_bar);
                 }
                 n(x) {
