26e658646b52e50228bca7ddedf24cc58bddab9f
Armando Aguirre <armanio123@outlook.com>
2021-03-22 20:34:54 -0700

    Added tests for single line trailing trivia

---
+++ tests/cases/fourslash/organizeImports6.ts
@@ -0,0 +1,16 @@
+/// <reference path="fourslash.ts" />
+
+// Regression test for GH#43107
+
+//// import * as something from "path"; /* small comment */ // single line one.
+//// /* some comment here
+//// * and there
+//// */
+//// import * as somethingElse from "anotherpath";
+
+verify.organizeImports(
+`/* some comment here
+* and there
+*/
+`
+); 
