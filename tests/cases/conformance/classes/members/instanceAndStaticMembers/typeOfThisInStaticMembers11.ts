40af136427123683e0bc2c98e5537517bd22ee7a
kingwl <kingwenlu@gmail.com>
2021-03-25 15:10:20 +0800

    Add class expression tests

---
+++ tests/cases/conformance/classes/members/instanceAndStaticMembers/typeOfThisInStaticMembers11.ts
@@ -0,0 +1,49 @@
+// @target: esnext, es6, es5
+// @useDefineForClassFields: false
+// @experimentalDecorators: true
+
+declare const foo: any;
+
+@foo
+class C {
+    static a = 1;
+    static b = this.a + 1;
+}
+
+const D = class extends C {
+        static c = 2;
+        static d = this.c + 1;
+        static e = super.a + this.c + 1;
+        static f = () => this.c + 1;
+        static ff = function () { this.c + 1 }
+        static foo () {
+            return this.c + 1;
+        }
+        static get fa () {
+            return this.c + 1;
+        }
+        static set fa (v: number) {
+            this.c = v + 1;
+        }
+    }
+}
+
+function ret () {
+    return class extends C {
+        static c = 2;
+        static d = this.c + 1;
+        static e = super.a + this.c + 1;
+        static f = () => this.c + 1;
+        static ff = function () { this.c + 1 }
+        static foo () {
+            return this.c + 1;
+        }
+        static get fa () {
+            return this.c + 1;
+        }
+        static set fa (v: number) {
+            this.c = v + 1;
+        }
+    }
+}
+
