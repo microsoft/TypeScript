b4c8ff6017ded58807d3040e5760102f1e661849
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2021-03-26 13:25:27 -0700

    Fix stray error in tst

--- tests/baselines/reference/divergentAccessors1.js
+++ tests/baselines/reference/divergentAccessors1.js
@@ -7,7 +7,7 @@
         set foo(): number | string;
     }
     
-    declare const ihgs: IHasGetSet;
+    const ihgs: IHasGetSet = null as any;
     ihgs.foo = "32";
     let r_ihgs_foo: number = ihgs.foo;
 }
@@ -18,7 +18,7 @@
         set foo(): number | string;
     }
     
-    declare const t_hgs: T_HasGetSet;
+    const t_hgs: T_HasGetSet = null as any;
     t_hgs.foo = "32";
     let r_t_hgs_foo: number = t_hgs.foo;
 }
@@ -28,10 +28,12 @@
 "use strict";
 // Accessors in interfaces/types
 {
+    var ihgs = null;
     ihgs.foo = "32";
     var r_ihgs_foo = ihgs.foo;
 }
 {
+    var t_hgs = null;
     t_hgs.foo = "32";
     var r_t_hgs_foo = t_hgs.foo;
 }
--- tests/cases/compiler/divergentAccessors1.ts
+++ tests/cases/compiler/divergentAccessors1.ts
@@ -8,7 +8,7 @@
         set foo(): number | string;
     }
     
-    declare const ihgs: IHasGetSet;
+    const ihgs: IHasGetSet = null as any;
     ihgs.foo = "32";
     let r_ihgs_foo: number = ihgs.foo;
 }
@@ -19,7 +19,7 @@
         set foo(): number | string;
     }
     
-    declare const t_hgs: T_HasGetSet;
+    const t_hgs: T_HasGetSet = null as any;
     t_hgs.foo = "32";
     let r_t_hgs_foo: number = t_hgs.foo;
 }
