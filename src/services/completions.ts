149c20d0687c17f01d261b944a5a03e09eddf6a6
Tiago Tristao <tiago.v.tristao@gmail.com>
2021-04-04 23:22:15 +0100

    Completion list for type literals in type arguments

--- src/services/completions.ts
+++ src/services/completions.ts
@@ -153,6 +153,11 @@ namespace ts.Completions {
             return stringCompletions;
         }
 
+        const typeLiteralCompletions = getTypeLiteralInTypeArgumentCompletions(sourceFile, position, contextToken, typeChecker, compilerOptions, log, preferences);
+        if (typeLiteralCompletions) {
+            return typeLiteralCompletions;
+        }
+
         if (contextToken && isBreakOrContinueStatement(contextToken.parent)
             && (contextToken.kind === SyntaxKind.BreakKeyword || contextToken.kind === SyntaxKind.ContinueKeyword || contextToken.kind === SyntaxKind.Identifier)) {
             return getLabelCompletionAtPosition(contextToken.parent);
@@ -981,6 +986,58 @@ namespace ts.Completions {
 
     function isModuleSymbol(symbol: Symbol): boolean {
         return !!symbol.declarations?.some(d => d.kind === SyntaxKind.SourceFile);
+    }
+
+    function getTypeLiteralInTypeArgumentCompletions(sourceFile: SourceFile, position: number, contextToken: Node | undefined, checker: TypeChecker, options: CompilerOptions, log: Log, preferences: UserPreferences): CompletionInfo | undefined{
+        if (!contextToken) return undefined;
+
+        const typeLiteralNode = tryGetTypeLiteralNode(contextToken);
+
+        if (typeLiteralNode) {
+            const intersectionTypeNode = isIntersectionTypeNode(typeLiteralNode.parent) ? typeLiteralNode.parent : undefined;
+            const containerTypeNode = intersectionTypeNode || typeLiteralNode;
+
+            const containerExpectedType = tryGetTypeArgumentSubType(containerTypeNode, checker);
+            if (!containerExpectedType) {
+                return undefined;
+            }
+
+            const containerActualType = checker.getTypeFromTypeNode(containerTypeNode);
+
+            const members = getPropertiesForCompletion(containerExpectedType, checker);
+            const existingMembers = getPropertiesForCompletion(containerActualType, checker);
+
+            const existingMemberEscapedNames: Set<__String> = new Set();
+            forEach(existingMembers, s => existingMemberEscapedNames.add(s.escapedName));
+
+            const missingMembers = filter(members, s => !existingMemberEscapedNames.has(s.escapedName));
+
+            const location = getTouchingToken(sourceFile, position);
+
+            const entries: CompletionEntry[] = [];
+            getCompletionEntriesFromSymbols(
+                missingMembers,
+                entries,
+                /* contextToken */ undefined,
+                location,
+                sourceFile,
+                checker,
+                options.target!,
+                log,
+                CompletionKind.MemberLike,
+                preferences,
+                options
+            );
+
+            return {
+                isGlobalCompletion: false,
+                isMemberCompletion: true,
+                isNewIdentifierLocation: false,
+                entries
+            };
+        }
+
+        return undefined;
     }
 
     function getCompletionData(
@@ -2855,6 +2912,49 @@ namespace ts.Completions {
                 return (isValidKeyword(contextToken.kind) || contextToken.kind === SyntaxKind.AsteriskToken || isIdentifier(contextToken) && isValidKeyword(stringToToken(contextToken.text)!)) // TODO: GH#18217
                     ? contextToken.parent.parent as ObjectTypeDeclaration : undefined;
         }
+    }
+
+    function tryGetTypeLiteralNode(node: Node): TypeLiteralNode | undefined {
+        const parent = node.parent;
+
+        switch (node.kind) {
+            case SyntaxKind.OpenBraceToken:
+                if (isTypeLiteralNode(parent)) {
+                    return parent;
+                }
+                break;
+            case SyntaxKind.SemicolonToken:
+            case SyntaxKind.CommaToken:
+            case SyntaxKind.Identifier:
+                if (parent.kind === SyntaxKind.PropertySignature && isTypeLiteralNode(parent.parent)) {
+                    return parent.parent;
+                }
+                break;
+        }
+
+        return undefined;
+    }
+
+    function tryGetTypeArgumentSubType(node: Node, checker: TypeChecker): Type | undefined {
+        if (!node) return undefined;
+
+        if (isTypeNode(node) && isTypeReferenceType(node.parent)) {
+            return checker.getTypeArgumentConstraint(node);
+        }
+
+        const t = tryGetTypeArgumentSubType(node.parent, checker);
+        if (t) {
+            switch (node.kind) {
+                case SyntaxKind.PropertySignature:
+                    return checker.getTypeOfPropertyOfContextualType(t, node.symbol.escapedName);
+                case SyntaxKind.IntersectionType:
+                case SyntaxKind.TypeLiteral:
+                case SyntaxKind.UnionType:
+                    return t;
+            }
+        }
+
+        return undefined;
     }
 
     // TODO: GH#19856 Would like to return `node is Node & { parent: (ClassElement | TypeElement) & { parent: ObjectTypeDeclaration } }` but then compilation takes > 10 minutes
