9065c87038cb5041b5f0933872213097cda80480
Andrew Branch <andrew@wheream.io>
2021-04-05 13:20:01 -0700

    Fix other tests

--- src/testRunner/unittests/tsserver/partialSemanticServer.ts
+++ src/testRunner/unittests/tsserver/partialSemanticServer.ts
@@ -219,7 +219,6 @@ function fooB() { }`
                 file: file2.path,
                 start: { line: 1, offset: 1 },
                 end: { line: 1, offset: 1 },
-                unverified: true,
             }]);
         });
     });
