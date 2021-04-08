e5777fa0a213d78e79c02fcdef1517a5c1daf103
Zzzen <callmetsing@gmail.com>
2021-04-08 23:40:37 +0800

    add test

--- tests/cases/compiler/narrowingOfQualifiedNames.ts
+++ tests/cases/compiler/narrowingOfQualifiedNames.ts
@@ -1,5 +1,7 @@
 // @strict: true
 
+// Repro from #43411
+
 interface IProperties {
     foo?: {
         aaa: string
@@ -14,6 +16,36 @@ function init(properties: IProperties) {
         for (const x of [1, 2, 3]) {
           properties.foo; // type is { aaa: string; bbb: string; }
           type FooOrUndefined = typeof properties.foo; //type is { aaa: string; bbb: string; } | undefined
+        }
+    }
+}
+
+interface DeepOptional {
+    a?: {
+        b?: {
+            c?: string
+        }
+    }
+}
+
+function init2(foo: DeepOptional) {
+    if (foo.a) {
+        for(const _ of [1]) {
+            type A = typeof foo.a;
+
+            if (foo.a.b) {
+                type B = typeof foo.a.b;
+                for(const _ of [1]) {
+                    type B = typeof foo.a.b;
+
+                    if (foo.a.b.c) {
+                        type C = typeof foo.a.b.c;
+                        for(const _ of [1]) {
+                            type C = typeof foo.a.b.c;
+                        }
+                    }
+                } 
+            }
         }
     }
 }
