5a0804b2cb174aadf41c260ae244d4ee5238e1d4
Josh Goldberg <josh@codecademy.com>
2021-04-11 15:37:25 -0400

    Corrected more baselines from the merge

--- tests/baselines/reference/emitBOM.js
+++ tests/baselines/reference/emitBOM.js
@@ -2,7 +2,7 @@
 // JS and d.ts output should have a BOM but not the sourcemap
 var x;
 
-tests/cases/compiler/emitBOM.js(1,1): error TS1433: Unexpected keyword or identifier.
+tests/cases/compiler/emitBOM.js(1,1): error TS1434: Unexpected keyword or identifier.
 tests/cases/compiler/emitBOM.js(1,2): error TS1127: Invalid character.
 tests/cases/compiler/emitBOM.js(1,2): error TS1128: Declaration or statement expected.
 tests/cases/compiler/emitBOM.js(1,3): error TS1127: Invalid character.
@@ -11,7 +11,7 @@ tests/cases/compiler/emitBOM.js(1,3): error TS1127: Invalid character.
 ==== tests/cases/compiler/emitBOM.js (4 errors) ====
     ï»¿// JS and d.ts output should have a BOM but not the sourcemap
     ~
-!!! error TS1433: Unexpected keyword or identifier.
+!!! error TS1434: Unexpected keyword or identifier.
      
 !!! error TS1127: Invalid character.
      ~
