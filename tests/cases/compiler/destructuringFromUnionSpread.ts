2950db90ed7c84a310e5e7cca6b7771e56ead36e
Andrew Branch <andrew@wheream.io>
2021-04-22 16:32:43 -0700

    Don’t allow an object literal with a spread as a fallback for destructuring a property not present in all constituents

--- tests/cases/compiler/destructuringFromUnionSpread.ts
+++ tests/cases/compiler/destructuringFromUnionSpread.ts
@@ -1,11 +1,5 @@
 interface A { a: string }
 interface B { b: number }
 
-function foo(x: A | B) {
-    const { a } = { ...x } // no error?! 
-    // const a: string | undefined
-    a?.toUpperCase();
-}
-
-const x = { a: 1, b: 2 }
-foo(x); // a.toUpperCase is not a function, oops
+declare const x: A | B;
+const { a } = { ...x } // error
