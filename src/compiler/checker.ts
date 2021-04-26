d5af89c552edca1bfeea903bf402b9898a514b1f
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-04-26 09:19:24 -0700

    Contextual typing checks property assignments for type annotation (#43598)
    Property assignments can have a type annotation in JS. This PR adds a
    check for it in contextual typing.
    
    Fixes #43379

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -25218,6 +25218,10 @@ namespace ts {
 
         function getContextualTypeForObjectLiteralElement(element: ObjectLiteralElementLike, contextFlags?: ContextFlags) {
             const objectLiteral = <ObjectLiteralExpression>element.parent;
+            const propertyAssignmentType = isPropertyAssignment(element) && getContextualTypeForVariableLikeDeclaration(element);
+            if (propertyAssignmentType) {
+                return propertyAssignmentType;
+            }
             const type = getApparentTypeOfContextualType(objectLiteral, contextFlags);
             if (type) {
                 if (hasBindableName(element)) {
