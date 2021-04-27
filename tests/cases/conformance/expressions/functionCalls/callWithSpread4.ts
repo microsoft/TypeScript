764416157c4b7eead717e4555355edd58702f135
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-04-27 09:29:40 -0700

    trim out too-real test case

--- tests/cases/conformance/expressions/functionCalls/callWithSpread4.ts
+++ tests/cases/conformance/expressions/functionCalls/callWithSpread4.ts
@@ -3,9 +3,8 @@ type R = { a: number }
 type W = { b: number }
 type RW = { a: number, b: number }
 declare const pli: {
-    // (s1: R, s2: RW, s3: RW, s4: RW, s5: W): Promise<void>;
-    // (streams: ReadonlyArray<R | W | RW>): Promise<void>;
-    // commenting this out keeps the error the same, but provides definite ranges, which explains the weird bheaviour a little better
+    (s1: R, s2: RW, s3: RW, s4: RW, s5: W): Promise<void>;
+    (streams: ReadonlyArray<R | W | RW>): Promise<void>;
     (s1: R, s2: RW | W, ...streams: Array<RW | W>): Promise<void>;
 }
 
@@ -24,45 +23,8 @@ pli(
     writes
 );
 
+declare function test(x: any, y: () => string): string | undefined;
 declare var anys: any[]
 test(...anys)
-jsonTransform(...anys)
 
 pli(...[reads, writes, writes] as const)
-
-
-import fs = require("fs");
-import stream = require("stream");
-import util = require("util");
-import zlib = require("zlib");
-
-declare function split(matcher: RegExp, mapper: (line: string) => any): stream.Transform;
-
-declare function test(x: any, y: () => string): string | undefined;
-declare function jsonTransform(t: typeof test): stream.Transform;
-declare function disp(x: any): string;
-
-declare const input: string;
-declare const output: string;
-
-const pipeline = util.promisify(stream.pipeline);
-
-async function processFile(fastMode: boolean) {
-    await pipeline(
-        fs.createReadStream(input),
-        ...(/\.gz$/.test(input) ? [zlib.createGunzip()]
-            : /\.br$/.test(input) ? [zlib.createBrotliDecompress()]
-            : []) as [(zlib.Gunzip | zlib.BrotliDecompress)],
-        fastMode
-            ? split(/,?\r?\n/, x => x.length > 1 ? test(JSON.parse(x), () => x) : undefined)
-            : jsonTransform(test),
-        async function* (inp: any) {
-            for await (const x of inp) yield disp(x);
-            yield "\n]\n";
-        },
-        ...(/\.gz$/.test(output) ? [zlib.createGzip()]
-            : /\.br$/.test(output) ? [zlib.createBrotliCompress()]
-            : []) as [(zlib.Gzip | zlib.BrotliCompress)],
-        fs.createWriteStream(output)
-    );
-}
