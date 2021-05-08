e863a9ced5a587be78c20c6389ec13879f5afee3
Zzzen <callmetsing@gmail.com>
2021-05-08 23:05:08 +0800

    add tests

--- tests/baselines/reference/typeofThis.js
+++ tests/baselines/reference/typeofThis.js
@@ -20,6 +20,44 @@ class Test1 {
 }
 
 
+function Test2() {
+    let x: typeof this.no = 1;
+}
+
+function Test3(this: { no: number }) {
+    let x: typeof this.no = 1;
+}
+
+function Test4(this: { no: number } | undefined) {
+    let x: typeof this.no = 1;
+}
+
+class Test5 {
+    no = 1;
+
+    f = () => {
+        let x: typeof this.no = 1;
+        let self: typeof this = this;
+    }
+}
+
+namespace Test6 {
+    export let f = () => {
+        let x: typeof this.no = 1;
+    }
+}
+
+module Test7 {
+    export let f = () => {
+        let x: typeof this.no = 1;
+    }
+}
+
+const Test8 = () => {
+    let x: typeof this.no = 1;
+}
+
+
 //// [typeofThis.js]
 var Test = /** @class */ (function () {
     function Test() {
@@ -39,3 +77,38 @@ var Test1 = /** @class */ (function () {
     }
     return Test1;
 }());
+function Test2() {
+    var x = 1;
+}
+function Test3() {
+    var x = 1;
+}
+function Test4() {
+    var x = 1;
+}
+var Test5 = /** @class */ (function () {
+    function Test5() {
+        var _this = this;
+        this.no = 1;
+        this.f = function () {
+            var x = 1;
+            var self = _this;
+        };
+    }
+    return Test5;
+}());
+var Test6;
+(function (Test6) {
+    Test6.f = function () {
+        var x = 1;
+    };
+})(Test6 || (Test6 = {}));
+var Test7;
+(function (Test7) {
+    Test7.f = function () {
+        var x = 1;
+    };
+})(Test7 || (Test7 = {}));
+var Test8 = function () {
+    var x = 1;
+};
--- tests/cases/conformance/types/specifyingTypes/typeQueries/typeofThis.ts
+++ tests/cases/conformance/types/specifyingTypes/typeQueries/typeofThis.ts
@@ -1,3 +1,5 @@
+// @noImplicitThis: true
+
 class Test {
     data = {};
     constructor() {
@@ -16,4 +18,42 @@ class Test1 {
 
         var str: typeof this.this = '';
     }
+}
+
+
+function Test2() {
+    let x: typeof this.no = 1;
+}
+
+function Test3(this: { no: number }) {
+    let x: typeof this.no = 1;
+}
+
+function Test4(this: { no: number } | undefined) {
+    let x: typeof this.no = 1;
+}
+
+class Test5 {
+    no = 1;
+
+    f = () => {
+        let x: typeof this.no = 1;
+        let self: typeof this = this;
+    }
+}
+
+namespace Test6 {
+    export let f = () => {
+        let x: typeof this.no = 1;
+    }
+}
+
+module Test7 {
+    export let f = () => {
+        let x: typeof this.no = 1;
+    }
+}
+
+const Test8 = () => {
+    let x: typeof this.no = 1;
 }
