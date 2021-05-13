1868cc76c8f03f95d414cdf9f8786c6fd1e6dcc8
Scharkee <matas2k@gmail.com>
2021-05-13 15:03:46 +0300

    Update baseline test

--- tests/baselines/reference/formatToPartsBigInt.js
+++ tests/baselines/reference/formatToPartsBigInt.js
@@ -5,6 +5,7 @@
 new Intl.NumberFormat("fr").formatToParts(3000n);
 new Intl.NumberFormat("fr").formatToParts(BigInt(123));
 
+
 //// [formatToPartsBigInt.js]
 // Intl.NumberFormat.formatToParts should support bigInt
 // Test Intl methods with new parameter type
