a746f9d4cb387434522a177dbad7add0214d5b7c
Scharkee <matas2k@gmail.com>
2021-05-15 22:06:04 +0300

    Move formatToParts to es2018

---
+++ scratch/file1.ts
@@ -0,0 +1,2 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+new Intl.NumberFormat().formatToParts();
--- src/lib/es2018.intl.d.ts
+++ src/lib/es2018.intl.d.ts
@@ -38,4 +38,15 @@ declare namespace Intl {
             options?: PluralRulesOptions,
         ): string[];
     };
+
+    type NumberFormatPartTypes = "compact" | "currency" | "decimal" | "exponentInteger" | "exponentMinusSign" | "exponentSeparator" | "fraction" | "group" | "infinity" | "integer" | "literal" | "minusSign" | "nan" | "plusSign" | "percentSign" | "unit" | "unknown";
+
+    interface NumberFormatPart {
+        type: NumberFormatPartTypes;
+        value: string;
+    }
+
+    interface NumberFormat {
+        formatToParts(number?: number): NumberFormatPart[];
+    }
 }
