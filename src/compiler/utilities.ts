15569550245c925a4ce4945513b3fe76f82ac5f1
Scharkee <matas2k@gmail.com>
2021-05-15 22:14:22 +0300

    Change target lib test and target features

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -601,6 +601,7 @@ namespace ts {
                 AsyncIterableIterator: emptyArray,
                 AsyncGenerator: emptyArray,
                 AsyncGeneratorFunction: emptyArray,
+                NumberFormat: ["formatToParts"]
             },
             es2019: {
                 Array: ["flat", "flatMap"],
@@ -621,9 +622,6 @@ namespace ts {
             es2021: {
                 PromiseConstructor: ["any"],
                 String: ["replaceAll"]
-            },
-            esnext: {
-                NumberFormat: ["formatToParts"]
             }
         };
     }
