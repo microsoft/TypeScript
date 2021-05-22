0847b23ad30d585de25c05d6ee0824f72f63e0da
ShuiRuTian <158983297@qq.com>
2021-05-22 23:23:55 +0800

    better command description for --jsx.

--- src/compiler/commandLineParser.ts
+++ src/compiler/commandLineParser.ts
@@ -437,7 +437,7 @@ namespace ts {
             paramType: Diagnostics.KIND,
             showInSimplifiedHelpView: true,
             category: Diagnostics.Basic_Options,
-            description: Diagnostics.Specify_JSX_code_generation_Colon_preserve_react_native_react_react_jsx_or_react_jsxdev,
+            description: Diagnostics.Specify_JSX_code_generation,
             defaultValueDescription: "undefined"
         },
         {
