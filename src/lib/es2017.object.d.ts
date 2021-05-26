58c659ed6d061e4b8b522a0eb6e76732f60e053b
Ben Beattie-Hood <BenBeattieHood@users.noreply.github.com>
2021-05-27 09:09:40 +1000

    Object.values(T)/entries(T) now returns [K, T[K]]

--- src/lib/es2017.object.d.ts
+++ src/lib/es2017.object.d.ts
@@ -3,25 +3,25 @@ interface ObjectConstructor {
      * Returns an array of values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    values<T>(o: { [s: string]: T } | ArrayLike<T>): T[];
+    values<T>(a: ArrayLike<T>): T[];
 
     /**
      * Returns an array of values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    values(o: {}): any[];
+    values<T extends {}>(o: T): T[keyof T][];
 
     /**
      * Returns an array of key/values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    entries<T>(o: { [s: string]: T } | ArrayLike<T>): [string, T][];
+    entries<T>(a: ArrayLike<T>): [string, T][];
 
     /**
      * Returns an array of key/values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
-    entries(o: {}): [string, any][];
+    entries<T extends {}, K extends keyof T>(o: T): [K, T[K]][];
 
     /**
      * Returns an object containing all own property descriptors of an object
