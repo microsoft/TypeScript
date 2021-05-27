f3bf29a71c492adfec4e782a823f69408e0f0af8
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2021-05-27 10:41:17 +0300

    fix(44273): preserves 'override' modifier in JavaScript output

---
+++ tests/baselines/reference/override16.js
@@ -0,0 +1,17 @@
+//// [override16.ts]
+class A {
+    foo?: string;
+}
+
+class B extends A {
+    override foo = "string";
+}
+
+
+//// [override16.js]
+class A {
+    foo;
+}
+class B extends A {
+    foo = "string";
+}
