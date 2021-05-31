b159f509898c669784d103d2416c459cf4c0f7e9
Kitson Kelly <me@kitsonkelly.com>
2021-05-31 14:21:38 +1000

    improve types

--- src/compiler/watch.ts
+++ src/compiler/watch.ts
@@ -1,10 +1,10 @@
 /*@internal*/
 namespace ts {
-    const sysFormatDiagnosticsHost: FormatDiagnosticsHost = sys ? {
+    const sysFormatDiagnosticsHost: FormatDiagnosticsHost | undefined = sys ? {
         getCurrentDirectory: () => sys.getCurrentDirectory(),
         getNewLine: () => sys.newLine,
         getCanonicalFileName: createGetCanonicalFileName(sys.useCaseSensitiveFileNames)
-    } : undefined!; // TODO: GH#18217
+    } : undefined;
 
     /**
      * Create a function that reports error by writing to the system and handles the formating of the diagnostic
