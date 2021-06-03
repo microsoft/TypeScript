abed90c977064914bf84335a9a5d56b9d49d673b
exoticknight <draco.knight0@gmail.com>
2021-06-03 13:03:46 +0800

    return errorType

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -24226,6 +24226,7 @@ namespace ts {
                 const firstFunctionLikeContainerOrClassLikeContainer = findAncestor(node.parent, node => isClassLike(node) || isFunctionLike(node))!;
                 if (firstFunctionLikeContainerOrClassLikeContainer.kind === SyntaxKind.ClassExpression || firstFunctionLikeContainerOrClassLikeContainer.kind === SyntaxKind.ClassDeclaration) {
                     error(node, Diagnostics.arguments_is_not_allowed_in_class_field_initializer_or_static_initialization_block);
+                    return errorType;
                 }
 
                 getNodeLinks(container).flags |= NodeCheckFlags.CaptureArguments;
