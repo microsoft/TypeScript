7ee9c40d26b1364e8d52f98ab7e386d472271b94
kingwl <kingwenlu@gmail.com>
2021-06-10 11:44:51 +0800

    Avoid whitespace

--- src/testRunner/unittests/config/convertCompilerOptionsFromJson.ts
+++ src/testRunner/unittests/config/convertCompilerOptionsFromJson.ts
@@ -296,7 +296,7 @@ namespace ts {
                         start: 0,
                         length: 0,
                         messageText: "Argument for '--lib' option must be: 'es5', 'es6', 'es2015', 'es7', 'es2016', 'es2017', 'es2018', 'esnext', 'dom', 'dom.iterable', 'webworker', " +
-                            "'webworker.importscripts', 'scripthost', 'es2015.core', 'es2015.collection', 'es2015.generator', 'es2015.iterable', 'es2015.promise', 'es2015.proxy', " + 
+                            "'webworker.importscripts', 'scripthost', 'es2015.core', 'es2015.collection', 'es2015.generator', 'es2015.iterable', 'es2015.promise', 'es2015.proxy', " +
                             "'es2015.reflect', 'es2015.symbol', 'es2015.symbol.wellknown', 'es2016.array.include', 'es2017.object', 'es2017.sharedmemory', 'es2017.string', 'es2017.intl', " +
                             "'es2017.typedarrays', 'es2018.asynciterable', 'es2018.intl', 'es2018.promise', 'es2018.regexp', 'esnext.array', 'esnext.symbol', 'esnext.intl', 'esnext.bigint', " +
                             "'esnext.array', 'esnext.string', 'esnext.promise'.",
