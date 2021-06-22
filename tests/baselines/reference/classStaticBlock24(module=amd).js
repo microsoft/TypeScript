045a9e85693bbd908cc5682b8bc14e255881203f
kingwl <kingwenlu@gmail.com>
2021-06-22 14:34:13 +0800

    Add more test cases

---
+++ tests/baselines/reference/classStaticBlock24(module=amd).js
@@ -0,0 +1,25 @@
+//// [classStaticBlock24.ts]
+export class C {
+  static x: number;
+  static {
+    C.x = 1;
+  }
+}
+
+
+//// [classStaticBlock24.js]
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var _C__;
+    exports.__esModule = true;
+    exports.C = void 0;
+    var C = /** @class */ (function () {
+        function C() {
+        }
+        return C;
+    }());
+    exports.C = C;
+    _C__ = { value: (function () {
+            C.x = 1;
+        })() };
+});
---
+++ tests/baselines/reference/classStaticBlock24(module=esnext).js
@@ -0,0 +1,20 @@
+//// [classStaticBlock24.ts]
+export class C {
+  static x: number;
+  static {
+    C.x = 1;
+  }
+}
+
+
+//// [classStaticBlock24.js]
+var _C__;
+var C = /** @class */ (function () {
+    function C() {
+    }
+    return C;
+}());
+export { C };
+_C__ = { value: (function () {
+        C.x = 1;
+    })() };
