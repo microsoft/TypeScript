1f173a9e68b6b8699339f8a1f7e73c45e6dd97a9
kingwl <kingwenlu@gmail.com>
2021-06-23 17:56:59 +0800

    Add more case

---
+++ tests/baselines/reference/classStaticBlock26.js
@@ -0,0 +1,26 @@
+//// [classStaticBlock26.ts]
+class C {
+    static {
+        let await = 1;
+    }
+    static {
+        var await = 1;
+    }
+    static {
+        const await = 1;
+    }
+}
+
+
+//// [classStaticBlock26.js]
+class C {
+    static {
+        let await = 1;
+    }
+    static {
+        var await = 1;
+    }
+    static {
+        const await = 1;
+    }
+}
---
+++ tests/cases/conformance/classes/classStaticBlock/classStaticBlock26.ts
@@ -0,0 +1,13 @@
+// @target: esnext
+
+class C {
+    static {
+        let await = 1;
+    }
+    static {
+        var await = 1;
+    }
+    static {
+        const await = 1;
+    }
+}
