9201fcbfde03e7024ea821a981c99078b29d4bf4
Ben Lichtman <belichtm@microsoft.com>
2021-06-28 15:44:20 -0700

    [WIP] add uri parsing

---
+++ src/uri/platform.ts
@@ -0,0 +1,18 @@
+/*@internal*/
+namespace ts.server.uri {
+    // !!!!!
+    // SEE https://github.com/microsoft/vscode/blob/master/src/vs/base/common/platform.ts
+    // !!!!!
+
+    declare const process: { platform: "win32" };
+    declare const navigator: { userAgent: string };
+
+    export let isWindows: boolean;
+
+    if (typeof process === "object") {
+        isWindows = process.platform === "win32";
+    } else if (typeof navigator === "object") {
+        let userAgent = navigator.userAgent;
+        isWindows = userAgent.indexOf("Windows") >= 0;
+    }
+}
