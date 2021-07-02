1ef029cb603b15e33f9b953b500ee3655c5a1df8
Anders Hejlsberg <andersh@microsoft.com>
2021-07-02 10:47:33 -1000

    Accept new baselines

---
+++ tests/baselines/reference/ignoredJsxAttributes.js
@@ -0,0 +1,32 @@
+//// [ignoredJsxAttributes.tsx]
+// Repro from #44797
+
+import * as React from "react";
+
+interface Props {
+    foo: string;
+    [dataProp: string]: string;
+}
+
+declare function Yadda(props: Props): JSX.Element;
+
+let props: Props = {
+    foo: "",
+    "data-yadda": 42,  // Error
+};
+
+let x1 = <Yadda foo="hello" data-yadda={42}/>;
+let x2 = <Yadda bar="hello" data-yadda={42}/>;  // Error
+
+
+//// [ignoredJsxAttributes.js]
+"use strict";
+// Repro from #44797
+exports.__esModule = true;
+var React = require("react");
+var props = {
+    foo: "",
+    "data-yadda": 42
+};
+var x1 = React.createElement(Yadda, { foo: "hello", "data-yadda": 42 });
+var x2 = React.createElement(Yadda, { bar: "hello", "data-yadda": 42 }); // Error
