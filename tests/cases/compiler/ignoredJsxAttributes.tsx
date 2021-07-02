4358730eeffc84177a03777f998da208c9244949
Anders Hejlsberg <andersh@microsoft.com>
2021-07-02 10:47:27 -1000

    Add regression test

---
+++ tests/cases/compiler/ignoredJsxAttributes.tsx
@@ -0,0 +1,21 @@
+// @strict: true
+// @jsx: react
+
+// Repro from #44797
+
+import * as React from "react";
+
+interface Props {
+    foo: string;
+    [dataProp: string]: string;
+}
+
+declare function Yadda(props: Props): JSX.Element;
+
+let props: Props = {
+    foo: "",
+    "data-yadda": 42,  // Error
+};
+
+let x1 = <Yadda foo="hello" data-yadda={42}/>;
+let x2 = <Yadda bar="hello" data-yadda={42}/>;  // Error
