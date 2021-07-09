8e5829df8317392a46b9dc68429b3174c185dea3
Scharkee <matas2k@gmail.com>
2021-07-09 11:35:16 +0300

    Revert lib changes

--- lib/lib.esnext.intl.d.ts
+++ lib/lib.esnext.intl.d.ts
@@ -18,5 +18,15 @@ and limitations under the License.
 /// <reference no-default-lib="true"/>
 
 
-/// <reference lib="es2021" />
-/// <reference lib="esnext.intl" />
+declare namespace Intl {
+    type NumberFormatPartTypes = "compact" | "currency" | "decimal" | "exponentInteger" | "exponentMinusSign" | "exponentSeparator" | "fraction" | "group" | "infinity" | "integer" | "literal" | "minusSign" | "nan" | "plusSign" | "percentSign" | "unit" | "unknown";
+
+    interface NumberFormatPart {
+        type: NumberFormatPartTypes;
+        value: string;
+    }
+
+    interface NumberFormat {
+        formatToParts(number?: number | bigint): NumberFormatPart[];
+    }
+}
