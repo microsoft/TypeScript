fa66faf1b327243ee558fefac4a432c1ae014d6c
Zzzen <callmetsing@gmail.com>
2021-07-10 14:48:15 +0800

    align ClassStaticBlockDeclaration with IIFE in CFA

---
+++ tests/baselines/reference/classStaticBlock28.js
@@ -0,0 +1,23 @@
+//// [classStaticBlock28.ts]
+let foo: number;
+
+class C {
+    static {
+        foo = 1
+    }
+}
+
+console.log(foo)
+
+//// [classStaticBlock28.js]
+"use strict";
+var foo;
+var C = /** @class */ (function () {
+    function C() {
+    }
+    return C;
+}());
+(function () {
+    foo = 1;
+})();
+console.log(foo);
