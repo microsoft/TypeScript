eff42d2d46a989a927b3a2e3792bbed47407a052
David Sherret <dsherret@gmail.com>
2021-07-30 15:34:33 -0400

    Copy built-in types/values test for go to definition.

--- tests/cases/fourslash/goToDefinitionMetaProperty.ts
+++ tests/cases/fourslash/goToDefinitionMetaProperty.ts
@@ -3,4 +3,6 @@
 ////im/*1*/port.met/*2*/a;
 ////function t() { n/*3*/ew.t/*4*/arget; }
 
-verify.goToDefinition(["1", "2", "3", "4"], []);
+for (const marker of test.markerNames()) {
+    verify.goToDefinition(marker, []);
+}
