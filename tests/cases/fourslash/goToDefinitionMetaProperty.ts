0ccbe56ce38c23712861654bb8355be7acb96907
David Sherret <dsherret@gmail.com>
2021-07-30 18:16:46 -0400

    Add tests for go to definition when not a module.

--- tests/cases/fourslash/goToDefinitionMetaProperty.ts
+++ tests/cases/fourslash/goToDefinitionMetaProperty.ts
@@ -1,7 +1,12 @@
 /// <reference path='fourslash.ts' />
 
+// @Filename: /a.ts
 ////im/*1*/port.met/*2*/a;
-////function t() { n/*3*/ew.t/*4*/arget; }
+////function f() { n/*3*/ew.t/*4*/arget; }
+
+// @Filename: /b.ts
+////im/*5*/port.m;
+////class c { constructor() { n/*6*/ew.t/*7*/arget; } }
 
 for (const marker of test.markerNames()) {
     verify.goToDefinition(marker, []);
