e9607f10f224743c76fe2243065fe066fe2bffe7
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-08-04 09:00:01 -0700

    Exclude tuples from suggestion

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -19580,6 +19580,7 @@ namespace ts {
         }
 
         function getExactOptionalUnassignableProperties(source: Type, target: Type) {
+            if (isTupleType(source) && isTupleType(target)) return emptyArray;
             return getPropertiesOfType(target)
                 .filter(targetProp => isExactOptionalPropertyMismatch(getTypeOfPropertyOfType(source, targetProp.escapedName), getTypeOfSymbol(targetProp)));
         }
