c435473474049f4533b1b4982f2787de2f5765f8
Orta Therox <git@orta.io>
2021-08-30 16:21:35 +0000

    Drops the es2020 definition of UnicodeBCP47LocaleIdentifier, it's now available in es2016

--- src/lib/es2020.intl.d.ts
+++ src/lib/es2020.intl.d.ts
@@ -1,14 +1,6 @@
 declare namespace Intl {
 
     /**
-     * [Unicode BCP 47 Locale Identifiers](https://unicode.org/reports/tr35/#Unicode_Language_and_Locale_Identifiers) definition.
-     *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#locales_argument).
-     *
-     */
-    type UnicodeBCP47LocaleIdentifier = string;
-
-    /**
      * Unit to use in the relative time internationalized message.
      *
      * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/format#Parameters).
