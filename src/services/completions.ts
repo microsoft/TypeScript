bac841ef18df797f9b169d011467986ae9a01c4e
Sang <11912225+sangxxh@users.noreply.github.com>
2021-09-08 06:20:58 +1000

    Fix missing abstract keyword in completion (#45710)
    * first commit
    
    * tests

--- src/services/completions.ts
+++ src/services/completions.ts
@@ -2960,6 +2960,7 @@ namespace ts.Completions {
                         || kind === SyntaxKind.ModuleKeyword
                         || kind === SyntaxKind.TypeKeyword
                         || kind === SyntaxKind.NamespaceKeyword
+                        || kind === SyntaxKind.AbstractKeyword
                         || isTypeKeyword(kind) && kind !== SyntaxKind.UndefinedKeyword;
                 case KeywordCompletionFilters.FunctionLikeBodyKeywords:
                     return isFunctionLikeBodyKeyword(kind);
