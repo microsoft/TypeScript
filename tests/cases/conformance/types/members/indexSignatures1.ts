1774fcf0762acb3c9cc0ee02c8a6b199f9dd0654
Anders Hejlsberg <andersh@microsoft.com>
2021-09-07 09:32:32 -0700

    Add regression test

--- tests/cases/conformance/types/members/indexSignatures1.ts
+++ tests/cases/conformance/types/members/indexSignatures1.ts
@@ -314,3 +314,12 @@ const aa: AA = { [sym]: '123' };
 const obj1: { [key: symbol]: string } = { [sym]: 'hello '};
 const obj2: { [key: string]: string } = { [sym]: 'hello '};  // Permitted for backwards compatibility
 const obj3: { [key: number]: string } = { [sym]: 'hello '};  // Error
+
+// Repro from #45772
+
+type Id = string & { __tag: 'id '};
+type Rec1 = { [key: Id]: number };
+type Rec2 = Record<Id, number>;
+
+type K1 = keyof Rec1;  // Id
+type K2 = keyof Rec2;  // Id
