f56d6089f286c1cb72bcdd74223ae9e4f72c082d
Ron Buckton <ron.buckton@microsoft.com>
2021-09-09 18:31:41 -0700

    Adds back in the older Promise.all overloads

--- src/lib/es2015.promise.d.ts
+++ src/lib/es2015.promise.d.ts
@@ -20,6 +20,60 @@ interface PromiseConstructor {
      */
     all<T extends readonly unknown[] | []>(values: T): Promise<{ -readonly [P in keyof T]: Awaited<T[P]> }>;
 
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`.
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3, ...]>(...)` instead.
+     */
+    all<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>, T6 | PromiseLike<T6>, T7 | PromiseLike<T7>, T8 | PromiseLike<T8>, T9 | PromiseLike<T9>, T10 | PromiseLike<T10>]): Promise<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`.
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3, ...]>(...)` instead.
+     */
+    all<T1, T2, T3, T4, T5, T6, T7, T8, T9>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>, T6 | PromiseLike<T6>, T7 | PromiseLike<T7>, T8 | PromiseLike<T8>, T9 | PromiseLike<T9>]): Promise<[T1, T2, T3, T4, T5, T6, T7, T8, T9]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`. 
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3, ...]>(...)` instead.
+     */
+    all<T1, T2, T3, T4, T5, T6, T7, T8>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>, T6 | PromiseLike<T6>, T7 | PromiseLike<T7>, T8 | PromiseLike<T8>]): Promise<[T1, T2, T3, T4, T5, T6, T7, T8]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`. 
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3, ...]>(...)` instead.
+     */
+    all<T1, T2, T3, T4, T5, T6, T7>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>, T6 | PromiseLike<T6>, T7 | PromiseLike<T7>]): Promise<[T1, T2, T3, T4, T5, T6, T7]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`. 
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3, ...]>(...)` instead.
+     */
+    all<T1, T2, T3, T4, T5, T6>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>, T6 | PromiseLike<T6>]): Promise<[T1, T2, T3, T4, T5, T6]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`. 
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3, ...]>(...)` instead.
+     */
+    all<T1, T2, T3, T4, T5>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>]): Promise<[T1, T2, T3, T4, T5]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`. 
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3, ...]>(...)` instead.
+     */
+    all<T1, T2, T3, T4>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>]): Promise<[T1, T2, T3, T4]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`. 
+     * If explicit type arguments are still necessary, please use `all<[T1, T2, T3]>(...)` instead.
+     */
+    all<T1, T2, T3>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>]): Promise<[T1, T2, T3]>;
+
+    /**
+     * @deprecated This overload has been removed in favor of `all<T extends readonly unknown[] | []>(values: T): Promise<...>`. 
+     * If explicit type arguments are still necessary, please use `all<[T1, T2]>(...)` instead.
+     */
+    all<T1, T2>(values: readonly [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>]): Promise<[T1, T2]>;
+
     // see: lib.es2015.iterable.d.ts
     // all<T>(values: Iterable<T | PromiseLike<T>>): Promise<T[]>;
 
