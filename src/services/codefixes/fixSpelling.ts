8ef4a672ead4c2c3971ef26beb4440dcde1f792a
stkevintan <stkevintan@zju.edu.cn>
2021-09-13 14:29:49 +0800

    fix: test errors

--- src/services/codefixes/fixSpelling.ts
+++ src/services/codefixes/fixSpelling.ts
@@ -10,6 +10,7 @@ namespace ts.codefix {
         Diagnostics.Cannot_find_name_0_Did_you_mean_the_static_member_1_0.code,
         Diagnostics._0_has_no_exported_member_named_1_Did_you_mean_2.code,
         Diagnostics.This_member_cannot_have_an_override_modifier_because_it_is_not_declared_in_the_base_class_0_Did_you_mean_1.code,
+        Diagnostics.This_member_cannot_have_a_JSDoc_comment_with_an_override_tag_because_it_is_not_declared_in_the_base_class_0_Did_you_mean_1.code,
         // for JSX class components
         Diagnostics.No_overload_matches_this_call.code,
         // for JSX FC
