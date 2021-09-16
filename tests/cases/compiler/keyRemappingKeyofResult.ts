19944dfafff8af4a7e7305d46dd2b1ebceb2b8eb
Wesley Wigham <wwigham@gmail.com>
2021-09-16 13:46:17 -0700

    Loosen check in getIndexTypeForMappedType to directly map property names when any indexy type is present

---
+++ tests/cases/compiler/keyRemappingKeyofResult.ts
@@ -0,0 +1,19 @@
+// @target: es6
+export const sym = Symbol("")
+type Orig = { [k: string]: any, str: any, [sym]: any }
+
+type Okay = Exclude<keyof Orig, never>
+// type Okay = string | number | typeof sym
+
+type Remapped = { [K in keyof Orig as {} extends Record<K, any> ? never : K]: any }
+/* type Remapped = {
+    str: any;
+    [sym]: any;
+} */
+// no string index signature, right?
+
+type Oops = Exclude<keyof Remapped, never>
+declare let x: Oops;
+x = sym;
+x = "str";
+// type Oops = typeof sym <-- what happened to "str"?
