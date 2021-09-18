0ba7e1a04dde48cfdaf49dc2fe99ce72fa9bf554
Armando Aguirre <armando.aguirre@microsoft.com>
2021-09-17 17:29:06 -0700

    Renamed

---
+++ tests/baselines/reference/comparisonOperatorWithIntersectionType.js
@@ -0,0 +1,9 @@
+//// [comparisonOperatorWithIntersectionType.ts]
+// Regression test for GH#45489
+
+declare let foo: { a: number } & { id: number }
+const bar = foo > 1;
+
+//// [comparisonOperatorWithIntersectionType.js]
+// Regression test for GH#45489
+var bar = foo > 1;
--- tests/baselines/reference/comparisonOperatorWithUnionType.js
+++
@@ -1,9 +0,0 @@
-//// [comparisonOperatorWithUnionType.ts]
-// Regression test for GH#45489
-
-declare let foo: { a: number } & { id: number }
-const bar = foo > 1;
-
-//// [comparisonOperatorWithUnionType.js]
-// Regression test for GH#45489
-var bar = foo > 1;
