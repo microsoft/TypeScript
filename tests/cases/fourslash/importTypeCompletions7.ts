e422ef017c9dc1e216d0c808c697c83bb2648822
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2021-09-23 21:12:40 +0300

    fix(45607): add snippet for type only import statements

---
+++ tests/cases/fourslash/importTypeCompletions7.ts
@@ -0,0 +1,29 @@
+/// <reference path="fourslash.ts" />
+
+// @target: es2020
+// @module: esnext
+
+// @Filename: /foo.d.ts
+//// declare namespace Foo {}
+//// export = Foo;
+
+// @Filename: /test.ts
+//// [|import F/**/|]
+
+goTo.file("/test.ts")
+verify.completions({
+    marker: "",
+    exact: [{
+        name: "Foo",
+        sourceDisplay: "./foo",
+        source: "./foo",
+        insertText: "import * as Foo from \"./foo\";",
+        replacementSpan: test.ranges()[0]
+    }],
+    isNewIdentifierLocation: true,
+    preferences: {
+        includeCompletionsForModuleExports: true,
+        includeCompletionsForImportStatements: true,
+        includeCompletionsWithInsertText: true
+    }
+});
