6403bc07a9adc15279cd2b2f05c0e6254908b3bc
Eero Tanskanen <pokute@draconus.com>
2021-10-03 21:57:50 +0300

    PA - test baselines

---
+++ tests/baselines/reference/partialApplicationWormVsBinaryNot.js
@@ -0,0 +1,18 @@
+//// [partialApplicationWormVsBinaryNot.ts]
+// Verify that binary NOT still works all right for ~(*).
+
+const enum EnumWithNOT {
+    Notted = 5 & ~(1 | 4 | 2),
+};
+
+var m = 5 & ~(1 | 4);
+var a = ~(1 | 4);
+var b = ~(1);
+
+
+//// [partialApplicationWormVsBinaryNot.js]
+// Verify that binary NOT still works all right for ~(*).
+;
+var m = 5 & ~(1 | 4);
+var a = ~(1 | 4);
+var b = ~(1);
---
+++ tests/cases/conformance/expressions/partialApplication/partialApplicationWormVsBinaryNot.ts
@@ -0,0 +1,9 @@
+// Verify that binary NOT still works all right for ~(*).
+
+const enum EnumWithNOT {
+    Notted = 5 & ~(1 | 4 | 2),
+};
+
+var m = 5 & ~(1 | 4);
+var a = ~(1 | 4);
+var b = ~(1);
