8741cdf686877e837117fcb7ca6588fef63aaa47
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2021-10-05 00:35:43 +0000

    Accepted baselines.

--- tests/baselines/reference/mappedTypeWithAny.js
+++ tests/baselines/reference/mappedTypeWithAny.js
@@ -24,6 +24,21 @@ for (let id in z) {
   let x = data.notAValue;  // Error
 }
 
+// Issue #46169.
+// We want mapped types whose constraint is `keyof T` to
+// map over `any` differently, depending on whether `T`
+// is constrained to an array-like type.
+type Arrayish<T extends unknown[]> = { [K in keyof T]: T[K] };
+type Objectish<T extends unknown> = { [K in keyof T]: T[K] };
+
+function bar(arrayish: Arrayish<any>, objectish: Objectish<any>) {
+    let arr: any[];
+    arr = arrayish;
+    arr = objectish;
+}
+
+declare function stringifyArray<T extends readonly any[]>(arr: T): { -readonly [K in keyof T]: string };
+let abc: any[] = stringifyArray(void 0 as any);
 
 //// [mappedTypeWithAny.js]
 "use strict";
@@ -31,6 +46,12 @@ for (var id in z) {
     var data = z[id];
     var x = data.notAValue; // Error
 }
+function bar(arrayish, objectish) {
+    var arr;
+    arr = arrayish;
+    arr = objectish;
+}
+var abc = stringifyArray(void 0);
 
 
 //// [mappedTypeWithAny.d.ts]
@@ -58,3 +79,14 @@ declare type StrictDataMap<T> = {
     [P in keyof T]: Data;
 };
 declare let z: StrictDataMap<any>;
+declare type Arrayish<T extends unknown[]> = {
+    [K in keyof T]: T[K];
+};
+declare type Objectish<T extends unknown> = {
+    [K in keyof T]: T[K];
+};
+declare function bar(arrayish: Arrayish<any>, objectish: Objectish<any>): void;
+declare function stringifyArray<T extends readonly any[]>(arr: T): {
+    -readonly [K in keyof T]: string;
+};
+declare let abc: any[];
