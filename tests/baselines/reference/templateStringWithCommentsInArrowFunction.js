67139227bc54695d6045c006043030543e34faeb
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2021-10-09 23:36:58 +0300

    fix(45336): add a blank line before the comment expression to avoid disrupting return statement

---
+++ tests/baselines/reference/templateStringWithCommentsInArrowFunction.js
@@ -0,0 +1,27 @@
+//// [templateStringWithCommentsInArrowFunction.ts]
+const a = 1;
+const f1 = () =>
+    `${
+      // a
+      a
+    }a`;
+
+const f2 = () =>
+    `${
+      // a
+      a
+    }`;
+
+
+//// [templateStringWithCommentsInArrowFunction.js]
+var a = 1;
+var f1 = function () {
+    return "" + 
+    // a
+    a + "a";
+};
+var f2 = function () {
+    return "" + 
+    // a
+    a;
+};
