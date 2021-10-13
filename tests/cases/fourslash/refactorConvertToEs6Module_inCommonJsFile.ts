b37f41b6925f1910a726d0c3d2180dab435405c6
Sidharth Vinod <sidharthv96@gmail.com>
2021-10-13 09:36:13 +0530

    tests: Cover more cases in a single file.

--- tests/cases/fourslash/refactorConvertToEs6Module_inCommonJsFile.ts
+++ tests/cases/fourslash/refactorConvertToEs6Module_inCommonJsFile.ts
@@ -9,10 +9,10 @@
 // @Filename: /b.cts
 ////module.exports = 0;
 
-// @Filename: /c.js
+// @Filename: /c.ts
 ////module.exports = 0;
 
-// @Filename: /d.ts
+// @Filename: /d.js
 ////module.exports = 0;
 
 goTo.file("/a.cjs");
@@ -21,13 +21,10 @@ verify.codeFixAvailable([]);
 goTo.file("/b.cts");
 verify.codeFixAvailable([]);
 
-goTo.file("/c.js");
-verify.codeFix({
-    description: "Convert to ES6 module",
-    newFileContent: 'export default 0;',
-});
+goTo.file("/c.ts");
+verify.codeFixAvailable([]);
 
-goTo.file("/d.ts");
+goTo.file("/d.js");
 verify.codeFix({
     description: "Convert to ES6 module",
     newFileContent: 'export default 0;',
