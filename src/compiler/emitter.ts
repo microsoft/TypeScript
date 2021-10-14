182e205b3ff7317cfa1cd5e8bc2e99a8f2578286
stkevintan <stkevintan@zju.edu.cn>
2021-10-14 16:38:07 +0800

    revert emitter changes

--- src/compiler/emitter.ts
+++ src/compiler/emitter.ts
@@ -1613,10 +1613,6 @@ namespace ts {
                         return emitJSDocSignature(node as JSDocSignature);
                     case SyntaxKind.JSDocTag:
                     case SyntaxKind.JSDocClassTag:
-                    case SyntaxKind.JSDocPublicTag:
-                    case SyntaxKind.JSDocPrivateTag:
-                    case SyntaxKind.JSDocProtectedTag:
-                    case SyntaxKind.JSDocOverrideTag:
                         return emitJSDocSimpleTag(node as JSDocTag);
                     case SyntaxKind.JSDocAugmentsTag:
                     case SyntaxKind.JSDocImplementsTag:
@@ -1625,7 +1621,11 @@ namespace ts {
                     case SyntaxKind.JSDocDeprecatedTag:
                         return;
                     // SyntaxKind.JSDocClassTag (see JSDocTag, above)
+                    case SyntaxKind.JSDocPublicTag:
+                    case SyntaxKind.JSDocPrivateTag:
+                    case SyntaxKind.JSDocProtectedTag:
                     case SyntaxKind.JSDocReadonlyTag:
+                    case SyntaxKind.JSDocOverrideTag:
                         return;
                     case SyntaxKind.JSDocCallbackTag:
                         return emitJSDocCallbackTag(node as JSDocCallbackTag);
