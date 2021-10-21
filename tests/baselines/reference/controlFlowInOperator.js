728a8a34d235149951364e1128b9c83d2333830a
Marek Dědič <developer@dedic.eu>
2021-10-21 12:41:54 +0200

    Added tests for in operator type widening in complex control flows

--- tests/baselines/reference/controlFlowInOperator.js
+++ tests/baselines/reference/controlFlowInOperator.js
@@ -51,9 +51,83 @@ if ('a' in e) {
 
 if (a in e) {
     e;      // { a: unknown; }
-    e[a]  // unknown
+    e[a]    // unknown
 } else {
     e;      // object
+}
+
+// More complex control flows
+
+e;              // object
+if ( 'a' in e ) {
+    e;          // { a: unknown; }
+    if ( 'b' in e ) {
+        e;      // { a: unknown; b: unknown; }
+        e['a']; // unknown
+        e['b']; // unknown
+    } else {
+        e;      // { a: unknown; }
+    }
+    e;          // { a: unknown; }
+}
+e;              // object
+
+e;              // object
+if ( a in e ) {
+    e;          // { a: unknown; }
+    if ( b in e ) {
+        e;      // { a: unknown; b: unknown; }
+        e[a];   // unknown
+        e[b];   // unknown
+    } else {
+        e;      // { a: unknown; }
+    }
+    e;          // { a: unknown; }
+}
+e;              // object
+
+e;          // object
+if ( 'a' in e ) {
+    e;      // { a: unknown; }
+    e['a']; // unknown
+} else if ( 'b' in e ) {
+    e;      // { b: unknown; }
+    e['b']; // unknown
+} else {
+    e;      // object
+}
+e;          // object
+
+e;          // object
+if ( a in e ) {
+    e;      // { a: unknown; }
+    e[a];   // unknown
+} else if ( b in e ) {
+    e;      // { b: unknown; }
+    e[b];   // unknown
+} else {
+    e;      // object
+}
+e;          // object
+
+declare const f: Array<object>
+
+for (const g of f) {
+    g;          // object
+    if ('a' in g) {
+        g;      // { a: unknown; }
+        g['a']; // unknown
+    }
+    g;          // object
+}
+
+for (const g of f) {
+    g;          // object
+    if (a in g) {
+        g;      // { a: unknown; }
+        g[a];   // unknown
+    }
+    g;          // object
 }
 
 
@@ -102,4 +176,77 @@ if (a in e) {
 }
 else {
     e; // object
+}
+// More complex control flows
+e; // object
+if ('a' in e) {
+    e; // { a: unknown; }
+    if ('b' in e) {
+        e; // { a: unknown; b: unknown; }
+        e['a']; // unknown
+        e['b']; // unknown
+    }
+    else {
+        e; // { a: unknown; }
+    }
+    e; // { a: unknown; }
+}
+e; // object
+e; // object
+if (a in e) {
+    e; // { a: unknown; }
+    if (b in e) {
+        e; // { a: unknown; b: unknown; }
+        e[a]; // unknown
+        e[b]; // unknown
+    }
+    else {
+        e; // { a: unknown; }
+    }
+    e; // { a: unknown; }
+}
+e; // object
+e; // object
+if ('a' in e) {
+    e; // { a: unknown; }
+    e['a']; // unknown
+}
+else if ('b' in e) {
+    e; // { b: unknown; }
+    e['b']; // unknown
+}
+else {
+    e; // object
+}
+e; // object
+e; // object
+if (a in e) {
+    e; // { a: unknown; }
+    e[a]; // unknown
+}
+else if (b in e) {
+    e; // { b: unknown; }
+    e[b]; // unknown
+}
+else {
+    e; // object
+}
+e; // object
+for (var _i = 0, f_1 = f; _i < f_1.length; _i++) {
+    var g = f_1[_i];
+    g; // object
+    if ('a' in g) {
+        g; // { a: unknown; }
+        g['a']; // unknown
+    }
+    g; // object
+}
+for (var _a = 0, f_2 = f; _a < f_2.length; _a++) {
+    var g = f_2[_a];
+    g; // object
+    if (a in g) {
+        g; // { a: unknown; }
+        g[a]; // unknown
+    }
+    g; // object
 }
--- tests/cases/conformance/controlFlow/controlFlowInOperator.ts
+++ tests/cases/conformance/controlFlow/controlFlowInOperator.ts
@@ -50,7 +50,81 @@ if ('a' in e) {
 
 if (a in e) {
     e;      // { a: unknown; }
-    e[a]  // unknown
+    e[a]    // unknown
 } else {
     e;      // object
+}
+
+// More complex control flows
+
+e;              // object
+if ( 'a' in e ) {
+    e;          // { a: unknown; }
+    if ( 'b' in e ) {
+        e;      // { a: unknown; b: unknown; }
+        e['a']; // unknown
+        e['b']; // unknown
+    } else {
+        e;      // { a: unknown; }
+    }
+    e;          // { a: unknown; }
+}
+e;              // object
+
+e;              // object
+if ( a in e ) {
+    e;          // { a: unknown; }
+    if ( b in e ) {
+        e;      // { a: unknown; b: unknown; }
+        e[a];   // unknown
+        e[b];   // unknown
+    } else {
+        e;      // { a: unknown; }
+    }
+    e;          // { a: unknown; }
+}
+e;              // object
+
+e;          // object
+if ( 'a' in e ) {
+    e;      // { a: unknown; }
+    e['a']; // unknown
+} else if ( 'b' in e ) {
+    e;      // { b: unknown; }
+    e['b']; // unknown
+} else {
+    e;      // object
+}
+e;          // object
+
+e;          // object
+if ( a in e ) {
+    e;      // { a: unknown; }
+    e[a];   // unknown
+} else if ( b in e ) {
+    e;      // { b: unknown; }
+    e[b];   // unknown
+} else {
+    e;      // object
+}
+e;          // object
+
+declare const f: Array<object>
+
+for (const g of f) {
+    g;          // object
+    if ('a' in g) {
+        g;      // { a: unknown; }
+        g['a']; // unknown
+    }
+    g;          // object
+}
+
+for (const g of f) {
+    g;          // object
+    if (a in g) {
+        g;      // { a: unknown; }
+        g[a];   // unknown
+    }
+    g;          // object
 }
