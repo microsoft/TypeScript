16db880e029437380e5a20f6ea447f8b78f27ac2
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-10-21 08:43:22 -0700

    Reduce test case

--- tests/cases/conformance/expressions/literals/constContextTemplateLiteral.ts
+++ tests/cases/conformance/expressions/literals/constContextTemplateLiteral.ts
@@ -1,13 +1,9 @@
-interface Person {
-    id: number;
-    name: string;
-}
+type Person = { id: number }
 
 declare function key(): `person-${number}`
 /* This only happens if index type is a template literal type */
 const persons: Record<`person-${Person["id"]}`, { a: any }> = {
-    ...{},
     [`person-${1}`]: { b: "something" }, // ok, error
-    [`person-${1}` as const]: { b: "something" }, // ok, error
-    [key()]: { b: "something" }, // still no error
+    // [`person-${1}` as const]: { b: "something" }, // ok, error
+    // [key()]: { b: "something" }, // still no error, it's not a literal
 }
