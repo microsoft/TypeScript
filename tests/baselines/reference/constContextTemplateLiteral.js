ea7d4c62c992bba84dca989fc8570159d86be72a
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-10-21 11:10:04 -0700

    Reduce test case + update baselines

--- tests/baselines/reference/constContextTemplateLiteral.js
+++ tests/baselines/reference/constContextTemplateLiteral.js
@@ -1,23 +1,12 @@
 //// [constContextTemplateLiteral.ts]
-interface Person {
-    id: number;
-    name: string;
-}
-
-declare function key(): `person-${number}`
-/* This only happens if index type is a template literal type */
-const persons: Record<`person-${Person["id"]}`, { a: any }> = {
+type Person = { id: number }
+const persons: Record<string, { a: any }> = {
     [`person-${1}`]: { b: "something" }, // ok, error
-    [`person-${1}` as const]: { b: "something" }, // ok, error
-    [key()]: { b: "something" }, // still no error, it's not a literal
 }
 
 
 //// [constContextTemplateLiteral.js]
 var _a;
-/* This only happens if index type is a template literal type */
 var persons = (_a = {},
     _a["person-".concat(1)] = { b: "something" },
-    _a["person-".concat(1)] = { b: "something" },
-    _a[key()] = { b: "something" },
     _a);
--- tests/cases/conformance/expressions/literals/constContextTemplateLiteral.ts
+++ tests/cases/conformance/expressions/literals/constContextTemplateLiteral.ts
@@ -1,9 +1,4 @@
 type Person = { id: number }
-
-declare function key(): `person-${number}`
-/* This only happens if index type is a template literal type */
-const persons: Record<`person-${Person["id"]}`, { a: any }> = {
+const persons: Record<string, { a: any }> = {
     [`person-${1}`]: { b: "something" }, // ok, error
-    // [`person-${1}` as const]: { b: "something" }, // ok, error
-    // [key()]: { b: "something" }, // still no error, it's not a literal
 }
