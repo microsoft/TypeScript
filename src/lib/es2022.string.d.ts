95b4ad8813368b15a8df1b5e6ea9e6d96b2b5516
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-10-29 02:50:56 +0200

    undefined

--- src/lib/es2022.string.d.ts
+++ src/lib/es2022.string.d.ts
@@ -3,5 +3,5 @@ interface String {
      * Returns a new String consisting of the single UTF-16 code unit located at the specified index.
      * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
      */
-    at(index: number): string;
+    at(index: number): string | undefined;
 }
