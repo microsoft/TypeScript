56f81075f9802c85365ab24046e7eb947cd7800a
Anders Hejlsberg <andersh@microsoft.com>
2021-11-02 12:46:26 -0700

    Properly check whether union type contains only primitive types (#46645)
    * Properly check whether union type contains only primitive types
    
    * Add regression test
    
    * Remove 'export' modifier from test

---
+++ tests/baselines/reference/primitiveUnionDetection.js
@@ -0,0 +1,24 @@
+//// [primitiveUnionDetection.ts]
+// Repro from #46624
+
+type Kind = "one" | "two" | "three";
+
+declare function getInterfaceFromString<T extends Kind>(options?: { type?: T } & { type?: Kind }): T;
+
+const result = getInterfaceFromString({ type: 'two' });
+
+
+//// [primitiveUnionDetection.js]
+"use strict";
+// Repro from #46624
+var result = getInterfaceFromString({ type: 'two' });
+
+
+//// [primitiveUnionDetection.d.ts]
+declare type Kind = "one" | "two" | "three";
+declare function getInterfaceFromString<T extends Kind>(options?: {
+    type?: T;
+} & {
+    type?: Kind;
+}): T;
+declare const result: "two";
