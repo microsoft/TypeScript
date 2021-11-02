f9aa40ed9e59ef985406d4fe09e805f69e580197
Anders Hejlsberg <andersh@microsoft.com>
2021-11-02 11:27:32 -0700

    Add regression test

---
+++ tests/baselines/reference/primitiveUnionDetection.js
@@ -0,0 +1,19 @@
+//// [primitiveUnionDetection.ts]
+// Repro from #46624
+
+export type Kind = "one" | "two" | "three";
+
+declare function getInterfaceFromString<T extends Kind>(options?: { type?: T } & { type?: Kind }): T;
+
+const result = getInterfaceFromString({ type: 'two' });
+
+
+//// [primitiveUnionDetection.js]
+"use strict";
+// Repro from #46624
+exports.__esModule = true;
+var result = getInterfaceFromString({ type: 'two' });
+
+
+//// [primitiveUnionDetection.d.ts]
+export declare type Kind = "one" | "two" | "three";
