a628e68de9df930614c6fd479691abfa946bca48
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2021-11-03 23:11:39 +0200

    fix(46615): remove useless assertion. change error suggestion for rest params (#46628)

--- src/services/codefixes/addNameToNamelessParameter.ts
+++ src/services/codefixes/addNameToNamelessParameter.ts
@@ -14,24 +14,24 @@ namespace ts.codefix {
 
     function makeChange(changeTracker: textChanges.ChangeTracker, sourceFile: SourceFile, pos: number) {
         const token = getTokenAtPosition(sourceFile, pos);
-        if (!isIdentifier(token)) {
-            return Debug.fail("add-name-to-nameless-parameter operates on identifiers, but got a " + Debug.formatSyntaxKind(token.kind));
-        }
         const param = token.parent;
         if (!isParameter(param)) {
             return Debug.fail("Tried to add a parameter name to a non-parameter: " + Debug.formatSyntaxKind(token.kind));
         }
+
         const i = param.parent.parameters.indexOf(param);
         Debug.assert(!param.type, "Tried to add a parameter name to a parameter that already had one.");
         Debug.assert(i > -1, "Parameter not found in parent parameter list.");
+
+        const typeNode = factory.createTypeReferenceNode(param.name as Identifier, /*typeArguments*/ undefined);
         const replacement = factory.createParameterDeclaration(
             /*decorators*/ undefined,
             param.modifiers,
             param.dotDotDotToken,
             "arg" + i,
             param.questionToken,
-            factory.createTypeReferenceNode(token, /*typeArguments*/ undefined),
+            param.dotDotDotToken ? factory.createArrayTypeNode(typeNode) : typeNode,
             param.initializer);
-        changeTracker.replaceNode(sourceFile, token, replacement);
+        changeTracker.replaceNode(sourceFile, param, replacement);
     }
 }
