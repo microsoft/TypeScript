734686494ed129f3166ef03c8a385ef51157bb2d
龙腾道 <LongTengDao@LongTengDao.com>
2021-11-17 01:51:19 +0800

    TemplateStringsArray could contains undefined

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -584,7 +584,7 @@ interface NumberConstructor {
 /** An object that represents a number of any kind. All JavaScript numbers are 64-bit floating-point numbers. */
 declare var Number: NumberConstructor;
 
-interface TemplateStringsArray extends ReadonlyArray<string> {
+interface TemplateStringsArray extends ReadonlyArray<string | undefined> {
     readonly raw: readonly string[];
 }
 
