a0fdb2ae0d3e3e3f0ba84759d4fa042239bc4aed
Eero Tanskanen <pokute@draconus.com>
2021-11-28 12:45:59 +0200

    Pipeline - Test case for hack&papp combination.

---
+++ tests/baselines/reference/pipelineHackAndPApp.js
@@ -0,0 +1,13 @@
+//// [pipelineHackAndPApp.ts]
+(1.1 + 2.6) |> (# |>> Math.round);
+
+(3) |> (3 |>> ((o) => Math.pow(o, #)));
+
+5.7 |>> ((a) => a |> # * 100);
+
+
+//// [pipelineHackAndPApp.js]
+var pipelineHackPlaceholder_1, pipelineHackPlaceholder_2;
+(pipelineHackPlaceholder_1 = (1.1 + 2.6), (Math.round(pipelineHackPlaceholder_1)));
+(pipelineHackPlaceholder_2 = (3), ((function (o) { return Math.pow(o, pipelineHackPlaceholder_2); })(3)));
+(function (a) { var pipelineHackPlaceholder_3; return (pipelineHackPlaceholder_3 = a, pipelineHackPlaceholder_3 * 100); })(5.7);
