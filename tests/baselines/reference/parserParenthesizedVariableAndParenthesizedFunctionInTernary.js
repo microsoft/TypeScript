cdf12f91c73c641738c2a3c57607986fb29321f3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2021-11-30 12:57:37 -0800

    Fix parsing of parenthesized functions in conditional expressions (#46960)

---
+++ tests/baselines/reference/parserParenthesizedVariableAndParenthesizedFunctionInTernary.js
@@ -0,0 +1,8 @@
+//// [parserParenthesizedVariableAndParenthesizedFunctionInTernary.ts]
+let a: any;
+const c = true ? (a) : (function() {});
+
+
+//// [parserParenthesizedVariableAndParenthesizedFunctionInTernary.js]
+var a;
+var c = true ? (a) : (function () { });
