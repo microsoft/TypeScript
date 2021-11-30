3ae38faff83235c69fd0361358774c897e7d1c65
Linus Unnebäck <linus@folkdatorn.se>
2021-11-30 19:17:31 +0100

    Add tests for generic startsWith/endsWith

---
+++ tests/baselines/reference/stringEndsWith.js
@@ -0,0 +1,30 @@
+//// [stringEndsWith.ts]
+declare var test: string;
+declare function requirePostfix(input: `${string}test`): void;
+
+// works
+if (test.endsWith('test')) requirePostfix(test);
+if (test.endsWith('test', undefined)) requirePostfix(test);
+
+// errors
+if (test.endsWith('test', 0)) requirePostfix(test);
+if (test.endsWith('test', 1)) requirePostfix(test);
+if (test.endsWith('test', 0 as number)) requirePostfix(test);
+if (test.endsWith('test', 1 as number)) requirePostfix(test);
+
+
+//// [stringEndsWith.js]
+// works
+if (test.endsWith('test'))
+    requirePostfix(test);
+if (test.endsWith('test', undefined))
+    requirePostfix(test);
+// errors
+if (test.endsWith('test', 0))
+    requirePostfix(test);
+if (test.endsWith('test', 1))
+    requirePostfix(test);
+if (test.endsWith('test', 0))
+    requirePostfix(test);
+if (test.endsWith('test', 1))
+    requirePostfix(test);
---
+++ tests/baselines/reference/stringStartsWith.js
@@ -0,0 +1,30 @@
+//// [stringStartsWith.ts]
+declare var test: string;
+declare function requirePrefix(input: `test${string}`): void;
+
+// works
+if (test.startsWith('test')) requirePrefix(test);
+if (test.startsWith('test', undefined)) requirePrefix(test);
+if (test.startsWith('test', 0)) requirePrefix(test);
+
+// errors
+if (test.startsWith('test', 1)) requirePrefix(test);
+if (test.startsWith('test', 0 as number)) requirePrefix(test);
+if (test.startsWith('test', 1 as number)) requirePrefix(test);
+
+
+//// [stringStartsWith.js]
+// works
+if (test.startsWith('test'))
+    requirePrefix(test);
+if (test.startsWith('test', undefined))
+    requirePrefix(test);
+if (test.startsWith('test', 0))
+    requirePrefix(test);
+// errors
+if (test.startsWith('test', 1))
+    requirePrefix(test);
+if (test.startsWith('test', 0))
+    requirePrefix(test);
+if (test.startsWith('test', 1))
+    requirePrefix(test);
