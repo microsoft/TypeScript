a831ccd6f63b841dcb9b93741532c83da947efa9
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2022-01-10 12:27:06 -0800

    add and update tests

--- tests/cases/compiler/narrowingDestructuring.ts
+++ tests/cases/compiler/narrowingDestructuring.ts
@@ -1,33 +1,39 @@
-// type X = { kind: "a", a: string } | { kind: "b", b: string }
+type X = { kind: "a", a: string } | { kind: "b", b: string }
 
-// function func<T extends X>(value: T) {
-//     if (value.kind === "a") {
-//         value.a;
-//         const { a } = value;
-//     } else {
-//         const { b } = value;
-//         value.b;
-//     }
-// }
+function func<T extends X>(value: T) {
+    if (value.kind === "a") {
+        value.a;
+        const { a } = value;
+    } else {
+        value.b;
+        const { b } = value;
+    }
+}
 
-// type Z = { e: 1, f: { a: number, b: string, c: number } }
-//     | { e: 2, g: { a: string, b: number, c: string }};
+type Z = { kind: "f", f: { a: number, b: string, c: number } }
+    | { kind: "g", g: { a: string, b: number, c: string }};
 
-// function func2<T extends Z>(value: T) {
-//     if (value.e === 1) {
-//         const { f: { a, ...spread } } = value;
-//         value.e;
-//         value.f;
-//     } else {
-//         const { g: { c, ...spread } } = value;
-//         value.e;
-//         value.g;
-//     }
-// }
+function func2<T extends Z>(value: T) {
+    if (value.kind === "f") {
+        const { f: f1 } = value;
+        const { f: { a, ...spread } } = value;
+        value.f;
+    } else {
+        const { g: { c, ...spread } } = value;
+        value.g;
+    }
+}
 
 function func3<T extends { kind: "a", a: string } | { kind: "b", b: number }>(t: T) {
     if (t.kind === "a") {
-        // const r = (({ kind, a }) => a)(t);
+        const { kind, ...r1 } = t;
         const r2 = (({ kind, ...rest }) => rest)(t);
+    }
+}
+
+function farr<T extends [number, string, string] | [string, number, number]>(x: T) {
+    const [head, ...tail] = x;
+    if (x[0] === 'number') {
+        const [head, ...tail] = x;
     }
 }
