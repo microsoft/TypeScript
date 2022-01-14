291aba9adcbdb39b1ee4e40ac286ba8cff9a2edf
ShuiRuTian <158983297@qq.com>
2022-01-14 21:53:22 +0800

    add tests of jsdoc for intance of class

--- tests/cases/fourslash/quickInfoJsDocInheritage.ts
+++ tests/cases/fourslash/quickInfoJsDocInheritage.ts
@@ -36,6 +36,11 @@
 ////     /*4*/foo2 = (q: string) => { return 1 }
 //// }
 //// 
+//// new C()./*5*/foo1;
+//// new C()./*6*/foo2;
+//// new D()./*7*/foo1;
+//// new D()./*8*/foo2;
+//// 
 //// class Base1 {
 ////     /**
 ////      * @description Base1.foo1 
@@ -53,15 +58,15 @@
 //// // extends class and implement interfaces with duplicate name
 //// // property override method
 //// class Drived1 extends Base1 implements A {
-////     /*5*/foo1: number = 1;
-////     /*6*/foo2(para1: string) { return 1 };
+////     /*9*/foo1: number = 1;
+////     /*10*/foo2(para1: string) { return 1 };
 //// }
 //// 
 //// // extends class and implement interfaces with duplicate name
 //// // method override method
 //// class Drived2 extends Base1 implements B {
-////     /*7*/foo1: number = 1;
-////     /*8*/foo2 = (para1: string) => { return 1; };
+////     /*11*/foo1: number = 1;
+////     /*12*/foo2 = (para1: string) => { return 1; };
 //// }
 //// 
 //// class Base2 {
@@ -80,15 +85,24 @@
 //// // extends class and implement interfaces with duplicate name
 //// // property override method
 //// class Drived3 extends Base2 implements A {
-////     /*9*/foo1: number = 1;
-////     /*10*/foo2(para1: string) { return 1 };
+////     /*13*/foo1: number = 1;
+////     /*14*/foo2(para1: string) { return 1 };
 //// }
 //// 
 //// // extends class and implement interfaces with duplicate name
 //// // method override method
 //// class Drived4 extends Base2 implements B {
-////     /*11*/foo1: number = 1;
-////     /*12*/foo2 = (para1: string) => { return 1; };
+////     /*15*/foo1: number = 1;
+////     /*16*/foo2 = (para1: string) => { return 1; };
 //// }
+//// 
+//// new Drived1()./*17*/foo1;
+//// new Drived1()./*18*/foo2;
+//// new Drived2()./*19*/foo1;
+//// new Drived2()./*20*/foo2;
+//// new Drived3()./*21*/foo1;
+//// new Drived3()./*22*/foo2;
+//// new Drived4()./*23*/foo1;
+//// new Drived4()./*24*/foo2;
 
 verify.baselineQuickInfo();
