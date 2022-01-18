8d9fa685b5a759f2dab9898e7a75de1cb675c188
Eli Barzilay <eli@barzilay.org>
2022-01-18 11:37:22 -0500

    Avoid global mutations
    This removes a few places where global compiler bindings are mutated:
    
    1. Make `sysLog` call an internal binding which is changed via
       `setSysLog`.
    
    2. Use `Object.assign` to change values *in* `objectAllocator` instead
       of mutating the binding itself.  (The type should verify that any
       future uses of this will properly override all bindings.)
    
    3. `localizedDiagnosticMessages` is not really needed as an exported
       value, there's only one place that uses it to test whether it is set
       or not.  So drop the export and replace it with a new
       `maybeSetLocalizedDiagnosticMessages` (internal) function.

--- src/services/services.ts
+++ src/services/services.ts
@@ -1259,10 +1259,9 @@ namespace ts {
             : NoopCancellationToken;
 
         const currentDirectory = host.getCurrentDirectory();
-        // Check if the localized messages json is set, otherwise query the host for it
-        if (!localizedDiagnosticMessages && host.getLocalizedDiagnosticMessages) {
-            setLocalizedDiagnosticMessages(host.getLocalizedDiagnosticMessages());
-        }
+
+        // Checks if the localized messages json is set, and if not, query the host for it
+        maybeSetLocalizedDiagnosticMessages(host.getLocalizedDiagnosticMessages?.bind(host));
 
         function log(message: string) {
             if (host.log) {
