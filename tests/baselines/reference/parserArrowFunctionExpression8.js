c8b46ce64b13d95b76da19edf18a3a17dbe6145d
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-01-21 10:49:17 -0800

    Add failing test case

---
+++ tests/baselines/reference/parserArrowFunctionExpression8.js
@@ -0,0 +1,11 @@
+//// [parserArrowFunctionExpression8.ts]
+x ? x => ({ x }) : x => ({ x })
+
+
+//// [parserArrowFunctionExpression8.js]
+x ? function (x) { return function (_a) {
+    var x = _a.x;
+    return ({ x: x });
+}; }
+    :
+;
