2d79e4f754791be4c3b05ad757678eaafaf66d5f
Anders Hejlsberg <andersh@microsoft.com>
2022-01-30 07:37:57 -0800

    Update tests

--- tests/cases/conformance/types/typeParameters/typeArgumentLists/instantiationExpressionErrors.ts
+++ tests/cases/conformance/types/typeParameters/typeArgumentLists/instantiationExpressionErrors.ts
@@ -1,20 +1,32 @@
 // @strict: true
 // @declaration: true
 
-declare let f: any;
+declare let f: { <T>(): T, g<U>(): U };
 
-// Type arguments only permitted at end of member expression
+// Type arguments in member expressions
 
-const a1 = f<number>;
-const a2 = f.g<number>;
-const a3 = f<number>.g;
-const a4 = f<number>.g<number>;
+const a1 = f<number>;  // { (): number; g<U>(): U; }
+const a2 = f.g<number>;  // () => number
+const a3 = f<number>.g;  // <U>() => U
+const a4 = f<number>.g<number>;  // () => number
+const a5 = f['g']<number>;  // () => number
 
-// Type arguments must follow ?. token
+// `[` is an expression starter and cannot immediately follow a type argument list
 
-const b1 = f?.<number>;
+const a6 = f<number>['g'];  // Error
+const a7 = (f<number>)['g'];
+
+// An `<` cannot immediately follow a type argument list
+
+const a8 = f<number><number>;  // Relational operator error
+const a9 = (f<number>)<number>;  // Error, no applicable signatures
+
+// Type arguments with `?.` token
+
+const b1 = f?.<number>;  // Error, `(` expected
 const b2 = f?.<number>();
 const b3 = f<number>?.();
+const b4 = f<number>?.<number>();  // Error, expected no type arguments
 
 // Parsed as function call, even though this differs from JavaScript
 
