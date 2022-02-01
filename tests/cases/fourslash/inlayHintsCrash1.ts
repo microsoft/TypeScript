c5ec5ecf168d362b1cb9d94ce67dc85f7a0818c7
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-02-01 08:47:34 -0800

    Fix inlay hint crash for jsdoc function type syntax
    Parameters in JSDoc function types do not have names. The type does not
    reflect this. This PR fixes the crash; I'll see how much churn it causes
    to fix the type as well.
    
    Fixes #47606

---
+++ tests/cases/fourslash/inlayHintsCrash1.ts
@@ -0,0 +1,15 @@
+/// <reference path="fourslash.ts" />
+// @allowJs: true
+// @checkJs: true
+// @Filename: foo.js
+//// /**
+////  * @param {function(string): boolean} f
+////  */
+//// function doThing(f/**/) {
+////     f(100)
+//// }
+const markers = test.markers();
+verify.getInlayHints([], undefined, {
+    includeInlayVariableTypeHints: true,
+    includeInlayParameterNameHints: "all",
+});
