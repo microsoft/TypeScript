f5f6447a8930d8f9db39258764cf30742833b136
Felipe Armoni <felipe.armoni@gmail.com>
2022-02-09 18:05:15 -0300

    Added unit test

---
+++ tests/cases/fourslash/organizeImports10.ts
@@ -0,0 +1,13 @@
+/// <reference path="fourslash.ts" />
+
+////import type { ZodType } from 'zod';
+////
+/////** Intended to be used in combination with {@link ZodType} */
+////export function fun() { /* ... */ }
+
+verify.organizeImports(
+  `import type { ZodType } from 'zod';
+
+  /** Intended to be used in combination with {@link ZodType} */
+  export function fun() { /* ... */ }`
+);
