ae39c08c62ae6d12f29c407f1771e10c9219c7a2
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2022-02-22 12:07:55 +0200

    fix(7410): allow using JSXElement as JSXAttributeValue

--- src/compiler/factory/nodeFactory.ts
+++ src/compiler/factory/nodeFactory.ts
@@ -4856,7 +4856,7 @@ namespace ts {
         }
 
         // @api
-        function createJsxAttribute(name: Identifier, initializer: StringLiteral | JsxExpression | undefined) {
+        function createJsxAttribute(name: Identifier, initializer: JsxAttributeValue | undefined) {
             const node = createBaseNode<JsxAttribute>(SyntaxKind.JsxAttribute);
             node.name = name;
             node.initializer = initializer;
@@ -4868,7 +4868,7 @@ namespace ts {
         }
 
         // @api
-        function updateJsxAttribute(node: JsxAttribute, name: Identifier, initializer: StringLiteral | JsxExpression | undefined) {
+        function updateJsxAttribute(node: JsxAttribute, name: Identifier, initializer: JsxAttributeValue | undefined) {
             return node.name !== name
                 || node.initializer !== initializer
                 ? update(createJsxAttribute(name, initializer), node)
