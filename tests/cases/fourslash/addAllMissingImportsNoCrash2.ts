82d6ff37b979b14a05c85e4e1e87afb965d9a44d
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-02-24 12:11:40 -0800

    Test for no fixes instead

--- tests/cases/fourslash/addAllMissingImportsNoCrash2.ts
+++ tests/cases/fourslash/addAllMissingImportsNoCrash2.ts
@@ -5,15 +5,4 @@
 
 goTo.marker();
 
-verify.codeFixAll({
-    fixId: "fixMissingImport",
-    fixAllDescription: "Add all missing imports",
-    newFileContent: 
-`import { Test1, Test2, Test3, Test4 } from './file1';
-interface Testing {
-    test1: Test1;
-    test2: Test2;
-    test3: Test3;
-    test4: Test4;
-}`
-});
+verify.not.codeFixAllAvailable("fixMissingImport");
