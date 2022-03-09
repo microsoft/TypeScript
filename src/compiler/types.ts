fc82c673578b2bfb2b2afe22dea5b172a01fa7e0
Wesley Wigham <wewigham@microsoft.com>
2022-03-09 12:02:11 -0800

    Don't eagerly simplify reducible generic union index types (#46812)

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -5244,6 +5244,8 @@ namespace ts {
         /* @internal */
         restrictiveInstantiation?: Type; // Instantiation with type parameters mapped to unconstrained form
         /* @internal */
+        uniqueLiteralFilledInstantiation?: Type;  // Instantiation with type parameters mapped to never type
+        /* @internal */
         immediateBaseConstraint?: Type;  // Immediate base constraint cache
         /* @internal */
         widened?: Type; // Cached widened form of the type
