78b9da69ae70393317aada3303bb61fa8071a52c
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2022-03-09 17:04:30 -0800

    update test

--- tests/cases/fourslash/completionsOverridingProperties.ts
+++ tests/cases/fourslash/completionsOverridingProperties.ts
@@ -25,12 +25,7 @@ verify.completions({
     includes: [
         {
             name: "foo",
-            sortText: completion.SortText.LocationPriority,
-            replacementSpan: {
-                fileName: "",
-                pos: 0,
-                end: 0,
-            },
+            sortText: completion.SortText.AutoImportSuggestions,
             insertText: "protected foo: string;",
         }
     ],
