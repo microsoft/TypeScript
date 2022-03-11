93c3a30edc7cc4be94bbeb3074a6a678ffd45d3b
Wesley Wigham <wewigham@microsoft.com>
2022-03-11 10:57:22 -0800

    Add assertion on invalid cache set (#43845)

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -32190,6 +32190,9 @@ namespace ts {
                     assignBindingElementTypes(declaration.name, links.type);
                 }
             }
+            else if (type) {
+                Debug.assertEqual(links.type, type, "Parameter symbol already has a cached type which differs from newly assigned type");
+            }
         }
 
         // When contextual typing assigns a type to a parameter that contains a binding pattern, we also need to push
