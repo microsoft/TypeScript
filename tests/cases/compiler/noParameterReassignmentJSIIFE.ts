426329fcc734711e4d7bc3668e2c77439980f1ed
Wesley Wigham <wwigham@gmail.com>
2022-03-14 17:04:42 -0700

    Minimize test

--- tests/cases/compiler/noParameterReassignmentJSIIFE.ts
+++ tests/cases/compiler/noParameterReassignmentJSIIFE.ts
@@ -2,18 +2,8 @@
 // @checkJs: true
 // @noEmit: true
 // @filename: index.js
-"use strict";
-
 self.importScripts = (function (importScripts) {
-  return function () {
-    setTimeout(function () {}, 0);
-    return importScripts.apply(this, arguments);
-  };
+    return function () {
+        return importScripts.apply(this, arguments);
+    };
 })(importScripts);
-
-importScripts("../node_modules/systemjs/dist/system.js");
-importScripts("../systemjs.config.js");
-
-SystemJS.import("pdfjs/core/worker.js").then(function () {
-    // Worker is loaded at this point.
-});
