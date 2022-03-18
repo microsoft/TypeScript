5715ed914c47ee003ff1af90c387d8670ba2d697
MQuy <quy.m@bokio.se>
2022-03-18 21:58:40 +0100

    Add completion for await

---
+++ tests/cases/fourslash/completionOfAwaitPromise8.ts
@@ -0,0 +1,11 @@
+/// <reference path='fourslash.ts'/>
+
+////async function demo() { return { abc: "" }}
+////async function foo() { const a = demo()./**/ }
+
+verify.completions({
+    marker: "",
+    exact: [],
+});
+
+
