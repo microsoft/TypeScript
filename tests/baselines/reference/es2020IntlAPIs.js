d1c4a020414d9a18315393085b8d6e167248b36c
jihndai <73680880+jihndai@users.noreply.github.com>
2022-03-26 19:33:03 -0400

    add another test

--- tests/baselines/reference/es2020IntlAPIs.js
+++ tests/baselines/reference/es2020IntlAPIs.js
@@ -49,6 +49,7 @@ new Intl.Locale(new Intl.Locale('en-US'));
 new Intl.DisplayNames(); // TypeError: invalid_argument
 new Intl.DisplayNames('en'); // TypeError: invalid_argument
 new Intl.DisplayNames('en', {}); // TypeError: invalid_argument
+console.log((new Intl.DisplayNames(undefined, {type: 'language'})).of('en-GB')); // "British English"
 
 const localesArg = ["es-ES", new Intl.Locale("en-US")];
 console.log((new Intl.DisplayNames(localesArg, {type: 'language'})).resolvedOptions().locale); // "es-ES"
@@ -92,6 +93,7 @@ new Intl.Locale(new Intl.Locale('en-US'));
 new Intl.DisplayNames(); // TypeError: invalid_argument
 new Intl.DisplayNames('en'); // TypeError: invalid_argument
 new Intl.DisplayNames('en', {}); // TypeError: invalid_argument
+console.log((new Intl.DisplayNames(undefined, { type: 'language' })).of('en-GB')); // "British English"
 const localesArg = ["es-ES", new Intl.Locale("en-US")];
 console.log((new Intl.DisplayNames(localesArg, { type: 'language' })).resolvedOptions().locale); // "es-ES"
 console.log(Intl.DisplayNames.supportedLocalesOf(localesArg)); // ["es-ES", "en-US"]
--- tests/cases/conformance/es2020/es2020IntlAPIs.ts
+++ tests/cases/conformance/es2020/es2020IntlAPIs.ts
@@ -50,6 +50,7 @@ new Intl.Locale(new Intl.Locale('en-US'));
 new Intl.DisplayNames(); // TypeError: invalid_argument
 new Intl.DisplayNames('en'); // TypeError: invalid_argument
 new Intl.DisplayNames('en', {}); // TypeError: invalid_argument
+console.log((new Intl.DisplayNames(undefined, {type: 'language'})).of('en-GB')); // "British English"
 
 const localesArg = ["es-ES", new Intl.Locale("en-US")];
 console.log((new Intl.DisplayNames(localesArg, {type: 'language'})).resolvedOptions().locale); // "es-ES"
