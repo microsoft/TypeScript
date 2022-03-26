3b70763a8a02b8c2a57b35c9c5f5756b20a450c7
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-03-26 09:59:38 +0100

    Add tests for concrete mapped tuples

---
+++ tests/cases/compiler/mappedTypeConcreteTupleHomomorphism.ts
@@ -0,0 +1,27 @@
+type A = [1, 2]
+
+type B = {
+    [K in keyof A]: `${A[K]}`
+}
+
+const b: B = ['1', '2']
+
+type C<T> = {
+    [K in keyof T]: [K, T[K]]
+}
+
+type D = {
+    [K in keyof C<['c', 'd', 'e']>]: 1
+}
+
+const d: D = [1, 1, 1]
+
+// repro from #27995
+type Foo = ['a', 'b'];
+
+interface Bar {
+    a: string;
+    b: number;
+}
+
+type Baz = { [K in keyof Foo]: Bar[Foo[K]]; };
