c95d6f799dd711852e4cff7fb49f7a5a4386e705
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2022-03-30 23:06:48 +0000

    Added test case.

---
+++ tests/cases/compiler/contextualNarrowingFromUnknownToObjects.ts
@@ -0,0 +1,63 @@
+
+// @strictNullChecks: true, false
+// @target: esnext
+
+declare function keysOfEmptyObject(o: {}): string[];
+declare function keysOfNonPrimitive(o: object): string[];
+
+namespace implicitConstraints {
+    export function keyLengthsEqualUsingEmptyObjectFn<T>(a: T, b: T): [T, T] | undefined {
+        if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
+            return undefined;
+        }
+        if (Array.isArray(a) || Array.isArray(b)) {
+            return undefined;
+        }
+        if (keysOfEmptyObject(a).length !== keysOfEmptyObject(b).length) {
+            return [a, b];
+        }
+        return undefined;
+    }
+    
+    export function keyLengthsEqualUsingNonPrimitiveFn<T>(a: T, b: T): [T, T] | undefined {
+        if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
+            return undefined;
+        }
+        if (Array.isArray(a) || Array.isArray(b)) {
+            return undefined;
+        }
+        if (keysOfNonPrimitive(a).length !== keysOfNonPrimitive(b).length) {
+            return [a, b];
+        }
+        return undefined;
+    }
+}
+
+// Explicit Constraints of 'unknown'
+namespace explicitConstraintsOfUnknown {
+    export function keyLengthsEqualUsingEmptyObjectFn<T extends unknown>(a: T, b: T): [T, T] | undefined {
+        if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
+            return undefined;
+        }
+        if (Array.isArray(a) || Array.isArray(b)) {
+            return undefined;
+        }
+        if (keysOfEmptyObject(a).length !== keysOfEmptyObject(b).length) {
+            return [a, b];
+        }
+        return undefined;
+    }
+    
+    export function keyLengthsEqualUsingNonPrimitiveFn<T extends unknown>(a: T, b: T): [T, T] | undefined {
+        if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
+            return undefined;
+        }
+        if (Array.isArray(a) || Array.isArray(b)) {
+            return undefined;
+        }
+        if (keysOfNonPrimitive(a).length !== keysOfNonPrimitive(b).length) {
+            return [a, b];
+        }
+        return undefined;
+    }
+}
