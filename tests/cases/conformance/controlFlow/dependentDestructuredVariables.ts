fd601ddf20af2a69e56d69fe418d0f80905ce66b
Anders Hejlsberg <andersh@microsoft.com>
2022-03-30 16:33:01 -0700

    CFA for dependent parameters typed by generic constraints (#48411)
    * Obtain apparent type in parameter destructuring check
    
    * Add regression test

--- tests/cases/conformance/controlFlow/dependentDestructuredVariables.ts
+++ tests/cases/conformance/controlFlow/dependentDestructuredVariables.ts
@@ -299,3 +299,16 @@ let fooAsyncGenM: FooAsyncGenMethod = {
     }
   }
 };
+
+// Repro from #48345
+
+type Func = <T extends ["a", number] | ["b", string]>(...args: T) => void;
+
+const f60: Func = (kind, payload) => {
+    if (kind === "a") {
+        payload.toFixed();  // error
+    }
+    if (kind === "b") {
+        payload.toUpperCase();  // error
+    }
+};
