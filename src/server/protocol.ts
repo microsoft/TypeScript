891b51d8931ba794e9716f1e343196a971270949
Andrew Casey <andrew.casey@microsoft.com>
2022-04-04 18:41:56 -0700

    Update doc comment

--- src/server/protocol.ts
+++ src/server/protocol.ts
@@ -1158,8 +1158,10 @@ namespace ts.server.protocol {
         isWriteAccess: boolean;
 
         /**
-         * True if reference is a definition, false otherwise.
-         * TODO (acasey)
+         * Present only if the search was triggered from a declaration.
+         * True indicates that the references refers to the same symbol
+         * (i.e. has the same meaning) as the declaration that began the
+         * search.
          */
         isDefinition?: boolean;
     }
