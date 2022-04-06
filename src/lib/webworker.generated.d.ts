9cedcbd7f9dd46a12e914de8ab7bca103b1450c9
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-04-05 17:00:39 -0700

    manually update webworker to remove AbortSignal.abort

--- src/lib/webworker.generated.d.ts
+++ src/lib/webworker.generated.d.ts
@@ -703,7 +703,7 @@ interface AbortController {
     /** Returns the AbortSignal object associated with this object. */
     readonly signal: AbortSignal;
     /** Invoking this method will set this object's AbortSignal's aborted flag and signal to any observers that the associated activity is to be aborted. */
-    abort(reason?: any): void;
+    // abort(): AbortSignal; - To be re-added in the future
 }
 
 declare var AbortController: {
