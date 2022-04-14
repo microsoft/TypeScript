33eb7391225513b17c693882622e07950b262452
Sheetal Nandi <shkamat@microsoft.com>
2022-04-14 15:00:01 -0700

    Make WriteFileCallback Api ready for future

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -4444,10 +4444,10 @@ namespace ts {
         return combinePaths(newDirPath, sourceFilePath);
     }
 
-    export function writeFile(host: { writeFile: WriteFileCallback; }, diagnostics: DiagnosticCollection, fileName: string, data: string, writeByteOrderMark: boolean, sourceFiles?: readonly SourceFile[], sourceMapUrlPos?: number) {
-        host.writeFile(fileName, data, writeByteOrderMark, hostErrorMessage => {
+    export function writeFile(host: { writeFile: WriteFileCallback; }, diagnostics: DiagnosticCollection, fileName: string, text: string, writeByteOrderMark: boolean, sourceFiles?: readonly SourceFile[], data?: WriteFileCallbackData) {
+        host.writeFile(fileName, text, writeByteOrderMark, hostErrorMessage => {
             diagnostics.add(createCompilerDiagnostic(Diagnostics.Could_not_write_file_0_Colon_1, fileName, hostErrorMessage));
-        }, sourceFiles, sourceMapUrlPos);
+        }, sourceFiles, data);
     }
 
     function ensureDirectoriesExist(
