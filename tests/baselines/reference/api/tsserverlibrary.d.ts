d95f7bf56ec455fd514aea8031566e8c17c86545
Andrew Branch <andrew@wheream.io>
2022-04-18 15:04:17 -0700

    Update API baseline

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -6433,7 +6433,18 @@ declare namespace ts {
         argumentIndex: number;
         argumentCount: number;
     }
+    enum CompletionInfoFlags {
+        None = 0,
+        MayIncludeAutoImports = 1,
+        IsImportStatementCompletion = 2,
+        IsContinuation = 4,
+        ResolvedModuleSpecifiers = 8,
+        ResolvedModuleSpecifiersBeyondLimit = 16,
+        MayIncludeMethodSnippets = 32
+    }
     interface CompletionInfo {
+        /** For performance telemetry. */
+        flags?: CompletionInfoFlags;
         /** Not true for all global completions. This will be true if the enclosing scope matches a few syntax kinds. See `isSnippetScope`. */
         isGlobalCompletion: boolean;
         isMemberCompletion: boolean;
@@ -8821,6 +8832,7 @@ declare namespace ts.server.protocol {
         body?: CompletionInfo;
     }
     interface CompletionInfo {
+        readonly flags?: number;
         readonly isGlobalCompletion: boolean;
         readonly isMemberCompletion: boolean;
         readonly isNewIdentifierLocation: boolean;
