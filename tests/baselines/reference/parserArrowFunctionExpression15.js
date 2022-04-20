562eb0ae15b24f5f88d785d865df30f1da39afb1
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-04-20 11:03:42 -0700

    Add test case

---
+++ tests/baselines/reference/parserArrowFunctionExpression15.js
@@ -0,0 +1,7 @@
+//// [parserArrowFunctionExpression15.ts]
+false ? (param): string => param : null
+
+
+//// [parserArrowFunctionExpression15.js]
+false ? (param) : function (string) { return param; };
+null;
---
+++ tests/baselines/reference/parserArrowFunctionExpression16.js
@@ -0,0 +1,7 @@
+//// [parserArrowFunctionExpression16.ts]
+true ? false ? (param): string => param : null : null
+
+
+//// [parserArrowFunctionExpression16.js]
+true ? false ? (param) : function (string) { return param; } : null;
+null;
