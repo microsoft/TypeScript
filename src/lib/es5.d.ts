c26809abe441a01a0e8974003f10b6ca1a8432a6
Gao Sheng <gaosheng08@meituan.com>
2022-05-01 20:20:27 +0800

    Fix first parameter's type of `JSON.parse`, and fix jsdoc.

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1087,11 +1087,11 @@ declare var URIError: URIErrorConstructor;
 interface JSON {
     /**
      * Converts a JavaScript Object Notation (JSON) string into an object.
-     * @param text A valid JSON string.
+     * @param text A valid JSON string, or can be converted to a valid JSON string.
      * @param reviver A function that transforms the results. This function is called for each member of the object.
      * If a member contains nested objects, the nested objects are transformed before the parent object is.
      */
-    parse(text: string, reviver?: (this: any, key: string, value: any) => any): any;
+    parse(text: any, reviver?: (this: any, key: string, value: any) => any): any;
     /**
      * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
      * @param value A JavaScript value, usually an object or array, to be converted.
