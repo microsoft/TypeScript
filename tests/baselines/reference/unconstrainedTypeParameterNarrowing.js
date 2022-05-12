dc319e766a555db0c8d5c509d979e5a3ef601b14
Wesley Wigham <wwigham@gmail.com>
2022-05-12 14:49:43 -0700

    Expand test

--- tests/baselines/reference/unconstrainedTypeParameterNarrowing.js
+++ tests/baselines/reference/unconstrainedTypeParameterNarrowing.js
@@ -11,6 +11,20 @@ function f2<T extends unknown>(x: T) {
     }
 }
 
+// #48468 but with an explicit constraint so as to not trigger the `{}` and unconstrained type parameter bug
+function deepEquals<T extends unknown>(a: T, b: T) {
+    if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
+        return false;
+    }
+    if (Array.isArray(a) || Array.isArray(b)) {
+        return false;
+    }
+    if (Object.keys(a).length !== Object.keys(b).length) {
+        return false;
+    }
+    return true;
+}
+
 function g(x: object) {}
 
 //// [unconstrainedTypeParameterNarrowing.js]
@@ -24,5 +38,18 @@ function f2(x) {
     if (typeof x === "object" && x) {
         g(x);
     }
+}
+// #48468 but with an explicit constraint so as to not trigger the `{}` and unconstrained type parameter bug
+function deepEquals(a, b) {
+    if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
+        return false;
+    }
+    if (Array.isArray(a) || Array.isArray(b)) {
+        return false;
+    }
+    if (Object.keys(a).length !== Object.keys(b).length) {
+        return false;
+    }
+    return true;
 }
 function g(x) { }
--- tests/cases/compiler/unconstrainedTypeParameterNarrowing.ts
+++ tests/cases/compiler/unconstrainedTypeParameterNarrowing.ts
@@ -11,4 +11,18 @@ function f2<T extends unknown>(x: T) {
     }
 }
 
+// #48468 but with an explicit constraint so as to not trigger the `{}` and unconstrained type parameter bug
+function deepEquals<T extends unknown>(a: T, b: T) {
+    if (typeof a !== "object" || typeof b !== "object" || !a || !b) {
+        return false;
+    }
+    if (Array.isArray(a) || Array.isArray(b)) {
+        return false;
+    }
+    if (Object.keys(a).length !== Object.keys(b).length) {
+        return false;
+    }
+    return true;
+}
+
 function g(x: object) {}
