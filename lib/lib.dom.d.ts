d25007b424287eaa411103fe39f9a93e7916e1d9
Anton Bershanskiy <8knots@protonmail.com>
2022-05-27 21:52:59 +0300

    Add Navigation API

--- lib/lib.dom.d.ts
+++ lib/lib.dom.d.ts
@@ -8902,6 +8902,68 @@ declare var History: {
     new(): History;
 };
 
+interface NavigationOptions {
+    info: any;
+}
+
+interface NavigationHistoryEntry extends EventTarget {
+    readonly url?: string;
+    readonly key: string;
+    readonly id: string;
+    readonly index: number;
+    readonly sameDocument: boolean;
+    getState(): any;
+    ondispose: ((this: NavigationHistoryEntry, ev: Event) => any) | null;
+}
+
+interface NavigationTransition {
+    readonly navigationType: NavigationType;
+    readonly from: NavigationHistoryEntry;
+    readonly finished:  Promise<undefined>;
+}
+
+interface NavigationResult {
+    committed: Promise<NavigationHistoryEntry>;
+    finished: Promise<NavigationHistoryEntry>;
+}
+
+interface NavigationNavigateOptions extends NavigationOptions {
+    state: any;
+    history?: NavigationHistoryBehavior;
+}
+
+interface NavigationReloadOptions extends NavigationOptions {
+    state: any;
+}
+
+interface NavigationUpdateCurrentEntryOptions {
+    state: any;
+}
+
+/** Like History, allows manipulation of the browser session history, that is the pages visited in the tab or frame that the current page is loaded in. */
+interface Navigation extends EventTarget {
+    entries(): Array<NavigationHistoryEntry>;
+    readonly currentEntry?: NavigationHistoryEntry;
+    updateCurrentEntry(options: NavigationUpdateCurrentEntryOptions): undefined;
+    readonly transition?: NavigationTransition;
+    readonly canGoBack: boolean;
+    readonly canGoForward: boolean;
+    navigate(url: string, options?: NavigationNavigateOptions): NavigationResult;
+    reload(options?: NavigationReloadOptions): NavigationResult;
+    traverseTo(key: string, options?: NavigationOptions): NavigationResult;
+    back(options?: NavigationOptions): NavigationResult;
+    forward(options?: NavigationOptions): NavigationResult;
+    onnavigate: ((this: Navigation, ev: Event) => any) | null;
+    onnavigatesuccess: ((this: Navigation, ev: Event) => any) | null;
+    onnavigateerror: ((this: Navigation, ev: Event) => any) | null;
+    oncurrententrychange: ((this: Navigation, ev: Event) => any) | null;
+}
+
+declare var Navigation: {
+    prototype: Navigation;
+    new(): Navigation;
+};
+
 /** This IndexedDB API interface represents a cursor for traversing or iterating over multiple records in a database. */
 interface IDBCursor {
     /**
@@ -17187,6 +17249,7 @@ interface Window extends EventTarget, AnimationFrameProvider, GlobalEventHandler
     readonly frameElement: Element | null;
     readonly frames: WindowProxy;
     readonly history: History;
+    readonly navigation: Navigation;
     readonly innerHeight: number;
     readonly innerWidth: number;
     readonly length: number;
@@ -18875,3 +18938,4 @@ type VisibilityState = "hidden" | "visible";
 type WebGLPowerPreference = "default" | "high-performance" | "low-power";
 type WorkerType = "classic" | "module";
 type XMLHttpRequestResponseType = "" | "arraybuffer" | "blob" | "document" | "json" | "text";
+type NavigationHistoryBehavior = "auto"| "push" | "replace";
--- tests/lib/lib.d.ts
+++ tests/lib/lib.d.ts
@@ -10952,6 +10952,68 @@ declare var History: {
     new(): History;
 }
 
+interface NavigationOptions {
+  info: any;
+}
+
+interface NavigationHistoryEntry extends EventTarget {
+  readonly url?: string;
+  readonly key: string;
+  readonly id: string;
+  readonly index: number;
+  readonly sameDocument: boolean;
+  getState(): any;
+  ondispose: ((this: NavigationHistoryEntry, ev: Event) => any) | null;
+}
+
+interface NavigationTransition {
+  readonly navigationType: NavigationType;
+  readonly from: NavigationHistoryEntry;
+  readonly finished:  Promise<undefined>;
+}
+
+interface NavigationResult {
+  committed: Promise<NavigationHistoryEntry>;
+  finished: Promise<NavigationHistoryEntry>;
+}
+
+interface NavigationNavigateOptions extends NavigationOptions {
+  state: any;
+  history?: NavigationHistoryBehavior;
+}
+
+interface NavigationReloadOptions extends NavigationOptions {
+  state: any;
+}
+
+interface NavigationUpdateCurrentEntryOptions {
+  state: any;
+}
+
+/** Like History, allows manipulation of the browser session history, that is the pages visited in the tab or frame that the current page is loaded in. */
+interface Navigation extends EventTarget {
+  entries(): Array<NavigationHistoryEntry>;
+  readonly currentEntry?: NavigationHistoryEntry;
+  updateCurrentEntry(options: NavigationUpdateCurrentEntryOptions): undefined;
+  readonly transition?: NavigationTransition;
+  readonly canGoBack: boolean;
+  readonly canGoForward: boolean;
+  navigate(url: string, options?: NavigationNavigateOptions): NavigationResult;
+  reload(options?: NavigationReloadOptions): NavigationResult;
+  traverseTo(key: string, options?: NavigationOptions): NavigationResult;
+  back(options?: NavigationOptions): NavigationResult;
+  forward(options?: NavigationOptions): NavigationResult;
+  onnavigate: ((this: Navigation, ev: Event) => any) | null;
+  onnavigatesuccess: ((this: Navigation, ev: Event) => any) | null;
+  onnavigateerror: ((this: Navigation, ev: Event) => any) | null;
+  oncurrententrychange: ((this: Navigation, ev: Event) => any) | null;
+}
+
+declare var Navigation: {
+  prototype: Navigation;
+  new(): Navigation;
+};
+
 interface IDBCursor {
     direction: string;
     key: any;
@@ -16741,6 +16803,7 @@ declare var menubar: BarProp;
 declare var msAnimationStartTime: number;
 declare var name: string;
 declare var navigator: Navigator;
+declare var navigation: Navigation;
 declare var offscreenBuffering: string | boolean;
 declare var onabort: (ev: Event) => any;
 declare var onafterprint: (ev: Event) => any;
