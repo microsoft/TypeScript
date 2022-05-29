7791b1f143ae4768fdfa804f4b560c4d24441344
uhyo <uhyo@uhy.ooo>
2022-05-29 20:43:20 +0900

    add test with undefined default value

--- tests/baselines/reference/destructuringAssignmentWithDefault2.js
+++ tests/baselines/reference/destructuringAssignmentWithDefault2.js
@@ -3,9 +3,15 @@ const a: { x?: number; y?: number } = { };
 
 let x: number;
 
+// Should not error out
 ({ x = 0 } = a);
 ({ x: x = 0} = a);
 ({ y: x = 0} = a);
+
+// Should be error
+({ x = undefined } = a);
+({ x: x = undefined } = a);
+({ y: x = undefined } = a);
 
 
 declare const r: Iterator<number>;
@@ -16,13 +22,18 @@ let value;
 ({ done: done = false, value } = r.next());
 
 //// [destructuringAssignmentWithDefault2.js]
-var _a, _b, _c, _d, _e, _f, _g;
+var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
 var a = {};
 var x;
+// Should not error out
 (_a = a.x, x = _a === void 0 ? 0 : _a);
 (_b = a.x, x = _b === void 0 ? 0 : _b);
 (_c = a.y, x = _c === void 0 ? 0 : _c);
+// Should be error
+(_d = a.x, x = _d === void 0 ? undefined : _d);
+(_e = a.x, x = _e === void 0 ? undefined : _e);
+(_f = a.y, x = _f === void 0 ? undefined : _f);
 var done;
 var value;
-(_d = r.next(), _e = _d.done, done = _e === void 0 ? false : _e, value = _d.value);
-(_f = r.next(), _g = _f.done, done = _g === void 0 ? false : _g, value = _f.value);
+(_g = r.next(), _h = _g.done, done = _h === void 0 ? false : _h, value = _g.value);
+(_j = r.next(), _k = _j.done, done = _k === void 0 ? false : _k, value = _j.value);
--- tests/cases/compiler/destructuringAssignmentWithDefault2.ts
+++ tests/cases/compiler/destructuringAssignmentWithDefault2.ts
@@ -4,9 +4,15 @@ const a: { x?: number; y?: number } = { };
 
 let x: number;
 
+// Should not error out
 ({ x = 0 } = a);
 ({ x: x = 0} = a);
 ({ y: x = 0} = a);
+
+// Should be error
+({ x = undefined } = a);
+({ x: x = undefined } = a);
+({ y: x = undefined } = a);
 
 
 declare const r: Iterator<number>;
