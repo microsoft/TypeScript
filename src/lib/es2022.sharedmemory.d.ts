9609e3260849b5d910c31e0b7d735bf3fafe8118
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-06-01 21:32:49 -0300

    Fix: Removed unused @see at sharedmemory

--- src/lib/es2022.sharedmemory.d.ts
+++ src/lib/es2022.sharedmemory.d.ts
@@ -2,7 +2,6 @@ interface Atomics {
     /**
      * A non-blocking, asynchronous version of wait and usable on the main thread.
      * Waits asynchronously on a shared memory location and returns a Promise
-     * @see wait
      */
     waitAsync(typedArray: BigInt64Array | Int32Array, index: number, value: bigint, timeout?: number): { async: false, value: "ok" | "not-equal" | "timed-out" } | { async: true, value: Promise<"ok" | "not-equal" | "timed-out"> };
 }
