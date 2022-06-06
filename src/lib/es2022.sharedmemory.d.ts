d6f31f78ad419f5e5d493c4608b0dafbfcf51f0d
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-06-06 14:17:02 -0300

    fix: Adjusted grammar changes in jsdoc

--- src/lib/es2022.sharedmemory.d.ts
+++ src/lib/es2022.sharedmemory.d.ts
@@ -1,6 +1,6 @@
 interface Atomics {
     /**
-     * A non-blocking, asynchronous version of wait and usable on the main thread.
+     * A non-blocking, asynchronous version of wait which is usable on the main thread.
      * Waits asynchronously on a shared memory location and returns a Promise
      */
     waitAsync(typedArray: BigInt64Array | Int32Array, index: number, value: bigint, timeout?: number): { async: false, value: "ok" | "not-equal" | "timed-out" } | { async: true, value: Promise<"ok" | "not-equal" | "timed-out"> };
