bdb42e196f57afe803f5954e18f58fbcc0d765c8
uhyo <uhyo@uhy.ooo>
2022-06-09 13:06:54 +0900

    add related span

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -37578,7 +37578,17 @@ namespace ts {
         function checkPotentialUncheckedRenamedBindingElementsInTypes() {
             for (const node of potentialUnusedRenamedBindingElementsInTypes) {
                 if (!(getSymbolOfNode(node)?.isReferenced! & SymbolFlags.Type)) {
-                    error(node.name, Diagnostics._0_is_an_unused_renaming_of_1_Did_you_intend_to_use_it_as_a_type_annotation, declarationNameToString(node.name), declarationNameToString(node.propertyName));
+                    const wrappingDeclaration = walkUpBindingElementsAndPatterns(node);
+                    Debug.assert(isParameterDeclaration(wrappingDeclaration), "Only parameter declaration should be checked here");
+                    const diagnostic = createDiagnosticForNode(node.name, Diagnostics._0_is_an_unused_renaming_of_1_Did_you_intend_to_use_it_as_a_type_annotation, declarationNameToString(node.name), declarationNameToString(node.propertyName));
+                    if (!wrappingDeclaration.type) {
+                        // entire parameter does not have type annotation, suggest adding an annotation
+                        addRelatedInfo(
+                            diagnostic,
+                            createFileDiagnostic(getSourceFileOfNode(wrappingDeclaration), wrappingDeclaration.end - 1, 1,Diagnostics.We_can_only_write_a_type_for_0_by_adding_a_type_for_the_entire_parameter_here, declarationNameToString(wrappingDeclaration.name))
+                        );
+                    }
+                    diagnostics.add(diagnostic);
                 }
             }
         }
