c74367d73f1b60802f37f7b00e53b1c0036c4c0d
Danay Fernandez Alfonso <t-danayf@microsoft.com>
2022-06-14 12:28:00 -0700

    jsdocPropertyDescription

--- tests/cases/compiler/jsdocPropertyDescription.ts
+++
@@ -1,6 +0,0 @@
-import { Example } from "./jsdocPropertyDescription1";
-
-function example(e: Example) {
-    console.log(e.property); // works, shows type and description on hover
-    console.log(e.anything); // shows type of property but not description on hover
-}
--- tests/cases/compiler/jsdocPropertyDescription1.ts
+++
@@ -1,6 +0,0 @@
-export interface Example {
-    /** Something generic */
-    [p: string]: any;
-    /** Something specific */
-    property: number;
-}
