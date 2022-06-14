3fc5f968ca002562508bb1ffb863b8b5e2df4492
Matt Bierner <matb@microsoft.com>
2022-06-14 12:35:53 -0700

    Enable TS Server plugins on web (#47377)
    * Prototype TS plugins on web
    
    This prototype allows service plugins to be loaded on web TSServer
    
    Main changes:
    
    - Adds a new host entryPoint called `importServicePlugin` for overriding how plugins can be loaded. This may be async
    - Implement `importServicePlugin` for webServer
    - The web server plugin implementation looks for a `browser` field in the plugin's `package.json`
    - It then uses `import(...)` to load the plugin (the plugin source must be compiled to support being loaded as a module)
    
    * use default export from plugins
    
    This more or less matches how node plugins expect the plugin module to be an init function
    
    * Allow configure plugin requests against any web servers in partial semantic mode
    
    * Addressing some comments
    
    - Use result value instead of try/catch (`ImportPluginResult`)
    - Add awaits
    - Add logging
    
    * add tsserverWeb to patch in dynamic import
    
    * Remove eval
    
    We should throw instead when dynamic import is not implemented
    
    * Ensure dynamically imported plugins are loaded in the correct order
    
    * Add tests for async service plugin timing
    
    * Update src/server/editorServices.ts
    
    Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
    
    * Partial PR feedback
    
    * Rename tsserverWeb to dynamicImportCompat
    
    * Additional PR feedback
    
    Co-authored-by: Ron Buckton <ron.buckton@microsoft.com>
    Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

--- src/server/session.ts
+++ src/server/session.ts
@@ -696,7 +696,6 @@ namespace ts.server {
         CommandNames.OrganizeImportsFull,
         CommandNames.GetEditsForFileRename,
         CommandNames.GetEditsForFileRenameFull,
-        CommandNames.ConfigurePlugin,
         CommandNames.PrepareCallHierarchy,
         CommandNames.ProvideCallHierarchyIncomingCalls,
         CommandNames.ProvideCallHierarchyOutgoingCalls,
@@ -3344,7 +3343,9 @@ namespace ts.server {
         public executeCommand(request: protocol.Request): HandlerResponse {
             const handler = this.handlers.get(request.command);
             if (handler) {
-                return this.executeWithRequestId(request.seq, () => handler(request));
+                const response = this.executeWithRequestId(request.seq, () => handler(request));
+                this.projectService.enableRequestedPlugins();
+                return response;
             }
             else {
                 this.logger.msg(`Unrecognized JSON command:${stringifyIndented(request)}`, Msg.Err);
--- src/server/types.ts
+++ src/server/types.ts
@@ -5,7 +5,11 @@ declare namespace ts.server {
         data: any;
     }
 
-    export type RequireResult = { module: {}, error: undefined } | { module: undefined, error: { stack?: string, message?: string } };
+    export type ModuleImportResult = { module: {}, error: undefined } | { module: undefined, error: { stack?: string, message?: string } };
+
+    /** @deprecated Use {@link ModuleImportResult} instead. */
+    export type RequireResult = ModuleImportResult;
+
     export interface ServerHost extends System {
         watchFile(path: string, callback: FileWatcherCallback, pollingInterval?: number, options?: WatchOptions): FileWatcher;
         watchDirectory(path: string, callback: DirectoryWatcherCallback, recursive?: boolean, options?: WatchOptions): FileWatcher;
@@ -15,6 +19,7 @@ declare namespace ts.server {
         clearImmediate(timeoutId: any): void;
         gc?(): void;
         trace?(s: string): void;
-        require?(initialPath: string, moduleName: string): RequireResult;
+        require?(initialPath: string, moduleName: string): ModuleImportResult;
+        importServicePlugin?(root: string, moduleName: string): Promise<ModuleImportResult>;
     }
 }
