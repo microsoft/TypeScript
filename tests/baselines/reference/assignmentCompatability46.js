d766cf58c8dbfe2b52feee57a65e85c03be50af4
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2022-06-17 21:12:51 +0300

    fix(49449): change error location when passing objects/arrays to an argument of type

---
+++ tests/baselines/reference/assignmentCompatability46.js
@@ -0,0 +1,10 @@
+//// [assignmentCompatability46.ts]
+declare function fn(x: never): void;
+
+fn([1, 2, 3])
+fn({ a: 1, b: 2 })
+
+
+//// [assignmentCompatability46.js]
+fn([1, 2, 3]);
+fn({ a: 1, b: 2 });
