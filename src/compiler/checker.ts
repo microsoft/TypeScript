a65ebb2a8f0853ec35d49c7f2ca34df8bf31af85
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-06-17 17:07:29 -0700

    Add a big comment

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -30733,7 +30733,13 @@ namespace ts {
             }
 
             result = getCandidateForOverloadFailure(node, candidates, args, !!candidatesOutArray, checkMode);
-            // TODO(jakebailey): explain this, and maybe move getResolvedSignature's caching here.
+            // Preemptively cache the result; getResolvedSignature will do this after we return, but
+            // we need to ensure that the result is present for the error checks below so that if
+            // this signature is encountered again, we handle the circularity (rather than producing a
+            // different result which may produce no errors and assert). Callers of getResolvedSignature
+            // don't hit this issue becuase they only observe this result after it's had a chance to
+            // be cached, but the error reporting code below executes before getResolvedSignature sets
+            // resolvedSignature.
             getNodeLinks(node).resolvedSignature = result;
 
             // No signatures were applicable. Now report errors based on the last applicable signature with
