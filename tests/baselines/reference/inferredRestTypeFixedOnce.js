3ab0071e7809da132be8f87d14ecbdaac9fce573
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-06-29 15:56:21 -0700

    Rename test

--- tests/baselines/reference/inferredRestTypeFixedOnce.js
+++ tests/baselines/reference/inferredRestTypeFixedOnce.js
@@ -1,9 +1,9 @@
-//// [issue49383.ts]
+//// [inferredRestTypeFixedOnce.ts]
 function wrap<Args extends unknown[]>(_: (...args: Args) => void) {}
 wrap(({ cancelable } = {}) => {});
 
 
-//// [issue49383.js]
+//// [inferredRestTypeFixedOnce.js]
 function wrap(_) { }
 wrap(function (_a) {
     var _b = _a === void 0 ? {} : _a, cancelable = _b.cancelable;
