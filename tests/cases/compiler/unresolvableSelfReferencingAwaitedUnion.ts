30a80a99b4702b1445cde1d5ca4153b339404d26
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-06-29 21:33:22 +0200

    Add test from #49723

--- tests/cases/compiler/unresolvableSelfReferencingAwaitedUnion.ts
+++ tests/cases/compiler/unresolvableSelfReferencingAwaitedUnion.ts
@@ -9,3 +9,11 @@ type SimpleType = string | Promise<SimpleType>;
 declare const simple: SimpleType;
 
 const env: EnvFunction = () => simple;
+
+// repro #49723
+
+type T1 = 1 | Promise<T1> | T1[];
+
+export async function myFunction(param: T1) {
+  const awaited = await param
+}
