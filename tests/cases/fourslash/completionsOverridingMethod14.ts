cda8455e451541e7af580f725b5fe1fc6bfe9f57
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2022-07-03 16:49:34 +0300

    fix(49392): show optional class methods with enabled strict option

---
+++ tests/cases/fourslash/completionsOverridingMethod14.ts
@@ -0,0 +1,29 @@
+/// <reference path="fourslash.ts" />
+
+// @Filename: a.ts
+// @strictNullChecks: true
+// @newline: LF
+
+////interface IFoo {
+////    foo?(arg: string): number;
+////}
+////class Foo implements IFoo {
+////    /**/
+////}
+
+verify.completions({
+    marker: "",
+    isNewIdentifierLocation: true,
+    preferences: {
+        includeCompletionsWithInsertText: true,
+        includeCompletionsWithSnippetText: false,
+        includeCompletionsWithClassMemberSnippets: true,
+    },
+    includes: [
+        {
+            name: "foo",
+            sortText: completion.SortText.ClassMemberSnippets,
+            insertText: "foo(arg: string): number {\n}"
+        },
+    ],
+});
