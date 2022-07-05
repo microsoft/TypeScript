67632ff13895a06dbe4916ac186f488be3f0137e
magic-akari <akari.ccino@gmail.com>
2022-07-06 01:06:47 +0800

    Update test case

--- tests/baselines/reference/systemModule18.js
+++ tests/baselines/reference/systemModule18.js
@@ -1,8 +1,32 @@
-//// [systemModule18.ts]
-export import React = require("react");
+//// [tests/cases/compiler/systemModule18.ts] ////
 
-//// [systemModule18.js]
-System.register(["react"], function (exports_1, context_1) {
+//// [react.ts]
+export function createElement() {}
+export function lazy() {}
+export function useState() {}
+
+//// [index.ts]
+export import React = require("./react.js");
+
+
+//// [react.js]
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    function createElement() { }
+    exports_1("createElement", createElement);
+    function lazy() { }
+    exports_1("lazy", lazy);
+    function useState() { }
+    exports_1("useState", useState);
+    return {
+        setters: [],
+        execute: function () {
+        }
+    };
+});
+//// [index.js]
+System.register(["./react.js"], function (exports_1, context_1) {
     "use strict";
     var React;
     var __moduleName = context_1 && context_1.id;
--- tests/cases/compiler/systemModule18.ts
+++ tests/cases/compiler/systemModule18.ts
@@ -1,3 +1,9 @@
 // @module: system
 
-export import React = require("react");
+// @filename: react.ts
+export function createElement() {}
+export function lazy() {}
+export function useState() {}
+
+// @filename: index.ts
+export import React = require("./react.js");
