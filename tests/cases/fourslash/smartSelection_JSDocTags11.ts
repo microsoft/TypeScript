3a4f43edcaa2d8e289c356ea6b650328265dea6e
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2022-07-06 10:57:25 +0000

    ⚗️ Add test case for JSDoc smart selection (#39618)
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

---
+++ tests/cases/fourslash/smartSelection_JSDocTags11.ts
@@ -0,0 +1,10 @@
+/// <reference path="fourslash.ts" />
+
+////const x = 1;
+////type Foo = {
+////  /** comment */
+////  /**/status: number;
+////};
+
+// https://github.com/microsoft/TypeScript/issues/39618
+verify.baselineSmartSelection();
