8ba9d2c98bed038e94f41591bec451f36928fad0
Danay Fernandez <dfern237@fiu.edu>
2022-07-07 11:58:01 -0700

    added additional test

--- tests/cases/fourslash/jsdocPropertyDescription.ts
+++ tests/cases/fourslash/jsdocPropertyDescription.ts
@@ -47,3 +47,14 @@ verify.quickInfoAt("literal", "(index) LiteralExample[`data-${string}`]: string"
 //// }
 
 verify.quickInfoAt("multiple", "(index) MultipleExample[string | number | symbol]: string","Something generic")
+
+////export interface Multiple1Example {
+////  /** Something generic */
+////    [key: number | symbol | `data-${string}` | `data-${number}`]: string;
+////}
+
+//// function multiple1Example(e: Multiple1Example) {
+////    console.log(e./*multiple1*/anything);
+//// }
+
+verify.quickInfoAt("multiple1", "(index) Multiple1Example[number | symbol | `data-${string}` | `data-${number}`]: string","Something generic")
