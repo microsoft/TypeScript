c850390991786be8a9d68a7c230cd53b8c554fcd
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-07-07 09:59:29 -0700

    More did-you-mean errors on classes in plain JS
    Classes
    - that are declared with class declarations or expressions, but not
      constructor functions
    - and which have nothing in their heritage clauses
    
    Now provide spelling suggestions on misspelled property accesses.
    
    Because of JS support for assignment-as-declaration, it's still easy
    to mistakenly declare a property with a typo and not get any
    suggestions.

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -29277,9 +29277,10 @@ m2: ${(this.mapper2 as unknown as DebugTypeMapper).__debugToString().split("\n")
             if (file) {
                 if (compilerOptions.checkJs === undefined && file.checkJsDirective === undefined && (file.scriptKind === ScriptKind.JS || file.scriptKind === ScriptKind.JSX)) {
                     const declarationFile = forEach(suggestion?.declarations, getSourceFileOfNode);
+                    const suggestionHasNoExtends = !suggestion?.valueDeclaration || !isClassLike(suggestion.valueDeclaration) || suggestion.valueDeclaration.heritageClauses?.length
                     return !(file !== declarationFile && !!declarationFile && isGlobalSourceFile(declarationFile))
-                        && !(excludeClasses && suggestion && suggestion.flags & SymbolFlags.Class)
-                        && !(!!node && excludeClasses && isPropertyAccessExpression(node) && node.expression.kind === SyntaxKind.ThisKeyword);
+                        && !(excludeClasses && suggestion && suggestion.flags & SymbolFlags.Class && suggestionHasNoExtends)
+                        && !(!!node && excludeClasses && isPropertyAccessExpression(node) && node.expression.kind === SyntaxKind.ThisKeyword && suggestionHasNoExtends);
                 }
             }
             return false;
