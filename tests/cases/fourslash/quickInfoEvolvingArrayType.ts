ee0b57867f04ee0454fbb347cbc600ebf12740ca
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-07-18 15:51:37 -0700

    Don't leak EvolvingArray out of code flow

---
+++ tests/cases/fourslash/quickInfoEvolvingArrayType.ts
@@ -0,0 +1,15 @@
+///<reference path="fourslash.ts" />
+
+// @strict: true
+// @checkJs: true
+// @allowJs: true
+// @outDir: ./dist
+
+// filename: index.js
+//// var C = [];
+//// for (var a in C)
+////     if (C.hasOwn/*1*/Property(a)) {
+////     }
+//// }
+
+verify.baselineQuickInfo();
