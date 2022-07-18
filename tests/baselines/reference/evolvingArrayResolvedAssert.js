9a5ef9b8d93fe4910a5cc75cc17a2a4279f3c6e9
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-07-18 16:34:25 -0700

    Switch to compiler test

---
+++ tests/baselines/reference/evolvingArrayResolvedAssert.js
@@ -0,0 +1,15 @@
+//// [index.js]
+var C = [];
+for (var a in C) {
+    if (C.hasOwnProperty(a)) {
+    }
+}
+
+
+//// [index.js]
+"use strict";
+var C = [];
+for (var a in C) {
+    if (C.hasOwnProperty(a)) {
+    }
+}
---
+++ tests/cases/compiler/evolvingArrayResolvedAssert.ts
@@ -0,0 +1,11 @@
+// @strict: true
+// @checkJs: true
+// @allowJs: true
+// @outDir: ./out
+
+// @filename: index.js
+var C = [];
+for (var a in C) {
+    if (C.hasOwnProperty(a)) {
+    }
+}
