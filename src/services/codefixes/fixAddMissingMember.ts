4faee2d42b3da4cab0da6a01bb5dfddd8de6f4ca
Danay <60160912+danay1999@users.noreply.github.com>
2022-07-20 11:32:28 -0700

    fixAddMissingMemberTruncation

--- src/services/codefixes/fixAddMissingMember.ts
+++ src/services/codefixes/fixAddMissingMember.ts
@@ -514,7 +514,7 @@ namespace ts.codefix {
         }
         if (type.flags & TypeFlags.EnumLike) {
             const enumMember = type.symbol.exports ? firstOrUndefined(arrayFrom(type.symbol.exports.values())) : type.symbol;
-            const name = checker.symbolToExpression(type.symbol.parent ? type.symbol.parent : type.symbol, SymbolFlags.Value, /*enclosingDeclaration*/ undefined, /*flags*/ undefined);
+            const name = checker.symbolToExpression(type.symbol.parent ? type.symbol.parent : type.symbol, SymbolFlags.Value, /*enclosingDeclaration*/ undefined, /*flags*/ NodeBuilderFlags.NoTruncation);
             return enumMember === undefined || name === undefined ? factory.createNumericLiteral(0) : factory.createPropertyAccessExpression(name, checker.symbolToString(enumMember));
         }
         if (type.flags & TypeFlags.NumberLiteral) {
