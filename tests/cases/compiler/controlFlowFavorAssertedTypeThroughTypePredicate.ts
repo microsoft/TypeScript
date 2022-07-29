7610468c2b405594e6e947df2b4ef7928c078110
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-07-29 20:52:08 +0200

    Add requested test cases

--- tests/cases/compiler/controlFlowFavorAssertedTypeThroughTypePredicate.ts
+++ tests/cases/compiler/controlFlowFavorAssertedTypeThroughTypePredicate.ts
@@ -3,20 +3,38 @@
 
 // repro 49988#issuecomment-1192016929
 
-declare function isObject(value: unknown): value is Record<string, unknown>;
+declare function isObject1(value: unknown): value is Record<string, unknown>;
 
 declare const obj1: {};
-obj1;  // {}
-if (isObject(obj1)) {
-    obj1;  // Record<string, unknown>
+if (isObject1(obj1)) {
+    obj1;
     obj1['attr'];
 }
-obj1;  // Record<string, unknown>
+// check type after conditional block
+obj1;
 
 declare const obj2: {} | undefined;
-obj2;  // {} | undefined
-if (isObject(obj2)) {
-    obj2;  // Record<string, unknown>
+if (isObject1(obj2)) {
+    obj2;
     obj2['attr'];
 }
-obj2;  // Record<string, unknown> | undefined
+// check type after conditional block
+obj2;
+
+declare function isObject2(value: unknown): value is {};
+
+declare const obj3: Record<string, unknown>;
+if (isObject2(obj3)) {
+    obj3;
+    obj3['attr'];
+}
+// check type after conditional block
+obj3;
+
+declare const obj4: Record<string, unknown> | undefined;
+if (isObject2(obj4)) {
+    obj4;
+    obj4['attr'];
+}
+// check type after conditional block
+obj4;
