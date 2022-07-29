7983a20bda3fb6b90bf30c2a74d93fae2a58993f
Nicolas Rodriguez <programnicolas@gmail.com>
2022-07-29 09:03:01 -0400

    test: add failing test for #19577

---
+++ tests/baselines/reference/nonNullFullInference.js
@@ -0,0 +1,33 @@
+//// [nonNullFullInference.ts]
+// https://github.com/microsoft/TypeScript/issues/19577
+
+function testNonNullInference(numbers: number[]) {
+    let last;
+
+    for (const n of numbers) {
+        if (n % 2) {
+            return n;
+        }
+
+        last = n;
+    }
+
+    last; // number | undefined
+    last!; // number
+}
+
+
+//// [nonNullFullInference.js]
+// https://github.com/microsoft/TypeScript/issues/19577
+function testNonNullInference(numbers) {
+    var last;
+    for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
+        var n = numbers_1[_i];
+        if (n % 2) {
+            return n;
+        }
+        last = n;
+    }
+    last; // number | undefined
+    last; // number
+}
---
+++ tests/cases/compiler/nonNullFullInference.ts
@@ -0,0 +1,17 @@
+// @noImplicitAny: true
+// https://github.com/microsoft/TypeScript/issues/19577
+
+function testNonNullInference(numbers: number[]) {
+    let last;
+
+    for (const n of numbers) {
+        if (n % 2) {
+            return n;
+        }
+
+        last = n;
+    }
+
+    last; // number | undefined
+    last!; // number
+}
