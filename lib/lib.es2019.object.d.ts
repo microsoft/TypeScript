898810ab274fd60d2cc971fbc93e6eee290eb0a4
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2022-08-05 20:27:18 -0700

    Add stricter Object.fromEntries for constructing from const tuples
    `Object.fromEntries` as is declared today resulted in a loss of key names and a unionization of all values, or a complete loss of information and type of `any`. 
    This change makes it so that it is possible to create strict objects from const tuples.
    
    ```ts
    const obj1 = Object.fromEntries([
      ['1', 2],
      ['3', 4],
    ] as const)
    
    // now results in an object of type: {1: 2, 3: 4}, previously { [k: string]: 2 | 4 }
    
    const obj2 = Object.fromEntries([
      ['1', 2],
      ['3', '4'],
    ] as const)
    
    // now results in an object of type: {1: 2, 3: '4'}, previously any
    ```

--- lib/lib.es2019.object.d.ts
+++ lib/lib.es2019.object.d.ts
@@ -25,6 +25,14 @@ interface ObjectConstructor {
      * Returns an object created by key-value entries for properties and methods
      * @param entries An iterable object that contains key-value entries for properties and methods.
      */
+    <KeyValue extends readonly [any, any]>(
+      entries: Iterable<KeyValue>,
+    ) => { [K in KeyValue[0]]: KeyValue extends readonly [K, infer V] ? V : never };
+
+    /**
+     * Returns an object created by key-value entries for properties and methods
+     * @param entries An iterable object that contains key-value entries for properties and methods.
+     */
     fromEntries<T = any>(entries: Iterable<readonly [PropertyKey, T]>): { [k: string]: T };
 
     /**
