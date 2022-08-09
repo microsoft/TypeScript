e5b400c162339747d99606f0343455a7b37d954d
Jack Morrison <32654372+jackmorrison12@users.noreply.github.com>
2022-08-10 00:21:56 +0100

    Fix misleading Date constructor documentation (addresses #49350) (#49649)
    * Update month parameter to monthIndex and add docs for Date constructor
    
    Signed-off-by: jmorrison152 <jmorrison152@bloomberg.net>
    
    * Add baseline changes
    
    Signed-off-by: jmorrison152 <jmorrison152@bloomberg.net>
    
    * Fix misleading Date constructor documentation (addresses #49350) (#73)
    
    * Update month parameter to monthIndex and add docs for Date constructor
    
    Signed-off-by: jmorrison152 <jmorrison152@bloomberg.net>
    
    * Add baseline changes
    
    Signed-off-by: jmorrison152 <jmorrison152@bloomberg.net>
    
    * Update baselines

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -888,7 +888,17 @@ interface Date {
 interface DateConstructor {
     new(): Date;
     new(value: number | string): Date;
-    new(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): Date;
+    /**
+     * Creates a new Date.
+     * @param year The full year designation is required for cross-century date accuracy. If year is between 0 and 99 is used, then year is assumed to be 1900 + year.
+     * @param monthIndex The month as a number between 0 and 11 (January to December).
+     * @param date The date as a number between 1 and 31.
+     * @param hours Must be supplied if minutes is supplied. A number from 0 to 23 (midnight to 11pm) that specifies the hour.
+     * @param minutes Must be supplied if seconds is supplied. A number from 0 to 59 that specifies the minutes.
+     * @param seconds Must be supplied if milliseconds is supplied. A number from 0 to 59 that specifies the seconds.
+     * @param ms A number from 0 to 999 that specifies the milliseconds.
+     */
+    new(year: number, monthIndex: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): Date;
     (): string;
     readonly prototype: Date;
     /**
@@ -899,14 +909,14 @@ interface DateConstructor {
     /**
      * Returns the number of milliseconds between midnight, January 1, 1970 Universal Coordinated Time (UTC) (or GMT) and the specified date.
      * @param year The full year designation is required for cross-century date accuracy. If year is between 0 and 99 is used, then year is assumed to be 1900 + year.
-     * @param month The month as a number between 0 and 11 (January to December).
+     * @param monthIndex The month as a number between 0 and 11 (January to December).
      * @param date The date as a number between 1 and 31.
      * @param hours Must be supplied if minutes is supplied. A number from 0 to 23 (midnight to 11pm) that specifies the hour.
      * @param minutes Must be supplied if seconds is supplied. A number from 0 to 59 that specifies the minutes.
      * @param seconds Must be supplied if milliseconds is supplied. A number from 0 to 59 that specifies the seconds.
      * @param ms A number from 0 to 999 that specifies the milliseconds.
      */
-    UTC(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): number;
+    UTC(year: number, monthIndex: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): number;
     now(): number;
 }
 
