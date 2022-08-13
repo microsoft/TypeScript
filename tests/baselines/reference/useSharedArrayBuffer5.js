cc4bc841f234a836ebb22ecfebd645929a014c8b
龙腾道 <LongTengDao@LongTengDao.com>
2022-08-14 06:49:00 +0800

    Update useSharedArrayBuffer5.js

--- tests/baselines/reference/useSharedArrayBuffer5.js
+++ tests/baselines/reference/useSharedArrayBuffer5.js
@@ -1,9 +1,9 @@
 //// [useSharedArrayBuffer5.ts]
 var foge = new SharedArrayBuffer(1024);
-var species = foge[Symbol.species];
+var species = SharedArrayBuffer[Symbol.species];
 var stringTag = foge[Symbol.toStringTag];
 
 //// [useSharedArrayBuffer5.js]
 var foge = new SharedArrayBuffer(1024);
-var species = foge[Symbol.species];
+var species = SharedArrayBuffer[Symbol.species];
 var stringTag = foge[Symbol.toStringTag];
