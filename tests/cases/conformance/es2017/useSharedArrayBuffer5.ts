7de170e35a99a93249d857d16dbed933d05b8c49
龙腾道 <LongTengDao@LongTengDao.com>
2022-08-13 16:59:34 +0800

    Update useSharedArrayBuffer5.ts

--- tests/cases/conformance/es2017/useSharedArrayBuffer5.ts
+++ tests/cases/conformance/es2017/useSharedArrayBuffer5.ts
@@ -2,5 +2,5 @@
 // @lib: es6,es2017.sharedmemory
 
 var foge = new SharedArrayBuffer(1024);
-var species = foge[Symbol.species];
+var species = foge.constructor[Symbol.species];
 var stringTag = foge[Symbol.toStringTag];
