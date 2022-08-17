03e442bf5e73f65af4b0704e2ed582a2315149cc
ltdk <usr@ltdk.xyz>
2022-08-17 01:46:48 -0400

    JSON value => JSON-representable value

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1115,29 +1115,29 @@ interface JSON {
      */
     parse(text: string, reviver?: (this: unknown, key: string, value: unknown) => unknown): any;
     /**
-     * Converts a JSON value to a JavaScript Object Notation (JSON) string.
-     * @param value A JSON value, usually an object or array, to be converted.
+     * Converts a JSON-representable value to a JavaScript Object Notation (JSON) string.
+     * @param value A JSON-representable value, usually an object or array, to be converted.
      * @param replacer A function that transforms the results.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
     stringify(value: boolean | number | string | object, replacer?: (this: unknown, key: string, value: unknown) => unknown, space?: string | number): string;
     /**
-     * Converts a JSON value to a JavaScript Object Notation (JSON) string.
-     * @param value A JSON value, usually an object or array, to be converted.
+     * Converts a JSON-representable value to a JavaScript Object Notation (JSON) string.
+     * @param value A JSON-representable value, usually an object or array, to be converted.
      * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
     stringify(value: boolean | number | string | object, replacer?: (number | string)[] | null, space?: string | number): string;
     /**
      * Converts a JavaScript value to a JavaScript Object Notation (JSON) string or undefined.
-     * @param value A JavaScript value to be converted, which could potentially be a non-JSON value.
+     * @param value A JavaScript value to be converted, which could potentially be a non-JSON-representable value.
      * @param replacer A function that transforms the results.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
     stringify(value: unknown, replacer?: (this: unknown, key: string, value: unknown) => unknown, space?: string | number): string | undefined;
     /**
      * Converts a JavaScript value to a JavaScript Object Notation (JSON) string or undefined.
-     * @param value A JavaScript value to be converted, which could potentially be a non-JSON value.
+     * @param value A JavaScript value to be converted, which could potentially be a non-JSON-representable value.
      * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
