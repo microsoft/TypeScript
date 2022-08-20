f19b25650e1cb6a54e96a0ab7292515bae42bba5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-08-19 19:02:58 -0700

    More closely match rulesdir implementation

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 const rulesDir = path.join(__dirname, "scripts", "eslint", "rules");
-const ruleFiles = fs.readdirSync(rulesDir).filter(p => p !== "utils.js");
+const ext = ".js";
+const ruleFiles = fs.readdirSync(rulesDir).filter((p) => p.endsWith(ext));
 
 module.exports = Object.fromEntries(ruleFiles.map((p) => {
-    const name = path.parse(p).name;
-    return [name, require(path.join(rulesDir, p))];
+    return [p.slice(0, -ext.length), require(path.join(rulesDir, p))];
 }));
