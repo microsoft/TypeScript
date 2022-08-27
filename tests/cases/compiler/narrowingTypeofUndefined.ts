5ffac1f3dc0e299449f3799bf08c279f3f048ad5
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2022-08-27 09:56:10 +0300

    add additional test cases

--- tests/cases/compiler/narrowingTypeofUndefined.ts
+++ tests/cases/compiler/narrowingTypeofUndefined.ts
@@ -1,5 +1,15 @@
-declare const a: { error: { a: string }, result: undefined } | { error: undefined, result: { b: number } }
+declare const a: { error: { prop: string }, result: undefined } | { error: undefined, result: { prop: number } }
 
 if (typeof a.error === 'undefined') {
-    a.result.b; // ok
+    a.result.prop; // number
+}
+else {
+    a.error.prop; // string
+}
+
+if (typeof a.error !== 'undefined') {
+    a.error.prop; // string
+}
+else {
+    a.result.prop; // number
 }
