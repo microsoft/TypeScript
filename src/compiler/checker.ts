cd8f630c3f410873dc3bad0fa15b4ea395bd50d7
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2022-09-01 22:39:26 +0300

    fix(50591): check declarations defined before use

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -23569,7 +23569,7 @@ namespace ts {
                     }
                 }
 
-                if (hasOnlyExpressionInitializer(declaration)) {
+                if (hasOnlyExpressionInitializer(declaration) && isBlockScopedNameDeclaredBeforeUse(declaration, node.argumentExpression)) {
                     const initializer = getEffectiveInitializer(declaration);
                     return initializer && tryGetNameFromType(getTypeOfExpression(initializer));
                 }
