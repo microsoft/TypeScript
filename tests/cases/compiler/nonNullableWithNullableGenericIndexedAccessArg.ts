873dfea5d2e871c7150e302c8ffd50344ed1a727
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-09-01 12:53:40 +0200

    Add an additional test case

--- tests/cases/compiler/nonNullableWithNullableGenericIndexedAccessArg.ts
+++ tests/cases/compiler/nonNullableWithNullableGenericIndexedAccessArg.ts
@@ -1,6 +1,8 @@
 // @noEmit: true
 // @strict: true
 
+// repro from #50539
+
 interface StateSchema {
   states?: {
     [key: string]: StateSchema;
@@ -14,3 +16,18 @@ declare class StateNode<TStateSchema extends StateSchema> {
 type StateNodesConfig<TStateSchema extends StateSchema> = {
   [K in keyof TStateSchema["states"]]: StateNode<NonNullable<TStateSchema["states"]>[K]>;
 };
+
+// repro from #50539#issuecomment-1234067835
+
+type Ordering<TOrderBy extends string> = {
+    orderBy: TOrderBy
+}
+
+type Query<TOrderBy extends string> = {
+    order?: Ordering<TOrderBy>
+}
+
+type QueryHandler<
+    TQuery extends Query<TOrderBy>,
+    TOrderBy extends string = NonNullable<TQuery["order"]>["orderBy"]
+> = {}
