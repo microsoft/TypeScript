a90305990725ecff566485a970d5603f5f0d7b3e
Isabel Duan <isabelduan@microsoft.com>
2022-09-06 16:13:18 -0700

    optional arguments in refactorAvailable return `true` even if there is no single refactor that satisfies both

--- tests/cases/fourslash/fixExtractToInnerFunctionDuplicaton.ts
+++ tests/cases/fourslash/fixExtractToInnerFunctionDuplicaton.ts
@@ -4,7 +4,11 @@
 
 goTo.select("x","y");
 verify.refactorAvailable("Extract Symbol", 'function_scope_0', "Extract to inner function in function 'foo'");
+verify.refactorAvailable("Extract Symbol", 'function_scope_1', "Extract to function in global scope");
+
 verify.not.refactorAvailable("Extract Symbol", 'constant_scope_0', "Extract to inner function in function 'foo'");
 verify.refactorAvailable("Extract Symbol", 'constant_scope_0', "Extract to constant in enclosing scope");
+verify.refactorAvailable("Extract Symbol", 'constant_scope_1', "Extract to constant in global scope");
+verify.refactorAvailable("Extract Symbol", 'constant_scope_0', "Extract to constant in global scope");
 
 
