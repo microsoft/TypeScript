c0643c1913d72dc04e6955f0ac7a391b7011c792
Laura Harker <lharker@google.com>
2022-09-14 11:41:16 -0700

    Update & rename new unit test

---
+++ tests/baselines/reference/declNamespaceReexportingLocal.js
@@ -0,0 +1,25 @@
+//// [tests/cases/compiler/declNamespaceReexportingLocal.ts] ////
+
+//// [a.d.ts]
+declare namespace ns {
+  class Local {}
+  export {Local as ExportedAlias};
+
+  namespace _debugger {
+    class Foo {}
+  }
+  export {_debugger as debugger};
+  export function getFoo(): _debugger.Foo;
+}
+
+//// [b.ts]
+export const NsExportedAlias = ns.ExportedAlias;
+export const nsDebuggerFoo = ns.getFoo();
+
+
+//// [b.js]
+"use strict";
+exports.__esModule = true;
+exports.nsDebuggerFoo = exports.NsExportedAlias = void 0;
+exports.NsExportedAlias = ns.ExportedAlias;
+exports.nsDebuggerFoo = ns.getFoo();
