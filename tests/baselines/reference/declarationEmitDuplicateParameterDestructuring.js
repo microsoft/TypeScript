ba4a46651dcf3e0f67697ad777f79c72b8589a40
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-09-14 14:32:10 -0700

    Partially revert #41044

--- tests/baselines/reference/declarationEmitDuplicateParameterDestructuring.js
+++ tests/baselines/reference/declarationEmitDuplicateParameterDestructuring.js
@@ -7,40 +7,11 @@ export const fn2 = ({ prop: a }: { prop: number }, { prop: b }: { prop: number }
 
 
 //// [declarationEmitDuplicateParameterDestructuring.d.ts]
-export declare const fn1: ({ prop, prop }: {
+export declare const fn1: ({ prop: a, prop: b }: {
     prop: number;
 }) => number;
-export declare const fn2: ({ prop }: {
+export declare const fn2: ({ prop: a }: {
     prop: number;
-}, { prop }: {
+}, { prop: b }: {
     prop: number;
 }) => number;
-
-
-//// [DtsFileErrors]
-
-
-tests/cases/compiler/declarationEmitDuplicateParameterDestructuring.d.ts(1,30): error TS2300: Duplicate identifier 'prop'.
-tests/cases/compiler/declarationEmitDuplicateParameterDestructuring.d.ts(1,36): error TS2300: Duplicate identifier 'prop'.
-tests/cases/compiler/declarationEmitDuplicateParameterDestructuring.d.ts(4,30): error TS2300: Duplicate identifier 'prop'.
-tests/cases/compiler/declarationEmitDuplicateParameterDestructuring.d.ts(6,6): error TS2300: Duplicate identifier 'prop'.
-
-
-==== tests/cases/compiler/declarationEmitDuplicateParameterDestructuring.d.ts (4 errors) ====
-    export declare const fn1: ({ prop, prop }: {
-                                 ~~~~
-!!! error TS2300: Duplicate identifier 'prop'.
-                                       ~~~~
-!!! error TS2300: Duplicate identifier 'prop'.
-        prop: number;
-    }) => number;
-    export declare const fn2: ({ prop }: {
-                                 ~~~~
-!!! error TS2300: Duplicate identifier 'prop'.
-        prop: number;
-    }, { prop }: {
-         ~~~~
-!!! error TS2300: Duplicate identifier 'prop'.
-        prop: number;
-    }) => number;
-    
