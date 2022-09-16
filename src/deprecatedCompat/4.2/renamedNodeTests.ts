bc1f2d72ce5beaee83959e51fa6fc68870ab3bb6
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-09-16 14:32:04 -0700

    CONVERSION STEP - unindent
    This step makes further commits look clearer by unindenting all of the top level namespaces preemptively.

--- src/deprecatedCompat/4.2/renamedNodeTests.ts
+++ src/deprecatedCompat/4.2/renamedNodeTests.ts
@@ -4,14 +4,14 @@
 //     - warn: 4.3
 //     - error: 5.0
 namespace ts {
-    /**
-     * @deprecated Use `isMemberName` instead.
-     */
-    export const isIdentifierOrPrivateIdentifier = Debug.deprecate(function isIdentifierOrPrivateIdentifier(node: Node): node is MemberName {
-        return isMemberName(node);
-    }, {
-        since: "4.2",
-        warnAfter: "4.3",
-        message: "Use `isMemberName` instead."
-    });
+/**
+ * @deprecated Use `isMemberName` instead.
+ */
+export const isIdentifierOrPrivateIdentifier = Debug.deprecate(function isIdentifierOrPrivateIdentifier(node: Node): node is MemberName {
+    return isMemberName(node);
+}, {
+    since: "4.2",
+    warnAfter: "4.3",
+    message: "Use `isMemberName` instead."
+});
 }
