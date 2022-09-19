e12b99df83942aaf98a8a9c4cb984ab57d6b3692
Andrew Branch <andrew@wheream.io>
2022-09-19 16:21:10 -0700

    Run test varying by strictNullChecks

--- tests/baselines/reference/stringLiteralFreshnessContextuallyTypedByGeneric2(strictnullchecks=true).js
+++ tests/baselines/reference/stringLiteralFreshnessContextuallyTypedByGeneric2(strictnullchecks=true).js
@@ -1,5 +1,4 @@
-// @strictNullChecks: true, false
-
+//// [stringLiteralFreshnessContextuallyTypedByGeneric2.ts]
 type Model = { s: string; b: boolean }
 declare let pick: <Keys extends keyof Model>(properties: readonly Keys[]) => Pick<Model, Keys>
 declare let transform1: <T>(obj: T) => T
@@ -10,3 +9,10 @@ const result2 = transform2(pick(["s"]))
 
 const intermediate = pick(["s"])
 const result3 = transform1(intermediate)
+
+
+//// [stringLiteralFreshnessContextuallyTypedByGeneric2.js]
+var result1 = transform1(pick(["s"]));
+var result2 = transform2(pick(["s"]));
+var intermediate = pick(["s"]);
+var result3 = transform1(intermediate);
