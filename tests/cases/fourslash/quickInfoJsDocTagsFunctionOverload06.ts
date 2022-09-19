57dff9e8e86e4641606805d93f98e7595a6e5423
Valerian Clerc <vclerc@microsoft.com>
2022-09-19 15:50:14 -0700

    Add test case to check for the propogation of @deprecation tag to multiple overloads

---
+++ tests/cases/fourslash/quickInfoJsDocTagsFunctionOverload06.ts
@@ -0,0 +1,23 @@
+/// <reference path="fourslash.ts" />
+// @target: esnext
+
+// @Filename: /a.ts
+/////**
+//// * A function that gets the length of an array.
+//// * @param testParam
+//// * @deprecated Use the new method instead.
+//// */
+////export function getArrayLength(arr: string[]): number;
+////export function getArrayLength(arr: boolean[]): number;
+
+// @Filename: /b.ts
+////import { getArrayLength } from "/a";
+/////*1*/getArrayLength(["1"]);
+/////*2*/getArrayLength([true]);
+
+
+goTo.file("/b.ts");
+goTo.marker("1")
+verify.quickInfoIs("(alias) getArrayLength(arr: string[]): number (+1 overload)\nimport getArrayLength", "A function that gets the length of an array.", [{name: "param", text: "testParam"}, {name: "deprecated", text: "Use the new method instead."}])
+goTo.marker("2")
+verify.quickInfoIs("(alias) getArrayLength(arr: boolean[]): number (+1 overload)\nimport getArrayLength", "A function that gets the length of an array.", [{name: "param", text: "testParam"}])
