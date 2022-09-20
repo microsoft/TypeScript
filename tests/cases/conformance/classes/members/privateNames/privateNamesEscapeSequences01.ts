77d71094cf2edb05b24fe962c603a4988d91b604
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2022-09-20 07:19:13 +0000

    Add tests for identifiers and private identifiers with escape sequences.

---
+++ tests/cases/conformance/classes/members/privateNames/privateNamesEscapeSequences01.ts
@@ -0,0 +1,105 @@
+// @target: es5,es2015
+
+// @filename: IdentifierNameWithEscape1.ts
+export class IdentifierNameWithEscape1 {
+    \u0078: number;
+
+    constructor() {
+        this.\u0078 = 0;
+    }
+
+    doThing() {
+        this.x = 42;
+    }
+}
+
+// @filename: IdentifierNameWithEscape2.ts
+export class IdentifierNameWithEscape2 {
+    x\u0078: number;
+
+    constructor() {
+        this.x\u0078 = 0;
+    }
+
+    doThing() {
+        this.xx = 42;
+    }
+}
+
+// @filename: IdentifierNameWithExtendedEscape1.ts
+export class IdentifierNameWithExtendedEscape1 {
+    \u{78}: number;
+
+    constructor() {
+        this.\u{78} = 0;
+    }
+
+    doThing() {
+        this.x = 42;
+    }
+}
+
+// @filename: IdentifierNameWithExtendedEscape2.ts
+export class IdentifierNameWithExtendedEscape2 {
+    x\u{78}: number;
+
+    constructor() {
+        this.x\u{78} = 0;
+    }
+
+    doThing() {
+        this.xx = 42;
+    }
+}
+
+// @filename: PrivateIdentifierNameWithEscape1.ts
+export class PrivateIdentifierWithEscape1 {
+    #\u0078: number;
+
+    constructor() {
+        this.#\u0078 = 0;
+    }
+
+    doThing() {
+        this.#x = 42;
+    }
+}
+
+// @filename: PrivateIdentifierNameWithEscape2.ts
+export class PrivateIdentifierWithEscape2 {
+    #x\u0078: number;
+
+    constructor() {
+        this.#x\u0078 = 0;
+    }
+
+    doThing() {
+        this.#xx = 42;
+    }
+}
+
+// @filename: PrivateIdentifierNameWithExtendedEscape1.ts
+export class PrivateIdentifierWithExtendedEscape1 {
+    #\u{78}: number;
+
+    constructor() {
+        this.#\u{78} = 0;
+    }
+
+    doThing() {
+        this.#x = 42;
+    }
+}
+
+// @filename: PrivateIdentifierNameWithExtendedEscape2.ts
+export class PrivateIdentifierWithExtendedEscape2 {
+    #x\u{78}: number;
+
+    constructor() {
+        this.#x\u{78} = 0;
+    }
+
+    doThing() {
+        this.#xx = 42;
+    }
+}
