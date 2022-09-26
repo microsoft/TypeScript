6242ee31a7cf36ae2ff29024d0145352220a6c45
Andrew Branch <andrew@wheream.io>
2022-09-26 11:40:13 -0700

    Test a non-portable declaration emit issue

---
+++ tests/baselines/reference/minimal_nodeModules_declarationEmit.js
@@ -0,0 +1,20 @@
+//// [tests/cases/conformance/moduleResolution/minimal_nodeModules_declarationEmit.ts] ////
+
+//// [module.d.ts]
+export declare class SomeExportedClass {
+  private foo: any;
+}
+
+declare global {
+  function returnsPrivateClassOhNo(): SomeExportedClass;
+}
+
+//// [main.ts]
+export const boom = returnsPrivateClassOhNo();
+
+
+//// [main.js]
+"use strict";
+exports.__esModule = true;
+exports.boom = void 0;
+exports.boom = returnsPrivateClassOhNo();
