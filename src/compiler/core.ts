dc8ee4f0197b1f36efdf44df79b068ab3c47b2fe
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-09-26 16:44:27 -0700

    Consolidate checks that test if the current environment is Node

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -2910,3 +2910,19 @@ function trimEndImpl(s: string) {
     }
     return s.slice(0, end + 1);
 }
+
+declare const process: any;
+
+/** @internal */
+export function isNodeLikeSystem(): boolean {
+    // This is defined here rather than in sys.ts to prevent a cycle from its
+    // use in performanceCore.ts.
+    //
+    // We don't use the presence of `require` to check if we are in Node;
+    // when bundled using esbuild, this function will be rewritten to `__require`
+    // and definitely exist.
+    return typeof process !== "undefined"
+        && process.nextTick
+        && !process.browser
+        && typeof module === "object";
+}
