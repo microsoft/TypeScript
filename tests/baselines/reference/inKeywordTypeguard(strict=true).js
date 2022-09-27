4fa7eacef023516a2b3b0747f54bb4852626e5eb
Anders Hejlsberg <andersh@microsoft.com>
2022-09-26 17:46:05 -0700

    Accept new baselines

--- tests/baselines/reference/inKeywordTypeguard(strict=true).js
+++ tests/baselines/reference/inKeywordTypeguard(strict=true).js
@@ -271,6 +271,60 @@ function f9(x: object) {
     }
 }
 
+function f10(x: { a: unknown }) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+
+function f11(x: { a: any }) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+
+function f12(x: { a: string }) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+
+function f13(x: { a?: string }) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+
+function f14(x: { a: string | undefined }) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+
+function f15(x: { a?: string | undefined }) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+
 // Repro from #50639
 
 function foo<A>(value: A) {
@@ -278,6 +332,11 @@ function foo<A>(value: A) {
         value;  // A & object & Record<"prop", unknown>
     }
 }
+
+// Repro from #50954
+
+const checkIsTouchDevice = () =>
+    "ontouchstart" in window || "msMaxTouchPoints" in window.navigator;
 
 
 //// [inKeywordTypeguard.js]
@@ -534,9 +593,59 @@ function f9(x) {
         x[sym];
     }
 }
+function f10(x) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+function f11(x) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+function f12(x) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+function f13(x) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+function f14(x) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
+function f15(x) {
+    if ("a" in x) {
+        x;
+    }
+    else {
+        x;
+    }
+}
 // Repro from #50639
 function foo(value) {
     if (typeof value === "object" && value !== null && "prop" in value) {
         value; // A & object & Record<"prop", unknown>
     }
 }
+// Repro from #50954
+const checkIsTouchDevice = () => "ontouchstart" in window || "msMaxTouchPoints" in window.navigator;
