297f892ef2e9d46ea4eafec41bea11642eadf7b9
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2022-09-28 15:46:58 -0700

    remove booleans from literals

--- tests/cases/fourslash/completionsLiterals.ts
+++ tests/cases/fourslash/completionsLiterals.ts
@@ -1,8 +1,7 @@
 /// <reference path="fourslash.ts" />
 
 ////const x: 0 | "one" = /**/;
-////const y: 0 | "one" | 1n | true = /*1*/;
-////const z: boolean = /*2*/;
+////const y: 0 | "one" | 1n = /*1*/;
 
 verify.completions({
     marker: "",
@@ -18,16 +17,6 @@ verify.completions({
         { name: "0", kind: "string", text: "0" },
         { name: '"one"', kind: "string", text: '"one"' },
         { name: "1n", kind: "string", text: "1n" },
-        { name: "true", kind: "keyword", text: "true", sortText: completion.SortText.GlobalsOrKeywords },
-    ],
-    isNewIdentifierLocation: true,
-});
-
-verify.completions({
-    marker: "2",
-    includes: [
-        { name: "true", kind: "keyword", text: "true", sortText: completion.SortText.GlobalsOrKeywords },
-        { name: "false", kind: "keyword", text: "false", sortText: completion.SortText.GlobalsOrKeywords },
     ],
     isNewIdentifierLocation: true,
 });
