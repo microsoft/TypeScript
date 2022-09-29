9695a8d588f18a96b0532d94b065b29ee675515e
Bruno Crosier <bruno.crosier@gmail.com>
2022-09-30 00:37:08 +0100

    feat: :label: stricter types for `JSON.parse` and `JSON.stringify`

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1108,6 +1108,16 @@ interface URIErrorConstructor extends ErrorConstructor {
 
 declare var URIError: URIErrorConstructor;
 
+interface CircularReference<T> {
+    [key: string | number | symbol]: T
+}
+type DisallowedJSONParseText = "" | `${string}${",]" | ",}" | ", ]" | ", }"}`;
+type DisallowedJSONValue<T> = bigint | {
+    [key: string | number | symbol]: bigint
+} | CircularReference<T>;
+
+type AllowedJSONParseText = "true" | "false" | "null" | `${number}` | `"${string}"` | `[${string}]` | `{${string}}`;
+
 interface JSON {
     /**
      * Converts a JavaScript Object Notation (JSON) string into an object.
@@ -1115,21 +1125,36 @@ interface JSON {
      * @param reviver A function that transforms the results. This function is called for each member of the object.
      * If a member contains nested objects, the nested objects are transformed before the parent object is.
      */
-    parse(text: string, reviver?: (this: any, key: string, value: any) => any): any;
+    parse<T extends string>(
+        text: T extends DisallowedJSONParseText
+            ? never
+            : T extends AllowedJSONParseText
+            ? T
+            : never,
+        reviver?: (this: any, key: string, value: any) => any
+    ): any;
     /**
      * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
      * @param value A JavaScript value, usually an object or array, to be converted.
      * @param replacer A function that transforms the results.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
-    stringify(value: any, replacer?: (this: any, key: string, value: any) => any, space?: string | number): string;
+    stringify<T>(
+        value: T extends DisallowedJSONValue<T> ? never : T,
+        replacer?: (this: any, key: string, value: any) => any,
+        space?: string | number
+    ): string;
     /**
      * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
      * @param value A JavaScript value, usually an object or array, to be converted.
      * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
-    stringify(value: any, replacer?: (number | string)[] | null, space?: string | number): string;
+    stringify<T>(
+        value: T extends DisallowedJSONValue<T> ? never : T,
+        replacer?: (number | string)[] | null,
+        space?: string | number
+    ): string;
 }
 
 /**
