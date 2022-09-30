938cd647073ce463d98a2c08d74a3087273ec22f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-09-30 19:02:41 +0200

    Fixed an issue with allowing too many generic accesses

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -12372,15 +12372,17 @@ namespace ts {
                 if (t.flags & TypeFlags.Index) {
                     if ((t as IndexType).type.flags & TypeFlags.IndexedAccess) {
                         const indexedAccess = (t as IndexType).type as IndexedAccessType;
-                        const baseObjectType = getBaseConstraint(indexedAccess.objectType);
-                        const baseIndexType = getBaseConstraint(indexedAccess.indexType);
-                        const indexedAccessType = baseObjectType && baseIndexType && getIndexedAccessTypeOrUndefined(baseObjectType, baseIndexType);
-                        const mappedIndexTypeOfIndexedAccess = indexedAccessType && mapType(indexedAccessType, getIndexType);
-                        const narrowed = mappedIndexTypeOfIndexedAccess && filterType(keyofConstraintType, t => {
-                            return !(getIntersectionType([t, mappedIndexTypeOfIndexedAccess]).flags & TypeFlags.Never);
-                        });
-                        if (narrowed) {
-                            return narrowed;
+                        // generic objects can always be instantiated with more keys so we can't narrow down those cases
+                        if (!isGenericObjectType(indexedAccess.objectType)) {
+                            const baseIndexType = getBaseConstraint(indexedAccess.indexType);
+                            const indexedAccessType = baseIndexType && getIndexedAccessTypeOrUndefined(indexedAccess.objectType, baseIndexType);
+                            const mappedIndexTypeOfIndexedAccess = indexedAccessType && mapType(indexedAccessType, getIndexType);
+                            const narrowed = mappedIndexTypeOfIndexedAccess && filterType(keyofConstraintType, t => {
+                                return !(getIntersectionType([t, mappedIndexTypeOfIndexedAccess]).flags & TypeFlags.Never);
+                            });
+                            if (narrowed) {
+                                return narrowed;
+                            }
                         }
                     }
                     return keyofConstraintType;
