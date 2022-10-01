6ef29046b1f4568f8f33e7398fe2a1e10c819a71
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-09-30 17:00:04 -0700

    Unify static/instance node->symbol->type path

--- src/services/goToDefinition.ts
+++ src/services/goToDefinition.ts
@@ -172,13 +172,14 @@ namespace ts.GoToDefinition {
 
         const baseTypeNode = getEffectiveBaseTypeNode(baseDeclaration);
         if (!baseTypeNode) return;
-        const base = typeChecker.getSymbolAtLocation(baseTypeNode);
+        const expression = skipParentheses(baseTypeNode.expression);
+        const base = isClassExpression(expression) ? expression.symbol : typeChecker.getSymbolAtLocation(expression);
         if (!base) return;
 
         const name = unescapeLeadingUnderscores(getTextOfPropertyName(classElement.name));
         const symbol = hasStaticModifier(classElement)
-            ? typeChecker.getPropertyOfType(typeChecker.getDeclaredTypeOfSymbol(base), name)
-            : typeChecker.getPropertyOfType(typeChecker.getTypeOfSymbolAtLocation(base, baseDeclaration), name);
+            ? typeChecker.getPropertyOfType(typeChecker.getTypeOfSymbol(base), name)
+            : typeChecker.getPropertyOfType(typeChecker.getDeclaredTypeOfSymbol(base), name)
         if (!symbol) return;
 
         return getDefinitionFromSymbol(typeChecker, symbol, node);
