92415cdf48d422f372d50a6cbe2fa55b3fa6b625
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-10-03 08:23:01 -0700

    Revert "Make getSymbolAtLocation support class expressions"
    This reverts commit 4c1b03135576c9e5d146ce6f38e691c804cbb0dd.

--- src/services/goToDefinition.ts
+++ src/services/goToDefinition.ts
@@ -172,7 +172,8 @@ namespace ts.GoToDefinition {
 
         const baseTypeNode = getEffectiveBaseTypeNode(baseDeclaration);
         if (!baseTypeNode) return;
-        const base = typeChecker.getSymbolAtLocation(baseTypeNode.expression);
+        const expression = skipParentheses(baseTypeNode.expression);
+        const base = isClassExpression(expression) ? expression.symbol : typeChecker.getSymbolAtLocation(expression);
         if (!base) return;
 
         const name = unescapeLeadingUnderscores(getTextOfPropertyName(classElement.name));
