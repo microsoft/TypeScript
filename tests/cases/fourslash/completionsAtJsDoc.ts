1d901bfa82f7f166ee3779f8541741d6a25dc52d
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2022-10-04 23:34:20 +0000

    Updated test case and baseline.

--- tests/cases/fourslash/completionsAtJsDoc.ts
+++ tests/cases/fourslash/completionsAtJsDoc.ts
@@ -5,4 +5,11 @@
 //// * @type {Foo<{/**/}>}
 //// */
 
-verify.completions({ marker: "", exact: [], isNewIdentifierLocation: true });
+verify.completions({
+    marker: "",
+    exact: {
+        name: "readonly",
+        sortText: completion.SortText.GlobalsOrKeywords
+    },
+    isNewIdentifierLocation: true,
+});
