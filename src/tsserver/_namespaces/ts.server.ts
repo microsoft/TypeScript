5365b45f08d6abd9471f470c4a65228857eebdde
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-10-13 11:00:11 -0700

    Export ts namespace from tsserver for hacky-post patching

--- src/tsserver/_namespaces/ts.server.ts
+++ src/tsserver/_namespaces/ts.server.ts
@@ -5,4 +5,4 @@ export * from "../../server/_namespaces/ts.server";
 export * from "../../webServer/_namespaces/ts.server";
 export * from "../nodeServer";
 export * from "../webServer";
-export * from "../server";
+export * from "../common";
