6f4b9024897736a42d8c9b4e2ba38dc47debd48f
Sheetal Nandi <shkamat@microsoft.com>
2022-10-13 15:51:13 -0700

    Remove affectsEmit from listFilesOnly

---
+++ tests/baselines/reference/tsc/listFilesOnly/combined-with-incremental.js
@@ -0,0 +1,105 @@
+Input::
+//// [/lib/lib.d.ts]
+/// <reference no-default-lib="true"/>
+interface Boolean {}
+interface Function {}
+interface CallableFunction {}
+interface NewableFunction {}
+interface IArguments {}
+interface Number { toExponential: any; }
+interface Object {}
+interface RegExp {}
+interface String { charAt: any; }
+interface Array<T> { length: number; [n: number]: T; }
+interface ReadonlyArray<T> {}
+declare const console: { log(msg: any): void; };
+
+//// [/src/test.ts]
+export const x = 1;
+
+//// [/src/tsconfig.json]
+{}
+
+
+
+Output::
+/lib/tsc -p /src --incremental --listFilesOnly
+/lib/lib.d.ts
+/src/test.ts
+exitCode:: ExitStatus.Success
+
+
+
+
+Change:: no-change-run
+Input::
+
+
+Output::
+/lib/tsc -p /src --incremental
+exitCode:: ExitStatus.Success
+
+
+//// [/src/test.js]
+"use strict";
+exports.__esModule = true;
+exports.x = void 0;
+exports.x = 1;
+
+
+//// [/src/tsconfig.tsbuildinfo]
+{"program":{"fileNames":["../lib/lib.d.ts","./test.ts"],"fileInfos":[{"version":"3858781397-/// <reference no-default-lib=\"true\"/>\ninterface Boolean {}\ninterface Function {}\ninterface CallableFunction {}\ninterface NewableFunction {}\ninterface IArguments {}\ninterface Number { toExponential: any; }\ninterface Object {}\ninterface RegExp {}\ninterface String { charAt: any; }\ninterface Array<T> { length: number; [n: number]: T; }\ninterface ReadonlyArray<T> {}\ndeclare const console: { log(msg: any): void; };","affectsGlobalScope":true},"-12038591281-export const x = 1;"],"referencedMap":[],"exportedModulesMap":[],"semanticDiagnosticsPerFile":[1,2]},"version":"FakeTSVersion"}
+
+//// [/src/tsconfig.tsbuildinfo.readable.baseline.txt]
+{
+  "program": {
+    "fileNames": [
+      "../lib/lib.d.ts",
+      "./test.ts"
+    ],
+    "fileInfos": {
+      "../lib/lib.d.ts": {
+        "version": "3858781397-/// <reference no-default-lib=\"true\"/>\ninterface Boolean {}\ninterface Function {}\ninterface CallableFunction {}\ninterface NewableFunction {}\ninterface IArguments {}\ninterface Number { toExponential: any; }\ninterface Object {}\ninterface RegExp {}\ninterface String { charAt: any; }\ninterface Array<T> { length: number; [n: number]: T; }\ninterface ReadonlyArray<T> {}\ndeclare const console: { log(msg: any): void; };",
+        "signature": "3858781397-/// <reference no-default-lib=\"true\"/>\ninterface Boolean {}\ninterface Function {}\ninterface CallableFunction {}\ninterface NewableFunction {}\ninterface IArguments {}\ninterface Number { toExponential: any; }\ninterface Object {}\ninterface RegExp {}\ninterface String { charAt: any; }\ninterface Array<T> { length: number; [n: number]: T; }\ninterface ReadonlyArray<T> {}\ndeclare const console: { log(msg: any): void; };",
+        "affectsGlobalScope": true
+      },
+      "./test.ts": {
+        "version": "-12038591281-export const x = 1;",
+        "signature": "-12038591281-export const x = 1;"
+      }
+    },
+    "referencedMap": {},
+    "exportedModulesMap": {},
+    "semanticDiagnosticsPerFile": [
+      "../lib/lib.d.ts",
+      "./test.ts"
+    ]
+  },
+  "version": "FakeTSVersion",
+  "size": 689
+}
+
+
+
+Change:: no-change-run
+Input::
+
+
+Output::
+/lib/tsc -p /src --incremental --listFilesOnly
+/lib/lib.d.ts
+/src/test.ts
+exitCode:: ExitStatus.Success
+
+
+
+
+Change:: no-change-run
+Input::
+
+
+Output::
+/lib/tsc -p /src --incremental
+exitCode:: ExitStatus.Success
+
+
