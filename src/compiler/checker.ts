9e69d6f561b3af5cfdefbd52b5d7c2fd3d196d4b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-10-18 10:30:20 +0200

    Use `TypeFlags.Simplifiable` instead of hand-written bits

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -27605,7 +27605,7 @@ namespace ts {
                 // replaced with their constraints similar to the apparent type.
                 if (
                     inferenceContext &&
-                    (contextFlags! & ContextFlags.Signature || maybeTypeOfKind(contextualType, TypeFlags.Conditional | TypeFlags.IndexedAccess)) &&
+                    (contextFlags! & ContextFlags.Signature || maybeTypeOfKind(contextualType, TypeFlags.Simplifiable)) &&
                     some(inferenceContext.inferences, hasInferenceCandidatesOrDefault)
                 ) {
                     // For contextual signatures we incorporate all inferences made so far, e.g. from return
