e17cca1eb37f81023628c6a5d2c7ba6da9307b99
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-10-19 16:20:51 -0700

    Generated module conversion step - unindent
    This step makes further commits look clearer by unindenting all of the top level namespaces preemptively.

--- src/testRunner/unittests/config/tsconfigParsingWatchOptions.ts
+++ src/testRunner/unittests/config/tsconfigParsingWatchOptions.ts
@@ -1,173 +1,173 @@
 namespace ts {
-    describe("unittests:: config:: tsconfigParsingWatchOptions:: parseConfigFileTextToJson", () => {
-        function createParseConfigHost(additionalFiles?: vfs.FileSet) {
-            return new fakes.ParseConfigHost(
-                new vfs.FileSystem(
-                    /*ignoreCase*/ false,
-                    {
-                        cwd: "/",
-                        files: { "/": {}, "/a.ts": "", ...additionalFiles }
-                    }
-                )
-            );
-        }
-        function getParsedCommandJson(json: object, additionalFiles?: vfs.FileSet, existingWatchOptions?: WatchOptions) {
-            return parseJsonConfigFileContent(
-                json,
-                createParseConfigHost(additionalFiles),
-                "/",
-                /*existingOptions*/ undefined,
-                "tsconfig.json",
-                /*resolutionStack*/ undefined,
-                /*extraFileExtensions*/ undefined,
-                /*extendedConfigCache*/ undefined,
-                existingWatchOptions,
-            );
-        }
+describe("unittests:: config:: tsconfigParsingWatchOptions:: parseConfigFileTextToJson", () => {
+    function createParseConfigHost(additionalFiles?: vfs.FileSet) {
+        return new fakes.ParseConfigHost(
+            new vfs.FileSystem(
+                /*ignoreCase*/ false,
+                {
+                    cwd: "/",
+                    files: { "/": {}, "/a.ts": "", ...additionalFiles }
+                }
+            )
+        );
+    }
+    function getParsedCommandJson(json: object, additionalFiles?: vfs.FileSet, existingWatchOptions?: WatchOptions) {
+        return parseJsonConfigFileContent(
+            json,
+            createParseConfigHost(additionalFiles),
+            "/",
+            /*existingOptions*/ undefined,
+            "tsconfig.json",
+            /*resolutionStack*/ undefined,
+            /*extraFileExtensions*/ undefined,
+            /*extendedConfigCache*/ undefined,
+            existingWatchOptions,
+        );
+    }
 
-        function getParsedCommandJsonNode(json: object, additionalFiles?: vfs.FileSet, existingWatchOptions?: WatchOptions) {
-            const parsed = parseJsonText("tsconfig.json", JSON.stringify(json));
-            return parseJsonSourceFileConfigFileContent(
-                parsed,
-                createParseConfigHost(additionalFiles),
-                "/",
-                /*existingOptions*/ undefined,
-                "tsconfig.json",
-                /*resolutionStack*/ undefined,
-                /*extraFileExtensions*/ undefined,
-                /*extendedConfigCache*/ undefined,
-                existingWatchOptions,
-            );
-        }
+    function getParsedCommandJsonNode(json: object, additionalFiles?: vfs.FileSet, existingWatchOptions?: WatchOptions) {
+        const parsed = parseJsonText("tsconfig.json", JSON.stringify(json));
+        return parseJsonSourceFileConfigFileContent(
+            parsed,
+            createParseConfigHost(additionalFiles),
+            "/",
+            /*existingOptions*/ undefined,
+            "tsconfig.json",
+            /*resolutionStack*/ undefined,
+            /*extraFileExtensions*/ undefined,
+            /*extendedConfigCache*/ undefined,
+            existingWatchOptions,
+        );
+    }
 
-        interface VerifyWatchOptions {
-            json: object;
-            additionalFiles?: vfs.FileSet;
-            existingWatchOptions?: WatchOptions | undefined;
-        }
+    interface VerifyWatchOptions {
+        json: object;
+        additionalFiles?: vfs.FileSet;
+        existingWatchOptions?: WatchOptions | undefined;
+    }
 
-        function verifyWatchOptions(subScenario: string, scenario: () => VerifyWatchOptions[]) {
-            describe(subScenario, () => {
-                it("with json api", () => {
-                    const baseline: string[] = [];
-                    for (const { json, additionalFiles, existingWatchOptions } of scenario()) {
-                        addToBaseLine(baseline, json, getParsedCommandJson(json, additionalFiles, existingWatchOptions));
-                    }
-                    runBaseline(`${subScenario} with json api`, baseline);
-                });
-
-                it("with json source file api", () => {
-                    const baseline: string[] = [];
-                    for (const { json, additionalFiles, existingWatchOptions, } of scenario()) {
-                        addToBaseLine(baseline, json, getParsedCommandJsonNode(json, additionalFiles, existingWatchOptions));
-                    }
-                    runBaseline(`${subScenario} with jsonSourceFile api`, baseline);
-                });
+    function verifyWatchOptions(subScenario: string, scenario: () => VerifyWatchOptions[]) {
+        describe(subScenario, () => {
+            it("with json api", () => {
+                const baseline: string[] = [];
+                for (const { json, additionalFiles, existingWatchOptions } of scenario()) {
+                    addToBaseLine(baseline, json, getParsedCommandJson(json, additionalFiles, existingWatchOptions));
+                }
+                runBaseline(`${subScenario} with json api`, baseline);
             });
-            function addToBaseLine(baseline: string[], json: object, parsed: ParsedCommandLine) {
-                baseline.push(`Input:: ${JSON.stringify(json, /*replacer*/ undefined, " ")}`);
-                baseline.push(`Result: WatchOptions::`);
-                baseline.push(JSON.stringify(parsed.watchOptions, /*replacer*/ undefined, " "));
-                baseline.push(`Result: Errors::`);
-                baseline.push(formatDiagnosticsWithColorAndContext(parsed.errors, {
-                    getCurrentDirectory: () => "/",
-                    getCanonicalFileName: identity,
-                    getNewLine: () => "\n"
-                }));
+
+            it("with json source file api", () => {
+                const baseline: string[] = [];
+                for (const { json, additionalFiles, existingWatchOptions, } of scenario()) {
+                    addToBaseLine(baseline, json, getParsedCommandJsonNode(json, additionalFiles, existingWatchOptions));
+                }
+                runBaseline(`${subScenario} with jsonSourceFile api`, baseline);
+            });
+        });
+        function addToBaseLine(baseline: string[], json: object, parsed: ParsedCommandLine) {
+            baseline.push(`Input:: ${JSON.stringify(json, /*replacer*/ undefined, " ")}`);
+            baseline.push(`Result: WatchOptions::`);
+            baseline.push(JSON.stringify(parsed.watchOptions, /*replacer*/ undefined, " "));
+            baseline.push(`Result: Errors::`);
+            baseline.push(formatDiagnosticsWithColorAndContext(parsed.errors, {
+                getCurrentDirectory: () => "/",
+                getCanonicalFileName: identity,
+                getNewLine: () => "\n"
+            }));
+        }
+        function runBaseline(subScenario: string, baseline: readonly string[]) {
+            Harness.Baseline.runBaseline(`config/tsconfigParsingWatchOptions/${subScenario}.js`, baseline.join("\n"));
+        }
+    }
+
+    verifyWatchOptions("no watchOptions specified option", () => [{
+        json: {},
+    }]);
+
+    verifyWatchOptions("empty watchOptions specified option", () => [{
+        json: { watchOptions: {} },
+    }]);
+
+    verifyWatchOptions("when extending config file without watchOptions", () => [
+        {
+            json: {
+                extends: "./base.json",
+                watchOptions: { watchFile: "UseFsEvents" }
+            },
+            additionalFiles: { "/base.json": "{}" }
+        },
+        {
+            json: { extends: "./base.json", },
+            additionalFiles: { "/base.json": "{}" }
+        }
+    ]);
+
+    verifyWatchOptions("when extending config file with watchOptions", () => [
+        {
+            json: {
+                extends: "./base.json",
+                watchOptions: {
+                    watchFile: "UseFsEvents",
+                }
+            },
+            additionalFiles: {
+                "/base.json": JSON.stringify({
+                    watchOptions: {
+                        watchFile: "UseFsEventsOnParentDirectory",
+                        watchDirectory: "FixedPollingInterval"
+                    }
+                })
             }
-            function runBaseline(subScenario: string, baseline: readonly string[]) {
-                Harness.Baseline.runBaseline(`config/tsconfigParsingWatchOptions/${subScenario}.js`, baseline.join("\n"));
+        },
+        {
+            json: {
+                extends: "./base.json",
+            },
+            additionalFiles: {
+                "/base.json": JSON.stringify({
+                    watchOptions: {
+                        watchFile: "UseFsEventsOnParentDirectory",
+                        watchDirectory: "FixedPollingInterval"
+                    }
+                })
             }
         }
+    ]);
 
-        verifyWatchOptions("no watchOptions specified option", () => [{
+    verifyWatchOptions("different options", () => [
+        {
+            json: { watchOptions: { watchFile: "UseFsEvents" } },
+        },
+        {
+            json: { watchOptions: { watchDirectory: "UseFsEvents" } },
+        },
+        {
+            json: { watchOptions: { fallbackPolling: "DynamicPriority" } },
+        },
+        {
+            json: { watchOptions: { synchronousWatchDirectory: true } },
+        },
+        {
+            json: { watchOptions: { excludeDirectories: ["**/temp"] } },
+        },
+        {
+            json: { watchOptions: { excludeFiles: ["**/temp/*.ts"] } },
+        },
+        {
+            json: { watchOptions: { excludeDirectories: ["**/../*"] } },
+        },
+        {
+            json: { watchOptions: { excludeFiles: ["**/../*"] } },
+        },
+    ]);
+
+    verifyWatchOptions("watch options extending passed in watch options", () => [
+        {
+            json: { watchOptions: { watchFile: "UseFsEvents" } },
+        },
+        {
             json: {},
-        }]);
-
-        verifyWatchOptions("empty watchOptions specified option", () => [{
-            json: { watchOptions: {} },
-        }]);
-
-        verifyWatchOptions("when extending config file without watchOptions", () => [
-            {
-                json: {
-                    extends: "./base.json",
-                    watchOptions: { watchFile: "UseFsEvents" }
-                },
-                additionalFiles: { "/base.json": "{}" }
-            },
-            {
-                json: { extends: "./base.json", },
-                additionalFiles: { "/base.json": "{}" }
-            }
-        ]);
-
-        verifyWatchOptions("when extending config file with watchOptions", () => [
-            {
-                json: {
-                    extends: "./base.json",
-                    watchOptions: {
-                        watchFile: "UseFsEvents",
-                    }
-                },
-                additionalFiles: {
-                    "/base.json": JSON.stringify({
-                        watchOptions: {
-                            watchFile: "UseFsEventsOnParentDirectory",
-                            watchDirectory: "FixedPollingInterval"
-                        }
-                    })
-                }
-            },
-            {
-                json: {
-                    extends: "./base.json",
-                },
-                additionalFiles: {
-                    "/base.json": JSON.stringify({
-                        watchOptions: {
-                            watchFile: "UseFsEventsOnParentDirectory",
-                            watchDirectory: "FixedPollingInterval"
-                        }
-                    })
-                }
-            }
-        ]);
-
-        verifyWatchOptions("different options", () => [
-            {
-                json: { watchOptions: { watchFile: "UseFsEvents" } },
-            },
-            {
-                json: { watchOptions: { watchDirectory: "UseFsEvents" } },
-            },
-            {
-                json: { watchOptions: { fallbackPolling: "DynamicPriority" } },
-            },
-            {
-                json: { watchOptions: { synchronousWatchDirectory: true } },
-            },
-            {
-                json: { watchOptions: { excludeDirectories: ["**/temp"] } },
-            },
-            {
-                json: { watchOptions: { excludeFiles: ["**/temp/*.ts"] } },
-            },
-            {
-                json: { watchOptions: { excludeDirectories: ["**/../*"] } },
-            },
-            {
-                json: { watchOptions: { excludeFiles: ["**/../*"] } },
-            },
-        ]);
-
-        verifyWatchOptions("watch options extending passed in watch options", () => [
-            {
-                json: { watchOptions: { watchFile: "UseFsEvents" } },
-            },
-            {
-                json: {},
-            },
-        ]);
-    });
+        },
+    ]);
+});
 }
--- src/testRunner/unittests/tsbuildWatch/configFileErrors.ts
+++ src/testRunner/unittests/tsbuildWatch/configFileErrors.ts
@@ -1,19 +1,19 @@
 namespace ts.tscWatch {
-    describe("unittests:: tsbuildWatch:: watchMode:: configFileErrors:: reports syntax errors in config file", () => {
-        function build(sys: WatchedSystem) {
-            sys.checkTimeoutQueueLengthAndRun(1); // build the project
-            sys.checkTimeoutQueueLength(0);
-        }
-        verifyTscWatch({
-            scenario: "configFileErrors",
-            subScenario: "reports syntax errors in config file",
-            sys: () => createWatchedSystem(
-                [
-                    { path: `${projectRoot}/a.ts`, content: "export function foo() { }" },
-                    { path: `${projectRoot}/b.ts`, content: "export function bar() { }" },
-                    {
-                        path: `${projectRoot}/tsconfig.json`,
-                        content: Utils.dedent`
+describe("unittests:: tsbuildWatch:: watchMode:: configFileErrors:: reports syntax errors in config file", () => {
+    function build(sys: WatchedSystem) {
+        sys.checkTimeoutQueueLengthAndRun(1); // build the project
+        sys.checkTimeoutQueueLength(0);
+    }
+    verifyTscWatch({
+        scenario: "configFileErrors",
+        subScenario: "reports syntax errors in config file",
+        sys: () => createWatchedSystem(
+            [
+                { path: `${projectRoot}/a.ts`, content: "export function foo() { }" },
+                { path: `${projectRoot}/b.ts`, content: "export function bar() { }" },
+                {
+                    path: `${projectRoot}/tsconfig.json`,
+                    content: Utils.dedent`
 {
     "compilerOptions": {
         "composite": true,
@@ -23,38 +23,38 @@ namespace ts.tscWatch {
         "b.ts"
     ]
 }`
-                    },
-                    libFile
-                ],
-                { currentDirectory: projectRoot }
-            ),
-            commandLineArgs: ["--b", "-w"],
-            changes: [
-                {
-                    caption: "reports syntax errors after change to config file",
-                    change: sys => replaceFileText(sys, `${projectRoot}/tsconfig.json`, ",", `,
+                },
+                libFile
+            ],
+            { currentDirectory: projectRoot }
+        ),
+        commandLineArgs: ["--b", "-w"],
+        changes: [
+            {
+                caption: "reports syntax errors after change to config file",
+                change: sys => replaceFileText(sys, `${projectRoot}/tsconfig.json`, ",", `,
         "declaration": true,`),
-                    timeouts: build,
-                },
-                {
-                    caption: "reports syntax errors after change to ts file",
-                    change: sys => replaceFileText(sys, `${projectRoot}/a.ts`, "foo", "fooBar"),
-                    timeouts: build,
-                },
-                {
-                    caption: "reports error when there is no change to tsconfig file",
-                    change: sys => replaceFileText(sys, `${projectRoot}/tsconfig.json`, "", ""),
-                    timeouts: build,
-                },
-                {
-                    caption: "builds after fixing config file errors",
-                    change: sys => sys.writeFile(`${projectRoot}/tsconfig.json`, JSON.stringify({
-                        compilerOptions: { composite: true, declaration: true },
-                        files: ["a.ts", "b.ts"]
-                    })),
-                    timeouts: build,
-                }
-            ]
-        });
+                timeouts: build,
+            },
+            {
+                caption: "reports syntax errors after change to ts file",
+                change: sys => replaceFileText(sys, `${projectRoot}/a.ts`, "foo", "fooBar"),
+                timeouts: build,
+            },
+            {
+                caption: "reports error when there is no change to tsconfig file",
+                change: sys => replaceFileText(sys, `${projectRoot}/tsconfig.json`, "", ""),
+                timeouts: build,
+            },
+            {
+                caption: "builds after fixing config file errors",
+                change: sys => sys.writeFile(`${projectRoot}/tsconfig.json`, JSON.stringify({
+                    compilerOptions: { composite: true, declaration: true },
+                    files: ["a.ts", "b.ts"]
+                })),
+                timeouts: build,
+            }
+        ]
     });
+});
 }
