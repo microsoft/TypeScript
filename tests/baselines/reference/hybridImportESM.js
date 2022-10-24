e82e61da9440cbf85ea9b39c1f26f323df18a005
Andrew Branch <andrew@wheream.io>
2022-10-24 16:43:58 -0700

    Add another test

---
+++ tests/baselines/reference/hybridImportESM.js
@@ -0,0 +1,26 @@
+//// [tests/cases/conformance/moduleResolution/hybrid/hybridImportESM.ts] ////
+
+//// [esm.mts]
+export const esm = 0;
+
+//// [not-actually-cjs.cts]
+import { esm } from "./esm.mjs";
+
+//// [package.json]
+{ "type": "commonjs" }
+
+//// [still-not-cjs.ts]
+import { esm } from "./esm.mjs";
+
+
+//// [esm.mjs]
+"use strict";
+exports.__esModule = true;
+exports.esm = void 0;
+exports.esm = 0;
+//// [not-actually-cjs.cjs]
+"use strict";
+exports.__esModule = true;
+//// [still-not-cjs.js]
+"use strict";
+exports.__esModule = true;
