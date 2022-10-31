c49c733c1f5d17363eb6c15aec855ff76a4d6067
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2022-10-31 13:42:28 -0700

    Bump version to 5.0.

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -14,7 +14,7 @@ and limitations under the License.
 ***************************************************************************** */
 
 declare namespace ts {
-    const versionMajorMinor = "4.9";
+    const versionMajorMinor = "5.0";
     /** The version of the TypeScript compiler release */
     const version: string;
     /**
