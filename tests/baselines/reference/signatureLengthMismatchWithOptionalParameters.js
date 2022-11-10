0de6f19daaadd318cefc5ce183122cb65e589231
Kevin Gibbons <bakkot@gmail.com>
2022-11-10 11:51:50 -0800

    add more tests

---
+++ tests/baselines/reference/signatureLengthMismatchWithOptionalParameters.js
@@ -0,0 +1,12 @@
+//// [signatureLengthMismatchWithOptionalParameters.ts]
+function callee(n: number | undefined, m: string) { }
+
+function caller(arg: (n?: number) => void) { }
+
+caller(callee);
+
+
+//// [signatureLengthMismatchWithOptionalParameters.js]
+function callee(n, m) { }
+function caller(arg) { }
+caller(callee);
