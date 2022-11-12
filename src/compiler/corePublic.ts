93bd577458d55cd720b2677705feab5c91eb12ce
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2022-11-12 00:32:14 +0000

    Bump version to 4.9.3 and LKG.

--- src/compiler/corePublic.ts
+++ src/compiler/corePublic.ts
@@ -5,7 +5,7 @@ namespace ts {
     // The following is baselined as a literal template type without intervention
     /** The version of the TypeScript compiler release */
     // eslint-disable-next-line @typescript-eslint/no-inferrable-types
-    export const version: string = `${versionMajorMinor}.2-rc`;
+    export const version: string = `${versionMajorMinor}.3`;
 
     /**
      * Type of objects whose values are all of the same type.
