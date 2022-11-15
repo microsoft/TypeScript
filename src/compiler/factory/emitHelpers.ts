dfc1242aafadfaab9bb8df63ced06e87e1ab8fd8
Tomasz Lenarcik <apendua@gmail.com>
2022-11-16 00:12:26 +0100

    Fix asyncDelegator reporting "done" too early (#51274)
    * Fix asyncDelegator reporting done too early
    
    * Add unit test for yields inside finally block
    
    See #45400

--- src/compiler/factory/emitHelpers.ts
+++ src/compiler/factory/emitHelpers.ts
@@ -541,7 +541,7 @@ export const asyncDelegator: UnscopedEmitHelper = {
             var __asyncDelegator = (this && this.__asyncDelegator) || function (o) {
                 var i, p;
                 return i = {}, verb("next"), verb("throw", function (e) { throw e; }), verb("return"), i[Symbol.iterator] = function () { return this; }, i;
-                function verb(n, f) { i[n] = o[n] ? function (v) { return (p = !p) ? { value: __await(o[n](v)), done: n === "return" } : f ? f(v) : v; } : f; }
+                function verb(n, f) { i[n] = o[n] ? function (v) { return (p = !p) ? { value: __await(o[n](v)), done: false } : f ? f(v) : v; } : f; }
             };`
 };
 
