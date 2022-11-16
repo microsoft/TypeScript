35a1bc2dc5f8dfa23d11b22006f04899953043ae
Anders Hejlsberg <andersh@microsoft.com>
2022-11-16 10:49:38 -0800

    Accept new baselines

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -6719,6 +6719,7 @@ declare namespace ts {
     interface EnumType extends Type {
     }
     enum ObjectFlags {
+        None = 0,
         Class = 1,
         Interface = 2,
         Reference = 4,
@@ -6889,6 +6890,7 @@ declare namespace ts {
         declaration?: IndexSignatureDeclaration;
     }
     enum InferencePriority {
+        None = 0,
         NakedTypeVariable = 1,
         SpeculativeTuple = 2,
         SubstituteSource = 4,
