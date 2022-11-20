71f6c9c7d0e2ac7c949fdbccedd589ae741e3457
Eyal Shalev <eyalsh@gmail.com>
2022-11-20 21:49:12 +0200

    Added method overloading for Atomics.waitAsync(Int32Array, number, number, number)

--- tests/cases/conformance/es2022/es2022SharedMemory.ts
+++ tests/cases/conformance/es2022/es2022SharedMemory.ts
@@ -3,14 +3,20 @@
 // @noemit: true
 // @strict: true
 
-const sab = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * 1024);
-const int32 = new Int32Array(sab);
+const sab32 = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * 1024);
+const sab64 = new SharedArrayBuffer(BigInt64Array.BYTES_PER_ELEMENT * 1024);
+const int32 = new Int32Array(sab32);
+const int64 = new BigInt64Array(sab64);
 const waitValue = Atomics.wait(int32, 0, 0);
-const { async, value } = Atomics.waitAsync(int32, 0, BigInt(0));
+const { async: async32, value: value32 } = Atomics.waitAsync(int32, 0, 0);
+const { async: async64, value: value64 } = Atomics.waitAsync(int64, 0, BigInt(0));
 
 const main = async () => {
-    if (async) {
-        await value;
+    if (async32) {
+        await value32;
+    }
+    if (async64) {
+        await value64;
     }
 }
 main();
