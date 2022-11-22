4c81435bc748218f5b2b50c8eacf7c14d0440d26
Sheetal Nandi <shkamat@microsoft.com>
2022-11-22 11:37:56 -0800

    When fsEvent for change is repeated

--- src/testRunner/unittests/tscWatch/watchEnvironment.ts
+++ src/testRunner/unittests/tscWatch/watchEnvironment.ts
@@ -702,4 +702,26 @@ describe("unittests:: tsc-watch:: watchEnvironment:: tsc-watch with different po
             ]
         });
     });
+
+    verifyTscWatch({
+        scenario,
+        subScenario: "fsEvent for change is repeated",
+        commandLineArgs: ["-w", "main.ts", "--extendedDiagnostics"],
+        sys: () => createWatchedSystem({
+            "/user/username/projects/project/main.ts": `let a: string = "Hello"`,
+            [libFile.path]: libFile.content,
+        }, { currentDirectory: "/user/username/projects/project" }),
+        changes: [
+            {
+                caption: "change main.ts",
+                change: sys => sys.replaceFileText("/user/username/projects/project/main.ts", "Hello", "Hello World"),
+                timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+            },
+            {
+                caption: "receive another change event without modifying the file",
+                change: sys => sys.invokeFsWatches("/user/username/projects/project/main.ts", "change", /*modifiedTime*/ undefined, /*useTildeSuffix*/ undefined),
+                timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+            }
+        ]
+    });
 });
