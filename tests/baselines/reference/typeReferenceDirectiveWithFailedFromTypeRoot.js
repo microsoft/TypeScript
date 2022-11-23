4ea628e90723738b9d13e4048ff1febc6e86cf22
Sheetal Nandi <shkamat@microsoft.com>
2022-11-22 21:25:39 -0800

    Test to show even when typeRoots are specified, resolution falls back to node_modules in nearest directory

---
+++ tests/baselines/reference/typeReferenceDirectiveWithFailedFromTypeRoot.js
@@ -0,0 +1,16 @@
+//// [tests/cases/compiler/typeReferenceDirectiveWithFailedFromTypeRoot.ts] ////
+
+//// [dummy.d.ts]
+declare const a2: number;
+
+//// [phaser.d.ts]
+declare const a: number;
+
+//// [package.json]
+{ "name": "phaser", "version": "1.2.3", "types": "types/phaser.d.ts" }
+
+//// [a.ts]
+a;
+
+//// [a.js]
+a;
---
+++ tests/cases/compiler/typeReferenceDirectiveWithFailedFromTypeRoot.ts
@@ -0,0 +1,17 @@
+// @noImplicitReferences: true
+// @typeRoots: /typings
+// @types: phaser
+// @traceResolution: true
+// @currentDirectory: /
+
+// @Filename: /typings/dummy.d.ts
+declare const a2: number;
+
+// @Filename: /node_modules/phaser/types/phaser.d.ts
+declare const a: number;
+
+// @Filename: /node_modules/phaser/package.json
+{ "name": "phaser", "version": "1.2.3", "types": "types/phaser.d.ts" }
+
+// @Filename: /a.ts
+a;
