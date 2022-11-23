c1427c90b6fa92834eba50b07568932535215bfb
Sheetal Nandi <shkamat@microsoft.com>
2022-11-22 20:58:21 -0800

    Convert reuse program structure and tracing of module resolution to baseline for easier updates (#51628)
    * Modify all usages of file name lower casing to use custom lower casing method
    
    * Baseline reuse program structure tests for easy update when making changes to module resolution

---
+++ tests/baselines/reference/reuseProgramStructure/change-doesnot-affect-type-references.js
@@ -0,0 +1,59 @@
+Program Reused:: Not
+File: c.ts
+
+
+var z = 1;
+resolvedModules: undefined
+resolvedTypeReferenceDirectiveNames: undefined
+
+File: b.ts
+/// <reference path='c.ts'/>
+
+var y = 2
+resolvedModules: undefined
+resolvedTypeReferenceDirectiveNames: undefined
+
+File: a.ts
+/// <reference path='b.ts'/>
+                /// <reference path='c.ts'/>
+                
+
+var x = 100
+resolvedModules: undefined
+resolvedTypeReferenceDirectiveNames: undefined
+
+
+MissingPaths:: ["lib.d.ts"]
+
+
+
+
+Program Reused:: Completely
+File: c.ts
+
+
+var z = 1;
+resolvedModules: undefined
+resolvedTypeReferenceDirectiveNames: undefined
+
+File: b.ts
+/// <reference path='c.ts'/>
+
+var y = 2
+resolvedModules: undefined
+resolvedTypeReferenceDirectiveNames: undefined
+
+File: a.ts
+/// <reference path='b.ts'/>
+                /// <reference path='c.ts'/>
+                
+
+var x = 100
+resolvedModules: undefined
+resolvedTypeReferenceDirectiveNames: undefined
+
+
+MissingPaths:: ["lib.d.ts"]
+
+
+
---
+++ tests/baselines/reference/reuseProgramStructure/resolvedImports-after-re-using-an-ambient-external-module-declaration.js
@@ -0,0 +1,29 @@
+Program Reused:: Not
+File: /a.ts
+
+
+import * as a from "a";
+resolvedModules: 
+a: {"resolvedFileName":"/a.ts","extension":".ts","isExternalLibraryImport":false}
+resolvedTypeReferenceDirectiveNames: undefined
+
+
+MissingPaths:: ["lib.d.ts"]
+
+
+
+
+Program Reused:: Completely
+File: /a.ts
+
+
+import * as aa from "a";
+resolvedModules: 
+a: {"resolvedFileName":"/a.ts","extension":".ts","isExternalLibraryImport":false}
+resolvedTypeReferenceDirectiveNames: undefined
+
+
+MissingPaths:: ["lib.d.ts"]
+
+
+
