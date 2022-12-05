a3a6cd436553927a1aaf47a6cee5c03bb3988b49
Andrew Branch <andrew@wheream.io>
2022-12-05 11:49:54 -0800

    Update protocol

--- src/server/protocol.ts
+++ src/server/protocol.ts
@@ -3515,6 +3515,7 @@ export interface UserPreferences {
     readonly includeInlayFunctionLikeReturnTypeHints?: boolean;
     readonly includeInlayEnumMemberValueHints?: boolean;
     readonly autoImportFileExcludePatterns?: string[];
+    readonly organizeImportsIgnoreCase?: "auto" | boolean;
 
     /**
      * Indicates whether {@link ReferencesResponseItem.lineText} is supported.
