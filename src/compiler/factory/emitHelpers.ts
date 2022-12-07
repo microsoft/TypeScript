9c00001a269eee49abb1fd27829f3c7bd0023f5e
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2022-12-07 09:51:27 -0800

    Have `__makeTemplateObject` freeze template strings objects if possible.

--- src/compiler/factory/emitHelpers.ts
+++ src/compiler/factory/emitHelpers.ts
@@ -663,7 +663,12 @@ export const templateObjectHelper: UnscopedEmitHelper = {
     priority: 0,
     text: `
             var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-                if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+                if (Object.freeze) {
+                    Object.freeze(Object.defineProperty(cooked, "raw", { value: Object.freeze(raw) }));
+                }
+                else {
+                    cooked.raw = raw;
+                }
                 return cooked;
             };`
 };
