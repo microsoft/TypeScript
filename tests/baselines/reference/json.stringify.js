c3124f7b4f099283e5cb2f134664b12dc564b114
ronyhe <ronyheruth@wix.com>
2022-12-14 23:32:33 +0200

    Second try

--- tests/baselines/reference/json.stringify.js
+++ tests/baselines/reference/json.stringify.js
@@ -4,7 +4,9 @@ JSON.stringify(value, undefined, 2);
 JSON.stringify(value, null, 2);
 JSON.stringify(value, ["a", 1], 2);
 JSON.stringify(value, (k) => undefined, 2);
-JSON.stringify(value, undefined, 2);
+JSON.stringify(() => {}, undefined, 2);
+JSON.stringify(Symbol('s'), undefined, 2);
+JSON.stringify(undefined, undefined, 2);
 
 //// [json.stringify.js]
 var value = null;
@@ -12,4 +14,6 @@ JSON.stringify(value, undefined, 2);
 JSON.stringify(value, null, 2);
 JSON.stringify(value, ["a", 1], 2);
 JSON.stringify(value, function (k) { return undefined; }, 2);
-JSON.stringify(value, undefined, 2);
+JSON.stringify(function () { }, undefined, 2);
+JSON.stringify(Symbol('s'), undefined, 2);
+JSON.stringify(undefined, undefined, 2);
