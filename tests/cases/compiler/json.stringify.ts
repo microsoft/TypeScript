23f641cddbcb6b55eec764eb6b3895beba731c38
ronyhe <ronyheruth@wix.com>
2022-12-14 22:50:10 +0200

    First attempt

--- tests/cases/compiler/json.stringify.ts
+++ tests/cases/compiler/json.stringify.ts
@@ -6,3 +6,5 @@ JSON.stringify(value, null, 2);
 JSON.stringify(value, ["a", 1], 2);
 JSON.stringify(value, (k) => undefined, 2);
 JSON.stringify(value, undefined, 2);
+JSON.stringify(() => { }, undefined, 2);
+JSON.stringify(undefined, undefined, 2);
