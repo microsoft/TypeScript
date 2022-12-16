15d7bf92cc19e0cb2705301f859301545a101cba
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-12-15 21:05:49 -0800

    Skip an array.from

--- src/services/codefixes/fixAddMissingMember.ts
+++ src/services/codefixes/fixAddMissingMember.ts
@@ -23,6 +23,7 @@ import {
     findIndex,
     firstDefined,
     firstOrUndefined,
+    firstOrUndefinedIterator,
     FunctionExpression,
     getCheckFlags,
     getClassLikeDeclarationOfSymbol,
@@ -664,7 +665,7 @@ function tryGetValueFromType(context: CodeFixContextBase, checker: TypeChecker, 
         return factory.createFalse();
     }
     if (type.flags & TypeFlags.EnumLike) {
-        const enumMember = type.symbol.exports ? firstOrUndefined(Array.from(type.symbol.exports.values())) : type.symbol;
+        const enumMember = type.symbol.exports ? firstOrUndefinedIterator(type.symbol.exports.values()) : type.symbol;
         const name = checker.symbolToExpression(type.symbol.parent ? type.symbol.parent : type.symbol, SymbolFlags.Value, /*enclosingDeclaration*/ undefined, /*flags*/ undefined);
         return enumMember === undefined || name === undefined ? factory.createNumericLiteral(0) : factory.createPropertyAccessExpression(name, checker.symbolToString(enumMember));
     }
