1428181253888a9e9f0e9738707d61f9d77fbc9e
Yamato Sasaki <sasaki.yamato@moneyforward.co.jp>
2022-12-26 12:16:57 +0900

    Update DOM

---
+++ src/lib/dom.asynciterable.generated.d.ts
@@ -0,0 +1,10 @@
+/////////////////////////////
+/// Window Async Iterable APIs
+/////////////////////////////
+
+interface FileSystemDirectoryHandle {
+    [Symbol.asyncIterator](): AsyncIterableIterator<[string, FileSystemHandle]>;
+    entries(): AsyncIterableIterator<[string, FileSystemHandle]>;
+    keys(): AsyncIterableIterator<string>;
+    values(): AsyncIterableIterator<FileSystemHandle>;
+}
---
+++ src/lib/webworker.asynciterable.generated.d.ts
@@ -0,0 +1,10 @@
+/////////////////////////////
+/// Worker Async Iterable APIs
+/////////////////////////////
+
+interface FileSystemDirectoryHandle {
+    [Symbol.asyncIterator](): AsyncIterableIterator<[string, FileSystemHandle]>;
+    entries(): AsyncIterableIterator<[string, FileSystemHandle]>;
+    keys(): AsyncIterableIterator<string>;
+    values(): AsyncIterableIterator<FileSystemHandle>;
+}
