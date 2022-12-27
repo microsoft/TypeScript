c4d98c3339be94349f738e6f2978ab9981e0dcd6
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2022-12-27 21:35:33 +0000

    fix bad comment

--- tests/cases/compiler/self-types-not-composition.ts
+++ tests/cases/compiler/self-types-not-composition.ts
@@ -4,13 +4,8 @@ type Not<T> =
     : self
 
 const divide = (a: number, b: number & Not<0>) => a / b
-// This doesn't work because currently the compiler assumes that
-// if a type A is a subtype of B then type A will be a subtype of B & C for all A, B and C.
-// But this is not true if C is a type like the above `Not` type.
-// TODO: change intersection types to not make this assumption if any of the consituents
-// have the ContainsSelf flag
 
-divide(1, 0)
+divide(1, 0) // TODO: shouldn't compile
 divide(1, 1)
 divide(1, "x")
 
