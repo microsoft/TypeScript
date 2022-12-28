46f2e57d76f26b74f596f2acb5d04a30572f13a7
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-28 20:00:45 +0100

    Add an additional test case for mixed length tuples in the target's rest

--- tests/cases/compiler/restTupleUnionWithRestContextualParams.ts
+++ tests/cases/compiler/restTupleUnionWithRestContextualParams.ts
@@ -2,3 +2,5 @@
 // @noEmit: true
 
 const f1: (...args: [number, string, ...boolean[]] | [string, number, ...boolean[]]) => void = (a, b, c) => {};
+
+const f2: (x: string, ...args: [string] | [number, boolean]) => void = (a, b, c) => {};
