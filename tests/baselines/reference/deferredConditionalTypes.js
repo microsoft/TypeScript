e9f81ce5048f01bc17d89cd7d16aa8cbce29d950
Anders Hejlsberg <andersh@microsoft.com>
2023-01-03 16:58:11 -0800

    Add tests

---
+++ tests/baselines/reference/deferredConditionalTypes.js
@@ -0,0 +1,23 @@
+//// [deferredConditionalTypes.ts]
+type T0<X, Y> = X extends Y ? true : false;  // Deferred
+type T1<X, Y> = [X] extends [Y] ? true : false;  // Deferred
+type T2<X, Y> = [X, X] extends [Y, Y] ? true : false;  // Deferred
+type T3<X, Y> = [X, X, X] extends [Y, Y, Y] ? true : false;  // Deferred
+
+type T4<X, Y> = [X] extends [Y, Y] ? true : false;  // false
+type T5<X, Y> = [X, X] extends [Y] ? true : false;  // false
+
+// Repro from #52068
+
+type Or<A extends boolean, B extends boolean> = [A, B] extends [false, false] ? false : true;
+type And<A extends boolean, B extends boolean> = [A, B] extends [true, true] ? true : false;
+type Not<T extends boolean> = T extends true ? false : true;
+type Extends<A, B> = A extends B ? true : false;
+
+type IsNumberLiteral<T> = And<Extends<T, number>, Not<Extends<number, T>>>;
+
+type IsLiteral<T> = Or<false, IsNumberLiteral<T>>;
+
+
+//// [deferredConditionalTypes.js]
+"use strict";
