65b9f7017c1be8426b42c0b53a4870183d719cf9
Ron Buckton <ron.buckton@microsoft.com>
2023-01-10 15:19:07 -0500

    Update API baselines

--- tests/baselines/reference/api/typescript.d.ts
+++ tests/baselines/reference/api/typescript.d.ts
@@ -479,7 +479,8 @@ declare namespace ts {
         ReachabilityCheckFlags = 768,
         ReachabilityAndEmitFlags = 2816,
         ContextFlags = 50720768,
-        TypeExcludesFlags = 40960
+        TypeExcludesFlags = 40960,
+        IdentifierIsInJSDocNamespace = 2048
     }
     enum ModifierFlags {
         None = 0,
@@ -649,8 +650,10 @@ declare namespace ts {
          * Text of identifier, but if the identifier begins with two underscores, this will begin with three.
          */
         readonly escapedText: __String;
+        /** @deprecated Use `idKeyword(identifier)` instead. */
         readonly originalKeywordKind?: SyntaxKind;
-        /** @deprecated */ readonly isInJSDocNamespace?: boolean;
+        /** @deprecated Use `identifier.flags & NodeFlags.IdentifierIsInJSDocNamespace` instead. */
+        readonly isInJSDocNamespace?: boolean;
     }
     interface Identifier {
         readonly text: string;
@@ -4663,6 +4666,11 @@ declare namespace ts {
      */
     function unescapeLeadingUnderscores(identifier: __String): string;
     function idText(identifierOrPrivateName: Identifier | PrivateIdentifier): string;
+    /**
+     * If the text of an Identifier matches a keyword (including contextual and TypeScript-specific keywords), returns the
+     * SyntaxKind for the matching keyword.
+     */
+    function idKeyword(node: Identifier): SyntaxKind | undefined;
     function symbolName(symbol: Symbol): string;
     function getNameOfJSDocTypedef(declaration: JSDocTypedefTag): Identifier | PrivateIdentifier | undefined;
     function getNameOfDeclaration(declaration: Declaration | Expression | undefined): DeclarationName | undefined;
