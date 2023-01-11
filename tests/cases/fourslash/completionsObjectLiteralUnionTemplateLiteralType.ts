28b2023d1328dbc9526b12703c925aad1ef31a8d
Tobeyyyyy <tobiasscholz2000@gmail.com>
2023-01-11 20:05:50 +0100

    refine test

--- tests/cases/fourslash/completionsObjectLiteralUnionTemplateLiteralType.ts
+++ tests/cases/fourslash/completionsObjectLiteralUnionTemplateLiteralType.ts
@@ -1,16 +1,31 @@
 /// <reference path="fourslash.ts" />
 
-////type BrokenIntellisense = {
-////  key: string;
+////type UnionType = {
+////  key1: string;
+////} | {
+////  key2: number;  
 ////} | `string literal ${string}`;
 ////
-////const brokenIntellisense: BrokenIntellisense = {
+////const obj1: UnionType = {
 ////  /*1*/
+////};
+////
+////const obj2: UnionType = {
+////  key1: "abc",
+////  /*2*/
 ////};
 
 verify.completions({
   marker: "1",
-  exact: {
-    name: "key"
-  }
+  exact: [
+    { name: "key1" },
+    { name: "key2" },
+  ]
+});
+
+verify.completions({
+  marker: "2",
+  exact: [
+    { name: "key2" },
+  ]
 });
