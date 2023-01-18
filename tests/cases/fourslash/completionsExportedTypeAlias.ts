db61252028dd94a8b885b381378dcda319dd80b7
Tobeyyyyy <tobiasscholz2000@gmail.com>
2023-01-18 09:32:59 +0100

    more tests

--- tests/cases/fourslash/completionsExportedTypeAlias.ts
+++ tests/cases/fourslash/completionsExportedTypeAlias.ts
@@ -1,20 +1,36 @@
 /// <reference path="fourslash.ts" />
 
-////export type Foo = 'a' | 'b';
+////export type A_Type = 'a' | 'b';
 ////
-////const x: Fo/*a*/
+////const x: A_Typ/*a*/
 ////
-////type Bar = 'c' | 'd';
+////type B_Type = 'c' | 'd';
 ////
-////const x: Ba/*b*/
+////const y: B_Typ/*b*/
+////
+////export interface C_Interface<T> {}
+////
+////const z: C_Inter/*c*/
+////
+////interface D_Interface<T> {}
+////
+////const a: D_Inter/*d*/
+////
+////export type E_Type<T> = {}
+////
+////const b: E_Typ/*e*/
+////
+////type F_Type<T> = {}
+////
+////const c: F_Typ/*f*/
 
 verify.completions(
     {
         marker: 'a',
         includes: [
             {
-                name: 'Foo',
-                text: 'type Foo = "a" | "b"'
+                name: 'A_Type',
+                text: 'type A_Type = "a" | "b"'
             }
         ]
     },
@@ -22,8 +38,44 @@ verify.completions(
         marker: 'b',
         includes: [
             {
-                name: 'Bar',
-                text: 'type Bar = "c" | "d"'
+                name: 'B_Type',
+                text: 'type B_Type = "c" | "d"'
+            }
+        ]
+    },
+    {
+        marker: 'c',
+        includes: [
+            {
+                name: 'C_Interface',
+                text: 'interface C_Interface'
+            }
+        ]
+    },
+    {
+        marker: 'd',
+        includes: [
+            {
+                name: 'D_Interface',
+                text: 'interface D_Interface<T>'
+            }
+        ]
+    },
+    {
+        marker: 'e',
+        includes: [
+            {
+                name: 'E_Type',
+                text: 'type E_Type = {}'
+            }
+        ]
+    },
+    {
+        marker: 'f',
+        includes: [
+            {
+                name: 'F_Type',
+                text: 'type F_Type<T> = {}'
             }
         ]
     }
