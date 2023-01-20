1d81c1dfefda8e7a2ac1a1dd9907851c337ca464
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-01-21 00:43:23 +0200

    fix(51521): Using a of property declared after an initializing constructor triggers an assertion failure in JS (#51524)
    Fixes https://github.com/microsoft/TypeScript/issues/51521

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -3531,7 +3531,7 @@ export function isSpecialPropertyDeclaration(expr: PropertyAccessExpression | El
 export function setValueDeclaration(symbol: Symbol, node: Declaration): void {
     const { valueDeclaration } = symbol;
     if (!valueDeclaration ||
-        !(node.flags & NodeFlags.Ambient && !(valueDeclaration.flags & NodeFlags.Ambient)) &&
+        !(node.flags & NodeFlags.Ambient && !isInJSFile(node) && !(valueDeclaration.flags & NodeFlags.Ambient)) &&
         (isAssignmentDeclaration(valueDeclaration) && !isAssignmentDeclaration(node)) ||
         (valueDeclaration.kind !== node.kind && isEffectiveModuleDeclaration(valueDeclaration))) {
         // other kinds of value declarations take precedence over modules and assignment declarations
