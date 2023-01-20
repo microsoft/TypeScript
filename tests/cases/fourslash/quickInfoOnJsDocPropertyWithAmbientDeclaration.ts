b3c27eb976d22705edc05d1bf41803a2f4d6a170
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-01-19 23:24:09 -0800

    Add test from the other PR
    Co-authored-by: Oleksandr T <oleksandr.tarasiuk@outlook.com>

---
+++ tests/cases/fourslash/quickInfoOnJsDocPropertyWithAmbientDeclaration.ts
@@ -0,0 +1,17 @@
+/// <reference path="fourslash.ts" />
+
+// @noLib: true
+// @allowJs: true
+// @filename: /test.js
+////class Foo {
+////    constructor() {
+////        this.prop = { };
+////    }
+////
+////    declare prop: string;
+////    method() {
+////        this.prop.foo/**/
+////    }
+////}
+
+verify.baselineQuickInfo();
