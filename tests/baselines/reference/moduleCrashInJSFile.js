965885efea106e07ad92fe810ec006c02d688069
navya9singh <navyasingh@microsoft.com>
2023-01-31 14:14:36 -0800

    Accepting baselines

---
+++ tests/baselines/reference/moduleCrashInJSFile.js
@@ -0,0 +1,23 @@
+//// [a.js]
+//// [thisKeyword.ts]
+module foo {
+    this.bar = 4;
+}
+
+//// [thisKeyword.js]
+var foo;
+(function (foo) {
+    this.bar = 4;
+})(foo || (foo = {}));
+
+//// [a.js]
+//// [thisKeyword.ts]
+var foo;
+(function (foo) {
+    this.bar = 4;
+})(foo || (foo = {}));
+//// [thisKeyword.js]
+var foo;
+(function (foo) {
+    this.bar = 4;
+})(foo || (foo = {}));
