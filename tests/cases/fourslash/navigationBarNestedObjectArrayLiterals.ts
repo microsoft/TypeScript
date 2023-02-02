7b23beb30a4ace8b9b17c18c8963ecb4f244b231
Vitaly Turovsky <vital2580@icloud.com>
2023-02-02 06:31:50 +0300

    actually update test result

--- tests/cases/fourslash/navigationBarNestedObjectArrayLiterals.ts
+++ tests/cases/fourslash/navigationBarNestedObjectArrayLiterals.ts
@@ -98,74 +98,111 @@ verify.navigationTree({
 });
 
 verify.navigationBar([
-    {
-      "text": "<global>",
-      "kind": "script",
-      "childItems": [
-        {
-          "text": "a",
-          "kind": "var"
-        }
-      ]
-    },
-    {
-      "text": "a",
-      "kind": "var",
-      "childItems": [
-        {
-          "text": "b",
-          "kind": "property"
-        },
-        {
-          "text": "c",
-          "kind": "property"
-        },
-        {
-          "text": "d",
-          "kind": "property"
-        },
-        {
-          "text": "f",
-          "kind": "property"
-        }
-      ],
-      "indent": 1
-    },
-    {
-      "text": "d",
-      "kind": "property",
-      "childItems": [
-        {
-          "text": "e",
-          "kind": "property"
-        }
-      ],
-      "indent": 2
-    },
-    {
-      "text": "f",
-      "kind": "property",
-      "childItems": [
-        {
-            "text": "g",
-            "kind": "property"
-        },
-        {
-            "text": "h",
-            "kind": "property"
-        }
-      ],
-      "indent": 2
-    },
-    {
-        "text": "h",
-        "kind": "property",
-        "childItems": [
-          {
-              "text": "i",
-              "kind": "property"
-          }
-        ],
-        "indent": 3
+  {
+    "text": "<global>",
+    "kind": "script",
+    "childItems": [
+      {
+        "text": "a",
+        "kind": "var"
       }
-  ]);
+    ]
+  },
+  {
+    "text": "a",
+    "kind": "var",
+    "childItems": [
+      {
+        "text": "b",
+        "kind": "property"
+      },
+      {
+        "text": "c",
+        "kind": "property"
+      },
+      {
+        "text": "d",
+        "kind": "property"
+      },
+      {
+        "text": "f",
+        "kind": "property"
+      },
+      {
+        "text": "j",
+        "kind": "property"
+      }
+    ],
+    "indent": 1
+  },
+  {
+    "text": "d",
+    "kind": "property",
+    "childItems": [
+      {
+        "text": "e",
+        "kind": "property"
+      }
+    ],
+    "indent": 2
+  },
+  {
+    "text": "f",
+    "kind": "property",
+    "childItems": [
+      {
+        "text": "g",
+        "kind": "property"
+      },
+      {
+        "text": "h",
+        "kind": "property"
+      }
+    ],
+    "indent": 2
+  },
+  {
+    "text": "h",
+    "kind": "property",
+    "childItems": [
+      {
+        "text": "i",
+        "kind": "property"
+      }
+    ],
+    "indent": 3
+  },
+  {
+    "text": "j",
+    "kind": "property",
+    "childItems": [
+      {
+        "text": "0"
+      },
+      {
+        "text": "1"
+      }
+    ],
+    "indent": 2
+  },
+  {
+    "text": "0",
+    "childItems": [
+      {
+        "text": "k",
+        "kind": "property"
+      }
+    ],
+    "indent": 3
+  },
+  {
+    "text": "1",
+    "childItems": [
+      {
+        "text": "l",
+        "kind": "property"
+      }
+    ],
+    "indent": 3
+  }
+]);
