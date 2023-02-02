4ac0e90ca08d4fdbcadfc3f6515c552b443de115
William Harrison <william@williamdavidharrison.com.au>
2023-02-02 14:10:52 +0800

    feat(demo): remove extra lines + spaces

--- tests/projects/demo/core/utilities.ts
+++ tests/projects/demo/core/utilities.ts
@@ -1,10 +1,8 @@
-
 export function makeRandomName() {
-    return "Bob!?! ";
+    return "Bob!?!";
 }
 
 export function lastElementOf<T>(arr: T[]): T | undefined {
     if (arr.length === 0) return undefined;
     return arr[arr.length - 1];
 }
-
