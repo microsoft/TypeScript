ee05056a23150ca43fb22ac079526359e8166153
William Harrison <william@williamdavidharrison.com.au>
2023-02-02 14:09:49 +0800

    chore(lib): remove empty lines

--- lib/lib.d.ts
+++ lib/lib.d.ts
@@ -13,10 +13,7 @@ See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
 
-
-
 /// <reference no-default-lib="true"/>
-
 
 /// <reference lib="es5" />
 /// <reference lib="dom" />
