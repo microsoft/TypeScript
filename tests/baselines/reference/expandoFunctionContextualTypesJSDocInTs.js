26c71ad8ae28a7d08411a7c74ccd3ba571af68dd
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-02-06 12:59:43 -0800

    Simplify test

--- tests/baselines/reference/expandoFunctionContextualTypesJSDocInTs.js
+++ tests/baselines/reference/expandoFunctionContextualTypesJSDocInTs.js
@@ -1,7 +1,4 @@
 //// [expandoFunctionContextualTypesJSDocInTs.ts]
-// GH #38532
-import Foo from "blah";
-
 export function Foo() { }
 
 // This comment should have no effect; this is a TS file.
@@ -13,10 +10,8 @@ Foo.bar = () => { };
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Foo = void 0;
-// GH #38532
-var blah_1 = require("blah");
 function Foo() { }
 exports.Foo = Foo;
 // This comment should have no effect; this is a TS file.
 /** @type {never} */
-blah_1.default.bar = function () { };
+Foo.bar = function () { };
--- tests/cases/compiler/expandoFunctionContextualTypesJSDocInTs.ts
+++ tests/cases/compiler/expandoFunctionContextualTypesJSDocInTs.ts
@@ -1,6 +1,3 @@
-// GH #38532
-import Foo from "blah";
-
 export function Foo() { }
 
 // This comment should have no effect; this is a TS file.
