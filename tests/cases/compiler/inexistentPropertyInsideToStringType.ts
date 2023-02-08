899db0117a8b581d7fb4169149d18e5b57ae00ae
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2023-02-08 15:26:58 -0800

    make test smaller

--- tests/cases/compiler/inexistentPropertyInsideToStringType.ts
+++ tests/cases/compiler/inexistentPropertyInsideToStringType.ts
@@ -1,12 +1,7 @@
 // @checkJs: true
 // @noEmit: true
 // @Filename: index.js
-function SomeClass() {
-}
-var _proto = SomeClass.prototype;
-_proto.toString = function toString() {
+function toString() {
     this.yadda
-    let someValue = "";
-    this.toString = () => someValue;
-    return someValue;
+    this.someValue = "";
 }
