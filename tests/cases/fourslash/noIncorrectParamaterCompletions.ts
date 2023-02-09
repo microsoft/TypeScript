fee4e96bd77c32321f7c2633c2760504f127b77e
Vitaly Turovsky <vital2580@icloud.com>
2023-02-09 04:51:23 +0300

    add positive testcases

--- tests/cases/fourslash/noIncorrectParamaterCompletions.ts
+++ tests/cases/fourslash/noIncorrectParamaterCompletions.ts
@@ -2,7 +2,7 @@
 
 //// function f1(a = /*1*/, b) { }
 //// function f2(a = a/*2*/, b) { }
-//// function f3(a = a + /*3*/, b) { }
+//// function f3(a = a + /*3*/, b = a/*4*/, c = /*5*/) { }
 
 verify.completions({
     marker: ["1", "2"],
@@ -11,4 +11,14 @@ verify.completions({
 verify.completions({
     marker: ["3"],
     excludes: ["a", "b"],
+})
+
+verify.completions({
+    marker: ["4"],
+    includes: ["a"],
+})
+
+verify.completions({
+    marker: ["5"],
+    includes: ["a", "b"],
 })
