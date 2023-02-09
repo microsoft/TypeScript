09caed3b49dbab4d0e60655c64365653fe6728cb
Vitaly Turovsky <vital2580@icloud.com>
2023-02-09 04:46:34 +0300

    be surprised with the number of tests to fix

--- tests/cases/fourslash/completionListInUnclosedFunction04.ts
+++ tests/cases/fourslash/completionListInUnclosedFunction04.ts
@@ -3,5 +3,4 @@
 ////function foo(x: string, y: number, z: boolean) {
 ////    function bar(a: number, b: string, c: typeof x = /*1*/
 
-// Note: Ideally `c` wouldn't be included since it hasn't been initialized yet.
-verify.completions({ marker: "1", includes: ["foo", "x", "y", "z", "bar", "a", "b", "c"]})
+verify.completions({ marker: "1", includes: ["foo", "x", "y", "z", "bar", "a", "b"]})
