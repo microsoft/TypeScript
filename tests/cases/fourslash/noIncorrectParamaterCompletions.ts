e27faedb32860da950454a2733e56963049ed592
Vitaly Turovsky <vital2580@icloud.com>
2023-02-09 04:49:21 +0300

    fix my tests

--- tests/cases/fourslash/noIncorrectParamaterCompletions.ts
+++ tests/cases/fourslash/noIncorrectParamaterCompletions.ts
@@ -1,16 +1,14 @@
 /// <reference path='fourslash.ts' />
 
-//// f(a = /*1*/, b) { }
-//// f(a = a/*2*/, b) { }
-//// f(a = a + /*3*/, b) { }
+//// function f1(a = /*1*/, b) { }
+//// function f2(a = a/*2*/, b) { }
+//// function f3(a = a + /*3*/, b) { }
 
 verify.completions({
     marker: ["1", "2"],
     excludes: ["a", "b"],
-    isNewIdentifierLocation: true,
 })
 verify.completions({
     marker: ["3"],
     excludes: ["a", "b"],
-    isNewIdentifierLocation: false,
 })
