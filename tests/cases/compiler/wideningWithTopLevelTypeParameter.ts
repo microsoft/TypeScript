354fd2ea63efe9d9de5149828e49235e5105ff12
Anders Hejlsberg <andersh@microsoft.com>
2023-02-13 09:37:54 -0800

    Add more tests

--- tests/cases/compiler/wideningWithTopLevelTypeParameter.ts
+++ tests/cases/compiler/wideningWithTopLevelTypeParameter.ts
@@ -1,6 +1,22 @@
 // @strict: true
 // @noEmit: true
 
+type C1<T> = T extends unknown ? T | undefined : never;
+type C2<T> = T extends unknown ? T | undefined : never;
+type C3<T> = T extends unknown ? T | undefined : never;
+
+declare function f0<T>(x: T): [T];
+declare function f1<T>(x: C1<T>): [T];
+declare function f2<T>(x: C1<C2<T>>): [T];
+declare function f3<T>(x: C1<C2<C3<T>>>): [T];
+
+const c0 = f0(7);  // [number]
+const c1 = f1(7);  // [number]
+const c2 = f2(7);  // [number]
+const c3 = f3(7);  // [number]
+
+// Repro from #52620
+
 class FormControl<T> {
     constructor(t: T extends undefined ? never : T) {}
 }
