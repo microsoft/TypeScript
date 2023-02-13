b7a4b043f980e7d69e57cd9eb337033a4c5af58e
Anders Hejlsberg <andersh@microsoft.com>
2023-02-13 15:59:31 -0800

    Add test of fourth level of nesting

--- tests/cases/compiler/wideningWithTopLevelTypeParameter.ts
+++ tests/cases/compiler/wideningWithTopLevelTypeParameter.ts
@@ -4,16 +4,19 @@
 type C1<T> = T extends unknown ? T | undefined : never;
 type C2<T> = T extends unknown ? T | undefined : never;
 type C3<T> = T extends unknown ? T | undefined : never;
+type C4<T> = T extends unknown ? T | undefined : never;
 
 declare function f0<T>(x: T): [T];
 declare function f1<T>(x: C1<T>): [T];
 declare function f2<T>(x: C1<C2<T>>): [T];
 declare function f3<T>(x: C1<C2<C3<T>>>): [T];
+declare function f4<T>(x: C1<C2<C3<C4<T>>>>): [T];
 
 const c0 = f0(7);  // [number]
 const c1 = f1(7);  // [number]
 const c2 = f2(7);  // [number]
 const c3 = f3(7);  // [number]
+const c4 = f4(7);  // [7] since we have an internal nesting limit of 3
 
 // Repro from #52620
 
