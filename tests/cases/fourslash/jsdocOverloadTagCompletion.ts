bcabb89111d2083813aa127fabff306ebc3cf79e
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-02-15 00:07:25 +0200

    fix(52761): add overload tag completions

---
+++ tests/cases/fourslash/jsdocOverloadTagCompletion.ts
@@ -0,0 +1,14 @@
+///<reference path="fourslash.ts" />
+
+// @noEmit: true
+// @allowJS: true
+// @checkJs: true
+
+// @filename: /a.js
+/////**
+//// * @/**/
+//// */
+
+verify.completions(
+    { marker: "", includes: ["overload"] },
+);
