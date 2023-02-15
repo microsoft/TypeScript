017709ed8af0afa11848975adfa12bc9a68f9f40
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-02-15 15:49:11 -0800

    Also add type imports

--- tests/baselines/reference/moduleExportNonStructured.js
+++ tests/baselines/reference/moduleExportNonStructured.js
@@ -14,6 +14,9 @@ import * as exportAny from "./exportAny.cjs";
 import * as exportUnknown from "./exportUnknown.cjs";
 import * as exportSymbol from "./exportSymbol.cjs";
 
+import type * as exportAnyType from "./exportAny.cjs";
+import type * as exportUnknownType from "./exportUnknown.cjs";
+import type * as exportSymbolType from "./exportSymbol.cjs";
 
 //// [exportAny.d.cts]
 declare const __: any;
