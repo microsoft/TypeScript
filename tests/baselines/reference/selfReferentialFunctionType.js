2365abecaae4deaffbd96a5e66011b063eaf3d99
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-02-15 14:28:32 -0800

    Rename test

--- tests/baselines/reference/selfReferentialFunctionType.js
+++ tests/baselines/reference/selfReferentialFunctionType.js
@@ -1,4 +1,4 @@
-//// [issue52757.ts]
+//// [selfReferentialFunctionType.ts]
 function f<T>(args: typeof f<T>): T { return args; }
 
 function g<T = typeof g>(args: T): T { return args; }
@@ -6,14 +6,14 @@ function g<T = typeof g>(args: T): T { return args; }
 function h<T>(): typeof h<T> { return h; }
 
 
-//// [issue52757.js]
+//// [selfReferentialFunctionType.js]
 "use strict";
 function f(args) { return args; }
 function g(args) { return args; }
 function h() { return h; }
 
 
-//// [issue52757.d.ts]
+//// [selfReferentialFunctionType.d.ts]
 declare function f<T>(args: typeof f<T>): T;
 declare function g<T = typeof g>(args: T): T;
 declare function h<T>(): typeof h<T>;
