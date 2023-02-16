389e1e26dad01ed8500cc854e38f33775e0fd338
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-02-16 22:59:07 +0200

    update tests

--- tests/baselines/reference/selfReferentialFunctionType.js
+++ tests/baselines/reference/selfReferentialFunctionType.js
@@ -1,16 +1,11 @@
 //// [selfReferentialFunctionType.ts]
-function f<T>(args: typeof f<T>): T { return args; }
-
-function g<T = typeof g>(args: T): T { return args; }
-
-function h<T>(): typeof h<T> { return h; }
+declare function f<T>(args: typeof f<T>): T;
+declare function g<T = typeof g>(args: T): T;
+declare function h<T>(): typeof h<T>;
 
 
 //// [selfReferentialFunctionType.js]
 "use strict";
-function f(args) { return args; }
-function g(args) { return args; }
-function h() { return h; }
 
 
 //// [selfReferentialFunctionType.d.ts]
--- tests/cases/compiler/selfReferentialFunctionType.ts
+++ tests/cases/compiler/selfReferentialFunctionType.ts
@@ -1,8 +1,6 @@
 // @strict: true
 // @declaration: true
 
-function f<T>(args: typeof f<T>): T { return args; }
-
-function g<T = typeof g>(args: T): T { return args; }
-
-function h<T>(): typeof h<T> { return h; }
+declare function f<T>(args: typeof f<T>): T;
+declare function g<T = typeof g>(args: T): T;
+declare function h<T>(): typeof h<T>;
