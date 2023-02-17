cd58bab99c5fbb6f7cc5f6cc845404a75ada9b8f
Vitaly Turovsky <vital2580@icloud.com>
2023-02-17 17:46:33 +0300

    show that jsdoc default takes precedence

--- tests/cases/fourslash/jsDocFunctionSignatures6.ts
+++ tests/cases/fourslash/jsDocFunctionSignatures6.ts
@@ -7,7 +7,7 @@
 //// * @param {string} [p3] - Another optional param
 //// * @param {string} [p4="test"] - An optional param with a default value
 //// */
-////function f1(p1, p2, p3, p4){}
+////function f1(p1, p2, p3, p4 = "Ignored"){}
 ////f1(/*1*/'foo', /*2*/'bar', /*3*/'baz', /*4*/'qux');
 
 verify.baselineSignatureHelp()
