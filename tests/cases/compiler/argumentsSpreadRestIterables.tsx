a570cdf61465592e4d4a5f8710e6c5b97253dba4
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-02-18 11:23:02 +0100

    add extra test cases

--- tests/cases/compiler/argumentsSpreadRestIterables.tsx
+++ tests/cases/compiler/argumentsSpreadRestIterables.tsx
@@ -11,8 +11,12 @@ declare const itNum: Iterable<number>
 declare function fn1<const T extends readonly unknown[]>(...args: T): T;
 const res1 = fn1(..."hello");
 const res2 = fn1(...itNum);
+const res3 = fn1(true, ..."hello");
+const res4 = fn1(true, ...itNum);
 
 // repro from #52781
 declare function foo<T extends unknown[]>(...args: T): T;
-const p = foo(..."hello");
+const p1 = foo(..."hello");
 const p2 = foo(...itNum);
+const p3 = foo(true, ..."hello");
+const p4 = foo(true, ...itNum);
