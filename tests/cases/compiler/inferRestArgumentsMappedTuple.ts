54221fd349cc8d9319c26bb49af25b27c4bbf3b5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-02-18 21:51:19 -0800

    Pull whole test from issue

--- tests/cases/compiler/inferRestArgumentsMappedTuple.ts
+++ tests/cases/compiler/inferRestArgumentsMappedTuple.ts
@@ -8,11 +8,18 @@ type TupleMapperOld<Tuple extends any[]> = {
 	[Key in keyof Tuple]: Tuple[Key] extends Tuple[number] ? MyMappedType<Tuple[Key]> : never;
 };
 
-// [MyMappedType<string>, MyMappedType<number>]
-type MyMappedTupleOld = TupleMapperOld<[string, number]>;
-//    ^?
+type MyMappedTupleOld = TupleMapperOld<[string, number]>; // [MyMappedType<string>, MyMappedType<number>]
 
 declare function extractPrimitivesOld<Tuple extends any[]>(...mappedTypes: TupleMapperOld<Tuple>): Tuple;
 
-// [string, number]
 const myPrimitiveTupleOld: [string, number] = extractPrimitivesOld({ primitive: "" }, { primitive: 0 });
+
+type TupleMapperNew<Tuple extends any[]> = {
+	[Key in keyof Tuple]: MyMappedType<Tuple[Key]>;
+};
+
+type MyMappedTupleNew = TupleMapperNew<[string, number]>;
+
+declare function extractPrimitivesNew<Tuple extends any[]>(...mappedTypes: TupleMapperNew<Tuple>): Tuple;
+
+const myPrimitiveTupleNew: [string, number] = extractPrimitivesNew({ primitive: "" }, { primitive: 0 });
