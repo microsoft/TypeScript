2214526138c92612d2c6089cc63f1e555110a328
Vitaly <vital2580@icloud.com>
2023-02-20 05:15:58 +0300

    Update tests/cases/fourslash/codeFixInferFromUsageAllAssignments.ts

--- tests/cases/fourslash/codeFixInferFromUsageAllAssignments.ts
+++ tests/cases/fourslash/codeFixInferFromUsageAllAssignments.ts
@@ -7,7 +7,6 @@
 ////x ||= "2"
 ////x &&= true
 
-// verify.rangeAfterCodeFix("let x: string | number | boolean;", /*includeWhiteSpace*/ undefined, /*errorCode*/ undefined, 0);
 verify.codeFix({
     description: "Infer type of 'x' from usage",
     index: 0,
