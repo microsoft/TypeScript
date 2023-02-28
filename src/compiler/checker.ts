f08a072da29da58b4f9ca5e739bbf2d038442c3e
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-02-28 22:15:13 +0200

    fix(53011): use unique names for spreading tuples in parameter positions

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -12629,11 +12629,10 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
 
         function expandSignatureParametersWithTupleMembers(restType: TupleTypeReference, restIndex: number) {
             const elementTypes = getTypeArguments(restType);
-            const associatedNames = restType.target.labeledElementDeclarations;
+            const associatedNames = getUniqAssociatedNamesFromTupleType(restType);
             const restParams = map(elementTypes, (t, i) => {
                 // Lookup the label from the individual tuple passed in before falling back to the signature `rest` parameter name
-                const tupleLabelName = !!associatedNames && getTupleElementLabel(associatedNames[i]);
-                const name = tupleLabelName || getParameterNameAtPosition(sig, restIndex + i, restType);
+                const name = associatedNames[i] ? associatedNames[i] : getParameterNameAtPosition(sig, restIndex + i, restType);
                 const flags = restType.target.elementFlags[i];
                 const checkFlags = flags & ElementFlags.Variable ? CheckFlags.RestParameter :
                     flags & ElementFlags.Optional ? CheckFlags.OptionalParameter : 0;
@@ -12642,6 +12641,24 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 return symbol;
             });
             return concatenate(sig.parameters.slice(0, restIndex), restParams);
+        }
+
+        function getUniqAssociatedNamesFromTupleType(type: TupleTypeReference): __String[] {
+            const associatedNamesMap = new Map<__String, number>();
+            const associatedNames: __String[] = [];
+            forEach(type.target.labeledElementDeclarations, (labeledElement, i) => {
+                const name = getTupleElementLabel(labeledElement);
+                const prevCounter = associatedNamesMap.get(name);
+                if (prevCounter === undefined) {
+                    associatedNames[i] = name;
+                    associatedNamesMap.set(name, 1);
+                }
+                else {
+                    associatedNames[i] = `${name}_${prevCounter}` as __String;
+                    associatedNamesMap.set(name, prevCounter + 1);
+                }
+            });
+            return associatedNames;
         }
     }
 
