7162f75f9eada04761aab440ffc15ee1f19bedb6
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2023-03-07 11:47:11 -0800

    Baseline update

---
+++ tests/baselines/reference/unawaitedPromise.js
@@ -0,0 +1,26 @@
+//// [unawaitedPromise.ts]
+declare function doThing(): Promise<string>;
+
+function f() {
+    // Should error
+    doThing();
+
+    let m: Promise<string>;
+    // Should not error
+    m = doThing();
+
+    // Should not error
+    void doThing();
+}
+
+//// [unawaitedPromise.js]
+"use strict";
+function f() {
+    // Should error
+    doThing();
+    var m;
+    // Should not error
+    m = doThing();
+    // Should not error
+    void doThing();
+}
