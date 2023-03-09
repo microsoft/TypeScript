ddcbd9ffbd48ed4e37dfd39c0895938e40c29621
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-03-09 14:32:15 -0800

    Remove old references to Node v4, small cleanup of node version stuff (#53174)

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -8285,7 +8285,6 @@ declare namespace ts {
         negative: boolean;
         base10Value: string;
     }
-    function getNodeMajorVersion(): number | undefined;
     enum FileWatcherEventKind {
         Created = 0,
         Changed = 1,
