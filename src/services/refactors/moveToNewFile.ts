2559bcbf8445dc613358e27250245f6d93306070
Vitaly Turovsky <vital2580@icloud.com>
2023-03-11 01:32:52 +0300

    handle cjs update though wasnt interested in cjs support at all

--- src/services/refactors/moveToNewFile.ts
+++ src/services/refactors/moveToNewFile.ts
@@ -510,7 +510,7 @@ function createRequireCall(moduleSpecifier: StringLiteralLike): CallExpression {
 function addExports(sourceFile: SourceFile, toMove: readonly Statement[], needExport: ReadonlySymbolSet, useEs6Exports: boolean): readonly Statement[] {
     return flatMap(toMove, statement => {
         if (isTopLevelDeclarationStatement(statement) &&
-            !isExported(sourceFile, statement, useEs6Exports) &&
+            !hasExport(sourceFile, statement, useEs6Exports) &&
             forEachTopLevelDeclaration(statement, d => needExport.has(Debug.checkDefined(tryCast(d, canHaveSymbol)?.symbol)))) {
             const exports = addExport(statement, useEs6Exports);
             if (exports) return exports;
@@ -978,6 +978,15 @@ function isExported(sourceFile: SourceFile, decl: TopLevelDeclarationStatement, 
         getNamesToExportInCommonJS(decl).some(name => sourceFile.symbol.exports!.has(escapeLeadingUnderscores(name)));
 }
 
+function hasExport(sourceFile: SourceFile, decl: TopLevelDeclarationStatement, useEs6Exports: boolean, name?: Identifier): boolean {
+    if (useEs6Exports) {
+        return isExported(sourceFile, decl, useEs6Exports, name)
+    }
+    if (!isExpressionStatement(decl)) return false
+    const { expression } = decl;
+    return isBinaryExpression(expression) && getAssignmentDeclarationKind(expression) === AssignmentDeclarationKind.ExportsProperty
+}
+
 function getDefaultExportName(sourceFile: SourceFile) {
     return tryCast((sourceFile.symbol.exports?.get("default" as __String)?.declarations?.[0] as undefined | ExportAssignment)?.expression, isIdentifier);
 }
@@ -991,9 +1000,7 @@ function isExportedLaterInOldFile(sourceFile: SourceFile, decl: TopLevelDeclarat
                 (isExportDeclaration(node) && tryCast(node.exportClause, isNamedExports)?.elements.some(el => el.name.text === name.text))
                 || (isExportAssignment(node) && tryCast(node.expression, isIdentifier)?.text === name.text));
     }
-    return false;
-    // return !!sourceFile.symbol && !!sourceFile.symbol.exports &&
-    //     getNamesToExportInCommonJS(decl).some(name => sourceFile.symbol.exports!.has(escapeLeadingUnderscores(name)));
+    return false
 }
 
 function addExport(decl: TopLevelDeclarationStatement, useEs6Exports: boolean): readonly Statement[] | undefined {
