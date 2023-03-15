074bf34633415df8207ac03350943ea49d187334
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-03-15 18:46:12 +0100

    Do not resolve apparent type of mapped types with a tuple constraint (#52651)

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -13859,7 +13859,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         const typeVariable = getHomomorphicTypeVariable(type);
         if (typeVariable && !type.declaration.nameType) {
             const constraint = getConstraintOfTypeParameter(typeVariable);
-            if (constraint && isArrayOrTupleType(constraint)) {
+            if (constraint && isArrayType(constraint)) {
                 return instantiateType(type, prependTypeMapping(typeVariable, constraint, type.mapper));
             }
         }
