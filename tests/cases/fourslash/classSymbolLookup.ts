6c1bbaaf9e80268706ec19c0c2961895b23b89e2
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2023-03-22 15:06:04 -0700

    remove comments from test

--- tests/cases/fourslash/classSymbolLookup.ts
+++ tests/cases/fourslash/classSymbolLookup.ts
@@ -4,17 +4,12 @@
 
 //// class Foo<T extends Object, C extends {}> {
 ////     private constructor(value : T, context: C){ }
-
 ////     static readonly makeFoo = <C extends {}>(context : C) => 
 ////         <T extends Object>(value : T) => 
 ////             new Foo<T, C>(value, context);
 //// }
-
-//// const x = Foo.makeFoo<{}>;  //// Cannot find name 'Foo'.ts(2304)
-////                             //// 'Foo' only refers to a type, but 
-////                             //// is being used as a namespace here.ts(2702)
-
-//// x({}); // 👍 `const x: <{}>(context: {}) => <T>(value: T) => Foo<T, {}>`
+//// const x = Foo.makeFoo<{}>;
+//// x({});
 
 verify.encodedSemanticClassificationsLength("2020", 75);
 verify.getSemanticDiagnostics([]);
