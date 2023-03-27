88fe45502593dbc351d937ab25f742755d54e6b9
Ron Buckton <ron.buckton@microsoft.com>
2023-03-27 22:20:41 +0000

    Cherry-pick PR #53547 into release-5.0
    Component commits:
    a937637808 Fix double-emit in constructor
    
    d9a6d9abcb Add extra test

--- tests/cases/conformance/esDecorators/classDeclaration/classSuper/esDecorators-classDeclaration-classSuper.7.ts
+++ tests/cases/conformance/esDecorators/classDeclaration/classSuper/esDecorators-classDeclaration-classSuper.7.ts
@@ -22,3 +22,22 @@ function foo(method: any, _context: any): any {
 }
 
 new B();
+
+// https://github.com/microsoft/TypeScript/issues/53448
+class C {
+	public constructor() {
+		this.val;
+	}
+
+	@foo
+	public get val(): number { return 3; }
+}
+class D extends A {
+	public constructor() {
+		super();
+		this.val;
+	}
+
+	@foo
+	public get val(): number { return 3; }
+}
