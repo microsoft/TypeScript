86295fb3d7ffa184f2f00f539171c56c56826961
Maria Solano <mariasolano@microsoft.com>
2023-04-04 13:46:24 -0700

    Include test for non empty literal

--- tests/cases/fourslash/completionsUnionProperty.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="fourslash.ts" />
-
-//// type Foo = { a: 0, b: 'x' } | { a: 0, b: 'y' } | { a: 1, b: 'z' };
-//// const foo: Foo = { a: 0, b: '/**/
-
-verify.completions({ marker: "", triggerCharacter: "'", includes: [ "x", "y" ], excludes: [ "z" ] });
