558746f275a807a38725dfcaad644c822d263889
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-04-06 12:05:38 -0700

    PR feedback

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -17790,11 +17790,11 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 const context = createInferenceContext(freshParams, /*signature*/ undefined, InferenceFlags.None);
                 if (freshMapper) {
                     const freshCombinedMapper = combineTypeMappers(mapper, freshMapper);
-                    forEach(freshParams, (p, i) => {
-                        if (p !== root.inferTypeParameters![i]) {
-                            p.mapper = freshCombinedMapper;
+                    for (let i = 0; i < freshParams.length; i++) {
+                        if (freshParams[i] !== root.inferTypeParameters[i]) {
+                            freshParams[i].mapper = freshCombinedMapper;
                         }
-                    });
+                    }
                 }
                 if (!checkTypeDeferred) {
                     // We don't want inferences from constraints as they may cause us to eagerly resolve the
