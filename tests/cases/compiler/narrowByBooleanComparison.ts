2540cc44e1032c3a993f6073b93de22de29a7517
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-04-09 23:56:09 +0200

    few more tests

--- tests/cases/compiler/narrowByBooleanComparison.ts
+++ tests/cases/compiler/narrowByBooleanComparison.ts
@@ -10,34 +10,50 @@ const isA = (x: MyUnion): x is A => x.type === "A";
 
 function test1(x: MyUnion) {
     if (isA(x) !== true) {
-        x
+        x;
     }
 
     if (isA(x) !== false) {
-        x
+        x;
     }
 
     if (isA(x) === false) {
-        x
+        x;
     }
 
     if (isA(x) === true) {
-        x
+        x;
     }
 
     if (isA(x) != true) {
-        x
+        x;
     }
 
     if (isA(x) == true) {
-        x
+        x;
     }
 
     if (true !== isA(x)) {
-        x
+        x;
     }
 
     if (true === isA(x)) {
-        x
+        x;
     }
+}
+
+// https://github.com/microsoft/TypeScript/issues/53093
+function test2(x: unknown) {
+    if (x instanceof Error === false) {
+        return;
+    }
+    x;
+}
+
+// https://github.com/microsoft/TypeScript/issues/50712
+function test3(foo: unknown) {
+    if (typeof foo !== 'string' && Array.isArray(foo) === false) {
+        throw new Error('Not a string or an array');
+    }
+    foo;
 }
