9adca32f66f190e7f8c57b2179d1f1a85bbb46a3
eps1lon <silbermann.sebastian@gmail.com>
2023-04-13 04:50:34 +0200

    Drop the `getApparentType`
    it's cleaner

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -30867,7 +30867,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 const tagType = isJsxIntrinsicIdentifier(jsxOpeningLikeNode.tagName)
                     ? // TODO: Should this be a literal that library authors could potentially check against?
                         stringType
-                    : getApparentType(checkExpression(jsxOpeningLikeNode.tagName));
+                    : checkExpression(jsxOpeningLikeNode.tagName);
                 checkTypeRelatedTo(tagType, elementTypeConstraint, assignableRelation, jsxOpeningLikeNode.tagName, Diagnostics.Its_type_0_is_not_a_valid_JSX_element_type, () => {
                     const componentName = getTextOfNode(jsxOpeningLikeNode.tagName);
                     return chainDiagnosticMessages(/*details*/ undefined, Diagnostics._0_cannot_be_used_as_a_JSX_component, componentName);
