81b00f693be502363635aef0d898aa8ad7b0200f
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-04-13 21:45:00 +0300

    update tests

--- tests/cases/compiler/accessorDeclarationEmitJs.ts
+++ tests/cases/compiler/accessorDeclarationEmitJs.ts
@@ -3,9 +3,22 @@
 // @declaration: true
 // @emitDeclarationOnly: true
 // @filename: /a.js
-export const object = {
-    property: 'value',
+export const t1 = {
+    p: 'value',
     get getter() {
         return 'value';
     },
+}
+
+export const t2 = {
+    v: 'value',
+    set setter(v) {},
+}
+
+export const t3 = {
+    p: 'value',
+    get value() {
+        return 'value';
+    },
+    set value(v) {},
 }
