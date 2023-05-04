29eac1be2aade1f2f1e0650649e3ebce402fec0e
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-05-04 13:58:29 -0700

    Test for checkJs

--- src/testRunner/unittests/tscWatch/programUpdates.ts
+++ src/testRunner/unittests/tscWatch/programUpdates.ts
@@ -2047,4 +2047,40 @@ import { x } from "../b";`),
             },
         ]
     });
+
+    verifyTscWatch({
+        scenario,
+        subScenario: "when changing `checkJs` of config file",
+        commandLineArgs: ["-w", "-p", ".", "--extendedDiagnostics"],
+        sys: () => {
+            const module1: File = {
+                path: `/user/username/projects/myproject/a.js`,
+                content: `export const aNumber: number = "string"`
+            };
+            const module2: File = {
+                path: `/user/username/projects/myproject/b.ts`,
+                content: `import { aNumber } from "./a.js";`
+            };
+            const config: File = {
+                path: `/user/username/projects/myproject/tsconfig.json`,
+                content: JSON.stringify({
+                    compilerOptions: {
+                        checkJs: false
+                    }
+                }),
+            };
+            return createWatchedSystem([module1, module2, config, libFile], { currentDirectory: "/user/username/projects/myproject" });
+        },
+        edits: [
+            {
+                caption: "Change checkJs to true",
+                edit: sys => sys.writeFile(`/user/username/projects/myproject/tsconfig.json`, JSON.stringify({
+                    compilerOptions: {
+                        checkJs: true
+                    }
+                })),
+                timeouts: sys => sys.runQueuedTimeoutCallbacks(),
+            },
+        ]
+    });
 });
