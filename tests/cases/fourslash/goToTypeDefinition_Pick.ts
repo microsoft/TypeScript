2f2f3f6852494464833edfe9bc36b4d6944e45d9
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-05-10 16:32:45 +0200

    add more tests

---
+++ tests/cases/fourslash/goToTypeDefinition_Pick.ts
@@ -0,0 +1,7 @@
+/// <reference path='fourslash.ts' />
+
+//// type User = { id: number; name: string; };
+//// declare const user: Pick<User, "name">
+//// /*reference*/user
+
+verify.baselineGoToType("reference");
--- tests/cases/fourslash/goToTypeDefinition_arrayType.ts
+++ tests/cases/fourslash/goToTypeDefinition_arrayType.ts
@@ -3,5 +3,9 @@
 //// type User = { name: string };
 //// declare const users: User[]
 //// /*reference*/users
+////
+//// class CustomArray<T> extends Array<T> { immutableReverse() { return [...this].reverse() } }
+//// declare const users2: CustomArray<User>
+//// /*reference2*/users2
 
-verify.baselineGoToType("reference");
+verify.baselineGoToType("reference", "reference2");
