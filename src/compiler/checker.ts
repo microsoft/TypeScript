13169870b49569025db0ffbde2174f7bfdcbc449
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-05-24 22:14:19 +0300

    fix(54152): Cannot use const enum to define an interface key in isolatedModules mode (#54165)

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -37825,7 +37825,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         if (getIsolatedModules(compilerOptions)) {
             Debug.assert(!!(type.symbol.flags & SymbolFlags.ConstEnum));
             const constEnumDeclaration = type.symbol.valueDeclaration as EnumDeclaration;
-            if (constEnumDeclaration.flags & NodeFlags.Ambient) {
+            if (constEnumDeclaration.flags & NodeFlags.Ambient && !isValidTypeOnlyAliasUseSite(node)) {
                 error(node, Diagnostics.Cannot_access_ambient_const_enums_when_0_is_enabled, isolatedModulesLikeFlagName);
             }
         }
