c679717448b166a0497cb39cdf726079b22816ae
Maria Solano <mariasolano@microsoft.com>
2023-05-25 14:18:17 -0700

    Exported variable test

---
+++ tests/cases/fourslash/inlineVariableExportedVariable.ts
@@ -0,0 +1,11 @@
+/// <reference path="fourslash.ts" />
+
+////export const x/*a*/ = 1;
+////const y/*b*/ = 2;
+////export { y };
+
+goTo.marker("a");
+verify.not.refactorAvailable("Inline variable");
+
+goTo.marker("b");
+verify.not.refactorAvailable("Inline variable");
