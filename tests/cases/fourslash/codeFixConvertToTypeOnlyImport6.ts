1e0b2f4912b9ff2c215129d75d50d4812089beee
Andrew Branch <andrewbranch@users.noreply.github.com>
2023-06-01 09:57:08 -0700

    Code fix should prefer `import type` (#54255)

--- tests/cases/fourslash/codeFixConvertToTypeOnlyImport6.ts
+++ tests/cases/fourslash/codeFixConvertToTypeOnlyImport6.ts
@@ -7,16 +7,20 @@
 ////export const foo = {};
 
 // @filename: /a.ts
-////import { I, foo } from "./b";
+//// import { I, type foo } from "./b";
+//// export declare const x: typeof foo;
 
 goTo.file("/a.ts");
 verify.codeFix({
     index: 0,
     description: ts.Diagnostics.Use_import_type.message,
-    newFileContent: `import type { I, foo } from "./b";`
+    newFileContent: `import type { I, foo } from "./b";
+export declare const x: typeof foo;`,
 });
+
 verify.codeFix({
     index: 1,
     description: `Use 'type I'`,
-    newFileContent: `import { type I, foo } from "./b";`
+    newFileContent: `import { type I, type foo } from "./b";
+export declare const x: typeof foo;`,
 });
