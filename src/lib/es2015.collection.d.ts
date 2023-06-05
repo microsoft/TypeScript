e6303df1f2ab0c259718fb4e17518e8a1786dd86
Ryuujo Zhang <me@ryuujo.com>
2023-06-05 19:28:14 +0800

    Update [es2015].collection.d.ts
    It's ok to call `map.get(key)` when `key` is undefined, and in this case, the result is usually `undefined`.

--- src/lib/es2015.collection.d.ts
+++ src/lib/es2015.collection.d.ts
@@ -13,7 +13,7 @@ interface Map<K, V> {
      * Returns a specified element from the Map object. If the value that is associated to the provided key is an object, then you will get a reference to that object and any change made to that object will effectively modify it inside the Map.
      * @returns Returns the element associated with the specified key. If no element is associated with the specified key, undefined is returned.
      */
-    get(key: K): V | undefined;
+    get(key: K | undefined): V | undefined;
     /**
      * @returns boolean indicating whether an element with the specified key exists or not.
      */
