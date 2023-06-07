a259b3a67635261e70c49e96c0944f8b36c7de4f
Maria José Solano <majosolano99@gmail.com>
2023-06-07 09:46:25 -0500

    Improve comments

--- src/services/refactors/inlineVariable.ts
+++ src/services/refactors/inlineVariable.ts
@@ -118,7 +118,7 @@ function getInliningInfo(file: SourceFile, startPosition: number, tryWithReferen
     // If the node is a variable declaration, make sure it's not in a catch clause or for-loop
     // and that it has a value.
     if (isInitializedVariable(parent) && isVariableDeclarationInVariableStatement(parent) && isIdentifier(parent.name)) {
-        // Don't inline the variable if it isn't declared exactly once.
+        // Don't inline the variable if it has multiple declarations.
         if (parent.symbol.declarations?.length !== 1) {
             return { error: getLocaleSpecificMessage(Diagnostics.Variables_that_share_a_name_with_a_type_or_namespace_in_the_same_scope_cannot_be_inlined) };
         }
@@ -133,8 +133,8 @@ function getInliningInfo(file: SourceFile, startPosition: number, tryWithReferen
         return references && { references, declaration: parent, replacement: parent.initializer };
     }
 
+    // Try finding the declaration and nodes to replace via the reference token.
     if (tryWithReferenceToken && isIdentifier(token)) {
-        // Try finding the declaration and nodes to replace via the reference token.
         const definition = checker.resolveName(token.text, token, SymbolFlags.Value, /*excludeGlobals*/ false);
         if (definition?.declarations?.length !== 1) {
             return { error: getLocaleSpecificMessage(Diagnostics.Variables_that_share_a_name_with_a_type_or_namespace_in_the_same_scope_cannot_be_inlined) };
