071857699164377868bf6aaed2e415ecabcbf99c
Matej Sadovsky <matej.sadov@gmail.com>
2023-06-16 02:40:45 -0700

    Enable parsing of nested parameters for `@callback` JSDoc tag.

---
+++ tests/cases/conformance/jsdoc/callbackTagNestedParameter.ts
@@ -0,0 +1,21 @@
+// @emitDeclarationOnly: true
+// @declaration: true
+// @allowJs: true
+// @checkJs: true
+// @Filename: cb_nested.js
+/**
+ * @callback WorksWithPeopleCallback
+ * @param {Object} person
+ * @param {String} person.name
+ * @param {Number} [person.age]
+ * @returns {void}
+ */
+
+/**
+ * For each person, calls your callback.
+ * @param {WorksWithPeopleCallback} callback
+ * @returns {void}
+ */
+function eachPerson(callback) {
+    callback({ name: "Empty" });
+}
