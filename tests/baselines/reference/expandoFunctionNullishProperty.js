ea4c002cfe9bf52d09e943f12eabf85e4e82b7e0
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-06-21 18:22:41 +0200

    Fixed expando functions with nullish properties

---
+++ tests/baselines/reference/expandoFunctionNullishProperty.js
@@ -0,0 +1,54 @@
+//// [tests/cases/compiler/expandoFunctionNullishProperty.ts] ////
+
+//// [expandoFunctionNullishProperty.ts]
+// mentioned in https://github.com/microsoft/TypeScript/issues/54220
+
+interface TestNull {
+  (): void;
+  readonly prop: null;
+}
+
+export function testNull(): TestNull {
+  function inner() {}
+  inner.prop = null;
+  return inner;
+}
+
+interface TestUndefined {
+  (): void;
+  readonly prop: undefined;
+}
+
+export function testUndefined(): TestUndefined {
+  function inner() {}
+  inner.prop = undefined;
+  return inner;
+}
+
+
+//// [expandoFunctionNullishProperty.js]
+// mentioned in https://github.com/microsoft/TypeScript/issues/54220
+export function testNull() {
+    function inner() { }
+    inner.prop = null;
+    return inner;
+}
+export function testUndefined() {
+    function inner() { }
+    inner.prop = undefined;
+    return inner;
+}
+
+
+//// [expandoFunctionNullishProperty.d.ts]
+interface TestNull {
+    (): void;
+    readonly prop: null;
+}
+export declare function testNull(): TestNull;
+interface TestUndefined {
+    (): void;
+    readonly prop: undefined;
+}
+export declare function testUndefined(): TestUndefined;
+export {};
