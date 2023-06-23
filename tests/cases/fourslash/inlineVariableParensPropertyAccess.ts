8b6227fadc06f8cbde816f520ef2b41eec9c74ac
Maria José Solano <majosolano99@gmail.com>
2023-06-23 15:48:53 -0700

    More tests

--- tests/cases/fourslash/inlineVariableParensPropertyAccess.ts
+++ tests/cases/fourslash/inlineVariableParensPropertyAccess.ts
@@ -4,6 +4,8 @@
 ////foo.toString();
 ////const /*a2*/bar/*b2*/ = 0;
 ////bar.toFixed().toString();
+////const /*a3*/pi/*b3*/ = 3.1416;
+////pi.toPrecision(2);
 
 goTo.select("a1", "b1");
 verify.refactorAvailable("Inline variable");
@@ -13,7 +15,9 @@ edit.applyRefactor({
     actionDescription: "Inline variable",
     newContent: `({}).toString();
 const bar = 0;
-bar.toFixed().toString();`
+bar.toFixed().toString();
+const pi = 3.1416;
+pi.toPrecision(2);`
 });
 
 goTo.select("a2", "b2");
@@ -23,5 +27,18 @@ edit.applyRefactor({
     actionName: "Inline variable",
     actionDescription: "Inline variable",
     newContent: `({}).toString();
-(0).toFixed().toString();`
+(0).toFixed().toString();
+const pi = 3.1416;
+pi.toPrecision(2);`
+});
+
+goTo.select("a3", "b3");
+verify.refactorAvailable("Inline variable");
+edit.applyRefactor({
+    refactorName: "Inline variable",
+    actionName: "Inline variable",
+    actionDescription: "Inline variable",
+    newContent: `({}).toString();
+(0).toFixed().toString();
+(3.1416).toPrecision(2);`
 });
