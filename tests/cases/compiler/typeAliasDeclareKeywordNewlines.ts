831fad4f66f9aea6e8fd2253e4abc7ed2db9282a
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-06-26 20:07:51 -0700

    Enable type checking in test and add more documentation and assertions

--- tests/cases/compiler/typeAliasDeclareKeywordNewlines.ts
+++ tests/cases/compiler/typeAliasDeclareKeywordNewlines.ts
@@ -1,12 +1,19 @@
-// @noTypesAndSymbols: true
-var declare, type, T;
+var declare: any, type: any;
 
+// The following is invalid but should declare a type alias named 'T1':
 declare type /*unexpected newline*/
 T1 = null;
+const t1: T1 = null;
 
+let T: null;
+// The following should use a variable named 'declare', use a variable named
+// 'type', and assign to a variable named 'T'.
 declare /*ASI*/
 type /*ASI*/
 T = null;
 
+// The following should use a variable named 'declare' and declare a type alias
+// named 'T2':
 declare /*ASI*/
 type T2 = null;
+const t2: T2 = null;
