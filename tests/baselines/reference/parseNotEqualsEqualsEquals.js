b43d880015954fd8a5cd3e6fd4ed227e3463caa8
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-07-05 14:35:40 -0700

    Add test case

---
+++ tests/baselines/reference/parseNotEqualsEqualsEquals.js
@@ -0,0 +1,10 @@
+//// [tests/cases/compiler/parseNotEqualsEqualsEquals.ts] ////
+
+//// [parseNotEqualsEqualsEquals.ts]
+if (1 !=== 2) {}
+
+
+//// [parseNotEqualsEqualsEquals.js]
+if (1 !== )
+     = 2;
+{ }
