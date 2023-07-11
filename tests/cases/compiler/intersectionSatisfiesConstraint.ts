5a6f4ee5b789b910e6b837ad5e9710c989987dc4
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-07-11 09:58:22 -0700

    Add test case for intersection/constraint bug fixed in 4.9

---
+++ tests/cases/compiler/intersectionSatisfiesConstraint.ts
@@ -0,0 +1,18 @@
+// @strict: true
+
+interface FirstInterface {
+    commonProperty: number
+}
+
+interface SecondInterface {
+    commonProperty: number
+}
+
+const myFirstFunction = <T extends FirstInterface | SecondInterface>(param1: T) => {
+    const newParam: T & { otherProperty: number } = Object.assign(param1, { otherProperty: 3 })
+    mySecondFunction(newParam)
+}
+
+const mySecondFunction = <T extends { commonProperty: number, otherProperty: number }>(newParam: T) => {
+    return newParam
+}
