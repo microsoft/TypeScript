706e1c91aafe83d95a67508745fcf9e163a0d193
Kevin Ring <kevin@kotachrome.com>
2023-07-15 11:01:48 +1000

    Add a test for comments on object literal accessors.

---
+++ tests/baselines/reference/commentsOnObjectLiteralAccessors.js
@@ -0,0 +1,44 @@
+//// [tests/cases/compiler/commentsOnObjectLiteralAccessors.ts] ////
+
+//// [commentsOnObjectLiteralAccessors.ts]
+var v = {
+    /**
+     * Get accessor
+     */
+    get bar(): number {
+     return 12;
+    },
+
+    /**
+     * Set accessor
+     */
+    set bar(v: number) {}
+   }
+   
+
+//// [commentsOnObjectLiteralAccessors.js]
+var v = {
+    /**
+     * Get accessor
+     */
+    get bar() {
+        return 12;
+    },
+    /**
+     * Set accessor
+     */
+    set bar(v) { }
+};
+
+
+//// [commentsOnObjectLiteralAccessors.d.ts]
+declare var v: {
+    /**
+     * Get accessor
+     */
+    get bar(): number;
+    /**
+     * Set accessor
+     */
+    set bar(v: number);
+};
