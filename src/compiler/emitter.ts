2950c05a1699ffbbc2b43050cba1c579a891724c
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-07-15 22:52:56 +0300

    fix(54992): parenthesize negative constant enums

--- src/compiler/emitter.ts
+++ src/compiler/emitter.ts
@@ -1158,6 +1158,7 @@ export const notImplementedResolver: EmitResolver = {
     isEntityNameVisible: notImplemented,
     // Returns the constant value this property access resolves to: notImplemented, or 'undefined' for a non-constant
     getConstantValue: notImplemented,
+    shouldParenthesizeConstantValue: notImplemented,
     getReferencedValueDeclaration: notImplemented,
     getReferencedValueDeclarations: notImplemented,
     getTypeReferenceSerializationKind: notImplemented,
