212931910035e2e2835085cd561abfeb0c1791a8
Sg <suica@users.noreply.github.com>
2023-07-18 23:24:01 +0800

    fix: fix failed test

--- tests/cases/fourslash/server/completions04.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../fourslash.ts"/>
-
-// issue: https://github.com/microsoft/TypeScript/issues/54729
-
-//// let foo
-//// /**/
-
-verify.completions({ marker: "", includes: "const" });
