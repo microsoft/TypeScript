0588091706982d311af2151f89c666e89f9dfdc2
Sg <suica@users.noreply.github.com>
2023-07-18 23:37:47 +0800

    test: modify test case

--- tests/cases/fourslash/completionAfterNewline2.ts
+++ tests/cases/fourslash/completionAfterNewline2.ts
@@ -3,18 +3,16 @@
 // issue: https://github.com/microsoft/TypeScript/issues/54729
 // Tests that `isCompletionListBlocker` is true at position 1, but false after a newline.
 
-////const obj = {
-////  name: '',
-////} as const /*1*/
+////let foo = 5 as const /*1*/
 /////*2*/
 
 verify.completions(
-  { marker: "1", exact: undefined },
+  { marker: ["1"], exact: undefined },
   {
     marker: "2",
     exact: completion.globalsPlus([
       {
-        name: "obj",
+        name: "foo",
       },
     ]),
   }
