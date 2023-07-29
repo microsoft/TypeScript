140d4e120cbc868f7ae555bc81e64dfc3b421b4d
Xiaokang Xin <xiaokang.xin@airbnb.com>
2023-07-29 16:03:28 -0700

    update Array#forEach: use a generic type parameter to ensure elements type are inferred correctly

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1237,7 +1237,7 @@ interface ReadonlyArray<T> {
      * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
      * @param thisArg  An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
      */
-    forEach(callbackfn: (value: T, index: number, array: readonly T[]) => void, thisArg?: any): void;
+    forEach<U>(callbackfn: (value: T, index: number, array: readonly T[]) => void, thisArg?: any): void;
     /**
      * Calls a defined callback function on each element of an array, and returns an array that contains the results.
      * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
