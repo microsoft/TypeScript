f5dc309695877aaafb5d07d26084625d970a480b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-08-01 15:57:11 +0200

    Allow escape characters within constructor

---
+++ tests/baselines/reference/constructorWithEscapeSequence(target=esnext).js
@@ -0,0 +1,18 @@
+//// [tests/cases/compiler/constructorWithEscapeSequence.ts] ////
+
+//// [constructorWithEscapeSequence.ts]
+export class C {
+  \u{63}onstructor() {}
+}
+
+
+//// [constructorWithEscapeSequence.js]
+export class C {
+    constructor() { }
+}
+
+
+//// [constructorWithEscapeSequence.d.ts]
+export declare class C {
+    constructor();
+}
