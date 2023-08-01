1071d916a720e32f4a0f1d0139015aa84ff595fe
Mvmo <mvmogo@gmail.com>
2023-08-01 05:59:02 +0200

    New error messages

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -48424,8 +48424,29 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         while (current) {
             if (isFunctionLikeOrClassStaticBlockDeclaration(current)) {
                 if (node.label) {
-                    return grammarErrorOnNode(node, Diagnostics.Cannot_find_label_0, node.label.escapedText.toString());
+                    const diagnostic = createDiagnosticForNode(node, Diagnostics.Cannot_find_label_0, node.label.escapedText.toString());
+
+                    const functionOrClassLike = current as FunctionLikeDeclaration | ClassStaticBlockDeclaration;
+                    if (!functionOrClassLike.body) {
+                        break;
+                    }
+
+                    let matchingLabelLine: number | undefined;
+                    forEachChild(functionOrClassLike.body, (childNode) => {
+                        if (matchingLabelLine) {
+                            return;
+                        }
+
+                        const labeledStatement = childNode as LabeledStatement;
+                        if (labeledStatement.kind === SyntaxKind.LabeledStatement && labeledStatement.label.escapedText === node.label!.escapedText) {
+                            diagnostic.relatedInformation = [createDiagnosticForNode(labeledStatement, Diagnostics.Label_0_declared_after_usage, labeledStatement.label.escapedText.toString())];
+                        }
+                    });
+
+                    diagnostics.add(diagnostic);
+                    return true;
                 }
+                // mvmo
                 return grammarErrorOnNode(node, Diagnostics.Jump_target_cannot_cross_function_boundary);
             }
 
