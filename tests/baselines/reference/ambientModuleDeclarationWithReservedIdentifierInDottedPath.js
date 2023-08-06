099106e5477a0ff3b1b03516196c3fa3a95ccb9e
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-08-06 10:27:47 +0200

    Parse reserved identifiers as dotted names of ambient modules

---
+++ tests/baselines/reference/ambientModuleDeclarationWithReservedIdentifierInDottedPath.js
@@ -0,0 +1,30 @@
+//// [tests/cases/conformance/ambient/ambientModuleDeclarationWithReservedIdentifierInDottedPath.ts] ////
+
+//// [ambientModuleDeclarationWithReservedIdentifierInDottedPath.ts]
+// https://github.com/microsoft/TypeScript/issues/7840
+
+declare module chrome.debugger {
+    declare var tabId: number;
+}
+
+export const tabId = chrome.debugger.tabId;
+
+declare module test.class {}
+
+declare module debugger {} // still an error
+
+
+//// [ambientModuleDeclarationWithReservedIdentifierInDottedPath.js]
+"use strict";
+// https://github.com/microsoft/TypeScript/issues/7840
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.tabId = void 0;
+exports.tabId = chrome.debugger.tabId;
+declare;
+module;
+debugger;
+{ } // still an error
+
+
+//// [ambientModuleDeclarationWithReservedIdentifierInDottedPath.d.ts]
+export declare const tabId: number;
