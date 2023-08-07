2af5eb508502be41d7466dfe7ff2cc3807602872
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-08-07 23:52:22 +0200

    Preserve input key style of computed properties in declaration emit

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -8082,7 +8082,14 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
 
         function isStringNamed(d: Declaration) {
             const name = getNameOfDeclaration(d);
-            return !!name && isStringLiteral(name);
+            if (!name) {
+                return false;
+            }
+            if (isComputedPropertyName(name)) {
+                const type = checkExpression(name.expression);
+                return !!(type.flags & TypeFlags.StringLike);
+            }
+            return isStringLiteral(name);
         }
 
         function isSingleQuotedStringNamed(d: Declaration) {
