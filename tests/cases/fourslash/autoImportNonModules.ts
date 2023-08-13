4dc42251aebb756c640b080a1aecb3486b9ff383
Hardik <creatinggalaxy@gmail.com>
2023-08-13 12:34:14 +0530

    Adding Unit Test

---
+++ tests/cases/fourslash/autoImportNonModules.ts
@@ -0,0 +1,10 @@
+/// <reference path='fourslash.ts'/>
+// @module: none
+// @Filename:test.ts
+//// export function f(){ return false }
+
+// @Filename:test1.ts
+//// /**/
+
+goTo.marker("");
+verify.completions();
