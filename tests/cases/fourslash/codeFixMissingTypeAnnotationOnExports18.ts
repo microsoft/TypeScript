7f574a8797fa3a30224a8f47a98f8306aeb89c02
Hana Joo <hanajoo@google.com>
2023-08-20 15:34:26 +0000

    Add a test case for Symbol
    Signed-off-by: Hana Joo <hanajoo@google.com>

---
+++ tests/cases/fourslash/codeFixMissingTypeAnnotationOnExports18.ts
@@ -0,0 +1,19 @@
+/// <reference path='fourslash.ts'/>
+
+// @isolatedDeclarations: true
+// @declaration: true
+// @lib: es2019
+//// export const a = Symbol();
+
+verify.codeFixAvailable([
+    { description: ts.Diagnostics.Declaration_emit_for_this_file_requires_type_resolution_An_explicit_type_annotation_may_unblock_declaration_emit.message },
+    { description: ts.Diagnostics.Declaration_emit_for_this_file_requires_type_resolution_An_explicit_type_annotation_may_unblock_declaration_emit.message }
+]);
+
+// TODO: the typeof operator here is wrong.
+verify.codeFix({
+    description: ts.Diagnostics.Declaration_emit_for_this_file_requires_type_resolution_An_explicit_type_annotation_may_unblock_declaration_emit.message,
+    index: 0,
+    newFileContent:
+`export const x: typeof x = Symbol();`
+});
