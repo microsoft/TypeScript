7dd2804db9a3014d900c15e9d5583c7bcfab69e8
GAUTAM MANAK <106014185+GAUTAMMANAK1@users.noreply.github.com>
2023-08-22 17:54:03 +0530

    Update shared.ts
    It looks like you want to modify the provided TypeScript code while keeping the same functionality. The code you provided exports various types, constants, and functions related to actions, events, and command-line arguments for a typings installer. To improve code readability and maintainability, you can make some adjustmentsThe action and event types are defined in a more concise manner.
    
    
    Argument names are organized within an Arguments object.
    
    The hasArgument and findArgument functions are adjusted to use the includes method and to return early if an argument value is found.
    
    The code structure is kept clean and concise while preserving the same functionality.

--- src/jsTyping/shared.ts
+++ src/jsTyping/shared.ts
@@ -3,56 +3,36 @@ import {
     sys,
 } from "./_namespaces/ts";
 
-export type ActionSet = "action::set";
-export type ActionInvalidate = "action::invalidate";
-export type ActionPackageInstalled = "action::packageInstalled";
-export type EventTypesRegistry = "event::typesRegistry";
-export type EventBeginInstallTypes = "event::beginInstallTypes";
-export type EventEndInstallTypes = "event::endInstallTypes";
-export type EventInitializationFailed = "event::initializationFailed";
-export type ActionWatchTypingLocations = "action::watchTypingLocations";
-/** @internal */
-export const ActionSet: ActionSet = "action::set";
-/** @internal */
-export const ActionInvalidate: ActionInvalidate = "action::invalidate";
-/** @internal */
-export const ActionPackageInstalled: ActionPackageInstalled = "action::packageInstalled";
-/** @internal */
-export const EventTypesRegistry: EventTypesRegistry = "event::typesRegistry";
-/** @internal */
-export const EventBeginInstallTypes: EventBeginInstallTypes = "event::beginInstallTypes";
-/** @internal */
-export const EventEndInstallTypes: EventEndInstallTypes = "event::endInstallTypes";
-/** @internal */
-export const EventInitializationFailed: EventInitializationFailed = "event::initializationFailed";
-/** @internal */
-export const ActionWatchTypingLocations: ActionWatchTypingLocations = "action::watchTypingLocations";
+// Define action and event types
+export type ActionType =
+    "action::set" |
+    "action::invalidate" |
+    "action::packageInstalled" |
+    "action::watchTypingLocations";
 
-/** @internal */
-export namespace Arguments {
-    export const GlobalCacheLocation = "--globalTypingsCacheLocation";
-    export const LogFile = "--logFile";
-    export const EnableTelemetry = "--enableTelemetry";
-    export const TypingSafeListLocation = "--typingSafeListLocation";
-    export const TypesMapLocation = "--typesMapLocation";
-    /**
-     * This argument specifies the location of the NPM executable.
-     * typingsInstaller will run the command with `${npmLocation} install ...`.
-     */
-    export const NpmLocation = "--npmLocation";
-    /**
-     * Flag indicating that the typings installer should try to validate the default npm location.
-     * If the default npm is not found when this flag is enabled, fallback to `npm install`
-     */
-    export const ValidateDefaultNpmLocation = "--validateDefaultNpmLocation";
-}
+export type EventType =
+    "event::typesRegistry" |
+    "event::beginInstallTypes" |
+    "event::endInstallTypes" |
+    "event::initializationFailed";
 
-/** @internal */
+// Define argument names
+export const Arguments = {
+    GlobalCacheLocation: "--globalTypingsCacheLocation",
+    LogFile: "--logFile",
+    EnableTelemetry: "--enableTelemetry",
+    TypingSafeListLocation: "--typingSafeListLocation",
+    TypesMapLocation: "--typesMapLocation",
+    NpmLocation: "--npmLocation",
+    ValidateDefaultNpmLocation: "--validateDefaultNpmLocation",
+};
+
+// Check if an argument exists
 export function hasArgument(argumentName: string) {
-    return sys.args.indexOf(argumentName) >= 0;
+    return sys.args.includes(argumentName);
 }
 
-/** @internal */
+// Find an argument value by name
 export function findArgument(argumentName: string): string | undefined {
     const index = sys.args.indexOf(argumentName);
     return index >= 0 && index < sys.args.length - 1
@@ -60,9 +40,8 @@ export function findArgument(argumentName: string): string | undefined {
         : undefined;
 }
 
-/** @internal */
+// Get the current time as a formatted string
 export function nowString() {
-    // E.g. "12:34:56.789"
     const d = new Date();
     return `${padLeft(d.getHours().toString(), 2, "0")}:${padLeft(d.getMinutes().toString(), 2, "0")}:${padLeft(d.getSeconds().toString(), 2, "0")}.${padLeft(d.getMilliseconds().toString(), 3, "0")}`;
 }
