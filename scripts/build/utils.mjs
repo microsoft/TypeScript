0e79ff66258dda325aa73ba20f6e64257d33c97e
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-08-23 15:39:45 -0700

    Update scripts/build/utils.mjs

--- scripts/build/utils.mjs
+++ scripts/build/utils.mjs
@@ -237,6 +237,6 @@ export function memoize(fn) {
  * @param {fs.PathLike} p
  */
 export function rimraf(p) {
-    // Node's recursive rm is just vendored rimraf, but without a different maxRetries default on Windows.
+    // The rimraf package uses maxRetries=10 on Windows, but Node's fs.rm does not have that special case.
     return fs.promises.rm(p, { recursive: true, force: true, maxRetries: process.platform === "win32" ? 10 : 0 });
 }
