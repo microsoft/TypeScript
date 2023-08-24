e0a324b0503be479f2b33fd2e17c6e86c94d1297
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-08-24 19:17:33 +0200

    Don't parse `DotDotDotToken` in JSX attribute values (#55284)

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -6214,7 +6214,9 @@ namespace Parser {
         let dotDotDotToken: DotDotDotToken | undefined;
         let expression: Expression | undefined;
         if (token() !== SyntaxKind.CloseBraceToken) {
-            dotDotDotToken = parseOptionalToken(SyntaxKind.DotDotDotToken);
+            if (!inExpressionContext) {
+                dotDotDotToken = parseOptionalToken(SyntaxKind.DotDotDotToken);
+            }
             // Only an AssignmentExpression is valid here per the JSX spec,
             // but we can unambiguously parse a comma sequence and provide
             // a better error message in grammar checking.
