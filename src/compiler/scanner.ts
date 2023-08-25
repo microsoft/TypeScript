dc5a8e02092c94cb3a385e78a7e3d532204da90c
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-08-25 15:45:04 -0700

    codePointAt

--- src/compiler/scanner.ts
+++ src/compiler/scanner.ts
@@ -2792,25 +2792,10 @@ export function createScanner(languageVersion: ScriptTarget, skipTrivia: boolean
 }
 
 /** @internal */
-const codePointAt: (s: string, i: number) => number = (String.prototype as any).codePointAt ? (s, i) => (s as any).codePointAt(i) : function codePointAt(str, i): number {
-    // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/codePointAt
-    const size = str.length;
-    // Account for out-of-bounds indices:
-    if (i < 0 || i >= size) {
-        return undefined!; // String.codePointAt returns `undefined` for OOB indexes
-    }
-    // Get the first code unit
-    const first = str.charCodeAt(i);
-    // check if it's the start of a surrogate pair
-    if (first >= 0xD800 && first <= 0xDBFF && size > i + 1) { // high surrogate and there is a next code unit
-        const second = str.charCodeAt(i + 1);
-        if (second >= 0xDC00 && second <= 0xDFFF) { // low surrogate
-            // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-            return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
-        }
-    }
-    return first;
-};
+function codePointAt(s: string, i: number): number {
+    // TODO(jakebailey): this is wrong and should have ?? 0; but all users are okay with it
+    return s.codePointAt(i)!;
+}
 
 /** @internal */
 function charSize(ch: number) {
