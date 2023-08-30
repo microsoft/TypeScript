8a75fd750211163b4b8d371973a1e1e0541e89f1
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-08-30 19:17:23 +0200

    add extra test case

--- tests/cases/compiler/discriminateWithDivergentAccessors1.ts
+++ tests/cases/compiler/discriminateWithDivergentAccessors1.ts
@@ -10,3 +10,14 @@ declare const weirdoBox: WeirdoBox<number>;
 if (weirdoBox.done) {
   weirdoBox.value;
 }
+
+type WeirdoBox2<T> =
+  | { get done(): false; set done(v: T | null) }
+  | { get done(): true; set done(v: T | null); value: T }
+  | { get done(): true; set done(v: T | null | undefined); value: number };
+
+declare const weirdoBox2: WeirdoBox2<string>;
+
+if (weirdoBox2.done) {
+  weirdoBox2.value;
+}
