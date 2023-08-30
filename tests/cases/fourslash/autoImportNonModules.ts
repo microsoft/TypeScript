c05c588f846a8d755510206e173f288eb451ceab
Hardik <creatinggalaxy@gmail.com>
2023-08-30 10:23:45 +0530

    adding @filename

--- tests/cases/fourslash/autoImportNonModules.ts
+++ tests/cases/fourslash/autoImportNonModules.ts
@@ -4,6 +4,7 @@
 //// /* Test scenarios start here */
 
 // Test scenario 1: module === ModuleKind.None
+//// @filename: file1.ts
 //// const compilerOptions1: CompilerOptions = { module: ModuleKind.None };
 //// const result1 = compilerOptionsIndicateEsModules(compilerOptions1);
 //// result1;
