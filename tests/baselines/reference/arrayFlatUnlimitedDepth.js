a983d8ff41cadc7239a1ce553296366e0df4448e
Przemysław Krasiński <przemek@krasinski.xyz>
2023-09-08 17:51:34 +0200

    Test array flat unlimited depth

---
+++ tests/baselines/reference/arrayFlatUnlimitedDepth.js
@@ -0,0 +1,11 @@
+//// [tests/cases/compiler/arrayFlatUnlimitedDepth.ts] ////
+
+//// [arrayFlatUnlimitedDepth.ts]
+const foo = [ [ ['string' ] ] ]
+const flatFoo : string[] = foo.flat(1000) 
+
+
+//// [arrayFlatUnlimitedDepth.js]
+"use strict";
+var foo = [[['string']]];
+var flatFoo = foo.flat(1000);
---
+++ tests/cases/compiler/arrayFlatUnlimitedDepth.ts
@@ -0,0 +1,4 @@
+// @strict: true
+// @lib: es2019
+const foo = [ [ ['string' ] ] ]
+const flatFoo : string[] = foo.flat(1000) 
