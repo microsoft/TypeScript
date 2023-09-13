49f8c32e619518e8f65cb56228599f712fa7268e
Andrew Branch <andrew@wheream.io>
2023-09-12 17:57:58 -0700

    Avoid unnecessary resolution-mode assertion in declaration emit

---
+++ tests/baselines/reference/dtsEmitTripleSlashAvoidUnnecessaryResolutionMode.js
@@ -0,0 +1,28 @@
+//// [tests/cases/compiler/dtsEmitTripleSlashAvoidUnnecessaryResolutionMode.ts] ////
+
+//// [package.json]
+{
+  "name": "@types/node",
+  "version": "1.0.0",
+  "types": "index.d.ts"
+}
+
+//// [globals.d.ts]
+declare namespace NodeJS {
+  interface ReadableStream {}
+}
+
+//// [index.d.ts]
+/// <reference path="globals.d.ts" />
+
+//// [app.mts]
+/// <reference types="node" />
+export async function drainStream(stream: NodeJS.ReadableStream): Promise<void> {
+}
+
+
+
+
+//// [app.d.mts]
+/// <reference types="node" />
+export declare function drainStream(stream: NodeJS.ReadableStream): Promise<void>;
