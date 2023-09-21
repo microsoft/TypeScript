d7231ad48a4a597baf2ba5add8c43abd61c7ea6e
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-09-21 16:40:59 -0700

    Update baselines

--- tests/baselines/reference/api/typescript.d.ts
+++ tests/baselines/reference/api/typescript.d.ts
@@ -6816,6 +6816,7 @@ declare namespace ts {
         None = 0,
         NoTruncation = 1,
         WriteArrayAsGenericType = 2,
+        GenerateNamesForShadowedTypeParams = 4,
         UseStructuralFallback = 8,
         WriteTypeArgumentsOfSignature = 32,
         UseFullyQualifiedType = 64,
@@ -6835,7 +6836,7 @@ declare namespace ts {
         InElementType = 2097152,
         InFirstTypeArgument = 4194304,
         InTypeAlias = 8388608,
-        NodeBuilderFlagsMask = 848330091,
+        NodeBuilderFlagsMask = 848330095,
     }
     enum SymbolFormatFlags {
         None = 0,
