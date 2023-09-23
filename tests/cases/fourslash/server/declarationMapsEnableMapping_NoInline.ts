eb3ee315b913265a71fdf743d97e4af90cdf6756
Ron Buckton <ron.buckton@microsoft.com>
2023-09-22 22:49:22 -0400

    Fix merge issue in tests

--- tests/cases/fourslash/server/declarationMapsEnableMapping_NoInline.ts
+++ tests/cases/fourslash/server/declarationMapsEnableMapping_NoInline.ts
@@ -1,89 +1,3 @@
-<<<<<<< HEAD
-/// <reference path="../fourslash.ts" />
-
-// @Filename: /tsconfig.json
-////{
-////    "compilerOptions": {
-////        "outDir": "./dist",
-////        "inlineSourceMap": true,
-////        "declaration": true,
-////        "declarationMap": true,
-////        "newLine": "lf",
-////    },
-////    "files": ["/index.ts"],
-////}
-
-// @Filename: /index.ts
-// @emitThisFile: true
-////export class Foo {
-////    member: string;
-////    /*2*/methodName(propName: SomeType): SomeType { return propName; }
-////    otherMethod() {
-////        if (Math.random() > 0.5) {
-////            return {x: 42};
-////        }
-////        return {y: "yes"};
-////    }
-////}
-////
-////export interface /*SomeType*/SomeType {
-////    member: number;
-////}
-
-// @Filename: /mymodule.ts
-////import * as mod from "/dist/index";
-////const instance = new mod.Foo();
-////instance.[|/*1*/methodName|]({member: 12});
-
-// @Filename: /dist/index.js
-////"use strict";
-////exports.__esModule = true;
-////exports.Foo = void 0;
-////var Foo = /** @class */ (function () {
-////    function Foo() {
-////    }
-////    Foo.prototype.methodName = function (propName) { return propName; };
-////    Foo.prototype.otherMethod = function () {
-////        if (Math.random() > 0.5) {
-////            return { x: 42 };
-////        }
-////        return { y: "yes" };
-////    };
-////    return Foo;
-////}());
-////exports.Foo = Foo;
-//////# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQTtJQUFBO0lBU0EsQ0FBQztJQVBHLHdCQUFVLEdBQVYsVUFBVyxRQUFrQixJQUFjLE9BQU8sUUFBUSxDQUFDLENBQUMsQ0FBQztJQUM3RCx5QkFBVyxHQUFYO1FBQ0ksSUFBSSxJQUFJLENBQUMsTUFBTSxFQUFFLEdBQUcsR0FBRyxFQUFFLENBQUM7WUFDdEIsT0FBTyxFQUFDLENBQUMsRUFBRSxFQUFFLEVBQUMsQ0FBQztRQUNuQixDQUFDO1FBQ0QsT0FBTyxFQUFDLENBQUMsRUFBRSxLQUFLLEVBQUMsQ0FBQztJQUN0QixDQUFDO0lBQ0wsVUFBQztBQUFELENBQUMsQUFURCxJQVNDO0FBVFksa0JBQUcifQ==
-
-// @Filename: /dist/index.d.ts.map
-////{"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../index.ts"],"names":[],"mappings":"AAAA,qBAAa,GAAG;IACZ,MAAM,EAAE,MAAM,CAAC;IACf,UAAU,CAAC,QAAQ,EAAE,QAAQ,GAAG,QAAQ;IACxC,WAAW;;;;;;;CAMd;AAED,MAAM,WAAW,QAAQ;IACrB,MAAM,EAAE,MAAM,CAAC;CAClB"}
-
-// @Filename: /dist/index.d.ts
-////export declare class Foo {
-////    member: string;
-////    methodName(propName: SomeType): SomeType;
-////    otherMethod(): {
-////        x: number;
-////        y?: undefined;
-////    } | {
-////        y: string;
-////        x?: undefined;
-////    };
-////}
-////export interface SomeType {
-////    member: number;
-////}
-//////# sourceMappingURL=index.d.ts.map
-
-goTo.file("/index.ts");
-verify.getEmitOutput(["/dist/index.js", "/dist/index.d.ts.map", "/dist/index.d.ts"]);
-
-verify.goToDefinition("1", "2"); // getDefinitionAndBoundSpan
-verify.goToType("1", "SomeType"); // getTypeDefinitionAtPosition
-goTo.marker("1");
-verify.goToDefinitionIs("2"); // getDefinitionAtPosition
-goTo.implementation(); // getImplementationAtPosition
-verify.caretAtMarker("2");
-=======
 /// <reference path="../fourslash.ts" />
 
 // @Filename: /tsconfig.json
@@ -137,7 +51,7 @@ verify.caretAtMarker("2");
 ////    return Foo;
 ////}());
 ////exports.Foo = Foo;
-//////# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQTtJQUFBO0lBU0EsQ0FBQztJQVBHLHdCQUFVLEdBQVYsVUFBVyxRQUFrQixJQUFjLE9BQU8sUUFBUSxDQUFDLENBQUMsQ0FBQztJQUM3RCx5QkFBVyxHQUFYO1FBQ0ksSUFBSSxJQUFJLENBQUMsTUFBTSxFQUFFLEdBQUcsR0FBRyxFQUFFO1lBQ3JCLE9BQU8sRUFBQyxDQUFDLEVBQUUsRUFBRSxFQUFDLENBQUM7U0FDbEI7UUFDRCxPQUFPLEVBQUMsQ0FBQyxFQUFFLEtBQUssRUFBQyxDQUFDO0lBQ3RCLENBQUM7SUFDTCxVQUFDO0FBQUQsQ0FBQyxBQVRELElBU0M7QUFUWSxrQkFBRyJ9
+//////# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQTtJQUFBO0lBU0EsQ0FBQztJQVBHLHdCQUFVLEdBQVYsVUFBVyxRQUFrQixJQUFjLE9BQU8sUUFBUSxDQUFDLENBQUMsQ0FBQztJQUM3RCx5QkFBVyxHQUFYO1FBQ0ksSUFBSSxJQUFJLENBQUMsTUFBTSxFQUFFLEdBQUcsR0FBRyxFQUFFLENBQUM7WUFDdEIsT0FBTyxFQUFDLENBQUMsRUFBRSxFQUFFLEVBQUMsQ0FBQztRQUNuQixDQUFDO1FBQ0QsT0FBTyxFQUFDLENBQUMsRUFBRSxLQUFLLEVBQUMsQ0FBQztJQUN0QixDQUFDO0lBQ0wsVUFBQztBQUFELENBQUMsQUFURCxJQVNDO0FBVFksa0JBQUcifQ==
 
 // @Filename: /dist/index.d.ts.map
 ////{"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../index.ts"],"names":[],"mappings":"AAAA,qBAAa,GAAG;IACZ,MAAM,EAAE,MAAM,CAAC;IACf,UAAU,CAAC,QAAQ,EAAE,QAAQ,GAAG,QAAQ;IACxC,WAAW;;;;;;;CAMd;AAED,MAAM,WAAW,QAAQ;IACrB,MAAM,EAAE,MAAM,CAAC;CAClB"}
@@ -168,4 +82,3 @@ verify.baselineCommands(
     { type: "goToDefinition", markerOrRange: "1" }, // getDefinitionAndBoundSpan
     { type: "getDefinitionAtPosition", markerOrRange: "1" }, // getDefinitionAtPosition
 );
->>>>>>> main
