1aed7dcbfd4e04a1b47c5fd960d8703df4662642
Sheetal Nandi <shkamat@microsoft.com>
2023-09-25 13:51:39 -0700

    Fix issue with symlink not using trailing directory separator Fixes #55135

--- tests/baselines/reference/tsserver/moduleResolution/using-referenced-project.js
+++ tests/baselines/reference/tsserver/moduleResolution/using-referenced-project.js
@@ -716,3 +716,231 @@ Info seq  [hh:mm:ss:mss] event:
      }
     }
 After running Immedidate callback:: count: 0
+
+Before request
+
+Info seq  [hh:mm:ss:mss] request:
+    {
+      "command": "updateOpen",
+      "arguments": {
+        "changedFiles": [
+          {
+            "fileName": "/home/src/projects/project/packages/package-b/src/index.ts",
+            "textChanges": [
+              {
+                "start": {
+                  "line": 1,
+                  "offset": 31
+                },
+                "end": {
+                  "line": 1,
+                  "offset": 32
+                },
+                "newText": ""
+              }
+            ]
+          }
+        ]
+      },
+      "seq": 5,
+      "type": "request"
+    }
+Info seq  [hh:mm:ss:mss] response:
+    {
+      "response": true,
+      "responseRequired": true
+    }
+After request
+
+Before request
+
+Info seq  [hh:mm:ss:mss] request:
+    {
+      "command": "geterr",
+      "arguments": {
+        "delay": 0,
+        "files": [
+          "/home/src/projects/project/packages/package-b/src/index.ts"
+        ]
+      },
+      "seq": 6,
+      "type": "request"
+    }
+Info seq  [hh:mm:ss:mss] response:
+    {
+      "responseRequired": false
+    }
+After request
+
+Before running Timeout callback:: count: 1
+3: checkOne
+
+Info seq  [hh:mm:ss:mss] Starting updateGraphWorker: Project: /home/src/projects/project/packages/package-b/tsconfig.json
+Info seq  [hh:mm:ss:mss] ======== Resolving module 'package-a' from '/home/src/projects/project/packages/package-b/src/index.ts'. ========
+Info seq  [hh:mm:ss:mss] Explicitly specified module resolution kind: 'Bundler'.
+Info seq  [hh:mm:ss:mss] Resolving in CJS mode with conditions 'import', 'types'.
+Info seq  [hh:mm:ss:mss] 'baseUrl' option is set to '/home/src/projects/project/packages/package-b', using this value to resolve non-relative module name 'package-a'.
+Info seq  [hh:mm:ss:mss] Resolving module name 'package-a' relative to base url '/home/src/projects/project/packages/package-b' - '/home/src/projects/project/packages/package-b/package-a'.
+Info seq  [hh:mm:ss:mss] Loading module as file / folder, candidate module location '/home/src/projects/project/packages/package-b/package-a', target file types: TypeScript, JavaScript, Declaration, JSON.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-b/package-a.ts' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-b/package-a.tsx' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-b/package-a.d.ts' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-b/package-a.js' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-b/package-a.jsx' does not exist.
+Info seq  [hh:mm:ss:mss] Directory '/home/src/projects/project/packages/package-b/package-a' does not exist, skipping all lookups in it.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-b/src/package.json' does not exist according to earlier cached lookups.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-b/package.json' exists according to earlier cached lookups.
+Info seq  [hh:mm:ss:mss] Loading module 'package-a' from 'node_modules' folder, target file types: TypeScript, JavaScript, Declaration, JSON.
+Info seq  [hh:mm:ss:mss] Searching all ancestor node_modules directories for preferred extensions: TypeScript, Declaration.
+Info seq  [hh:mm:ss:mss] Directory '/home/src/projects/project/packages/package-b/src/node_modules' does not exist, skipping all lookups in it.
+Info seq  [hh:mm:ss:mss] Directory '/home/src/projects/project/packages/package-b/node_modules' does not exist, skipping all lookups in it.
+Info seq  [hh:mm:ss:mss] Directory '/home/src/projects/project/packages/node_modules' does not exist, skipping all lookups in it.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/node_modules/package-a/package.json' exists according to earlier cached lookups.
+Info seq  [hh:mm:ss:mss] Using 'exports' subpath '.' with target './build/index.js'.
+Info seq  [hh:mm:ss:mss] File name '/home/src/projects/project/node_modules/package-a/build/index.js' has a '.js' extension - stripping it.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/node_modules/package-a/build/index.ts' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/node_modules/package-a/build/index.tsx' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/node_modules/package-a/build/index.d.ts' exists - use it as a name resolution result.
+Info seq  [hh:mm:ss:mss] Resolving real path for '/home/src/projects/project/node_modules/package-a/build/index.d.ts', result '/home/src/projects/project/packages/package-a/build/index.d.ts'.
+Info seq  [hh:mm:ss:mss] ======== Module name 'package-a' was successfully resolved to '/home/src/projects/project/packages/package-a/build/index.d.ts' with Package ID 'package-a/build/index.d.ts@1.0.0'. ========
+Info seq  [hh:mm:ss:mss] ======== Resolving module './subfolder' from '/home/src/projects/project/packages/package-a/src/index.ts'. ========
+Info seq  [hh:mm:ss:mss] Using compiler options of project reference redirect '/home/src/projects/project/packages/package-a/tsconfig.json'.
+Info seq  [hh:mm:ss:mss] Explicitly specified module resolution kind: 'Bundler'.
+Info seq  [hh:mm:ss:mss] Resolving in CJS mode with conditions 'import', 'types'.
+Info seq  [hh:mm:ss:mss] Loading module as file / folder, candidate module location '/home/src/projects/project/packages/package-a/src/subfolder', target file types: TypeScript, JavaScript, Declaration, JSON.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-a/src/subfolder.ts' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-a/src/subfolder.tsx' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-a/src/subfolder.d.ts' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-a/src/subfolder.js' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-a/src/subfolder.jsx' does not exist.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-a/src/subfolder/package.json' does not exist according to earlier cached lookups.
+Info seq  [hh:mm:ss:mss] File '/home/src/projects/project/packages/package-a/src/subfolder/index.ts' exists - use it as a name resolution result.
+Info seq  [hh:mm:ss:mss] ======== Module name './subfolder' was successfully resolved to '/home/src/projects/project/packages/package-a/src/subfolder/index.ts'. ========
+Info seq  [hh:mm:ss:mss] DirectoryWatcher:: Added:: WatchInfo: /home/src/projects/project/packages/package-a 1 undefined Project: /home/src/projects/project/packages/package-b/tsconfig.json WatchType: Failed Lookup Locations
+Info seq  [hh:mm:ss:mss] Elapsed:: *ms DirectoryWatcher:: Added:: WatchInfo: /home/src/projects/project/packages/package-a 1 undefined Project: /home/src/projects/project/packages/package-b/tsconfig.json WatchType: Failed Lookup Locations
+Info seq  [hh:mm:ss:mss] Reusing resolution of module '@typescript/lib-es2021' from '/home/src/projects/project/packages/package-b/__lib_node_modules_lookup_lib.es2021.d.ts__.ts' of old program, it was not resolved.
+Info seq  [hh:mm:ss:mss] DirectoryWatcher:: Added:: WatchInfo: /home/src/projects/project/packages/package-b/package-a 1 undefined Project: /home/src/projects/project/packages/package-b/tsconfig.json WatchType: Failed Lookup Locations
+Info seq  [hh:mm:ss:mss] Elapsed:: *ms DirectoryWatcher:: Added:: WatchInfo: /home/src/projects/project/packages/package-b/package-a 1 undefined Project: /home/src/projects/project/packages/package-b/tsconfig.json WatchType: Failed Lookup Locations
+Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /home/src/projects/project/packages/package-a/package.json 2000 undefined Project: /home/src/projects/project/packages/package-b/tsconfig.json WatchType: File location affecting resolution
+Info seq  [hh:mm:ss:mss] DirectoryWatcher:: Close:: WatchInfo: /home/src/projects/project/packages/package-b/package-aX 1 undefined Project: /home/src/projects/project/packages/package-b/tsconfig.json WatchType: Failed Lookup Locations
+Info seq  [hh:mm:ss:mss] Elapsed:: *ms DirectoryWatcher:: Close:: WatchInfo: /home/src/projects/project/packages/package-b/package-aX 1 undefined Project: /home/src/projects/project/packages/package-b/tsconfig.json WatchType: Failed Lookup Locations
+Info seq  [hh:mm:ss:mss] Finishing updateGraphWorker: Project: /home/src/projects/project/packages/package-b/tsconfig.json Version: 3 structureChanged: true structureIsReused:: SafeModules Elapsed:: *ms
+Info seq  [hh:mm:ss:mss] Project '/home/src/projects/project/packages/package-b/tsconfig.json' (Configured)
+Info seq  [hh:mm:ss:mss] 	Files (4)
+	/a/lib/lib.es2021.d.ts Text-1 "/// <reference no-default-lib=\"true\"/>\ninterface Boolean {}\ninterface Function {}\ninterface CallableFunction {}\ninterface NewableFunction {}\ninterface IArguments {}\ninterface Number { toExponential: any; }\ninterface Object {}\ninterface RegExp {}\ninterface String { charAt: any; }\ninterface Array<T> { length: number; [n: number]: T; }\ninterface ReadonlyArray<T> {}\ndeclare const console: { log(msg: any): void; };"
+	/home/src/projects/project/packages/package-a/src/subfolder/index.ts Text-1 "export const FOO = \"bar\";"
+	/home/src/projects/project/packages/package-a/src/index.ts Text-1 "export * from \"./subfolder\";"
+	/home/src/projects/project/packages/package-b/src/index.ts SVC-1-2 "import { FOO } from \"package-a\";\nconsole.log(FOO);\n"
+
+
+	../../../../../../a/lib/lib.es2021.d.ts
+	  Library 'lib.es2021.d.ts' specified in compilerOptions
+	../package-a/src/subfolder/index.ts
+	  Imported via "./subfolder" from file '../package-a/src/index.ts'
+	../package-a/src/index.ts
+	  Imported via "package-a" from file 'src/index.ts' with packageId 'package-a/build/index.d.ts@1.0.0'
+	src/index.ts
+	  Matched by include pattern './src/**/*.ts' in 'tsconfig.json'
+
+Info seq  [hh:mm:ss:mss] -----------------------------------------------
+Info seq  [hh:mm:ss:mss] event:
+    {
+     "seq": 0,
+     "type": "event",
+     "event": "syntaxDiag",
+     "body": {
+      "file": "/home/src/projects/project/packages/package-b/src/index.ts",
+      "diagnostics": []
+     }
+    }
+After running Timeout callback:: count: 0
+
+PolledWatches::
+/home/src/projects/node_modules:
+  {"pollingInterval":500}
+/home/src/projects/node_modules/@types:
+  {"pollingInterval":500}
+/home/src/projects/project/node_modules/@types:
+  {"pollingInterval":500}
+/home/src/projects/project/packages/node_modules:
+  {"pollingInterval":500}
+/home/src/projects/project/packages/node_modules/@types:
+  {"pollingInterval":500}
+/home/src/projects/project/packages/package-b/node_modules:
+  {"pollingInterval":500}
+/home/src/projects/project/packages/package-b/node_modules/@types:
+  {"pollingInterval":500}
+/home/src/projects/project/packages/package-b/package-a: *new*
+  {"pollingInterval":500}
+
+PolledWatches *deleted*::
+/home/src/projects/project/packages/package-b/package-ax:
+  {"pollingInterval":500}
+
+FsWatches::
+/a/lib/lib.es2021.d.ts:
+  {}
+/home/src/projects/project/packages/package-a/package.json: *new*
+  {}
+/home/src/projects/project/packages/package-a/src/index.ts:
+  {}
+/home/src/projects/project/packages/package-a/src/subfolder/index.ts:
+  {}
+/home/src/projects/project/packages/package-a/tsconfig.json:
+  {}
+/home/src/projects/project/packages/package-b:
+  {}
+/home/src/projects/project/packages/package-b/package.json:
+  {}
+/home/src/projects/project/packages/package-b/tsconfig.json:
+  {}
+
+FsWatchesRecursive::
+/home/src/projects/project/node_modules:
+  {}
+/home/src/projects/project/packages/package-a: *new*
+  {}
+/home/src/projects/project/packages/package-a/src:
+  {}
+/home/src/projects/project/packages/package-b/src:
+  {}
+
+Before running Immedidate callback:: count: 1
+5: semanticCheck
+
+Info seq  [hh:mm:ss:mss] event:
+    {
+     "seq": 0,
+     "type": "event",
+     "event": "semanticDiag",
+     "body": {
+      "file": "/home/src/projects/project/packages/package-b/src/index.ts",
+      "diagnostics": []
+     }
+    }
+After running Immedidate callback:: count: 1
+6: suggestionCheck
+
+Before running Immedidate callback:: count: 1
+6: suggestionCheck
+
+Info seq  [hh:mm:ss:mss] event:
+    {
+     "seq": 0,
+     "type": "event",
+     "event": "suggestionDiag",
+     "body": {
+      "file": "/home/src/projects/project/packages/package-b/src/index.ts",
+      "diagnostics": []
+     }
+    }
+Info seq  [hh:mm:ss:mss] event:
+    {
+     "seq": 0,
+     "type": "event",
+     "event": "requestCompleted",
+     "body": {
+      "request_seq": 6
+     }
+    }
+After running Immedidate callback:: count: 0
