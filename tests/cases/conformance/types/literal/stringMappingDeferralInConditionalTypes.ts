9f56c7f72f69fb793ced78eb9c95441ec6ecbc1c
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-09-25 12:57:50 +0200

    add extra test cases

--- tests/cases/conformance/types/literal/stringMappingDeferralInConditionalTypes.ts
+++ tests/cases/conformance/types/literal/stringMappingDeferralInConditionalTypes.ts
@@ -8,3 +8,9 @@ let x1: A<"foo"> = 1; // ok
 
 type B<S> = Lowercase<S & string> extends `f${string}` ? 1 : 0;
 let x2: B<"foo"> = 1; // ok
+
+type C<S> = Capitalize<Lowercase<S & string>> extends "Foo" ? 1 : 0;
+let x3: C<"foo"> = 1; // ok
+
+type D<S extends string> = Capitalize<Lowercase<S>> extends "Foo" ? 1 : 0;
+let x4: D<"foo"> = 1; // ok
