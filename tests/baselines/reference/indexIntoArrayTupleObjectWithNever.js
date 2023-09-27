eadf94df6b2060b5b56ba20a5fa53e7ac2dfe305
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-09-27 13:10:09 +0200

    enhance test taking the example from the issue

--- tests/baselines/reference/indexIntoArrayTupleObjectWithNever.js
+++ tests/baselines/reference/indexIntoArrayTupleObjectWithNever.js
@@ -7,4 +7,28 @@ type B = [42][never]
 
 type C = Array<42>[never]
 
+
+// -----------------------------------------------------------------
+
+type Indexes<T extends readonly any[]> = keyof T & `${number}`
+
+declare namespace IfNoDuplicateElements {
+    export type Duplicate = "Duplicate"
+    export type NotDuplicate = "NotDuplicate"
+    export type HasDuplicates<T extends readonly any[]> = {
+         [K in Indexes<T>]: T[K] extends T[Exclude<Indexes<T>, K>] ? Duplicate : NotDuplicate
+    }[Indexes<T>]
+}
+
+export type IfNoDuplicateElements<
+    T extends readonly any[],
+    True = T,
+    False = never
+> = IfNoDuplicateElements.Duplicate extends IfNoDuplicateElements.HasDuplicates<T> ? False : True
+
+type T0 = IfNoDuplicateElements<[1], true, false>
+type T1 = IfNoDuplicateElements<[1,1], true, false>
+
 //// [indexIntoArrayTupleObjectWithNever.js]
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
