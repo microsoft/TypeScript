274821eb8860cba5f01946c4b1751c3d73d38795
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-10-04 20:15:43 +0200

    Don't create children prop out of non-semantic JSX children (#55981)

---
+++ tests/baselines/reference/jsxContainsOnlyTriviaWhiteSpacesNotCountedAsChild.js
@@ -0,0 +1,34 @@
+//// [tests/cases/compiler/jsxContainsOnlyTriviaWhiteSpacesNotCountedAsChild.tsx] ////
+
+//// [jsxContainsOnlyTriviaWhiteSpacesNotCountedAsChild.tsx]
+namespace JSX {
+  export interface ElementChildrenAttribute {
+    children: {};
+  }
+}
+
+interface Props {
+  className?: string | undefined;
+}
+
+function NoticeList(props: Props) {
+  return null;
+}
+
+<NoticeList className="my-notice-list">
+</NoticeList>;
+
+<NoticeList className="my-notice-list">
+
+</NoticeList>;
+
+//// [jsxContainsOnlyTriviaWhiteSpacesNotCountedAsChild.jsx]
+"use strict";
+function NoticeList(props) {
+    return null;
+}
+<NoticeList className="my-notice-list">
+</NoticeList>;
+<NoticeList className="my-notice-list">
+
+</NoticeList>;
