0603e72c4a34eab984aa162f980b95e38c79bf44
Wesley Wigham <wwigham@gmail.com>
2023-10-09 13:46:24 -0700

    Reuse more input nodes when they contain local type parameter references

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -6620,8 +6620,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 }
                 if (
                     context.flags & NodeBuilderFlags.GenerateNamesForShadowedTypeParams &&
-                    type.flags & TypeFlags.TypeParameter &&
-                    !isTypeSymbolAccessible(type.symbol, context.enclosingDeclaration)
+                    type.flags & TypeFlags.TypeParameter
                 ) {
                     const name = typeParameterToName(type, context);
                     context.approximateLength += idText(name).length;
@@ -7510,7 +7509,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 && signature.declaration
                 && signature.declaration !== context.enclosingDeclaration
                 && !isInJSFile(signature.declaration)
-                && some(expandedParams)
+                && (some(expandedParams) || some(signature.typeParameters))
             ) {
                 // As a performance optimization, reuse the same fake scope within this chain.
                 // This is especially needed when we are working on an excessively deep type;
@@ -7534,7 +7533,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 const locals = existingFakeScope?.locals ?? createSymbolTable();
 
                 let newLocals: __String[] | undefined;
-                for (const param of expandedParams) {
+                for (const param of concatenate(expandedParams, map(signature.typeParameters, p => p.symbol))) {
                     if (!locals.has(param.escapedName)) {
                         newLocals = append(newLocals, param.escapedName);
                         locals.set(param.escapedName, param);
@@ -8149,7 +8148,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 // `i` we've used thus far, to save work later
                 (context.typeParameterNamesByTextNextNameCount ||= new Map()).set(rawtext, i);
                 (context.typeParameterNames ||= new Map()).set(getTypeId(type), result);
-                (context.typeParameterNamesByText ||= new Set()).add(rawtext);
+                (context.typeParameterNamesByText ||= new Set()).add(text);
             }
             return result;
         }
@@ -8407,8 +8406,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                     includePrivateSymbol?.(sym);
                 }
                 if (isIdentifier(node)) {
-                    const type = getDeclaredTypeOfSymbol(sym);
-                    const name = sym.flags & SymbolFlags.TypeParameter && !isTypeSymbolAccessible(type.symbol, context.enclosingDeclaration) ? typeParameterToName(type, context) : factory.cloneNode(node);
+                    const name = sym.flags & SymbolFlags.TypeParameter ? typeParameterToName(getDeclaredTypeOfSymbol(sym), context) : factory.cloneNode(node);
                     name.symbol = sym; // for quickinfo, which uses identifier symbol information
                     return { introducesError, node: setEmitFlags(setOriginalNode(name, node), EmitFlags.NoAsciiEscaping) };
                 }
--- tests/baselines/reference/typeParametersAvailableInNestedScope3.js
+++ tests/baselines/reference/typeParametersAvailableInNestedScope3.js
@@ -5,10 +5,13 @@ function foo<T>(v: T) {
     function a<T>(a: T) { return a; }
     function b(): T { return v; }
 
+    type Alias = T;
     function c<T>(v: T) {
+        type Alias2 = T;
         function a<T>(a: T) { return a; }
         function b(): T { return v; }
-        return { a, b };
+        function c<T>(): [Alias, Alias2, T] { return null as any; }
+        return { a, b, c };
     }
 
     return { a, b, c };
@@ -22,7 +25,8 @@ function foo(v) {
     function c(v) {
         function a(a) { return a; }
         function b() { return v; }
-        return { a: a, b: b };
+        function c() { return null; }
+        return { a: a, b: b, c: c };
     }
     return { a: a, b: b, c: c };
 }
@@ -35,5 +39,6 @@ declare function foo<T>(v: T): {
     c: <T_2>(v: T_2) => {
         a: <T_3>(a: T_3) => T_3;
         b: () => T_2;
+        c: <T_4>() => [T, T_2, T_4];
     };
 };
