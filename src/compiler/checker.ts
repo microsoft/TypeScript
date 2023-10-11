358a7bd218b2b0bdaf2de03ef12ddd0d23015eea
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-10-11 14:16:01 -0700

    Don't just use instantiation expression node in printing, as it will encode info from decl that may not match actual instantiation

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -6869,13 +6869,6 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                 else {
                     const isInstantiationExpressionType = !!(getObjectFlags(type) & ObjectFlags.InstantiationExpressionType);
                     if (isInstantiationExpressionType) {
-                        const instantiationExpressionType = type as InstantiationExpressionType;
-                        if (isTypeQueryNode(instantiationExpressionType.node)) {
-                            const typeNode = serializeExistingTypeNode(context, instantiationExpressionType.node);
-                            if (typeNode) {
-                                return typeNode;
-                            }
-                        }
                         if (context.visitedTypes?.has(typeId)) {
                             return createElidedInformationPlaceholder(context);
                         }
