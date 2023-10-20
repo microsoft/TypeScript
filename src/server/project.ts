592bba486bf73877b5981981e535d4950dc975d8
Sheetal Nandi <shkamat@microsoft.com>
2023-10-20 14:19:19 -0700

    Verify getUnresolvedImports incrementally

--- src/server/project.ts
+++ src/server/project.ts
@@ -1418,6 +1418,13 @@ export abstract class Project implements LanguageServiceHost, ModuleResolutionHo
         updateProjectIfDirty(this);
     }
 
+    /** @internal */
+    useTypingsFromGlobalCache(): boolean {
+        return this.languageServiceEnabled &&
+            this.projectService.serverMode === LanguageServiceMode.Semantic &&
+            !this.isOrphan();
+    }
+
     /**
      * Updates set of files that contribute to this project
      * @returns: true if set of files in the project stays the same and false - otherwise.
@@ -1440,7 +1447,7 @@ export abstract class Project implements LanguageServiceHost, ModuleResolutionHo
 
         // update builder only if language service is enabled
         // otherwise tell it to drop its internal state
-        if (this.languageServiceEnabled && this.projectService.serverMode === LanguageServiceMode.Semantic && !this.isOrphan()) {
+        if (this.useTypingsFromGlobalCache()) {
             // 1. no changes in structure, no changes in unresolved imports - do nothing
             // 2. no changes in structure, unresolved imports were changed - collect unresolved imports for all files
             // (can reuse cached imports for files that were not changed)
@@ -1460,6 +1467,7 @@ export abstract class Project implements LanguageServiceHost, ModuleResolutionHo
         else {
             this.lastCachedUnresolvedImportsList = undefined;
         }
+        this.projectService.verifyUnresovedImports(this);
 
         const isFirstProgramLoad = this.projectProgramVersion === 0 && hasNewProgram;
         if (hasNewProgram) {
@@ -2361,7 +2369,8 @@ export abstract class Project implements LanguageServiceHost, ModuleResolutionHo
     }
 }
 
-function getUnresolvedImports(
+/** @internal */
+export function getUnresolvedImports(
     program: Program,
     cachedUnresolvedImportsPerFile: Map<Path, readonly string[]>,
     writeLog: (s: string) => void,
