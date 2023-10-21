7bd508fe9a0f16044409376f50a7895196935e86
Josh Goldberg <git@joshuakgoldberg.com>
2023-10-21 09:27:26 -0400

    Hmm, tests seem to be passing locally...

---
+++ tests/baselines/reference/nonTsxExtensionSelfClosingTag.js
@@ -0,0 +1,16 @@
+//// [tests/cases/conformance/jsx/nonTsxExtensionSelfClosingTag.ts] ////
+
+//// [nonTsxExtensionSelfClosingTag.ts]
+console.log("before");
+export const _ = () => <span />;
+console.log("after");
+
+
+//// [nonTsxExtensionSelfClosingTag.js]
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._ = void 0;
+console.log("before");
+var _ = function () { return ; };
+exports._ = _;
+console.log("after");
---
+++ tests/baselines/reference/nonTsxExtensionSelfClosingTagMultiline.js
@@ -0,0 +1,17 @@
+//// [tests/cases/conformance/jsx/nonTsxExtensionSelfClosingTagMultiline.ts] ////
+
+//// [nonTsxExtensionSelfClosingTagMultiline.ts]
+console.log("before");
+export const _ = () => <span
+/>;
+console.log("after");
+
+
+//// [nonTsxExtensionSelfClosingTagMultiline.js]
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._ = void 0;
+console.log("before");
+var _ = function () { return ; };
+exports._ = _;
+console.log("after");
