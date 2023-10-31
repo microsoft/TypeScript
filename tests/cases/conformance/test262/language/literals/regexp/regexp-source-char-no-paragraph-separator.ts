2104ec61a657ad045004ae1eb4242746934df07d
graphemecluster <graphemecluster@gmail.com>
2023-10-31 16:30:18 +0800

    Revert "Add More Test Cases from test262"
    This reverts commit fefcf29bdc9ce403c357c107e286cad38b357ccc.

--- tests/cases/conformance/test262/language/literals/regexp/regexp-source-char-no-paragraph-separator.ts
+++
@@ -1,32 +0,0 @@
-// Copyright 2009 the Sputnik authors.  All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-
-/*---
-esid: prod-RegularExpressionNonTerminator
-info: |
-  RegularExpressionBody ::
-    RegularExpressionFirstChar RegularExpressionChars
-
-  RegularExpressionChars ::
-    [empty]
-    RegularExpressionChars RegularExpressionChar
-
-  RegularExpressionFirstChar ::
-    RegularExpressionNonTerminator but not one of * or \ or / or [
-
-  RegularExpressionNonTerminator ::
-    SourceCharacter but not LineTerminator
-
-description: >
-  A regular expression may not contain a <PS> as a SourceCharacter
-negative:
-  phase: parse
-  type: SyntaxError
----*/
-
-
-/a\\  /
-
-/*
-There is a <PS> between "a\\ " and "/"
-*/
