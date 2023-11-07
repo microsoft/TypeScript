32048c9551e9640907091c4ecb6d4c1df999f817
Sheetal Nandi <shkamat@microsoft.com>
2023-11-07 15:24:28 -0800

    Add test when type acquisition changes

--- src/testRunner/unittests/tsserver/typeAquisition.ts
+++ src/testRunner/unittests/tsserver/typeAquisition.ts
@@ -7,6 +7,7 @@ import {
     TestSession,
     toExternalFile,
 } from "../helpers/tsserver.js";
+import { FileWithPackageName } from "../helpers/typingsInstaller.js";
 import { TestServerHost } from "../helpers/virtualFileSystemWithWatch.js";
 
 describe("unittests:: tsserver:: typeAquisition:: autoDiscovery", () => {
@@ -57,4 +58,118 @@ describe("unittests:: tsserver:: typeAquisition:: prefer typings to js", () => {
 
         baselineTsserverLogs("typeAquisition", "prefer typings in second pass", session);
     });
+});
+
+describe("unittests:: tsserver:: typeAquisition:: changes", () => {
+    it("changes to typeAquisition with already aquired typing", () => {
+        const { host, session, disableTypeAcquisition, verifyEnabledTypeAcquisition } = setup(
+            /*hostHasBarTyping*/ true,
+            /*enablebleTypeAquisition*/ true,
+        );
+        disableTypeAcquisition();
+        host.runQueuedTimeoutCallbacks();
+        verifyEnabledTypeAcquisition();
+        baselineTsserverLogs("typeAquisition", "changes to typeAquisition with already aquired typing", session);
+    });
+
+    it("changes to typeAquisition when typing installer installs typing", () => {
+        const { host, session, disableTypeAcquisition, verifyEnabledTypeAcquisition } = setup(
+            /*hostHasBarTyping*/ false,
+            /*enablebleTypeAquisition*/ true,
+        );
+        host.runPendingInstalls();
+        host.runQueuedTimeoutCallbacks(); // First project upate after typings are installed
+        host.runQueuedTimeoutCallbacks(); // Update scheduled after the typings from unresolved imports are discovered again
+        host.runQueuedTimeoutCallbacks();
+        disableTypeAcquisition();
+        host.runQueuedTimeoutCallbacks();
+        verifyEnabledTypeAcquisition();
+        baselineTsserverLogs("typeAquisition", "changes to typeAquisition when typing installer installs typing", session);
+    });
+
+    it("midway changes to typeAquisition when typing installer installs typing", () => {
+        const { host, session, disableTypeAcquisition, verifyEnabledTypeAcquisition } = setup(
+            /*hostHasBarTyping*/ false,
+            /*enablebleTypeAquisition*/ true,
+        );
+        host.runPendingInstalls();
+        disableTypeAcquisition();
+        host.runQueuedTimeoutCallbacks(); // First project upate after typings are installed
+        verifyEnabledTypeAcquisition();
+        baselineTsserverLogs("typeAquisition", "midway changes to typeAquisition when typing installer installs typing", session);
+    });
+
+    it("receives update of typings after project changes", () => {
+        const { host, session, disableTypeAcquisition, verifyEnabledTypeAcquisition } = setup(
+            /*hostHasBarTyping*/ false,
+            /*enablebleTypeAquisition*/ true,
+        );
+        disableTypeAcquisition();
+        host.runQueuedTimeoutCallbacks(); // Update project
+        host.runPendingInstalls();
+        host.runQueuedTimeoutCallbacks();
+        verifyEnabledTypeAcquisition();
+        baselineTsserverLogs("typeAquisition", "receives update of typings after project changes", session);
+    });
+
+    it("change after enabling typeAquisition", () => {
+        const { host, session, verifyEnabledTypeAcquisition } = setup(
+            /*hostHasBarTyping*/ true,
+            /*enablebleTypeAquisition*/ false,
+        );
+        verifyEnabledTypeAcquisition();
+        host.runPendingInstalls();
+        host.runQueuedTimeoutCallbacks();
+        host.runQueuedTimeoutCallbacks();
+        host.runQueuedTimeoutCallbacks();
+        baselineTsserverLogs("typeAquisition", "change after enabling typeAquisition", session);
+    });
+
+    function setup(
+        hostHasBarTyping: boolean,
+        enablebleTypeAquisition: boolean,
+    ) {
+        const host = TestServerHost.createServerHost({
+            "/users/user/projects/project1/app.js": `var x = require('bar');`,
+            "/users/user/projects/project1/node_modules/bar/index.js": "export const x = 1",
+        }, { typingsInstallerTypesRegistry: "bar" });
+        typeAcquisition(enablebleTypeAquisition);
+        const barTyping: FileWithPackageName = {
+            path: getPathForTypeScriptTypingInstallerCacheTest("/node_modules/@types/bar/index.d.ts"),
+            content: "export const x = 1;",
+            package: "bar",
+        };
+        if (hostHasBarTyping) host.ensureFileOrFolder(barTyping);
+        const session = new TestSession({
+            host,
+            installAction: [barTyping],
+        });
+        openFilesForSession(["/users/user/projects/project1/app.js"], session);
+        return { host, session, disableTypeAcquisition, verifyEnabledTypeAcquisition };
+
+        function typeAcquisition(enable: boolean) {
+            host.writeFile("/users/user/projects/project1/jsconfig.json", config(enable));
+        }
+
+        function config(enablebleTypeAquisition: boolean) {
+            return jsonToReadableText({
+                compilerOptions: {
+                    allowJs: true,
+                    traceResolution: true,
+                },
+                typeAcquisition: enablebleTypeAquisition ? undefined : { enable: false },
+            });
+        }
+
+        function verifyEnabledTypeAcquisition() {
+            typeAcquisition(/*enable*/ true);
+            host.runQueuedTimeoutCallbacks();
+            host.runQueuedTimeoutCallbacks();
+            host.runQueuedTimeoutCallbacks();
+        }
+
+        function disableTypeAcquisition() {
+            typeAcquisition(/*enable*/ false);
+        }
+    }
 });
