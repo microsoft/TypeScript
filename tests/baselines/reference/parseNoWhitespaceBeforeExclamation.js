ad09b6ae7e5e1ba938622bc97bd6c2f4ab67bf0f
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-13 16:11:58 -0800

    PR feedback

--- tests/baselines/reference/parseNoWhitespaceBeforeExclamation.js
+++ tests/baselines/reference/parseNoWhitespaceBeforeExclamation.js
@@ -5,10 +5,22 @@ declare var value: any;
 
 value !;
 value !instanceof String;
+value ! instanceof String
+value/* this is a comment */!instanceof String;
 value/* this is a comment */! instanceof String;
+value !in String;
+value ! in String
+value/* this is a comment */!in String;
+value/* this is a comment */! in String;
 
 
 //// [parseNoWhitespaceBeforeExclamation.js]
 value;
 value instanceof String;
+value instanceof String;
 value /* this is a comment */ instanceof String;
+value /* this is a comment */ instanceof String;
+value in String;
+value in String;
+value /* this is a comment */ in String;
+value /* this is a comment */ in String;
--- tests/cases/compiler/parseNoWhitespaceBeforeExclamation.ts
+++ tests/cases/compiler/parseNoWhitespaceBeforeExclamation.ts
@@ -2,4 +2,10 @@ declare var value: any;
 
 value !;
 value !instanceof String;
+value ! instanceof String
+value/* this is a comment */!instanceof String;
 value/* this is a comment */! instanceof String;
+value !in String;
+value ! in String
+value/* this is a comment */!in String;
+value/* this is a comment */! in String;
