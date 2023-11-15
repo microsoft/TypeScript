94dcd89ad694cc531768432b3b82b3e89dedff1c
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-15 15:17:33 -0800

    Add tests from design meeting

---
+++ tests/baselines/reference/fakeInfinity2.js
@@ -0,0 +1,62 @@
+//// [tests/cases/compiler/fakeInfinity2.ts] ////
+
+//// [fakeInfinity2.ts]
+export enum Foo {
+    A = 1e999,
+    B = -1e999,
+}
+
+namespace X {
+    type A = 1e999;
+    type B = 2e999;
+
+    export function f(): A {
+        throw new Error()
+    }
+}
+
+export const m = X.f();
+
+
+//// [fakeInfinity2.js]
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.m = exports.Foo = void 0;
+var Foo;
+(function (Foo) {
+    Foo[Foo["A"] = Infinity] = "A";
+    Foo[Foo["B"] = -Infinity] = "B";
+})(Foo || (exports.Foo = Foo = {}));
+var X;
+(function (X) {
+    function f() {
+        throw new Error();
+    }
+    X.f = f;
+})(X || (X = {}));
+exports.m = X.f();
+
+
+//// [fakeInfinity2.d.ts]
+export declare enum Foo {
+    A = Infinity,
+    B = -Infinity
+}
+export declare const m: Infinity;
+
+
+//// [DtsFileErrors]
+
+
+fakeInfinity2.d.ts(5,25): error TS2749: 'Infinity' refers to a value, but is being used as a type here. Did you mean 'typeof Infinity'?
+
+
+==== fakeInfinity2.d.ts (1 errors) ====
+    export declare enum Foo {
+        A = Infinity,
+        B = -Infinity
+    }
+    export declare const m: Infinity;
+                            ~~~~~~~~
+!!! error TS2749: 'Infinity' refers to a value, but is being used as a type here. Did you mean 'typeof Infinity'?
+    
---
+++ tests/cases/compiler/fakeInfinity3.ts
@@ -0,0 +1,20 @@
+// @strict: true
+// @declaration: true
+
+export enum Foo {
+    A = 1e999,
+    B = -1e999,
+}
+
+namespace X {
+    type A = 1e999;
+    type B = 2e999;
+
+    export function f(): A {
+        throw new Error()
+    }
+}
+
+export const m = X.f();
+
+export const Infinity = "oops";
