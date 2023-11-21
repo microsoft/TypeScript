b94509a4773a3673397b99354449af16059c770b
Andrew Branch <andrew@wheream.io>
2023-11-20 16:51:06 -0800

    Add test demonstrating interop for esbuild and webpack

---
+++ tests/cases/conformance/module/esbuildInterop.ts
@@ -0,0 +1,25 @@
+// @module: esnext
+// @moduleResolution: bundler
+// @moduleFormatDetection: bundler
+// @moduleFormatInterop: bundlernode
+
+// @Filename: /node_modules/dep/package.json
+{
+    "name": "dep",
+    "version": "1.0.0",
+    "main": "index.js"
+}
+
+// @Filename: /node_modules/dep/index.d.ts
+declare const _default: string;
+export default _default;
+
+// @Filename: /package.json
+{
+  "type": "module"
+}
+
+// @Filename: /index.ts
+import dep from "dep";
+dep.toLowerCase(); // Error
+dep.default.toLowerCase(); // Ok
