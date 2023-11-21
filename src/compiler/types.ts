1532d6d56a6833df50056c094972f7d01f173162
Anders Hejlsberg <andersh@microsoft.com>
2023-11-21 10:47:10 -0800

    Fast path for unions of string literals only

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -6154,6 +6154,8 @@ export const enum TypeFlags {
     IncludesInstantiable = Substitution,
     /** @internal */
     NotPrimitiveUnion = Any | Unknown | Void | Never | Object | Intersection | IncludesInstantiable,
+    /** @internal */
+    NotStringLiteral = ~(StringLiteral | EnumLiteral | TemplateLiteral | StringMapping)
 }
 
 export type DestructuringPattern = BindingPattern | ObjectLiteralExpression | ArrayLiteralExpression;
@@ -6307,6 +6309,7 @@ export const enum ObjectFlags {
     /** @internal */
     IsUnknownLikeUnion = 1 << 26, // Union of null, undefined, and empty object type
     /** @internal */
+    StringLiteralUnion = 1 << 27, // Union of only string and template literals
 
     // Flags that require TypeFlags.Intersection
     /** @internal */
