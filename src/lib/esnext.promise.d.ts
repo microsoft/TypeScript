aef29e400ebd174bd1ccd3a9489194ce9e3df948
Linus Groh <mail@linusgroh.de>
2023-11-29 16:55:55 +0000

    Add types for Promise.withResolvers() (#56593)

---
+++ src/lib/esnext.promise.d.ts
@@ -0,0 +1,17 @@
+interface PromiseWithResolvers<T> {
+    promise: Promise<T>;
+    resolve: (value: T | PromiseLike<T>) => void;
+    reject: (reason?: any) => void;
+}
+
+interface PromiseConstructor {
+    /**
+     * Creates a new Promise and returns it in an object, along with its resolve and reject functions.
+     * @returns An object with the properties `promise`, `resolve`, and `reject`.
+     *
+     * ```ts
+     * const { promise, resolve, reject } = Promise.withResolvers<T>();
+     * ```
+     */
+    withResolvers<T>(): PromiseWithResolvers<T>;
+}
