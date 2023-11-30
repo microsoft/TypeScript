7bffe7ec7ca08eacb1b9c08bf81141ae70bb40d4
Titian Cernicova-Dragomir <tcernicovad1@bloomberg.net>
2023-11-30 16:22:54 -0500

    Suppress tests that fail due to TS bug

--- tests/cases/conformance/es6/computedProperties/computedPropertyNamesDeclarationEmit6_ES5.ts
+++ tests/cases/conformance/es6/computedProperties/computedPropertyNamesDeclarationEmit6_ES5.ts
@@ -1,5 +1,7 @@
 // @target: es5
 // @declaration: true
+// @isolatedDeclarations: false
+// @isolatedDeclarationDiffReason: TODO: Negative number causes issue. GH#56562
 var v = {
   [-1]: {},
   [+1]: {},
