47cb9bc4ad05c23720a1ea947d587f364bf2be6f
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2023-11-30 18:34:22 -0800

    add regression test

---
+++ tests/cases/compiler/mappedTypeInferenceFromApparentType.ts
@@ -0,0 +1,14 @@
+// @strict: true
+// @noEmit: true
+
+type Obj = {
+    [s: string]: number;
+    [u: symbol]: boolean;
+};
+
+type foo = <T>(target: { [K in keyof T]: T[K] }) => void;
+type bar = <U extends string[]>(source: { [K in keyof U]: Obj[K] }) => void;
+
+declare let f: foo;
+declare let b: bar;
+b = f;
