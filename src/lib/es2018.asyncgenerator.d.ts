54a5b82a3b55f05e38e08eb3080dc36ac4b11822
龙腾道 <LongTengDao@LongTengDao.com>
2023-12-02 20:02:24 +0800

    Update es2018.asyncgenerator.d.ts

--- src/lib/es2018.asyncgenerator.d.ts
+++ src/lib/es2018.asyncgenerator.d.ts
@@ -13,11 +13,6 @@ interface AsyncGeneratorFunction {
      * Creates a new AsyncGenerator object.
      * @param args A list of arguments the function accepts.
      */
-    new (...args: any[]): AsyncGenerator;
-    /**
-     * Creates a new AsyncGenerator object.
-     * @param args A list of arguments the function accepts.
-     */
     (...args: any[]): AsyncGenerator;
     /**
      * The length of the arguments.
