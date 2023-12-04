02453a0a1dd7a1b8da6b9a42413c1cacfe2d924b
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2023-12-04 21:11:00 +0200

    fix(56052):  Rename Symbol F2 with "pure keyof" is not possible - but it is possible if "wrapped as calculated property"  (#56061)
    Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

---
+++ tests/cases/fourslash/renameStringLiteralTypes4.ts
@@ -0,0 +1,11 @@
+/// <reference path="fourslash.ts" />
+
+////interface I {
+////    "Prop 1": string;
+////}
+////
+////declare const fn: <K extends keyof I>(p: K) => void
+////
+////fn("Prop 1"/**/)
+
+verify.baselineRename("", {});
