7a7f6aa602007aa2f97f75619fd8bf0045d31448
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-12-04 21:40:16 +0100

    tweak test case

--- tests/cases/compiler/contextuallyTypedParametersWithInitializers3.ts
+++ tests/cases/compiler/contextuallyTypedParametersWithInitializers3.ts
@@ -9,8 +9,10 @@ interface GraphActions {
 
 export declare function create<T>(config: T): void;
 
+declare function takesDirection(direction: CanvasDirection): void;
+
 create<GraphActions>({
   setDirection: (direction = "RIGHT") => {
-    direction; // CanvasDirection
+    takesDirection(direction);
   },
 });
