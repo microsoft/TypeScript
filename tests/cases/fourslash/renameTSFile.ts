ff631c2919c440c8f6740f88e95220215e99c7eb
Isabel Duan <isabelduan@microsoft.com>
2023-12-05 12:47:59 -0800

    fix tests

--- tests/cases/fourslash/renameTSFile.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='fourslash.ts'/>
-
-// @filename: a.ts
-//// import * as foo from './[|b.js|]';
-
-// @filename: b.ts
-//// export const b = 1;
-
-goTo.eachRange(range => { verify.renameInfoSucceeded() });
