eb74ff77dcc40fe72113228f3a79f8cb7ce943fd
David Sherret <dsherret@gmail.com>
2023-12-06 13:49:01 -0500

    Add PromiseCOnstructor.withResolvers

--- src/lib/es2021.promise.d.ts
+++ src/lib/es2021.promise.d.ts
@@ -27,4 +27,10 @@ interface PromiseConstructor {
      * @returns A new Promise.
      */
     any<T>(values: Iterable<T | PromiseLike<T>>): Promise<Awaited<T>>;
+
+    /**
+     * Creates a Promise that can be resolved or rejected using provided functions.
+     * @returns An object containing `promise` promise object, `resolve` and `reject` functions.
+     */
+    withResolvers<T>(): { promise: Promise<T>; resolve: (value: T | PromiseLike<T>) => void; reject: (reason?: any) => void; };
 }
