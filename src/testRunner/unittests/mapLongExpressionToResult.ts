0685b4a5b6b76539ae8603842ab1d43146625355
PUNAMRAMPUKALE <p_pukale@u.pacific.edu>
2023-12-08 21:30:10 -0800

    implementation of ternary operations all cases

---
+++ src/testRunner/unittests/mapLongExpressionToResult.ts
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+
+import { mapLongExpressionToResult } from '../../../src/services/utilities';
+
+describe('mapLongExpressionToResult', () => {
+  it('should return the mapped value if it exists', () => {
+    const mappings = {
+      'a': 10,
+      'b':20,
+      'c':30
+
+    };
+    const value = 'a';
+    const x = 5;
+
+    expect(mapLongExpressionToResult(value, x, mappings)).to.equal(10);
+  });
+
+  it('should return the default value if no mapping exists', () => {
+    const mappings = {
+        'a': 10,
+        'b':20,
+        'c':30
+  
+      };
+    const value = 'd';
+    const x = 5;
+
+    expect(mapLongExpressionToResult(value, x, mappings)).to.equal(5);
+  });
+
+  it('should handle empty mappings', () => {
+    const mappings = {};
+    const value = 'any_value';
+    const x = 100;
+
+    expect(mapLongExpressionToResult(value, x, mappings)).to.equal(100);
+  });
+});
