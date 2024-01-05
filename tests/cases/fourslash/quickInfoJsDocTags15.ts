75a86ca362f3d049697cf16752b916f230de6afe
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2024-01-06 01:03:39 +0200

    add tests

--- tests/cases/fourslash/quickInfoJsDocTags15.ts
+++ tests/cases/fourslash/quickInfoJsDocTags15.ts
@@ -18,9 +18,19 @@
 // @filename: /b.js
 ////import * as _a from "./a.js";
 /////**
-//// * @implements {_a.Foo/*2*/}
+//// * @implements {_a.Foo/*1*/}
 //// */
-////class Foo { }
+////class C1 { }
+////
+/////**
+//// * @extends {_a.Foo/*2*/}
+//// */
+////class C2 { }
+////
+/////**
+//// * @augments {_a.Foo/*3*/}
+//// */
+////class C3 { }
 
 goTo.file("/b.js")
 verify.baselineQuickInfo();
