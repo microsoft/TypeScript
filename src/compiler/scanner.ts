2a591bdac8e0b8f35a96522c45be264e4040f6d3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-08 14:57:11 -0800

    Make JSDoc parsing slow again for perf testing

--- src/compiler/scanner.ts
+++ src/compiler/scanner.ts
@@ -2290,6 +2290,11 @@ export function createScanner(languageVersion: ScriptTarget, skipTrivia: boolean
     }
 
     function shouldParseJSDoc() {
+        // eslint-disable-next-line no-constant-condition
+        if (1 === 1) {
+            return true;
+        }
+
         switch (jsDocParsingMode) {
             case JSDocParsingMode.ParseAll:
                 return true;
