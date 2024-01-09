0ad2847572de5c24408a40ed27d84ae2fa3737c6
Titian Cernicova-Dragomir <tcernicovad1@bloomberg.net>
2024-01-09 16:29:11 +0000

    Updated baseline.

---
+++ tests/baselines/reference/isolated-declarations/auto-fixed/dte/declarationEmitDuplicateParameterDestructuring.d.ts
@@ -0,0 +1,22 @@
+//// [tests/cases/compiler/declarationEmitDuplicateParameterDestructuring.ts] ////
+
+//// [declarationEmitDuplicateParameterDestructuring.ts]
+export const fn1 = ({ prop: a, prop: b }: { prop: number }): number => a + b;
+
+export const fn2 = ({ prop: a }: { prop: number }, { prop: b }: { prop: number }): number => a + b;
+
+
+/// [Declarations] ////
+
+
+
+//// [declarationEmitDuplicateParameterDestructuring.d.ts]
+export declare const fn1: ({ prop, prop }: {
+    prop: number;
+}) => number;
+export declare const fn2: ({ prop }: {
+    prop: number;
+}, { prop }: {
+    prop: number;
+}) => number;
+//# sourceMappingURL=declarationEmitDuplicateParameterDestructuring.d.ts.map
---
+++ tests/baselines/reference/isolated-declarations/auto-fixed/tsc/declarationEmitBindingPatterns.d.ts
@@ -0,0 +1,22 @@
+//// [tests/cases/compiler/declarationEmitBindingPatterns.ts] ////
+
+//// [declarationEmitBindingPatterns.ts]
+const k = ({x: z = 'y'}: {
+        x?: string;
+    }): void => { }
+
+var a: any;
+function f({}: any = a, []: any = a, { p: {} = a}: any = a): void {
+}
+
+/// [Declarations] ////
+
+
+
+//// [declarationEmitBindingPatterns.d.ts]
+declare const k: ({ x: z }: {
+    x?: string;
+}) => void;
+declare var a: any;
+declare function f({}?: any, []?: any, { p: {} }?: any): void;
+//# sourceMappingURL=declarationEmitBindingPatterns.d.ts.map
