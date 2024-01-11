bdabd745f8ac72e2eb92c89a4b868f86ee6127e9
Titian Cernicova-Dragomir <tcernicovad1@bloomberg.net>
2024-01-11 06:17:20 -0500

    Do not remove binding alias in function declarations

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -5592,7 +5592,6 @@ export type LateVisibilityPaintedStatement =
 /** @internal */
 export interface SymbolVisibilityResult {
     accessibility: SymbolAccessibility;
-    bindingElementToMakeVisible?: BindingElement;
     aliasesToMakeVisible?: LateVisibilityPaintedStatement[]; // aliases that need to have this symbol visible
     errorSymbolName?: string; // Optional symbol name that results in error
     errorNode?: Node; // optional node that results in error
