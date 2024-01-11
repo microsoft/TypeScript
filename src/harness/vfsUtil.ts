3a62fcb7c52fd481b9b16cf2a0bb55335b2f75e3
Titian Cernicova-Dragomir <tcernicovad1@bloomberg.net>
2024-01-11 13:29:24 +0000

    Fix code review suggestion.

--- src/harness/vfsUtil.ts
+++ src/harness/vfsUtil.ts
@@ -650,7 +650,7 @@ export class FileSystem {
      * NOTE: do not rename this method as it is intended to align with the same named export of the "fs" module.
      */
     public readFileSync(path: string, encoding?: BufferEncoding | null): string | Buffer;
-    public readFileSync(path: string, encoding: BufferEncoding | null | undefined = undefined) { // eslint-disable-line no-null/no-null
+    public readFileSync(path: string, encoding: BufferEncoding | null = null) { // eslint-disable-line no-null/no-null
         const { node } = this._walk(this._resolve(path));
         if (!node) throw createIOError("ENOENT");
         if (isDirectory(node)) throw createIOError("EISDIR");
