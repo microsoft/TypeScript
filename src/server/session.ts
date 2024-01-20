695ed0fab4314fc7330fd460e637cd6cb5e86eb7
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-19 21:07:10 -0800

    More triage

--- src/server/session.ts
+++ src/server/session.ts
@@ -203,7 +203,7 @@ export const nullCancellationToken: ServerCancellationToken = {
 function hrTimeToMilliseconds(time: [number, number]): number {
     const seconds = time[0];
     const nanoseconds = time[1];
-    return ((1e9 * seconds) + nanoseconds) / 1000000.0;
+    return ((1e9 * seconds) + nanoseconds) / 1000000;
 }
 
 function isDeclarationFileInJSOnlyNonConfiguredProject(project: Project, file: NormalizedPath) {
