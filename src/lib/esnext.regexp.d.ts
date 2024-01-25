cc8f3995264dd2f68402f6e74f166e28d5f1eb98
Joshua Chen <sidachen2003@gmail.com>
2024-01-24 20:51:50 -0500

    dupe words

--- src/lib/esnext.regexp.d.ts
+++ src/lib/esnext.regexp.d.ts
@@ -1,6 +1,6 @@
 interface RegExp {
     /**
-     * Returns a Boolean value indicating the state of the unicodeSets flag (v) used with with a regular expression.
+     * Returns a Boolean value indicating the state of the unicodeSets flag (v) used with a regular expression.
      * Default is false. Read-only.
      */
     readonly unicodeSets: boolean;
