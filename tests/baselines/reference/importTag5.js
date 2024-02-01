08806b6f8881179607d982cd733eb0e82ab56359
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2024-02-01 02:09:11 +0200

    add tests

---
+++ tests/baselines/reference/importTag5.js
@@ -0,0 +1,32 @@
+//// [tests/cases/conformance/jsdoc/importTag5.ts] ////
+
+//// [types.ts]
+export interface Foo {
+    a: number;
+}
+
+//// [foo.js]
+/**
+ * @import { Foo } from "./types"
+ */
+
+/**
+ * @param { Foo } foo
+ */
+function f(foo) {}
+
+
+
+
+//// [types.d.ts]
+export interface Foo {
+    a: number;
+}
+//// [foo.d.ts]
+/**
+ * @import { Foo } from "./types"
+ */
+/**
+ * @param { Foo } foo
+ */
+declare function f(foo: import("./types").Foo): void;
