82378a9b6bf09b605f0314c631135686c16036eb
Kevin Gibbons <bakkot@gmail.com>
2024-01-31 22:11:56 -0800

    update types

--- src/lib/esnext.collection.d.ts
+++ src/lib/esnext.collection.d.ts
@@ -10,7 +10,7 @@ interface MapConstructor {
     ): Map<K, T[]>;
 }
 
-interface SetLike<T> {
+interface ReadonlySetLike<T> {
     /**
      * Despite its name, returns an iterable of the values in the set-like.
      */
@@ -29,29 +29,29 @@ interface Set<T> {
     /**
      * @returns a new Set containing all the elements in this Set and also all the elements in the argument.
      */
-    union(other: SetLike<T>): Set<T>;
+    union<U>(other: ReadonlySetLike<U>): Set<T | U>;
     /**
      * @returns a new Set containing all the elements which are both in this Set and in the argument.
      */
-    intersection(other: SetLike<T>): Set<T>;
+    intersection<U>(other: ReadonlySetLike<U>): Set<T & U>;
     /**
      * @returns a new Set containing all the elements in this Set which are not also in the argument.
      */
-    difference(other: SetLike<T>): Set<T>;
+    difference<U>(other: ReadonlySetLike<U>): Set<T>;
     /**
      * @returns a new Set containing all the elements in this Set which are in this or in the argument, but not in both.
      */
-    symmetricDifference(other: SetLike<T>): Set<T>;
+    symmetricDifference<U>(other: ReadonlySetLike<U>): Set<T | U>;
     /**
      * @returns a boolean indicating whether all the elements in this Set are also in the argument.
      */
-    isSubsetOf(other: SetLike<T>): Set<T>;
+    isSubsetOf(other: ReadonlySetLike<unknown>): boolean;
     /**
      * @returns a boolean indicating whether all the elements in the argument are also in this Set.
      */
-    isSupersetOf(other: SetLike<T>): Set<T>;
+    isSupersetOf(other: ReadonlySetLike<unknown>): boolean;
     /**
      * @returns a boolean indicating whether this Set has no elements in common with the argument.
      */
-    isDisjointFrom(other: SetLike<T>): Set<T>;
+    isDisjointFrom(other: ReadonlySetLike<unknown>): boolean;
 }
