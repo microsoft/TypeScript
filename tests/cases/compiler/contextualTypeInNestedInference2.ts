163b34fb27fa6bf33bb0d4fd99a71c67e3137d59
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-02-12 12:41:16 +0100

    add an extra test case

---
+++ tests/cases/compiler/contextualTypeInNestedInference2.ts
@@ -0,0 +1,14 @@
+// @strictNullChecks: true, false
+
+// https://github.com/microsoft/TypeScript/issues/50787
+
+type Model = { s: string; b: boolean }
+declare let pick: <Keys extends keyof Model>(properties: readonly Keys[]) => Pick<Model, Keys>
+declare let transform1: <T>(obj: T) => T
+declare let transform2: <T extends {}>(obj: T) => T
+
+const result1 = transform1(pick(["s"]))
+const result2 = transform2(pick(["s"]))
+
+const intermediate = pick(["s"])
+const result3 = transform1(intermediate)
