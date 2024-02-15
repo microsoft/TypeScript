d258aa37803edd9782ed591616f0483410bbf259
Wesley Wigham <wewigham@microsoft.com>
2024-02-15 13:43:20 -0800

    Add extra tests

---
+++ tests/cases/compiler/overloadResolutionNoInferenceLeaksBetweenFailedOverloads.ts
@@ -0,0 +1,48 @@
+// @strict: true
+// @target: es2020
+// @moduleDetection: force
+// @filename: 49820.ts
+type A1<T> = { type: "a1", v: T };
+type B1<T> = { type: "b1", v: T };
+type A2 = { a2: string };
+type B2 = { b2: string };
+
+function fn<T>(p1: (pp1: 0) => A1<T>, p2: (pp2: A2) => 0): void;
+function fn<T>(p1: (pp1: 0) => B1<T>, p2: (pp2: B2) => 0): void;
+function fn<T>(
+  p1:
+    | ((pp1: 0) => A1<T>)
+    | ((pp1: 0) => B1<T>),
+  p2:
+    | ((pp2: A2) => 0)
+    | ((pp2: B2) => 0)
+) {}
+
+const valA1: A1<string> = ({ type: "a1", v: "" });
+const valB1: B1<string> = ({ type: "b1", v: "" });
+
+// expect A
+fn((ap1) => valA1, (ap2) => 0);
+fn((ap1) => valA1, (ap2: A2) => 0); 
+fn((ap1) => valA1, (ap2: any) => 0);
+fn((ap1) => valA1, (ap2: unknown) => 0);
+fn((ap1: 0) => valA1, (ap2) => 0);
+// expect B
+fn((bp1) => valB1, (bp2) => 0); // but it will be A, only this will result in an error
+fn((bp1) => valB1, (bp2: B2) => 0); 
+fn((bp1) => valB1, (bp2: any) => 0);
+fn((bp1) => valB1, (bp2: unknown) => 0);
+fn((bp1: 0) => valB1, (bp2) => 0);
+// @filename: 13430.ts
+declare function it(f: () => void): number;
+declare function it(f: (x: string) => void): string;
+
+let r = it((x) => {x});
+// @filename: 21525.ts
+interface TestFunction {
+    <T>(input: { [key: number]: T }, callback: (value: T, key: number, collection: { [key: number]: T }) => boolean): boolean;
+    <T extends object>(input: T, callback: (value: T[keyof T], key: string, collection: T) => boolean): boolean;
+}
+
+const fn: TestFunction = {} as any;
+fn({ a: "a", b: "b" }, (value, key) => true);
