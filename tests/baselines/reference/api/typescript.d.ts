577543a7a31697a8445e2250af71776efffed976
Sheetal Nandi <shkamat@microsoft.com>
2024-02-21 13:39:35 -0800

    Make "prepend" of projectReferences as internal only to check for deprecation error

--- tests/baselines/reference/api/typescript.d.ts
+++ tests/baselines/reference/api/typescript.d.ts
@@ -7432,8 +7432,6 @@ declare namespace ts {
         path: string;
         /** The path as the user originally wrote it */
         originalPath?: string;
-        /** True if the output of this reference should be prepended to the output of this project. Only valid for --outFile compilations */
-        prepend?: boolean;
         /** True if it is intended that this reference form a circularity */
         circular?: boolean;
     }
