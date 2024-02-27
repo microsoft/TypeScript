b0a9ecb0caee1781bdf26efc694c4ba1f29ed5b5
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-02-28 00:00:44 +0100

    Add more tests

--- tests/cases/compiler/argumentsReferenceAndParameterWithInitializer1.ts
+++ tests/cases/compiler/argumentsReferenceAndParameterWithInitializer1.ts
@@ -48,4 +48,84 @@ const test5 = function inner(value, options = undefined) {
   const args = [].slice.call(arguments);
 }
 
+/** @type {(a: number, b: boolean | undefined, ...rest: [string, ...number[]]) => void} */
+const test6 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b: boolean | undefined, ...rest: number[]) => void} */
+const test7 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: string[]) => void} */
+const test8 = function inner(value, options = undefined) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: string[]) => void} */
+const test9 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string, ...number[]]) => void} */
+const test10 = function inner(value, options = undefined) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string?, ...number[]]) => void} */
+const test11 = function inner(value, options = undefined) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string, ...number[]]) => void} */
+const test12 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string?, ...number[]]) => void} */
+const test13 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string?, string?]) => void} */
+const test14 = function inner(value, options = undefined) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string?, string?]) => void} */
+const test15 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string, string]) => void} */
+const test16 = function inner(value, options = undefined) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string, string]) => void} */
+const test17 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+const test18 = function inner(value, options = undefined) {
+  const args = [].slice.call(arguments);
+}
+
+const test19 = function inner(value, options = undefined, third) {
+  const args = [].slice.call(arguments);
+}
+
+/**
+ * @param {number} [b]
+ */
+function test20(a, b = 10, c) {
+  const args = [].slice.call(arguments);
+}
+
+/** @type {(a: number, b?: boolean, ...rest: [string, string]) => void} */
+function test21(a, b = true, c) {
+  const args = [].slice.call(arguments);
+}
+
 export {}
