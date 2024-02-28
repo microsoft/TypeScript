358ab6e9a58597441108c331cbfae5a7309bd677
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-02-28 15:17:31 -0800

    Add test

---
+++ tests/baselines/reference/typeNamedUndefined.js
@@ -0,0 +1,30 @@
+//// [tests/cases/compiler/typeNamedUndefined.ts] ////
+
+//// [typeNamedUndefined.ts]
+export namespace ns {
+    const s = Symbol();
+    export type undefined = typeof s;
+    export function x(p: undefined): undefined { // global undefined
+        return p;
+    }
+}
+
+export function x(p: ns.undefined) { // undefined from the namespace
+    return p;
+}
+
+export type undefined = "";
+
+
+//// [typeNamedUndefined.js]
+export var ns;
+(function (ns) {
+    const s = Symbol();
+    function x(p) {
+        return p;
+    }
+    ns.x = x;
+})(ns || (ns = {}));
+export function x(p) {
+    return p;
+}
