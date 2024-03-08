ab218720652ecadd58fe79ba9b5214b9face7ebb
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-03-08 14:26:32 -0800

    [experiment] make tsc use the public API

--- src/typescript/_namespaces/ts.ts
+++ src/typescript/_namespaces/ts.ts
@@ -4,5 +4,6 @@ export * from "../../compiler/_namespaces/ts";
 export * from "../../jsTyping/_namespaces/ts";
 export * from "../../services/_namespaces/ts";
 export * from "../../server/_namespaces/ts";
+export * from "../../executeCommandLine/_namespaces/ts";
 import * as server from "./ts.server";
 export { server };
