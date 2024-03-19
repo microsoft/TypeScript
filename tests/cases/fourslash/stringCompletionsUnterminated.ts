147d14424f407fd893e55c18b218ea2282d083a8
Isabel Duan <isabelduan@microsoft.com>
2024-03-18 17:20:36 -0700

    more tests

--- tests/cases/fourslash/stringCompletionsUnterminated.ts
+++ tests/cases/fourslash/stringCompletionsUnterminated.ts
@@ -1,12 +1,28 @@
 ///<reference path="fourslash.ts" />
 
-//// const a = { "foo.bar": 1}
+// @Filename: file1.ts
+//// const a = { "foo.bar": 1 }
 //// a["[|foo.b/*0*/|]
 //// a["[|foo.b           /*1*/|]
 //// a[    "[|foo.b/*2*/|]
 //// a["[|foo.b/*3*/|]"
 //// a["[|foo./*4*/b|]
+//// a['[|foo./*5*/b|]
+//// a[`[|foo./*6*/b|]
 
+// @Filename: file2.ts
+//// const a = { "foo.bar": 1 }
+//// a[`/*7*/
+
+// @Filename: file3.ts
+//// const a = { "foo.bar": 1 }
+//// a["/*8*/
+
+// @Filename: file4.ts
+//// const a = { "foo.bar": 1 }
+//// a['/*9*/
+
+// tests 7-9 should return no replacementSpan
 for (let i = 0; i < test.markers().length; i++) {
     verify.completions({
         marker: test.markers()[i],
