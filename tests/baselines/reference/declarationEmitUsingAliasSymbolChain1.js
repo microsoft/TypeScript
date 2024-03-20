a9f1dff53ff64d805fc02f603706c5c5e1a769fe
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-03-20 10:18:23 +0100

    update baselines

--- tests/baselines/reference/declarationEmitUsingAliasSymbolChain1.js
+++ tests/baselines/reference/declarationEmitUsingAliasSymbolChain1.js
@@ -64,31 +64,11 @@ export class Foo extends Data.TaggedClass("Foo")<{}> {}
 
 //// [index.js]
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Foo = void 0;
-var effect_1 = require("effect");
-var Foo = /** @class */ (function (_super) {
-    __extends(Foo, _super);
-    function Foo() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return Foo;
-}(effect_1.Data.TaggedClass("Foo")));
+const effect_1 = require("effect");
+class Foo extends effect_1.Data.TaggedClass("Foo") {
+}
 exports.Foo = Foo;
 
 
