31a87a208cc8d94ae3e38968b0ee2a07be561f09
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2024-03-21 17:07:27 +0100

    chore: update baselines

--- tests/baselines/reference/arrayToLocaleStringES2020.js
+++ tests/baselines/reference/arrayToLocaleStringES2020.js
@@ -1,16 +1,26 @@
-//// [tests/cases/compiler/arrayToLocaleStringES5.ts] ////
+//// [tests/cases/compiler/arrayToLocaleStringES2020.ts] ////
 
-//// [arrayToLocaleStringES5.ts]
+//// [arrayToLocaleStringES2020.ts]
 let str: string;
 const arr = [1, 2, 3];
 str = arr.toLocaleString(); // OK
-str = arr.toLocaleString('en-US'); // should be error
-str = arr.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
+str = arr.toLocaleString('en-US'); // OK
+str = arr.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // OK
 
 const dates: readonly Date[] = [new Date(), new Date()];
 str = dates.toLocaleString(); // OK
-str = dates.toLocaleString('fr'); // should be error
-str = dates.toLocaleString('fr', { timeZone: 'UTC' }); // should be error
+str = dates.toLocaleString('fr'); // OK
+str = dates.toLocaleString('fr', { timeZone: 'UTC' }); // OK
+
+const mixed = [1, new Date(), 59782, new Date()];
+str = mixed.toLocaleString(); // OK
+str = mixed.toLocaleString('de', { style: 'currency', currency: 'EUR' }); // OK
+str = (mixed as ReadonlyArray<number | Date>).toLocaleString('de', { currency: 'EUR', style: 'currency', timeZone: 'UTC' }); // OK
+
+const bigInts = [BigInt(1), BigInt(2), BigInt(3)];
+str = bigInts.toLocaleString(); // OK
+str = bigInts.toLocaleString('en-US'); // OK
+str = bigInts.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // OK
 
 const int8Array = new Int8Array(3);
 str = int8Array.toLocaleString(); // OK
@@ -57,50 +67,66 @@ str = float64Array.toLocaleString(); // OK
 str = float64Array.toLocaleString('en-US'); // should be error
 str = float64Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
 
+const bigInt64Array = new BigInt64Array(3);
+str = bigInt64Array.toLocaleString(); // OK
+str = bigInt64Array.toLocaleString('en-US'); // should be error
+str = bigInt64Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
 
-//// [arrayToLocaleStringES5.js]
-var str;
-var arr = [1, 2, 3];
+//// [arrayToLocaleStringES2020.js]
+let str;
+const arr = [1, 2, 3];
 str = arr.toLocaleString(); // OK
-str = arr.toLocaleString('en-US'); // should be error
-str = arr.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var dates = [new Date(), new Date()];
+str = arr.toLocaleString('en-US'); // OK
+str = arr.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // OK
+const dates = [new Date(), new Date()];
 str = dates.toLocaleString(); // OK
-str = dates.toLocaleString('fr'); // should be error
-str = dates.toLocaleString('fr', { timeZone: 'UTC' }); // should be error
-var int8Array = new Int8Array(3);
+str = dates.toLocaleString('fr'); // OK
+str = dates.toLocaleString('fr', { timeZone: 'UTC' }); // OK
+const mixed = [1, new Date(), 59782, new Date()];
+str = mixed.toLocaleString(); // OK
+str = mixed.toLocaleString('de', { style: 'currency', currency: 'EUR' }); // OK
+str = mixed.toLocaleString('de', { currency: 'EUR', style: 'currency', timeZone: 'UTC' }); // OK
+const bigInts = [BigInt(1), BigInt(2), BigInt(3)];
+str = bigInts.toLocaleString(); // OK
+str = bigInts.toLocaleString('en-US'); // OK
+str = bigInts.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // OK
+const int8Array = new Int8Array(3);
 str = int8Array.toLocaleString(); // OK
 str = int8Array.toLocaleString('en-US'); // should be error
 str = int8Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var uint8Array = new Uint8Array(3);
+const uint8Array = new Uint8Array(3);
 str = uint8Array.toLocaleString(); // OK
 str = uint8Array.toLocaleString('en-US'); // should be error
 str = uint8Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var uint8ClampedArray = new Uint8ClampedArray(3);
+const uint8ClampedArray = new Uint8ClampedArray(3);
 str = uint8ClampedArray.toLocaleString(); // OK
 str = uint8ClampedArray.toLocaleString('en-US'); // should be error
 str = uint8ClampedArray.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var int16Array = new Int16Array(3);
+const int16Array = new Int16Array(3);
 str = int16Array.toLocaleString(); // OK
 str = int16Array.toLocaleString('en-US'); // should be error
 str = int16Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var uint16Array = new Uint16Array(3);
+const uint16Array = new Uint16Array(3);
 str = uint16Array.toLocaleString(); // OK
 str = uint16Array.toLocaleString('en-US'); // should be error
 str = uint16Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var int32Array = new Int32Array(3);
+const int32Array = new Int32Array(3);
 str = int32Array.toLocaleString(); // OK
 str = int32Array.toLocaleString('en-US'); // should be error
 str = int32Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var uint32Array = new Uint32Array(3);
+const uint32Array = new Uint32Array(3);
 str = uint32Array.toLocaleString(); // OK
 str = uint32Array.toLocaleString('en-US'); // should be error
 str = uint32Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var float32Array = new Float32Array(3);
+const float32Array = new Float32Array(3);
 str = float32Array.toLocaleString(); // OK
 str = float32Array.toLocaleString('en-US'); // should be error
 str = float32Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
-var float64Array = new Float64Array(3);
+const float64Array = new Float64Array(3);
 str = float64Array.toLocaleString(); // OK
 str = float64Array.toLocaleString('en-US'); // should be error
 str = float64Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
+const bigInt64Array = new BigInt64Array(3);
+str = bigInt64Array.toLocaleString(); // OK
+str = bigInt64Array.toLocaleString('en-US'); // should be error
+str = bigInt64Array.toLocaleString('en-US', { style: 'currency', currency: 'EUR' }); // should be error
