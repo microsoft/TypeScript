331a794a1f891ec8b2d88ddfba67f78d159cb348
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2024-03-30 01:08:31 +0200

    cleanup

--- tests/cases/fourslash/codeFixConvertToTypeOnlyImport4.ts
+++ tests/cases/fourslash/codeFixConvertToTypeOnlyImport4.ts
@@ -10,13 +10,11 @@
 ////import { I, foo } from "./b";
 
 goTo.file("/a.ts");
-
 verify.codeFix({
     index: 0,
     description: ts.Diagnostics.Use_import_type.message,
     newFileContent: `import type { I, foo } from "./b";`
 });
-
 verify.codeFix({
     index: 1,
     description: `Use 'type I'`,
