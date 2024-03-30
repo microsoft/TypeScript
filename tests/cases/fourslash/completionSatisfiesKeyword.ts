fc939fbb0af4e3b4759129039cf7a00b9c733f12
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2024-03-31 00:36:45 +0200

    add completion test

--- tests/cases/fourslash/completionSatisfiesKeyword.ts
+++ tests/cases/fourslash/completionSatisfiesKeyword.ts
@@ -3,9 +3,11 @@
 ////const x = { a: 1 } /*1*/
 ////function foo() {
 ////    const x = { a: 1 } /*2*/
-////}
+////} /*3*/
+////
+////class C {} /*4*/
 
 verify.completions({
-    marker: ["1", "2"],
+    marker: ["1", "2", "3", "4"],
     includes: [{ name: "satisfies", sortText: completion.SortText.GlobalsOrKeywords }]
 });
