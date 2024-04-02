b1cf4247e532e8480a15aa939bc3b8a269a7e9bb
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2024-04-02 23:07:19 +0100

    Intl.Segmenter: fixes
    - add union types
    - Segments#containing() may return undefined
    - add constructor interface

--- src/lib/es2022.intl.d.ts
+++ src/lib/es2022.intl.d.ts
@@ -21,92 +21,40 @@ declare namespace Intl {
         languageDisplay?: DisplayNamesOptionsLanguageDisplay;
     }
 
-    /**
-     * An object with some or all properties of the `Intl.Segmenter` constructor `options` parameter.
-     *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Segmenter/Segmenter#parameters)
-     */
-    interface SegmenterOptions {
-        /** The locale matching algorithm to use. For information about this option, see [Intl page](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Intl#Locale_negotiation). */
-        localeMatcher?: "best fit" | "lookup" | undefined;
-        /** The type of input to be split */
-        granularity?: "grapheme" | "word" | "sentence" | undefined;
-    }
+    type SegmenterOptionsGranularity = "grapheme" | "word" | "sentence";
 
-    interface Segmenter {
-        /**
-         * Returns `Segments` object containing the segments of the input string, using the segmenter's locale and granularity.
-         *
-         * @param input - The text to be segmented as a `string`.
-         *
-         * @returns A new iterable Segments object containing the segments of the input string, using the segmenter's locale and granularity.
-         */
-        segment(input: string): Segments;
-        resolvedOptions(): ResolvedSegmenterOptions;
+    interface SegmenterOptions {
+        localeMatcher?: "best fit" | "lookup" | undefined;
+        granularity?: SegmenterOptionsGranularity | undefined;
     }
 
     interface ResolvedSegmenterOptions {
         locale: string;
-        granularity: "grapheme" | "word" | "sentence";
+        granularity: SegmenterOptionsGranularity;
     }
 
     interface Segments {
-        /**
-         * Returns an object describing the segment in the original string that includes the code unit at a specified index.
-         *
-         * @param codeUnitIndex - A number specifying the index of the code unit in the original input string. If the value is omitted, it defaults to `0`.
-         */
-        containing(codeUnitIndex?: number): SegmentData;
-
-        /** Returns an iterator to iterate over the segments. */
+        containing(codeUnitIndex?: number): SegmentData | undefined;
         [Symbol.iterator](): IterableIterator<SegmentData>;
     }
 
     interface SegmentData {
-        /** A string containing the segment extracted from the original input string. */
         segment: string;
-        /** The code unit index in the original input string at which the segment begins. */
         index: number;
-        /** The complete input string that was segmented. */
         input: string;
-        /**
-         * A boolean value only if granularity is "word"; otherwise, undefined.
-         * If granularity is "word", then isWordLike is true when the segment is word-like (i.e., consists of letters/numbers/ideographs/etc.); otherwise, false.
-         */
         isWordLike?: boolean;
     }
 
-    const Segmenter: {
-        prototype: Segmenter;
+    interface Segmenter {
+        segment(input: string): Segments;
+        resolvedOptions(): ResolvedSegmenterOptions;
+    }
 
-        /**
-         * Creates a new `Intl.Segmenter` object.
-         *
-         * @param locales - A string with a [BCP 47 language tag](http://tools.ietf.org/html/rfc5646), or an array of such strings.
-         *  For the general form and interpretation of the `locales` argument,
-         *  see the [`Intl` page](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#Locale_identification_and_negotiation).
-         *
-         * @param options - An [object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Segmenter/Segmenter#parameters)
-         *  with some or all options of `SegmenterOptions`.
-         *
-         * @returns [Intl.Segmenter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Segments) object.
-         *
-         * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Segmenter).
-         */
+    interface SegmenterConstructor {
         new (locales?: LocalesArgument, options?: SegmenterOptions): Segmenter;
+        supportedLocalesOf(locales: LocalesArgument, options?: SegmenterOptions): string[];
+        readonly prototype: Segmenter;
+    }
 
-        /**
-         * Returns an array containing those of the provided locales that are supported without having to fall back to the runtime's default locale.
-         *
-         * @param locales - A string with a [BCP 47 language tag](http://tools.ietf.org/html/rfc5646), or an array of such strings.
-         *  For the general form and interpretation of the `locales` argument,
-         *  see the [`Intl` page](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#Locale_identification_and_negotiation).
-         *
-         * @param options An [object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Segmenter/supportedLocalesOf#parameters).
-         *  with some or all possible options.
-         *
-         * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Segmenter/supportedLocalesOf)
-         */
-        supportedLocalesOf(locales: LocalesArgument, options?: Pick<SegmenterOptions, "localeMatcher">): string[];
-    };
+    const Segmenter: SegmenterConstructor;
 }
