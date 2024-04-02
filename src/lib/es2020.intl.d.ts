ff38affbf2d2c26fd00a163c50618bc6512639c3
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2024-04-02 23:04:12 +0100

    Intl.getCanonicalLocales: add es2020 override

--- src/lib/es2020.intl.d.ts
+++ src/lib/es2020.intl.d.ts
@@ -1,11 +1,8 @@
 /// <reference lib="es2018.intl" />
 declare namespace Intl {
-    /**
-     * The locale or locales to use
-     *
-     * See [MDN - Intl - locales argument](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#locales_argument).
-     */
     type LocalesArgument = string | Locale | readonly (string | Locale)[] | undefined;
+
+    function getCanonicalLocales(locales?: LocalesArgument): string[];
 
     type RelativeTimeFormatOptionsStyle = "long" | "short" | "narrow";
 
