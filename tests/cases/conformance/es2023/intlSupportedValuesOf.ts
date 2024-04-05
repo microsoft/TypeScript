ffe0b9961161ff5891507f5a6eb9e02edc7169ef
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2024-04-05 01:35:02 +0100

    Remove leftover testfile

--- tests/cases/conformance/es2023/intlSupportedValuesOf.ts
+++
@@ -1,7 +0,0 @@
-// @target: es2022
-// @lib: es2022,es2023.intl
-
-const enumerationKeys = ['calendar', 'collation', 'currency', 'numberingSystem', 'timeZone', 'unit'] as const;
-for (const key of enumerationKeys) {
-  var supported = Intl.supportedValuesOf(key);
-}
