76d7f4e286463b316d8c36da5fa8b9821a228c59
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-04-08 14:18:26 +0200

    add extra test case

--- tests/cases/conformance/es6/spread/arraySpreadElementwiseError.ts
+++ tests/cases/conformance/es6/spread/arraySpreadElementwiseError.ts
@@ -5,3 +5,8 @@
 
 declare const a: Array<number | string>;
 const b: Array<number> = [...a, 1];
+
+// https://github.com/microsoft/TypeScript/issues/58110
+type Abc = "a" | "b" | "c";
+const array1: Abc[] = ["a", "b"];
+const array2: Abc[] = [...array1, "a", "b", "d"];
