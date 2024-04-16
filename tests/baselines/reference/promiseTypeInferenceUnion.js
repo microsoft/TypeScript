5f3e0eb53ad7ac7ebc7f840f0b9d49adee04735f
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-16 11:30:12 -0700

    Actually fix

--- tests/baselines/reference/promiseTypeInferenceUnion.js
+++ tests/baselines/reference/promiseTypeInferenceUnion.js
@@ -27,6 +27,6 @@ function f2(x) {
 function f3(x) {
     return Promise.resolve(x);
 }
-var g1 = Promise.resolve(f1(42));
-var g2 = Promise.resolve(f2(42));
-var g3 = Promise.resolve(f3(42));
+const g1 = Promise.resolve(f1(42));
+const g2 = Promise.resolve(f2(42));
+const g3 = Promise.resolve(f3(42));
