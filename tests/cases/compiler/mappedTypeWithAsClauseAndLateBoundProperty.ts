4a45c4c6faaf3becfe6676c5604711af67d8072a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-04-17 22:36:29 +0200

    Revert "Revert "Add numeric constraint to type parameter of mapped types with… (#57076)"
    This reverts commit 59b6f78c29680f2e7cbd40e008b4b2715bf3559f.

--- tests/cases/compiler/mappedTypeWithAsClauseAndLateBoundProperty.ts
+++ tests/cases/compiler/mappedTypeWithAsClauseAndLateBoundProperty.ts
@@ -1,4 +1,4 @@
 // @target: ES2020
 declare let tgt2: number[];
-declare let src2: { [K in keyof number[] as Exclude<K, "length">]: (number[])[K] };
+declare let src2: { [K in keyof number[] & PropertyKey as Exclude<K, "length">]: (number[])[K] };
 tgt2 = src2; // Should error
