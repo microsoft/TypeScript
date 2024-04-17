7238055918f38a9591e70a87f06aba176331624a
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-17 08:53:19 -0700

    Add test

---
+++ tests/baselines/reference/parseReplacementCharacter.js
@@ -0,0 +1,12 @@
+//// [tests/cases/compiler/parseReplacementCharacter.ts] ////
+
+//// [parseReplacementCharacter.ts]
+"oops �� oops";
+'oops �� oops';
+`oops �� oops`;
+`${"oops �� oops"}`;
+// oops �� oops
+/* oops �� oops */
+/** oops �� oops */
+
+//// [parseReplacementCharacter.js]
