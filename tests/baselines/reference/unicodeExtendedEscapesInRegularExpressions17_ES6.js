42f238bfbb2a83ea71204fd90c4bf9062874e559
形素叢集・ぐらふぃーむ <graphemecluster@gmail.com>
2024-04-20 07:51:08 +0800

    Provide Syntax Checking for Regular Expressions (#55600)
    Co-authored-by: Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
    Co-authored-by: Jake Bailey <5341706+jakebailey@users.noreply.github.com>

--- tests/baselines/reference/unicodeExtendedEscapesInRegularExpressions17_ES6.js
+++
@@ -1,8 +0,0 @@
-//// [tests/cases/conformance/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInRegularExpressions17_ES6.ts] ////
-
-//// [unicodeExtendedEscapesInRegularExpressions17_ES6.ts]
-var x = /\u{r}\u{n}\u{t}/g;
-
-
-//// [unicodeExtendedEscapesInRegularExpressions17_ES6.js]
-var x = /\u{r}\u{n}\u{t}/g;
