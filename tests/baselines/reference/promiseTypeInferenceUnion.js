21f5f394b9c10bdee00ec36c7b2aebc15d45a8eb
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-19 13:51:19 -0700

    Add test for type inference of Promise/PromiseLike union bug (#58213)

---
+++ tests/baselines/reference/promiseTypeInferenceUnion.js
@@ -0,0 +1,32 @@
+//// [tests/cases/compiler/promiseTypeInferenceUnion.ts] ////
+
+//// [promiseTypeInferenceUnion.ts]
+function f1(x: number): number | Promise<number> {
+  return Promise.resolve(x);
+}
+
+function f2(x: number): number | PromiseLike<number> {
+  return Promise.resolve(x);
+}
+
+function f3(x: number): number | Promise<number> | PromiseLike<number> {
+  return Promise.resolve(x);
+}
+
+const g1: Promise<number> = Promise.resolve(f1(42));
+const g2: Promise<number> = Promise.resolve(f2(42));
+const g3: Promise<number> = Promise.resolve(f3(42));
+
+//// [promiseTypeInferenceUnion.js]
+function f1(x) {
+    return Promise.resolve(x);
+}
+function f2(x) {
+    return Promise.resolve(x);
+}
+function f3(x) {
+    return Promise.resolve(x);
+}
+const g1 = Promise.resolve(f1(42));
+const g2 = Promise.resolve(f2(42));
+const g3 = Promise.resolve(f3(42));
