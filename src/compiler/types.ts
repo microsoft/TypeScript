8d2e2d53c20a5c39b698d0499f2df8b71e275ac7
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-25 15:22:21 -0700

    Move RegExp flag version mapping to LanguageFeatureMinimumTarget (#58311)

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -8231,6 +8231,8 @@ export const enum LanguageFeatureMinimumTarget {
     ArrowFunctions = ScriptTarget.ES2015,
     BlockScopedVariables = ScriptTarget.ES2015,
     ObjectAssign = ScriptTarget.ES2015,
+    RegularExpressionFlagsUnicode = ScriptTarget.ES2015,
+    RegularExpressionFlagsSticky = ScriptTarget.ES2015,
 
     // ES2016 Features
     Exponentiation = ScriptTarget.ES2016, // `x ** y`
@@ -8243,6 +8245,7 @@ export const enum LanguageFeatureMinimumTarget {
     AsyncGenerators = ScriptTarget.ES2018, // `async function * f() { }`
     AsyncIteration = ScriptTarget.ES2018, // `Symbol.asyncIterator`
     ObjectSpreadRest = ScriptTarget.ES2018, // `{ ...obj }`
+    RegularExpressionFlagsDotAll = ScriptTarget.ES2018,
 
     // ES2019 Features
     BindinglessCatch = ScriptTarget.ES2019, // `try { } catch { }`
@@ -8259,6 +8262,7 @@ export const enum LanguageFeatureMinimumTarget {
     TopLevelAwait = ScriptTarget.ES2022,
     ClassFields = ScriptTarget.ES2022,
     PrivateNamesAndClassStaticBlocks = ScriptTarget.ES2022, // `class C { static {} #x = y, #m() {} }`, `#x in y`
+    RegularExpressionFlagsHasIndices = ScriptTarget.ES2022,
 
     // ES2023 Features
     ShebangComments = ScriptTarget.ESNext,
@@ -8269,6 +8273,7 @@ export const enum LanguageFeatureMinimumTarget {
     //       transformers/esnext.ts, commandLineParser.ts, and the contents of each lib/esnext.*.d.ts file.
     UsingAndAwaitUsing = ScriptTarget.ESNext, // `using x = y`, `await using x = y`
     ClassAndClassElementDecorators = ScriptTarget.ESNext, // `@dec class C {}`, `class C { @dec m() {} }`
+    RegularExpressionFlagsUnicodeSets = ScriptTarget.ESNext,
 }
 
 // dprint-ignore
