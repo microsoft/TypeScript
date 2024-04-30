87da460b4e737dd8f0d9b2ffd52e1a62b3b0f5f5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-30 16:18:10 -0700

    Add test

---
+++ tests/baselines/reference/unreachableEnum(preserveconstenums=false).js
@@ -0,0 +1,84 @@
+//// [tests/cases/compiler/unreachableEnum.ts] ////
+
+//// [unreachableEnum.ts]
+function func1() {
+    aFunc();
+
+    console.log(EnumA.Value);
+    console.log(EnumB.Value);
+
+    return;
+
+    function aFunc() {
+        console.log(EnumA.Value);
+        console.log(EnumB.Value);
+    }
+
+    enum EnumA { Value }
+    const enum EnumB { Value }
+}
+
+function func2() {
+    aFunc();
+
+    console.log(EnumA.Value);
+
+    return;
+
+    function aFunc() {
+        console.log(EnumA.Value);
+    }
+
+    enum EnumA { Value }
+}
+
+function func3() {
+    aFunc();
+
+    console.log(EnumB.Value);
+
+    return;
+
+    function aFunc() {
+        console.log(EnumB.Value);
+    }
+
+    const enum EnumB { Value }
+}
+
+//// [unreachableEnum.js]
+"use strict";
+function func1() {
+    aFunc();
+    console.log(EnumA.Value);
+    console.log(0 /* EnumB.Value */);
+    return;
+    function aFunc() {
+        console.log(EnumA.Value);
+        console.log(0 /* EnumB.Value */);
+    }
+    var EnumA;
+    (function (EnumA) {
+        EnumA[EnumA["Value"] = 0] = "Value";
+    })(EnumA || (EnumA = {}));
+}
+function func2() {
+    aFunc();
+    console.log(EnumA.Value);
+    return;
+    function aFunc() {
+        console.log(EnumA.Value);
+    }
+    var EnumA;
+    (function (EnumA) {
+        EnumA[EnumA["Value"] = 0] = "Value";
+    })(EnumA || (EnumA = {}));
+}
+function func3() {
+    aFunc();
+    console.log(0 /* EnumB.Value */);
+    return;
+    function aFunc() {
+        console.log(0 /* EnumB.Value */);
+    }
+}
---
+++ tests/cases/compiler/unreachableEnum.ts
@@ -0,0 +1,47 @@
+// @strict: true
+// @preserveConstEnums: true, false
+
+function func1() {
+    aFunc();
+
+    console.log(EnumA.Value);
+    console.log(EnumB.Value);
+
+    return;
+
+    function aFunc() {
+        console.log(EnumA.Value);
+        console.log(EnumB.Value);
+    }
+
+    enum EnumA { Value }
+    const enum EnumB { Value }
+}
+
+function func2() {
+    aFunc();
+
+    console.log(EnumA.Value);
+
+    return;
+
+    function aFunc() {
+        console.log(EnumA.Value);
+    }
+
+    enum EnumA { Value }
+}
+
+function func3() {
+    aFunc();
+
+    console.log(EnumB.Value);
+
+    return;
+
+    function aFunc() {
+        console.log(EnumB.Value);
+    }
+
+    const enum EnumB { Value }
+}
