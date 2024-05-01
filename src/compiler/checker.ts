365a101c789a9703d739b97090328db839d7abe6
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-05-01 22:34:57 +0200

    reduce redundant branching

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -20769,10 +20769,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         for (let i = 0; i < paramCount; i++) {
             const sourceType = i === restIndex ? getRestOrAnyTypeAtPosition(source, i) : tryGetTypeAtPosition(source, i);
             const targetType = i === restIndex ? getRestOrAnyTypeAtPosition(target, i) : tryGetTypeAtPosition(target, i);
-            if (sourceType && targetType) {
-                if (sourceType === targetType) {
-                    continue;
-                }
+            if (sourceType && targetType && sourceType !== targetType) {
                 // In order to ensure that any generic type Foo<T> is at least co-variant with respect to T no matter
                 // how Foo uses T, we need to relate parameters bi-variantly (given that parameters are input positions,
                 // they naturally relate only contra-variantly). However, if the source and target parameters both have
