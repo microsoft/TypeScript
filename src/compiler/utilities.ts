e154d47851797a58dc32e8d1049ae1e68a210023
Wesley Wigham <wewigham@microsoft.com>
2024-05-03 10:34:35 -0700

    Type alias declarations should not return an effective annotation node (#58410)

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -6668,6 +6668,7 @@ export function getAllAccessorDeclarations(declarations: readonly Declaration[] 
  */
 export function getEffectiveTypeAnnotationNode(node: Node): TypeNode | undefined {
     if (!isInJSFile(node) && isFunctionDeclaration(node)) return undefined;
+    if (isTypeAliasDeclaration(node)) return undefined; // has a .type, is not a type annotation
     const type = (node as HasType).type;
     if (type || !isInJSFile(node)) return type;
     return isJSDocPropertyLikeTag(node) ? node.typeExpression && node.typeExpression.type : getJSDocType(node);
