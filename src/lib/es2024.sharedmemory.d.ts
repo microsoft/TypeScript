e306c6a64f7b0f353894b16ebddd2de78fa60353
Kenta Moriuchi <moriken@kimamass.com>
2024-05-20 05:08:41 +0900

    Tweak MDN URLs

--- src/lib/es2024.sharedmemory.d.ts
+++ src/lib/es2024.sharedmemory.d.ts
@@ -2,21 +2,21 @@ interface SharedArrayBuffer {
     /**
      * Returns true if this SharedArrayBuffer can be grown.
      *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer/growable)
+     * [MDN](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer/growable)
      */
     get growable(): number;
 
     /**
      * If this SharedArrayBuffer is growable, returns the maximum byte length given during construction; returns the byte length if not.
      *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer/maxByteLength)
+     * [MDN](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer/maxByteLength)
      */
     get maxByteLength(): number;
 
     /**
      * Grows the SharedArrayBuffer to the specified size (in bytes).
      *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer/grow)
+     * [MDN](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer/grow)
      */
     grow(newByteLength?: number): void;
 }
