378fdb954d9d4fe0dc20aef864e9f314db2c6f9c
Kenta Moriuchi <moriken@kimamass.com>
2024-05-19 23:26:54 +0900

    Update baselines

--- tests/baselines/reference/tsserver/fourslashServer/autoImportProvider6.js
+++ tests/baselines/reference/tsserver/fourslashServer/autoImportProvider6.js
@@ -57,9 +57,6 @@ lib.es2017.arraybuffer.d.ts-Text
 //// [/lib.es2017.d.ts]
 lib.es2017.d.ts-Text
 
-//// [/lib.es2017.dataview.d.ts]
-lib.es2017.dataview.d.ts-Text
-
 //// [/lib.es2017.date.d.ts]
 lib.es2017.date.d.ts-Text
 
@@ -173,7 +170,6 @@ Info seq  [hh:mm:ss:mss] Config: /tsconfig.json : {
   "/lib.es2016.intl.d.ts",
   "/lib.es2017.arraybuffer.d.ts",
   "/lib.es2017.d.ts",
-  "/lib.es2017.dataview.d.ts",
   "/lib.es2017.date.d.ts",
   "/lib.es2017.intl.d.ts",
   "/lib.es2017.object.d.ts",
@@ -223,7 +219,6 @@ Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2016.d.ts 500 u
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2016.intl.d.ts 500 undefined WatchType: Closed Script info
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2017.arraybuffer.d.ts 500 undefined WatchType: Closed Script info
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2017.d.ts 500 undefined WatchType: Closed Script info
-Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2017.dataview.d.ts 500 undefined WatchType: Closed Script info
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2017.date.d.ts 500 undefined WatchType: Closed Script info
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2017.intl.d.ts 500 undefined WatchType: Closed Script info
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /lib.es2017.object.d.ts 500 undefined WatchType: Closed Script info
@@ -248,7 +243,7 @@ Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /node_modules/@types/r
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /node_modules/@types/react/package.json 2000 undefined Project: /tsconfig.json WatchType: File location affecting resolution
 Info seq  [hh:mm:ss:mss] Finishing updateGraphWorker: Project: /tsconfig.json projectStateVersion: 1 projectProgramVersion: 0 structureChanged: true structureIsReused:: Not Elapsed:: *ms
 Info seq  [hh:mm:ss:mss] Project '/tsconfig.json' (Configured)
-Info seq  [hh:mm:ss:mss] 	Files (40)
+Info seq  [hh:mm:ss:mss] 	Files (39)
 	/lib.es5.d.ts Text-1 lib.d.ts-Text
 	/lib.es2015.generator.d.ts Text-1 lib.es2015.generator.d.ts-Text
 	/lib.es2015.iterable.d.ts Text-1 lib.es2015.iterable.d.ts-Text
@@ -258,7 +253,6 @@ Info seq  [hh:mm:ss:mss] 	Files (40)
 	/lib.es2015.symbol.d.ts Text-1 lib.es2015.symbol.d.ts-Text
 	/lib.es2015.symbol.wellknown.d.ts Text-1 lib.es2015.symbol.wellknown.d.ts-Text
 	/lib.es2016.intl.d.ts Text-1 lib.es2016.intl.d.ts-Text
-	/lib.es2017.dataview.d.ts Text-1 lib.es2017.dataview.d.ts-Text
 	/lib.es2017.date.d.ts Text-1 lib.es2017.date.d.ts-Text
 	/lib.es2017.object.d.ts Text-1 lib.es2017.object.d.ts-Text
 	/lib.es2017.sharedmemory.d.ts Text-1 lib.es2017.sharedmemory.d.ts-Text
@@ -325,9 +319,6 @@ Info seq  [hh:mm:ss:mss] 	Files (40)
 	  Library referenced via 'es2015.symbol.wellknown' from file 'lib.es2017.sharedmemory.d.ts'
 	lib.es2016.intl.d.ts
 	  Library referenced via 'es2016.intl' from file 'lib.es2016.d.ts'
-	  Matched by default include pattern '**/*'
-	lib.es2017.dataview.d.ts
-	  Library referenced via 'es2017.dataview' from file 'lib.es2017.d.ts'
 	  Matched by default include pattern '**/*'
 	lib.es2017.date.d.ts
 	  Library referenced via 'es2017.date' from file 'lib.es2017.d.ts'
@@ -466,7 +457,7 @@ Info seq  [hh:mm:ss:mss] 	Files (5)
 Info seq  [hh:mm:ss:mss] -----------------------------------------------
 Info seq  [hh:mm:ss:mss] FileWatcher:: Added:: WatchInfo: /package.json 250 undefined WatchType: package.json file
 Info seq  [hh:mm:ss:mss] Project '/tsconfig.json' (Configured)
-Info seq  [hh:mm:ss:mss] 	Files (40)
+Info seq  [hh:mm:ss:mss] 	Files (39)
 
 Info seq  [hh:mm:ss:mss] -----------------------------------------------
 Info seq  [hh:mm:ss:mss] Project '/dev/null/inferredProject1*' (Inferred)
@@ -515,8 +506,6 @@ watchedFiles::
 /lib.es2017.arraybuffer.d.ts: *new*
   {"pollingInterval":500}
 /lib.es2017.d.ts: *new*
-  {"pollingInterval":500}
-/lib.es2017.dataview.d.ts: *new*
   {"pollingInterval":500}
 /lib.es2017.date.d.ts: *new*
   {"pollingInterval":500}
@@ -659,10 +648,6 @@ ScriptInfos::
     version: Text-1
     containingProjects: 1
         /tsconfig.json
-/lib.es2017.dataview.d.ts *new*
-    version: Text-1
-    containingProjects: 1
-        /tsconfig.json
 /lib.es2017.date.d.ts *new*
     version: Text-1
     containingProjects: 1
@@ -761,7 +746,7 @@ Info seq  [hh:mm:ss:mss] request:
 Info seq  [hh:mm:ss:mss] FileWatcher:: Close:: WatchInfo: /index.ts 500 undefined WatchType: Closed Script info
 Info seq  [hh:mm:ss:mss] getConfigFileNameForFile:: File: /index.ts ProjectRootPath: undefined:: Result: /tsconfig.json
 Info seq  [hh:mm:ss:mss] Project '/tsconfig.json' (Configured)
-Info seq  [hh:mm:ss:mss] 	Files (40)
+Info seq  [hh:mm:ss:mss] 	Files (39)
 
 Info seq  [hh:mm:ss:mss] -----------------------------------------------
 Info seq  [hh:mm:ss:mss] Project '/dev/null/inferredProject1*' (Inferred)
@@ -810,8 +795,6 @@ watchedFiles::
 /lib.es2017.arraybuffer.d.ts:
   {"pollingInterval":500}
 /lib.es2017.d.ts:
-  {"pollingInterval":500}
-/lib.es2017.dataview.d.ts:
   {"pollingInterval":500}
 /lib.es2017.date.d.ts:
   {"pollingInterval":500}
@@ -956,10 +939,6 @@ ScriptInfos::
     containingProjects: 1
         /tsconfig.json
 /lib.es2017.d.ts
-    version: Text-1
-    containingProjects: 1
-        /tsconfig.json
-/lib.es2017.dataview.d.ts
     version: Text-1
     containingProjects: 1
         /tsconfig.json
