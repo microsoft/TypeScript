660fa9f1e1c9feda14cab23ea8e75fefb75e8e31
Ryan Baxley <ryan.baxley1@gmail.com>
2024-05-19 16:00:19 -0700

    more tests

---
+++ tests/cases/fourslash/completionsTupleUnion4.ts
@@ -0,0 +1,6 @@
+/// <reference path="fourslash.ts" />
+
+////declare const a: "a";
+////declare const x: ['a', 'b', 'f', 'g'] | ['a', 'b'] = [a, 'b', 'f', "/**/"];
+
+verify.completions({ marker: "", exact: ["g"] });
