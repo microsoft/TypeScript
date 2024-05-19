2b68ab70bd3ac9f239729ed6c3ee2d166ff45723
Kenta Moriuchi <moriken@kimamass.com>
2024-05-19 17:32:02 +0900

    Update: migrate to ES2024

--- src/lib/es2024.sharedmemory.d.ts
+++ src/lib/es2024.sharedmemory.d.ts
@@ -1,30 +1,3 @@
-interface ArrayBuffer {
-    /**
-     * If this ArrayBuffer is resizable, returns the maximum byte length given during construction; returns the byte length if not.
-     *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer/maxByteLength)
-     */
-    get maxByteLength(): number;
-
-    /**
-     * Returns true if this ArrayBuffer can be resized.
-     *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer/resizable)
-     */
-    get resizable(): boolean;
-
-    /**
-     * Resizes the ArrayBuffer to the specified size (in bytes).
-     *
-     * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer/resize)
-     */
-    resize(newByteLength: number): undefined;
-}
-
-interface ArrayBufferConstructor {
-    new (byteLength: number, options?: { maxByteLength?: number; }): ArrayBuffer;
-}
-
 interface SharedArrayBuffer {
     /**
      * Returns true if this SharedArrayBuffer can be grown.
--- src/lib/esnext.collection.d.ts
+++ src/lib/esnext.collection.d.ts
@@ -1,15 +1,3 @@
-interface MapConstructor {
-    /**
-     * Groups members of an iterable according to the return value of the passed callback.
-     * @param items An iterable.
-     * @param keySelector A callback which will be invoked for each item in items.
-     */
-    groupBy<K, T>(
-        items: Iterable<T>,
-        keySelector: (item: T, index: number) => K,
-    ): Map<K, T[]>;
-}
-
 interface ReadonlySetLike<T> {
     /**
      * Despite its name, returns an iterator of the values in the set-like.
