0a2338103d82bdb1a3b5c0b73a1b137054c8dfb7
Sheetal Nandi <shkamat@microsoft.com>
2024-05-21 09:40:40 -0700

    Check directly for forceDtsEmit before getting diagnostics and returning emitResolver

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -2431,7 +2431,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
     function getEmitResolver(sourceFile: SourceFile, cancellationToken: CancellationToken, forceDtsEmit?: boolean) {
         // Ensure we have all the type information in place for this file so that all the
         // emitter questions of this resolver will return the right information.
-        getDiagnostics(sourceFile, cancellationToken, forceDtsEmit);
+        if (!forceDtsEmit) getDiagnostics(sourceFile, cancellationToken);
         return emitResolver;
     }
 
@@ -47483,10 +47483,10 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         tracing?.pop();
     }
 
-    function checkSourceFile(node: SourceFile, forceDtsEmit: boolean | undefined) {
+    function checkSourceFile(node: SourceFile) {
         tracing?.push(tracing.Phase.Check, "checkSourceFile", { path: node.path }, /*separateBeginAndEnd*/ true);
         performance.mark("beforeCheck");
-        checkSourceFileWorker(node, forceDtsEmit);
+        checkSourceFileWorker(node);
         performance.mark("afterCheck");
         performance.measure("Check", "beforeCheck", "afterCheck");
         tracing?.pop();
@@ -47511,10 +47511,10 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
     }
 
     // Fully type check a source file and collect the relevant diagnostics.
-    function checkSourceFileWorker(node: SourceFile, forceDtsEmit: boolean | undefined) {
+    function checkSourceFileWorker(node: SourceFile) {
         const links = getNodeLinks(node);
         if (!(links.flags & NodeCheckFlags.TypeChecked)) {
-            if (forceDtsEmit || skipTypeChecking(node, compilerOptions, host)) {
+            if (skipTypeChecking(node, compilerOptions, host)) {
                 return;
             }
 
@@ -47578,13 +47578,13 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         }
     }
 
-    function getDiagnostics(sourceFile: SourceFile, ct: CancellationToken, forceDtsEmit?: boolean): Diagnostic[] {
+    function getDiagnostics(sourceFile: SourceFile, ct: CancellationToken): Diagnostic[] {
         try {
             // Record the cancellation token so it can be checked later on during checkSourceElement.
             // Do this in a finally block so we can ensure that it gets reset back to nothing after
             // this call is done.
             cancellationToken = ct;
-            return getDiagnosticsWorker(sourceFile, forceDtsEmit);
+            return getDiagnosticsWorker(sourceFile);
         }
         finally {
             cancellationToken = undefined;
@@ -47599,7 +47599,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         deferredDiagnosticsCallbacks = [];
     }
 
-    function checkSourceFileWithEagerDiagnostics(sourceFile: SourceFile, forceDtsEmit?: boolean) {
+    function checkSourceFileWithEagerDiagnostics(sourceFile: SourceFile) {
         ensurePendingDiagnosticWorkComplete();
         // then setup diagnostics for immediate invocation (as we are about to collect them, and
         // this avoids the overhead of longer-lived callbacks we don't need to allocate)
@@ -47608,11 +47608,11 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         // thus much more likely retaining the same union ordering as before we had lazy diagnostics)
         const oldAddLazyDiagnostics = addLazyDiagnostic;
         addLazyDiagnostic = cb => cb();
-        checkSourceFile(sourceFile, forceDtsEmit);
+        checkSourceFile(sourceFile);
         addLazyDiagnostic = oldAddLazyDiagnostics;
     }
 
-    function getDiagnosticsWorker(sourceFile: SourceFile, forceDtsEmit: boolean | undefined): Diagnostic[] {
+    function getDiagnosticsWorker(sourceFile: SourceFile): Diagnostic[] {
         if (sourceFile) {
             ensurePendingDiagnosticWorkComplete();
             // Some global diagnostics are deferred until they are needed and
@@ -47621,7 +47621,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
             const previousGlobalDiagnostics = diagnostics.getGlobalDiagnostics();
             const previousGlobalDiagnosticsSize = previousGlobalDiagnostics.length;
 
-            checkSourceFileWithEagerDiagnostics(sourceFile, forceDtsEmit);
+            checkSourceFileWithEagerDiagnostics(sourceFile);
 
             const semanticDiagnostics = diagnostics.getDiagnostics(sourceFile.fileName);
             const currentGlobalDiagnostics = diagnostics.getGlobalDiagnostics();
@@ -47642,7 +47642,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
 
         // Global diagnostics are always added when a file is not provided to
         // getDiagnostics
-        forEach(host.getSourceFiles(), sourceFile => checkSourceFileWithEagerDiagnostics(sourceFile, forceDtsEmit));
+        forEach(host.getSourceFiles(), checkSourceFileWithEagerDiagnostics);
         return diagnostics.getDiagnostics();
     }
 
