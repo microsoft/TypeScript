f1dfcf9ca57042a4be57bc1d9e17c47ce2ad57eb
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-05-22 14:22:52 -0700

    Use dprint package for dtsBundler formatting rather than dprint CLI

--- scripts/dtsBundler.mjs
+++ scripts/dtsBundler.mjs
@@ -5,8 +5,9 @@
  * bundle as namespaces again, even though the project is modules.
  */
 
+import { createFromBuffer } from "@dprint/formatter";
+import { getPath } from "@dprint/typescript";
 import assert, { fail } from "assert";
-import cp from "child_process";
 import fs from "fs";
 import minimist from "minimist";
 import path from "path";
@@ -475,23 +476,22 @@ if (publicContents.includes("@internal")) {
     console.error("Output includes untrimmed @internal nodes!");
 }
 
-const dprintPath = path.resolve(__dirname, "..", "node_modules", "dprint", "bin.js");
+const buffer = fs.readFileSync(getPath());
+const formatter = createFromBuffer(buffer);
+formatter.setConfig({
+    newLineKind: "lf",
+    indentWidth: 4,
+    lineWidth: 1000,
+}, {
+    quoteStyle: "preferDouble",
+});
 
 /**
  * @param {string} contents
  * @returns {string}
  */
 function dprint(contents) {
-    const result = cp.execFileSync(
-        process.execPath,
-        [dprintPath, "fmt", "--stdin", "ts"],
-        {
-            stdio: ["pipe", "pipe", "inherit"],
-            encoding: "utf-8",
-            input: contents,
-            maxBuffer: 100 * 1024 * 1024, // 100 MB "ought to be enough for anyone"; https://github.com/nodejs/node/issues/9829
-        },
-    );
+    const result = formatter.formatText("dummy.d.ts", contents);
     return result.replace(/\r\n/g, "\n");
 }
 
