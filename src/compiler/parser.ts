e0dfa9c072c7348dcaec371ed879c91a999e0b19
jean <jean.antoine.michelet@gmail.com>
2024-05-30 16:17:56 +0200

    restore blank line

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -2320,6 +2320,7 @@ namespace Parser {
         if (token() === SyntaxKind.AwaitKeyword && inAwaitContext()) {
             return false;
         }
+        
         return token() > SyntaxKind.LastReservedWord;
     }
 
