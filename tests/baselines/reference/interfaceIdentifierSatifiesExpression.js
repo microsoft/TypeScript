c36733f40552bc8c093f3ed6d38026fdc3d41b28
jean <jean.antoine.michelet@gmail.com>
2024-06-01 07:33:39 +0200

    test: ensure there is no regression for interface as/satisfies cases

---
+++ tests/baselines/reference/interfaceIdentifierSatifiesExpression.js
@@ -0,0 +1,15 @@
+//// [tests/cases/compiler/interfaceIdentifierSatifiesExpression.ts] ////
+
+//// [interfaceIdentifierSatifiesExpression.ts]
+// These were written with the expectation of parse errors.
+const interface = 'x';
+
+interface satisfies string;
+interface as string;
+
+
+//// [interfaceIdentifierSatifiesExpression.js]
+// These were written with the expectation of parse errors.
+var interface = 'x';
+string;
+string;
---
+++ tests/cases/compiler/interfaceIdentifierSatifiesExpression.ts
@@ -0,0 +1,5 @@
+// These were written with the expectation of parse errors.
+const interface = 'x';
+
+interface satisfies string;
+interface as string;
