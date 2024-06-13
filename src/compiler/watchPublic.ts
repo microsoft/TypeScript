e30b5fbafe78a8196904597a3f4c60cfa97c80c7
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-06-13 13:04:51 -0700

    Remove typescript-etw (#58832)

--- src/compiler/watchPublic.ts
+++ src/compiler/watchPublic.ts
@@ -62,7 +62,6 @@ import {
     parseConfigHostFromCompilerHostLike,
     ParsedCommandLine,
     Path,
-    perfLogger,
     PollingInterval,
     ProgramUpdateLevel,
     ProjectReference,
@@ -891,19 +890,15 @@ export function createWatchProgram<T extends BuilderProgram>(host: WatchCompiler
     function updateProgram() {
         switch (updateLevel) {
             case ProgramUpdateLevel.RootNamesAndUpdate:
-                perfLogger?.logStartUpdateProgram("PartialConfigReload");
                 reloadFileNamesFromConfigFile();
                 break;
             case ProgramUpdateLevel.Full:
-                perfLogger?.logStartUpdateProgram("FullConfigReload");
                 reloadConfigFile();
                 break;
             default:
-                perfLogger?.logStartUpdateProgram("SynchronizeProgram");
                 synchronizeProgram();
                 break;
         }
-        perfLogger?.logStopUpdateProgram("Done");
         return getCurrentBuilderProgram();
     }
 
