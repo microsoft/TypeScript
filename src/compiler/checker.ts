b3165107cb3dc091ed0ca66980702c86f5be1d8c
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2024-06-17 02:01:20 +0300

    feat(58561): allow leading underscore for types to bypass noUnusedLocals warning

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -44390,6 +44390,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
             }
         }
     }
+
     function isTypeParameterUnused(typeParameter: TypeParameterDeclaration): boolean {
         return !(getMergedSymbol(typeParameter.symbol).isReferenced! & SymbolFlags.TypeParameter) && !isIdentifierThatStartsWithUnderscore(typeParameter.name);
     }
@@ -44410,6 +44411,13 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
     }
 
     function isValidUnusedLocalDeclaration(declaration: Declaration): boolean {
+        if (isTypeAliasDeclaration(declaration)) {
+            /**
+             * ignore starts with underscore names _
+             * type _T = number;
+             */
+            return isIdentifierThatStartsWithUnderscore(declaration.name);
+        }
         if (isBindingElement(declaration)) {
             if (isObjectBindingPattern(declaration.parent)) {
                 /**
