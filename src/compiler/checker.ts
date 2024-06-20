b7eda587942e91d265710d2edd0040ff4310db14
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-06-19 21:32:27 -0700

    Enable this by default for testing

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -47127,7 +47127,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                     }
                 }
             }
-            else if (resolveBareImports && !importClause) {
+            else if (resolveBareImports !== false && !importClause) { // TODO(jakebailey): revert after testing impact
                 void resolveExternalModuleName(node, node.moduleSpecifier);
             }
         }
