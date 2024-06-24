920eeb8d32dfa4acdc71cdcd853525c08e35e55f
Randolf J <rjung@jeongjh.com>
2024-06-23 21:27:22 -0700

    Relax search element in Array.prototype.includes

--- src/lib/es2016.array.include.d.ts
+++ src/lib/es2016.array.include.d.ts
@@ -4,7 +4,7 @@ interface Array<T> {
      * @param searchElement The element to search for.
      * @param fromIndex The position in this array at which to begin searching for searchElement.
      */
-    includes(searchElement: T, fromIndex?: number): boolean;
+    includes(searchElement: unknown, fromIndex?: number): searchElement is T;
 }
 
 interface ReadonlyArray<T> {
@@ -13,7 +13,7 @@ interface ReadonlyArray<T> {
      * @param searchElement The element to search for.
      * @param fromIndex The position in this array at which to begin searching for searchElement.
      */
-    includes(searchElement: T, fromIndex?: number): boolean;
+    includes(searchElement: unknown, fromIndex?: number): searchElement is T;
 }
 
 interface Int8Array {
