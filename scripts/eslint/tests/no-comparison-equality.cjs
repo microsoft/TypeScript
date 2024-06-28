5e20b7528204750b1cc08dda48eeb009a550461a
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-06-28 14:10:00 -0700

    Add lint rule to prevent misuse of Comparison enum

---
+++ scripts/eslint/tests/no-comparison-equality.cjs
@@ -0,0 +1,106 @@
+const { RuleTester } = require("./support/RuleTester.cjs");
+const rule = require("../rules/no-comparison-equality.cjs");
+
+const ruleTester = new RuleTester({
+    parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: false,
+    },
+    parser: require.resolve("@typescript-eslint/parser"),
+});
+
+ruleTester.run("no-comparison-equality", rule, {
+    valid: [
+        {
+            code: `if (value > 0) {}`,
+        },
+        {
+            code: `
+if (value === Comparison.EqualTo) {}
+if (value !== Comparison.EqualTo) {}
+if (value == Comparison.EqualTo) {}
+if (value != Comparison.EqualTo) {}
+if (Comparison.EqualTo === value) {}
+if (Comparison.EqualTo !== value) {}
+if (Comparison.EqualTo == value) {}
+if (Comparison.EqualTo != value) {}
+            `,
+        },
+    ],
+
+    invalid: [
+        {
+            code: `
+switch (value) {
+    case Comparison.EqualTo:
+        break;
+    case Comparison.GreaterThan:
+        break;
+    case Comparison.LessThan:
+        break;
+}
+            `,
+            errors: [
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+            ],
+        },
+        {
+            code: `
+if (value === Comparison.LessThan) {}
+if (value !== Comparison.LessThan) {}
+if (value == Comparison.LessThan) {}
+if (value != Comparison.LessThan) {}
+if (Comparison.LessThan === value) {}
+if (Comparison.LessThan !== value) {}
+if (Comparison.LessThan == value) {}
+if (Comparison.LessThan != value) {}
+            `,
+            errors: [
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+            ],
+        },
+        {
+            code: `
+if (value === Comparison.GreaterThan) {}
+if (value !== Comparison.GreaterThan) {}
+if (value == Comparison.GreaterThan) {}
+if (value != Comparison.GreaterThan) {}
+if (Comparison.GreaterThan === value) {}
+if (Comparison.GreaterThan !== value) {}
+if (Comparison.GreaterThan == value) {}
+if (Comparison.GreaterThan != value) {}
+            `,
+            errors: [
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+            ],
+        },
+        {
+            code: `
+if (value === Comparison.Something) {}
+if (value !== Comparison.Something) {}
+if (value == Comparison.Something) {}
+if (value != Comparison.Something) {}
+            `,
+            errors: [
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+                { messageId: "noCompare" },
+            ],
+        },
+    ],
+});
