7af3e102018c812f7f5faac3678fe854b6becc0d
Isabel Duan <isabelduan@microsoft.com>
2024-06-28 14:49:53 -0700

    more tests

--- tests/cases/compiler/unconstrainedTypeComparisons.ts
+++ tests/cases/compiler/unconstrainedTypeComparisons.ts
@@ -2,6 +2,18 @@
 // @target: es2015
 // @strict: true
 
+export function f<T extends {} | undefined | null>(a: T, b: T): boolean {
+  return a > b;
+}
+
 export function g<T>(a: T, b: T): boolean {
+  return a > b;
+}
+
+function h<T>(a: T, b: T): boolean {
+  if (a === undefined) {
+      return true;
+  }
+
   return a > b;
 }
