813e449f61c849567558e3f43a6a00ebeaba0500
Mitchell de Rijcke <mdr@rijx.com>
2024-07-04 04:02:57 +0200

    Make sure isShorthandPropertyNameUseSite is only internally exposed

--- src/compiler/utilities.ts
+++ src/compiler/utilities.ts
@@ -10195,6 +10195,7 @@ export function isValidTypeOnlyAliasUseSite(useSite: Node): boolean {
         || !(isExpressionNode(useSite) || isShorthandPropertyNameUseSite(useSite));
 }
 
+/** @internal */
 export function isShorthandPropertyNameUseSite(useSite: Node) {
     return isIdentifier(useSite) && isShorthandPropertyAssignment(useSite.parent) && useSite.parent.name === useSite;
 }
