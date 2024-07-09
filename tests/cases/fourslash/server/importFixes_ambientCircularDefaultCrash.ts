6325a2bf8397eed95ccefaca35d2353ebffc3321
Andrew Branch <andrew@wheream.io>
2024-07-09 16:35:11 -0700

    Add failing test

---
+++ tests/cases/fourslash/server/importFixes_ambientCircularDefaultCrash.ts
@@ -0,0 +1,21 @@
+/// <reference path="../fourslash.ts" />
+
+// @Filename: /tsconfig.json
+//// {
+////   "compilerOptions": {
+////     "module": "preserve"
+////   }
+//// }
+
+// @Filename: /types.d.ts
+//// declare module "mymod" {
+////   import mymod from "mymod";
+////   export default mymod;
+//// }
+
+// @Filename: /index.ts
+//// my/**/
+
+verify.importFixModuleSpecifiers("", ["mymod"]);
+edit.insert("mod");
+verify.importFixModuleSpecifiers("", ["mymod"]);
