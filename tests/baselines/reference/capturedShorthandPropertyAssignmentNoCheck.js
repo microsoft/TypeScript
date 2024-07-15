7e1e976c807b8f87f3770a07ffb431d0b6f322d0
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-07-15 13:27:18 -0700

    Use getResolvedSymbol to pass into checkIdentifierCalculateNodeCheckFlags like other call

--- tests/baselines/reference/capturedShorthandPropertyAssignmentNoCheck.js
+++ tests/baselines/reference/capturedShorthandPropertyAssignmentNoCheck.js
@@ -20,22 +20,3 @@ for (var _i = 0, _a = [1, 2, 3]; _i < _a.length; _i++) {
 }
 var result = fns.map(function (fn) { return fn(); });
 console.log(result);
-
-
-!!!! File capturedShorthandPropertyAssignmentNoCheck.js differs from original emit in noCheck emit
-//// [capturedShorthandPropertyAssignmentNoCheck.js]
-===================================================================
---- Expected	The full check baseline
-+++ Actual	with noCheck set
-@@ -1,10 +1,7 @@
- var fns = [];
--var _loop_1 = function (value) {
--    fns.push(function () { return ({ value: value }); });
--};
- for (var _i = 0, _a = [1, 2, 3]; _i < _a.length; _i++) {
-     var value = _a[_i];
--    _loop_1(value);
-+    fns.push(function () { return ({ value: value }); });
- }
- var result = fns.map(function (fn) { return fn(); });
- console.log(result);
