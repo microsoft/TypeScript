956752305635420ee45b27ba63292b6207657150
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-07-16 16:51:57 -0700

    Test not a module

---
+++ tests/cases/compiler/sideEffectImports3.ts
@@ -0,0 +1,9 @@
+// @resolveSideEffectImports: true,false
+// @module: nodenext
+// @moduleDetection: legacy,auto,force
+
+// @filename: index.ts
+import "./not-a-module";
+
+// @filename: not-a-module.ts
+console.log("Hello, world!");
