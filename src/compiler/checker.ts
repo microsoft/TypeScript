371e64987d303e23aa46ed2187f9745cbdf4718a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-07-16 18:54:08 +0200

    Allow protected property to be accessed on union of types with the correct base type

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -33601,7 +33601,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
             // get the original type -- represented as the type constraint of the 'this' type
             containingType = (containingType as TypeParameter).isThisType ? getConstraintOfTypeParameter(containingType as TypeParameter)! : getBaseConstraintOfType(containingType as TypeParameter)!; // TODO: GH#18217 Use a different variable that's allowed to be undefined
         }
-        if (!containingType || !hasBaseType(containingType, enclosingClass)) {
+        if (!containingType || !everyType(containingType, t => hasBaseType(t, enclosingClass))) {
             if (errorNode) {
                 error(errorNode, Diagnostics.Property_0_is_protected_and_only_accessible_through_an_instance_of_class_1_This_is_an_instance_of_class_2, symbolToString(prop), typeToString(enclosingClass), typeToString(containingType));
             }
