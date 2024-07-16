51c818d263d33f97b0c2052e06d508eb4714a581
Sheetal Nandi <shkamat@microsoft.com>
2024-07-16 15:41:44 -0700

    Lint

--- src/compiler/resolutionCache.ts
+++ src/compiler/resolutionCache.ts
@@ -38,7 +38,6 @@ import {
     isDiskPathRoot,
     isEmittedFileOfProgram,
     isExternalModuleNameRelative,
-    isExternalOrCommonJsModule,
     isNodeModulesDirectory,
     isRootedDiskPath,
     isTraceEnabled,
