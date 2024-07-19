7f978c7ffea3ef17ab198674a4b0b5150e083c42
Ron Buckton <ron.buckton@microsoft.com>
2024-07-19 14:53:18 -0400

    Use tuple name inference for `Iterator.next` et al (#59360)

--- src/lib/es2015.iterable.d.ts
+++ src/lib/es2015.iterable.d.ts
@@ -22,7 +22,7 @@ type IteratorResult<T, TReturn = any> = IteratorYieldResult<T> | IteratorReturnR
 
 interface Iterator<T, TReturn = any, TNext = any> {
     // NOTE: 'next' is defined using a tuple to ensure we report the correct assignability errors in all places.
-    next(...args: [] | [TNext]): IteratorResult<T, TReturn>;
+    next(...[value]: [] | [TNext]): IteratorResult<T, TReturn>;
     return?(value?: TReturn): IteratorResult<T, TReturn>;
     throw?(e?: any): IteratorResult<T, TReturn>;
 }
