49e1c7b2f4b5ee71d08cbe269cc6a5956e76e589
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-07-20 18:49:16 +0200

    Fixed a crash when transforming modules with top-level labels

---
+++ tests/baselines/reference/labeledStatementExportDeclarationNoCrash1(module=system).js
@@ -0,0 +1,23 @@
+//// [tests/cases/conformance/statements/labeledStatements/labeledStatementExportDeclarationNoCrash1.ts] ////
+
+//// [labeledStatementExportDeclarationNoCrash1.ts]
+// https://github.com/microsoft/TypeScript/issues/59372
+
+export const box: string
+subTitle:
+export const title: string
+
+
+//// [labeledStatementExportDeclarationNoCrash1.js]
+// https://github.com/microsoft/TypeScript/issues/59372
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var box, title;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            subTitle: ;
+        }
+    };
+});
