f4d700200c0edc4261c959c4b4d33333e6984aab
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-07-25 21:04:30 +0200

    Fixed type argument completions at the end of files in incomplete nodes

---
+++ tests/cases/fourslash/completionsScopeTypeParamsTypeArgListAtEndOfFile3.ts
@@ -0,0 +1,10 @@
+/// <reference path='fourslash.ts'/>
+
+// @lib: esnext
+
+//// function invertKeysAndValues<K, V>(map: Map<K, V>): Map</**/
+
+verify.completions({
+  marker: "",
+  includes: ["K", "V"],
+});
---
+++ tests/cases/fourslash/completionsScopeTypeParamsTypeArgListAtEndOfFile6.ts
@@ -0,0 +1,11 @@
+/// <reference path='fourslash.ts'/>
+
+// @lib: esnext
+
+//// function invertKeysAndValues<K, V>(map: Map<K, /**/
+////
+
+verify.completions({
+  marker: "",
+  includes: ["K", "V"],
+});
