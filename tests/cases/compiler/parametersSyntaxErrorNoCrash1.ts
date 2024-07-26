32bef9453ad13deeb5d84a1ca4f964be4cabf6f3
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-07-26 10:48:42 +0200

    add extra test case

--- tests/cases/compiler/parametersSyntaxErrorNoCrash1.ts
+++ tests/cases/compiler/parametersSyntaxErrorNoCrash1.ts
@@ -1,6 +1,8 @@
 // @strict: true
 // @noEmit: true
 
+// https://github.com/microsoft/TypeScript/issues/59422
+
 function identity<T>(arg: T: T {
     return arg;
 }
---
+++ tests/cases/compiler/parametersSyntaxErrorNoCrash2.ts
@@ -0,0 +1,6 @@
+// @strict: true
+// @noEmit: true
+
+// https://github.com/microsoft/TypeScript/issues/59353
+
+export default function getThing( { return 'thing'; }
