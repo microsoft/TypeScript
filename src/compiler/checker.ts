3511b6413f2770dfbe79df04dfebad53df82a0e5
Andrew Branch <andrew@wheream.io>
2024-07-29 17:11:16 -0700

    Clarify comment and formatting

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -39520,11 +39520,21 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
             error(node, Diagnostics.const_enums_can_only_be_used_in_property_or_index_access_expressions_or_the_right_hand_side_of_an_import_declaration_or_export_assignment_or_type_query);
         }
 
-        // Do not check for --verbatimModuleSyntax here because imports of ambient const enums
-        // will already be checked for the same error
+        // --verbatimModuleSyntax only gets checked here when the enum usage does not
+        // resolve to an import, because imports of ambient const enums get checked
+        // separately in `checkAliasSymbol`.
         if (
-            compilerOptions.isolatedModules || compilerOptions.verbatimModuleSyntax
-                && ok && !(resolveName(node, getFirstIdentifier(node as EntityNameOrEntityNameExpression), SymbolFlags.Alias, /*nameNotFoundMessage*/ undefined, /*isUse*/ false, /*excludeGlobals*/ true))
+            compilerOptions.isolatedModules
+            || compilerOptions.verbatimModuleSyntax
+                && ok
+                && !(resolveName(
+                    node,
+                    getFirstIdentifier(node as EntityNameOrEntityNameExpression),
+                    SymbolFlags.Alias,
+                    /*nameNotFoundMessage*/ undefined,
+                    /*isUse*/ false,
+                    /*excludeGlobals*/ true,
+                ))
         ) {
             Debug.assert(!!(type.symbol.flags & SymbolFlags.ConstEnum));
             const constEnumDeclaration = type.symbol.valueDeclaration as EnumDeclaration;
