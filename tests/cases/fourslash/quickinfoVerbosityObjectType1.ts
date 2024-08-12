0e03e9b6bbfc5db654350b580bd7974e85064f3c
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2024-08-12 13:06:15 -0700

    update test

--- tests/cases/fourslash/quickinfoVerbosityObjectType1.ts
+++ tests/cases/fourslash/quickinfoVerbosityObjectType1.ts
@@ -6,7 +6,8 @@
 //// type Sym = symbol | (() => void);
 //// type BarType = Sym | boolean;
 //// type Obj = { foo: FooType, bar: BarType, str: Str };
-//// const obj/*o*/: Obj = { foo: 1, bar: true, str: "3"};
+//// const obj1/*o1*/: Obj = { foo: 1, bar: true, str: "3"};
+//// const obj2/*o2*/: { foo: FooType, bar: BarType, str: Str } = { foo: 1, bar: true, str: "3"};
 
 
-verify.baselineQuickInfo({ "o": [0, 1, 2, 3] });
+verify.baselineQuickInfo({ "o1": [0, 1, 2, 3], "o2": [0, 1, 2] });
