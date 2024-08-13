6a6c1f91e5006924c20b09ec8c0f914f6f8248c1
Isabel Duan <isabelduan@microsoft.com>
2024-08-12 21:04:38 -0700

    update ScriptTarget and LanguageFeatureMinimumTarget enums

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -7541,6 +7541,7 @@ export const enum ScriptKind {
 // NOTE: We must reevaluate the target for upcoming features when each successive TC39 edition is ratified in
 //       June of each year. This includes changes to `LanguageFeatureMinimumTarget`, `ScriptTarget`,
 //       transformers/esnext.ts, commandLineParser.ts, and the contents of each lib/esnext.*.d.ts file.
+// IF UPDATING 'ScriptTarget', ALSO UPDATE 'LanguageFeatureMinimumTarget' (if needed)
 export const enum ScriptTarget {
     /** @deprecated */
     ES3 = 0,
@@ -8306,62 +8307,80 @@ export type EmitHelperUniqueNameCallback = (name: string) => string;
  * @internal
  */
 export const enum LanguageFeatureMinimumTarget {
+    // These are copied from 'ScriptTarget' since under isolatedDeclarations, enums cannot have references to external symbols.
+    // Make sure these are updated alongside any changes to 'ScriptTarget'.
+    /**@deprecated */
+    ES3 = 0,
+    ES5 = 1,
+    ES2015 = 2,
+    ES2016 = 3,
+    ES2017 = 4,
+    ES2018 = 5,
+    ES2019 = 6,
+    ES2020 = 7,
+    ES2021 = 8,
+    ES2022 = 9,
+    ES2023 = 10,
+    ESNext = 99,
+    JSON = 100,
+    Latest = ESNext,
+
     // ES2015 Features
-    Classes = ScriptTarget.ES2015,
-    ForOf = ScriptTarget.ES2015,
-    Generators = ScriptTarget.ES2015,
-    Iteration = ScriptTarget.ES2015,
-    SpreadElements = ScriptTarget.ES2015,
-    RestElements = ScriptTarget.ES2015,
-    TaggedTemplates = ScriptTarget.ES2015,
-    DestructuringAssignment = ScriptTarget.ES2015,
-    BindingPatterns = ScriptTarget.ES2015,
-    ArrowFunctions = ScriptTarget.ES2015,
-    BlockScopedVariables = ScriptTarget.ES2015,
-    ObjectAssign = ScriptTarget.ES2015,
-    RegularExpressionFlagsUnicode = ScriptTarget.ES2015,
-    RegularExpressionFlagsSticky = ScriptTarget.ES2015,
+    Classes = ES2015,
+    ForOf = ES2015,
+    Generators = ES2015,
+    Iteration = ES2015,
+    SpreadElements = ES2015,
+    RestElements = ES2015,
+    TaggedTemplates = ES2015,
+    DestructuringAssignment = ES2015,
+    BindingPatterns = ES2015,
+    ArrowFunctions = ES2015,
+    BlockScopedVariables = ES2015,
+    ObjectAssign = ES2015,
+    RegularExpressionFlagsUnicode = ES2015,
+    RegularExpressionFlagsSticky = ES2015,
 
     // ES2016 Features
-    Exponentiation = ScriptTarget.ES2016, // `x ** y`
+    Exponentiation = ES2016, // `x ** y`
 
     // ES2017 Features
-    AsyncFunctions = ScriptTarget.ES2017, // `async function f() {}`
+    AsyncFunctions = ES2017, // `async function f() {}`
 
     // ES2018 Features
-    ForAwaitOf = ScriptTarget.ES2018, // `for await (const x of y)`
-    AsyncGenerators = ScriptTarget.ES2018, // `async function * f() { }`
-    AsyncIteration = ScriptTarget.ES2018, // `Symbol.asyncIterator`
-    ObjectSpreadRest = ScriptTarget.ES2018, // `{ ...obj }`
-    RegularExpressionFlagsDotAll = ScriptTarget.ES2018,
+    ForAwaitOf = ES2018, // `for await (const x of y)`
+    AsyncGenerators = ES2018, // `async function * f() { }`
+    AsyncIteration = ES2018, // `Symbol.asyncIterator`
+    ObjectSpreadRest = ES2018, // `{ ...obj }`
+    RegularExpressionFlagsDotAll = ES2018,
 
     // ES2019 Features
-    BindinglessCatch = ScriptTarget.ES2019, // `try { } catch { }`
+    BindinglessCatch = ES2019, // `try { } catch { }`
 
     // ES2020 Features
-    BigInt = ScriptTarget.ES2020, // `0n`
-    NullishCoalesce = ScriptTarget.ES2020, // `a ?? b`
-    OptionalChaining = ScriptTarget.ES2020, // `a?.b`
+    BigInt = ES2020, // `0n`
+    NullishCoalesce = ES2020, // `a ?? b`
+    OptionalChaining = ES2020, // `a?.b`
 
     // ES2021 Features
-    LogicalAssignment = ScriptTarget.ES2021, // `a ||= b`, `a &&= b`, `a ??= b`
+    LogicalAssignment = ES2021, // `a ||= b`, `a &&= b`, `a ??= b`
 
     // ES2022 Features
-    TopLevelAwait = ScriptTarget.ES2022,
-    ClassFields = ScriptTarget.ES2022,
-    PrivateNamesAndClassStaticBlocks = ScriptTarget.ES2022, // `class C { static {} #x = y, #m() {} }`, `#x in y`
-    RegularExpressionFlagsHasIndices = ScriptTarget.ES2022,
+    TopLevelAwait = ES2022,
+    ClassFields = ES2022,
+    PrivateNamesAndClassStaticBlocks = ES2022, // `class C { static {} #x = y, #m() {} }`, `#x in y`
+    RegularExpressionFlagsHasIndices = ES2022,
 
     // ES2023 Features
-    ShebangComments = ScriptTarget.ESNext,
+    ShebangComments = ESNext,
 
     // Upcoming Features
     // NOTE: We must reevaluate the target for upcoming features when each successive TC39 edition is ratified in
     //       June of each year. This includes changes to `LanguageFeatureMinimumTarget`, `ScriptTarget`,
     //       transformers/esnext.ts, commandLineParser.ts, and the contents of each lib/esnext.*.d.ts file.
-    UsingAndAwaitUsing = ScriptTarget.ESNext, // `using x = y`, `await using x = y`
-    ClassAndClassElementDecorators = ScriptTarget.ESNext, // `@dec class C {}`, `class C { @dec m() {} }`
-    RegularExpressionFlagsUnicodeSets = ScriptTarget.ESNext,
+    UsingAndAwaitUsing = ESNext, // `using x = y`, `await using x = y`
+    ClassAndClassElementDecorators = ESNext, // `@dec class C {}`, `class C { @dec m() {} }`
+    RegularExpressionFlagsUnicodeSets = ESNext,
 }
 
 // dprint-ignore
