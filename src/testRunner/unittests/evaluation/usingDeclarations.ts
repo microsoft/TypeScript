8b41d8bc582a0d457463091a022b863772aa1992
Ron Buckton <ron.buckton@microsoft.com>
2024-08-14 14:09:27 -0400

    built-in iterators should be disposable

--- src/testRunner/unittests/evaluation/usingDeclarations.ts
+++ src/testRunner/unittests/evaluation/usingDeclarations.ts
@@ -1811,49 +1811,40 @@ describe("unittests:: evaluation:: usingDeclarations", () => {
         ]);
     });
 
-    it("deterministic collapse of Await", async () => {
-        const { main, output } = evaluator.evaluateTypeScript(
-            `
-        export const output: any[] = [];
-
-        let asyncId = 0;
-        function increment() { asyncId++; }
-
-        export async function main() {
-            // increment asyncId at the top of each turn of the microtask queue
-            let pending = Promise.resolve();
-            for (let i = 0; i < 10; i++) {
-                pending = pending.then(increment);
-            }
-
-            {
-                using sync1 = { [Symbol.dispose]() { output.push(asyncId); } }; // asyncId: 2
-                await using async1 = null, async2 = null;
-                using sync2 = { [Symbol.dispose]() { output.push(asyncId); } }; // asyncId: 1
-                await using async3 = null, async4 = null;
-                output.push(asyncId); // asyncId: 0
-            }
-
-            output.push(asyncId); // asyncId: Ideally, 2, but ends up being 4 due to delays imposed by 'await'
-
-            await pending; // wait for the remaining 'increment' frames to complete.
+    it("'using' with downlevel generators", () => {
+        abstract class Iterator {
+            return?(): void;
+            [evaluator.FakeSymbol.iterator]() { return this; }
+            [evaluator.FakeSymbol.dispose]() { this.return?.(); }
         }
 
-        `,
-            { target: ts.ScriptTarget.ES2018 },
-        );
+        const { main } = evaluator.evaluateTypeScript(`
+            let exited = false;
 
-        await main();
+            function * f() {
+                try {
+                    yield;
+                }
+                finally {
+                    exited = true;
+                }
+            }
 
-        assert.deepEqual(output, [
-            0,
-            1,
-            2,
+            export function main() {
+                {
+                    using g = f();
+                    g.next();
+                }
 
-            // This really should be 2, but our transpile introduces an extra `await` by necessity to observe the
-            // result of __disposeResources. The process of adopting the result ends up taking two turns of the
-            // microtask queue.
-            4,
-        ]);
+                return exited;
+            }
+        `, {
+            target: ts.ScriptTarget.ES5,
+        }, {
+            Iterator
+        });
+
+        const exited = main();
+        assert.isTrue(exited, "Expected 'using' to dispose generator");
     });
 });
