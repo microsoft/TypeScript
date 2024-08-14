2cfc2a2ce8836f925762154d7f58c32543d61336
zojize <jizezou2@illinois.edu>
2024-08-14 13:30:36 +0800

    Test TupleGenerator

---
+++ tests/baselines/reference/spreadTupleGenerator.js
@@ -0,0 +1,25 @@
+//// [tests/cases/compiler/spreadTupleGenerator.ts] ////
+
+//// [spreadTupleGenerator.ts]
+class V {
+  *[Symbol.iterator](): TupleGenerator<[number, number]> {
+    yield 1; yield 2;
+  }
+}
+
+declare const v: V;
+declare function foo(x: number, y: number): void;
+
+foo(...v);
+const a: [number, number] = [...v];
+
+
+//// [spreadTupleGenerator.js]
+class V {
+    *[Symbol.iterator]() {
+        yield 1;
+        yield 2;
+    }
+}
+foo(...v);
+const a = [...v];
