35e727628242ca8cbc013eb765777abeae0a733d
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-08-22 13:32:31 -0700

    Split APILibCheck into multiple tests

---
+++ tests/cases/compiler/APILibCheck-typescript.ts
@@ -0,0 +1,16 @@
+// @module: commonjs
+// @noImplicitAny: true
+// @strictNullChecks: true
+// @lib: es2018
+// @exactOptionalPropertyTypes: true
+// @noTypesAndSymbols: true
+// @noEmit: true
+
+// @filename: node_modules/typescript/package.json
+{
+    "name": "typescript",
+    "types": "/.ts/typescript.d.ts"
+}
+
+// @filename: index.ts
+import ts = require("typescript");
