77cb42b3d596543bb89a8c338e72bcc3aed52bd1
Yi Sun <yi.sun@konghq.com>
2024-08-25 03:18:04 +0800

    Narrow destructured variables based on initializer's analysis result
    This PR introduces a method to narrow the types of destructured
    variables through control flow analysis without relying on the
    discriminant member.
    
    When the initializer of the `ObjectBindingPattern` is available,
    we attempt to narrow its type, which in turn helps further refine
    the type of the destructured variable.
    
    Partially fixes #59657

---
+++ tests/baselines/reference/narrowDestructuredVariables.js
@@ -0,0 +1,72 @@
+//// [tests/cases/compiler/narrowDestructuredVariables.ts] ////
+
+//// [narrowDestructuredVariables.ts]
+interface Ref<T> { current: T };
+type ToRefs<T> = { [K in keyof T]: Ref<T[K]> };
+declare function toRefs<T>(o: T): ToRefs<T>;
+
+interface DataPrepared {
+    prepared: true
+    payload: string
+};
+
+interface DataPending {
+    prepared: false
+    payload: null
+};
+
+type Data = DataPrepared | DataPending;
+
+declare function isDataRefsPrepared(refs: ToRefs<Data>): refs is ToRefs<DataPrepared>;
+
+declare const data: Data;
+const dataRefs = toRefs(data);
+const { prepared, payload } = dataRefs;
+
+if (prepared.current) {
+    prepared.current;
+    payload.current;
+    data.prepared;
+    data.payload;
+}
+
+if (isDataRefsPrepared(dataRefs)) {
+    prepared.current;
+    payload.current;
+    data.prepared;
+    data.payload;
+}
+
+if (data.prepared) {
+    prepared.current;
+    payload.current;
+    data.prepared;
+    data.payload;
+}
+
+
+//// [narrowDestructuredVariables.js]
+"use strict";
+;
+;
+;
+const dataRefs = toRefs(data);
+const { prepared, payload } = dataRefs;
+if (prepared.current) {
+    prepared.current;
+    payload.current;
+    data.prepared;
+    data.payload;
+}
+if (isDataRefsPrepared(dataRefs)) {
+    prepared.current;
+    payload.current;
+    data.prepared;
+    data.payload;
+}
+if (data.prepared) {
+    prepared.current;
+    payload.current;
+    data.prepared;
+    data.payload;
+}
