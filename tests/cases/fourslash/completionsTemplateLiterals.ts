8d4927f9a4a78ab6af21571583f64ef891daccc2
Michal Maršálek <MichalMarsalek@users.noreply.github.com>
2024-08-29 08:22:43 +0200

    add tests

--- tests/cases/fourslash/completionsTemplateLiterals.ts
+++ tests/cases/fourslash/completionsTemplateLiterals.ts
@@ -1,7 +1,8 @@
 /// <reference path="fourslash.ts" />
 
-////type T = `${"prefix1"|"prefix2"}${string}${"middle1"|"middle2"}${string}${"suffix1"|"suffix2"}`;
-////let x: T = /*1*/
+// @Filename: file1.ts
+////declare let x: `${"prefix1"|"prefix2"}${string}${"middle1"|"middle2"}${string}${"suffix1"|"suffix2"}`;
+//// //x = /*1*/
 ////x = "/*2a*/
 ////x = "/*2b*/"
 ////x = "pre/*3a*/
@@ -12,49 +13,12 @@
 ////x = "prefix1_middle1_/*5b*/"
 ////x = "prefix1_middle1_suffix1/*6a*/
 ////x = "prefix1_middle1_suffix1/*6b*/"
+////
+////declare let y: `"'${string}'"`;
+////y = "/*7a*/
+////y = "/*7b*/"
+////y = '/*8a*/
+////y = '/*8b*/'
 
 
-/*verify.completions({
-    marker: "1",
-    unsorted: ['"prefix1"', '"prefix2"']
-});*/
-/*verify.completions({
-    marker: "2a",
-    unsorted: ["prefix1", "prefix2"]
-});
-verify.completions({
-    marker: "2b",
-    unsorted: ["prefix1", "prefix2"]
-});
-verify.completions({
-    marker: "3a",
-    unsorted: ["prefix1", "prefix2"]
-});
-verify.completions({
-    marker: "3b",
-    unsorted: ["prefix1", "prefix2"]
-});*/
-verify.completions({
-    marker: "4a",
-    unsorted: ["prefix1_middle1", "prefix1_middle2"]
-});
-verify.completions({
-    marker: "4b",
-    unsorted: ["prefix1_middle1", "prefix1_middle2"]
-});
-verify.completions({
-    marker: "5a",
-    unsorted: ["prefix1_middle1_suffix1", "prefix1_middle1_suffix2"]
-});
-verify.completions({
-    marker: "5b",
-    unsorted: ["prefix1_middle1_suffix1", "prefix1_middle1_suffix2"]
-});
-verify.completions({
-    marker: "6a",
-    unsorted: ["prefix1_middle1_suffix1", "prefix1_middle1_suffix1suffix1"]
-});
-verify.completions({
-    marker: "6b",
-    unsorted: ["prefix1_middle1_suffix1", "prefix1_middle1_suffix1suffix2"]
-});
+verify.baselineCompletions()
