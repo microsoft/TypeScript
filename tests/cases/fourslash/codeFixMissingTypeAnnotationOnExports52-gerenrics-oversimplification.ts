a603aa226b066dce0e8abd3f0f452ec8c86bb60c
Ben Lickly <blickly@users.noreply.github.com>
2024-08-29 14:49:32 -0700

    Update tests/cases/fourslash/codeFixMissingTypeAnnotationOnExports52-gerenrics-oversimplification.ts
    Fix typo
    
    Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

--- tests/cases/fourslash/codeFixMissingTypeAnnotationOnExports52-gerenrics-oversimplification.ts
+++ tests/cases/fourslash/codeFixMissingTypeAnnotationOnExports52-gerenrics-oversimplification.ts
@@ -2,7 +2,7 @@
 
 // In the abstract, we might prefer the inferred return type annotation to
 // be identical to the parameter type (with 2 type parameters).
-// Our current heursitic to avoid overly complex types in this case creates
+// Our current heuristic to avoid overly complex types in this case creates
 // "overly simple" types, but this tradeoff seems reasonable.
 
 // @isolatedDeclarations: true
