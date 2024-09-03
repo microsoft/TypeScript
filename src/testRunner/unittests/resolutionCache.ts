009daa65389f071fa79a6128b99746204bf2effa
Aman Karmani <aman@tmm1.net>
2024-09-03 01:27:14 -0700

    Fix lint

--- src/testRunner/unittests/resolutionCache.ts
+++ src/testRunner/unittests/resolutionCache.ts
@@ -2,15 +2,14 @@ import * as ts from "../_namespaces/ts.js";
 
 describe("unittests:: resolution cache ", () => {
     it("perceivedOsRootLengthForWatching", () => {
-        const toPathComp = (path: string) => ts.getPathComponents(ts.toPath(path, undefined, (f: string) => f));
         const osRootBase = (path: string) => {
-            let pathComp = toPathComp(path);
-            let idx = ts.perceivedOsRootLengthForWatching(pathComp, pathComp.length);
+            const pathComp = ts.getPathComponents(ts.toPath(path, /*basePath*/ undefined, (f: string) => f));
+            const idx = ts.perceivedOsRootLengthForWatching(pathComp, pathComp.length);
             return pathComp[idx];
         };
-        assert.strictEqual(osRootBase("/Users/username"), undefined);
-        assert.strictEqual(osRootBase("/Users/username/Library"), undefined);
-        assert.strictEqual(osRootBase("/Users/username/Library/Caches"), undefined);
+        assert.isUndefined(osRootBase("/Users/username"));
+        assert.isUndefined(osRootBase("/Users/username/Library"));
+        assert.isUndefined(osRootBase("/Users/username/Library/Caches"));
         assert.strictEqual(osRootBase("/Users/username/Library/Caches/typescript/5.5/package.json"), "typescript");
     });
 });
