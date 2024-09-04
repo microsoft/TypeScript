0009efcd7c3bb9a9dcc378e61c8a9950921a2e20
Isabel Duan <isabelduan@microsoft.com>
2024-09-04 16:21:09 -0700

    test case (before)

---
+++ tests/baselines/reference/jsxChildWrongFragment.js
@@ -0,0 +1,24 @@
+//// [tests/cases/compiler/jsxChildWrongFragment.tsx] ////
+
+//// [index.tsx]
+/// <reference path="/.lib/react18/react18.d.ts" />
+/// <reference path="/.lib/react18/global.d.ts" />
+
+const test = () => "asd";
+
+// No Errors
+const jsxWithJsxFragment = <>{test}</>;
+
+// Type '() => string' is not assignable to type 'ReactNode'.
+const jsxWithReactFragment = <React.Fragment>{test}</React.Fragment>;
+
+
+//// [index.js]
+"use strict";
+/// <reference path="react18/react18.d.ts" />
+/// <reference path="react18/global.d.ts" />
+const test = () => "asd";
+// No Errors
+const jsxWithJsxFragment = React.createElement(React.Fragment, null, test);
+// Type '() => string' is not assignable to type 'ReactNode'.
+const jsxWithReactFragment = React.createElement(React.Fragment, null, test);
---
+++ tests/cases/compiler/jsxChildWrongFragment.tsx
@@ -0,0 +1,19 @@
+// @jsx: react
+// @strict: true
+// @target: ES2017
+// @module: ESNext
+// @esModuleInterop: true
+// @skipLibCheck: true
+
+
+// @filename: index.tsx
+/// <reference path="/.lib/react18/react18.d.ts" />
+/// <reference path="/.lib/react18/global.d.ts" />
+
+const test = () => "asd";
+
+// No Errors
+const jsxWithJsxFragment = <>{test}</>;
+
+// Type '() => string' is not assignable to type 'ReactNode'.
+const jsxWithReactFragment = <React.Fragment>{test}</React.Fragment>;
