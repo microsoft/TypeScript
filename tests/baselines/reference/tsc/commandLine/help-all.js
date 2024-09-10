c8ba95d2b91d619b088ce106e3184a267e3a82f5
Andrew Branch <andrew@wheream.io>
2024-09-10 15:28:11 -0700

    Update baseline

--- tests/baselines/reference/tsc/commandLine/help-all.js
+++ tests/baselines/reference/tsc/commandLine/help-all.js
@@ -122,6 +122,11 @@ Use the package.json 'imports' field when resolving imports.
 type: boolean
 default: `true` when 'moduleResolution' is 'node16', 'nodenext', or 'bundler'; otherwise `false`.
 
+[94m--rewriteRelativeImportExtensions[39m
+Rewrite '.ts', '.tsx', '.mts', and '.cts' file extensions in relative import paths to their JavaScript equivalent in output files.
+type: boolean
+default: false
+
 [94m--rootDir[39m
 Specify the root folder within your source files.
 type: string
