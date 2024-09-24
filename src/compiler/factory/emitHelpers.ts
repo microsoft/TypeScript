d7b7e627fc35b8014e9834de0f023f49d3de99e5
Andrew Branch <andrew@wheream.io>
2024-09-24 09:39:12 -0700

    Sync helper with tslib PR

--- src/compiler/factory/emitHelpers.ts
+++ src/compiler/factory/emitHelpers.ts
@@ -1445,17 +1445,17 @@ const rewriteRelativeImportExtensionsHelper: UnscopedEmitHelper = {
     text: `
         var __rewriteRelativeImportExtension = (this && this.__rewriteRelativeImportExtension) || function (path, preserveJsx) {
             if (typeof path === "string" && path[0] === "." && (path[1] === "/" || path[1] === "." && path[2] === "/")) {
-                if (path.substring(path.length - 4) === ".tsx") {
+                if (path.substring(path.length - 4).toLowerCase() === ".tsx") {
                     return path.substring(0, path.length - 4) + (preserveJsx ? ".jsx" : ".js");
                 }
-                if (path.substring(path.length - 3) === ".ts") {
-                    var dot = path.lastIndexOf(".", path.length - 4);
-                    if (dot >= 0 && (path.substring(dot - 2, dot) === ".d" || path.substring(dot, dot + 2) === ".d")) {
-                        return path;
+                var extMatch = path.match(/\\.[cm]?ts$/i);
+                if (extMatch) {
+                    var ext = extMatch[0].toLowerCase();
+                    var dot = path.lastIndexOf(".", path.length - (ext.length + 1));
+                    if (dot < 0 || !(path.substring(dot - 2, dot).toLowerCase() === ".d" || path.substring(dot, dot + 2).toLowerCase() === ".d")) {
+                        return path.substring(0, path.length - ext.length) + (ext === ".mts" ? ".mjs" : ext === ".cts" ? ".cjs" : ".js");
                     }
-                    return path.substring(0, path.length - 3) + ".js";
                 }
-                return path.replace(/(?<!\\.d)\\.[cm]ts$/, function (ext) { return ext === ".mts" ? ".mjs" : ".cjs"; });
             }
             return path;
         };`,
--- tests/baselines/reference/emitModuleCommonJS(module=commonjs).js
+++ tests/baselines/reference/emitModuleCommonJS(module=commonjs).js
@@ -15,17 +15,17 @@
 //// [a.js]
 var __rewriteRelativeImportExtension = (this && this.__rewriteRelativeImportExtension) || function (path, preserveJsx) {
     if (typeof path === "string" && path[0] === "." && (path[1] === "/" || path[1] === "." && path[2] === "/")) {
-        if (path.substring(path.length - 4) === ".tsx") {
+        if (path.substring(path.length - 4).toLowerCase() === ".tsx") {
             return path.substring(0, path.length - 4) + (preserveJsx ? ".jsx" : ".js");
         }
-        if (path.substring(path.length - 3) === ".ts") {
-            var dot = path.lastIndexOf(".", path.length - 4);
-            if (dot >= 0 && (path.substring(dot - 2, dot) === ".d" || path.substring(dot, dot + 2) === ".d")) {
-                return path;
+        var extMatch = path.match(/\.[cm]?ts$/i);
+        if (extMatch) {
+            var ext = extMatch[0].toLowerCase();
+            var dot = path.lastIndexOf(".", path.length - (ext.length + 1));
+            if (dot < 0 || !(path.substring(dot - 2, dot).toLowerCase() === ".d" || path.substring(dot, dot + 2).toLowerCase() === ".d")) {
+                return path.substring(0, path.length - ext.length) + (ext === ".mts" ? ".mjs" : ext === ".cts" ? ".cjs" : ".js");
             }
-            return path.substring(0, path.length - 3) + ".js";
         }
-        return path.replace(/(?<!\.d)\.[cm]ts$/, function (ext) { return ext === ".mts" ? ".mjs" : ".cjs"; });
     }
     return path;
 };
@@ -36,17 +36,17 @@ var __rewriteRelativeImportExtension = (this && this.__rewriteRelativeImportExte
 //// [b.js]
 var __rewriteRelativeImportExtension = (this && this.__rewriteRelativeImportExtension) || function (path, preserveJsx) {
     if (typeof path === "string" && path[0] === "." && (path[1] === "/" || path[1] === "." && path[2] === "/")) {
-        if (path.substring(path.length - 4) === ".tsx") {
+        if (path.substring(path.length - 4).toLowerCase() === ".tsx") {
             return path.substring(0, path.length - 4) + (preserveJsx ? ".jsx" : ".js");
         }
-        if (path.substring(path.length - 3) === ".ts") {
-            var dot = path.lastIndexOf(".", path.length - 4);
-            if (dot >= 0 && (path.substring(dot - 2, dot) === ".d" || path.substring(dot, dot + 2) === ".d")) {
-                return path;
+        var extMatch = path.match(/\.[cm]?ts$/i);
+        if (extMatch) {
+            var ext = extMatch[0].toLowerCase();
+            var dot = path.lastIndexOf(".", path.length - (ext.length + 1));
+            if (dot < 0 || !(path.substring(dot - 2, dot).toLowerCase() === ".d" || path.substring(dot, dot + 2).toLowerCase() === ".d")) {
+                return path.substring(0, path.length - ext.length) + (ext === ".mts" ? ".mjs" : ext === ".cts" ? ".cjs" : ".js");
             }
-            return path.substring(0, path.length - 3) + ".js";
         }
-        return path.replace(/(?<!\.d)\.[cm]ts$/, function (ext) { return ext === ".mts" ? ".mjs" : ".cjs"; });
     }
     return path;
 };
