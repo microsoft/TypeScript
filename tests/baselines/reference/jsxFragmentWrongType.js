36bb8e7fc326735491d4e12d43dd1e5d32989675
Isabel Duan <isabelduan@microsoft.com>
2024-09-24 12:29:33 -0700

    rename test

--- tests/baselines/reference/jsxFragmentWrongType.js
+++ tests/baselines/reference/jsxFragmentWrongType.js
@@ -1,25 +1,19 @@
-//// [tests/cases/compiler/jsxChildWrongFragment.tsx] ////
+//// [tests/cases/compiler/jsxFragmentWrongType.tsx] ////
 
-//// [index.tsx]
+//// [a.tsx]
 /// <reference path="/.lib/react18/react18.d.ts" />
 /// <reference path="/.lib/react18/global.d.ts" />
 
 const test = () => "asd";
 
-// these two should error in the same way 
 const jsxWithJsxFragment = <>{test}</>;
 const jsxWithReactFragment = <React.Fragment>{test}</React.Fragment>;
 
-const jsxNestedFragment = <><>{test}</></>;
 
-
-//// [index.js]
+//// [a.js]
 "use strict";
 /// <reference path="react18/react18.d.ts" />
 /// <reference path="react18/global.d.ts" />
 const test = () => "asd";
-// these two should error in the same way 
 const jsxWithJsxFragment = React.createElement(React.Fragment, null, test);
 const jsxWithReactFragment = React.createElement(React.Fragment, null, test);
-const jsxNestedFragment = React.createElement(React.Fragment, null,
-    React.createElement(React.Fragment, null, test));
