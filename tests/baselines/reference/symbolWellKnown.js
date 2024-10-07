4bf9f7590d98956e84cd8ce8906e7c7826c4a4c7
milkcask <1572863+milkcask@users.noreply.github.com>
2024-10-07 08:04:22 +0100

    Add baselines

---
+++ tests/baselines/reference/symbolWellKnown.js
@@ -0,0 +1,52 @@
+//// [tests/cases/conformance/es6/Symbols/symbolWellKnown.ts] ////
+
+//// [symbolWellKnown.ts]
+var symbol = Symbol();
+symbol[Symbol.toStringTag];
+
+var map = new Map();
+map[Symbol.toStringTag];
+
+var readonlyMap = <ReadonlyMap<unknown, unknown>>new Map();
+readonlyMap[Symbol.toStringTag];
+
+var weakMap = new WeakMap();
+weakMap[Symbol.toStringTag];
+
+var set = new Set();
+set[Symbol.toStringTag];
+
+var readonlySet = <ReadonlySet<any>>new Set();
+readonlySet[Symbol.toStringTag];
+
+var weakSet = new WeakSet();
+weakSet[Symbol.toStringTag];
+
+JSON[Symbol.toStringTag];
+
+Math[Symbol.toStringTag];
+
+var promise = new Promise(()=>{});
+promise[Symbol.toStringTag];
+
+
+
+//// [symbolWellKnown.js]
+var symbol = Symbol();
+symbol[Symbol.toStringTag];
+var map = new Map();
+map[Symbol.toStringTag];
+var readonlyMap = new Map();
+readonlyMap[Symbol.toStringTag];
+var weakMap = new WeakMap();
+weakMap[Symbol.toStringTag];
+var set = new Set();
+set[Symbol.toStringTag];
+var readonlySet = new Set();
+readonlySet[Symbol.toStringTag];
+var weakSet = new WeakSet();
+weakSet[Symbol.toStringTag];
+JSON[Symbol.toStringTag];
+Math[Symbol.toStringTag];
+var promise = new Promise(() => { });
+promise[Symbol.toStringTag];
---
+++ tests/cases/conformance/es6/Symbols/symbolWellKnown.ts
@@ -0,0 +1,30 @@
+// @target: es2015
+
+var symbol = Symbol();
+symbol[Symbol.toStringTag];
+
+var map = new Map();
+map[Symbol.toStringTag];
+
+var readonlyMap = <ReadonlyMap<unknown, unknown>>new Map();
+readonlyMap[Symbol.toStringTag];
+
+var weakMap = new WeakMap();
+weakMap[Symbol.toStringTag];
+
+var set = new Set();
+set[Symbol.toStringTag];
+
+var readonlySet = <ReadonlySet<any>>new Set();
+readonlySet[Symbol.toStringTag];
+
+var weakSet = new WeakSet();
+weakSet[Symbol.toStringTag];
+
+JSON[Symbol.toStringTag];
+
+Math[Symbol.toStringTag];
+
+var promise = new Promise(()=>{});
+promise[Symbol.toStringTag];
+
