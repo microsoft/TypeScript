d6ecddb122b4d7adf7dc20a9d10bb0b6e378e653
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-10-27 10:17:15 +0100

    Fixed an issue with JSX spreads sometimes reported as always overriding other attributes

--- tests/baselines/reference/jsxSpreadOverwritesAttributeStrict.js
+++ tests/baselines/reference/jsxSpreadOverwritesAttributeStrict.js
@@ -24,6 +24,27 @@ const b2 = <Foo a={1} b={2} {...props}></Foo>;
 const b3 = <Foo a={1} d={1} {...props} {...{ d: 1 }}></Foo>;
 const b4 = <Foo a={1} d={1} {...props} {...{ a: 1, d: 1 }}></Foo>;
 
+// https://github.com/microsoft/TypeScript/issues/60343
+
+function Component60343(props: any) {
+  return <div></div>;
+}
+
+function Test60343({ shouldOverride }: { shouldOverride: boolean }) {
+  return (
+    <Component60343
+      a={false}
+      b={false} // no error
+      c={false}
+      {...(shouldOverride
+        ? {
+            b: true,
+          }
+        : {})}
+    />
+  );
+}
+
 
 //// [file.jsx]
 "use strict";
@@ -39,3 +60,16 @@ var b1 = <Foo a={1} {...props}></Foo>;
 var b2 = <Foo a={1} b={2} {...props}></Foo>;
 var b3 = <Foo a={1} d={1} {...props} {...{ d: 1 }}></Foo>;
 var b4 = <Foo a={1} d={1} {...props} {...{ a: 1, d: 1 }}></Foo>;
+// https://github.com/microsoft/TypeScript/issues/60343
+function Component60343(props) {
+    return <div></div>;
+}
+function Test60343(_a) {
+    var shouldOverride = _a.shouldOverride;
+    return (<Component60343 a={false} b={false} // no error
+     c={false} {...(shouldOverride
+        ? {
+            b: true,
+        }
+        : {})}/>);
+}
