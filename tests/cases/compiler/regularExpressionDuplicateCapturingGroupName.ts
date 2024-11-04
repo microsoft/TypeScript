24c16f9efe7c62f70d9d7e8aebcd3e6ad1e2cfc6
graphemecluster <graphemecluster@gmail.com>
2024-11-04 10:31:46 +0800

    Add Test Cases for Duplicate Capturing Group Name

---
+++ tests/cases/compiler/regularExpressionDuplicateCapturingGroupName.ts
@@ -0,0 +1,33 @@
+// @strict: true
+// @target: esnext
+
+// Adjacent homonymous capturing groups
+/(?<foo>)(?<foo>)/;
+/(?<foo>)((?<foo>))/;
+/((?<foo>))(?<foo>)/;
+/((?<foo>))((?<foo>))/;
+/(?<foo>)(?=(?<foo>))/;
+/(?<=(?<foo>))(?<foo>)/;
+/(?<foo>)(?!(?<foo>))/;
+/(?<!(?<foo>))(?<foo>)/;
+
+/((?<foo>))((?=(?<foo>)))/;
+/((?<!(?<foo>)))((?<foo>))/;
+
+/(?=(?<foo>))(?=(?<foo>))/;
+/(?!(?<foo>))(?!(?<foo>))/;
+/(?=(?<foo>))(?!(?<foo>))/;
+
+// Nested homonymous capturing groups
+/(?<foo>(?<foo>))/;
+/(?<foo>((?<foo>)))/;
+
+// Complicated cases
+/(?<a>)((?<b>)((?<c>)|(?<d>))|(?<b>)|((?<c>)))(?<c>)((?<b>)|(?<d>))/;
+/(?<a>)(((?<b>)|(?<c>))((?<d>)|(?<a>)|(?<b>))|(?<c>)|((?<d>)))(?<a>)(((?<b>)|(?<c>))|(?<d>))/;
+
+// Should not error
+/(?<foo>)|(?<foo>)/;
+/(?<foo>)|((?<foo>))/;
+/((?<foo>))|(?<foo>)/;
+/((?<foo>))|((?<foo>))/;
