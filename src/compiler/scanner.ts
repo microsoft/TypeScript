3ddc5da691cb84ec66e93b6470d7650ca9f683a2
graphemecluster <graphemecluster@gmail.com>
2024-11-09 14:13:32 +0800

    Mark `RegExpDigits` as character class (for fast path)

--- src/compiler/scanner.ts
+++ src/compiler/scanner.ts
@@ -323,6 +323,7 @@ const regExpFlagToFirstAvailableLanguageVersion = new Map<RegularExpressionFlags
 ]);
 
 const RegExpDigits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) as RegularExpressionPatternUnion;
+RegExpDigits.isCharacterClass = true;
 
 /** @internal */
 export const RegExpAnyString: unique symbol = {} as never;
