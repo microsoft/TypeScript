0cf763d5cfaf9a81f60ff6af432860b0155d23bf
graphemecluster <graphemecluster@gmail.com>
2024-11-10 11:24:31 +0800

    Separate type checking test case into 2 files which tests for different methods

---
+++ tests/baselines/reference/regularExpressionTypeCheckingReplace(target=esnext).js
@@ -0,0 +1,84 @@
+//// [tests/cases/compiler/regularExpressionTypeCheckingReplace.ts] ////
+
+//// [regularExpressionTypeCheckingReplace.ts]
+"foo_foo_bar".replace(/foo/g, (match, index, input, ...args) => {
+    match; // "foo"
+    index; // number
+    input; // `${string}foo${string}`
+    args; // []
+    return match;
+});
+
+"foo42_foo24_bar".replace(/foo(\d+)/g, (match, id, index, input, ...args) => {
+    match; // `foo${string}`
+    id; // string
+    index; // number
+    input; // `${string}foo${string}`
+    args; // []
+    return match;
+});
+
+"foo42_foo24_bar".replace(/foo(?<id>\d+)/g, (match, id, index, input, capturingGroups, ...args) => {
+    match; // `foo${string}`
+    id; // string
+    index; // number
+    input; // `${string}foo${string}`
+    // for target ≥ ES2018
+    capturingGroups.id; // string
+    capturingGroups.foo; // error
+    args; // []
+    return match;
+});
+
+"foo_foo_bar".replace(/foo(?<empty>){0}/g, (match, empty, index, input, capturingGroups, ...args) => {
+    match; // "foo"
+    empty; // "" | undefined
+    index; // number
+    input; // `${string}foo${string}`
+    // for target ≥ ES2018
+    capturingGroups.empty; // "" | undefined
+    capturingGroups.foo; // error
+    args; // []
+    return match;
+});
+
+
+//// [regularExpressionTypeCheckingReplace.js]
+"use strict";
+"foo_foo_bar".replace(/foo/g, (match, index, input, ...args) => {
+    match; // "foo"
+    index; // number
+    input; // `${string}foo${string}`
+    args; // []
+    return match;
+});
+"foo42_foo24_bar".replace(/foo(\d+)/g, (match, id, index, input, ...args) => {
+    match; // `foo${string}`
+    id; // string
+    index; // number
+    input; // `${string}foo${string}`
+    args; // []
+    return match;
+});
+"foo42_foo24_bar".replace(/foo(?<id>\d+)/g, (match, id, index, input, capturingGroups, ...args) => {
+    match; // `foo${string}`
+    id; // string
+    index; // number
+    input; // `${string}foo${string}`
+    // for target ≥ ES2018
+    capturingGroups.id; // string
+    capturingGroups.foo; // error
+    args; // []
+    return match;
+});
+"foo_foo_bar".replace(/foo(?<empty>){0}/g, (match, empty, index, input, capturingGroups, ...args) => {
+    match; // "foo"
+    empty; // "" | undefined
+    index; // number
+    input; // `${string}foo${string}`
+    // for target ≥ ES2018
+    capturingGroups.empty; // "" | undefined
+    capturingGroups.foo; // error
+    args; // []
+    return match;
+});
