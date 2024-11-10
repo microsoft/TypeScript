402760c2bb5b3bacc485ab380c8cfbf9af33d484
graphemecluster <graphemecluster@gmail.com>
2024-11-11 01:22:05 +0800

    Separate the type checking test case further … since the performance issue is not able to be fixed for now.

---
+++ tests/baselines/reference/regularExpressionTypeCheckingMatchExecWithHasIndicesFlag(target=es2015).js
@@ -0,0 +1,89 @@
+//// [tests/cases/compiler/regularExpressionTypeCheckingMatchExecWithHasIndicesFlag.ts] ////
+
+//// [regularExpressionTypeCheckingMatchExecWithHasIndicesFlag.ts]
+// a very loose Temporal (Zoned)DateTime format regex, identical to the one in `tests/cases/conformance/types/literal/regularExpressionLiteralTypes.ts`
+const dateTimeRegexWithIndices = /(?<date>(?<year>\d{4}|(?!-000000)[+-]\d{6})(?<dateSeparator>-)?(?!(?:0[2469]|11)\k<dateSeparator>31|02\k<dateSeparator>30)(?<month>0[1-9]|1[0-2])\k<dateSeparator>(?<day>0[1-9]|[12]\d|3[01]))(?:[ T](?<time>(?<hour>[01]\d|2[0-3])(?:(?<timeSeparator>:)?(?<minute>[0-5]\d)(?:\k<timeSeparator>(?<second>(?:[0-5]\d|60)(?:[.,]\d{1,9})?))?)?)(?<timeZone>Z|(?<timeZoneSign>[+-])(?:(?<timeZoneHour>[01]\d|2[0-3])(?:(?<timeZoneTimeSeparator>:)?(?<timeZoneMinute>[0-5]\d)(?:\k<timeZoneTimeSeparator>(?<timeZoneSecond>[0-5]\d(?:[.,]\d{1,9})?))?)?))?)?/id;
+const dateTimeString = "2048-10-24 12:34:56";
+
+let match = dateTimeString.match(dateTimeRegexWithIndices)!; // RegExpExecArray
+match.index; // number
+match.input; // string
+match.length; // 17
+match[0].length; // number
+match[1].length; // number
+match[16]?.length; // number | undefined
+match[17]; // should error, but see #45560#issuecomment-1111121849
+match.groups.date.length; // number
+match.groups.timeZoneSecond?.length; // number | undefined
+match.groups.foo; // error
+
+// Each element is of type [startIndex: number, endIndex: number]
+match.indices.length; // 17
+match.indices[0].length; // 2
+match.indices[1].length; // 2
+match.indices[2].length; // 2
+match.indices[3]?.length; // 2 | undefined
+match.indices[4].length; // 2
+match.indices[5].length; // 2
+match.indices[6]?.length; // 2 | undefined
+match.indices[16]?.length; // 2 | undefined
+match.indices[17]; // should error, but see #45560#issuecomment-1111121849
+match.indices.groups.date.length; // 2
+match.indices.groups.year.length; // 2
+match.indices.groups.month.length; // 2
+match.indices.groups.day.length; // 2
+match.indices.groups.time?.length; // 2 | undefined
+match.indices.groups.hour?.length; // 2 | undefined
+match.indices.groups.minute?.length; // 2 | undefined
+match.indices.groups.second?.length; // 2 | undefined
+match.indices.groups.timeZone?.length; // 2 | undefined
+match.indices.groups.timeZoneSecond?.length; // 2 | undefined
+match.indices.groups.foo; // error
+
+let execResult = dateTimeRegexWithIndices.exec(dateTimeString)!;
+match = execResult; // should not error
+execResult = match; // should not error
+
+
+//// [regularExpressionTypeCheckingMatchExecWithHasIndicesFlag.js]
+"use strict";
+var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l;
+// a very loose Temporal (Zoned)DateTime format regex, identical to the one in `tests/cases/conformance/types/literal/regularExpressionLiteralTypes.ts`
+const dateTimeRegexWithIndices = /(?<date>(?<year>\d{4}|(?!-000000)[+-]\d{6})(?<dateSeparator>-)?(?!(?:0[2469]|11)\k<dateSeparator>31|02\k<dateSeparator>30)(?<month>0[1-9]|1[0-2])\k<dateSeparator>(?<day>0[1-9]|[12]\d|3[01]))(?:[ T](?<time>(?<hour>[01]\d|2[0-3])(?:(?<timeSeparator>:)?(?<minute>[0-5]\d)(?:\k<timeSeparator>(?<second>(?:[0-5]\d|60)(?:[.,]\d{1,9})?))?)?)(?<timeZone>Z|(?<timeZoneSign>[+-])(?:(?<timeZoneHour>[01]\d|2[0-3])(?:(?<timeZoneTimeSeparator>:)?(?<timeZoneMinute>[0-5]\d)(?:\k<timeZoneTimeSeparator>(?<timeZoneSecond>[0-5]\d(?:[.,]\d{1,9})?))?)?))?)?/id;
+const dateTimeString = "2048-10-24 12:34:56";
+let match = dateTimeString.match(dateTimeRegexWithIndices); // RegExpExecArray
+match.index; // number
+match.input; // string
+match.length; // 17
+match[0].length; // number
+match[1].length; // number
+(_a = match[16]) === null || _a === void 0 ? void 0 : _a.length; // number | undefined
+match[17]; // should error, but see #45560#issuecomment-1111121849
+match.groups.date.length; // number
+(_b = match.groups.timeZoneSecond) === null || _b === void 0 ? void 0 : _b.length; // number | undefined
+match.groups.foo; // error
+// Each element is of type [startIndex: number, endIndex: number]
+match.indices.length; // 17
+match.indices[0].length; // 2
+match.indices[1].length; // 2
+match.indices[2].length; // 2
+(_c = match.indices[3]) === null || _c === void 0 ? void 0 : _c.length; // 2 | undefined
+match.indices[4].length; // 2
+match.indices[5].length; // 2
+(_d = match.indices[6]) === null || _d === void 0 ? void 0 : _d.length; // 2 | undefined
+(_e = match.indices[16]) === null || _e === void 0 ? void 0 : _e.length; // 2 | undefined
+match.indices[17]; // should error, but see #45560#issuecomment-1111121849
+match.indices.groups.date.length; // 2
+match.indices.groups.year.length; // 2
+match.indices.groups.month.length; // 2
+match.indices.groups.day.length; // 2
+(_f = match.indices.groups.time) === null || _f === void 0 ? void 0 : _f.length; // 2 | undefined
+(_g = match.indices.groups.hour) === null || _g === void 0 ? void 0 : _g.length; // 2 | undefined
+(_h = match.indices.groups.minute) === null || _h === void 0 ? void 0 : _h.length; // 2 | undefined
+(_j = match.indices.groups.second) === null || _j === void 0 ? void 0 : _j.length; // 2 | undefined
+(_k = match.indices.groups.timeZone) === null || _k === void 0 ? void 0 : _k.length; // 2 | undefined
+(_l = match.indices.groups.timeZoneSecond) === null || _l === void 0 ? void 0 : _l.length; // 2 | undefined
+match.indices.groups.foo; // error
+let execResult = dateTimeRegexWithIndices.exec(dateTimeString);
+match = execResult; // should not error
+execResult = match; // should not error
--- tests/cases/compiler/regularExpressionTypeCheckingMatchExec.ts
+++ tests/cases/compiler/regularExpressionTypeCheckingMatchExec.ts
@@ -3,100 +3,36 @@
 // @noUncheckedIndexedAccess: true
 // @exactOptionalPropertyTypes: true
 
+// a very loose Temporal (Zoned)DateTime format regex, identical to the one in `tests/cases/conformance/types/literal/regularExpressionLiteralTypes.ts`
+const dateTimeRegex = /^(?<date>(?<year>\d{4}|(?!-000000)[+-]\d{6})(?<dateSeparator>-)?(?!(?:0[2469]|11)\k<dateSeparator>31|02\k<dateSeparator>30)(?<month>0[1-9]|1[0-2])\k<dateSeparator>(?<day>0[1-9]|[12]\d|3[01]))(?:[ T](?<time>(?<hour>[01]\d|2[0-3])(?:(?<timeSeparator>:)?(?<minute>[0-5]\d)(?:\k<timeSeparator>(?<second>(?:[0-5]\d|60)(?:[.,]\d{1,9})?))?)?)(?<timeZone>Z|(?<timeZoneSign>[+-])(?:(?<timeZoneHour>[01]\d|2[0-3])(?:(?<timeZoneTimeSeparator>:)?(?<timeZoneMinute>[0-5]\d)(?:\k<timeZoneTimeSeparator>(?<timeZoneSecond>[0-5]\d(?:[.,]\d{1,9})?))?)?))?)?$/i;
 const dateTimeString = "2048-10-24 12:34:56";
 
-// a very loose Temporal (Zoned)DateTime format regex, identical to the one in `tests/cases/conformance/types/literal/regularExpressionLiteralTypes.ts`
-{
-    const dateTimeRegex = /^(?<date>(?<year>\d{4}|(?!-000000)[+-]\d{6})(?<dateSeparator>-)?(?!(?:0[2469]|11)\k<dateSeparator>31|02\k<dateSeparator>30)(?<month>0[1-9]|1[0-2])\k<dateSeparator>(?<day>0[1-9]|[12]\d|3[01]))(?:[ T](?<time>(?<hour>[01]\d|2[0-3])(?:(?<timeSeparator>:)?(?<minute>[0-5]\d)(?:\k<timeSeparator>(?<second>(?:[0-5]\d|60)(?:[.,]\d{1,9})?))?)?)(?<timeZone>Z|(?<timeZoneSign>[+-])(?:(?<timeZoneHour>[01]\d|2[0-3])(?:(?<timeZoneTimeSeparator>:)?(?<timeZoneMinute>[0-5]\d)(?:\k<timeZoneTimeSeparator>(?<timeZoneSecond>[0-5]\d(?:[.,]\d{1,9})?))?)?))?)?$/i;
+let match = dateTimeString.match(dateTimeRegex)!; // RegExpExecArray
+match.index; // number
+match.input; // string
+match.length; // 17
+match[0].length; // number
+match[1].length; // number
+match[2].length; // number
+match[3]?.length; // number | undefined
+match[4].length; // number
+match[5].length; // number
+match[6]?.length; // number | undefined
+match[16]?.length; // number | undefined
+match[17]; // should error, but see #45560#issuecomment-1111121849
+match.groups.date.length; // number
+match.groups.year.length; // number
+match.groups.month.length; // number
+match.groups.day.length; // number
+match.groups.time?.length; // number | undefined
+match.groups.hour?.length; // number | undefined
+match.groups.minute?.length; // number | undefined
+match.groups.second?.length; // number | undefined
+match.groups.timeZone?.length; // number | undefined
+match.groups.timeZoneSecond?.length; // number | undefined
+match.groups.foo; // error
+match.indices; // error
 
-    let match = dateTimeString.match(dateTimeRegex)!; // RegExpExecArray
-    match.index; // number
-    match.input; // string
-    match.length; // 17
-    match[0].length; // number
-    match[1].length; // number
-    match[2].length; // number
-    match[3]?.length; // number | undefined
-    match[4].length; // number
-    match[5].length; // number
-    match[6]?.length; // number | undefined
-    match[16]?.length; // number | undefined
-    match[17]; // should error, but see #45560#issuecomment-1111121849
-    match.groups.date.length; // number
-    match.groups.year.length; // number
-    match.groups.month.length; // number
-    match.groups.day.length; // number
-    match.groups.time?.length; // number | undefined
-    match.groups.hour?.length; // number | undefined
-    match.groups.minute?.length; // number | undefined
-    match.groups.second?.length; // number | undefined
-    match.groups.timeZone?.length; // number | undefined
-    match.groups.timeZoneSecond?.length; // number | undefined
-    match.groups.foo; // error
-    match.indices; // error
-
-    let execResult = dateTimeRegex.exec(dateTimeString)!;
-    match = execResult; // should not error
-    execResult = match; // should not error
-}
-
-{
-    const globalDateTimeRegex = /(?<date>(?<year>\d{4}|(?!-000000)[+-]\d{6})(?<dateSeparator>-)?(?!(?:0[2469]|11)\k<dateSeparator>31|02\k<dateSeparator>30)(?<month>0[1-9]|1[0-2])\k<dateSeparator>(?<day>0[1-9]|[12]\d|3[01]))(?:[ T](?<time>(?<hour>[01]\d|2[0-3])(?:(?<timeSeparator>:)?(?<minute>[0-5]\d)(?:\k<timeSeparator>(?<second>(?:[0-5]\d|60)(?:[.,]\d{1,9})?))?)?)(?<timeZone>Z|(?<timeZoneSign>[+-])(?:(?<timeZoneHour>[01]\d|2[0-3])(?:(?<timeZoneTimeSeparator>:)?(?<timeZoneMinute>[0-5]\d)(?:\k<timeZoneTimeSeparator>(?<timeZoneSecond>[0-5]\d(?:[.,]\d{1,9})?))?)?))?)?/ig;
-
-    let match = dateTimeString.match(globalDateTimeRegex)!; // RegExpMatchArray
-    match.index; // error
-    match.input; // error
-    match.length; // number
-    match[0].length; // number
-    match[1]?.length; // number | undefined
-    match[99]?.length; // number | undefined
-    match.groups; // error
-    match.indices; // error
-
-    let execResult = globalDateTimeRegex.exec(dateTimeString)!;
-    match = execResult; // should error
-    execResult = match; // should error
-}
-
-{
-    const dateTimeRegexWithIndices = /(?<date>(?<year>\d{4}|(?!-000000)[+-]\d{6})(?<dateSeparator>-)?(?!(?:0[2469]|11)\k<dateSeparator>31|02\k<dateSeparator>30)(?<month>0[1-9]|1[0-2])\k<dateSeparator>(?<day>0[1-9]|[12]\d|3[01]))(?:[ T](?<time>(?<hour>[01]\d|2[0-3])(?:(?<timeSeparator>:)?(?<minute>[0-5]\d)(?:\k<timeSeparator>(?<second>(?:[0-5]\d|60)(?:[.,]\d{1,9})?))?)?)(?<timeZone>Z|(?<timeZoneSign>[+-])(?:(?<timeZoneHour>[01]\d|2[0-3])(?:(?<timeZoneTimeSeparator>:)?(?<timeZoneMinute>[0-5]\d)(?:\k<timeZoneTimeSeparator>(?<timeZoneSecond>[0-5]\d(?:[.,]\d{1,9})?))?)?))?)?/id;
-
-    let match = dateTimeString.match(dateTimeRegexWithIndices)!; // RegExpExecArray
-    match.index; // number
-    match.input; // string
-    match.length; // 17
-    match[0].length; // number
-    match[1].length; // number
-    match[16]?.length; // number | undefined
-    match[17]; // should error, but see #45560#issuecomment-1111121849
-    match.groups.date.length; // number
-    match.groups.timeZoneSecond?.length; // number | undefined
-    match.groups.foo; // error
-
-    // Each element is of type [startIndex: number, endIndex: number]
-    match.indices.length; // 17
-    match.indices[0].length; // 2
-    match.indices[1].length; // 2
-    match.indices[2].length; // 2
-    match.indices[3]?.length; // 2 | undefined
-    match.indices[4].length; // 2
-    match.indices[5].length; // 2
-    match.indices[6]?.length; // 2 | undefined
-    match.indices[16]?.length; // 2 | undefined
-    match.indices[17]; // should error, but see #45560#issuecomment-1111121849
-    match.indices.groups.date.length; // 2
-    match.indices.groups.year.length; // 2
-    match.indices.groups.month.length; // 2
-    match.indices.groups.day.length; // 2
-    match.indices.groups.time?.length; // 2 | undefined
-    match.indices.groups.hour?.length; // 2 | undefined
-    match.indices.groups.minute?.length; // 2 | undefined
-    match.indices.groups.second?.length; // 2 | undefined
-    match.indices.groups.timeZone?.length; // 2 | undefined
-    match.indices.groups.timeZoneSecond?.length; // 2 | undefined
-    match.indices.groups.foo; // error
-
-    let execResult = dateTimeRegexWithIndices.exec(dateTimeString)!;
-    match = execResult; // should not error
-    execResult = match; // should not error
-}
+let execResult = dateTimeRegex.exec(dateTimeString)!;
+match = execResult; // should not error
+execResult = match; // should not error
