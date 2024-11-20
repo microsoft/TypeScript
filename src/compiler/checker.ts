6148904495cd7795e79fb9784554054fdd4dcd6f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-11-20 09:44:12 +0100

    tweak logic

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -6104,13 +6104,19 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                     case SyntaxKind.PropertyDeclaration:
                     case SyntaxKind.PropertySignature:
                     case SyntaxKind.JSDocPropertyTag:
+                        if (!isOptionalDeclaration(declaration)) {
+                            return false;
+                        }
                         symbol ??= getSymbolOfDeclaration(declaration);
                         if (!(symbol.flags & SymbolFlags.Property) || !(symbol as MappedSymbol).links?.mappedType) {
                             return false;
                         }
                         const type = getTypeOfSymbol(symbol);
-                        const declaredType = getEffectiveTypeAnnotationNode(declaration);
-                        return containsNonMissingUndefinedType(type) && !!declaredType && !containsUndefinedType(getTypeFromTypeNodeWithoutContext(declaredType));
+                        if (!containsNonMissingUndefinedType(type)) {
+                            return false;
+                        }
+                        const declaredType = getEffectiveTypeAnnotationNode(declaration)
+                        return !!(declaredType && !containsUndefinedType(getTypeFromTypeNodeWithoutContext(declaredType)));
                     case SyntaxKind.Parameter:
                     case SyntaxKind.JSDocParameterTag:
                         return requiresAddingImplicitUndefined(declaration, enclosingDeclaration);
