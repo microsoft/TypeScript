75372aa4d620191627d5431e76830ce2b05ae44f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-11-27 00:17:29 +0100

    Fixed syntactic nullisness semantics for tagged template expressions (#60616)

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -39783,6 +39783,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         switch (node.kind) {
             case SyntaxKind.AwaitExpression:
             case SyntaxKind.CallExpression:
+            case SyntaxKind.TaggedTemplateExpression:
             case SyntaxKind.ElementAccessExpression:
             case SyntaxKind.MetaProperty:
             case SyntaxKind.NewExpression:
