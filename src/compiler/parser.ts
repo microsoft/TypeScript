ac51b11e74c06e2bf180266ac716ce5bd8bf6fdd
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2024-11-29 14:43:35 +0200

    fix formatting

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -9536,7 +9536,7 @@ namespace Parser {
                 const node = factory.createExpressionWithTypeArguments(expression, typeArguments) as ExpressionWithTypeArguments & { expression: Identifier | PropertyAccessEntityNameExpression; };
                 const res = finishNode(node, pos);
                 if (usedBrace) {
-                    skipWhitespace()
+                    skipWhitespace();
                     parseExpected(SyntaxKind.CloseBraceToken);
                 }
                 return res;
