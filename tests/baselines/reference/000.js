1bc0a5538a5aa3f7b3b501793ecb5dada1c7b0ad
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-12-05 15:13:57 +0100

    clean test

--- tests/baselines/reference/000.js
+++ tests/baselines/reference/000.js
@@ -10,64 +10,21 @@ interface UserName {
   last?: string;
 }
 
-// Can't union narrow of string | object:
 
-const nameState1 = {} as unknown as {
+const nameState = {} as {
   value: string;
   state: State<string>;
 } | {
   value: UserName;
   state: State<UserName>;
-} 
-//  | {
-//   value: undefined;
-//   state: State<undefined>;
-// };
-
-if (typeof nameState1.value === "string") {
-  const a: State<string> = nameState1.state;
-
-  //               ^^^^^^^^^
-  // Type 'State<string> | State<UserName>' does not satisfy the expected type 'State<string>'.
-  //   Type 'State<UserName>' is not assignable to type 'State<string>'.
-  //     Type 'UserName' is not assignable to type 'string'.(1360)
 }
 
-// But it works if I add undefined to the mix:
-
-// const nameState2 = {} as unknown as {
-//   value: undefined;
-//   state: State<undefined>;
-// } | {
-//   value: string;
-//   state: State<string>;
-// } | {
-//   value: UserName;
-//   state: State<UserName>;
-// };
+if (typeof nameState.value === "string") {
+  nameState.state satisfies  State<string>;
+}
 
 //// [000.js]
-// Can't union narrow of string | object:
-var nameState1 = {};
-//  | {
-//   value: undefined;
-//   state: State<undefined>;
-// };
-if (typeof nameState1.value === "string") {
-    var a = nameState1.state;
-    //               ^^^^^^^^^
-    // Type 'State<string> | State<UserName>' does not satisfy the expected type 'State<string>'.
-    //   Type 'State<UserName>' is not assignable to type 'State<string>'.
-    //     Type 'UserName' is not assignable to type 'string'.(1360)
+var nameState = {};
+if (typeof nameState.value === "string") {
+    nameState.state;
 }
-// But it works if I add undefined to the mix:
-// const nameState2 = {} as unknown as {
-//   value: undefined;
-//   state: State<undefined>;
-// } | {
-//   value: string;
-//   state: State<string>;
-// } | {
-//   value: UserName;
-//   state: State<UserName>;
-// };
--- tests/cases/compiler/000.ts
+++ tests/cases/compiler/000.ts
@@ -7,38 +7,15 @@ interface UserName {
   last?: string;
 }
 
-// Can't union narrow of string | object:
 
-const nameState1 = {} as unknown as {
+const nameState = {} as {
   value: string;
   state: State<string>;
 } | {
   value: UserName;
   state: State<UserName>;
-} 
-//  | {
-//   value: undefined;
-//   state: State<undefined>;
-// };
-
-if (typeof nameState1.value === "string") {
-  const a: State<string> = nameState1.state;
-
-  //               ^^^^^^^^^
-  // Type 'State<string> | State<UserName>' does not satisfy the expected type 'State<string>'.
-  //   Type 'State<UserName>' is not assignable to type 'State<string>'.
-  //     Type 'UserName' is not assignable to type 'string'.(1360)
 }
 
-// But it works if I add undefined to the mix:
-
-// const nameState2 = {} as unknown as {
-//   value: undefined;
-//   state: State<undefined>;
-// } | {
-//   value: string;
-//   state: State<string>;
-// } | {
-//   value: UserName;
-//   state: State<UserName>;
-// };
+if (typeof nameState.value === "string") {
+  nameState.state satisfies  State<string>;
+}
