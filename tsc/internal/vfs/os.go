74e732a4e79c4273687696c82466e32f6a3525e5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-12-13 15:12:19 -0800

    Make isFileSystemCaseSensitive more reliable (microsoft/typescript-go#175)

--- tsc/internal/vfs/os.go
+++ tsc/internal/vfs/os.go
@@ -1,11 +1,11 @@
 package vfs
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
-	"sync"
 	"unicode"
 
 	"github.com/microsoft/typescript-go/internal/tspath"
@@ -26,18 +26,31 @@ type osFS struct {
 	common
 }
 
-var isFileSystemCaseSensitive = sync.OnceValue(func() bool {
+// We do this right at startup to minimize the chance that executable gets moved or deleted.
+var isFileSystemCaseSensitive = func() bool {
 	// win32/win64 are case insensitive platforms
 	if runtime.GOOS == "windows" {
 		return false
 	}
 
+	// As a proxy for case-insensitivity, we check if the current executable exists under a different case.
+	// This is not entirely correct, since different OSs can have differing case sensitivity in different paths,
+	// but this is largely good enough for our purposes (and what sys.ts used to do with __filename).
+	exe, err := os.Executable()
+	if err != nil {
+		panic(fmt.Sprintf("vfs: failed to get executable path: %v", err))
+	}
+
 	// If the current executable exists under a different case, we must be case-insensitve.
-	if _, err := os.Stat(swapCase(os.Args[0])); os.IsNotExist(err) {
-		return false
+	swapped := swapCase(exe)
+	if _, err := os.Stat(swapped); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		panic(fmt.Sprintf("vfs: failed to stat %q: %v", swapped, err))
 	}
 	return true
-})
+}()
 
 // Convert all lowercase chars to uppercase, and vice-versa
 func swapCase(str string) string {
@@ -52,7 +65,7 @@ func swapCase(str string) string {
 }
 
 func (vfs *osFS) UseCaseSensitiveFileNames() bool {
-	return isFileSystemCaseSensitive()
+	return isFileSystemCaseSensitive
 }
 
 var osReadSema = make(chan struct{}, 128)
