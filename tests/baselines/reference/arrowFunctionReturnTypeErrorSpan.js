f6c6b4de33a7ee9b14b928165e7a832eedb9fa05
Oliver Joseph Ash <oliverjash@gmail.com>
2024-12-17 20:51:21 +0000

    Add tests

---
+++ tests/baselines/reference/arrowFunctionReturnTypeErrorSpan.js
@@ -0,0 +1,33 @@
+//// [tests/cases/compiler/arrowFunctionReturnTypeErrorSpan.ts] ////
+
+//// [arrowFunctionReturnTypeErrorSpan.ts]
+// block body
+const a = (): number => {
+  return "foo";
+};
+
+const a = (): number => {
+  return missing;
+};
+
+// expression body
+const b = (): number => "foo";
+
+type F<T> = T;
+const c = (): F<number> => "foo";
+
+const d = (): number => missing;
+
+
+//// [arrowFunctionReturnTypeErrorSpan.js]
+// block body
+var a = function () {
+    return "foo";
+};
+var a = function () {
+    return missing;
+};
+// expression body
+var b = function () { return "foo"; };
+var c = function () { return "foo"; };
+var d = function () { return missing; };
