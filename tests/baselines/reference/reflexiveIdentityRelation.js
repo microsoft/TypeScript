7b7a9b2ca5146b3dc4dfdf13267c3bd3a9ca2fbf
MichaelMitchell-at <=>
2024-12-29 00:41:37 -0500

    Make identity comparison reflexive across unions or intersections of the same type

---
+++ tests/baselines/reference/reflexiveIdentityRelation.js
@@ -0,0 +1,23 @@
+//// [tests/cases/compiler/reflexiveIdentityRelation.ts] ////
+
+//// [reflexiveIdentityRelation.ts]
+namespace reflexiveIdentityRelation {
+    type Equals<A, B> = (<T>() => T extends B ? 1 : 0) extends (<T>() => T extends A ? 1 : 0) ? true : false;
+
+    type Intersection = Equals<{a: 1} & {a: 1}, {a: 1}>;  // true
+    type Union = Equals<{a: 1} | {a: 1}, {a: 1}>;  // true
+    type UnionOfIntersection = Equals<{a: 1} & {b: 2} | {a: 1} & {b: 2}, {a: 1} & {b: 2}>;  // true
+
+    // The intersection distributes to `{a: 1} & {a: 1} | {a: 1} & {b: 2} | {b: 2} & {a: 1} | {b: 2} & {b: 2}`
+    // which is not identical to `{a: 1} | {b: 2}`
+    type IntersectionOfUnion = Equals<({a: 1} | {b: 2}) & ({a: 1} | {b: 2}), {a: 1} | {b: 2}>;  // false
+}
+
+
+//// [reflexiveIdentityRelation.js]
+"use strict";
+
+
+//// [reflexiveIdentityRelation.d.ts]
+declare namespace reflexiveIdentityRelation {
+}
