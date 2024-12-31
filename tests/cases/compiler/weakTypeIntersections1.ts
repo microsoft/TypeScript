0b454af177071402294f1baa8ca6d8024ce7cdd4
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-12-31 13:49:30 +0100

    add extra test case

--- tests/cases/compiler/weakTypeIntersections1.ts
+++ tests/cases/compiler/weakTypeIntersections1.ts
@@ -7,11 +7,15 @@ interface EmptyInterface2 {}
 interface Foo {
   a: string;
 }
+interface Bar {
+  b: string;
+}
 
 const m1: Partial<Foo> & ThisType<{ x: string }> = () => null; // error
 const m2: Partial<Foo> = () => null; // error
 const m3: Partial<Foo> & EmptyInterface = () => null; // error
 const m4: EmptyInterface & EmptyInterface2 = () => null; // ok
+const m5: Partial<Foo> & Partial<Bar> = () => null; // error
 
 // https://github.com/microsoft/TypeScript/issues/56995
 declare function fun0(arg: () => Foo & ThisType<Foo>): void;
