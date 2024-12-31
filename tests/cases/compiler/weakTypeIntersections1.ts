589e9e2f391f687b3962b77b301144b2f334957f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-12-31 13:42:02 +0100

    Improve how intersections are classified as weak types

---
+++ tests/cases/compiler/weakTypeIntersections1.ts
@@ -0,0 +1,22 @@
+// @strict: true
+// @noEmit: true
+
+interface EmptyInterface {}
+interface EmptyInterface2 {}
+
+interface Foo {
+  a: string;
+}
+
+const m1: Partial<Foo> & ThisType<{ x: string }> = () => null; // error
+const m2: Partial<Foo> = () => null; // error
+const m3: Partial<Foo> & EmptyInterface = () => null; // error
+const m4: EmptyInterface & EmptyInterface2 = () => null; // ok
+
+// https://github.com/microsoft/TypeScript/issues/56995
+declare function fun0(arg: () => Foo & ThisType<Foo>): void;
+declare function fun0(arg: Partial<Foo> & ThisType<Foo>): void;
+
+fun0({ a: "1" }); // ok
+fun0(() => ({ a: "1" })); // ok
+fun0(() => ({ a: 1 })); // error
