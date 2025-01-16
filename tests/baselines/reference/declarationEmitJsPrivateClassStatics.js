dcfd2676db8d44fab5caae2ec4e326d5135a652e
Wesley Wigham <wewigham@microsoft.com>
2025-01-15 16:05:57 -0800

    Filter #private static members from js declaration emit

---
+++ tests/baselines/reference/declarationEmitJsPrivateClassStatics.js
@@ -0,0 +1,20 @@
+//// [tests/cases/compiler/declarationEmitJsPrivateClassStatics.ts] ////
+
+//// [input.js]
+class Test {
+    static #privateStaticProp = 'Not removed for JS declaration generation; is removed for TS.';
+
+    static #privateStaticFn() { return 'Not removed for JS declaration generation; is removed for TS.'; }
+
+    #privateProp = 'This is removed in JS / TS declaration generation.';
+
+    #privateFn() { return 'This is removed in JS / TS declaration generation.'; }
+}
+
+
+
+
+//// [input.d.ts]
+declare class Test {
+    #private;
+}
