8ee35e5a57dd80856b7a5d7299857ef25adcff13
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2025-01-21 21:37:34 -0800

    Test + baseline

---
+++ tests/cases/compiler/erasableSyntaxOnly.ts
@@ -0,0 +1,50 @@
+// @erasableSyntaxOnly: true
+
+class MyClassErr {
+    // No parameter properties
+    constructor(public foo: string) { }
+}
+
+namespace IllegalBecauseInstantiated {
+    export const m = 1;
+}
+
+namespace AlsoIllegalBecauseInstantiated {
+    class PrivateClass {
+
+    }
+}
+
+enum NotLegalEnum {
+    B = 1
+}
+
+const enum NotLegalConstEnum {
+    C = 2
+}
+
+// No errors after this point
+class MyClassOk {
+    // Not a parameter property, ok
+    constructor(foo: string) { }
+}
+namespace NotInstantiated {
+    export interface JustAType { }
+    export type ATypeInANamespace = {};
+}
+declare namespace AmbientIsNotInstantiated {
+    export const stillOk = 12;
+}
+
+declare enum LegalEnum {
+    A = 1
+}
+
+declare namespace AmbientStuff {
+    namespace Nested {
+        export const stillOk = 12;
+    }
+    enum EnumInAmbientContext {
+        B = 1
+    }
+}
