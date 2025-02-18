27d4fe8f7f90383c2c4e118ce2466cfa69ba9e9e
Ben Lickly <blickly@google.com>
2025-02-18 15:28:53 -0800

    Minimial change to fix bug

--- src/services/codefixes/helpers.ts
+++ src/services/codefixes/helpers.ts
@@ -621,6 +621,13 @@ function endOfRequiredTypeParameters(checker: TypeChecker, type: GenericType): n
     const fullTypeArguments = type.typeArguments;
     const target = type.target;
     for (let cutoff = 0; cutoff < fullTypeArguments.length; cutoff++) {
+        const firstTypeToDrop = fullTypeArguments[cutoff];
+        // Our trick for figuring out if generics have unnecessary defaults by filling in
+        // missing type arguments incorrectly guesses that trailing unknowns are unnneceesary.
+        // Simply bail on trailing unknowns to avoid this case.
+        if ((firstTypeToDrop.getFlags() | TypeFlags.Unknown) === TypeFlags.Unknown) {
+            continue;
+        }
         const typeArguments = fullTypeArguments.slice(0, cutoff);
         const filledIn = checker.fillMissingTypeArguments(typeArguments, target.typeParameters, cutoff, /*isJavaScriptImplicitAny*/ false);
         if (filledIn.every((fill, i) => fill === fullTypeArguments[i])) {
