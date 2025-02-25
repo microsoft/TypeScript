ee9a6fe3c992f5d146e2d6e7514b2b5f1d747694
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-02-24 16:00:43 -0800

    Reimplement diagnosticPullOptions match function for unopened files (microsoft/typescript-go#378)

--- tsc/_extension/src/extension.ts
+++ tsc/_extension/src/extension.ts
@@ -4,7 +4,9 @@ import * as vscode from "vscode";
 import {
     LanguageClient,
     LanguageClientOptions,
+    NotebookDocumentFilter,
     ServerOptions,
+    TextDocumentFilter,
     TransportKind,
 } from "vscode-languageclient/node";
 
@@ -55,11 +57,45 @@ export function activate(context: vscode.ExtensionContext) {
             onSave: true,
             onTabs: true,
             match(documentSelector, resource) {
-                const document = vscode.workspace.textDocuments.find(doc => doc.uri.toString() === resource.toString());
-                if (!document) {
-                    return false;
+                // This function is called when diagnostics are requested but
+                // only the URI itself is known (e.g. open but not yet focused tabs),
+                // so will not be present in vscode.workspace.textDocuments.
+                // See if this file matches without consulting vscode.languages.match
+                // (which requires a TextDocument).
+
+                const language = getLanguageForUri(resource);
+
+                for (const selector of documentSelector) {
+                    if (typeof selector === "string") {
+                        if (selector === language) {
+                            return true;
+                        }
+                        continue;
+                    }
+                    if (NotebookDocumentFilter.is(selector)) {
+                        continue;
+                    }
+                    if (TextDocumentFilter.is(selector)) {
+                        if (selector.language !== undefined && selector.language !== language) {
+                            continue;
+                        }
+
+                        if (selector.scheme !== undefined && selector.scheme !== resource.scheme) {
+                            continue;
+                        }
+
+                        if (selector.pattern !== undefined) {
+                            // VS Code's glob matcher is not available via the API;
+                            // see: https://github.com/microsoft/vscode/issues/237304
+                            // But, we're only called on selectors passed above, so just ignore this for now.
+                            throw new Error("Not implemented");
+                        }
+
+                        return true;
+                    }
                 }
-                return vscode.languages.match(documentSelector, document) > 0;
+
+                return false;
             },
         },
     };
@@ -80,4 +116,24 @@ export function deactivate(): Thenable<void> | undefined {
         return undefined;
     }
     return client.stop();
+}
+
+function getLanguageForUri(uri: vscode.Uri): string | undefined {
+    const ext = path.posix.extname(uri.path);
+    switch (ext) {
+        case ".ts":
+        case ".mts":
+        case ".cts":
+            return "typescript";
+        case ".js":
+        case ".mjs":
+        case ".cjs":
+            return "javascript";
+        case ".tsx":
+            return "typescriptreact";
+        case ".jsx":
+            return "javascriptreact";
+        default:
+            return undefined;
+    }
 }
