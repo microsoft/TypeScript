d219f3974a1e5a6ababb9d33a2bf0f64d509e811
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-03-10 13:59:25 -0700

    Error in tsgo on positional args (microsoft/typescript-go#418)
    Co-authored-by: Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>

--- tsc/cmd/tsgo/main.go
+++ tsc/cmd/tsgo/main.go
@@ -112,6 +112,11 @@ func parseArgs() *cliOptions {
 	flag.StringVar(&opts.devel.pprofDir, "pprofDir", "", "Generate pprof CPU/memory profiles to the given directory.")
 	flag.Parse()
 
+	if len(flag.Args()) > 0 {
+		fmt.Fprintf(os.Stderr, "Unknown positional arguments %v. Current compiler is not identical to tsc but can be partially emulated by running:\n\ntsgo tsc <args>\n", flag.Args())
+		os.Exit(1)
+	}
+
 	return opts
 }
 
