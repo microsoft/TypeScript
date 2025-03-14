bce1491fee057f01cef13658adabdfbc9354f7d7
Kangkona D.Han <kangkona@users.noreply.github.com>
2025-03-15 01:46:03 +0800

    enhance binary search (microsoft/typescript-go#603)
    Co-authored-by: hanyunfei <hanyunfei@bytedance.com>

--- tsc/internal/core/binarysearch.go
+++ tsc/internal/core/binarysearch.go
@@ -7,6 +7,9 @@ package core
 // compared, instead of the target element.
 func BinarySearchUniqueFunc[S ~[]E, E any](x S, cmp func(int, E) int) (int, bool) {
 	n := len(x)
+	if n == 0 {
+		return 0, false
+	}
 	low, high := 0, n-1
 	for low <= high {
 		middle := low + ((high - low) >> 1)
