36d187d35bde3daeb1e6bc105cb79c864e74af05
Segev Finer <segev@swimm.io>
2025-03-19 19:26:40 +0200

    Add types for `String.{matchAll,replaceAll}` with a well known symbol

--- src/lib/es2021.d.ts
+++ src/lib/es2021.d.ts
@@ -1,5 +1,6 @@
 /// <reference lib="es2020" />
 /// <reference lib="es2021.promise" />
 /// <reference lib="es2021.string" />
+/// <reference lib="es2021.symbol.wellknown" />
 /// <reference lib="es2021.weakref" />
 /// <reference lib="es2021.intl" />
