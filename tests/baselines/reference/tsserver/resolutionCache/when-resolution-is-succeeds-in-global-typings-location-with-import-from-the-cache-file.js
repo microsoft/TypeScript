1ae47bd37ffae2bbdbeca38d1360ede85092b39a
Segev Finer <segev@swimm.io>
2025-04-03 02:25:42 +0300

    Fix tests

--- tests/baselines/reference/tsserver/resolutionCache/when-resolution-is-succeeds-in-global-typings-location-with-import-from-the-cache-file.js
+++ tests/baselines/reference/tsserver/resolutionCache/when-resolution-is-succeeds-in-global-typings-location-with-import-from-the-cache-file.js
@@ -27,10 +27,10 @@ interface Array<T> { length: number; [n: number]: T; }
 interface ReadonlyArray<T> {}
 declare const console: { log(msg: any): void; };
 
-//// [/home/src/Library/Caches/typescript/package.json] Inode:: 116
+//// [/home/src/Library/Caches/typescript/package.json] Inode:: 117
 { "private": true }
 
-//// [/home/src/Library/Caches/typescript/node_modules/types-registry/index.json] Inode:: 118
+//// [/home/src/Library/Caches/typescript/node_modules/types-registry/index.json] Inode:: 119
 {
   "entries": {}
 }
@@ -211,7 +211,7 @@ PolledWatches::
 
 FsWatches::
 /home/src/Library/Caches/typescript/package.json: *new*
-  {"inode":116}
+  {"inode":117}
 /home/src/Vscode: *new*
   {"inode":12}
 /home/src/Vscode/Projects: *new*
@@ -407,7 +407,7 @@ PolledWatches::
 
 FsWatches::
 /home/src/Library/Caches/typescript/package.json:
-  {"inode":116}
+  {"inode":117}
 /home/src/Vscode:
   {"inode":12}
 /home/src/Vscode/Projects:
