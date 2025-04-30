47a68eff459d2116f0b632bde8f476f7612de73c
Joseph Martre <martre.joseph@gmail.com>
2025-04-30 14:39:49 +0200

    Mirror requested JSDoc change on second matching line

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -1156,7 +1156,7 @@ interface JSON {
      * @param value A JavaScript value, usually an object or array, to be converted.
      * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
-     * @throws {TypeError} If a circular reference is found or a BigInt value is encountered.
+     * @throws {TypeError} If a circular reference or a BigInt value is found.
      */
     stringify(value: any, replacer?: (number | string)[] | null, space?: string | number): string;
 }
