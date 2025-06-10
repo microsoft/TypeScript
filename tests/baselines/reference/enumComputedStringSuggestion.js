3aaa32dca1de08b85d6c445e935dc57f2e7bf981
yeomgahui <ykh4933@naver.com>
2025-06-10 14:01:07 +0900

    add suggestion diagnostic for enum computed string keys (#42468)

---
+++ tests/baselines/reference/enumComputedStringSuggestion.js
@@ -0,0 +1,13 @@
+//// [tests/cases/compiler/enumComputedStringSuggestion.ts] ////
+
+//// [enumComputedStringSuggestion.ts]
+enum Test {
+  ["Hello"] = 1,
+}
+
+
+//// [enumComputedStringSuggestion.js]
+var Test;
+(function (Test) {
+    Test[Test["Hello"] = 1] = "Hello";
+})(Test || (Test = {}));
---
+++ tests/cases/compiler/enumComputedStringSuggestion.ts
@@ -0,0 +1,3 @@
+enum Test {
+  ["Hello"] = 1,
+}
