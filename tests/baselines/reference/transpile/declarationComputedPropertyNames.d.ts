59f7dda87753634f4d53382601d199d230f136aa
Tristan Smith <tjlsmith0831@gmail.com>
2025-06-22 08:56:49 -0500

    Accept new baselines in transpile .d.ts files

--- tests/baselines/reference/transpile/declarationComputedPropertyNames.d.ts
+++ tests/baselines/reference/transpile/declarationComputedPropertyNames.d.ts
@@ -87,6 +87,7 @@ export interface B {
 }
 export declare class C {
     [x: number]: number;
+    [presentNs.a]: number;
     [Symbol.iterator]: number;
     [globalThis.Symbol.toStringTag]: number;
     [1]: number;
@@ -113,12 +114,18 @@ declarationComputedPropertyNames.ts(27,5): error TS9014: Computed properties mus
 declarationComputedPropertyNames.ts(31,5): error TS9014: Computed properties must be number or string literals, variables or dotted expressions with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(32,5): error TS9014: Computed properties must be number or string literals, variables or dotted expressions with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(36,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+declarationComputedPropertyNames.ts(36,6): error TS9013: Expression type can't be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(37,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
-declarationComputedPropertyNames.ts(38,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+declarationComputedPropertyNames.ts(37,6): error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+declarationComputedPropertyNames.ts(38,6): error TS9013: Expression type can't be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(41,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+declarationComputedPropertyNames.ts(41,6): error TS9013: Expression type can't be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(42,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+declarationComputedPropertyNames.ts(42,6): error TS9013: Expression type can't be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(45,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+declarationComputedPropertyNames.ts(45,6): error TS9013: Expression type can't be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(46,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+declarationComputedPropertyNames.ts(46,6): error TS9013: Expression type can't be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(50,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(51,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
 declarationComputedPropertyNames.ts(52,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
@@ -128,7 +135,7 @@ declarationComputedPropertyNames.ts(59,5): error TS9038: Computed property names
 declarationComputedPropertyNames.ts(60,5): error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
 
 
-==== declarationComputedPropertyNames.ts (22 errors) ====
+==== declarationComputedPropertyNames.ts (28 errors) ====
     export namespace presentNs {
         export const a = Symbol();
                      ~
@@ -185,28 +192,54 @@ declarationComputedPropertyNames.ts(60,5): error TS9038: Computed property names
         [missing]: number = 1;
         ~~~~~~~~~
 !!! error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+         ~~~~~~~
+!!! error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+!!! related TS9029 declarationComputedPropertyNames.ts:36:5: Add a type annotation to the property [missing].
+!!! related TS9035 declarationComputedPropertyNames.ts:36:6: Add satisfies and a type assertion to this expression (satisfies T as T) to make the type explicit.
         [ns.missing]: number = 1;
         ~~~~~~~~~~~~
 !!! error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+         ~~~~~~~~~~
+!!! error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+!!! related TS9029 declarationComputedPropertyNames.ts:37:5: Add a type annotation to the property [ns.missing].
+!!! related TS9035 declarationComputedPropertyNames.ts:37:6: Add satisfies and a type assertion to this expression (satisfies T as T) to make the type explicit.
         [presentNs.a]: number = 1;
-        ~~~~~~~~~~~~~
-!!! error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+         ~~~~~~~~~~~
+!!! error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+!!! related TS9029 declarationComputedPropertyNames.ts:38:5: Add a type annotation to the property [presentNs.a].
+!!! related TS9035 declarationComputedPropertyNames.ts:38:6: Add satisfies and a type assertion to this expression (satisfies T as T) to make the type explicit.
         [Symbol.iterator]: number = 1;
         [globalThis.Symbol.toStringTag]: number = 1;
         [(globalThis.Symbol).unscopables]: number = 1;
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !!! error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!!! error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+!!! related TS9029 declarationComputedPropertyNames.ts:41:5: Add a type annotation to the property [(globalThis.Symbol).unscopables].
+!!! related TS9035 declarationComputedPropertyNames.ts:41:6: Add satisfies and a type assertion to this expression (satisfies T as T) to make the type explicit.
         [aliasing.isConcatSpreadable]: number = 1;
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !!! error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+         ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!!! error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+!!! related TS9029 declarationComputedPropertyNames.ts:42:5: Add a type annotation to the property [aliasing.isConcatSpreadable].
+!!! related TS9035 declarationComputedPropertyNames.ts:42:6: Add satisfies and a type assertion to this expression (satisfies T as T) to make the type explicit.
         [1]: number = 1;
         ["2"]: number = 1;
         [(missing2)]: number = 1;
         ~~~~~~~~~~~~
 !!! error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+         ~~~~~~~~~~
+!!! error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+!!! related TS9029 declarationComputedPropertyNames.ts:45:5: Add a type annotation to the property [(missing2)].
+!!! related TS9035 declarationComputedPropertyNames.ts:45:6: Add satisfies and a type assertion to this expression (satisfies T as T) to make the type explicit.
         [Math.random() > 0.5 ? "f1" : "f2"]: number = 1;
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !!! error TS9038: Computed property names on class or object literals cannot be inferred with --isolatedDeclarations.
+         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!!! error TS9013: Expression type can't be inferred with --isolatedDeclarations.
+!!! related TS9029 declarationComputedPropertyNames.ts:46:5: Add a type annotation to the property [Math.random() > 0.5 ? "f1" : "f2"].
+!!! related TS9035 declarationComputedPropertyNames.ts:46:6: Add satisfies and a type assertion to this expression (satisfies T as T) to make the type explicit.
     }
     
     export const D = {
