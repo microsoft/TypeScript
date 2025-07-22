d89419e60f7cc9e7aa3d3125aa386747c68b724b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-07-22 10:20:45 +0200

    simplify further

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -9552,11 +9552,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                     symbol.flags & SymbolFlags.ExportDoesNotSupportDefaultModifier
                     || (symbol.flags & SymbolFlags.Function && length(getPropertiesOfType(getTypeOfSymbol(symbol))))
                 ) && !(symbol.flags & SymbolFlags.Alias); // An alias symbol should preclude needing to make an alias ourselves
-                let needsExportDeclaration = !needsPostExportDefault && !isPrivate && isStringANonContextualKeyword(symbolName) && !isDefault;
-                // `serializeVariableOrProperty` will handle adding the export declaration if it is run (since `getInternalSymbolName` will create the name mapping), so we need to ensuer we unset `needsExportDeclaration` if it is
-                if (needsPostExportDefault || needsExportDeclaration) {
-                    isPrivate = true;
-                }
+                isPrivate ||= needsPostExportDefault;
                 const modifierFlags = (!isPrivate ? ModifierFlags.Export : 0) | (isDefault && !needsPostExportDefault ? ModifierFlags.Default : 0);
                 const isConstMergedWithNS = symbol.flags & SymbolFlags.Module &&
                     symbol.flags & (SymbolFlags.BlockScopedVariable | SymbolFlags.FunctionScopedVariable | SymbolFlags.Property) &&
@@ -9581,7 +9577,6 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                     if (propertyAsAlias) {
                         const createdExport = serializeMaybeAliasAssignment(symbol);
                         if (createdExport) {
-                            needsExportDeclaration = false;
                             needsPostExportDefault = false;
                         }
                     }
@@ -9679,7 +9674,6 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
                                         ),
                                         ModifierFlags.None,
                                     );
-                                    needsExportDeclaration = false;
                                     needsPostExportDefault = false;
                                 }
                             }
