3f21e2e80d8b58e78e5490b42c48f483a2ab842b
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-07-31 09:47:19 -0700

    Add regression test

---
+++ tests/baselines/reference/inferencePromiseArrays1.js
@@ -0,0 +1,21 @@
+//// [tests/cases/compiler/inferencePromiseArrays1.ts] ////
+
+//// [inferencePromiseArrays1.ts]
+declare function getNum(): Promise<number>;
+declare function getStr(): Promise<string>;
+declare function useTuple(tuple: [number, string]): void;
+const p1 = Promise.resolve([]).then(() => Promise.all([getNum(), getStr()])).then(useTuple);
+
+const p2 = Promise.resolve([]).then(()=> {
+    return Promise.all([0, ""]);
+})
+const p3: Promise<[number, string]> = p2;
+
+
+//// [inferencePromiseArrays1.js]
+"use strict";
+const p1 = Promise.resolve([]).then(() => Promise.all([getNum(), getStr()])).then(useTuple);
+const p2 = Promise.resolve([]).then(() => {
+    return Promise.all([0, ""]);
+});
+const p3 = p2;
