f28291da25233d9feedabad9b0172f189b7fa36f
dontwanttothink <barquean.09excavacion@icloud.com>
2025-10-03 11:39:05 -0500

    Allow passing `undefined` wherever appropriate

--- src/lib/esnext.typedarrays.d.ts
+++ src/lib/esnext.typedarrays.d.ts
@@ -4,10 +4,12 @@ interface Uint8Array<TArrayBuffer extends ArrayBufferLike> {
      * @param options If provided, sets the alphabet and padding behavior used.
      * @returns A base64-encoded string.
      */
-    toBase64(options?: {
-        alphabet?: "base64" | "base64url";
-        omitPadding?: boolean;
-    }): string;
+    toBase64(
+        options?: {
+            alphabet?: "base64" | "base64url" | undefined;
+            omitPadding?: boolean | undefined;
+        } | undefined,
+    ): string;
 
     /**
      * Sets the `Uint8Array` from a base64-encoded string.
@@ -17,10 +19,13 @@ interface Uint8Array<TArrayBuffer extends ArrayBufferLike> {
      * @throws {SyntaxError} If the input string contains characters outside the specified alphabet, or if the last
      * chunk is inconsistent with the `lastChunkHandling` option.
      */
-    setFromBase64(string: string, options?: {
-        alphabet?: "base64" | "base64url";
-        lastChunkHandling?: "loose" | "strict" | "stop-before-partial";
-    }): {
+    setFromBase64(
+        string: string,
+        options?: {
+            alphabet?: "base64" | "base64url" | undefined;
+            lastChunkHandling?: "loose" | "strict" | "stop-before-partial" | undefined;
+        } | undefined,
+    ): {
         read: number;
         written: number;
     };
@@ -54,9 +59,9 @@ interface Uint8ArrayConstructor {
     fromBase64(
         string: string,
         options?: {
-            alphabet?: "base64" | "base64url";
-            lastChunkHandling?: "loose" | "strict" | "stop-before-partial";
-        },
+            alphabet?: "base64" | "base64url" | undefined;
+            lastChunkHandling?: "loose" | "strict" | "stop-before-partial" | undefined;
+        } | undefined,
     ): Uint8Array<ArrayBuffer>;
 
     /**
