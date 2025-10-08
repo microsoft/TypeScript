5ed42ae95665d9068b4877e99f7aea722adec15d
Andrew Branch <andrew@wheream.io>
2025-10-08 12:49:05 -0700

    Deprecate esModuleInterop=false and allowSyntheticDefaultImports=false

--- tests/baselines/reference/tscWatch/incremental/editing-module-augmentation-incremental.js
+++ tests/baselines/reference/tscWatch/incremental/editing-module-augmentation-incremental.js
@@ -38,8 +38,11 @@ Output::
 
 //// [/users/username/projects/project/src/index.js]
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var classnames_1 = require("classnames");
+var classnames_1 = __importDefault(require("classnames"));
 (0, classnames_1.default)().foo;
 
 
