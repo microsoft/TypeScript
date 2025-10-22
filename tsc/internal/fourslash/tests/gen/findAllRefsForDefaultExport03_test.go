aec083766a402f67934f8075e092e5dcf565c508
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-10-22 11:41:05 -0700

    Unskip passing fourslash test (microsoft/typescript-go#1922)

--- tsc/internal/fourslash/tests/gen/findAllRefsForDefaultExport03_test.go
+++ tsc/internal/fourslash/tests/gen/findAllRefsForDefaultExport03_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestFindAllRefsForDefaultExport03(t *testing.T) {
 	t.Parallel()
-	t.Skip()
+
 	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
 	const content = `/*1*/function /*2*/f() {
     return 100;
