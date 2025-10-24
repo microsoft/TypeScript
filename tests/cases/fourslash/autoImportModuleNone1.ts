6436e88b626a9f0f27c2246201b999f018c26253
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-10-24 12:59:02 -0700

    Modify fourslash test

--- tests/cases/fourslash/autoImportModuleNone1.ts
+++ tests/cases/fourslash/autoImportModuleNone1.ts
@@ -19,8 +19,4 @@ verify.completions({
 });
 
 edit.replaceLine(0, "import { x } from 'dep'; x;");
-verify.getSemanticDiagnostics([{
-  range: { fileName: "/index.ts", pos: 0, end: "import { x } from 'dep';".length },
-  code: ts.Diagnostics.Cannot_use_imports_exports_or_module_augmentations_when_module_is_none.code,
-  message: ts.Diagnostics.Cannot_use_imports_exports_or_module_augmentations_when_module_is_none.message
-}]);
+verify.getSemanticDiagnostics([]);
