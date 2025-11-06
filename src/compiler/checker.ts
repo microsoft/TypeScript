2cbd27b6750a88e050e18dd3520dfed1c02c1133
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-11-06 09:40:13 -0800

    Always set up host in node builder

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -6560,9 +6560,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
             cb: (context: NodeBuilderContext) => T,
             out?: WriterContextOut,
         ): T | undefined {
-            const moduleResolverHost = tracker?.trackSymbol ? tracker.moduleResolverHost :
-                (internalFlags || InternalNodeBuilderFlags.None) & InternalNodeBuilderFlags.DoNotIncludeSymbolChain ? createBasicNodeBuilderModuleSpecifierResolutionHost(host) :
-                undefined;
+            const moduleResolverHost = tracker?.moduleResolverHost ?? createBasicNodeBuilderModuleSpecifierResolutionHost(host);
             flags = flags || NodeBuilderFlags.None;
             const maxTruncationLength = maximumLength ||
                 (flags & NodeBuilderFlags.NoTruncation ? noTruncationMaximumTruncationLength : defaultMaximumTruncationLength);
