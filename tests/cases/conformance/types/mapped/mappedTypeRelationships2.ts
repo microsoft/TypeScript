f68bcc336dd56861f3bc1a047ed57a210f66264a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-11-17 13:06:28 +0100

    add test

--- tests/cases/conformance/types/mapped/mappedTypeRelationships2.ts
+++ tests/cases/conformance/types/mapped/mappedTypeRelationships2.ts
@@ -33,4 +33,56 @@ type Alias5<T extends object, U extends Partial<WithNumber<Record<keyof T, any>>
 type Test9 = Alias5<AB, ABC>; // ok
 type Test10<T extends AB> = Alias5<T, ABC>; // ok
 
+// part of https://github.com/microsoft/TypeScript/issues/62770
+
+interface WithCommon {
+  field1: number;
+  field2: string;
+}
+
+interface WithSpecial extends WithCommon {
+  special: number;
+}
+
+interface SpecialLookup {
+  special: WithSpecial;
+}
+
+interface CommonLookup extends SpecialLookup {
+  common: WithCommon;
+}
+
+type Lookup<Name extends keyof CommonLookup> = {
+  [K in keyof CommonLookup[Name]]?: CommonLookup[Name][K];
+};
+function someName<K extends keyof SpecialLookup>(): void {
+  // ok
+  const works1: Lookup<"special"> = {
+    field1: 1,
+    field2: "common",
+    special: 203,
+  };
+
+  // error
+  const works2: Lookup<"common"> = {
+    field1: 5,
+    field2: "something",
+    special: 32, 
+  };
+
+  // ok
+  const works3: Lookup<K> = {
+    special: undefined,
+    field1: undefined,
+    field2: undefined,
+  };
+
+  // error
+  const works4: Lookup<K> = {
+    field1: undefined,
+    field2: undefined,
+    special: 32,
+  };
+}
+
 export {};
