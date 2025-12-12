d7fdf8eec46e7b14f45b7344a5ee13ef2b6f95d8
Sheetal Nandi <shkamat@microsoft.com>
2025-12-12 15:42:29 -0800

    Format

--- src/server/project.ts
+++ src/server/project.ts
@@ -1530,10 +1530,12 @@ export abstract class Project implements LanguageServiceHost, ModuleResolutionHo
 
     /** @internal */
     watchTypingLocations(files: readonly string[] | undefined): void {
-        // Skip watching typing locations for inferred project whose currentDirectory is not watchable or 
+        // Skip watching typing locations for inferred project whose currentDirectory is not watchable or
         // is same as server's current directory
-        if (this.currentDirectory === this.projectService.currentDirectory ||
-            !canWatchDirectoryOrFilePath(this.toPath(this.currentDirectory))) {
+        if (
+            this.currentDirectory === this.projectService.currentDirectory ||
+            !canWatchDirectoryOrFilePath(this.toPath(this.currentDirectory))
+        ) {
             return;
         }
 
