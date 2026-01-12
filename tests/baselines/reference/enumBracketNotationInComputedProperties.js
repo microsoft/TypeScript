b3eafa01b4fd478393bf93fb6e338ebfd32ae266
Rodrigo Sanchez <Rodrigo@sanchezdev.com>
2026-01-12 13:35:53 -0700

    Add test baselines for enumBracketNotationInComputedProperties
    Update reference baselines for the new test case that demonstrates
    bracket notation working correctly with enum keys in computed properties.
    
    Signed-off-by: Rodrigo Sanchez <Rodrigo@sanchezdev.com>

---
+++ tests/baselines/reference/enumBracketNotationInComputedProperties.js
@@ -0,0 +1,133 @@
+//// [tests/cases/compiler/enumBracketNotationInComputedProperties.ts] ////
+
+//// [enumBracketNotationInComputedProperties.ts]
+// Test for issue #25083: Enum keys should be accepted as computed properties
+// when using bracket notation
+
+enum Type {
+    Foo = 'foo',
+    Bar = 'bar',
+    '3x14' = '3x14', // Non-identifier name
+    '0digit' = '0digit' // Starts with digit
+}
+
+// These should all work - dot notation (already works)
+type TypeMapDot = {
+    [Type.Foo]: string;
+    [Type.Bar]: number;
+}
+
+// These should also work - bracket notation with identifier names
+type TypeMapBracketIdentifier = {
+    [Type['Foo']]: string;
+    [Type['Bar']]: number;
+}
+
+// These should work - bracket notation with non-identifier names
+// This is the main fix for #25083
+type TypeMapBracketNonIdentifier = {
+    [Type['3x14']]: boolean;
+    [Type['0digit']]: object;
+}
+
+// Mixed usage should also work
+type TypeMapMixed = {
+    [Type.Foo]: string;
+    [Type['Bar']]: number;
+    [Type['3x14']]: boolean;
+}
+
+// Should work with const enums too
+const enum ConstType {
+    Alpha = 'alpha',
+    '2beta' = '2beta'
+}
+
+type ConstTypeMap = {
+    [ConstType.Alpha]: string;
+    [ConstType['Alpha']]: string;
+    [ConstType['2beta']]: number;
+}
+
+// Verify that the types work correctly at runtime level
+const dotNotation: TypeMapDot = {
+    [Type.Foo]: 'test',
+    [Type.Bar]: 123
+};
+
+const bracketNotation: TypeMapBracketIdentifier = {
+    [Type['Foo']]: 'test',
+    [Type['Bar']]: 456
+};
+
+const nonIdentifierBracket: TypeMapBracketNonIdentifier = {
+    [Type['3x14']]: true,
+    [Type['0digit']]: {}
+};
+
+const mixed: TypeMapMixed = {
+    [Type.Foo]: 'test',
+    [Type['Bar']]: 789,
+    [Type['3x14']]: false
+};
+
+
+//// [enumBracketNotationInComputedProperties.js]
+"use strict";
+// Test for issue #25083: Enum keys should be accepted as computed properties
+// when using bracket notation
+var _a, _b, _c, _d;
+var Type;
+(function (Type) {
+    Type["Foo"] = "foo";
+    Type["Bar"] = "bar";
+    Type["3x14"] = "3x14";
+    Type["0digit"] = "0digit"; // Starts with digit
+})(Type || (Type = {}));
+// Verify that the types work correctly at runtime level
+var dotNotation = (_a = {},
+    _a[Type.Foo] = 'test',
+    _a[Type.Bar] = 123,
+    _a);
+var bracketNotation = (_b = {},
+    _b[Type['Foo']] = 'test',
+    _b[Type['Bar']] = 456,
+    _b);
+var nonIdentifierBracket = (_c = {},
+    _c[Type['3x14']] = true,
+    _c[Type['0digit']] = {},
+    _c);
+var mixed = (_d = {},
+    _d[Type.Foo] = 'test',
+    _d[Type['Bar']] = 789,
+    _d[Type['3x14']] = false,
+    _d);
+
+
+//// [enumBracketNotationInComputedProperties.d.ts]
+declare enum Type {
+    Foo = "foo",
+    Bar = "bar",
+    '3x14' = "3x14",// Non-identifier name
+    '0digit' = "0digit"
+}
+type TypeMapDot = {
+    [Type.Foo]: string;
+    [Type.Bar]: number;
+};
+type TypeMapBracketIdentifier = {};
+type TypeMapBracketNonIdentifier = {};
+type TypeMapMixed = {
+    [Type.Foo]: string;
+};
+declare const enum ConstType {
+    Alpha = "alpha",
+    '2beta' = "2beta"
+}
+type ConstTypeMap = {
+    [ConstType.Alpha]: string;
+};
+declare const dotNotation: TypeMapDot;
+declare const bracketNotation: TypeMapBracketIdentifier;
+declare const nonIdentifierBracket: TypeMapBracketNonIdentifier;
+declare const mixed: TypeMapMixed;
