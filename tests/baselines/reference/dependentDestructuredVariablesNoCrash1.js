dbf025bea887fdaa433c2db0d4e67cd2a184746b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-02-04 21:45:32 +0100

    add test

--- tests/baselines/reference/dependentDestructuredVariablesNoCrash1.js
+++ tests/baselines/reference/dependentDestructuredVariablesNoCrash1.js
@@ -11,9 +11,7 @@ function f([first, undefined]?: () => any = undefined) {}
 //// [dependentDestructuredVariablesNoCrash1.js]
 "use strict";
 // https://github.com/microsoft/TypeScript/issues/63044
-function f(_a) {
-    var _b = _a === void 0 ? undefined : _a, first = _b[0], undefined = _b[1];
-}
+function f([first, undefined] = undefined) { }
 
 
 //// [dependentDestructuredVariablesNoCrash1.d.ts]
