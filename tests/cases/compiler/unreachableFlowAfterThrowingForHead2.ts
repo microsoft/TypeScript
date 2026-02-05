76f2f2fdc52bf1a4dd9f27cc85bac1745d5d030c
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-02-05 21:11:22 +0100

    add test

--- tests/cases/compiler/unreachableFlowAfterThrowingForHead2.ts
+++ tests/cases/compiler/unreachableFlowAfterThrowingForHead2.ts
@@ -3,6 +3,18 @@
 // @allowUnreachableCode: true, false
 
 function test1(v: 0 | 1 | 2) {
+  for (
+    (function () {
+      throw new Error("");
+    })();
+    v;
+    v++
+  ) {
+    console.log("1");
+  }
+}
+
+function test2(v: 0 | 1 | 2) {
   try {
     for (
       (function () {
