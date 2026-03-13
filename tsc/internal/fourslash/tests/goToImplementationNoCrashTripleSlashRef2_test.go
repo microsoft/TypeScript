9fbee55ed8416f572304f7da038110f792aa88f2
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-03-13 21:14:30 +0100

    Fixes a crash when requesting implementations for a module referenced by a tripleslash directive (microsoft/typescript-go#3024)

---
+++ tsc/internal/fourslash/tests/goToImplementationNoCrashTripleSlashRef2_test.go
@@ -0,0 +1,25 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/microsoft/typescript-go/internal/fourslash"
+	"github.com/microsoft/typescript-go/internal/testutil"
+)
+
+func TestGoToImplementationNoCrashTripleSlashRef2(t *testing.T) {
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `// @Filename: /node_modules/@types/react/index.d.ts
+export type JSX = {};
+
+// @Filename: /node_modules/excalidraw/index.d.ts
+/// <reference types="react" />
+
+// @Filename: /index.ts
+import type {JSX} from '/*m*/react';
+`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.VerifyBaselineGoToImplementation(t, "m")
+}
--- tsc/internal/ls/findallreferences.go
+++ tsc/internal/ls/findallreferences.go
@@ -621,7 +621,7 @@ func (l *LanguageService) provideSymbolsAndEntries(ctx context.Context, uri lspr
 
 		entry := queue[0]
 		queue = queue[1:]
-		if !seenNodes.Has(entry.node) {
+		if entry.node != nil && !seenNodes.Has(entry.node) {
 			seenNodes.Add(entry.node)
 			addToQueue(l.getSymbolAndEntries(ctx, entry.node.Pos(), entry.node, program, isRename, implementations))
 		}
