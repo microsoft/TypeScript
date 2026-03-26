cd3b96f4024cffcbb9f2d3e5ca2bead824e47942
Kanashimia <chad@redpilled.dev>
2026-03-26 21:51:51 +0200

    Use process.execve where it is supported (microsoft/typescript-go#3241)

--- tsc/_packages/native-preview/bin/tsgo.js
+++ tsc/_packages/native-preview/bin/tsgo.js
@@ -5,6 +5,16 @@ import { execFileSync } from "node:child_process";
 
 const exe = getExePath();
 
+if (typeof process.execve === "function") {
+    // > v22.15.0
+    try {
+        process.execve(exe, [exe, ...process.argv.slice(2)]);
+    }
+    catch {
+        // not available on windows, ignore the error and fallback
+    }
+}
+
 try {
     execFileSync(exe, process.argv.slice(2), { stdio: "inherit" });
 }
