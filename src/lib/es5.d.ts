5b27d43a8da897f5d2ee9aa147a8b92ce61028dc
bwalter007 <boulder.holt_7a@icloud.com>
2026-04-11 15:05:45 +0300

    Document substring edge case behavior

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -473,7 +473,7 @@ interface String {
     split(separator: string | RegExp, limit?: number): string[];
 
     /**
-     * Returns the substring at the specified location within a String object.
+     * Returns the substring at the specified location within a String object. Negative values are treated as 0.
      * @param start The zero-based index number indicating the beginning of the substring.
      * @param end Zero-based index number indicating the end of the substring. The substring includes the characters up to, but not including, the character indicated by end.
      * If end is omitted, the characters from start through the end of the original string are returned.
