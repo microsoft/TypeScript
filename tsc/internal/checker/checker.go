0a0d73cc7872bd5940bfc2d5c0add237b4b6bb9d
Anders Hejlsberg <andersh@microsoft.com>
2026-06-06 18:20:28 -0700

    Obtain late-bound symbol in error reporting (microsoft/typescript-go#4233)

--- tsc/internal/checker/checker.go
+++ tsc/internal/checker/checker.go
@@ -4475,8 +4475,7 @@ func (c *Checker) issueMemberSpecificError(node *ast.Node, typeWithThis *Type, b
 		if ast.IsStatic(member) {
 			continue
 		}
-		declaredProp := member.Symbol()
-		if declaredProp != nil && declaredProp.Name != ast.InternalSymbolNameComputed {
+		if declaredProp := c.getSymbolOfDeclaration(member); declaredProp != nil && declaredProp.Name != ast.InternalSymbolNameComputed {
 			prop := c.getPropertyOfType(typeWithThis, declaredProp.Name)
 			baseProp := c.getPropertyOfType(baseWithThis, declaredProp.Name)
 			if prop != nil && baseProp != nil {
