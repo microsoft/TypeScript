40e71d9722fd17d115f0ab577a1b724556a80dfe
liu-idf18 <reideromazeu@gmail.com>
2026-07-05 15:40:21 -0300

    Revert "Fix infinite loop (#63581)"
    This reverts commit 637d5746b70257028fb95aad32ddec6b26ab0a14.

--- src/testRunner/unittests/comments.ts
+++ src/testRunner/unittests/comments.ts
@@ -11,7 +11,6 @@ describe("comment parsing", () => {
     const withTrailing = `;/* comment */
 // another one
 `;
-    const endingInHyphen = "/**comment-*/";
     it("skips shebang", () => {
         const result = ts.getLeadingCommentRanges(withShebang, 0);
         assert.isDefined(result);
@@ -29,17 +28,5 @@ describe("comment parsing", () => {
         assert.isDefined(result);
         assert.strictEqual(result.length, 1);
         assert.strictEqual(result[0].kind, ts.SyntaxKind.SingleLineCommentTrivia);
-    });
-
-    it("parses /** block comments ending in hyphen", () => {
-        const sourceFile = ts.createSourceFile(
-            "file.ts",
-            `${endingInHyphen}\nconst x = 1;`,
-            ts.ScriptTarget.ESNext,
-            /*setParentNodes*/ true,
-        );
-
-        assert.strictEqual(sourceFile.parseDiagnostics.length, 0);
-        assert.strictEqual(sourceFile.statements.length, 1);
     });
 });
