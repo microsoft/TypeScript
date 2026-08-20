3bb8f5893656c07678ad11aefc6cafe31ff03728
Anders Hejlsberg <andersh@microsoft.com>
2026-08-20 13:19:56 -0700

    Fix getDeclarationModifierFlagsFromSymbolEx for synthetic properties

--- tsc/internal/checker/utilities.go
+++ tsc/internal/checker/utilities.go
@@ -715,6 +715,22 @@ func getDeclarationModifierFlagsFromSymbol(s *ast.Symbol) ast.ModifierFlags {
 }
 
 func getDeclarationModifierFlagsFromSymbolEx(s *ast.Symbol, isWrite bool) ast.ModifierFlags {
+	if s.CheckFlags&ast.CheckFlagsSynthetic != 0 {
+		var accessModifier ast.ModifierFlags
+		switch {
+		case s.CheckFlags&ast.CheckFlagsContainsPrivate != 0:
+			accessModifier = ast.ModifierFlagsPrivate
+		case s.CheckFlags&ast.CheckFlagsContainsPublic != 0:
+			accessModifier = ast.ModifierFlagsPublic
+		default:
+			accessModifier = ast.ModifierFlagsProtected
+		}
+		var staticModifier ast.ModifierFlags
+		if s.CheckFlags&ast.CheckFlagsContainsStatic != 0 {
+			staticModifier = ast.ModifierFlagsStatic
+		}
+		return accessModifier | staticModifier
+	}
 	if s.ValueDeclaration != nil {
 		var declaration *ast.Node
 		if isWrite {
@@ -731,22 +747,6 @@ func getDeclarationModifierFlagsFromSymbolEx(s *ast.Symbol, isWrite bool) ast.Mo
 			return flags
 		}
 		return flags & ^ast.ModifierFlagsAccessibilityModifier
-	}
-	if s.CheckFlags&ast.CheckFlagsSynthetic != 0 {
-		var accessModifier ast.ModifierFlags
-		switch {
-		case s.CheckFlags&ast.CheckFlagsContainsPrivate != 0:
-			accessModifier = ast.ModifierFlagsPrivate
-		case s.CheckFlags&ast.CheckFlagsContainsPublic != 0:
-			accessModifier = ast.ModifierFlagsPublic
-		default:
-			accessModifier = ast.ModifierFlagsProtected
-		}
-		var staticModifier ast.ModifierFlags
-		if s.CheckFlags&ast.CheckFlagsContainsStatic != 0 {
-			staticModifier = ast.ModifierFlagsStatic
-		}
-		return accessModifier | staticModifier
 	}
 	if s.Flags&ast.SymbolFlagsPrototype != 0 {
 		return ast.ModifierFlagsPublic | ast.ModifierFlagsStatic
