07ef369693efb3a444b0c53f2afce84face3cd54
Anders Hejlsberg <andersh@microsoft.com>
2026-08-22 13:45:05 -0700

    Add tests

--- tsc/testdata/tests/cases/compiler/syntheticProtectedProperties.ts
+++ tsc/testdata/tests/cases/compiler/syntheticProtectedProperties.ts
@@ -25,6 +25,50 @@ if (w instanceof Mock) {
 declare const w2: Mock & Public
 w2.content;
 
-
 declare const w3: Public & Mock
 w3.content;
+
+// private/protected set accessors are tracked in unions and intersections
+
+declare class C1 {
+    get foo(): number;
+    set foo(value: number);
+}
+
+declare class C2 {
+    get foo(): number;
+    protected set foo(value: number);
+}
+
+declare class C3 {
+    protected get foo(): number;
+    protected set foo(value: number);
+}
+
+// Unions properties have most restricted accessibility
+
+declare const cu12: C1 | C2;
+cu12.foo;
+cu12.foo = 123;  // Error
+
+declare const cu13: C1 | C3;
+cu13.foo;  // Error
+cu13.foo = 123;  // Error
+
+declare const cu22: C2 | C3;
+cu13.foo;  // Error
+cu13.foo = 123;  // Error
+
+// Intersection properties have most permissive accessibility
+
+declare const ci12: C1 & C2;
+ci12.foo;
+ci12.foo = 123;
+
+declare const ci13: C1 & C3;
+ci12.foo;
+ci12.foo = 123;
+
+declare const ci23: C2 & C3;
+ci23.foo;
+ci23.foo = 123;  // Error
