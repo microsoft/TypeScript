74271abb91c9295e821ae1030ef5f85d3b5db60e
Tom Mrazauskas <tom@mrazauskas.de>
2026-08-26 17:58:47 +0300

    Apply suggestions from code review
    Co-authored-by: Andrew Branch <andrewbranch@users.noreply.github.com>

--- packages/typescript/src/api/sync/api.ts
+++ packages/typescript/src/api/sync/api.ts
@@ -1750,7 +1750,16 @@ export class Checker {
         });
     }
 
-    isReadonlySymbol(symbol: Symbol): boolean {
+    /**
+     * The following symbols are considered read-only:
+     * - Properties with a `readonly` modifier
+     * - Variables declared with `const`
+     * - Get accessors without matching set accessors
+     * - Enum members
+     * - `Object.defineProperty` assignments with `writable: false` or no setter
+     * - Unions and intersections of the above
+     */
+    isReadonlySymbol(symbol: Symbol): boolean {
         return this.client.apiRequest("isReadonlySymbol", {
             snapshot: this.snapshotId,
             project: this.project.id,
