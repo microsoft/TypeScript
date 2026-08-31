211ae958f86eadb0194d7e69818da694ffdb22eb
Baris Arı <226740765+rapsoidmorningstar@users.noreply.github.com>
2026-09-01 02:35:43 +0300

    Add typed array constructor regression coverage

--- tsc/testdata/tests/cases/compiler/typedArrayConstructorOverloads.ts
+++ tsc/testdata/tests/cases/compiler/typedArrayConstructorOverloads.ts
@@ -22,3 +22,13 @@ export function makeTypedArray(buffer: ArrayBuffer, ctr: TypedArrayConstructor) 
     new ctr(buffer);
     new ctr(buffer, 0, 0);
 }
+
+// https://github.com/microsoft/TypeScript/issues/44191
+
+const uint8Array = new Uint8Array(4);
+// @ts-expect-error typed array inputs do not accept byteOffset and length
+new Uint8Array(uint8Array, 0, 1);
+
+const bigInt64Array = new BigInt64Array(4);
+// @ts-expect-error typed array inputs do not accept byteOffset and length
+new BigInt64Array(bigInt64Array, 0, 1);
