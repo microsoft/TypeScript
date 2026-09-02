01644355ccc6cad4e62705501c040659de26ceb9
Cameron <cclark@cloudflare.com>
2026-09-02 08:30:13 +0100

    Keep the triple-slash comment emit coverage

---
+++ tsc/testdata/baselines/reference/compiler/doNotEmitTripleSlashCommentsInEmptyFile.js
@@ -0,0 +1,17 @@
+//// [tests/cases/compiler/doNotEmitTripleSlashCommentsInEmptyFile.ts] ////
+
+//// [file0.ts]
+
+//// [file1.ts]
+
+//// [file2.ts]
+/// <reference path="file0.ts" />
+/// <reference path="file1.ts" />
+
+
+//// [file0.js]
+"use strict";
+//// [file1.js]
+"use strict";
+//// [file2.js]
+"use strict";
---
+++ tsc/testdata/tests/cases/compiler/doNotEmitTripleSlashCommentsInEmptyFile.ts
@@ -0,0 +1,10 @@
+﻿// @target: es2015
+// @removeComments: true
+
+// @Filename: file0.ts
+
+// @Filename: file1.ts
+
+// @Filename: file2.ts
+/// <reference path="file0.ts" />
+/// <reference path="file1.ts" />
