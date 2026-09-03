6ed3c968d8f71807d2897732e42b572541d68e08
luo2430 <luo2430@qq.com>
2026-09-03 21:13:21 +0800

    Fix completions inside tuple types suggesting value symbols

---
+++ tsc/internal/fourslash/tests/completionsInEmptyTupleType_test.go
@@ -0,0 +1,36 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/microsoft/TypeScript/tsc/internal/fourslash"
+	. "github.com/microsoft/TypeScript/tsc/internal/fourslash/tests/util"
+	"github.com/microsoft/TypeScript/tsc/internal/testutil"
+)
+
+func TestCompletionsInEmptyTupleType(t *testing.T) {
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `type UserTuple = [["name", string], ["age", number], ["address", string]];
+type AdminTuple = [/*1*/];
+type OtherTuple = [string, /*2*/];
+
+const User: UserTuple = [["name", "2333"], ["age", 2333], ["address", "2333"]];`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.VerifyCompletions(t, []string{"1", "2"}, &fourslash.CompletionsExpectedList{
+		IsIncomplete: false,
+		ItemDefaults: &fourslash.CompletionsExpectedItemDefaults{
+			CommitCharacters: &DefaultCommitCharacters,
+			EditRange:        Ignored,
+		},
+		Items: &fourslash.CompletionsExpectedItems{
+			Includes: []fourslash.CompletionsExpectedItem{
+				"UserTuple",
+			},
+			Excludes: []string{
+				"User",
+			},
+		},
+	})
+}
