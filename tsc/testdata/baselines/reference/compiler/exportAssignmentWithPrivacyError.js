0e32aa196a8f1814fb6a4fa294e2f49029d6de88
Cameron <cclark@cloudflare.com>
2026-09-03 18:35:06 +0000

    chore: remove `outFile`,`module:amd` config from test cases (#64122)

---
+++ tsc/testdata/baselines/reference/compiler/exportAssignmentWithPrivacyError.js
@@ -0,0 +1,25 @@
+//// [tests/cases/compiler/exportAssignmentWithPrivacyError.ts] ////
+
+//// [exportAssignmentWithPrivacyError.ts]
+interface connectmodule {
+    (res, req, next): void;
+}
+interface connectexport {
+    use: (mod: connectmodule) => connectexport;
+    listen: (port: number) => void;
+}
+
+var server: {
+    (): connectexport;
+    test1: connectmodule;
+    test2(): connectmodule;
+};
+
+export = server;
+
+
+
+//// [exportAssignmentWithPrivacyError.js]
+"use strict";
+var server;
+module.exports = server;
---
+++ tsc/testdata/baselines/reference/conformance/jsdocNeverUndefinedNull.js
@@ -0,0 +1,23 @@
+//// [tests/cases/conformance/jsdoc/jsdocNeverUndefinedNull.ts] ////
+
+//// [in.js]
+/**
+ * @param {never} p1
+ * @param {undefined} p2
+ * @param {null} p3
+ * @returns {void} nothing
+ */
+function f(p1, p2, p3) {
+}
+
+
+//// [in.js]
+"use strict";
+/**
+ * @param {never} p1
+ * @param {undefined} p2
+ * @param {null} p3
+ * @returns {void} nothing
+ */
+function f(p1, p2, p3) {
+}
