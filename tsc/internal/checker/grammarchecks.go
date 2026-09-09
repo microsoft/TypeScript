84bed3788d1a97cc3d3ccfaf6c3b2c075138d509
vaibhavsrv <srvvaibhav8@gmail.com>
2026-09-09 10:15:07 +0530

    checker: emit distinct diagnostic for import() in optional chains

--- tsc/internal/checker/grammarchecks.go
+++ tsc/internal/checker/grammarchecks.go
@@ -2168,8 +2168,11 @@ func (c *Checker) checkGrammarImportCallExpression(node *ast.Node) bool {
 	}
 
 	nodeAsCall := node.AsCallExpression()
-	if nodeAsCall.TypeArguments != nil || nodeAsCall.QuestionDotToken != nil {
+	if nodeAsCall.TypeArguments != nil {
 		return c.grammarErrorOnNode(node, diagnostics.This_use_of_import_is_invalid_import_calls_can_be_written_but_they_must_have_parentheses_and_cannot_have_type_arguments)
+	}
+	if nodeAsCall.QuestionDotToken != nil {
+		return c.grammarErrorOnNode(node, diagnostics.Calls_to_import_are_not_permitted_in_an_optional_chain)
 	}
 
 	nodeArguments := nodeAsCall.Arguments
