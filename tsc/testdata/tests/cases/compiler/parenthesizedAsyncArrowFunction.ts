3ffd9951afe83425fb8af3c19fe9f904cd1e0fc0
Oleksandr Tarasiuk <oleksandr.tarasiuk@outlook.com>
2026-09-10 20:38:14 +0300

    fix(64231): avoid parsing async() calls as typed arrows in conditionals

--- tsc/testdata/tests/cases/compiler/parenthesizedAsyncArrowFunction.ts
+++ tsc/testdata/tests/cases/compiler/parenthesizedAsyncArrowFunction.ts
@@ -3,3 +3,19 @@
 // Repro from #20096
 
 let foo = (async bar => bar);
+
+const a = true;
+const b = 1;
+
+function async(): number {
+    return b;
+}
+
+const f1 = a ? async() : 0;
+const f2 = a ? async() : x => x;
+const f3 = a ? a ? async() : 0 : b;
+const f4 = a ? x => async() : x => x;
+const f5 = a ? async (): Promise<number> => b : () => b;
+const f6 = a ? async (): Promise<number> => { return b; } : () => b;
+const f7 = a ? async (): Promise<number> => a ? async() : 0 : () => b;
+const f8 = a ? (): number => b : () => b;
