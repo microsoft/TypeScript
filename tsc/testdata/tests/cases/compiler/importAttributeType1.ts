0afbd7266c8d166cd9d35dff3a9aabb25c53ae02
Cameron Clark <cameron.clark@hey.com>
2026-09-11 11:36:33 +0100

    fix: disallow empty template literals in module import attribute types

--- tsc/testdata/tests/cases/compiler/importAttributeType1.ts
+++ tsc/testdata/tests/cases/compiler/importAttributeType1.ts
@@ -34,6 +34,7 @@ data.version.toFixed();
 // @filename: /checkErrors.d.ts
 declare module "*.numberValue" with { type: number } {}
 declare module "*.objectValue" with { type: { name: string } } {}
+declare module "*.css" with { type: `css` } {}
 declare module "*" with { type: "md" | "markdown" } {}
 // @filename: /augmentation.ts
 export {};
