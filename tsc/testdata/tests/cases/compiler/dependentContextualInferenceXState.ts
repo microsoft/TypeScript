14db142fe24280e60e3c9edd60cb40370a5fd8b7
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2026-09-12 18:09:51 +0530

    add a test for cyclic machine

--- tsc/testdata/tests/cases/compiler/dependentContextualInferenceXState.ts
+++ tsc/testdata/tests/cases/compiler/dependentContextualInferenceXState.ts
@@ -6,7 +6,7 @@ declare const runMachine:
     },
     transitions: {
       [TState in keyof TDefinition["transitions"]]:
-        (current: { context: Context<TDefinition, TState> }) =>
+        (current: { context: MapNeverToAny<Context<TDefinition, TState>> }) =>
           | void
           | { state: keyof TDefinition["transitions"], context: object }
     }
@@ -20,7 +20,9 @@ type Context<TDefinition extends Definition, TState> =
         ReturnType<TDefinition["transitions"][S]> extends infer R
           ? R extends unknown
               ? R extends { state: TState, context: infer C }
-                  ? C
+                  ? IsAny<C> extends true ? never : C
+                    // if it's any it usually means the transition hasn't got it's parameter types yet so we ignore it
+                    // this is needed to make the `cyclicResult` case work
                   : never
               : never
           : never
@@ -32,6 +34,12 @@ type Context<TDefinition extends Definition, TState> =
 // just to satisfy the type checker
 type Definition =
   { initial: { state: keyof any, context: object }, transitions: Record<keyof any, (...a: never) => unknown> }
+
+type MapNeverToAny<T> =
+  [T] extends [never] ? any : T
+
+type IsAny<T> =
+  0 extends (1 & T) ? true : false
 
 const result = runMachine({
   initial: {
@@ -51,6 +59,21 @@ const result = runMachine({
     },
     loggedIn: ({ context }) => {
       console.log(context.accessToken)
+    }
+  }
+})
+
+const cyclicResult = runMachine({
+  initial: {
+    state: "a",
+    context: {}
+  },
+  transitions: {
+    a: ({ context }) => {
+      return { state: "b", context: { ...context, a: ""} }
+    },
+    b: ({ context }) => {
+      return { state: "a", context:  { ...context, b: "" } }
     }
   }
 })
