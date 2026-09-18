c975de5011fb7dfb32a491cf3fcf02d4f811f50e
Cameron <cameron.clark@hey.com>
2026-09-18 03:00:37 +0000

    perf: preallocate instantiated symbol table (#64306)

--- tsc/internal/checker/checker.go
+++ tsc/internal/checker/checker.go
@@ -21062,7 +21062,7 @@ func (c *Checker) createInstantiatedSymbolTable(symbols []*ast.Symbol, m *TypeMa
 	if len(symbols) == 0 {
 		return nil
 	}
-	result := make(ast.SymbolTable)
+	result := make(ast.SymbolTable, len(symbols))
 	for _, symbol := range symbols {
 		result[symbol.Name] = c.instantiateSymbol(symbol, m)
 	}
