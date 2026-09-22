abc21937529a6d12fee1990c1d64d6187bd97a1f
Andrew Branch <andrew@wheream.io>
2026-09-22 13:07:39 -0700

    Don’t round-trip snapshot API request’s `OpenFiles` and `EnsureFiles` through lossy URI conversion

--- tsc/internal/api/session.go
+++ tsc/internal/api/session.go
@@ -1257,15 +1257,16 @@ func (s *Session) toAPISnapshotRequest(changes *SnapshotRequestChangesParams) (*
 	}
 
 	for _, f := range changes.OpenFiles {
-		uri := f.ToURI(s.currentDirectory())
-		if apiRequest.EnsureFiles == nil {
-			apiRequest.EnsureFiles = collections.NewSetWithSizeHint[lsproto.DocumentUri](len(changes.OpenFiles))
-		}
-		apiRequest.EnsureFiles.Add(uri)
+		fileName := f.ToAbsoluteFileName(s.currentDirectory())
+		path := s.toPath(fileName)
 		if apiRequest.OpenFiles == nil {
-			apiRequest.OpenFiles = collections.NewSetWithSizeHint[lsproto.DocumentUri](len(changes.OpenFiles))
+			apiRequest.OpenFiles = make(map[tspath.Path]string, len(changes.OpenFiles))
+			apiRequest.EnsureFiles = make(map[tspath.Path]string, len(changes.OpenFiles))
 		}
-		apiRequest.OpenFiles.Add(uri)
+		if _, ok := apiRequest.OpenFiles[path]; !ok {
+			apiRequest.OpenFiles[path] = fileName
+			apiRequest.EnsureFiles[path] = fileName
+		}
 	}
 
 	for _, f := range changes.CloseFiles {
@@ -1423,10 +1424,9 @@ func (s *Session) reconcileSnapshotOpens(apiRequest *project.APISnapshotRequest,
 			apiRequest.CloseFiles.Delete(path)
 		}
 	}
-	for uri := range apiRequest.OpenFiles.Keys() {
-		path := s.toPath(uri.FileName())
+	for path := range apiRequest.OpenFiles {
 		if state.openFiles.Has(path) {
-			apiRequest.OpenFiles.Delete(uri)
+			delete(apiRequest.OpenFiles, path)
 		} else {
 			state.openFiles.Add(path)
 		}
