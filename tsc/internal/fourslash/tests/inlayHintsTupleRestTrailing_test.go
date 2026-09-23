96b101abae563470ef850941832c33c04ad49dc3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-09-23 11:42:14 -0700

    Test inlay hints with multiple variadic tuple elements

--- tsc/internal/fourslash/tests/inlayHintsTupleRestTrailing_test.go
+++ tsc/internal/fourslash/tests/inlayHintsTupleRestTrailing_test.go
@@ -19,9 +19,14 @@ onlyTrailing(1, 2, "end");
 onlyTrailing("end");
 declare function noTrailing(...args: [first: number, ...rest: string[]]): void;
 noTrailing(1, "a", "b");
+declare function multi<T extends string[], U extends string[]>(...args: [first: number, ...left: T, ...right: U, penultimate: string, last: boolean]): void;
+function combined<T extends string[], U extends string[]>(middle: [...T, ...U]) {
+    multi<T, U>(1, ...middle, "end", true);
+}
 declare function skip({ x }: { x: number }, next: number): void;
 skip({ x: 1 }, 2);`
 	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
 	defer done()
+	f.VerifyNoErrors(t)
 	f.VerifyBaselineInlayHints(t, nil /*span*/, &lsutil.UserPreferences{InlayHints: lsutil.InlayHintsPreferences{IncludeInlayParameterNameHints: lsutil.IncludeInlayParameterNameHintsAll}})
 }
