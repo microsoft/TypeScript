3f6f1b8dec587ea3e20f1657e889eb667fb98de2
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-09-23 15:12:06 -0700

    Test ambiguous tuple spread parameter hints

--- tsc/internal/fourslash/tests/inlayHintsRestTrailingSpreadEdgeCases_test.go
+++ tsc/internal/fourslash/tests/inlayHintsRestTrailingSpreadEdgeCases_test.go
@@ -33,3 +33,28 @@ f(...xs, "middle", "end");`
 	f.VerifyNoErrors(t)
 	f.VerifyBaselineInlayHints(t, nil /*span*/, &lsutil.UserPreferences{InlayHints: lsutil.InlayHintsPreferences{IncludeInlayParameterNameHints: lsutil.IncludeInlayParameterNameHintsAll}})
 }
+
+func TestInlayHintsOptionalTrailingAfterTupleSpread(t *testing.T) {
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `declare function optional<T extends unknown[]>(...args: [...head: T, first?: number, second?: string]): void;
+function forward<T extends unknown[]>(x: [...T]) {
+    optional<T>(...x, 1);
+}`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.VerifyNoErrors(t)
+	f.VerifyBaselineInlayHints(t, nil /*span*/, &lsutil.UserPreferences{InlayHints: lsutil.InlayHintsPreferences{IncludeInlayParameterNameHints: lsutil.IncludeInlayParameterNameHintsAll}})
+}
+
+func TestInlayHintsOptionalParameterAfterTupleSpread(t *testing.T) {
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `declare function g(a: number, b?: number, ...rest: string[]): void;
+declare const x: [number, number?];
+g(...x, "end");`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.VerifyNoErrors(t)
+	f.VerifyBaselineInlayHints(t, nil /*span*/, &lsutil.UserPreferences{InlayHints: lsutil.InlayHintsPreferences{IncludeInlayParameterNameHints: lsutil.IncludeInlayParameterNameHintsAll}})
+}
